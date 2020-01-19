143a9cc37fb84fd0bf3e6e9db037c7a559edd4d5
Valentin Ignatev <valentignatev@gmail.com>
2020-01-19 20:02:39 +0300

    gnu: Add rust-stb-truetype-0.3.
    * gnu/packages/crates-io.scm (rust-stb-truetype-0.3): New variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -12990,6 +12990,37 @@ are met.")
     (properties '((hidden? . #t)))
     (license (list license:expat license:asl2.0))))
 
+(define-public rust-stb-truetype-0.3
+  (package
+    (name "rust-stb-truetype")
+    (version "0.3.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "stb_truetype" version))
+       (file-name
+        (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32
+         "0lgvnh3ma6cz811bk8imj45djz76zs47b8327sgnmik2x03nnyzp"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs
+       (("rust-byteorder" ,rust-byteorder-1.3)
+        ("rust-libm" ,rust-libm-0.2))
+       #:cargo-development-inputs
+       (("rust-approx" ,rust-approx-0.3))))
+    (home-page
+     "https://gitlab.redox-os.org/redox-os/stb_truetype-rs")
+    (synopsis
+     "A straight translation of the font loading code in stb_truetype.h
+from C to Rust")
+    (description
+     "This package provides a straight translation of the font loading code
+in stb_truetype.h from C to Rust.")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-stdweb-0.4
   (package
     (name "rust-stdweb")
