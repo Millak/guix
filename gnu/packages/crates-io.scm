7e6ee162b14270f749052dd73be3daf46b8a6baf
Valentin Ignatev <valentignatev@gmail.com>
2020-01-26 01:46:39 +0300

    gnu: Add rust-obj-0.9.
    * gnu/packages/crates-io.scm (rust-obj-0.9): New variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -9363,6 +9363,30 @@ including bigint,complex, rational, range iterators, generic integers, and more!
     (properties '((hidden? . #t)))
     (license (list license:expat license:asl2.0))))
 
+(define-public rust-obj-0.9
+  (package
+    (name "rust-obj")
+    (version "0.9.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "obj" version))
+       (file-name
+        (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32
+         "10z1r2r0xyhr4j1n07135kz4bc0zhqy98vabs99vz0x171bi8gy0"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs (("rust-genmesh" ,rust-genmesh-0.6))))
+    (home-page "https://github.com/kvark/obj")
+    (synopsis
+     "A package for loading Wavefront .obj files")
+    (description
+     "This package provides a package for loading Wavefront .obj files")
+    (license license:asl2.0)))
+
 (define-public rust-objc-0.2
   (package
     (name "rust-objc")
