099ca0e0b52bbb13b5e91a3526ae9285c49efeeb
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2021-03-11 17:34:33 -0500

    gnu: libnotify: Propagate gdk-pixbuf+svg rather than gdk-pixbuf.
    Propagating gdk-pixbuf rather than gdk-pixbuf+svg is dangerous as it clashes
    with GTK+'s own gdk-pixbuf+svg propagation.
    
    * gnu/packages/gnome.scm (libnotify)[propagated-inputs]: Replace gdk-pixbuf by
    gdk-pixbuf+svg.

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -3042,7 +3042,7 @@ configuring CUPS.")
              #t)))))
     (propagated-inputs
      `(;; In Requires of libnotify.pc.
-       ("gdk-pixbuf" ,gdk-pixbuf)
+       ("gdk-pixbuf" ,gdk-pixbuf+svg)
        ("glib" ,glib)))
     (inputs
      `(("gtk+" ,gtk+)
