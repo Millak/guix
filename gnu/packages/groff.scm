d5e8732a149b5d07d5bda79993155d9fb570ac71
Maxime Devos <maximedevos@telenet.be>
2021-07-02 12:13:07 +0200

    gnu: groff: Add 'bash' input for 'wrap-program'.
    It is required for cross-compilation.
    
    * gnu/packages/groff.scm (roffit): Remove trailing #t
    [native-inputs]: Remove labels.
    [inputs]: Add 'bash-minimal'.
    (ronn-ng)[inputs]: Add 'bash-minimal'.
    
    Signed-off-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>
    Modified-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>
    Change-Id: I6ffaf53d1389c49b96c964447989b9db45d6b509

--- gnu/packages/groff.scm
+++ gnu/packages/groff.scm
@@ -34,6 +34,7 @@
   #:use-module (guix build-system ruby)
   #:use-module (guix gexp)
   #:use-module (gnu packages)
+  #:use-module (gnu packages bash)
   #:use-module (gnu packages ruby)
   #:use-module (gnu packages bison)
   #:use-module (gnu packages flex)
@@ -199,17 +200,15 @@ is usually the formatter of \"man\" documentation pages.")
              (lambda* (#:key outputs #:allow-other-keys)
                (let ((out (assoc-ref outputs "out")))
                  (install-file "roffit" (string-append out "/bin"))
-                 (install-file "roffit.1" (string-append out "/share/man/man1"))
-                 #t)))
+                 (install-file "roffit.1"
+                               (string-append out "/share/man/man1")))))
            (add-after 'install 'wrap-program
              (lambda* (#:key outputs #:allow-other-keys)
                (let ((out (assoc-ref outputs "out")))
                  (wrap-program (string-append out "/bin/roffit")
-                   `("PERL5LIB" ":" prefix (,(getenv "PERL5LIB"))))
-                 #t))))))
-      (native-inputs `(("html-tree" ,perl-html-tree))) ; for test
-      (inputs
-       (list perl))
+                   `("PERL5LIB" ":" prefix (,(getenv "PERL5LIB"))))))))))
+      (native-inputs (list perl-html-tree)) ; for test
+      (inputs (list bash-minimal perl))
       (home-page "https://daniel.haxx.se/projects/roffit/")
       (synopsis "Convert nroff files to HTML")
       (description
@@ -258,7 +257,7 @@ It is typically used to display man pages on a web site.")
                                           "ruby-mustache"
                                           "ruby-nokogiri"))))))))))
     (inputs
-     (list ruby-kramdown ruby-mustache ruby-nokogiri))
+     (list bash-minimal ruby-kramdown ruby-mustache ruby-nokogiri))
     (synopsis
      "Build manuals in HTML and Unix man page format from Markdown")
     (description
