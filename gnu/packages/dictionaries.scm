b7ff2afdfffc21a9c367642fa7da6c0cbb1c74b4
Maxime Devos <maximedevos@telenet.be>
2021-07-02 12:07:00 +0200

    gnu: translate-shell: Add 'bash' input for 'wrap-program'.
    It is required for cross-compilation.
    
    * gnu/packages/dictionaries.scm
    (translate-shell): Fix indentation.
    [inputs]: Add 'bash-minimal'.
    [native-inputs]: Remove labels.
    
    Signed-off-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>
    Modified-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>
    Change-Id: I3fcca53b2645f5eb15be7eb9b91217525a2e4e3a

--- gnu/packages/dictionaries.scm
+++ gnu/packages/dictionaries.scm
@@ -38,6 +38,7 @@
   #:use-module (gnu packages)
   #:use-module (gnu packages autotools)
   #:use-module (gnu packages base)
+  #:use-module (gnu packages bash)
   #:use-module (gnu packages curl)
   #:use-module (gnu packages emacs)
   #:use-module (gnu packages flex)
@@ -242,14 +243,14 @@ and a Python library.")
     (name "translate-shell")
     (version "0.9.7.1")
     (source
-      (origin
-        (method git-fetch)
-        (uri (git-reference
-               (url"https://github.com/soimort/translate-shell")
-               (commit (string-append "v" version))))
-        (file-name (git-file-name name version))
-        (sha256
-         (base32 "0jfrypcz963pfvwwaz2i0xvwp2909ldzp15v68mgd2mbqkqw9d90"))))
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url"https://github.com/soimort/translate-shell")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0jfrypcz963pfvwwaz2i0xvwp2909ldzp15v68mgd2mbqkqw9d90"))))
     (build-system gnu-build-system)
     (arguments
      `(#:phases
@@ -281,17 +282,17 @@ and a Python library.")
        #:make-flags (list (string-append "PREFIX=" %output)
                           "NETWORK_ACCESS=no test")
        #:imported-modules (,@%default-gnu-imported-modules
-                            (guix build emacs-build-system)
-                            (guix build emacs-utils))
+                           (guix build emacs-build-system)
+                           (guix build emacs-utils))
        #:modules ((guix build gnu-build-system)
                   ((guix build emacs-build-system) #:prefix emacs:)
                   (guix build utils))
        #:test-target "test"))
     (inputs
-     (list curl fribidi rlwrap))
+     (list bash-minimal curl fribidi rlwrap))
     (native-inputs
-     `(("emacs" ,emacs-minimal)
-       ("util-linux" ,util-linux)))     ; hexdump, for the test
+     (list emacs-minimal
+           util-linux))                 ; hexdump, for the test
     (home-page "https://www.soimort.org/translate-shell/")
     (synopsis "Translations from the command line")
     (description
