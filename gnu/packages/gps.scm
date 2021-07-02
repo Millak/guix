aea782866980a5755bba45459c92c97d5ffbd8fd
Maxime Devos <maximedevos@telenet.be>
2021-07-02 12:13:04 +0200

    gnu: gpsd: Add 'bash' input for 'wrap-program'.
    It is required for cross-compilation.
    
    * gnu/packages/gpsd.scm (gpsd)[inputs]: Add 'bash-minimal'.
    
    Signed-off-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>
    Modified-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>
    Change-Id: I005ee0b9d223717219463fbb01e8cbfe97a90b80

--- gnu/packages/gps.scm
+++ gnu/packages/gps.scm
@@ -38,6 +38,7 @@
   #:use-module (gnu packages)
   #:use-module (gnu packages algebra)
   #:use-module (gnu packages base)
+  #:use-module (gnu packages bash)
   #:use-module (gnu packages build-tools)
   #:use-module (gnu packages compression)
   #:use-module (gnu packages docbook)
@@ -239,7 +240,8 @@ such as elevation, speed, heart rate, power, temperature, and gear shifts.")
     (native-inputs
      (list bc pkg-config))
     (inputs
-     (list bluez
+     (list bash-minimal
+           bluez
            dbus
            gtk+
            libcap
