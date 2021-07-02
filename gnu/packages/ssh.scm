ec292f38958c4be8b518ab37ac0c67f9ed51af18
Maxime Devos <maximedevos@telenet.be>
2021-07-02 12:07:15 +0200

    gnu: mosh: Add 'bash' input for 'wrap-program'.
    It is required for cross-compilation.
    
    * gnu/packages/ssh.scm (mosh)[inputs]: Add 'bash-minimal'.
    
    Signed-off-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>
    Change-Id: I2d03972a6acf31e63e2629356eb862445eecf1aa

--- gnu/packages/ssh.scm
+++ gnu/packages/ssh.scm
@@ -41,6 +41,7 @@
   #:use-module (gnu packages admin)
   #:use-module (gnu packages autotools)
   #:use-module (gnu packages base)
+  #:use-module (gnu packages bash)
   #:use-module (gnu packages boost)
   #:use-module (gnu packages check)
   #:use-module (gnu packages compression)
@@ -491,7 +492,8 @@ with optional @acronym{TLS, Transport-Level Security} to protect credentials.")
     (native-inputs
      (list pkg-config))
     (inputs
-     (list boost
+     (list bash-minimal
+           boost
            ncurses
            openssl
            perl
