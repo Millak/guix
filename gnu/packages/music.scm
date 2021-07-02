d0a6ae50b4d31e3b5c4dab972adbcd521a1baaba
Maxime Devos <maximedevos@telenet.be>
2021-07-02 12:06:44 +0200

    gnu: music: Add 'bash' input for 'wrap-program'.
    It is required for cross-compilation.
    
    * gnu/packages/music.scm
    (clementine): Remove trailing #t, input labels.
    [inputs]: Add 'bash-minimal'.
    (strawberry)[inputs]: Likewise.
    (solfege)[inputs]: Likewise.
    (demlo)[inputs]: Likewise.
    (fmit)[inputs]: Likewise.
    (pragha)[inputs]: Likewise.
    
    Signed-off-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>
    Modified-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>
    Change-Id: I73ceb7c22d20328412a01e88ac8c35c77168d9c3

--- gnu/packages/music.scm
+++ gnu/packages/music.scm
@@ -525,8 +525,7 @@ enables iPod support in music players such as Clementine.")
                    (find-files "3rdparty"
                                (lambda (file stat)
                                  (string-match "^3rdparty/[^/]*$" file))
-                               #:directories? #t))
-                  #t))))
+                               #:directories? #t))))))
     (build-system cmake-build-system)
     (arguments
      '(#:test-target "clementine_test"
@@ -548,15 +547,16 @@ enables iPod support in music players such as Clementine.")
              (let ((out             (assoc-ref outputs "out"))
                    (gst-plugin-path (getenv "GST_PLUGIN_SYSTEM_PATH")))
                (wrap-program (string-append out "/bin/clementine")
-                 `("GST_PLUGIN_SYSTEM_PATH" ":" prefix (,gst-plugin-path)))
-               #t))))))
+                 `("GST_PLUGIN_SYSTEM_PATH" ":" prefix
+                   (,gst-plugin-path)))))))))
     (native-inputs
-     `(("gettext" ,gettext-minimal)
-       ("googletest" ,googletest)
-       ("pkg-config" ,pkg-config)
-       ("qtlinguist" ,qttools-5)))
+     (list gettext-minimal
+           googletest
+           pkg-config
+           qttools-5))
     (inputs
-     (list boost
+     (list bash-minimal
+           boost
            chromaprint
            fftw
            glib
@@ -776,7 +776,8 @@ Winamp/XMMS skins.")
              (setenv "DISPLAY" ":1")
              (setenv "HOME" (getcwd)))))))
     (native-inputs
-     (list gettext-minimal
+     (list bash-minimal
+           gettext-minimal
            googletest
            pkg-config
            qttools
@@ -2286,7 +2287,8 @@ for path in [path for path in sys.path if 'site-packages' in path]: site.addsite
                 (wrap-program (search-input-file outputs "bin/solfege")
                   `("GUIX_PYTHONPATH" ":" prefix (,path)))))))))
     (inputs
-     (list python-wrapper
+     (list bash-minimal
+           python-wrapper
            python-pygobject
            gettext-minimal
            gtk+
@@ -6133,7 +6135,7 @@ console music players.")
              go-github-com-wtolson-go-taglib
              go-github-com-yookoala-realpath))
       (inputs
-       (list chromaprint ffmpeg))
+       (list bash-minimal chromaprint ffmpeg))
       (arguments
        `(#:go ,go-1.17
          #:import-path "gitlab.com/ambrevar/demlo"
@@ -6150,8 +6152,7 @@ console music players.")
                      ,(map (lambda (dir)
                              (string-append dir "/bin:"
                                             dir "/sbin"))
-                           (list ffmpeg chromaprint))))
-                 #t)))
+                           (list ffmpeg chromaprint)))))))
            (add-after 'install 'install-scripts
              (lambda* (#:key outputs #:allow-other-keys)
                (let* ((out (assoc-ref outputs "out"))
@@ -6164,8 +6165,8 @@ console music players.")
                  (install-file (string-append root "/config.lua") xdg-data-dirs)
                  ;; TODO: Test fish completion.
                  (install-file (string-append root "/completion/demlo.fish")
-                               (string-append out "/share/fish/vendor_completions.d"))
-                 #t))))))
+                               (string-append
+                                out "/share/fish/vendor_completions.d"))))))))
       (home-page "https://gitlab.com/ambrevar/demlo")
       (synopsis "Dynamic and extensible music library organizer")
       (description "Demlo is a music library organizer.  It can encode, fix
@@ -6230,6 +6231,7 @@ discard bad quality ones.
                          '("qtmultimedia"))))))))))
     (inputs
      (list alsa-lib
+           bash-minimal
            fftw
            jack-1
            portaudio
@@ -6262,7 +6264,8 @@ with error and volume history, and advanced features.")
     (native-inputs
      (list intltool pkg-config))
     (inputs
-     (list glib
+     (list bash-minimal
+           glib
            grilo
            gstreamer
            gst-plugins-base
@@ -6285,8 +6288,8 @@ with error and volume history, and advanced features.")
              (let ((out (assoc-ref outputs "out"))
                    (gst-plugin-path (getenv "GST_PLUGIN_SYSTEM_PATH")))
                (wrap-program (string-append out "/bin/pragha")
-                 `("GST_PLUGIN_SYSTEM_PATH" ":" prefix (,gst-plugin-path)))
-               #t))))))
+                 `("GST_PLUGIN_SYSTEM_PATH" ":" prefix
+                   (,gst-plugin-path)))))))))
     (home-page "https://pragha-music-player.github.io")
     (synopsis "Music player")
     (description "Pragha is a lightweight music player based on Gtk and
