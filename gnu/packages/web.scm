31ca01e7324b8efce6fc698fddc5405b6f39d13b
Maxime Devos <maximedevos@telenet.be>
2021-07-02 12:06:49 +0200

    gnu: web: Add 'bash' input for 'wrap-program'.
    It is required for cross-compilation.
    
    * gnu/packages/web.scm
    (krona-tools)[inputs]: Add 'bash-minimal'.
    (guix-data-service)[inputs]: Likewise.
    (ikiwiki): Delete trailing #t.
    [inputs]: Likewise.
    (hiawatha)[inputs]: Likewise.
    (hpcguix-web)[inputs]: Likewise.
    
    Signed-off-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>
    Modified-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>
    Change-Id: I3fa7dbf05a72bc41a089ba62160dbe1fb82ec7e1

--- gnu/packages/web.scm
+++ gnu/packages/web.scm
@@ -1557,7 +1557,7 @@ efficiently.  It gives the application developer no more than 4 methods.")
                (invoke (string-append (assoc-ref outputs "out") "/bin/ktImportText")
                        "ec.tsv")))))))
    (inputs
-    (list curl gnu-make perl))
+    (list bash-minimal curl gnu-make perl))
    (home-page "https://github.com/marbl/Krona/wiki")
    (synopsis "Hierarchical data exploration with zoomable HTML5 pie charts")
    (description
@@ -5266,7 +5266,8 @@ Cloud.")
                               (_ #t)))))))
             (delete 'strip))))          ; As the .go files aren't compatible
       (inputs
-       (list ephemeralpg
+       (list bash-minimal
+             ephemeralpg
              util-linux
              postgresql-13
              sqitch
@@ -5746,8 +5747,7 @@ NetSurf project.")
         '(begin
            ;; The POT file requires write permission during the build
            ;; phase.
-           (chmod "po/ikiwiki.pot" #o644)
-           #t))))
+           (chmod "po/ikiwiki.pot" #o644)))))
     (build-system perl-build-system)
     (arguments
      `(#:phases
@@ -5758,16 +5758,14 @@ NetSurf project.")
                           (("SYSCONFDIR\\?=") "SYSCONFDIR?=$(PREFIX)"))
              (with-directory-excursion "po"
                (substitute* "Makefile"
-                            (("PERL5LIB=") "PERL5LIB=${PERL5LIB}:")))
-             #t))
+                            (("PERL5LIB=") "PERL5LIB=${PERL5LIB}:")))))
          (add-before 'build 'set-modification-times
            ;; The wiki '--refresh' steps, which are executed during
            ;; the check phase, require recent timestamps on files in
            ;; the 'doc' and 'underlays' directories.
            (lambda _
              (invoke "find"  "doc" "underlays" "-type" "f" "-exec"
-                     "touch" "{}" "+")
-             #t))
+                     "touch" "{}" "+")))
          (add-before 'check 'pre-check
            (lambda* (#:key inputs #:allow-other-keys)
              ;; Six tests use IPC::Run.  For these tests the PERL5LIB
@@ -5793,8 +5791,7 @@ NetSurf project.")
                      (string-append (assoc-ref inputs "shared-mime-info")
                                     "/share"))
              ;; CC is needed by IkiWiki/Wrapper.pm.
-             (setenv "CC" "gcc")
-             #t))
+             (setenv "CC" "gcc")))
          (add-after 'install 'wrap-programs
            (lambda* (#:key outputs #:allow-other-keys)
              (let* ((out  (assoc-ref outputs "out"))
@@ -5803,8 +5800,7 @@ NetSurf project.")
                (for-each (lambda (file)
                            (wrap-program file
                              `("PERL5LIB" ":" prefix (,path))))
-                         (find-files bin))
-               #t))))))
+                         (find-files bin))))))))
     (native-inputs
      (list which
            gettext-minimal
@@ -5814,7 +5810,8 @@ NetSurf project.")
            cvs
            mercurial))
     (inputs
-     (list python-wrapper
+     (list bash-minimal
+           python-wrapper
            perl-authen-passphrase
            perl-cgi-simple
            perl-db-file
@@ -6783,7 +6780,7 @@ functions of Tidy.")
                  `("PATH" ":" prefix (,mbed)))))))))
     (inputs
      ;; TODO: package "hiawatha-monitor", an optional dependency of "hiawatha".
-     (list libxslt libxml2 mbedtls-for-hiawatha
+     (list bash-minimal libxslt libxml2 mbedtls-for-hiawatha
            `(,nghttp2 "lib") zlib))
     (home-page "https://www.hiawatha-webserver.org")
     (synopsis "Webserver with focus on security")
@@ -8586,7 +8583,8 @@ It does not support server push.")
      (list autoconf automake uglify-js pkg-config
            (lookup-package-native-input guix "guile")))
     (inputs
-     (list (lookup-package-native-input guix "guile")
+     (list bash-minimal
+           (lookup-package-native-input guix "guile")
            guix
            guile-zlib
            guile-commonmark
