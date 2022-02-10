7b676317b3e49fe0bd2a44741d8d383f5c85d76a
Ricardo Wurmus <rekado@elephly.net>
2022-02-10 16:31:32 +0100

    gnu: Add python-pytorch-for-r-torch.
    * gnu/packages/patches/python-pytorch-1.9.0-system-libraries.patch: New file.
    * gnu/local.mk (dist_patch_DATA): Add it.
    * gnu/packages/machine-learning.scm (python-pytorch-for-r-torch): New variable.

--- gnu/local.mk
+++ gnu/local.mk
@@ -1741,6 +1741,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/python-pytest-asyncio-python-3.8.patch	\
   %D%/packages/patches/python-pytorch-runpath.patch		\
   %D%/packages/patches/python-pytorch-system-libraries.patch	\
+  %D%/packages/patches/python-pytorch-1.9.0-system-libraries.patch \
   %D%/packages/patches/python-robotframework-source-date-epoch.patch \
   %D%/packages/patches/python-seaborn-kde-test.patch		\
   %D%/packages/patches/python-seaborn-2690.patch		\
