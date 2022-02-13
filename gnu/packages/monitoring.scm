ae0ba592ba6f6cf19ebd70a2cd5074a31afa3d70
Marius Bakke <marius@gnu.org>
2022-02-13 09:37:43 +0100

    gnu: zabbix-agentd: Use the correct license.
    * gnu/packages/monitoring.scm (zabbix-agentd)[license]: Change from GPL2 to GPL2+.

--- gnu/packages/monitoring.scm
+++ gnu/packages/monitoring.scm
@@ -195,7 +195,7 @@ etc. via a Web interface.  Features include:
     (synopsis "Distributed monitoring solution (client-side agent)")
     (description "This package provides a distributed monitoring
 solution (client-side agent)")
-    (license license:gpl2)
+    (license license:gpl2+)
     (properties
      '((release-monitoring-url . "https://www.zabbix.com/download_sources")
        (upstream-name . "zabbix")))))
