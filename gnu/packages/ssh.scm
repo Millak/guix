Millak/guix

commit 1cdd6b09c8bc45c1ae5a16f16d5bcf33ab0ceb5e

gnu: lsh: Fix build with GCC 10.
* gnu/packages/ssh.scm (lsh)[arguments]: Add "CFLAGS=-fcommon"
to #:configure-flags.  Don't explicitly return #t from phases.
[source]: Don't explicitly return #t from snippet.

gnu/packages/ssh.scm
@@ -594,8 +594,7 @@ basis for almost any application.")
                     (("localhost") "127.0.0.1"))
 
                   (substitute* "src/testsuite/login-auth-test"
-                    (("/bin/cat") "cat"))
-                  #t))
+                    (("/bin/cat") "cat"))))
               (patches (search-patches "lsh-fix-x11-forwarding.patch"))))
     (build-system gnu-build-system)
     (native-inputs
@@ -629,7 +628,10 @@ basis for almost any application.")
 
                            ;; 'lsh_argp.h' checks HAVE_ARGP_PARSE but nothing
                            ;; defines it.
-                           "CPPFLAGS=-DHAVE_ARGP_PARSE")
+                           "CPPFLAGS=-DHAVE_ARGP_PARSE"
+
+                           ;; Fix the build of lsh@2.1 with GCC 10.
+                           "CFLAGS=-fcommon")
        #:phases
        (modify-phases %standard-phases
          (add-after 'unpack 'disable-failing-tests
@@ -642,8 +644,7 @@ basis for almost any application.")
                (("seed-test \\\\")        ;prevent trailing slash
                 "seed-test")
                (("^\t(lsh|daemon|tcpip|socks|lshg|lcp|rapid7|lshd).*test.*")
-                ""))
-             #t))
+                ""))))
          (add-before 'configure 'pre-configure
            (lambda* (#:key inputs #:allow-other-keys)
              (let* ((nettle    (assoc-ref inputs "nettle"))

commit 33ce3f1c866231a3015411fdce18a3e72649e2f6

gnu: lsh: Build with optimizations.
This is a followup to 1cdd6b09c8bc45c1ae5a16f16d5bcf33ab0ceb5e, which
inadvertently turned off optimizations.

* gnu/packages/ssh.scm (lsh)[arguments]: Add "-O2 -g" next to "-fcommon".

gnu/packages/ssh.scm
@@ -1,5 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2012-2021 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2012-2022 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2013, 2014 Andreas Enge <andreas@enge.fr>
 ;;; Copyright © 2014, 2015, 2016 Mark H Weaver <mhw@netris.org>
 ;;; Copyright © 2015, 2016, 2018, 2019, 2020, 2021, 2022 Efraim Flashner <efraim@flashner.co.il>
@@ -631,7 +631,7 @@ basis for almost any application.")
                            "CPPFLAGS=-DHAVE_ARGP_PARSE"
 
                            ;; Fix the build of lsh@2.1 with GCC 10.
-                           "CFLAGS=-fcommon")
+                           "CFLAGS=-O2 -g -fcommon")
        #:phases
        (modify-phases %standard-phases
          (add-after 'unpack 'disable-failing-tests