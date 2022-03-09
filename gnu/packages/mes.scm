Millak/guix

commit f15ca337bec89f238b9b9ced08c2e02c35629a33

gnu: nyacc: Install all modules.
* gnu/packages/mes.scm (nyacc)[arguments]: Add build phase
'install-system-module.
[propagated-inputs]: Add guile-bytestructures.

gnu/packages/mes.scm
@@ -2,7 +2,7 @@
 ;;; Copyright © 2017, 2018, 2019, 2020, 2021 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
 ;;; Copyright © 2017, 2018 Efraim Flashner <efraim@flashner.co.il>
 ;;; Copyright © 2018 Tobias Geerinckx-Rice <me@tobias.gr>
-;;; Copyright © 2020, 2021 Ricardo Wurmus <rekado@elephly.net>
+;;; Copyright © 2020, 2021, 2022 Ricardo Wurmus <rekado@elephly.net>
 ;;; Copyright © 2021 Xinglu Chen <public@yoctocell.xyz>
 ;;; Copyright © 2021 Arun Isaac <arunisaac@systemreboot.net>
 ;;;
@@ -120,8 +120,13 @@ $prefix/share/guile/site/$GUILE_EFFECTIVE_VERSION\n")))))
            (lambda _
              (substitute* "module/nyacc/lang/c99/parser.scm"
                (("\\(memq \\(car stmt\\) '\\(include include-next\\)\\)")
-                "(memq (car stmt) '(include include-next define))")))))))
+                "(memq (car stmt) '(include include-next define))"))))
+         (add-after 'unpack 'install-system-module
+           (lambda _
+             (substitute* "module/Makefile.in"
+               (("@NYACC_FH_BINS@") "$(NYACC_FH_BINS)")))))))
     (inputs (list guile-3.0))
+    (propagated-inputs (list guile-bytestructures))
     (description
      "@acronym{NYACC, Not Yet Another Compiler Compiler} is set of Guile modules
 for generating parsers and lexical analyzers.  It provides sample parsers,

commit e2ca8119d5ddf240b492cd4af6acdaf4ed3f6632

gnu: nyacc@1.00.2: Fix build.
Fixes a regression introduced in
f15ca337bec89f238b9b9ced08c2e02c35629a33, whereby adding a dependency on
GUILE-BYTESTRUCTURES would cause a build failure of the FFI helper of
nyacc@1.00.2.

* gnu/packages/mes.scm (nyacc-1.00.2)[propagated-inputs]: New field.

gnu/packages/mes.scm
@@ -157,6 +157,11 @@ parsers to allow execution with Guile as extension languages.")))
               (sha256
                (base32
                 "065ksalfllbdrzl12dz9d9dcxrv97wqxblslngsc6kajvnvlyvpk"))))
+
+    ;; XXX: Remove GUILE-BYTESTRUCTURES, an optional dependency needed to
+    ;; build the FFI helper, because it fails to build.
+    (propagated-inputs '())
+
     (inputs (list guile-3.0))))
 
 (define-public mes