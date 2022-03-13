12c9da35389dfba86ae0d863132a6b2c4374205a
Giacomo Leidi <goodoldpaul@autistici.org>
2022-03-13 02:04:14 +0100

    gnu: python-pytest-cov: Update to 3.0.0.
    * gnu/packages/pdf.scm (python-pytest-cov): Update to 3.0.0.
    
    Signed-off-by: Mathieu Othacehe <othacehe@gnu.org>

--- gnu/packages/check.scm
+++ gnu/packages/check.scm
@@ -1150,13 +1150,13 @@ following improvements:
 (define-public python-pytest-cov
   (package
     (name "python-pytest-cov")
-    (version "2.8.1")
+    (version "3.0.0")
     (source
       (origin
         (method url-fetch)
         (uri (pypi-uri "pytest-cov" version))
         (sha256
-         (base32 "0avzlk9p4nc44k7lpx9109dybq71xqnggxb9f4hp0l64pbc44ryc"))))
+         (base32 "0w6lfv8gc1lxmnvsz7mq5z9shxac5zz6s9mwrai108kxc6qzbw77"))))
     (build-system python-build-system)
     (arguments
      `(#:phases
