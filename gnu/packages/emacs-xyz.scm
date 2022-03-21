114af5e286dc2398ff7e51dc17dda2854df59b9c
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2022-03-21 11:17:52 +0100

    gnu: emacs-ebdb: Update to 0.8.13.
    * gnu/packages/emacs-xyz.scm (emacs-ebdb): Update to 0.8.13.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -26953,14 +26953,14 @@ federated microblogging social network.")
 (define-public emacs-ebdb
   (package
     (name "emacs-ebdb")
-    (version "0.8.12")
+    (version "0.8.13")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://elpa.gnu.org/packages/"
                            "ebdb-" version ".tar"))
        (sha256
-        (base32 "1k53crdmaw6lzvprsmpdfvg96ck54bzs4z1d4q9x890anglxq5m6"))))
+        (base32 "0bbv2n01x9zacnrd00krjkra6amn2cbxlpb5rkjcsyg05zmdkr5q"))))
     (build-system emacs-build-system)
     (home-page "https://github.com/girzel/ebdb")
     (synopsis "EIEIO port of BBDB, Emacs's contact-management package")
