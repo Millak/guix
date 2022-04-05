Millak/guix

commit a5c2e7961c21ef8620ce4cfb3569109fbd694e1e

gnu: python-gst: Fix build failure.
Building python-gst fails with:

  Unbound variable: python-version

Therefore, add python-build-system to #:modules so that it's imported, and
give it a prefix to avoid clashing with the gnu-build-system.

Also, simplify the code by using the site-packages function from the
python-build-system module.

* gnu/packages/gstreamer.scm (python-gst)[arguments]: Rework to fix build
failure.

gnu/packages/gstreamer.scm
@@ -431,17 +431,14 @@ compression formats through the use of the libav library.")
                 "0f1d9rvy2qxlymmfzyknnfr5rz1vx69jv17gp7wnamc5s6p7mp2m"))))
     (build-system gnu-build-system)
     (arguments
-     ;; XXX: Factorize python-sitedir with python-build-system.
-     `(#:imported-modules (,@%gnu-build-system-modules
+     `(#:modules ((guix build gnu-build-system)
+                  ((guix build python-build-system) #:prefix python:))
+       #:imported-modules (,@%gnu-build-system-modules
                            (guix build python-build-system))
        #:configure-flags
-       (let* ((python (assoc-ref %build-inputs "python"))
-              (python-version (python-version python))
-              (python-sitedir (string-append
-                               "lib/python" python-version "/site-packages")))
-         (list (string-append
-                "--with-pygi-overrides-dir=" %output "/" python-sitedir
-                "/gi/overrides")))))
+       (list (string-append
+              "--with-pygi-overrides-dir="
+              (python:site-packages %build-inputs %outputs) "gi/overrides"))))
     (native-inputs
      `(("pkg-config" ,pkg-config)
        ("python" ,python)))

commit 27a6e8501f0299d438916c3d11d37f18ecb4f4eb

gnu: python-gst: Fix the concatenation of install path.
Fixes a regression introduced in a5c2e7961c21ef8620ce4cfb3569109fbd694e1e.

* gnu/packages/gstreamer.scm (python-gst):
[arguments]:
<configure-flags>: Add missing leading slash.

Signed-off-by: Ludovic Courtès <ludo@gnu.org>

gnu/packages/gstreamer.scm
@@ -1076,7 +1076,7 @@ given, also pass them to the build system instead of the ones used by PKG."
        #:configure-flags
        (list (string-append
               "-Dpygi-overrides-dir="
-              (python:site-packages %build-inputs %outputs) "gi/overrides"))))
+              (python:site-packages %build-inputs %outputs) "/gi/overrides"))))
     (native-inputs
      (list pkg-config python))
     (propagated-inputs