d5954101fd0a5123067231382b57d8246e929e34
Petr Hodina <phodina@protonmail.com>
2022-04-08 13:24:20 +0200

    gnu: Add plasma-phonebook.
    * gnu/packages/kde-plasma.scm (plasma-phonebook): New variable.
    
    Signed-off-by: Marius Bakke <marius@gnu.org>

--- gnu/packages/kde-plasma.scm
+++ gnu/packages/kde-plasma.scm
@@ -1928,6 +1928,35 @@ PulseAudio.")
      "This package provides a Plasma applet for the Pass password manager.")
     (license license:lgpl2.1+)))
 
+(define-public plasma-phonebook
+  (package
+    (name "plasma-phonebook")
+    (version "0.1")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "mirror://kde/stable/plasma-phonebook/"
+                                  version "/plasma-phonebook-" version
+                                  ".tar.xz"))
+              (sha256
+               (base32
+                "13nnzdzpganlp319sc9dm9w5hsjhw4f3w8rb80q3nd8q6nyrpky8"))))
+    (build-system cmake-build-system)
+    (native-inputs (list extra-cmake-modules pkg-config))
+    (inputs (list kpeople
+                  kirigami
+                  kpeoplevcard
+                  kcoreaddons
+                  kcontacts
+                  qtbase-5
+                  qtdeclarative-5
+                  qtquickcontrols2-5
+                  qtsvg-5))
+    (home-page "https://plasma-mobile.org/")
+    (synopsis "Phonebook for Plasma Mobile devices")
+    (description "This package provides contacts application which allows
+adding, modifying and removing contacts.")
+    (license license:lgpl2.0+)))
+
 (define-public plasma-phone-components
   (package
     (name "plasma-phone-components")
