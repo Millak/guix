badd61bed29dadb148f64f9d21654b513441d057
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2022-04-21 21:57:15 -0400

    gnu: python-sphinx-argparse: Update to 0.3.1.
    * gnu/packages/sphinx.scm (python-sphinx-argparse): Update to 0.3.1.

--- gnu/packages/sphinx.scm
+++ gnu/packages/sphinx.scm
@@ -682,13 +682,13 @@ theme for the Sphinx documentation system.  It's the default theme of Sphinx.")
 (define-public python-sphinx-argparse
   (package
     (name "python-sphinx-argparse")
-    (version "0.2.5")
+    (version "0.3.1")
     (source
      (origin
        (method url-fetch)
        (uri (pypi-uri "sphinx-argparse" version))
        (sha256
-        (base32 "05wc8f5hb3jsg2vh2jf7jsyan8d4i09ifrz2c8fp6f7x1zw9iav0"))))
+        (base32 "07nw68nrbpzsswb5bz8gdb5allgj6jnz8m81afhr9v6c8fyiq5c2"))))
     (build-system python-build-system)
     (propagated-inputs
      (list python-sphinx))
