Millak/guix

commit 2703daf6eb0098ad064a9b6ec64a9ae7b5b35343

gnu: rust-criterion-0.3: Update to 0.3.5.
* gnu/packages/crates-io.scm (rust-criterion-0.3): Update to 0.3.5.
[arguments]<cargo-inputs>: Replace rust-approx-0.4 with 0.5,
rust-quickcheck-0.9 with 1.

gnu/packages/crates-io.scm
@@ -13927,7 +13927,7 @@ Rust.")
 (define-public rust-criterion-0.3
   (package
     (name "rust-criterion")
-    (version "0.3.4")
+    (version "0.3.5")
     (source
      (origin
        (method url-fetch)
@@ -13935,7 +13935,7 @@ Rust.")
        (file-name
         (string-append name "-" version ".tar.gz"))
        (sha256
-        (base32 "08rx789365x6l9kbsg2r9c5yg25rd3pj1giwyhpcqis56pbpwcmb"))))
+        (base32 "044d2x7cxfvw2g558lzyllcv7jcdkw9xmacmb0nzx8pv4pyxl10n"))))
     (build-system cargo-build-system)
     (arguments
      `(#:cargo-inputs
@@ -13962,9 +13962,9 @@ Rust.")
         ("rust-tokio" ,rust-tokio-1)
         ("rust-walkdir" ,rust-walkdir-2))
        #:cargo-development-inputs
-       (("rust-approx" ,rust-approx-0.4)
+       (("rust-approx" ,rust-approx-0.5)
         ("rust-futures" ,rust-futures-0.3)
-        ("rust-quickcheck" ,rust-quickcheck-0.9)
+        ("rust-quickcheck" ,rust-quickcheck-1)
         ("rust-rand" ,rust-rand-0.8)
         ("rust-tempfile" ,rust-tempfile-3))))
     (home-page "https://bheisler.github.io/criterion.rs/book/index.html")

commit 09fcd191e14824dc9569611d6b2d4e423ef5b106

gnu: rust-pyo3: Allow compilation with Criterion 0.3.5.
Fixes a regression introduced in
2703daf6eb0098ad064a9b6ec64a9ae7b5b35343.

* gnu/packages/crates-io.scm (rust-pyo3-0.15)[modules, snippet]: New
fields.

gnu/packages/crates-io.scm
@@ -45716,7 +45716,14 @@ ecosystem.")
        (uri (crate-uri "pyo3" version))
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
-        (base32 "082p014xd8ipwnfsq1ln871wkslxmbrxd7kpqwa0mbq53jzivw3w"))))
+        (base32 "082p014xd8ipwnfsq1ln871wkslxmbrxd7kpqwa0mbq53jzivw3w"))
+       (modules '((guix build utils)))
+       (snippet
+        ;; XXX: Relax "dev-dependencies.criterion"; this must match the
+        ;; version of RUST-CRITERION-0.3.
+        '(substitute* "Cargo.toml"
+           (("\"=0\\.3\\.4\"")
+            "\"=0.3.5\"")))))
     (build-system cargo-build-system)
     (arguments
      `(#:cargo-inputs