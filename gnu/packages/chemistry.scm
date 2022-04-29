99d5c9b54afb78579b6a8f2e431195e7ca8cb1e5
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2022-04-29 13:42:26 -0400

    gnu: Remove nmoldyn.
    * gnu/packages/chemistry.scm (nmoldyn): Delete variable.

--- gnu/packages/chemistry.scm
+++ gnu/packages/chemistry.scm
@@ -306,66 +306,6 @@ staged, and output files collected using a standard interface.")
 (define with-numpy-1.8
   (package-input-rewriting `((,python2-numpy . ,python2-numpy-1.8))))
 
-(define-public nmoldyn
-  (package
-    (name "nmoldyn")
-    (version "3.0.11")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://github.com/khinsen/nMOLDYN3")
-             (commit (string-append "v" version))))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32
-         "016h4bqg419p6s7bcx55q5iik91gqmk26hbnfgj2j6zl0j36w51r"))))
-    (build-system python-build-system)
-    (inputs
-     (list (with-numpy-1.8 python2-matplotlib) python2-scientific netcdf
-           gv))
-    (propagated-inputs
-     (list python2-mmtk))
-    (arguments
-     `(#:python ,python-2
-       #:tests? #f  ; No test suite
-       #:phases
-       (modify-phases %standard-phases
-         (add-before 'build 'create-linux2-directory
-           (lambda _
-             (mkdir-p "nMOLDYN/linux2")))
-         (add-before 'build 'change-PDF-viewer
-           (lambda* (#:key inputs #:allow-other-keys)
-             (substitute* "nMOLDYN/Preferences.py"
-               ;; Set the paths for external executables, substituting
-               ;; gv for acroread.
-               ;; There is also vmd_path, but VMD is not free software
-               ;; and Guix contains currently no free molecular viewer that
-               ;; could be substituted.
-               (("PREFERENCES\\['acroread_path'\\] = ''")
-                (format #f "PREFERENCES['acroread_path'] = '~a'"
-                        (which "gv")))
-               (("PREFERENCES\\['ncdump_path'\\] = ''")
-                (format #f "PREFERENCES['ncdump_path'] = '~a'"
-                        (which "ncdump")))
-               (("PREFERENCES\\['ncgen_path'\\] = ''")
-                (format #f "PREFERENCES['ncgen_path'] = '~a'"
-                        (which "ncgen3")))
-               (("PREFERENCES\\['task_manager_path'\\] = ''")
-                (format #f "PREFERENCES['task_manager_path'] = '~a'"
-                        (which "task_manager")))
-               ;; Show documentation as PDF
-               (("PREFERENCES\\['documentation_style'\\] = 'html'")
-                "PREFERENCES['documentation_style'] = 'pdf'") ))))))
-    (home-page "http://dirac.cnrs-orleans.fr/nMOLDYN.html")
-    (synopsis "Analysis software for Molecular Dynamics trajectories")
-    (description "nMOLDYN is an interactive analysis program for Molecular Dynamics
-simulations.  It is especially designed for the computation and decomposition of
-neutron scattering spectra, but also computes other quantities.  The software
-is currently not actively maintained and works only with Python 2 and
-NumPy < 1.9.")
-    (license license:cecill)))
-
 (define-public tng
   (package
     (name "tng")
