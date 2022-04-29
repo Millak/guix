5ed4cfb924518a83a7e02f27fb77b8a295d15ae3
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2022-04-29 13:42:37 -0400

    gnu: Remove python2-trollius-redis.
    * gnu/packages/databases.scm (python2-trollius-redis): Delete variable.

--- gnu/packages/databases.scm
+++ gnu/packages/databases.scm
@@ -4117,9 +4117,6 @@ is designed to have a low barrier to entry.")
   Redis protocol.")
     (license license:bsd-2)))
 
-(define-public python2-trollius-redis
-  (package-with-python2 python-trollius-redis))
-
 (define-public python-sqlparse
   (package
     (name "python-sqlparse")
