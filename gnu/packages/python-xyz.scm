03ecf0a3f4109c3b018c5659f1866042756d5619
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2022-04-29 17:41:17 -0400

    gnu: Remove python2-mmtk.
    * gnu/packages/python-xyz.scm (python2-mmtk): Delete variable.

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -22474,48 +22474,6 @@ domain-specific (e.g. netCDF and PDB support).  The library is currently
 not actively maintained and works only with Python 2 and NumPy < 1.9.")
     (license license:cecill-c)))
 
-(define-public python2-mmtk
-  (package
-    (name "python2-mmtk")
-    (version "2.7.12")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://github.com/khinsen/MMTK")
-             (commit (string-append "rel" version))))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32
-         "1fqwh3ba9jd42nigvn5shndgwb1zy7kh9520ncvqci7n8ffjr6p1"))))
-    (build-system python-build-system)
-    (native-inputs
-     (list netcdf))
-    (propagated-inputs
-     `(("python-scientific" ,python2-scientific)
-       ("python-tkinter" ,python-2 "tk")))
-    (arguments
-     `(#:python ,python-2
-       #:tests? #f
-       #:phases
-       (modify-phases %standard-phases
-         (add-before 'build 'includes-from-scientific
-           (lambda* (#:key inputs #:allow-other-keys)
-             (mkdir-p "Include/Scientific")
-             (copy-recursively
-                     (string-append
-                      (assoc-ref inputs "python-scientific")
-                      "/include/python2.7/Scientific")
-                     "Include/Scientific"))))))
-    (home-page "http://dirac.cnrs-orleans.fr/MMTK")
-    (synopsis "Python library for molecular simulation")
-    (description "MMTK is a library for molecular simulations with an emphasis
-on biomolecules.  It provides widely used methods such as Molecular Dynamics
-and normal mode analysis, but also basic routines for implementing new methods
-for simulation and analysis.  The library is currently not actively maintained
-and works only with Python 2 and NumPy < 1.9.")
-    (license license:cecill-c)))
-
 (define-public python-phonenumbers
   (package
     (name "python-phonenumbers")
