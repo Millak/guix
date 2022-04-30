722fb58519fbd40fc1d4fabb7eb3845950ce7414
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2022-04-29 23:12:29 -0400

    gnu: Remove python2-pep8.
    * gnu/packages/python-xyz.scm (python2-pep8): Delete variable.

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -10727,9 +10727,6 @@ application monitoring and error tracking software.")
 PEP 8.")
     (license license:expat)))
 
-(define-public python2-pep8
-  (package-with-python2 python-pep8))
-
 (define-public python-pep8-naming
   (package
     (name "python-pep8-naming")
