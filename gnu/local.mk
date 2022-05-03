092ee3515520c759174d235910aefc3a54b00b97
Greg Hogan <code@greghogan.com>
2022-05-03 17:54:16 +0000

    gnu: casync: Update to 99559cd1.
    * gnu/packages/sync.scm (casync): Update to 99559cd1.
    [source]: Remove patch.
    [arguments]: Fix installation.
    [native-inputs]: Add python.
    [inputs]: Add eudev.
    * gnu/packages/patches/casync-renameat2-declaration.patch: Delete file.
    * gnu/local.mk: Remove it.
    
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/local.mk
+++ gnu/local.mk
@@ -926,7 +926,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/calibre-no-updates-dialog.patch		\
   %D%/packages/patches/calibre-remove-test-sqlite.patch		\
   %D%/packages/patches/calibre-remove-test-unrar.patch		\
-  %D%/packages/patches/casync-renameat2-declaration.patch	\
   %D%/packages/patches/catdoc-CVE-2017-11110.patch		\
   %D%/packages/patches/circos-remove-findbin.patch		\
   %D%/packages/patches/cdparanoia-fpic.patch			\
