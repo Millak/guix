Millak/guix

commit bec6bf64bcbcf559eb0b91abcb7f3b2488916797

gnu: rust-instant-0.1: Update to 0.1.9.
* gnu/packages/crates-io.scm (rust-instant-0.1): Update to 0.1.9.

gnu/packages/crates-io.scm
@@ -20,7 +20,7 @@
 ;;; Copyright © 2021 Maxim Cournoyer <maxim.cournoyer@gmail.com>
 ;;; Copyright © 2021 Vinicius Monego <monego@posteo.net>
 ;;; Copyright © 2021, 2022 Petr Hodina <phodina@protonmail.com>
-;;; Copyright © 2021 Ricardo Wurmus <rekado@elephly.net>
+;;; Copyright © 2021, 2022 Ricardo Wurmus <rekado@elephly.net>
 ;;; Copyright © 2021 Jacob Hrbek <kreyren@rixotstudio.cz>
 ;;; Copyright © 2021 Nicolas Graves <ngraves@ngraves.fr>
 ;;; Copyright © 2022 Aleksandr Vityazev <avityazev@posteo.org>
@@ -29075,7 +29075,7 @@ data efficiently.")
 (define-public rust-instant-0.1
   (package
     (name "rust-instant")
-    (version "0.1.4")
+    (version "0.1.9")
     (source
      (origin
        (method url-fetch)
@@ -29084,7 +29084,7 @@ data efficiently.")
         (string-append name "-" version ".tar.gz"))
        (sha256
         (base32
-         "10k1170waz1na056wvjvkps3lz28z9pc8kp8vpy4kpp53i5a4xvp"))))
+         "1v659qqm55misvjijfbl1p7azjp4yynjbwldan8836ynpgp4w4k1"))))
     (build-system cargo-build-system)
     (arguments
      `(#:tests? #f  ; Issue during the wasm test.

commit 6b974845c7460d1f3dc84a633eafa785b5e72b9d

gnu: rust-instant: Fix dependencies.
Fixes a build failure introduced in
bec6bf64bcbcf559eb0b91abcb7f3b2488916797.

* gnu/packages/crates-io.scm (rust-instant-0.1)[arguments]: Adjust
inputs as per "guix import crate rust@0.1.9".

gnu/packages/crates-io.scm
@@ -29089,13 +29089,14 @@ data efficiently.")
     (arguments
      `(#:tests? #f  ; Issue during the wasm test.
        #:cargo-inputs
-       (("rust-js-sys" ,rust-js-sys-0.3)
+       (("rust-cfg-if" ,rust-cfg-if-1)
+        ("rust-js-sys" ,rust-js-sys-0.3)
         ("rust-stdweb" ,rust-stdweb-0.4)
-        ("rust-time" ,rust-time-0.1)
+        ("rust-time" ,rust-time-0.2)
         ("rust-wasm-bindgen" ,rust-wasm-bindgen-0.2)
         ("rust-web-sys" ,rust-web-sys-0.3))
        #:cargo-development-inputs
-       (("rust-wasm-bindgen-test" ,rust-wasm-bindgen-test-0.2))))
+       (("rust-wasm-bindgen-test" ,rust-wasm-bindgen-test-0.3))))
     (home-page "https://github.com/sebcrozet/instant")
     (synopsis
      "Partial replacement for std::time::Instant that works on WASM too")