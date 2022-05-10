Millak/guix

commit d16298196c23cf4d05c4010af399270a2dbbd831

gnu: rust-parking-lot-core-0.8: Update to 0.8.4.
* gnu/packages/crates-io.scm (rust-parking-lot-core-0.8): Update to 0.8.4.

gnu/packages/crates-io.scm
@@ -40613,7 +40613,7 @@ synchronization primitives.")
 (define-public rust-parking-lot-core-0.8
   (package
     (name "rust-parking-lot-core")
-    (version "0.8.0")
+    (version "0.8.4")
     (source
      (origin
        (method url-fetch)
@@ -40621,7 +40621,7 @@ synchronization primitives.")
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
         (base32
-         "16yazfg3sq9mz6cfdkhgbv8yvc1kkasyhys4y7r3g16hgmralqf3"))))
+         "08n7w09q6b2prvazbzgwrc9ml7aaf8yg3132ifsayrkwy1nwwzs6"))))
     (build-system cargo-build-system)
     (arguments
      `(#:cargo-inputs

commit b36ce0275644055c2ee9cb2aa90a0253dd108633

gnu: rust-parking-lot-core@0.8: Fix compilation.
Fixes a build failure introduced in
d16298196c23cf4d05c4010af399270a2dbbd831.

* gnu/packages/crates-io.scm (rust-parking-lot-core-0.8): Update
dependencies as reported by "guix import crate parking-lot-core@0.8.4".
Add 'relax-dependencies' phase.

gnu/packages/crates-io.scm
@@ -40651,15 +40651,22 @@ synchronization primitives.")
     (arguments
      `(#:cargo-inputs
        (("rust-backtrace" ,rust-backtrace-0.3)
-        ("rust-cfg-if" ,rust-cfg-if-0.1)
-        ("rust-cloudabi" ,rust-cloudabi-0.1)
+        ("rust-cfg-if" ,rust-cfg-if-1)
         ("rust-instant" ,rust-instant-0.1)
         ("rust-libc" ,rust-libc-0.2)
         ("rust-petgraph" ,rust-petgraph-0.5)
-        ("rust-redox-syscall" ,rust-redox-syscall-0.1)
+        ("rust-redox-syscall" ,rust-redox-syscall-0.2)
         ("rust-smallvec" ,rust-smallvec-1)
-        ("rust-thread-id" ,rust-thread-id-3)
-        ("rust-winapi" ,rust-winapi-0.3))))
+        ("rust-thread-id" ,rust-thread-id-4)
+        ("rust-winapi" ,rust-winapi-0.3))
+       #:phases (modify-phases %standard-phases
+                  (add-after 'unpack 'relax-dependencies
+                    (lambda _
+                      ;; XXX: The file demands 0.3.60; we have 0.3.56, but
+                      ;; that works well, really.
+                      (substitute* "Cargo.toml"
+                        (("0\\.3\\.60")
+                         ,(package-version rust-backtrace-0.3))))))))
     (home-page "https://github.com/Amanieu/parking_lot")
     (synopsis "API for creating custom synchronization primitives")
     (description "This package provides an advanced API for creating custom