4771e67c049043a0c894bd6ef2a59aedec0cc85c
Tobias Geerinckx-Rice <me@tobias.gr>
2022-05-15 02:00:01 +0200

    gnu: mailutils: Update to 3.15.
    * gnu/packages/mail.scm (mailutils): Update to 3.15.

--- gnu/packages/mail.scm
+++ gnu/packages/mail.scm
@@ -265,14 +265,14 @@ example, modify the message headers or body, or encrypt or sign the message.")
 (define-public mailutils
   (package
     (name "mailutils")
-    (version "3.14")
+    (version "3.15")
     (source (origin
              (method url-fetch)
              (uri (string-append "mirror://gnu/mailutils/mailutils-"
                                  version ".tar.xz"))
              (sha256
               (base32
-               "0fbi2b144mgblq9qhdyhj9855mbglzr8fr23rnps8icbxa7v7if0"))
+               "1nrd9wsidxami3wa86l9z8hnnwv6rhbxdkvqg7dcgz2jqf3c5l5p"))
              (patches
               (search-patches "mailutils-variable-lookup.patch"))))
     (build-system gnu-build-system)
