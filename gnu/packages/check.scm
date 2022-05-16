962d02904fe907d3cb3d96bc220ce7c759ad4425
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2022-05-16 10:01:57 -0400

    gnu: Remove python2-pytest-cov.
    * gnu/packages/check.scm (python2-pytest-cov): Delete variable.

--- gnu/packages/check.scm
+++ gnu/packages/check.scm
@@ -1078,38 +1078,6 @@ distributed testing in both @code{load} and @code{each} modes.  It also
 supports coverage of subprocesses.")
   (license license:expat)))
 
-(define-public python2-pytest-cov
-  (package
-    (name "python2-pytest-cov")
-    (version "2.8.1")
-    (source
-      (origin
-        (method url-fetch)
-        (uri (pypi-uri "pytest-cov" version))
-        (sha256
-         (base32 "0avzlk9p4nc44k7lpx9109dybq71xqnggxb9f4hp0l64pbc44ryc"))))
-    (build-system python-build-system)
-    (arguments
-     `(#:python ,python-2
-       #:phases
-       (modify-phases %standard-phases
-         (replace 'check
-          (lambda _
-            ;; Options taken from tox.ini.
-            ;; TODO: make "--restructuredtext" tests pass. They currently fail
-            ;; with "Duplicate implicit target name".
-            (invoke "python" "./setup.py" "check"
-                    "--strict" "--metadata"))))))
-    (propagated-inputs
-     (list python2-coverage python2-pytest))
-    (home-page "https://github.com/pytest-dev/pytest-cov")
-    (synopsis "Pytest plugin for measuring coverage")
-    (description
-     "Pytest-cov produces coverage reports.  It supports centralised testing and
-distributed testing in both @code{load} and @code{each} modes.  It also
-supports coverage of subprocesses.")
-  (license license:expat)))
-
 (define-public python-pytest-httpserver
   (package
     (name "python-pytest-httpserver")
