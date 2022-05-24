37e29ccd332f371aacedc71388e805fbc9e947fa
Ludovic Courtès <ludovic.courtes@inria.fr>
2022-05-24 14:54:30 +0200

    gnu: gcc: Add 12.1.0.
    * gnu/packages/gcc.scm (gcc-12): New variable.

--- gnu/packages/gcc.scm
+++ gnu/packages/gcc.scm
@@ -698,6 +698,24 @@ It also includes runtime support libraries for these languages.")
        ("armhf" ,@%gcc-11-armhf-micro-architectures)
        ("x86_64" ,@%gcc-11-x86_64-micro-architectures))))))
 
+(define-public gcc-12
+  (package
+    (inherit gcc-11)
+    ;; Note: 'compiler-cpu-architectures' is unchanged compared to GCC 11.
+    (version "12.1.0")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "mirror://gnu/gcc/gcc-"
+                                  version "/gcc-" version ".tar.xz"))
+              (sha256
+               (base32
+                "0ywws66myjxcwsmla721g35d2ymlckq6ii7j9av0477ki5467zb2"))
+              (patches (search-patches "gcc-12-strmov-store-file-names.patch"
+                                       "gcc-5.0-libvtv-runpath.patch"))
+              (modules '((guix build utils)))
+              (snippet gcc-canadian-cross-objdump-snippet)))))
+
+
 ;; Note: When changing the default gcc version, update
 ;;       the gcc-toolchain-* definitions.
 (define-public gcc gcc-10)
