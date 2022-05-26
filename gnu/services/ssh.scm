Millak/guix

commit 1f29ed4a812f86c45e2d9c37fd9f80f6d0418293

services: ssh: Fix extend-openssh-authorized-keys.
Fixes #55359.

* gnu/services/ssh.scm (extend-openssh-authorized-keys): Use KEYS argument.

gnu/services/ssh.scm
@@ -571,7 +571,7 @@ of user-name/file-like tuples."
   (openssh-configuration
    (inherit config)
    (authorized-keys
-    (match (openssh-configuration-authorized-keys config)
+    (match (append (openssh-configuration-authorized-keys config) keys)
       (((users _ ...) ...)
        ;; Build a user/key-list mapping.
        (let ((user-keys (alist->vhash

commit 0dc63ce519c5f98b2186d1871176e2fac3a6926b

services: openssh: Fix computation of the authorized-key directory.
Fixes a bug introduced in 1f29ed4a812f86c45e2d9c37fd9f80f6d0418293,
whereby 'authorized-key-directory' would end up creating empty files for
authorized keys passed as an extension.

See <https://issues.guix.gnu.org/55359>.

* gnu/services/ssh.scm (extend-openssh-authorized-keys): Call
'alist->vhash' on the alist resulting from the 'append' call.

gnu/services/ssh.scm
@@ -572,10 +572,9 @@ of user-name/file-like tuples."
    (inherit config)
    (authorized-keys
     (match (append (openssh-configuration-authorized-keys config) keys)
-      (((users _ ...) ...)
+      ((and alist ((users _ ...) ...))
        ;; Build a user/key-list mapping.
-       (let ((user-keys (alist->vhash
-                         (openssh-configuration-authorized-keys config))))
+       (let ((user-keys (alist->vhash alist)))
          ;; Coalesce the key lists associated with each user.
          (map (lambda (user)
                 `(,user