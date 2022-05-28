c02dbd5c801dc88c77b6e5f292abf74901a026e3
Ricardo Wurmus <rekado@elephly.net>
2022-05-28 08:30:13 +0200

    gnu: r-circlize: Update to 0.4.15.
    * gnu/packages/cran.scm (r-circlize): Update to 0.4.15.

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -4020,14 +4020,14 @@ validation and filtering on the values, making options invisible or private.")
 (define-public r-circlize
   (package
     (name "r-circlize")
-    (version "0.4.14")
+    (version "0.4.15")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "circlize" version))
        (sha256
         (base32
-         "0b0yqjmbj1s1jg7y6cgyawadlfv0i6xrls8a4qy9qs7hz8zrpvb1"))))
+         "1iswb7dy08as3pbzxvhx5qlsbnwvp79nwg8m158nfz7y2d9xa0nn"))))
     (build-system r-build-system)
     (propagated-inputs
      (list r-colorspace r-globaloptions r-shape))
