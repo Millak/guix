d690341eeeecf7bbc0618eb22253de065ed0e64a
Ricardo Wurmus <rekado@elephly.net>
2022-05-28 08:30:44 +0200

    gnu: r-tam: Update to 4.0-16.
    * gnu/packages/cran.scm (r-tam): Update to 4.0-16.

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -25236,14 +25236,14 @@ well as Ravand and Robitzsch (2015).")
 (define-public r-tam
   (package
     (name "r-tam")
-    (version "3.7-16")
+    (version "4.0-16")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "TAM" version))
        (sha256
         (base32
-         "1969xm1f7s183h0xnd4g1bxhjp80rl7kad29zwcig0fmw6bs2mpd"))))
+         "00ww277hsyci5rph4gb155h0y95dmjhqgn9amz6g8zbrjb2748dw"))))
     (properties `((upstream-name . "TAM")))
     (build-system r-build-system)
     (propagated-inputs
