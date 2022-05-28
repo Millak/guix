7fe83026b7491108c6bbfd76a13b0c8cb57d2406
Ricardo Wurmus <rekado@elephly.net>
2022-05-28 12:46:48 +0200

    gnu: r-biomvcclass: Update to 1.64.0.
    * gnu/packages/bioconductor.scm (r-biomvcclass): Update to 1.64.0.

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -15601,14 +15601,14 @@ design.")
 (define-public r-biomvcclass
   (package
     (name "r-biomvcclass")
-    (version "1.62.0")
+    (version "1.64.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "BioMVCClass" version))
        (sha256
         (base32
-         "0jqgazwz35gz11a7vzanyy2yalzalx0z0rw6y18nmk8dbv01nv2j"))))
+         "078pnyygbvbfxziqspfr1nn78w67xyb4qmiwc34czga5psblvfwz"))))
     (properties `((upstream-name . "BioMVCClass")))
     (build-system r-build-system)
     (propagated-inputs
