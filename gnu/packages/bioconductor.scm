d15f46c5cf381bc79cd3dcb96c879892b3c6c99d
Ricardo Wurmus <rekado@elephly.net>
2022-05-28 12:44:32 +0200

    gnu: r-deepsnv: Update to 1.42.0.
    * gnu/packages/bioconductor.scm (r-deepsnv): Update to 1.42.0.

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -2219,13 +2219,13 @@ targeted by a kinase.")
 (define-public r-deepsnv
   (package
     (name "r-deepsnv")
-    (version "1.40.0")
+    (version "1.42.0")
     (source (origin
               (method url-fetch)
               (uri (bioconductor-uri "deepSNV" version))
               (sha256
                (base32
-                "1wi6j6yb6i9fs9yszfywqz3w50mhl85dkfmr4w3phwvkf0xkn81w"))))
+                "0si1x2bi5pff1dicpqg7hv4ziq46vm7lic72724ljsclpz9bfv6q"))))
     (properties `((upstream-name . "deepSNV")))
     (build-system r-build-system)
     (propagated-inputs
