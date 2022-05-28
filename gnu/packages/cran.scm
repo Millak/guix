b6b8f8c39593403b7abc6ee3446d173a3accefe9
Ricardo Wurmus <rekado@elephly.net>
2022-05-28 08:30:35 +0200

    gnu: r-r2html: Update to 2.3.3.
    * gnu/packages/cran.scm (r-r2html): Update to 2.3.3.

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -18066,14 +18066,14 @@ handle missing genotypes at some SNPs.")
 (define-public r-r2html
   (package
     (name "r-r2html")
-    (version "2.3.2")
+    (version "2.3.3")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "R2HTML" version))
        (sha256
         (base32
-         "00kxny7hajs9r2kw63qk7d03ggdxx2j1g8vbrmzp806y8aczvik9"))))
+         "12qg9rp1j26h2f48dfg5k9jds26ycacv3f3ymk686ks43hd1hzja"))))
     (properties `((upstream-name . "R2HTML")))
     (build-system r-build-system)
     (home-page "https://github.com/nalimilan/R2HTML")
