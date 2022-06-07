337441609cb3209c416eb40c92b0815b1ba4072c
Leo Famulari <leo@famulari.name>
2022-06-06 20:33:13 -0400

    gnu: mu: Update to 1.6.11.
    * gnu/packages/mail.scm (mu): Update to 1.6.11.

--- gnu/packages/mail.scm
+++ gnu/packages/mail.scm
@@ -1158,7 +1158,7 @@ security functionality including PGP, S/MIME, SSH, and SSL.")
 (define-public mu
   (package
     (name "mu")
-    (version "1.6.10")
+    (version "1.6.11")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://github.com/djcb/mu/releases/"
@@ -1166,7 +1166,7 @@ security functionality including PGP, S/MIME, SSH, and SSL.")
                                   "mu-" version ".tar.xz"))
               (sha256
                (base32
-                "1dh0x4lqnjflb0k8fybr5clqjxv35scf055g1590pr5znam29hhb"))))
+                "13gchks9znyw0axw1wlks7f7h4442rfagmj7kx0jm3qhvi0b5sk0"))))
     (build-system gnu-build-system)
     (native-inputs
      `(("pkg-config" ,pkg-config)
