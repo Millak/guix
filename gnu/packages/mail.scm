41bc69886a9ef184fea66531668ac39da8fdcf95
Tobias Geerinckx-Rice <me@tobias.gr>
2022-06-19 02:00:00 +0200

    gnu: exim: Don't address build inputs by their labels.
    The package's output remains identical.
    
    * gnu/packages/mail.scm (exim)[arguments]: Use SEARCH-INPUT-FILE or
    PATCH-SHEBANG in place of ASSOC-REF for all inputs.

--- gnu/packages/mail.scm
+++ gnu/packages/mail.scm
@@ -1822,10 +1822,7 @@ delivery.")
                 (string-append var "rm\n")))
              (copy-file "src/EDITME" "Local/Makefile")
              (copy-file "exim_monitor/EDITME" "Local/eximon.conf")
-             (let ((out (assoc-ref outputs "out"))
-                   (gzip (assoc-ref inputs "gzip"))
-                   (bzip2 (assoc-ref inputs "bzip2"))
-                   (xz (assoc-ref inputs "xz")))
+             (let ((out (assoc-ref outputs "out")))
                (substitute* '("Local/Makefile")
                  (("(BIN_DIRECTORY=).*" all var)
                   (string-append var out "/bin\n"))
@@ -1836,9 +1833,9 @@ delivery.")
                  (("(FIXED_NEVER_USERS=).*" all var)
                   (string-append var "\n")) ; XXX no root in build environment
                  (("(COMPRESS_COMMAND=).*" all var)
-                  (string-append var gzip "/bin/gzip\n"))
+                  (string-append var (search-input-file inputs "bin/gzip") "\n"))
                  (("(ZCAT_COMMAND=).*" all var)
-                  (string-append var gzip "/bin/zcat\n"))
+                  (string-append var (search-input-file inputs "bin/zcat") "\n"))
                  (("# (USE_GNUTLS(|_PC)=.*)" all line)
                   (string-append line "\n"))
                  (("# (AUTH_CRAM_MD5=yes)" all line) line)
@@ -1850,19 +1847,17 @@ delivery.")
                ;; This file has hard-coded relative file names for tools despite
                ;; the zcat configuration above.
                (substitute* '("src/exigrep.src")
-                 (("'zcat'") (string-append "'" gzip "/bin/zcat'"))
-                 (("'bzcat'") (string-append "'" bzip2 "/bin/bzcat'"))
-                 (("'xzcat'") (string-append "'" xz "/bin/xzcat'"))
-                 (("'lzma'") (string-append "'" xz "/bin/lzma'"))))))
+                 (("'(bzcat|xzcat|zcat|lzma)'" _ command)
+                  (format #f "'~a'"
+                          (search-input-file
+                           inputs (string-append "bin/" command))))))))
          (add-before 'build 'fix-sh-paths
-           (lambda* (#:key inputs #:allow-other-keys)
+           (lambda _
              (substitute* '("scripts/lookups-Makefile" "scripts/reversion")
                (("SHELL=/bin/sh") "SHELL=sh"))
              (substitute* '("scripts/Configure-config.h")
                (("\\| /bin/sh") "| sh"))
-             (let ((bash (assoc-ref inputs "bash")))
-               (substitute* '("scripts/Configure-eximon")
-                 (("#!/bin/sh") (string-append "#!" bash "/bin/sh"))))))
+             (patch-shebang "scripts/Configure-eximon")))
          (add-before 'build 'build-reproducibly
            (lambda _
              ;; The ‘compilation number’ is incremented for every build from the
