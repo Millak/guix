eed4494da7fbb952a019052466c7a8adcf62a1da
muradm <mail@muradm.net>
2022-06-20 07:22:38 +0300

    gnu: Add go-github-com-sourcegraph-jsonrpc2 0.1.0.
    * gnu/packages/golang.scm (go-github-com-sourcegraph-jsonrpc2): New variable.
    
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/packages/golang.scm
+++ gnu/packages/golang.scm
@@ -9865,3 +9865,29 @@ implementation.")
     (synopsis "Fast WebSocket implementation for Go")
     (description "Gorilla WebSocket is a Go implementation of the WebSocket protocol.")
     (license license:bsd-2)))
+
+(define-public go-github-com-sourcegraph-jsonrpc2
+  (package
+    (name "go-github-com-sourcegraph-jsonrpc2")
+    (version "0.1.0")
+    (home-page "https://github.com/sourcegraph/jsonrpc2")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url home-page)
+                    (commit (string-append "v" version))))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32 "1dk0w32k96vxrwnmm24wqx337dn8ylch65qwrbc3wh7whw2xx71q"))))
+    (build-system go-build-system)
+    (arguments
+     '(#:import-path "github.com/sourcegraph/jsonrpc2"))
+    (propagated-inputs
+     (list
+      go-github-com-daviddengcn-go-colortext
+      go-github-com-motemen-go-colorine
+      go-github-com-gorilla-websocket))
+    (synopsis "Provides a client and server implementation of JSON-RPC 2.0")
+    (description
+     "Package jsonrpc2 provides a Go implementation of JSON-RPC 2.0.")
+    (license license:expat)))
