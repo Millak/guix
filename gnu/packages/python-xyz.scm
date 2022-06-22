4a1550516ab7d90864f93f2e4ad2aefb5fc18d11
Efraim Flashner <efraim@flashner.co.il>
2022-06-22 12:33:11 +0300

    gnu: python-py-cpuinfo: Update to 8.0.0-1.4d6987e.
    * gnu/packages/python-xyz.scm (python-py-cpuinfo): Update to 8.0.0-1.4d6987e.
    [source]: Download using git-fetch.

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -25181,23 +25181,30 @@ choose to use Base64 without the “=” padding.")
     (license license:asl2.0)))
 
 (define-public python-py-cpuinfo
-  (package
-    (name "python-py-cpuinfo")
-    (version "5.0.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (pypi-uri "py-cpuinfo" version))
-       (sha256
-        (base32
-         "0045y6832gqjg63jmw0qj2jwyypgjwr7sfdq3lfv49b6fxpl5xic"))))
-    (build-system python-build-system)
-    (home-page "https://github.com/workhorsy/py-cpuinfo")
-    (synopsis "Get CPU info with Python")
-    (description
-     "This Python module returns the CPU info by using the best sources of
+  ;; This is the first commit where riscv64-linux support is available.
+  ;; We can move back to pypi releases with the next release.
+  (let ((commit "4d6987e5c30f2ebacb20781892c01329042cce60")
+        (revision "1"))
+    (package
+      (name "python-py-cpuinfo")
+      (version (git-version "8.0.0" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+                (url "https://github.com/workhorsy/py-cpuinfo")
+                (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32
+           "0h5wi1bfcqqr1x3j1pa7dmkx7siprsyksbsy80fl2sdrrgpji0b0"))))
+      (build-system python-build-system)
+      (home-page "https://github.com/workhorsy/py-cpuinfo")
+      (synopsis "Get CPU info with Python")
+      (description
+       "This Python module returns the CPU info by using the best sources of
 information for your operating system.")
-    (license license:expat)))
+      (license license:expat))))
 
 (define-public python-canonicaljson
   (package
