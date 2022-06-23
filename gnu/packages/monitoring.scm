de52ffc6b44d5fb63961c9a39b6a4eeca2200f10
Marius Bakke <marius@gnu.org>
2022-06-23 13:08:56 +0200

    gnu: Zabbix: Update to 6.0.5.
    * gnu/packages/monitoring.scm (zabbix-agentd): Update to 6.0.5.

--- gnu/packages/monitoring.scm
+++ gnu/packages/monitoring.scm
@@ -165,7 +165,7 @@ etc. via a Web interface.  Features include:
 (define-public zabbix-agentd
   (package
     (name "zabbix-agentd")
-    (version "6.0.3")
+    (version "6.0.5")
     (source
      (origin
        (method url-fetch)
@@ -173,7 +173,7 @@ etc. via a Web interface.  Features include:
              "https://cdn.zabbix.com/zabbix/sources/stable/"
              (version-major+minor version) "/zabbix-" version ".tar.gz"))
        (sha256
-        (base32 "0hihi94dk235cn4rwhrzm496dlihk0pv8785y2jyqi17jhl566g6"))
+        (base32 "1hmx6dgsag84dpv867p12bkln141nypgkp6zhipxbnn5xxip1sry"))
        (modules '((guix build utils)))
        (snippet
         '(substitute* '("src/zabbix_proxy/proxy.c"
