8d0e69624357c0ce9e7c0b0713910a3e35f16226
Antero Mejr <antero@mailbox.org>
2022-06-25 04:25:03 +0000

    gnu: Add python-sendgrid.
    * gnu/packages/python-web.scm (python-sendgrid): New variable.
    
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/packages/python-web.scm
+++ gnu/packages/python-web.scm
@@ -7569,3 +7569,28 @@ resources using Web Application Description Language (WADL) files as guides.")
     (description
      "This package provides access to any RESTful or RESTful-like API.")
     (license license:expat)))
+
+(define-public python-sendgrid
+  (package
+    (name "python-sendgrid")
+    (version "6.9.7")
+    (home-page "https://github.com/sendgrid/sendgrid-python/")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url home-page)
+                    (commit version)))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "0kvp4gm3bpcsj2mkv05pgvlcv1jlsfhcljcv61wz5kq9d273h7rg"))))
+    (build-system python-build-system)
+    (arguments
+     (list #:tests? #f))       ;241/340 tests fail due to attempted web access
+    (propagated-inputs (list python-http-client python-starkbank-ecdsa))
+    (synopsis "SendGrid API library for Python")
+    (description
+     "The @code{sendgrid} Python library allows access to the
+SendGrid Web API v3.  Version 3+ of the library provides full support for all
+SendGrid Web API v3 endpoints, including the new v3 /mail/send.")
+    (license license:expat)))
