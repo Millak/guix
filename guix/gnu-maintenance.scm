abc72eeac0e2763a5662564813a19211869a6796
Hartmut Goebel <h.goebel@crazy-compilers.com>
2022-06-29 15:02:07 +0200

    import: sourceforge: Issue error-message if version is given.
    Due to the complicated directory structure at sourceforce,
    enabling the  sourceforge importer to update to a specific version
    is very complicated to implement. Since only 2.0% of the
    packages in guix are covered by this updater I dedided to not
    implement this.
    
    * guix/gnu-maintenance.scm (latest-sourceforge-release): Add #:version
      argument.  Issue error-message if version is given.

--- guix/gnu-maintenance.scm
+++ guix/gnu-maintenance.scm
@@ -37,6 +37,8 @@
   #:autoload   (guix download) (%mirrors)
   #:use-module (guix ftp-client)
   #:use-module (guix utils)
+  #:use-module (guix diagnostics)
+  #:use-module (guix i18n)
   #:use-module (guix memoization)
   #:use-module (guix records)
   #:use-module (guix upstream)
@@ -705,7 +707,7 @@ GNOME packages; EMMS is included though, because its releases are on gnu.org."
                          #:base-url %savannah-base
                          #:directory directory)))
 
-(define (latest-sourceforge-release package)
+(define* (latest-sourceforge-release package #:key (version #f))
   "Return the latest release of PACKAGE."
   (define (uri-append uri extension)
     ;; Return URI with EXTENSION appended.
@@ -719,6 +721,12 @@ GNOME packages; EMMS is included though, because its releases are on gnu.org."
      (case (response-code (http-head uri #:port port #:keep-alive? #t))
        ((200 302) #t)
        (else #f))))
+
+  (when version
+    (error
+     (formatted-message
+      (G_ "Updating to a specific version is not yet implemented for ~a, sorry.")
+      "sourceforge")))
 
   (let* ((name     (package-upstream-name package))
          (base     (string-append "https://sourceforge.net/projects/"
