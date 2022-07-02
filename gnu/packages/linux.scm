5df7a33af70a54aab83293d25a8c1903da3c6363
Leo Famulari <leo@famulari.name>
2022-07-02 13:10:14 -0400

    gnu: linux-libre 5.15: Update to 5.15.52.
    * gnu/packages/linux.scm (linux-libre-5.15-version): Update to 5.15.52.
    (linux-libre-5.15-pristine-source): Update hash.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -371,7 +371,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
 ;; The "longterm" kernels — the older releases with long-term upstream support.
 ;; Here are the support timelines:
 ;; <https://www.kernel.org/category/releases.html>
-(define-public linux-libre-5.15-version "5.15.51")
+(define-public linux-libre-5.15-version "5.15.52")
 (define-public linux-libre-5.15-gnu-revision "gnu")
 (define deblob-scripts-5.15
   (linux-libre-deblob-scripts
@@ -381,7 +381,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
    (base32 "129qlhwdv2mfb85gbvq03kkbdfp73b444rryr4rrbvi0jmq4cp24")))
 (define-public linux-libre-5.15-pristine-source
   (let ((version linux-libre-5.15-version)
-        (hash (base32 "1229m4r4n61n5l8anp2pcqdhajkwmavhr1z00n8gvx3yn9w4ifhz")))
+        (hash (base32 "01bdmgbl31nm0wg5mrzn6xgls4h5gig96mjrmn790npjm5fhls7l")))
    (make-linux-libre-source version
                             (%upstream-linux-source version hash)
                             deblob-scripts-5.15)))
