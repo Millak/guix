076146818bcaecc5afdaa3a903e378f6d30b2df9
Marius Bakke <marius@gnu.org>
2022-07-07 00:53:10 +0200

    gnu: Remove OpenJPEG@1.
    * gnu/packages/image.scm (openjpeg-1): Remove variable.

--- gnu/packages/image.scm
+++ gnu/packages/image.scm
@@ -852,19 +852,6 @@ JPEG 2000 Reference Software.")
     (home-page "https://github.com/uclouvain/openjpeg")
     (license license:bsd-2)))
 
-(define-public openjpeg-1
-  (package (inherit openjpeg)
-    (name "openjpeg")
-    (version "1.5.2")
-    (source
-     (origin
-       (method url-fetch)
-       (uri
-        (string-append "mirror://sourceforge/openjpeg.mirror/" version "/"
-                       name "-" version ".tar.gz"))
-       (sha256
-        (base32 "11waq9w215zvzxrpv40afyd18qf79mxc28fda80bm3ax98cpppqm"))))))
-
 (define-public giflib
   (package
     (name "giflib")
