4ea381aa401eed66e211831ee9ceeeb944f7abb9
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2022-07-06 20:11:42 +0200

    gnu: emacs-crdt: Update to 0.3.0.
    * gnu/packages/emacs-xyz.scm (emacs-crdt): Update to 0.3.0.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -19454,9 +19454,9 @@ never confused by comments or @code{foo-bar} matching @code{foo}.")
 
 (define-public emacs-crdt
   ;; XXX: Upstream does not always tag new releases.  The commit below
-  ;; corresponds exactly to 0.2.7 bump.
-  (let ((commit "b8a02a5c1f759003343c10f8cedef4daae95f9fe")
-        (version "0.2.7"))
+  ;; corresponds exactly to latest version bump.
+  (let ((commit "77be6cded0e2d24fc9c132032ae3bf7ae8ed2cf9")
+        (version "0.3.0"))
     (package
       (name "emacs-crdt")
       (version version)
@@ -19468,7 +19468,7 @@ never confused by comments or @code{foo-bar} matching @code{foo}.")
                (commit commit)))
          (file-name (git-file-name name version))
          (sha256
-          (base32 "0av65dnfzc0qapl5cvm6ha3vax4c19y6rxbn2qy1ir87q6lpk2xc"))))
+          (base32 "0irf2allz7bp696nqlgg70ma6n336bspz8794bff1s1pramrzlhz"))))
       (build-system emacs-build-system)
       (home-page "https://code.librehq.com/qhong/crdt.el")
       (synopsis "Real-time collaborative editing environment")
