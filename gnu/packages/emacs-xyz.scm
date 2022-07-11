87d0bd9a142cdcf654d5c375819ffad1d2b6483f
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2022-07-11 11:24:17 +0200

    gnu: emacs-taxy: Update to 0.9.
    * gnu/packages/emacs-xyz.scm (emacs-taxy): Update to 0.9.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -16597,7 +16597,7 @@ an elisp expression.")
 (define-public emacs-taxy
   (package
     (name "emacs-taxy")
-    (version "0.8")
+    (version "0.9")
     (source
      (origin
        (method url-fetch)
@@ -16605,7 +16605,7 @@ an elisp expression.")
              "https://elpa.gnu.org/packages/taxy-" version ".tar"))
        (sha256
         (base32
-         "00pc6lh35gj8vzcsn17fyazb9jsc4m6nr7cvb32w02isadv8qd3m"))))
+         "1nzafs3yvcnmg07zi345n2yvjsw9wixlybzibnhf5k6jnd0vvpjh"))))
     (build-system emacs-build-system)
     (propagated-inputs
      (list emacs-magit))
