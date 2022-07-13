9e4cbb307665f061d6e8bc75660ae9421886f6b2
Ludovic Courtès <ludo@gnu.org>
2022-07-13 17:13:12 +0200

    home: services: shell: Do not use "setup" as a verb.
    * gnu/home/services/shells.scm (zsh-file-zprofile)
    (add-bash-configuration): Do not use "setup" as a word in generated
    files.  End sentences with a period.

--- gnu/home/services/shells.scm
+++ gnu/home/services/shells.scm
@@ -192,9 +192,9 @@ another process for example)."))
   (mixed-text-file
    "zprofile"
    "\
-# Setups system and user profiles and related variables
+# Set up the system, user profile, and related variables.
 source /etc/profile
-# Setups home environment profile
+# Set up the home environment profile.
 source ~/.profile
 
 # It's only necessary if zsh is a login shell, otherwise profiles will
@@ -443,9 +443,9 @@ alias grep='grep --color=auto'\n")
       ,(mixed-text-file
         "bash_profile"
         "\
-# Setups system and user profiles and related variables
+# Set up the system, user profile, and related variables.
 # /etc/profile will be sourced by bash automatically
-# Setups home environment profile
+# Set up the home environment profile.
 if [ -f ~/.profile ]; then source ~/.profile; fi
 
 # Honor per-interactive-shell startup file
