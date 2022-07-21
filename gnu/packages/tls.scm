67921f5f4f456f6a3e25eabb228748ee83684d71
Marius Bakke <marius@gnu.org>
2022-07-22 01:26:25 +0200

    gnu: OpenSSL: Incorporate grafted changes.
    * gnu/packages/tls.scm (openssl): Update to 1.1.1q.
    [replacement]: Remove.
    (openssl/fixed): Remove variable.

--- gnu/packages/tls.scm
+++ gnu/packages/tls.scm
@@ -350,8 +350,7 @@ OpenSSL for TARGET."
 (define-public openssl
   (package
     (name "openssl")
-    (version "1.1.1p")
-    (replacement openssl/fixed)
+    (version "1.1.1q")
     (source (origin
               (method url-fetch)
               (uri (list (string-append "https://www.openssl.org/source/openssl-"
@@ -364,7 +363,7 @@ OpenSSL for TARGET."
               (patches (search-patches "openssl-1.1-c-rehash-in.patch"))
               (sha256
                (base32
-                "0vyfibydji26wk1fmm0piz0810mfr7j4vaa2k5iwgiv6m8mbcqdz"))))
+                "1jhhzp4gh6ymidxm1ckjk948l583awp0w3y2nvqdz7022kk9r4yp"))))
     (build-system gnu-build-system)
     (outputs '("out"
                "doc"        ;6.8 MiB of man3 pages and full HTML documentation
@@ -473,25 +472,6 @@ OpenSSL for TARGET."
      "OpenSSL is an implementation of SSL/TLS.")
     (license license:openssl)
     (home-page "https://www.openssl.org/")))
-
-(define openssl/fixed
-  (package
-    (inherit openssl)
-    (name "openssl")
-    (version "1.1.1q")
-    (source (origin
-              (method url-fetch)
-              (uri (list (string-append "https://www.openssl.org/source/openssl-"
-                                        version ".tar.gz")
-                         (string-append "ftp://ftp.openssl.org/source/"
-                                        "openssl-" version ".tar.gz")
-                         (string-append "ftp://ftp.openssl.org/source/old/"
-                                        (string-trim-right version char-set:letter)
-                                        "/openssl-" version ".tar.gz")))
-              (patches (search-patches "openssl-1.1-c-rehash-in.patch"))
-              (sha256
-               (base32
-                "1jhhzp4gh6ymidxm1ckjk948l583awp0w3y2nvqdz7022kk9r4yp"))))))
 
 (define-public openssl-3.0
   (package
