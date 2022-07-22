bc3eaf9d83c5f227681f43bbc70067d92fc72193
Morgan Smith <Morgan.J.Smith@outlook.com>
2022-07-22 10:54:32 -0400

    gnu: go-gitlab.com-shackra-goimapnotify: Update to 2.3.7.
    * gnu/packages/mail.scm (go-gitlab.com-shackra-goimapnotify): Update to 2.3.7.
    
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/packages/mail.scm
+++ gnu/packages/mail.scm
@@ -404,34 +404,32 @@ software.  GNU Mailutils provides the following commands:
      (list license:gpl3+ license:lgpl3+))))
 
 (define-public go-gitlab.com-shackra-goimapnotify
-  (let ((commit "832bc7112db9b28e28d69e90b91ea6c005244c9b")
-        (revision "0"))
-    (package
-      (name "go-gitlab.com-shackra-goimapnotify")
-      (version (git-version "0.0.0" revision commit))
-      (source (origin
-                (method git-fetch)
-                (uri (git-reference
-                      (url "https://gitlab.com/shackra/goimapnotify")
-                      (commit commit)))
-                (file-name (git-file-name name version))
-                (sha256
-                 (base32
-                  "1h27kshx4vwl5k6vc2szsq3d701fzs4gczjypz907f8hj0lrnjmy"))))
-      (build-system go-build-system)
-      (arguments
-       `(#:import-path "gitlab.com/shackra/goimapnotify"))
-      (propagated-inputs
-       (list go-github-com-emersion-go-imap
-             go-github-com-emersion-go-imap-idle
-             go-github-com-emersion-go-sasl go-github-com-sirupsen-logrus
-             go-golang-org-x-text))
-      (synopsis "Execute scripts on IMAP mailbox changes")
-      (description
-       "Script to execute scripts on IMAP mailbox changes (new/deleted/updated
+  (package
+    (name "go-gitlab.com-shackra-goimapnotify")
+    (version "2.3.7")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://gitlab.com/shackra/goimapnotify")
+                    (commit version)))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "06jhxvhdvdv049qpvp8ahnhvswvbpakpw7aq2lw790f3x89px2ss"))))
+    (build-system go-build-system)
+    (arguments
+     `(#:import-path "gitlab.com/shackra/goimapnotify"))
+    (propagated-inputs
+     (list go-github-com-emersion-go-imap
+           go-github-com-emersion-go-imap-idle
+           go-github-com-emersion-go-sasl go-github-com-sirupsen-logrus
+           go-golang-org-x-text))
+    (synopsis "Execute scripts on IMAP mailbox changes")
+    (description
+     "Script to execute scripts on IMAP mailbox changes (new/deleted/updated
 messages) using IDLE.  Implemented in Go.")
-      (home-page "https://gitlab.com/shackra/goimapnotify")
-      (license license:gpl3+))))
+    (home-page "https://gitlab.com/shackra/goimapnotify")
+    (license license:gpl3+)))
 
 (define-public guile2.2-mailutils
   (package
