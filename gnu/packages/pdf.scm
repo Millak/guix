84be8bc1f4202d1c6e33995815d2c42ab4cb29bd
Tobias Geerinckx-Rice <me@tobias.gr>
2022-07-24 02:00:01 +0200

    gnu: podofo: Update to 0.9.8.
    * gnu/packages/pdf.scm (podofo): Update to 0.9.8.
    [arguments]: Don't explicitly return #t from phases.

--- gnu/packages/pdf.scm
+++ gnu/packages/pdf.scm
@@ -660,14 +660,14 @@ interaction.")
 (define-public podofo
   (package
     (name "podofo")
-    (version "0.9.7")
+    (version "0.9.8")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://sourceforge/podofo/podofo/" version
                                   "/podofo-" version ".tar.gz"))
               (sha256
                (base32
-                "1f0yvkx6nf99fp741w2y706d8bs9824x1z2gqm3rdy5fv8bfgwkw"))))
+                "0m2icjy35jd0900g0fyfrmf0zsldv1chfc1q0zcqlaqrbzhhgrjx"))))
     (build-system cmake-build-system)
     (native-inputs
      (list cppunit pkg-config))
@@ -690,8 +690,7 @@ interaction.")
              (let ((freetype (assoc-ref inputs "freetype")))
                ;; Look for freetype include files in the correct place.
                (substitute* "cmake/modules/FindFREETYPE.cmake"
-                 (("/usr/local") freetype)))
-             #t)))))
+                 (("/usr/local") freetype))))))))
     (home-page "http://podofo.sourceforge.net")
     (synopsis "Tools to work with the PDF file format")
     (description
