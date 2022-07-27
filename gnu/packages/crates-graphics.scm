a9107f2703d002bbc37c5649c6d5bcf327946d5f
Marius Bakke <marius@gnu.org>
2022-07-27 14:16:33 +0200

    gnu: rust-wayland-protocols: Add 0.29.4.
    * gnu/packages/crates-graphics.scm (rust-wayland-protocols-0.29): New variable.
    (rust-wayland-protocols-0.28): Inherit from it.

--- gnu/packages/crates-graphics.scm
+++ gnu/packages/crates-graphics.scm
@@ -2681,8 +2681,35 @@ WlSurface, which can then play the role of the base surface for
 initializing an OpenGL or Vulkan context.")
     (license license:expat)))
 
+(define-public rust-wayland-protocols-0.29
+  (package
+    (name "rust-wayland-protocols")
+    (version "0.29.4")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "wayland-protocols" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0hap8vky2fwsq05c98c8xs00gb9m5kxp8kq3zr0jwh036gi7l530"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs
+       (("rust-bitflags" ,rust-bitflags-1.2))))
+    (inputs
+     (list rust-bitflags-1.2 rust-wayland-client-0.29
+           rust-wayland-commons-0.29 rust-wayland-scanner-0.29
+           rust-wayland-server-0.29))
+    (home-page "https://github.com/smithay/wayland-rs")
+    (synopsis "Generated API for the officials Wayland protocol extensions")
+    (description
+     "This package provides a generated API for the officials Wayland protocol
+extensions.")
+    (license license:expat)))
+
 (define-public rust-wayland-protocols-0.28
   (package
+    (inherit rust-wayland-protocols-0.29)
     (name "rust-wayland-protocols")
     (version "0.28.3")
     (source
@@ -2699,13 +2726,7 @@ initializing an OpenGL or Vulkan context.")
     (inputs
      (list rust-bitflags-1 rust-wayland-client-0.28
            rust-wayland-commons-0.28 rust-wayland-scanner-0.28
-           rust-wayland-server-0.28))
-    (home-page "https://github.com/smithay/wayland-rs")
-    (synopsis "Generated API for the officials Wayland protocol extensions")
-    (description
-     "This package provides a generated API for the officials Wayland protocol
-extensions.")
-    (license license:expat)))
+           rust-wayland-server-0.28))))
 
 (define-public rust-wayland-protocols-0.23
   (package
