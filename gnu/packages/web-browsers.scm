Millak/guix

commit a0beb297a352930b89051ff258a06d78f224d6b9

gnu: qtwebengine: Rename to qtwebengine-5.
Automated with:

  git grep -l qtwebengine | xargs sed 's/\bqtwebengine\b/\0-5/g' -i
  git checkout NEWS

gnu/packages/web-browsers.scm
@@ -492,11 +492,11 @@ interface.")
            ;; that it's __init__.py is used first.
            python-pyqtwebengine
            python-pyqt-without-qtwebkit
-           ;; While qtwebengine is provided by python-pyqtwebengine, it's
+           ;; While qtwebengine-5 is provided by python-pyqtwebengine, it's
            ;; included here so we can wrap QTWEBENGINEPROCESS_PATH.
-           qtwebengine))
+           qtwebengine-5))
     (arguments
-     `(;; FIXME: With the existance of qtwebengine, tests can now run.  But
+     `(;; FIXME: With the existance of qtwebengine-5, tests can now run.  But
        ;; they are still disabled because test phase hangs.  It's not readily
        ;; apparent as to why.
        #:tests? #f
@@ -543,7 +543,7 @@ interface.")
              (let* ((out (assoc-ref outputs "out"))
                     (bin (string-append out "/bin/qutebrowser"))
                     (qt-process-path (string-append
-                                      (assoc-ref inputs "qtwebengine")
+                                      (assoc-ref inputs "qtwebengine-5")
                                       "/lib/qt5/libexec/QtWebEngineProcess")))
                (wrap-program bin
                  `("QTWEBENGINEPROCESS_PATH" = (,qt-process-path)))))))))

gnu/packages/audio.scm
@@ -3237,7 +3237,7 @@ link REQUIRED)"))))))
                   qtwebchannel-5
                   qtwebsockets-5))
     (propagated-inputs                  ;to get native-search-path
-     (list qtwebengine))
+     (list qtwebengine-5))
     (home-page "https://github.com/supercollider/supercollider")
     (synopsis "Synthesis engine and programming language")
     (description "SuperCollider is a synthesis engine (@code{scsynth} or

gnu/packages/documentation.scm
@@ -371,7 +371,7 @@ local system.")
                (let* ((out (assoc-ref outputs "out"))
                       (bin (string-append out "/bin/zeal"))
                       (qt-process-path (string-append
-                                        (assoc-ref inputs "qtwebengine")
+                                        (assoc-ref inputs "qtwebengine-5")
                                         "/lib/qt5/libexec/QtWebEngineProcess")))
                  (wrap-program bin
                    `("QTWEBENGINEPROCESS_PATH" = (,qt-process-path)))
@@ -384,7 +384,7 @@ local system.")
          ("qtbase" ,qtbase-5)
          ("qtdeclarative-5" ,qtdeclarative-5)
          ("qtwebchannel-5" ,qtwebchannel-5)
-         ("qtwebengine" ,qtwebengine)
+         ("qtwebengine-5" ,qtwebengine-5)
          ("qtquickcontrols-5" ,qtquickcontrols-5)
          ("qtx11extras" ,qtx11extras)
          ("xcb-util-keyms" ,xcb-util-keysyms)))

gnu/packages/ebook.scm
@@ -202,7 +202,7 @@ with Microsoft Compiled HTML (CHM) files")
            python-regex
            speech-dispatcher
            python-zeroconf
-           qtwebengine
+           qtwebengine-5
            sqlite))
     (arguments
      (list

gnu/packages/education.scm
@@ -448,7 +448,7 @@ specialized device.")
 (define-public openboard
   ;; The last release builds from qtwebkit, which is planned for removal in
   ;; Guix, so use the latest commit of the 1.7-dev branch, which builds with
-  ;; qtwebengine.
+  ;; qtwebengine-5.
   (let ((commit "39e914f600d26565706f0e5b6ea2482b8b4038c7") ;1.6.2-rc0311
         (revision "1"))
     (package
@@ -562,7 +562,7 @@ specialized device.")
              qtmultimedia-5
              qtsvg-5
              qtwebchannel-5
-             qtwebengine
+             qtwebengine-5
              qtxmlpatterns
              quazip-0
              sdl
@@ -807,9 +807,9 @@ stored and user can review his performance in any time.")
        ("python-requests" ,python-requests)
        ("python-send2trash" ,python-send2trash)
        ("python-sip" ,python-sip)
-       ;; `qtwebengine' is included in `pyqtwebengine', included here for easy
+       ;; `qtwebengine-5' is included in `pyqtwebengine', included here for easy
        ;; wrapping.
-       ("qtwebengine" ,qtwebengine)))
+       ("qtwebengine-5" ,qtwebengine-5)))
     (home-page "https://apps.ankiweb.net/")
     (synopsis "Powerful, intelligent flash cards")
     (description "Anki is a program which makes remembering things
@@ -999,7 +999,7 @@ formats.")
            qtquickcontrols2-5
            qtdeclarative-5
            qtwebchannel-5
-           qtwebengine
+           qtwebengine-5
            zlib))
     (synopsis "EPWING dictionary reader")
     (description "qolibri is a dictionary viewer for the EPWING dictionary

gnu/packages/emacs-xyz.scm
@@ -13111,7 +13111,7 @@ programming and reproducible research.")
        (sha256
         (base32 "17aca4mc3gbdh6nhlcaa5ymh1yy76nwysrvy9sfcqkzvd5lgagzc"))
        ;; XXX: ob-sclang.el is packaged separately to avoid the dependency on
-       ;; SuperCollider and qtwebengine.  This will be unnecessary in 0.4+
+       ;; SuperCollider and qtwebengine-5.  This will be unnecessary in 0.4+
        ;; release as the file is going to be removed from the repository.
 
        ;; XXX: org-contacts.el is now maintained in a separate repository and

gnu/packages/engineering.scm
@@ -2493,7 +2493,7 @@ comments.")))
            qtdeclarative-5
            qtsvg-5
            qtwebchannel-5
-           qtwebengine
+           qtwebengine-5
            qtx11extras
            qtxmlpatterns
            sqlite

gnu/packages/fcitx5.scm
@@ -394,7 +394,7 @@ the Anthy input method.")
        ("fcitx5-qt" ,fcitx5-qt)
        ("qtdeclarative-5" ,qtdeclarative-5)
        ("qtwebchannel-5" ,qtwebchannel-5)
-       ("qtwebengine" ,qtwebengine)))
+       ("qtwebengine-5" ,qtwebengine-5)))
     (native-inputs
      (list extra-cmake-modules pkg-config))
     (outputs '("out" "gui"))

commit aea756ea3312ba7e8229804492ba12001c8de568

gnu: qutebrowser: Fix build.
Fixes <https://issues.guix.gnu.org/56864>, which is a regression introduced
with a0beb297a352930b89051ff258a06d78f224d6b9.

* gnu/packages/web-browsers.scm (qutebrowser)
[phases]{wrap-qt-process-path}: Do not use the (no longer valid)
"qtwebengine-5" label; instead, locate QtWebEngineProcess via
'search-input-file'.

Reported-by: Jack Hill <jackhill@jackhill.us>

gnu/packages/web-browsers.scm
@@ -540,13 +540,10 @@ interface.")
                      (string-append out "/share/qutebrowser/userscripts")))))))
          (add-after 'wrap 'wrap-qt-process-path
            (lambda* (#:key inputs outputs #:allow-other-keys)
-             (let* ((out (assoc-ref outputs "out"))
-                    (bin (string-append out "/bin/qutebrowser"))
-                    (qt-process-path (string-append
-                                      (assoc-ref inputs "qtwebengine-5")
-                                      "/lib/qt5/libexec/QtWebEngineProcess")))
-               (wrap-program bin
-                 `("QTWEBENGINEPROCESS_PATH" = (,qt-process-path)))))))))
+             (wrap-program (search-input-file outputs "bin/qutebrowser")
+               `("QTWEBENGINEPROCESS_PATH" =
+                 (,(search-input-file
+                    inputs "/lib/qt5/libexec/QtWebEngineProcess")))))))))
     (home-page "https://qutebrowser.org/")
     (synopsis "Minimal, keyboard-focused, vim-like web browser")
     (description "qutebrowser is a keyboard-focused browser with a minimal