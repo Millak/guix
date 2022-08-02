b4d9493ceb101cb773298bdc89375e2a0778ab08
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2022-08-02 11:52:54 +0200

    gnu: scummvm: Update to 2.6.0.
    * gnu/packages/emulators.scm (scummvm): Update to 2.6.0.

--- gnu/packages/emulators.scm
+++ gnu/packages/emulators.scm
@@ -1505,14 +1505,14 @@ multi-system game/emulator system.")
 (define-public scummvm
   (package
     (name "scummvm")
-    (version "2.5.1")
+    (version "2.6.0")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://downloads.scummvm.org/frs/scummvm/" version
                            "/scummvm-" version ".tar.xz"))
        (sha256
-        (base32 "00az0dm85kh8rq0yqj74x0c5zrq4ybvwvnilijzl8qa5whwdpn4z"))))
+        (base32 "05zw9xqdix88f8p3py2rfnyiaxr2sbifkqi9s5gy3nf9s3l3h50w"))))
     (build-system gnu-build-system)
     (arguments
      `(#:tests? #f                                 ;require "git"
