544ff0b6c2b189b61ae2fa252e6e5233420d06a5
Efraim Flashner <efraim@flashner.co.il>
2022-08-07 16:13:36 +0300

    gnu: shishi: Update to 1.0.3.
    * gnu/packages/kerberos.scm (shishi): Update to 1.0.3.
    [source]: Remove patch.
    [arguments]: Remove configure flag overriding libgcrypt detection.
    * gnu/packages/patches/shishi-fix-libgcrypt-detection.patch: Remove file.
    * gnu/local.mk (dist_patch_DATA): Remove it.

--- gnu/local.mk
+++ gnu/local.mk
@@ -1815,7 +1815,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/serf-python3.patch			\
   %D%/packages/patches/shakespeare-spl-fix-grammar.patch		\
   %D%/packages/patches/sharutils-CVE-2018-1000097.patch		\
-  %D%/packages/patches/shishi-fix-libgcrypt-detection.patch	\
   %D%/packages/patches/slim-session.patch			\
   %D%/packages/patches/slim-config.patch			\
   %D%/packages/patches/slim-sigusr1.patch			\
