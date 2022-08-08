64a070717c3de32332201df5d6d2d52a7f99dce9
Ludovic Courtès <ludo@gnu.org>
2022-08-08 17:37:12 +0200

    channels: Add 'repository->guix-channel'.
    * guix/channels.scm (repository->guix-channel): New procedure.
    * guix/scripts/describe.scm (display-checkout-info): Use it instead of
    the (git) interface, and adjust accordingly.

--- guix/channels.scm
+++ guix/channels.scm
@@ -77,6 +77,7 @@
             %default-guix-channel
             %default-channels
             guix-channel?
+            repository->guix-channel
 
             channel-instance?
             channel-instance-channel
@@ -201,6 +202,26 @@ introduction, add it."
       (channel (inherit chan)
                (introduction %guix-channel-introduction))
       chan))
+
+(define* (repository->guix-channel directory
+                                   #:key
+                                   (introduction %guix-channel-introduction))
+  "Look for a Git repository in DIRECTORY or its ancestors and return a
+channel that uses that repository and the commit HEAD currently points to; use
+INTRODUCTION as the channel's introduction.  Return #f if no Git repository
+could be found at DIRECTORY or one of its ancestors."
+  (catch 'git-error
+    (lambda ()
+      (with-repository (repository-discover directory) repository
+        (let* ((head   (repository-head repository))
+               (commit (oid->string (reference-target head))))
+          (channel
+           (inherit %default-guix-channel)
+           (url (repository-working-directory repository))
+           (commit commit)
+           (branch (reference-shorthand head))
+           (introduction introduction)))))
+    (const #f)))
 
 (define-record-type <channel-instance>
   (channel-instance channel commit checkout)
