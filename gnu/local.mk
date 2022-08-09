9a53a79fc54e01932bd727ec519403ea4c2c3efc
Efraim Flashner <efraim@flashner.co.il>
2022-08-09 22:39:26 +0300

    gnu: gnupg: Patch CVE-2022-34903.
    * gnu/packages/gnupg.scm (gnupg)[replacement]: New field.
    (gnupg/replacement): New variable.
    * gnu/packages/patches/gnupg-CVE-2022-34903.patch: New file.
    * gnu/local.mk (dist_patch_DATA): Register it.

--- gnu/local.mk
+++ gnu/local.mk
@@ -1213,6 +1213,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/gnome-session-support-elogind.patch      \
   %D%/packages/patches/gnome-todo-libportal.patch		\
   %D%/packages/patches/gnome-tweaks-search-paths.patch		\
+  %D%/packages/patches/gnupg-CVE-2022-34903.patch		\
   %D%/packages/patches/gnupg-default-pinentry.patch		\
   %D%/packages/patches/gnupg-1-build-with-gcc10.patch		\
   %D%/packages/patches/gnutls-skip-trust-store-test.patch	\
