05c7e5533979cc56d34082568bbc0d5b70a41b34
Ricardo Wurmus <rekado@elephly.net>
2022-08-10 12:20:07 +0200

    gnu: r-partitions: Update to 1.10-7.
    * gnu/packages/cran.scm (r-partitions): Update to 1.10-7.
    [propagated-inputs]: Add r-rdpack.

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -17523,17 +17523,17 @@ customizable sets, and intervals.")
 (define-public r-partitions
   (package
     (name "r-partitions")
-    (version "1.10-4")
+    (version "1.10-7")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "partitions" version))
        (sha256
         (base32
-         "1ik7cj2yfybx4v847nvnjrbkd0k66pfqqv6lkygfnlrldi0r205i"))))
+         "1kxiy1wl7dr39kyfg2y9hd67s32knd83r2jsb7j9p3vg8iqq7yqb"))))
     (build-system r-build-system)
     (propagated-inputs
-     (list r-gmp r-mathjaxr r-polynom r-sets))
+     (list r-gmp r-mathjaxr r-polynom r-rdpack r-sets))
     (home-page "https://cran.r-project.org/web/packages/partitions")
     (synopsis "Additive partitions of integers")
     (description
