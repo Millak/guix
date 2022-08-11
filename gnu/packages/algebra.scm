c1a6c6e6d74861b3371fecc9e992b99993564b85
Andreas Enge <andreas@enge.fr>
2022-08-11 12:13:47 +0200

    gnu: gap: Update to 4.11.1.
    * gnu/packages/algebra.scm (gap): Update to 4.11.1.
    [source]<snippet>: Remove trailing #t.
    [arguments]<phases>: Remove trailing #t. Install missing config.h header.
    
    Co-authored-by: vicvbcun <guix@ikherbers.com>

--- gnu/packages/algebra.scm
+++ gnu/packages/algebra.scm
@@ -1206,17 +1206,17 @@ xtensor provides:
 (define-public gap
   (package
     (name "gap")
-    (version "4.11.0")
+    (version "4.11.1")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://files.gap-system.org/gap-"
                            (version-major+minor version)
-                           "/tar.bz2/gap-"
+                           "/tar.gz/gap-"
                            version
-                           ".tar.bz2"))
+                           ".tar.gz"))
        (sha256
-        (base32 "00l6hvy4iggnlrib4vp805sxdm3j7n3hzpv5zs9hbiiavh80l1xz"))
+        (base32 "01535s81h254zcs84zi95xqmhvvn6fn9qss8761myxc2gpdcadb6"))
        (modules '((guix build utils) (ice-9 ftw) (srfi srfi-1)))
        (snippet
         '(begin
@@ -1249,9 +1249,7 @@ xtensor provides:
                    "alnuth-"
                    "autpgrp-"
                    "crisp-"      ; bsd-2
-                   "ctbllib"     ; gpl3+, clarified in the next release;
-                                 ; see
-                                 ; http://www.math.rwth-aachen.de/~Thomas.Breuer/ctbllib/README.md
+                   "ctbllib"     ; gpl3+
                    "FactInt-"
                    "fga"
                    "irredsol-"   ; bsd-2
@@ -1262,8 +1260,7 @@ xtensor provides:
                    "resclasses-"
                    "sophus-"
                    "tomlib-"
-                   "utils-"))))
-           #t))))
+                   "utils-"))))))))
     (build-system gnu-build-system)
     (inputs
      (list gmp readline zlib))
@@ -1280,14 +1277,12 @@ xtensor provides:
            (lambda _
              (setenv "CONFIG_SHELL" (which "bash"))
              (with-directory-excursion "pkg"
-               (invoke "../bin/BuildPackages.sh")
-             #t)))
+               (invoke "../bin/BuildPackages.sh"))))
          (add-after 'build-packages 'build-doc
            ;; The documentation is bundled, but we create it from source.
            (lambda _
              (with-directory-excursion "doc"
-               (invoke "./make_doc"))
-             #t))
+               (invoke "./make_doc"))))
          (replace 'install
            (lambda* (#:key outputs #:allow-other-keys)
              (let* ((out (assoc-ref outputs "out"))
@@ -1312,6 +1307,8 @@ xtensor provides:
                (chmod prog #o755)
                ;; Install the headers and library, which are needed by Sage.
                (invoke "make" "install-headers")
+               (install-file "gen/config.h"
+                             (string-append out "/include/gap"))
                (invoke "make" "install-libgap")
                ;; Remove information on the build directory from sysinfo.gap.
                (substitute* "sysinfo.gap"
@@ -1321,8 +1318,7 @@ xtensor provides:
                (invoke "make" "install-gaproot")
                ;; Copy the directory of compiled packages; the make target
                ;; install-pkg is currently empty.
-               (copy-recursively "pkg" (string-append share "/pkg")))
-             #t)))))
+               (copy-recursively "pkg" (string-append share "/pkg"))))))))
     (home-page "https://www.gap-system.org/")
     (synopsis
      "System for computational group theory")
