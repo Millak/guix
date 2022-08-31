c188cf57f161c0c26e2d7c8516bd1ddd1492d686
Leo Famulari <leo@famulari.name>
2022-08-31 14:42:17 -0400

    gnu: linux-libre: Update to 5.19.6.
    * gnu/packages/linux.scm (linux-libre-version, linux-libre-gnu-revision,
    linux-libre-pristine-source, linux-libre-source, linux-libre,
    linux-libre-with-bpf): Use linux-libre 5.19.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -964,11 +964,11 @@ It has been modified to remove all non-free binary blobs.")
                      '("x86_64-linux" "i686-linux" "armhf-linux" "aarch64-linux" "riscv64-linux")
                      #:configuration-file kernel-config))
 
-(define-public linux-libre-version         linux-libre-5.18-version)
-(define-public linux-libre-gnu-revision    linux-libre-5.18-gnu-revision)
-(define-public linux-libre-pristine-source linux-libre-5.18-pristine-source)
-(define-public linux-libre-source          linux-libre-5.18-source)
-(define-public linux-libre                 linux-libre-5.18)
+(define-public linux-libre-version         linux-libre-5.19-version)
+(define-public linux-libre-gnu-revision    linux-libre-5.19-gnu-revision)
+(define-public linux-libre-pristine-source linux-libre-5.19-pristine-source)
+(define-public linux-libre-source          linux-libre-5.19-source)
+(define-public linux-libre                 linux-libre-5.19)
 
 (define-public linux-libre-5.15
   (make-linux-libre* linux-libre-5.15-version
@@ -1193,9 +1193,9 @@ It has been modified to remove all non-free binary blobs.")
 (define-public linux-libre-with-bpf
   (let ((base-linux-libre
          (make-linux-libre*
-          linux-libre-5.18-version
-          linux-libre-5.18-gnu-revision
-          linux-libre-5.18-source
+          linux-libre-5.19-version
+          linux-libre-5.19-gnu-revision
+          linux-libre-5.19-source
           '("x86_64-linux" "i686-linux" "armhf-linux"
             "aarch64-linux" "riscv64-linux")
           #:extra-version "bpf"
