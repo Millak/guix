5841b21c4f158294ccd3503157db64254d9d3488
Ricardo Wurmus <rekado@elephly.net>
2022-09-01 11:13:44 +0200

    gnu: Add rust-windows-x86-64-gnu-0.32.
    * gnu/packages/crates-io.scm (rust-windows-x86-64-gnu-0.32): New variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -73930,6 +73930,26 @@ crate.")
 crate.")
     (license (list license:expat license:asl2.0))))
 
+(define-public rust-windows-x86-64-gnu-0.32
+  (package
+    (name "rust-windows-x86-64-gnu")
+    (version "0.32.0")
+    (source (origin
+              (method url-fetch)
+              (uri (crate-uri "windows_x86_64_gnu" version))
+              (file-name (string-append name "-" version ".tar.gz"))
+              (sha256
+               (base32
+                "1g34xhcayig9sndq3555w95q6lr7jr839zxv6l365ijlfhpv24n9"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t))
+    (home-page "https://github.com/microsoft/windows-rs")
+    (synopsis "Code gen support for the windows crate")
+    (description "This package provides code gen support for the windows
+crate.")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-windows-i686-msvc-0.28
   (package
     (name "rust-windows-i686-msvc")
