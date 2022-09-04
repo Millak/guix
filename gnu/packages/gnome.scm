e40c57ba49917a34906a8769379df120c075536f
Christopher Baines <mail@cbaines.net>
2022-09-04 18:21:52 +0100

    gnu: shotwell: Update to 0.30.16.
    * gnu/packages/gnome.scm (shotwell): Update to 0.30.16.

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -7107,7 +7107,7 @@ metadata in photo and video files of various formats.")
 (define-public shotwell
   (package
     (name "shotwell")
-    (version "0.30.12")
+    (version "0.30.16")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://gnome/sources/shotwell/"
@@ -7115,7 +7115,7 @@ metadata in photo and video files of various formats.")
                                   "shotwell-" version ".tar.xz"))
               (sha256
                (base32
-                "1h5crjq59lqi8f8mdkadzi8pc3i9i2ik4lsx2nrhzq486fzdfhw6"))))
+                "1yhqfmz49qkls4i6xaznm5ybwzv19jxsqjdic60wn0ykvbxjr269"))))
     (build-system meson-build-system)
     (arguments
      '(#:glib-or-gtk? #t
