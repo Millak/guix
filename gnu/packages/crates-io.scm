4d4e2a9444783843cbe0cbe6ecea47e351db211d
Ricardo Wurmus <rekado@elephly.net>
2022-09-20 16:53:38 +0200

    gnu: Add rust-sprs-0.11.
    * gnu/packages/crates-io.scm (rust-sprs-0.11): New variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -59401,6 +59401,40 @@ OIDs)")
     (description "Simple SPMC channel")
     (license (list license:expat license:asl2.0))))
 
+(define-public rust-sprs-0.11
+  (package
+    (name "rust-sprs")
+    (version "0.11.0")
+    (source (origin
+              (method url-fetch)
+              (uri (crate-uri "sprs" version))
+              (file-name (string-append name "-" version ".tar.gz"))
+              (sha256
+               (base32
+                "0w6xishm9jxqjy83as7g14474cn1xb55vqz8afqw9d7and41x9vf"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t ;8 test failures due to "No such file or directory"
+       #:cargo-inputs
+       (("rust-alga" ,rust-alga-0.9)
+        ("rust-approx" ,rust-approx-0.5)
+        ("rust-ndarray" ,rust-ndarray-0.15)
+        ("rust-num-complex" ,rust-num-complex-0.4)
+        ("rust-num-traits" ,rust-num-traits-0.2)
+        ("rust-num-cpus" ,rust-num-cpus-1)
+        ("rust-rayon" ,rust-rayon-1)
+        ("rust-serde" ,rust-serde-1)
+        ("rust-smallvec" ,rust-smallvec-1))
+       #:cargo-development-inputs
+       (("rust-bencher" ,rust-bencher-0.1)
+        ("rust-bincode" ,rust-bincode-1)
+        ("rust-tobj" ,rust-tobj-3))))
+    (home-page "https://github.com/vbarrielle/sprs")
+    (synopsis "Sparse matrix library")
+    (description "This package provides a sparse matrix library.")
+    ;; Either license can be chosen
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-sprs-0.10
   (package
     (name "rust-sprs")
