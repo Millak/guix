40a42c4348635f186553938015f7e6a06be1e10c
Efraim Flashner <efraim@flashner.co.il>
2022-09-23 09:05:47 +0300

    gnu: linux-libre: Support zram on all kernels.
    * gnu/packages/linux.scm (%default-extra-linux-options): Add config
    options to add zram support.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -696,6 +696,10 @@ for ARCH and optionally VARIANT, or #f if there is no such configuration."
     ("CONFIG_PROC_PID_CPUSET" . #t)
     ;; Allow disk encryption by default
     ("CONFIG_DM_CRYPT" . m)
+    ;; Support zram on all kernel configs
+    ("CONFIG_ZSWAP" . #t)
+    ("CONFIG_ZSMALLOC" . #t)
+    ("CONFIG_ZRAM" . m)
     ;; Modules required for initrd:
     ("CONFIG_NET_9P" . m)
     ("CONFIG_NET_9P_VIRTIO" . m)
