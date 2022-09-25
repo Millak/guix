7a8988730c3e53e0c5bda3b2167d2fc2d06b9e26
Fredrik Salomonsson <plattfot@posteo.net>
2022-09-25 01:50:13 +0000

    gnu: Add emacs-ol-notmuch.
    * gnu/packages/emacs-xyz.scm (emacs-ol-notmuch): New variable.
    
    Signed-off-by: Nicolas Goaziou <mail@nicolasgoaziou.fr>

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -7055,6 +7055,34 @@ source code using IPython.")
 src blocks.")
     (license license:gpl3+)))
 
+(define-public emacs-ol-notmuch
+  (let ((commit "1a53d6c707514784cabf33d865b577bf77f45913")
+        (revision "0"))
+    (package
+      (name "emacs-ol-notmuch")
+      (version (git-version "2.0.0" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://git.sr.ht/~tarsius/ol-notmuch")
+               (commit commit)))
+         (sha256
+          (base32
+           "16p7j51z8r047alwn2hkb6944f7ds29ckb97b4k8ia00vwch0d67"))
+         (file-name (git-file-name name version))))
+      (build-system emacs-build-system)
+      (propagated-inputs
+       (list emacs-compat emacs-notmuch emacs-org))
+      (home-page "https://git.sr.ht/~tarsius/ol-notmuch")
+      (synopsis "Links to notmuch messages for Emacs' Org mode")
+      (description
+       "This package implements links to Notmuch messages and searches for
+Emacs' Org mode.  A search is a query to be performed by Notmuch; it is the
+equivalent to folders in other mail clients.  Similarly, mails are referred to
+by a query, so both a link can refer to several mails.")
+      (license license:gpl3+))))
+
 (define-public emacs-debbugs
   (package
     (name "emacs-debbugs")
