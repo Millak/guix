Millak/guix

commit d6d861c6151fb2cb9296b993425d9a64a7653d95

gnu: eog-plugins: Update to 42.1.
* gnu/packages/gnome.scm (eog-plugins): Update to 42.1.
[build-system]: Use meson-build-system.
[arguments]: Delete field.
[inputs]: Sort; add python.
[native-inputs]: Sort; add libgdata.

gnu/packages/gnome.scm
@@ -6736,7 +6736,7 @@ supports image conversion, rotation, and slideshows.")
   ;; 'XDG_DATA_DIRS' appropriately set.
   (package
     (name "eog-plugins")
-    (version "3.26.8")
+    (version "42.1")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://gnome/sources/eog-plugins/"
@@ -6744,30 +6744,22 @@ supports image conversion, rotation, and slideshows.")
                                   "eog-plugins-" version ".tar.xz"))
               (sha256
                (base32
-                "1dcxdjp7x092wn0iq7975f9b05ksb5kl5mxmyiqmydrja91ryw40"))))
-    (build-system gnu-build-system)
-    (arguments
-     '(#:phases
-       (modify-phases %standard-phases
-         (add-after 'install 'fix-plugins-path
-           ;; XXX: EOG looks its plugins in "share/eog/plugins", not in
-           ;; "lib/eog/plugins".
-           (lambda* (#:key outputs #:allow-other-keys)
-             (let* ((out (assoc-ref outputs "out"))
-                    (plugins (string-append out "/share/eog/plugins")))
-               (mkdir (dirname plugins))
-               (symlink (string-append out "/lib/eog/plugins") plugins)))))))
+                "0prymlrfh66p03va5aj30wazshp7bn80gzcsj9dgsmss2k512wlb"))))
+    (build-system meson-build-system)
     (home-page "https://wiki.gnome.org/Apps/EyeOfGnome/Plugins")
     (synopsis "Extensions for the Eye of GNOME image viewer")
     (native-inputs
-     (list pkg-config gettext-minimal))
+     (list gettext-minimal
+           pkg-config
+           python))
     (inputs
      (list eog
            glib
            gtk+
-           libpeas
+           libchamplain
            libexif
-           libchamplain))
+           libgdata
+           libpeas))
     (description
      "This package provides plugins for the Eye of GNOME (EOG) image viewer,
 notably:

commit ecee392ef1b9335afb2268c889d891a9ba1a4681

gnu: eog-plugins: Fix source URL.
The incorrect URL was introduced in
d6d861c6151fb2cb9296b993425d9a64a7653d95.

* gnu/packages/gnome.scm (eog-plugins)[source]: Change directory in URL.

gnu/packages/gnome.scm
@@ -6904,7 +6904,7 @@ supports image conversion, rotation, and slideshows.")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://gnome/sources/eog-plugins/"
-                                  (version-major+minor version) "/"
+                                  (version-major version) "/"
                                   "eog-plugins-" version ".tar.xz"))
               (sha256
                (base32