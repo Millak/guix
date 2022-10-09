Millak/guix

commit ccc2658e2a111590ec25e308946e47a24d357785

build-system: Factorize 'strip' flags and directories.
Previously only 'gnu-build-system' would default to "--strip-unneeded",
since commit e0f31baacc6ad30096a332b69433c85f5830bb2c; most other build
systems would default to "--strip-debug".

This change brings consistency.

* guix/build-system/gnu.scm (%strip-flags, %strip-directories): Export.
* guix/build-system/cmake.scm (cmake-build, cmake-cross-build): Use them.
* guix/build-system/copy.scm (copy-build): Likewise.
* guix/build-system/dune.scm (dune-build): Likewise.
* guix/build-system/glib-or-gtk.scm (glib-or-gtk-build)
(glib-or-gtk-cross-build): Likewise.
* guix/build-system/maven.scm (maven-build): Likewise.
* guix/build-system/meson.scm (meson-build, meson-cross-build): Likewise.
* guix/build-system/ocaml.scm (ocaml-build): Likewise.
* guix/build-system/qt.scm (qt-build, qt-cross-build): Likewise.

guix/build-system/cmake.scm
@@ -110,9 +110,8 @@
                       (validate-runpath? #t)
                       (patch-shebangs? #t)
                       (strip-binaries? #t)
-                      (strip-flags ''("--strip-debug"))
-                      (strip-directories ''("lib" "lib64" "libexec"
-                                            "bin" "sbin"))
+                      (strip-flags %strip-flags)
+                      (strip-directories %strip-directories)
                       (phases '%standard-phases)
                       (system (%current-system))
                       (substitutable? #t)
@@ -185,10 +184,8 @@ provides a 'CMakeLists.txt' file as its build system."
                             (validate-runpath? #t)
                             (patch-shebangs? #t)
                             (strip-binaries? #t)
-                            (strip-flags ''("--strip-debug"
-                                            "--enable-deterministic-archives"))
-                            (strip-directories ''("lib" "lib64" "libexec"
-                                                  "bin" "sbin"))
+                            (strip-flags %strip-flags)
+                            (strip-directories %strip-directories)
                             (phases '%standard-phases)
                             (substitutable? #t)
                             (system (%current-system))

guix/build-system/copy.scm
@@ -1,7 +1,7 @@
 ;;; GNU Guix --- Functional package management for GNU
 ;;; Copyright © 2019 Julien Lepiller <julien@lepiller.eu>
 ;;; Copyright © 2020 Pierre Neidhardt <mail@ambrevar.xyz>
-;;; Copyright © 2021 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2021, 2022 Ludovic Courtès <ludo@gnu.org>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -88,9 +88,8 @@
                      (validate-runpath? #t)
                      (patch-shebangs? #t)
                      (strip-binaries? #t)
-                     (strip-flags ''("--strip-debug"))
-                     (strip-directories ''("lib" "lib64" "libexec"
-                                           "bin" "sbin"))
+                     (strip-flags %strip-flags)
+                     (strip-directories %strip-directories)
                      (phases '(@ (guix build copy-build-system)
                                  %standard-phases))
                      (system (%current-system))

guix/build-system/glib-or-gtk.scm
@@ -1,5 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2013, 2014, 2015, 2019, 2020, 2021 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2013-2015, 2019-2022 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2013 Cyril Roelandt <tipecaml@gmail.com>
 ;;; Copyright © 2014 Federico Beffa <beffa@fbengineering.ch>
 ;;; Copyright © 2021 Maxime Devos <maximedevos@telenet.be>
@@ -139,9 +139,8 @@
                             (validate-runpath? #t)
                             (patch-shebangs? #t)
                             (strip-binaries? #t)
-                            (strip-flags ''("--strip-debug"))
-                            (strip-directories ''("lib" "lib64" "libexec"
-                                                  "bin" "sbin"))
+                            (strip-flags %strip-flags)
+                            (strip-directories %strip-directories)
                             (phases '(@ (guix build glib-or-gtk-build-system)
                                         %standard-phases))
                             (glib-or-gtk-wrap-excluded-outputs ''())
@@ -214,9 +213,8 @@
                                   (make-dynamic-linker-cache? #f)
                                   (patch-shebangs? #t)
                                   (strip-binaries? #t)
-                                  (strip-flags ''("--strip-debug"))
-                                  (strip-directories ''("lib" "lib64" "libexec"
-                                                        "bin" "sbin"))
+                                  (strip-flags %strip-flags)
+                                  (strip-directories %strip-directories)
                                   (phases '(@ (guix build glib-or-gtk-build-system)
                                               %standard-phases))
                                   (glib-or-gtk-wrap-excluded-outputs ''())

guix/build-system/maven.scm
@@ -1,6 +1,6 @@
 ;;; GNU Guix --- Functional package management for GNU
 ;;; Copyright © 2020 Julien Lepiller <julien@lepiller.eu>
-;;; Copyright © 2021 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2021, 2022 Ludovic Courtès <ludo@gnu.org>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -154,9 +154,8 @@
                       (exclude %default-exclude)
                       (local-packages '())
                       (tests? #t)
-                      (strip-flags ''("--strip-debug"))
-                      (strip-directories ''("lib" "lib64" "libexec"
-                                            "bin" "sbin"))
+                      (strip-flags %strip-flags)
+                      (strip-directories %strip-directories)
                       (phases '%standard-phases)
                       (system (%current-system))
                       (imported-modules %maven-build-system-modules)

guix/build-system/meson.scm
@@ -1,7 +1,7 @@
 ;;; GNU Guix --- Functional package management for GNU
 ;;; Copyright © 2017 Peter Mikkelsen <petermikkelsen10@gmail.com>
 ;;; Copyright © 2018, 2019 Marius Bakke <mbakke@fastmail.com>
-;;; Copyright © 2021 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2021, 2022 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2021 Maxime Devos <maximedevos@telenet.be>
 ;;; Copyright © 2022 Efraim Flashner <efraim@flashner.co.il>
 ;;;
@@ -180,9 +180,8 @@ TRIPLET."
                       (validate-runpath? #t)
                       (patch-shebangs? #t)
                       (strip-binaries? #t)
-                      (strip-flags ''("--strip-debug"))
-                      (strip-directories ''("lib" "lib64" "libexec"
-                                            "bin" "sbin"))
+                      (strip-flags %strip-flags)
+                      (strip-directories %strip-directories)
                       (elf-directories ''("lib" "lib64" "libexec"
                                           "bin" "sbin"))
                       (phases '%standard-phases)
@@ -262,9 +261,8 @@ has a 'meson.build' file."
                             (validate-runpath? #t)
                             (patch-shebangs? #t)
                             (strip-binaries? #t)
-                            (strip-flags ''("--strip-debug"))
-                            (strip-directories ''("lib" "lib64" "libexec"
-                                                  "bin" "sbin"))
+                            (strip-flags %strip-flags)
+                            (strip-directories %strip-directories)
                             (elf-directories ''("lib" "lib64" "libexec"
                                                 "bin" "sbin"))
                             ;; See 'gnu-cross-build' for why this needs to be

guix/build-system/dune.scm
@@ -1,7 +1,7 @@
 ;;; GNU Guix --- Functional package management for GNU
 ;;; Copyright © 2016, 2017, 2018 Julien Lepiller <julien@lepiller.eu>
 ;;; Copyright © 2017 Ben Woodcroft <donttrustben@gmail.com>
-;;; Copyright © 2021 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2021, 2022 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2021 pukkamustard <pukkamustard@posteo.net>
 ;;;
 ;;; This file is part of GNU Guix.
@@ -112,9 +112,8 @@
                      (validate-runpath? #t)
                      (patch-shebangs? #t)
                      (strip-binaries? #t)
-                     (strip-flags ''("--strip-debug"))
-                     (strip-directories ''("lib" "lib64" "libexec"
-                                           "bin" "sbin"))
+                     (strip-flags gnu:%strip-flags)
+                     (strip-directories gnu:%strip-directories)
                      (phases '(@ (guix build dune-build-system)
                                  %standard-phases))
                      (system (%current-system))

guix/build-system/gnu.scm
@@ -1,5 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2012-2022 Ludovic Courtès <ludo@gnu.org>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -29,6 +29,8 @@
   #:use-module (srfi srfi-1)
   #:use-module (ice-9 match)
   #:export (%gnu-build-system-modules
+            %strip-flags
+            %strip-directories
             gnu-build
             gnu-build-system
             standard-packages

guix/build-system/ocaml.scm
@@ -243,9 +243,8 @@ pre-defined variants."
                       (validate-runpath? #t)
                       (patch-shebangs? #t)
                       (strip-binaries? #t)
-                      (strip-flags ''("--strip-debug"))
-                      (strip-directories ''("lib" "lib64" "libexec"
-                                            "bin" "sbin"))
+                      (strip-flags %strip-flags)
+                      (strip-directories %strip-directories)
                       (phases '(@ (guix build ocaml-build-system)
                                   %standard-phases))
                       (system (%current-system))

commit 77759234a3c436816297dd1e989704e37489a129

build-system: Fix uses of #:strip-flags and #:strip-directories.
This fixes a bug introduced in ccc2658e2a111590ec25e308946e47a24d357785,
reported at <https://issues.guix.gnu.org/issue/58384>.

Tested with "./pre-inst-env guix build openjpeg-data".

Fixes: https://issues.guix.gnu.org/issue/58384

* guix/build-system/copy.scm (copy-build): 'strip-flags' and
'#:strip-directories' are G-exps, not S-exps, so don't use sexp->gexp.
* guix/build-system/cmake.scm (copy-build): Likewise.
* guix/build-system/glib-or-gtk.scm
(glib-or-gtk-build,glib-or-gtk-cross-build): Likewise.
* guix/build-system/maven.scm (maven-build): Likewise.
* guix/build-system/meson.scm
(meson-build,meson-cross-build): Likewise.

Signed-off-by: Ludovic Courtès <ludo@gnu.org>

guix/build-system/cmake.scm
@@ -149,8 +149,8 @@ provides a 'CMakeLists.txt' file as its build system."
                              #:validate-runpath? #$validate-runpath?
                              #:patch-shebangs? #$patch-shebangs?
                              #:strip-binaries? #$strip-binaries?
-                             #:strip-flags #$(sexp->gexp strip-flags)
-                             #:strip-directories #$(sexp->gexp strip-directories))))))
+                             #:strip-flags #$strip-flags
+                             #:strip-directories #$strip-directories)))))
 
   (mlet %store-monad ((guile (package->derivation (or guile (default-guile))
                                                   system #:graft? #f)))

guix/build-system/copy.scm
@@ -122,8 +122,8 @@
                             #:validate-runpath? #$validate-runpath?
                             #:patch-shebangs? #$patch-shebangs?
                             #:strip-binaries? #$strip-binaries?
-                            #:strip-flags #$(sexp->gexp strip-flags)
-                            #:strip-directories #$(sexp->gexp strip-directories))))))
+                            #:strip-flags #$strip-flags
+                            #:strip-directories #$strip-directories)))))
 
   (mlet %store-monad ((guile (package->derivation (or guile (default-guile))
                                                   system #:graft? #f)))

guix/build-system/glib-or-gtk.scm
@@ -178,9 +178,9 @@
                                    #:validate-runpath? #$validate-runpath?
                                    #:patch-shebangs? #$patch-shebangs?
                                    #:strip-binaries? #$strip-binaries?
-                                   #:strip-flags #$(sexp->gexp strip-flags)
+                                   #:strip-flags #$strip-flags
                                    #:strip-directories
-                                   #$(sexp->gexp strip-directories))))))
+                                   #$strip-directories)))))
 
 
   (mlet %store-monad ((guile (package->derivation (or guile (default-guile))
@@ -271,9 +271,9 @@
                            #:make-dynamic-linker-cache? #$make-dynamic-linker-cache?
                            #:patch-shebangs? #$patch-shebangs?
                            #:strip-binaries? #$strip-binaries?
-                           #:strip-flags #$(sexp->gexp strip-flags)
+                           #:strip-flags #$strip-flags
                            #:strip-directories
-                           #$(sexp->gexp strip-directories))))
+                           #$strip-directories)))
 
 
   (mlet %store-monad ((guile (package->derivation (or guile (default-guile))

guix/build-system/maven.scm
@@ -183,8 +183,8 @@ provides its own binaries."
                        #:validate-runpath? #$validate-runpath?
                        #:patch-shebangs? #$patch-shebangs?
                        #:strip-binaries? #$strip-binaries?
-                       #:strip-flags #$(sexp->gexp strip-flags)
-                       #:strip-directories #$(sexp->gexp strip-directories)))))
+                       #:strip-flags #$strip-flags
+                       #:strip-directories #$strip-directories))))
 
   (mlet %store-monad ((guile (package->derivation (or guile (default-guile))
                                                   system #:graft? #f)))

guix/build-system/meson.scm
@@ -227,8 +227,8 @@ has a 'meson.build' file."
                              #:validate-runpath? #$validate-runpath?
                              #:patch-shebangs? #$patch-shebangs?
                              #:strip-binaries? #$strip-binaries?
-                             #:strip-flags #$(sexp->gexp strip-flags)
-                             #:strip-directories #$(sexp->gexp strip-directories)
+                             #:strip-flags #$strip-flags
+                             #:strip-directories #$strip-directories
                              #:elf-directories #$(sexp->gexp elf-directories))))))
 
   (mlet %store-monad ((guile (package->derivation (or guile (default-guile))
@@ -339,8 +339,8 @@ SOURCE has a 'meson.build' file."
                        #:validate-runpath? #$validate-runpath?
                        #:patch-shebangs? #$patch-shebangs?
                        #:strip-binaries? #$strip-binaries?
-                       #:strip-flags #$(sexp->gexp strip-flags)
-                       #:strip-directories #$(sexp->gexp strip-directories)
+                       #:strip-flags #$strip-flags
+                       #:strip-directories #$strip-directories
                        #:elf-directories #$(sexp->gexp elf-directories)))))
 
   (mlet %store-monad ((guile (package->derivation (or guile (default-guile))