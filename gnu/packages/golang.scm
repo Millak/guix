638d60c4a22deb7c7cfa66b0707260fb6b742241
Nicolas Graves <ngraves@ngraves.fr>
2022-10-13 23:09:44 +0200

    gnu: Add go-filippo-io-edwards25519.
    * gnu/packages/golang.scm (go-filippo-io-edwards25519): New variable.
    
    Signed-off-by: Tobias Geerinckx-Rice <me@tobias.gr>

--- gnu/packages/golang.scm
+++ gnu/packages/golang.scm
@@ -8365,6 +8365,29 @@ configuration languages, but other uses may be possible too.")
     (description "This package implements a low-level key/value store in Go.")
     (license license:expat)))
 
+(define-public go-filippo-io-edwards25519
+  (package
+    (name "go-filippo-io-edwards25519")
+    (version "1.0.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/FiloSottile/edwards25519")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "01m8hpaj0cwp250f7b0din09cf8j6j5y631grx67qfhvfrmwr1zr"))))
+    (build-system go-build-system)
+    (arguments
+     '(#:import-path "filippo.io/edwards25519"))
+    (home-page "https://filippo.io/edwards25519")
+    (synopsis "Group logic for the twisted Edwards curve")
+    (description
+     "This package implements the edwards25519 elliptic curve in Go, exposing
+the necessary APIs to build a wide array of higher-level primitives.")
+    (license license:bsd-3)))
+
 (define-public go-github-com-rogpeppe-go-internal
   (package
     (name "go-github-com-rogpeppe-go-internal")
