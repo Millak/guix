Millak/guix

commit 602527ab9778165ca9a8f9cb62036038b5354688

git: Factorize 'commit-id?' predicate.
* guix/git.scm (commit-id?): New procedure, copied from (guix swh).
(resolve-reference): Use it instead of inline code.
* guix/inferior.scm (channel-full-commit): Likewise.

guix/git.scm
@@ -62,6 +62,7 @@
             commit-difference
             commit-relation
             commit-descendant?
+            commit-id?
 
             remote-refs
 
@@ -219,6 +220,12 @@ of SHA1 string."
     (last (string-split url #\/)) ".git" "")
    "-" (string-take sha1 7)))
 
+(define (commit-id? str)
+  "Return true if STR is likely a Git commit ID, false otherwise---e.g., if it
+is a tag name.  This is based on a simple heuristic so use with care!"
+  (and (= (string-length str) 40)
+       (string-every char-set:hex-digit str)))
+
 (define (resolve-reference repository ref)
   "Resolve the branch, commit or tag specified by REF, and return the
 corresponding Git object."
@@ -254,8 +261,7 @@ corresponding Git object."
                           #f))
                      (_ #f)))
               => (lambda (commit) (resolve `(commit . ,commit))))
-             ((or (> (string-length str) 40)
-                  (not (string-every char-set:hex-digit str)))
+             ((not (commit-id? str))
               (resolve `(tag . ,str)))      ;definitely a tag
              (else
               (catch 'git-error

guix/inferior.scm
@@ -40,7 +40,7 @@
   #:use-module (guix search-paths)
   #:use-module (guix profiles)
   #:use-module (guix channels)
-  #:use-module ((guix git) #:select (update-cached-checkout))
+  #:use-module ((guix git) #:select (update-cached-checkout commit-id?))
   #:use-module (guix monads)
   #:use-module (guix store)
   #:use-module (guix derivations)
@@ -833,7 +833,7 @@ CHANNEL's 'commit' field is a full SHA1, return it as-is; if it's a SHA1
 prefix, resolve it; and if 'commit' is unset, fetch CHANNEL's branch tip."
   (let ((commit (channel-commit channel))
         (branch (channel-branch channel)))
-    (if (and commit (= (string-length commit) 40))
+    (if (and commit (commit-id? commit))
         commit
         (let* ((ref (if commit `(commit . ,commit) `(branch . ,branch)))
                (cache commit relation

commit 26965563ef581553c9dd37192293a75de84176bd

git: 'update-cached-checkout' recognizes truncated commit IDs.
Fixes a regression introduced in
602527ab9778165ca9a8f9cb62036038b5354688 that broke
'tests/guix-build-branch.sh'.

* guix/git.scm (resolve-reference): Revert change from
602527ab9778165ca9a8f9cb62036038b5354688.

guix/git.scm
@@ -261,7 +261,8 @@ corresponding Git object."
                           #f))
                      (_ #f)))
               => (lambda (commit) (resolve `(commit . ,commit))))
-             ((not (commit-id? str))
+             ((or (> (string-length str) 40)
+                  (not (string-every char-set:hex-digit str)))
               (resolve `(tag . ,str)))      ;definitely a tag
              (else
               (catch 'git-error