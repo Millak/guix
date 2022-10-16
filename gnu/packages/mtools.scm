0c5bc867a1d7eaaba6e7dc5583a79b7b1a2be6be
Tobias Geerinckx-Rice <me@tobias.gr>
2022-10-16 02:00:00 +0200

    gnu: mtools: Update to 4.0.41.
    * gnu/packages/mtools.scm (mtools): Update to 4.0.41.

--- gnu/packages/mtools.scm
+++ gnu/packages/mtools.scm
@@ -29,14 +29,14 @@
 (define-public mtools
   (package
     (name "mtools")
-    (version "4.0.40")
+    (version "4.0.41")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://gnu/mtools/mtools-"
                                   version ".tar.bz2"))
               (sha256
                (base32
-                "1x2xknvyrbqjb6bv0h3p5s0bvss6ici1zxd7jcids4a06m1clbx2"))
+                "04qaz2sz4394dil7l2vxqcxi7y7fyksanqh6sxzgygpvchi1ahi5"))
               (patches
                (search-patches "mtools-mformat-uninitialized.patch"))))
     (build-system gnu-build-system)
