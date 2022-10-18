e5f03e9b19e4f6dc46a166c96ea78e3259aabf8c
Efraim Flashner <efraim@flashner.co.il>
2022-10-18 22:31:45 +0300

    gnu: Remove rust-core-text-13.
    * gnu/packages/crates-io.scm (rust-core-text-13): Delete variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -13914,28 +13914,6 @@ intrinsics.")
     (description "This package provides bindings to the Core Text framework.")
     (license (list license:expat license:asl2.0))))
 
-(define-public rust-core-text-13
-  (package
-    (inherit rust-core-text-19)
-    (name "rust-core-text")
-    (version "13.3.2")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (crate-uri "core-text" version))
-       (file-name
-        (string-append name "-" version ".tar.gz"))
-       (sha256
-        (base32
-         "0v9lxn277p39cf81pb45r7k0lzf17pwgd5cpry1c04ajv556b16v"))))
-    (arguments
-     `(#:skip-build? #t     ; only for macOS
-       #:cargo-inputs
-       (("rust-core-foundation" ,rust-core-foundation-0.6)
-        ("rust-core-graphics" ,rust-core-graphics-0.17)
-        ("rust-foreign-types" ,rust-foreign-types-0.3)
-        ("rust-libc" ,rust-libc-0.2))))))
-
 (define-public rust-cov-mark-2
   (package
     (name "rust-cov-mark")
