62fc55529d2eed7c7ef7eb96c549a0b97f263bc6
Efraim Flashner <efraim@flashner.co.il>
2022-10-18 22:53:05 +0300

    gnu: Remove rust-serde-cbor-0.10.
    * gnu/packages/crates-io.scm (rust-serde-cbor-0.10): Delete variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -53493,29 +53493,6 @@ fragment of code.")
     (description "CBOR support for serde.")
     (license (list license:expat license:asl2.0))))
 
-(define-public rust-serde-cbor-0.10
-  (package
-    (inherit rust-serde-cbor-0.11)
-    (name "rust-serde-cbor")
-    (version "0.10.2")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (crate-uri "serde_cbor" version))
-       (file-name
-        (string-append name "-" version ".tar.gz"))
-       (sha256
-        (base32
-         "0kyizacjabsa78p9f7qvj31zirpnsgsr4zpfv1p6lwpcb3biw27p"))))
-    (arguments
-     `(#:skip-build? #t
-       #:cargo-inputs
-       (("rust-byteorder" ,rust-byteorder-1)
-        ("rust-half" ,rust-half-1)
-        ("rust-serde" ,rust-serde-1))
-       #:cargo-development-inputs
-       (("rust-serde-derive" ,rust-serde-derive-1))))))
-
 (define-public rust-serde-codegen-0.4
   (package
     (name "rust-serde-codegen")
