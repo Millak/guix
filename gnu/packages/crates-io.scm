192f6edcf947b9f3737394102981701225de4248
Efraim Flashner <efraim@flashner.co.il>
2022-10-24 12:37:39 +0300

    gnu: Remove rust-onig-sys-69.2.
    * gnu/packages/crates-io.scm (rust-onig-sys-69.2): Delete variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -37295,25 +37295,6 @@ This crate exposes a set of unsafe functions which can then be used by
 other crates to create safe wrappers around Oniguruma.")
     (license license:expat)))
 
-(define-public rust-onig-sys-69.2
-  (package
-    (inherit rust-onig-sys-69.6)
-    (name "rust-onig-sys")
-    (version "69.2.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (crate-uri "onig_sys" version))
-       (file-name (string-append name "-" version ".tar.gz"))
-       (sha256
-        (base32 "0kjijq29yx05xxg9snvqnfn53dl52hchb4sk3zhfr77mypxlx38a"))))
-    (arguments
-     `(#:skip-build? #t
-       #:cargo-inputs
-       (("rust-bindgen" ,rust-bindgen-0.50)
-        ("rust-cc" ,rust-cc-1)
-        ("rust-pkg-config" ,rust-pkg-config-0.3))))))
-
 (define-public rust-once-cell-1
   (package
     (name "rust-once-cell")
