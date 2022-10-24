c714f9aba80f46cff97e8f864f72e4d5aa7b4aae
Efraim Flashner <efraim@flashner.co.il>
2022-10-24 13:41:27 +0300

    gnu: Remove rust-sysinfo-0.18.
    * gnu/packages/crates-io.scm (rust-sysinfo-0.18): Delete variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -56961,31 +56961,6 @@ processors, disks, components and networks.")
         ("rust-rayon" ,rust-rayon-1)
         ("rust-winapi" ,rust-winapi-0.3))))))
 
-(define-public rust-sysinfo-0.18
-  (package
-    (inherit rust-sysinfo-0.20)
-    (name "rust-sysinfo")
-    (version "0.18.2")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (crate-uri "sysinfo" version))
-       (file-name
-        (string-append name "-" version ".tar.gz"))
-       (sha256
-        (base32 "0zd974yydc4yszxgx9hiqm5ah2rqnsgyq3qrl7raf90scpxaw16l"))))
-    (arguments
-     `(#:skip-build? #t
-       #:cargo-inputs
-       (("rust-cfg-if" ,rust-cfg-if-1)
-        ("rust-core-foundation-sys" ,rust-core-foundation-sys-0.8)
-        ("rust-doc-comment" ,rust-doc-comment-0.3)
-        ("rust-libc" ,rust-libc-0.2)
-        ("rust-ntapi" ,rust-ntapi-0.3)
-        ("rust-once-cell" ,rust-once-cell-1)
-        ("rust-rayon" ,rust-rayon-1)
-        ("rust-winapi" ,rust-winapi-0.3))))))
-
 (define-public rust-syslog-4
   (package
     (name "rust-syslog")
