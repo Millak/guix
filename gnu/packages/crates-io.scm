533f0528cce81368373a6aa6b57885609fe47f49
Efraim Flashner <efraim@flashner.co.il>
2022-10-24 13:40:51 +0300

    gnu: Remove rust-sval-derive-0.4.
    * gnu/packages/crates-io.scm (rust-sval-derive-0.4): Delete variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -56417,27 +56417,6 @@ values without proliferating generics.")
         ("rust-quote" ,rust-quote-1)
         ("rust-syn" ,rust-syn-1))))))
 
-(define-public rust-sval-derive-0.4
-  (package
-    (inherit rust-sval-derive-0.5)
-    (name "rust-sval-derive")
-    (version "0.4.7")
-    (source
-      (origin
-        (method url-fetch)
-        (uri (crate-uri "sval_derive" version))
-        (file-name
-         (string-append name "-" version ".tar.gz"))
-        (sha256
-         (base32
-          "07s7jqsdczsg0wnydfnxyrsj8zyrjmiwl4is1dfgn8dfvyi8n2bj"))))
-    (arguments
-     `(#:skip-build? #t
-       #:cargo-inputs
-       (("rust-proc-macro2" ,rust-proc-macro2-1)
-        ("rust-syn" ,rust-syn-1)
-        ("rust-quote" ,rust-quote-1))))))
-
 (define-public rust-swayipc-2
   (package
     (name "rust-swayipc")
