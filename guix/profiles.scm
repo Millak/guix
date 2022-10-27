b42b0e6d62544a118460678c08f9bf308316f7f9
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2022-10-27 19:59:05 -0400

    profiles: info-dir-file: Remove install-info '--silent' option.
    Relates to <https://issues.guix.gnu.org/58821>.
    
    Revert 4fef1e850e4872f2bc7c1f0a10cbac176b50895f, which silenced useful
    information such as the following warning:
    
       install-info: warning: no info dir entry in
       `/gnu/store/...-rottlog-0.72.2/share/info/rottlog.info.gz'
    
    Rationale: profile hooks output is now hidden by default, and can be made
    visible via the --verbosity option.

--- guix/profiles.scm
+++ guix/profiles.scm
@@ -1034,7 +1034,7 @@ MANIFEST."
               (setenv "LANGUAGE" language)
               (zero?
                (system* #+(file-append texinfo "/bin/install-info")
-                        "--silent" info
+                        info
                         (apply string-append #$output "/share/info/dir"
                                (if (string=? "en" language)
                                    '("")
