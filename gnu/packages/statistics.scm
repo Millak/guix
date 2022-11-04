f49667ca64700aadd98de3f073f6cfdb6fc1863e
Ricardo Wurmus <rekado@elephly.net>
2022-11-04 09:59:03 +0100

    gnu: r-crayon: Update to 1.5.2.
    * gnu/packages/statistics.scm (r-crayon): Update to 1.5.2.

--- gnu/packages/statistics.scm
+++ gnu/packages/statistics.scm
@@ -1661,13 +1661,13 @@ you call it again with the same arguments it returns the pre-computed value.")
 (define-public r-crayon
   (package
     (name "r-crayon")
-    (version "1.5.1")
+    (version "1.5.2")
     (source (origin
               (method url-fetch)
               (uri (cran-uri "crayon" version))
               (sha256
                (base32
-                "1yaxwkmvjaillngfafgapakqjl6s86ly3j338f78xs58g0xwf9f0"))))
+                "0yjsnhifr3nipaid0q11jjglvqmp51g9d2xdh9kfxh5knl2sbabh"))))
     (build-system r-build-system)
     (home-page "https://github.com/gaborcsardi/crayon")
     (synopsis "Colored terminal output for R")
