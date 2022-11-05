4ba780aeb8dec8e813f0cdfb53c386164b1364cd
kiasoc5 <kiasoc5@disroot.org>
2022-11-04 23:50:28 -0400

    gnu: kanshi: Update to 1.3.0.
    * gnu/packages/wm.scm (kanshi): Update to 1.3.0.
    
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/packages/wm.scm
+++ gnu/packages/wm.scm
@@ -1844,7 +1844,7 @@ compositors that support the layer-shell protocol.")
 (define-public kanshi
   (package
     (name "kanshi")
-    (version "1.2.0")
+    (version "1.3.0")
     (source
      (origin
        (method git-fetch)
@@ -1853,7 +1853,7 @@ compositors that support the layer-shell protocol.")
              (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "10lxagwc2pkq86g2sxkwljjd39sahp3w1j5yx853d3c4d95iwls5"))))
+        (base32 "0sa8k74d24ijw6ml1yyy75dk763r2sbm7fgk033g5xnx28kd394j"))))
     (build-system meson-build-system)
     (inputs (list wayland))
     (native-inputs (list pkg-config scdoc))
