7de6aa8a0abdb54727e1dd8f775802284b99e707
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2022-11-08 21:43:14 +0100

    gnu: emacs-detached: Update to 0.9.2.
    * gnu/packages/emacs-xyz.scm (emacs-detached): Update to 0.9.2.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -26030,7 +26030,7 @@ according to their use.")
 (define-public emacs-detached
   (package
     (name "emacs-detached")
-    (version "0.9.1")
+    (version "0.9.2")
     (source (origin
               (method git-fetch)
               (uri (git-reference
@@ -26039,7 +26039,7 @@ according to their use.")
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "0v7g23jmxh2z1v484pzmcavcsxkd5qydfj6075a8abx3kv7n6nj4"))))
+                "0bcrqqim6is75d4zadswxp162m9gd34xmb2wxsx9rhvkwq0ln35k"))))
     (arguments
      (list
       #:tests? #t
