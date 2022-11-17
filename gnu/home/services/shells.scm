Millak/guix

commit 543d971ed2a1d9eb934af1f51930741d7cc4e7ef

services: configuration: Re-order generated record fields.
This is so that the first field of the generated record matches the first one
declared, which makes 'define-configuration' record API compatible with
define-record-type* ones.

* gnu/services/configuration.scm (define-configuration-helper): Move the
%location field below the ones declared by the user.
* gnu/services/monitoring.scm (zabbix-front-end-config): Adjust match pattern
accordingly.

gnu/services/monitoring.scm
@@ -622,8 +622,8 @@ create it manually.")
 
 (define (zabbix-front-end-config config)
   (match-record config <zabbix-front-end-configuration>
-    (%location db-host db-port db-name db-user db-password db-secret-file
-               zabbix-host zabbix-port)
+    (db-host db-port db-name db-user db-password db-secret-file
+             zabbix-host zabbix-port %location)
     (mixed-text-file "zabbix.conf.php"
                      "\
 <?php

gnu/services/configuration.scm
@@ -242,17 +242,17 @@ does not have a default value" field kind)))
                stem
                #,(id #'stem #'make- #'stem)
                #,(id #'stem #'stem #'?)
-               (%location #,(id #'stem #'stem #'-location)
-                          (default (and=> (current-source-location)
-                                          source-properties->location))
-                          (innate))
                #,@(map (lambda (name getter def)
                          #`(#,name #,getter (default #,def)
                                    (sanitize
                                     #,(id #'stem #'validate- #'stem #'- name))))
                        #'(field ...)
                        #'(field-getter ...)
-                       #'(field-default ...)))
+                       #'(field-default ...))
+               (%location #,(id #'stem #'stem #'-location)
+                          (default (and=> (current-source-location)
+                                          source-properties->location))
+                          (innate)))
 
              (define #,(id #'stem #'stem #'-fields)
                (list (configuration-field

commit 238248aac98daa8d455fc0c2244ba3e0175cb8b7

Fix problems initially introduced in commit 543d971ed2, now reinstated.
Commit 543d971ed2 ("services: configuration: Re-order generated record
fields") introduced two regressions, one in guix home and another one in the
zabbix service.

* gnu/home/services/shells.scm (home-bash-extensions): Remove the first
pattern in the match, which used to be to ignore %location.
* gnu/services/monitoring.scm (zabbix-front-end-nginx-extension): Likewise.

gnu/home/services/shells.scm
@@ -480,7 +480,7 @@ with text blocks from other extensions and the base service."))
 
 (define (home-bash-extensions original-config extension-configs)
   (match original-config
-    (($ <home-bash-configuration> _ _ _ environment-variables aliases
+    (($ <home-bash-configuration> _ _ environment-variables aliases
                                   bash-profile bashrc bash-logout)
      (home-bash-configuration
       (inherit original-config)

gnu/services/monitoring.scm
@@ -576,7 +576,7 @@ fastcgi_param PHP_VALUE \"post_max_size = 16M
 
 (define (zabbix-front-end-nginx-extension config)
   (match config
-    (($ <zabbix-front-end-configuration> _ server nginx)
+    (($ <zabbix-front-end-configuration> server nginx)
      (if (null? nginx)
          (list
           (nginx-server-configuration