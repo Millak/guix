b7359c3b172981e5e195ce920125c878ac023a6a
zimoun <zimon.toutoune@gmail.com>
2022-11-17 03:50:07 +0100

    gnu: r-affy: Update to 1.76.0.
    * gnu/packages/bioconductor.scm (r-affy): Update to 1.76.0.

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -4062,14 +4062,14 @@ performing parallel computations on multicore machines.")
 (define-public r-affy
   (package
     (name "r-affy")
-    (version "1.74.0")
+    (version "1.76.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "affy" version))
        (sha256
         (base32
-         "02l77y4d4m4jwgkb3jdaskv6shmba5292whp0i29mg9asxv4rdc7"))))
+         "1iix9mq4aph6avs2qr64a7hip461cif56k6npvs5g69nj3rn50gj"))))
     (build-system r-build-system)
     (propagated-inputs
      (list r-affyio
