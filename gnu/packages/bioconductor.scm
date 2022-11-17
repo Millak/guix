d9b593d522ca85fc9bae0c1b1fe9572ae2e47f3a
zimoun <zimon.toutoune@gmail.com>
2022-11-17 03:50:26 +0100

    gnu: r-reqon: Update to 1.44.0.
    * gnu/packages/bioconductor.scm (r-reqon): Update to 1.44.0.

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -13793,14 +13793,14 @@ genome sequence.")
 (define-public r-reqon
   (package
     (name "r-reqon")
-    (version "1.42.0")
+    (version "1.44.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "ReQON" version))
        (sha256
         (base32
-         "1f5pplm8fy3wvl0b6n18gph4dq9i9x5qiyjrj0bk0kwlkbpba74r"))))
+         "1r4dzwnqv6a4zy5q9l3axqq23sbk9pjfb2y3fmwxjvdwq6c1qnp0"))))
     (properties `((upstream-name . "ReQON")))
     (build-system r-build-system)
     (propagated-inputs
