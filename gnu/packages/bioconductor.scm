83ff82481c9758a586f15533958e5ad063f088bf
zimoun <zimon.toutoune@gmail.com>
2022-11-17 03:50:16 +0100

    gnu: r-plgem: Update to 1.70.0.
    * gnu/packages/bioconductor.scm (r-plgem): Update to 1.70.0.

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -8593,14 +8593,14 @@ in omics data.")
 (define-public r-plgem
   (package
     (name "r-plgem")
-    (version "1.68.0")
+    (version "1.70.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "plgem" version))
        (sha256
         (base32
-         "07zxflxcay17hxjw3wh5kfdwl2x8537csb18p1qzmyrkvscnja77"))))
+         "0hnhfdrlg4907dc4s17cy4kgmq5nr616f1wi7jn72acxwqfl4bk8"))))
     (build-system r-build-system)
     (propagated-inputs
      (list r-biobase r-mass))
