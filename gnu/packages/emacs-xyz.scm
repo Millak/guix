9be7ffa5b44929e66bd773ce3121a4edf0a30a87
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2022-11-19 10:07:33 +0100

    gnu: emacs-auctex: Update to 13.1.6.
    * gnu/packages/emacs-xyz.scm (emacs-auctex): Update to 13.1.6.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -3148,14 +3148,14 @@ as a library for other Emacs packages.")
 (define-public emacs-auctex
   (package
     (name "emacs-auctex")
-    (version "13.1.5")
+    (version "13.1.6")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://elpa.gnu.org/packages/"
                            "auctex-" version ".tar"))
        (sha256
-        (base32 "00g6js6089637w8alch4dvk140chjkyirsa8inh9ai6a6kkfvc3p"))))
+        (base32 "0pdinnhkv7vqib01a6vxq1iixs7sw72r0sxzryv78c9hxn2k4552"))))
     (build-system emacs-build-system)
     ;; We use 'emacs' because AUCTeX requires dbus at compile time
     ;; ('emacs-minimal' does not provide dbus).
