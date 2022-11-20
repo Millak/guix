fe0a1b49c1b6e19e05e164e4d3115474935d9a3a
Marius Bakke <marius@gnu.org>
2022-11-20 16:49:05 +0100

    gnu: postgresql@10: Update to 10.23.
    * gnu/packages/databases.scm (postgresql-10): Update to 10.23.

--- gnu/packages/databases.scm
+++ gnu/packages/databases.scm
@@ -1328,14 +1328,14 @@ pictures, sounds, or video.")
 (define-public postgresql-10
   (package
     (inherit postgresql-11)
-    (version "10.21")
+    (version "10.23")
     (source (origin
               (inherit (package-source postgresql-11))
               (uri (string-append "https://ftp.postgresql.org/pub/source/v"
                                   version "/postgresql-" version ".tar.bz2"))
               (sha256
                (base32
-                "1la5dx4hhy5yaznwk9gwdsymih3sd23fyhh6spssdaajdn2rh8fk"))))
+                "1sgfssjc9lnzijhn108r6z26fri655k413f1c9b8wibjhd9b594l"))))
     (native-inputs
      (modify-inputs (package-native-inputs postgresql-11)
        (append opensp docbook-sgml-4.2)
