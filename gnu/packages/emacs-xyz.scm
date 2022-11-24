b438e1f2311a06c309f7b4cf29683463bf446da1
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2022-11-24 12:59:08 +0100

    gnu: emacs-projectile: Improve description.
    * gnu/packages/emacs-xyz.scm (emacs-projectile)[description]: Use proper
    capitalization, expand some words, add Texinfo markup, remove fancy typography.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -11851,10 +11851,10 @@ like @code{org-edit-src-code} but for arbitrary regions.")
     (synopsis "Manage and navigate projects in Emacs easily")
     (description
      "This library provides easy project management and navigation.  The
-concept of a project is pretty basic - just a folder containing special file.
-Currently git, mercurial and bazaar repos are considered projects by default.
-If you want to mark a folder manually as a project just create an empty
-.projectile file in it.")
+concept of a project is pretty basic: just a folder containing special file.
+Currently Git, Mercurial and Bazaar repositories are considered projects by
+default.  If you want to mark a folder manually as a project just create an
+empty @file{.projectile} file in it.")
     (license license:gpl3+)))
 
 (define-public emacs-skeletor
