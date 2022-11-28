18545301e0a3c087c9983b42f43974ec82c629ab
宋文武 <iyzsong@member.fsf.org>
2022-11-28 11:14:00 +0800

    gnu: lxqt-admin: Update to 1.2.0.
    * gnu/packages/lxqt.scm (lxqt-admin): Update to 1.2.0.

--- gnu/packages/lxqt.scm
+++ gnu/packages/lxqt.scm
@@ -305,14 +305,14 @@ LXQt and the system it's running on.")
 (define-public lxqt-admin
   (package
     (name "lxqt-admin")
-    (version "1.1.0")
+    (version "1.2.0")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://github.com/lxqt/" name "/releases/download/"
                            version "/" name "-" version ".tar.xz"))
        (sha256
-        (base32 "1zah3xdnif9miaq52mmfbbzvqjhca7w7h81ngrn25j9pvd2bflm8"))))
+        (base32 "1bjmarrra43gcnzi8i0g0lcam12hhgr2yi8dji6klmrcp4k67y89"))))
     (build-system cmake-build-system)
     (inputs
      (list kwindowsystem
