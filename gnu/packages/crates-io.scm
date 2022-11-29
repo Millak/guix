545fc7285b84833ab9d097a40be2a7e5d06ddc06
( <paren@disroot.org>
2022-11-29 07:00:16 +0000

    gnu: Add rust-propfuzz-macro-0.0.1.
    * gnu/packages/crates-io.scm (rust-propfuzz-macro-0.0.1): New
    variable.
    
    Signed-off-by: Tobias Geerinckx-Rice <me@tobias.gr>

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -42268,6 +42268,29 @@ status.")
 progress-bars for Rust.")
     (license license:asl2.0)))
 
+(define-public rust-propfuzz-macro-0.0.1
+  (package
+    (name "rust-propfuzz-macro")
+    (version "0.0.1")
+    (source (origin
+              (method url-fetch)
+              (uri (crate-uri "propfuzz-macro" version))
+              (file-name (string-append name "-" version ".tar.gz"))
+              (sha256
+               (base32
+                "1xizaahjxxvcz9n91pgpji3nd7b755qgq3m2kmmg53zwjwv9nnsx"))))
+    (build-system cargo-build-system)
+    (arguments
+     (list #:cargo-inputs
+           `(("rust-proc-macro2" ,rust-proc-macro2-1)
+             ("rust-quote" ,rust-quote-1)
+             ("rust-syn" ,rust-syn-1))))
+    (home-page "https://github.com/facebookincubator/propfuzz")
+    (synopsis "Support macro for propfuzz")
+    (description
+     "This package provides a support macro for propfuzz.")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-proptest-1
   (package
     (name "rust-proptest")
