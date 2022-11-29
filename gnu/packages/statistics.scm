d197e94cff79b5c1a51b06e309ef7156076ddb52
Ricardo Wurmus <rekado@elephly.net>
2022-11-29 15:22:35 +0100

    gnu: r-ade4: Update to 1.7-20.
    * gnu/packages/statistics.scm (r-ade4): Update to 1.7-20.

--- gnu/packages/statistics.scm
+++ gnu/packages/statistics.scm
@@ -2111,14 +2111,14 @@ chain.")
 (define-public r-ade4
   (package
     (name "r-ade4")
-    (version "1.7-19")
+    (version "1.7-20")
     (source
       (origin
         (method url-fetch)
         (uri (cran-uri "ade4" version))
         (sha256
           (base32
-            "1vv5y6badksnpjb3bcphhjdzzh6i2grmwnsalcf2cfpr3y412lf8"))))
+            "0yxd9dgci3rzz807wsb76wis12ipgjv9w86smdyz20jrnn45giyx"))))
     (build-system r-build-system)
     (propagated-inputs
      (list r-mass r-pixmap r-sp))
