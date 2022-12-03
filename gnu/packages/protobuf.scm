Millak/guix

commit 9b016e29912b11f52e67e59b211fa016d88fea4a

gnu: python-protobuf: Use C++ implementation.
* gnu/packages/protobuf.scm (python-protobuf): Use C++ implementation.
  [inputs]: Add protobuf.
  [arguments]: Add --cpp_implementation configure flags.

Signed-off-by: Ludovic Courtès <ludo@gnu.org>

gnu/packages/protobuf.scm
@@ -406,6 +406,12 @@ from protobuf specification files.")
         (base32
          "1ja2vpk9nklllmsirmil2s4l7ni9yfqvbvj47zz5xx17s1k1bhxd"))))
     (build-system python-build-system)
+    (inputs (list protobuf))
+    (arguments
+     `(;; Favor C++ implementation from protobuf over the native Python
+       ;; implementation. The additional dependency yields significant
+       ;; performance improvements for some workloads.
+       #:configure-flags '("--cpp_implementation")))
     (home-page "https://github.com/google/protobuf")
     (synopsis "Protocol buffers is a data interchange format")
     (description

commit bf46192d4c7c4cd8d71edb8ace2cdf86322aafe7

gnu: python-protobuf@3.6: Fix inputs and arguments.
Fixes a regression introduced in
9b016e29912b11f52e67e59b211fa016d88fea4a, whereby the 'inputs' field
would be invalid, plus "--cpp_implementation" wouldn't work.

* gnu/packages/protobuf.scm (python-protobuf-3.6)[arguments]: New
field.
[inputs]: No longer refer to the inputs of PYTHON-PROTOBUF.

gnu/packages/protobuf.scm
@@ -1,5 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2014 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2014, 2022 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2016 Daniel Pimentel <d4n1@d4n1.org>
 ;;; Copyright © 2016 Leo Famulari <leo@famulari.name>
 ;;; Copyright © 2017, 2018, 2019, 2022 Ricardo Wurmus <rekado@elephly.net>
@@ -468,9 +468,8 @@ structured data.")
        (sha256
         (base32
          "04bqb12smlckzmgkj6vgmpbr3cby0n6726cmz33bqr7kn1vb728l"))))
-    (inputs
-     (cons python-six
-           (package-inputs python-protobuf)))))
+    (arguments '())                            ;no "--cpp_implementation" here
+    (inputs (list python-six))))
 
 (define-public python-proto-plus
   (package