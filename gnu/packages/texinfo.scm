Millak/guix

commit a3264f31df9774ea514e18ed8e7b6bcb44061edc

gnu: texinfo: Disable tests on GNU/Hurd.
* gnu/packages/texinfo.scm (texinfo)[arguments]: Add #:tests? argument.

gnu/packages/texinfo.scm
@@ -40,6 +40,7 @@
   #:use-module (gnu packages autotools)
   #:use-module (gnu packages compression)
   #:use-module (gnu packages gettext)
+  #:use-module ((gnu packages hurd) #:select (hurd-target?))
   #:use-module (gnu packages ncurses)
   #:use-module (gnu packages perl)
   #:use-module (gnu packages readline))
@@ -71,7 +72,10 @@
                     (("env -i")
                      "env "))
                   #t)))
-            %standard-phases)))
+            %standard-phases)
+
+       ;; XXX: Work around <https://issues.guix.gnu.org/59616>.
+       #:tests? ,(not (hurd-target?))))
     (inputs (list ncurses perl))
     ;; When cross-compiling, texinfo will build some of its own binaries with
     ;; the native compiler. This means ncurses is needed both in both inputs

commit fe563a87ad7c171448c56ed6b628c8e32a52c94a

gnu: texinfo, info-reader: Do not run tests when cross-compiling.
Fixes a regression introduced in
a3264f31df9774ea514e18ed8e7b6bcb44061edc.

Reported by Mathieu Othacehe <othacehe@gnu.org>.

* gnu/packages/texinfo.scm (texinfo)[arguments]: Change #:tests? to not
run tests when cross-compiling.

gnu/packages/texinfo.scm
@@ -75,7 +75,8 @@
             %standard-phases)
 
        ;; XXX: Work around <https://issues.guix.gnu.org/59616>.
-       #:tests? ,(not (hurd-target?))))
+       #:tests? ,(and (not (hurd-target?))
+                      (not (%current-target-system)))))
     (inputs (list ncurses perl))
     ;; When cross-compiling, texinfo will build some of its own binaries with
     ;; the native compiler. This means ncurses is needed both in both inputs