b129026e2e242e9068158ae6e6fcd8d7c5ea092e
Ludovic Courtès <ludo@gnu.org>
2022-12-10 10:56:48 +0100

    deduplicate: Use 'sendfile' for small file copies.
    * guix/store/deduplication.scm (dump-file/deduplicate): Use 'sendfile'
    instead of 'dump-port'.
    * tests/store-deduplication.scm ("copy-file/deduplicate, below %deduplication-minimum-size"):
    New test.

--- guix/store/deduplication.scm
+++ guix/store/deduplication.scm
@@ -1,6 +1,6 @@
 ;;; GNU Guix --- Functional package management for GNU
 ;;; Copyright © 2017 Caleb Ristvedt <caleb.ristvedt@cune.org>
-;;; Copyright © 2018-2021 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2018-2022 Ludovic Courtès <ludo@gnu.org>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -262,7 +262,7 @@ down the road."
       (deduplicate file (dump-and-compute-hash) #:store store)
       (call-with-output-file file
         (lambda (output)
-          (dump-port input output size)))))
+          (sendfile output input size 0)))))
 
 (define* (copy-file/deduplicate source target
                                 #:key (store (%store-directory)))
