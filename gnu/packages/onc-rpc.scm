Millak/guix

commit 517618b2401c2ab5861e63df60ae49918b94f727

gnu: libtirpc: fix cross-compilation.
* gnu/packages/onc-rpc.scm (libtirpc)[native-inputs]: add mit-krb5.

Signed-off-by: Ludovic Courtès <ludo@gnu.org>

gnu/packages/onc-rpc.scm
@@ -6,6 +6,7 @@
 ;;; Copyright © 2019 Marius Bakke <mbakke@fastmail.com>
 ;;; Copyright © 2020 Ricardo Wurmus <rekado@elephly.net>
 ;;; Copyright © 2022 Efraim Flashner <efraim@flashner.co.il>
+;;; Copyright © 2022 Zheng Junjie <873216071@qq.com>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -63,6 +64,7 @@
                             "tirpc/netconfig.h")
                (("/etc/netconfig") (string-append (assoc-ref outputs "out")
                                                   "/etc/netconfig"))))))))
+    (native-inputs (list mit-krb5)) ;; for cross-compilation
     (inputs (list mit-krb5))
     (home-page "https://sourceforge.net/projects/libtirpc/")
     (synopsis "Transport-independent Sun/ONC RPC implementation")

commit 3532fc6ba4d36ae055f11245a29fb6e8e54c545c

gnu: libtirpc-hurd: Fix cross-compilation.
Fixes a regression introduced in
517618b2401c2ab5861e63df60ae49918b94f727, whereby the "wrong"
'krb5-config' script would be picked.

* gnu/packages/onc-rpc.scm (libtirpc/hurd)[arguments]: Turn
 #:configure-flags into a gexp to refer to the right "mit-krb5".

gnu/packages/onc-rpc.scm
@@ -1,5 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2014, 2017, 2018 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2014, 2017, 2018, 2022 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2016 John Darrington <jmd@gnu.org>
 ;;; Copyright © 2017, 2018 Leo Famulari <leo@famulari.name>
 ;;; Copyright © 2018, 2021 Tobias Geerinckx-Rice <me@tobias.gr>
@@ -27,6 +27,7 @@
   #:use-module (guix licenses)
   #:use-module (guix packages)
   #:use-module (guix download)
+  #:use-module (guix gexp)
   #:use-module (guix git-download)
   #:use-module (gnu packages)
   #:use-module (gnu packages autotools)
@@ -84,9 +85,9 @@ IPv4 and IPv6.  ONC RPC is notably used by the network file system (NFS).")
      (substitute-keyword-arguments (package-arguments libtirpc)
        ((#:configure-flags flags ''())
         ;; When cross-building the target system's krb5-config should be used.
-        `(list (string-append "ac_cv_prog_KRB5_CONFIG="
-                              (assoc-ref %build-inputs "mit-krb5")
-                              "/bin/krb5-config")))))))
+        #~(list (string-append "ac_cv_prog_KRB5_CONFIG="
+                               #$(this-package-input "mit-krb5")
+                               "/bin/krb5-config")))))))
 
 (define libtirpc/fixed
   (package