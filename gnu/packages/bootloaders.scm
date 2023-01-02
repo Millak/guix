ea0d3e1ec47171eb70f5c5ef25cff583138abfb9
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2023-01-01 19:25:25 -0500

    gnu: u-boot-rockpro64-rk3399: Use gexps and fix build.
    * gnu/packages/bootloaders.scm (u-boot-rockpro64-rk3399)
    [phases]: Use gexps.
    [native-inputs]: Use modify-inputs and turn into...
    [inputs]: ... this.
    
    Signed-off-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>

--- gnu/packages/bootloaders.scm
+++ gnu/packages/bootloaders.scm
@@ -1176,37 +1176,36 @@ partition."))
       (arguments
        (substitute-keyword-arguments (package-arguments base)
          ((#:phases phases)
-          `(modify-phases ,phases
-             (add-after 'unpack 'set-environment
-               (lambda* (#:key inputs #:allow-other-keys)
-                 (setenv "BL31"
-                         (search-input-file inputs "/bl31.elf"))))
-             (add-after 'unpack 'patch-header
-               (lambda _
-                 (substitute* "include/config_distro_bootcmd.h"
-                   (("\"scsi_need_init=false")
-                    "\"setenv scsi_need_init false")
-                   (("#define BOOTENV_SET_SCSI_NEED_INIT \"scsi_need_init=;")
-                    "#define BOOTENV_SET_SCSI_NEED_INIT \"setenv scsi_need_init;"))
-                 (substitute* "include/configs/rockchip-common.h"
-                   (("#define BOOT_TARGET_DEVICES\\(func\\)")
-                    "
+          #~(modify-phases #$phases
+              (add-after 'unpack 'set-environment
+                (lambda* (#:key inputs #:allow-other-keys)
+                  (setenv "BL31" (search-input-file inputs "/bl31.elf"))))
+              (add-after 'unpack 'patch-header
+                (lambda _
+                  (substitute* "include/config_distro_bootcmd.h"
+                    (("\"scsi_need_init=false")
+                     "\"setenv scsi_need_init false")
+                    (("#define BOOTENV_SET_SCSI_NEED_INIT \"scsi_need_init=;")
+                     "#define BOOTENV_SET_SCSI_NEED_INIT \"setenv scsi_need_init;"))
+                  (substitute* "include/configs/rockchip-common.h"
+                    (("#define BOOT_TARGET_DEVICES\\(func\\)")
+                     "
 #if CONFIG_IS_ENABLED(CMD_SCSI)
        #define BOOT_TARGET_SCSI(func) func(SCSI, scsi, 0)
 #else
        #define BOOT_TARGET_SCSI(func)
 #endif
 #define BOOT_TARGET_DEVICES(func)")
-                   (("BOOT_TARGET_NVME\\(func\\) \\\\")
-                    "\
+                    (("BOOT_TARGET_NVME\\(func\\) \\\\")
+                     "\
 BOOT_TARGET_NVME(func) \\
        BOOT_TARGET_SCSI(func) \\"))))
-             ;; Phases do not succeed on the bl31 ELF.
-             (delete 'strip)
-             (delete 'validate-runpath)))))
-      (native-inputs
-       `(("firmware" ,arm-trusted-firmware-rk3399)
-         ,@(package-native-inputs base))))))
+              ;; Phases do not succeed on the bl31 ELF.
+              (delete 'strip)
+              (delete 'validate-runpath)))))
+      (inputs
+       (modify-inputs (package-inputs base)
+         (append arm-trusted-firmware-rk3399))))))
 
 (define-public u-boot-pinebook-pro-rk3399
   (let ((base (make-u-boot-package "pinebook-pro-rk3399" "aarch64-linux-gnu")))
