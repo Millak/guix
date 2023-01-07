Millak/guix

commit 520cdf80581669646ff61e9f18f3b27045556e76

gnu: python-setuptools: Update to 62.0.0.
* gnu/packages/python-xyz.scm (python-setuptools): Update to 62.0.0.
[source]: Simplify snippet and strip trailing #t.
[arguments]: Re-indent.
[synopsis]: Likewise.
[description]: Use @itemize.
[license]: Strip leading spaces from in-line comments.

gnu/packages/python-xyz.scm
@@ -1842,44 +1842,41 @@ other machines, such as over the network.")
 (define-public python-setuptools
   (package
     (name "python-setuptools")
-    (version "52.0.0")
+    (version "62.0.0")
     (source
      (origin
-      (method url-fetch)
-      (uri (pypi-uri "setuptools" version))
-      (sha256
-       (base32
-        "15ibjdjhkwgj6qbmpsxikkqdfsb1550z46fly7dm15ah4bk1wfpv"))
-      (modules '((guix build utils)))
-      (snippet
-       '(begin
-          ;; Remove included binaries which are used to build self-extracting
-          ;; installers for Windows.
-          ;; TODO: Find some way to build them ourself so we can include them.
-          (for-each delete-file (find-files "setuptools" "^(cli|gui).*\\.exe$"))
-          #t))))
+       (method url-fetch)
+       (uri (pypi-uri "setuptools" version))
+       (sha256
+        (base32
+         "0sm8n6y6q640cpac9wjyggidbgi4n9la7vs7pwriyvhvgzccp6br"))
+       (modules '((guix build utils)))
+       (snippet
+        ;; Remove included binaries which are used to build self-extracting
+        ;; installers for Windows.
+        ;; TODO: Find some way to build them ourself so we can include them.
+        '(for-each delete-file (find-files "setuptools"
+                                           "^(cli|gui).*\\.exe$")))))
     (build-system python-build-system)
     ;; FIXME: Tests require pytest, which itself relies on setuptools.
     ;; One could bootstrap with an internal untested setuptools.
-    (arguments
-     `(#:tests? #f))
+    (arguments (list #:tests? #f))
     (home-page "https://pypi.org/project/setuptools/")
-    (synopsis
-     "Library designed to facilitate packaging Python projects")
-    (description
-     "Setuptools is a fully-featured, stable library designed to facilitate
-packaging Python projects, where packaging includes:
-Python package and module definitions,
-distribution package metadata,
-test hooks,
-project installation,
-platform-specific details,
-Python 3 support.")
+    (synopsis "Library designed to facilitate packaging Python projects")
+    (description "Setuptools is a fully-featured, stable library designed to
+facilitate packaging Python projects, where packaging includes:
+@itemize
+@item Python package and module definitions
+@item distribution package metadata
+@item test hooks
+@item project installation
+@item platform-specific details.
+@end itemize")
     ;; TODO: setuptools now bundles the following libraries:
     ;; packaging, pyparsing, six and appdirs. How to unbundle?
-    (license (list license:psfl        ; setuptools itself
-                   license:expat       ; six, appdirs, pyparsing
-                   license:asl2.0      ; packaging is dual ASL2/BSD-2
+    (license (list license:psfl         ;setuptools itself
+                   license:expat        ;six, appdirs, pyparsing
+                   license:asl2.0       ;packaging is dual ASL2/BSD-2
                    license:bsd-2))
     (properties `((python2-variant . ,(delay python2-setuptools))))))
 

commit edbacab9953144a3aa204f9f0eee1584ef321a8f

gnu: Use old setuptools in packages that relies on use_2to3 conversion.
This fixes a regression since 520cdf80581669646ff61e9f18f3b27045556e76 where
the use_2to3 functionality was removed from setuptools.

* gnu/packages/graph.scm (python-louvain)[native-inputs]: Change from
PYTHON-SETUPTOOLS to PYTHON-SETUPTOOLS-57.
* gnu/packages/python-check.scm (python-parameterizedtestcase)[native-inputs]:
Likewise.
* gnu/packages/python-xyz.scm (python-anyjson)[native-inputs]: Add
PYTHON-SETUPTOOLS-57.
* gnu/packages/bioinformatics.scm (python-pyvcf)[native-inputs]: Change from
PYTHON-SETUPTOOLS-FOR-TENSORFLOW to PYTHON-SETUPTOOLS-57.

gnu/packages/bioinformatics.scm
@@ -16782,7 +16782,7 @@ for the analysis and visualization of raw nanopore signal.")
                                          "/vcf/test")))))))
       (native-inputs
        ;; Older setuptools is needed for use_2to3.
-       (list python-cython python-setuptools-for-tensorflow))
+       (list python-cython python-setuptools-57))
       (propagated-inputs
        (list python-pysam python-rpy2))
       (home-page "https://github.com/jamescasbon/PyVCF")

gnu/packages/graph.scm
@@ -295,7 +295,7 @@ subplots, multiple-axes, polar charts, and bubble charts.")
         (base32 "0sx53l555rwq0z7if8agirjgw4ddp8r9b949wwz8vlig03sjvfmp"))))
     (build-system python-build-system)
     (native-inputs
-     (list python-setuptools))          ;for use_2to3 support
+     (list python-setuptools-57))       ;for use_2to3 support
     (propagated-inputs
      (list python-networkx python-numpy))
     (home-page "https://github.com/taynaud/python-louvain")

gnu/packages/python-check.scm
@@ -2268,7 +2268,7 @@ Avocado machine readable outputs this one is streamlined (per test results).
        (sha256
         (base32 "0zhjmsd16xacg4vd7zb75kw8q9khn52wvad634v1bvz7swaivk2c"))))
     (build-system python-build-system)
-    (native-inputs (list python-setuptools)) ;for use_2to3
+    (native-inputs (list python-setuptools-57)) ;for use_2to3
     (home-page
      "https://github.com/msabramo/python_unittest_parameterized_test_case")
     (synopsis "Parameterized tests for Python's unittest module")

gnu/packages/python-xyz.scm
@@ -15847,8 +15847,8 @@ fast xml and html manipulation.")
        ;; whatever) so this transformation needs to be done before the tests
        ;; can be run.  Maybe we could add a build step to transform beforehand
        ;; but it could be annoying/difficult.
-       ;; We can enable tests for the Python 2 version, though, and do below.
        #:tests? #f))
+    (native-inputs (list python-setuptools-57)) ;for use_2to3 support
     (home-page "https://bitbucket.org/runeh/anyjson/")
     (synopsis
      "Wraps best available JSON implementation in a common interface")