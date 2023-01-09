47184a91f14164bc8360addff5a3a3a15da8c036
Ricardo Wurmus <rekado@elephly.net>
2023-01-09 22:15:13 +0100

    gnu: r-rstpm2: Update to 1.5.9.
    * gnu/packages/cran.scm (r-rstpm2): Update to 1.5.9.

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -3113,14 +3113,14 @@ rules and utility functions for adaptive GH quadrature.")
 (define-public r-rstpm2
   (package
     (name "r-rstpm2")
-    (version "1.5.8")
+    (version "1.5.9")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "rstpm2" version))
        (sha256
         (base32
-         "038qq5kar9f5ygd18aicwfqrr7fl37p0r9shgpc6wf2vmj564crn"))))
+         "1zl7z1l19s960mrhlzvpga07kzqfrhhq7qlkgln4wj0gcmhicyc6"))))
     (properties `((upstream-name . "rstpm2")))
     (build-system r-build-system)
     (propagated-inputs
