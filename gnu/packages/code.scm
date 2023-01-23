9e4f1f8c339cb5f1d3dcf9b22ae74cd5b1c25053
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2023-01-23 09:36:55 -0500

    gnu: universal-ctags: Update to 6.0.20230122.0.
    * gnu/packages/code.scm (universal-ctags): Update to 6.0.20230122.0.

--- gnu/packages/code.scm
+++ gnu/packages/code.scm
@@ -430,7 +430,7 @@ features that are not supported by the standard @code{stdio} implementation.")
 (define-public universal-ctags
   (package
     (name "universal-ctags")
-    (version "5.9.20221127.0")
+    (version "6.0.20230122.0")
     (source
      (origin
        (method git-fetch)
@@ -440,7 +440,7 @@ features that are not supported by the standard @code{stdio} implementation.")
        (file-name (git-file-name name version))
        (sha256
         (base32
-         "0nvkx5j2vyzjf935a2s5w56gamlr6f12jy1x38bkqz78p5l0d3ja"))
+         "121d1dyc3wd4bzv4wky3x66j1va3d6ywbw71abp3l88fv7sc0f73"))
        (modules '((guix build utils)))
        (snippet
         '(begin
