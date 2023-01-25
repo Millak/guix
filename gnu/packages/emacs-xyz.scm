bd55412fc297b07b5cbe4e22abdec0419ef23010
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-01-25 17:28:50 +0100

    gnu: emacs-compat: Update to 29.1.3.1.
    * gnu/packages/emacs-xyz.scm (emacs-compat): Update to 29.1.3.1.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -7822,14 +7822,14 @@ variables, and so on.  The mode also allows you to execute Tup commands.")
 (define-public emacs-compat
   (package
     (name "emacs-compat")
-    (version "29.1.2.0")
+    (version "29.1.3.1")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://elpa.gnu.org/packages/"
                                   "compat-" version ".tar"))
               (sha256
                (base32
-                "1jh6kjsqmpccry3n6x89gb8428vcxg8z451inn08lcy9fn1xqq1x"))))
+                "025idv426a2igaz7ijf5njjdi53zm5lmf345a1smv03iqx3fyj81"))))
     (build-system emacs-build-system)
     (home-page "https://git.sr.ht/~pkal/compat")
     (synopsis "Emacs Lisp Compatibility Library")
