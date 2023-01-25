92caedb60c8fb6961446d223b623c41bed808acd
Lars-Dominik Braun <lars@6xq.net>
2023-01-25 10:01:36 +0100

    gnu: idris: Fix overly zealous regular expression.
    * gnu/packages/idris.scm (idris)[arguments]: Limit characters matched
    after package name in cabal file substitutions.

--- gnu/packages/idris.scm
+++ gnu/packages/idris.scm
@@ -99,7 +99,7 @@
          (add-before 'configure 'update-constraints
            (lambda _
              (substitute* "idris.cabal"
-               (("(aeson|ansi-terminal|bytestring|haskeline|libffi|megaparsec|network|optparse-applicative)\\s+[^,]+" all dep)
+               (("(aeson|ansi-terminal|bytestring|haskeline|libffi|megaparsec|network|optparse-applicative)\\s+[<>=0-9. &|]+" all dep)
                 dep))))
          (add-before 'configure 'set-cc-command
            (lambda _
