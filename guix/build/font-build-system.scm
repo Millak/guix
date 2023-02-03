af96ccad533444b5506c5f115c2e0ecf9067d0e9
Simon South <simon@simonsouth.net>
2023-02-03 07:42:59 -0500

    build-system/font: Customize %license-file-regexp.
    * guix/build/font-build-system.scm (%license-file-regexp): Customize to
    include names of license files commonly found in font packages.

--- guix/build/font-build-system.scm
+++ guix/build/font-build-system.scm
@@ -58,8 +58,9 @@ archive, or a font file."
               (find-files source "\\.(woff|woff2)$"))))
 
 (define %license-file-regexp
-  ;; Regexp matching license files.
-  "^(COPYING.*|LICEN[CS]E.*|[Ll]icen[cs]e.*|Copy[Rr]ight(\\.(txt|md))?)$")
+  ;; Regexp matching license files commonly found in font packages.
+  "^((COPY(ING|RIGHT)|LICEN[CS]E).*\
+|(([Cc]opy[Rr]ight|[Ll]icen[cs]es?|IPA_.*|OFL(-?1\\.?1)?)(\\.(txt|md)?))$)")
 
 (define %standard-phases
   (modify-phases gnu:%standard-phases
