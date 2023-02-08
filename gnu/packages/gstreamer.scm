d2b9b4b861b71d11eaeaa12fe544c9ffb0b6644d
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2023-02-08 08:54:33 -0500

    gnu: gstreamer: Do not reference Python.
    * gnu/packages/gstreamer.scm (gstreamer) [native-inputs]: Replace
    python-wrapper with python.
    [arguments]: List python in #:disallowed-references.  Add a
    'do-not-capture-python phase.

--- gnu/packages/gstreamer.scm
+++ gnu/packages/gstreamer.scm
@@ -10,6 +10,7 @@
 ;;; Copyright © 2019, 2020 Marius Bakke <mbakke@fastmail.com>
 ;;; Copyright © 2020 Liliana Marie Prikler <liliana.prikler@gmail.com>
 ;;; Copyright © 2020 Michael Rohleder <mike@rohleder.de>
+;;; Copyright © 2023 Maxim Cournoyer <maxim.cournoyer@gmail.com>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -477,28 +478,40 @@ the GStreamer multimedia framework.")
          "0aisl8nazcfi4b5j6fz8zwpp0k9csb022zniz65b2pxxpdjayzb0"))))
     (build-system meson-build-system)
     (arguments
-     (list #:phases
-           #~(modify-phases %standard-phases
-               #$@%common-gstreamer-phases
-               #$@(if (string-prefix? "i686" (or (%current-target-system)
-                                                 (%current-system)))
-                      ;; FIXME: These tests consistently fail in the Guix CI:
-                      ;;   https://issues.guix.gnu.org/57868
-                      '((add-after 'unpack 'disable-systemclock-test
-                          (lambda _
-                            (substitute* "tests/check/gst/gstsystemclock.c"
-                              (("tcase_add_test \\(tc_chain, \
+     (list
+      #:disallowed-references (list python)
+      #:phases
+      #~(modify-phases %standard-phases
+          #$@%common-gstreamer-phases
+          #$@(if (string-prefix? "i686" (or (%current-target-system)
+                                            (%current-system)))
+                 ;; FIXME: These tests consistently fail in the Guix CI:
+                 ;;   https://issues.guix.gnu.org/57868
+                 '((add-after 'unpack 'disable-systemclock-test
+                     (lambda _
+                       (substitute* "tests/check/gst/gstsystemclock.c"
+                         (("tcase_add_test \\(tc_chain, \
 test_stress_cleanup_unschedule.*")
-                               "")
-                              (("tcase_add_test \\(tc_chain, \
+                          "")
+                         (("tcase_add_test \\(tc_chain, \
 test_stress_reschedule.*")
-                               "")))))
-                      '()))))
+                          "")))))
+                 '())
+          (add-after 'patch-shebangs 'do-not-capture-python
+            (lambda _
+              ;; The patch-source-shebangs phase causes the following build
+              ;; script to reference Python in its shebang, which is
+              ;; unnecessary.
+              (substitute* (string-append #$output
+                                          "/libexec/gstreamer-1.0/"
+                                          "gst-plugins-doc-cache-generator")
+                (((which "python3"))
+                 "/usr/bin/env python3")))))))
     (propagated-inputs
      ;; In gstreamer-1.0.pc:
      ;;   Requires: glib-2.0, gobject-2.0
      ;;   Requires.private: gmodule-no-export-2.0 libunwind libdw
-     (list elfutils ; libdw
+     (list elfutils                     ;libdw
            glib libunwind))
     (native-inputs
      (list bash-completion
@@ -508,7 +521,7 @@ test_stress_reschedule.*")
            gobject-introspection
            perl
            pkg-config
-           python-wrapper))
+           python))
     (inputs
      (list gmp libcap
            ;; For tests.
