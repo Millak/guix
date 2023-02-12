89a1d52821c1d8f7accb812ca371738fab577227
Tobias Geerinckx-Rice <me@tobias.gr>
2023-02-12 01:00:00 +0100

    gnu: memtest86+: Update to 6.10.
    * gnu/packages/hardware.scm (memtest86+): Update to 6.10.

--- gnu/packages/hardware.scm
+++ gnu/packages/hardware.scm
@@ -824,7 +824,7 @@ specific SMBIOS tables.")
 (define-public memtest86+
   (package
     (name "memtest86+")
-    (version "6.01")
+    (version "6.10")
     (source
      (origin
        (method git-fetch)
@@ -833,7 +833,7 @@ specific SMBIOS tables.")
              (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "1axf8y3nppyskcysypqpkjl0sxz39f1x0v4syplx35qj3y2kq1h4"))
+        (base32 "1igb648rsmbp0s95790qib6mhdsvbsrpigl91gk7yfkz32bip3bz"))
        (patches
         (search-patches "memtest86+-build-reproducibly.patch"))))
     (build-system gnu-build-system)
