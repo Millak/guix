3164d48e55b2d614bcd8def064d4671578617963
conses <contact@conses.eu>
2023-02-20 20:58:46 +0100

    gnu: Add postmarketos-theme.
    * gnu/packages/gnome-xyz.scm (postmarketos-theme): New variable.
    
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/packages/gnome-xyz.scm
+++ gnu/packages/gnome-xyz.scm
@@ -1436,6 +1436,30 @@ variants.")
                    license:lgpl2.1         ; Some style sheets.
                    license:cc-by-sa4.0)))) ; Some icons
 
+(define-public postmarketos-theme
+  (package
+    (name "postmarketos-theme")
+    (version "0.6.0")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://gitlab.com/postmarketOS/postmarketos-theme")
+                    (commit version)))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "09in7737cirmw2c0ac40ac29szfgdva6q0zl32mdi12marybd2g5"))))
+    (build-system meson-build-system)
+    (native-inputs (list sassc))
+    (home-page "https://gitlab.com/postmarketOS/postmarketos-theme")
+    (synopsis "PostmarketOS themed themes")
+    (description
+     "@code{postmarketos-theme} contains a GTK3 and GTK4 theme which is based
+on Adwaita but replaces the standard blue highlights in the theme with
+postmarketOS green.  There's also the oled and paper variants of the theme
+that are completely black and completely white.")
+    (license license:lgpl2.0+)))
+
 (define-public eiciel
   (package
     (name "eiciel")
