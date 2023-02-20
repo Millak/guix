9682fd19bfe4e421da1aa4ca43eecae1bbe54e81
Simon Tournier <zimon.toutoune@gmail.com>
2023-02-20 19:53:57 +0100

    gnu: r-limma: Update to 3.54.1.
    * gnu/packages/bioconductor.scm (r-limma): Update to 3.54.1.

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -6517,13 +6517,13 @@ Binomial data via estimation of latent structure in the natural parameter.")
 (define-public r-limma
   (package
     (name "r-limma")
-    (version "3.54.0")
+    (version "3.54.1")
     (source (origin
               (method url-fetch)
               (uri (bioconductor-uri "limma" version))
               (sha256
                (base32
-                "1jy75nbkhl0kgv4gw88acx58r9f1kywrd36405x6g05xy05bprma"))))
+                "0x6wkbw8v0hq9dfr433165jmii05rswjsm97dpxvyvxvya3sxqa1"))))
     (build-system r-build-system)
     (home-page "https://bioinf.wehi.edu.au/limma")
     (synopsis "Package for linear models for microarray and RNA-seq data")
