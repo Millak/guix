e4edd209996f98706f1c0f278e09b125d7ecf4c1
Efraim Flashner <efraim@flashner.co.il>
2023-02-22 12:14:17 +0200

    gnu: Add rust-clap-derive-4.
    * gnu/packages/crates-io.scm (rust-clap-derive-4): New variable.
    (rust-clap-derive-3): Inherit from rust-clap-derive-4.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -11031,8 +11031,34 @@ files and environment variables.  And make it easier for users to tell your prog
 how to behave across the three main input sources")
     (license license:expat)))
 
+(define-public rust-clap-derive-4
+  (package
+    (name "rust-clap-derive")
+    (version "4.1.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "clap_derive" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1s4c4005xsdw5zlq8z5h02mgjcsqgcd1ydxg6iird49fcxyjfjk8"))))
+    (build-system cargo-build-system)
+    (arguments
+     (list #:cargo-inputs
+           `(("rust-heck" ,rust-heck-0.4)
+             ("rust-proc-macro-error" ,rust-proc-macro-error-1)
+             ("rust-proc-macro2" ,rust-proc-macro2-1)
+             ("rust-quote" ,rust-quote-1)
+             ("rust-syn" ,rust-syn-1))))
+    (home-page "https://clap.rs/")
+    (synopsis "Procedural macro crate for Clap")
+    (description
+     "This package provides the procedural macro crate for Clap.")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-clap-derive-3
   (package
+    (inherit rust-clap-derive-4)
     (name "rust-clap-derive")
     (version "3.2.18")
     (source
@@ -11042,7 +11068,6 @@ how to behave across the three main input sources")
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
         (base32 "0r9az0cl33xx0i9g18l56l3vd5ayjvcflvza2gdf8jwcab78n37a"))))
-    (build-system cargo-build-system)
     (arguments
      (list #:skip-build? #t
            #:cargo-inputs
@@ -11051,12 +11076,7 @@ how to behave across the three main input sources")
               ,rust-proc-macro-error-1)
              ("rust-proc-macro2" ,rust-proc-macro2-1)
              ("rust-quote" ,rust-quote-1)
-             ("rust-syn" ,rust-syn-1))))
-    (home-page "https://clap.rs/")
-    (synopsis "Procedural macro crate for Clap")
-    (description
-     "This package provides the procedural macro crate for Clap.")
-    (license (list license:expat license:asl2.0))))
+             ("rust-syn" ,rust-syn-1))))))
 
 (define-public rust-clap-3
   (package
