af95f2d8f98eb2c8c64954bb2fd0b70838899174
Juliana Sims <jtsims@protonmail.com>
2023-03-01 15:18:09 -0500

    gnu: pcmanfm-qt: Update to 1.2.1.
    * gnu/packages/lxqt.scm (pcmanfm-qt): Update to 1.2.1.
    
    Signed-off-by: 宋文武 <iyzsong@member.fsf.org>

--- gnu/packages/lxqt.scm
+++ gnu/packages/lxqt.scm
@@ -817,14 +817,14 @@ components to build desktop file managers which belongs to LXDE.")
 (define-public pcmanfm-qt
   (package
     (name "pcmanfm-qt")
-    (version "1.2.0")
+    (version "1.2.1")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://github.com/lxqt/" name "/releases/download/"
                            version "/" name "-" version ".tar.xz"))
        (sha256
-        (base32 "1k44a659mval4513p4yv63hqrbg9jqc8vrinl4mx5aja33pww5yg"))))
+        (base32 "0nz66b9mv6hqaxf5k3ijaf1694za5nv121y6jfq39db3a1qx7rm5"))))
     (build-system cmake-build-system)
     (arguments
      (list
