7f43f000b7e7ac659626fc692b3d85e6da70fa7b
Efraim Flashner <efraim@flashner.co.il>
2023-03-09 16:49:23 +0200

    gnu: Add rust-nu-ansi-term-0.46.
    * gnu/packages/crates-io.scm (rust-nu-ansi-term-0.46): New variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -38161,6 +38161,33 @@ framework.")
 macros used in ntest.")
     (license license:expat)))
 
+(define-public rust-nu-ansi-term-0.46
+  (package
+    (name "rust-nu-ansi-term")
+    (version "0.46.0")
+    (source (origin
+              (method url-fetch)
+              (uri (crate-uri "nu-ansi-term" version))
+              (file-name (string-append name "-" version ".tar.gz"))
+              (sha256
+               (base32 "115sywxh53p190lyw97alm14nc004qj5jm5lvdj608z84rbida3p"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs
+       (("rust-overload" ,rust-overload-0.1)
+        ("rust-serde" ,rust-serde-1)
+        ("rust-winapi" ,rust-winapi-0.3))
+       #:cargo-development-inputs
+       (("rust-doc-comment" ,rust-doc-comment-0.3)
+        ("rust-regex" ,rust-regex-1)
+        ("rust-serde-json" ,rust-serde-json-1))))
+    (home-page "https://github.com/nushell/nu-ansi-term")
+    (synopsis "Library for ANSI terminal colors and styles (bold, underline)")
+    (description
+     "This package provides a library for ANSI terminal colors and styles (bold,
+underline) in rust.")
+    (license license:expat)))
+
 (define-public rust-num-0.4
   (package
     (name "rust-num")
