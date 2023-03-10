Millak/guix

commit 835565777722d61f2b1e7a41df4ee74cd7fdb04e

gnu: emacs-adoc-mode: Improve package style.
* gnu/packages/emacs-xyz.scm (emacs-adoc-mode)[arguments]: Use G-expressions.
Remove trailing #T from phases.

gnu/packages/emacs-xyz.scm
@@ -25893,31 +25893,31 @@ and 'text viewing modes' respectively.")
          "0bp2i66a9gp41r7nvbx8f4s334gd7lwjdxi3qw5yhgaav6gk3bkc"))))
     (build-system emacs-build-system)
     (arguments
-     `(#:phases
-       (modify-phases %standard-phases
-         ;; See: https://github.com/sensorflo/adoc-mode/issues/39.
-         (add-after 'unpack 'disable-failing-tests
-           (lambda _
-             (let-syntax
-                 ((disable-tests
-                   (syntax-rules ()
-                     ((_ file ())
-                      (syntax-error "test names list must not be empty"))
-                     ((_ file (test-name ...))
-                      (substitute* file
-                        (((string-append "^\\(ert-deftest " test-name ".*") all)
-                         (string-append all "(skip-unless nil)\n")) ...)))))
-               (disable-tests "test/adoc-mode-test.el"
-                              ("adoctest-test-tempo-delimited-blocks"
-                               "adoctest-test-tempo-macros"
-                               "adoctest-test-tempo-paragraphs"
-                               "adoctest-test-tempo-passthroug-macros"
-                               "adoctest-test-tempo-quotes")))
-             #t)))
-       #:tests? #t
-       #:test-command '("emacs" "-Q" "-batch"
-                        "-l" "test/adoc-mode-test.el"
-                        "-f" "ert-run-tests-batch-and-exit")))
+     (list
+      #:phases
+      #~(modify-phases %standard-phases
+          ;; See: https://github.com/sensorflo/adoc-mode/issues/39.
+          (add-after 'unpack 'disable-failing-tests
+            (lambda _
+              (let-syntax
+                  ((disable-tests
+                    (syntax-rules ()
+                      ((_ file ())
+                       (syntax-error "test names list must not be empty"))
+                      ((_ file (test-name ...))
+                       (substitute* file
+                         (((string-append "^\\(ert-deftest " test-name ".*") all)
+                          (string-append all "(skip-unless nil)\n")) ...)))))
+                (disable-tests "test/adoc-mode-test.el"
+                               ("adoctest-test-tempo-delimited-blocks"
+                                "adoctest-test-tempo-macros"
+                                "adoctest-test-tempo-paragraphs"
+                                "adoctest-test-tempo-passthroug-macros"
+                                "adoctest-test-tempo-quotes"))))))
+      #:tests? #t
+      #:test-command '("emacs" "-Q" "-batch"
+                       "-l" "test/adoc-mode-test.el"
+                       "-f" "ert-run-tests-batch-and-exit")))
     (propagated-inputs
      (list emacs-markup-faces))
     (home-page "https://github.com/sensorflo/adoc-mode/wiki")

commit 60a211ec705ac98483d76da7f2523f2b8966343a

gnu: emacs-adoc-mode: Fix build.
* gnu/packages/emacs-xyz.scm (emacs-adoc-mode)[arguments]<#:test-command>: Fix
typo introduced in 835565777722d61f2b1e7a41df4ee74cd7fdb04e.

gnu/packages/emacs-xyz.scm
@@ -25914,9 +25914,9 @@ and 'text viewing modes' respectively.")
                                 "adoctest-test-tempo-passthroug-macros"
                                 "adoctest-test-tempo-quotes"))))))
       #:tests? #t
-      #:test-command '("emacs" "-Q" "-batch"
-                       "-l" "test/adoc-mode-test.el"
-                       "-f" "ert-run-tests-batch-and-exit")))
+      #:test-command #~(list "emacs" "-Q" "-batch"
+                             "-l" "test/adoc-mode-test.el"
+                             "-f" "ert-run-tests-batch-and-exit")))
     (propagated-inputs
      (list emacs-markup-faces))
     (home-page "https://github.com/sensorflo/adoc-mode/wiki")