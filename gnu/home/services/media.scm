Millak/guix

commit 70056b1b2beebbc9f8ea2c34eacc57f379368ab3

gnu: home: services: Add home-kodi-service-type.
* gnu/home/services/media.scm: New file.
* gnu/local.mk (GNU_SYSTEM_MODULES): Add it.
* po/guix/POTFILES.in: Likewise.
* doc/guix.texi (Media Home Services): Document it in new subsection.

gnu/home/services/media.scm
@@ -1,0 +1,73 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2023 Janneke Nieuwenhuizen <janneke@gnu.org>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (gnu home services media)
+  #:use-module (srfi srfi-26)
+
+  #:use-module (ice-9 match)
+
+  #:use-module (shepherd support)
+
+  #:use-module (gnu home services)
+  #:use-module (gnu home services shepherd)
+  #:use-module (gnu packages kodi)
+  #:use-module (gnu services configuration)
+  #:use-module (gnu services shepherd)
+  #:use-module (guix records)
+  #:use-module (guix gexp)
+
+  #:export (home-kodi-configuration
+            home-kodi-service-type))
+
+;;;
+;;; Kodi.
+;;;
+(define-record-type* <home-kodi-configuration>
+  home-kodi-configuration make-home-kodi-configuration
+  home-kodi-configuration?
+  (kodi          home-kodi-kodi          ;file-like
+                 (default kodi))
+  (extra-options home-kodi-extra-options ;list of string
+                 (default '())))
+
+(define (home-kodi-services config)
+  "Return a <shepherd-service> for kodi with CONFIG."
+  (match config
+    (($ <home-kodi-configuration> kodi extra-options)
+     (let* ((kodi (file-append kodi "/bin/kodi"))
+            (command `(kodi
+                       "-fs"
+                       ,@extra-options))
+            (log-file (string-append %user-log-dir "/kodi.log")))
+       (list (shepherd-service
+              (documentation "Run the kodi media center.")
+              (provision '(kodi))
+              (start #~(make-forkexec-constructor '#$command
+                                                  #:log-file #$log-file))
+              (stop #~(make-kill-destructor))))))))
+
+(define home-kodi-service-type
+  (service-type
+   (name 'home-kodi)
+   (default-value (home-kodi-configuration))
+   (extensions
+    (list (service-extension home-shepherd-service-type
+                             home-kodi-services)))
+   (description
+    "Install and configure the Kodi media center so that it runs as a Shepherd
+service.")))

doc/guix.texi
@@ -41706,6 +41706,7 @@ services)}.
 * Fonts: Fonts Home Services.   Services for managing User's fonts.
 * Sound: Sound Home Services.   Dealing with audio.
 * Messaging: Messaging Home Services.   Services for managing messaging.
+* Media: Media Home Services.   Services for managing media.
 @end menu
 @c In addition to that Home Services can provide
 
@@ -42830,6 +42831,42 @@ The git package to use.
 @item @code{extra-options} (default: @code{'()})
 Extra options will be passed to @command{znc}, please run @command{man
 znc} for more information.
+
+@end table
+@end deftp
+
+@node Media Home Services
+@subsection Media Home Services
+
+@cindex kodi
+The @uref{https://kodi.tv, KODI media center} can be run as a daemon on
+a media server.  With the @code{(gnu home services kodi)} service, you
+can configure KODI to run upon login.
+
+Here is an example of a service and its configuration that you could add
+to the @code{services} field of your @code{home-environment}:
+
+@lisp
+(service home-kodi-service-type
+  (home-kodi-configuration
+   (extra-options '("--settings="<settings-file>"))))
+@end lisp
+
+@defvr {Scheme Variable} home-kodi-service-type
+This is the type of the @code{git daemon} home service, whose value is an
+@code{home-kodi-configuration} object.
+@end defvr
+
+@deftp {Data Type} home-kodi-configuration
+Available @code{home-kodi-configuration} fields are:
+
+@table @asis
+@item @code{git} (default: @code{git}) (type: file-like)
+The git package to use.
+
+@item @code{extra-options} (default: @code{'()})
+Extra options will be passed to @command{kodi}, please run @command{man
+kodi} for more information.
 
 @end table
 @end deftp

gnu/local.mk
@@ -91,6 +91,7 @@ GNU_SYSTEM_MODULES =				\
   %D%/home/services/symlink-manager.scm		\
   %D%/home/services/fontutils.scm		\
   %D%/home/services/guix.scm			\
+  %D%/home/services/media.scm			\
   %D%/home/services/messaging.scm		\
   %D%/home/services/pm.scm			\
   %D%/home/services/shells.scm			\

po/guix/POTFILES.in
@@ -13,6 +13,7 @@ gnu/services/shepherd.scm
 gnu/services/samba.scm
 gnu/services/version-control.scm
 gnu/home/services.scm
+gnu/home/services/media.scm
 gnu/home/services/messaging.scm
 gnu/home/services/ssh.scm
 gnu/home/services/symlink-manager.scm

commit 6afd1fd6bccf3093c6e7b29ed816ee0d5a10c537

home: services: kodi: Remove host-side use of (shepherd support).
This is a followup to 70056b1b2beebbc9f8ea2c34eacc57f379368ab3, which
inadvertently pulled in (shepherd support) on the host side.

* gnu/home/services/media.scm (home-kodi-services): Change 'command' and
'logfile' to gexps.  Add 'modules' field to 'shepherd-service'.

gnu/home/services/media.scm
@@ -18,11 +18,7 @@
 
 (define-module (gnu home services media)
   #:use-module (srfi srfi-26)
-
   #:use-module (ice-9 match)
-
-  #:use-module (shepherd support)
-
   #:use-module (gnu home services)
   #:use-module (gnu home services shepherd)
   #:use-module (gnu packages kodi)
@@ -30,13 +26,14 @@
   #:use-module (gnu services shepherd)
   #:use-module (guix records)
   #:use-module (guix gexp)
-
   #:export (home-kodi-configuration
             home-kodi-service-type))
 
+
 ;;;
 ;;; Kodi.
 ;;;
+
 (define-record-type* <home-kodi-configuration>
   home-kodi-configuration make-home-kodi-configuration
   home-kodi-configuration?
@@ -50,14 +47,13 @@
   (match config
     (($ <home-kodi-configuration> kodi extra-options)
      (let* ((kodi (file-append kodi "/bin/kodi"))
-            (command `(kodi
-                       "-fs"
-                       ,@extra-options))
-            (log-file (string-append %user-log-dir "/kodi.log")))
+            (command #~'(#$kodi "-fs" #$@extra-options))
+            (log-file #~(string-append %user-log-dir "/kodi.log")))
        (list (shepherd-service
               (documentation "Run the kodi media center.")
               (provision '(kodi))
-              (start #~(make-forkexec-constructor '#$command
+              (modules '((shepherd support)))     ;for '%user-log-dir'
+              (start #~(make-forkexec-constructor #$command
                                                   #:log-file #$log-file))
               (stop #~(make-kill-destructor))))))))
 