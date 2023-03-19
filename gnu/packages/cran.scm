ba6d2701bbf5f3a06cbccc325c0b0c2f3b346404
Ricardo Wurmus <rekado@elephly.net>
2023-03-19 14:44:44 +0100

    gnu: r-filehash: Update to 2.4-5.
    * gnu/packages/cran.scm (r-filehash): Update to 2.4-5.
    [propagated-inputs]: Add r-digest.

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -37011,16 +37011,17 @@ internally managed by a zoolog thesaurus.")
 (define-public r-filehash
   (package
     (name "r-filehash")
-    (version "2.4-3")
+    (version "2.4-5")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "filehash" version))
        (sha256
         (base32
-         "1ga17smgjd4qsnf5a7qk0giwhpc5969ylqj520favs1k6b4y557k"))))
+         "0b52bnsqp65s3r4aw9mvfwfnjmsrqq8ndcadwig547nn9mwy47iv"))))
     (properties `((upstream-name . "filehash")))
     (build-system r-build-system)
+    (propagated-inputs (list r-digest))
     (home-page "http://github.com/rdpeng/filehash")
     (synopsis "Simple key-value database")
     (description
