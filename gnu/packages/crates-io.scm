cdb01c4c2942a427fb37d65b8f4c497242f93b60
Ricardo Wurmus <rekado@elephly.net>
2023-03-22 14:50:38 +0100

    gnu: rust-windows-x86-64-gnu-0.42: Update to 0.42.2.
    * gnu/packages/crates-io.scm (rust-windows-x86-64-gnu-0.42): Update to 0.42.2.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -71536,17 +71536,16 @@ windows crate.")
 (define-public rust-windows-x86-64-gnu-0.42
   (package
     (name "rust-windows-x86-64-gnu")
-    (version "0.42.1")
+    (version "0.42.2")
     (source (origin
               (method url-fetch)
               (uri (crate-uri "windows_x86_64_gnu" version))
               (file-name (string-append name "-" version ".tar.gz"))
               (sha256
                (base32
-                "0icwd66wd6p9hhzds93333wy2knlqbyiynf2rfy7xqc528i0y161"))
+                "0dnbf2xnp3xrvy8v9mgs3var4zq9v9yh9kv79035rdgyp2w15scd"))
               (snippet
-               '(begin
-                  (delete-file "lib/libwindows.a")))))
+               '(delete-file "lib/libwindows.a"))))
     (build-system cargo-build-system)
     (arguments
      (list #:skip-build? #t))
