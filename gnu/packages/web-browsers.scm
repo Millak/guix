f1cb3055ef13c1912bc75947b195fe6ad8e224f9
Efraim Flashner <efraim@flashner.co.il>
2023-03-22 10:31:16 +0200

    gnu: links: Update to 2.29.
    * gnu/packages/web-browsers.scm (links): Update to 2.29.

--- gnu/packages/web-browsers.scm
+++ gnu/packages/web-browsers.scm
@@ -159,14 +159,14 @@ management, extensions such as advertisement blocker and colorful tabs.")
 (define-public links
   (package
     (name "links")
-    (version "2.28")
+    (version "2.29")
     (source (origin
               (method url-fetch)
               (uri (string-append "http://links.twibright.com/download/"
                                   "links-" version ".tar.bz2"))
               (sha256
                (base32
-                "1d2lyj9k2s6brk38k51qfpddwh2w96w6gh9jq5br9rfy2fdlkm9g"))))
+                "163rmng8zkwy0pv9wxcpc0j3gz27g8ba9myrgs7ny6lfng09dai2"))))
     (build-system gnu-build-system)
     (arguments
      (list
