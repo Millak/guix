9ec1af39e29d0fa95d4db4b5b8ef1cf3613c5956
Leo Famulari <leo@famulari.name>
2023-03-26 15:52:07 -0400

    gnu: xdg-utils: Use a pinned version of w3m.
    * gnu/packages/freedesktop.scm (xdg-utils)[native-inputs]: Replace w3m with
    w3m-for-tests.

--- gnu/packages/freedesktop.scm
+++ gnu/packages/freedesktop.scm
@@ -484,7 +484,7 @@ display servers.  It supports many different languages and emoji.")
              "1nai806smz3zcb2l5iny4x7li0fak0rzmjg6vlyhdqm8z25b166p"))))
     (build-system gnu-build-system)
     (native-inputs
-     (list docbook-xsl docbook-xml-4.1.2 libxslt w3m xmlto))
+     (list docbook-xsl docbook-xml-4.1.2 libxslt w3m-for-tests xmlto))
     (inputs
      `(("awk" ,gawk)
        ;; TODO(staging): Make this unconditional, to avoid canonical packages,
