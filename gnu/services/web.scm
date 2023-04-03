6920334e7be57f793accbf2f30347a1516db8aab
Bruno Victal <mirai@makinata.eu>
2023-04-03 12:58:03 +0100

    services: nginx: Add reopen action.
    This is required to allow log file rotations using rottlog, etc.
    
    * gnu/services/web.scm (nginx-shepherd-service): Add reopen shepherd action.
    
    Signed-off-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>

--- gnu/services/web.scm
+++ gnu/services/web.scm
@@ -840,7 +840,11 @@ This has the effect of killing old worker processes and starting new ones, using
 the same configuration file.  It is useful for situations where the same nginx
 configuration file can point to different things after a reload, such as
 renewed TLS certificates, or @code{include}d files.")
-                 (procedure (nginx-action "-s" "reload"))))))))))
+                 (procedure (nginx-action "-s" "reload")))
+               (shepherd-action
+                (name 'reopen)
+                (documentation "Re-open log files.")
+                (procedure (nginx-action "-s" "reopen"))))))))))
 
 (define nginx-service-type
   (service-type (name 'nginx)
