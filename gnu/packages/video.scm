226e7e0437292710aec6e18c15d6521292b280fd
Yovan Naumovski <yovan@gorski.stream>
2023-04-05 23:15:08 +0300

    gnu: gallery-dl: Update to 1.25.1.
    * gnu/packages/video.scm (gallery-dl): Update to 1.25.1.
    
    Signed-off-by: Nicolas Goaziou <mail@nicolasgoaziou.fr>

--- gnu/packages/video.scm
+++ gnu/packages/video.scm
@@ -2334,7 +2334,7 @@ the last played position, etc.")
 (define-public gallery-dl
   (package
     (name "gallery-dl")
-    (version "1.23.0")
+    (version "1.25.1")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://github.com/mikf/gallery-dl"
@@ -2342,7 +2342,7 @@ the last played position, etc.")
                                   version ".tar.gz"))
               (sha256
                (base32
-                "12rhzf8cwsvlg0vxzydgc7x04gmccxkjk8ghb5s8lk0gjp7ysqkh"))))
+                "1zhcd5qnnlmr0qp72rj4bfw0lz4gz1bl65wfn7w21c2xr36nbkxs"))))
     (build-system python-build-system)
     (inputs (list python-requests ffmpeg))
     (home-page "https://github.com/mikf/gallery-dl")
