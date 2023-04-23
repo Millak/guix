6f3be37c682970276b006409267e5c0ff59672c2
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-04-23 17:16:03 +0200

    gnu: open-adventure: Update to 1.16.
    * gnu/packages/games.scm (open-adventure): Update to 1.16.

--- gnu/packages/games.scm
+++ gnu/packages/games.scm
@@ -7050,7 +7050,7 @@ at their peak of economic growth and military prowess.
 (define-public open-adventure
   (package
     (name "open-adventure")
-    (version "1.15")
+    (version "1.16")
     (source
      (origin
        (method git-fetch)
@@ -7059,7 +7059,7 @@ at their peak of economic growth and military prowess.
              (commit version)))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "0gair1dfqgzjzvsasisv2szh3wgy8pfgmpxpisybn6svn294yzhf"))))
+        (base32 "0spciwqcyldalzdd813zwigbldcnyaxi7kfslq1yp0fg4c4a10aa"))))
     (build-system gnu-build-system)
     (arguments
      (list
