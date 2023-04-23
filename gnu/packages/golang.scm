01544d32fb43a7dae0dfe531cc7a04b58c62a447
Nicolas Graves <ngraves@ngraves.fr>
2023-04-23 14:27:29 +0200

    gnu: Add go-github-com-jcmturner-dnsutils-v2.
    * gnu/packages/golang.scm (go-github-com-jcmturner-dnsutils-v2): New variable.
    
    Signed-off-by: Leo Famulari <leo@famulari.name>

--- gnu/packages/golang.scm
+++ gnu/packages/golang.scm
@@ -1431,6 +1431,33 @@ messages automatically.")
 encryption and decryption methods.")
     (license license:asl2.0)))
 
+(define-public go-github-com-jcmturner-dnsutils-v2
+  (package
+    (name "go-github-com-jcmturner-dnsutils-v2")
+    (version "2.0.0")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/jcmturner/dnsutils")
+                    (commit (string-append "v" version))))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "116zbgvfj88vv93fnapmmgyd5g8kzy774cdyzsnnzyzng92j61c9"))))
+    (build-system go-build-system)
+    (arguments
+     '(#:import-path "github.com/jcmturner/dnsutils/v2"))
+    (propagated-inputs (list go-github-com-stretchr-testify))
+    (home-page "https://github.com/jcmturner/dnsutils")
+    (synopsis "Go library with DNS utils")
+    (description
+     "The dnsutils package provides a Go function to return a map of Service
+Records (SRV) in the order they should be used for a given service, protocol
+and name.  The order is determined by the records' priority and randomized
+selection based on their relative weighting.  This package is useful for
+network applications that require accessing services using SRV records.")
+    (license license:asl2.0)))
+
 (define-public go-github-com-kataras-golog
   (package
     (name "go-github-com-kataras-golog")
