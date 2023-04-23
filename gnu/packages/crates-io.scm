fd08ef3077f719474eff61a5256d3eec04393cfc
Efraim Flashner <efraim@flashner.co.il>
2023-04-23 20:52:34 +0300

    gnu: rust-alacritty-terminal-0.15: Update to 0.18.0.
    * gnu/packages/crates-io.scm (rust-alacritty-terminal-0.15): Rename to
    rust-alacritty-terminal-0.18. Update to 0.18.0.
    [arguments]: Don't skip the build.
    [cargo-inputs]: Add rust-alacritty-config-0.1, rust-signal-hook-mio-0.2,
    rust-windows-sys-0.36.  Replace rust-alacritty-config-derive-0.1 with
    0.2, rust-dirs-2 with 4, rust-nix-0.23 with 0.24, rust-parking-lot-0.11
    with 0.12, rust-signal-hook-0.1 with 0.3.  Remove rust-winapi-0.3.
    [cargo-development-inputs]: Add rust-serde-json-1.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -2536,40 +2536,42 @@ Alacritty terminal emulator.")
      "This package provides a failure resistant deserialization derive.")
     (license (list license:expat license:asl2.0))))
 
-(define-public rust-alacritty-terminal-0.15
+(define-public rust-alacritty-terminal-0.18
   (package
     (name "rust-alacritty-terminal")
-    (version "0.15.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (crate-uri "alacritty_terminal" version))
-       (file-name (string-append name "-" version ".tar.gz"))
-       (sha256
-        (base32 "1qv8zv20qgn407v8m196p68yddvhalcppc21702698cj99b45ckc"))))
+    (version "0.18.0")
+    (source (origin
+              (method url-fetch)
+              (uri (crate-uri "alacritty-terminal" version))
+              (file-name (string-append name "-" version ".tar.gz"))
+              (sha256
+               (base32
+                "1dk6zp5646ss0l9ka48jf6ajdzaq0zxl2pndblfl7a0f9lmwkwfx"))))
     (build-system cargo-build-system)
     (arguments
-     `(#:skip-build? #t
-       #:cargo-inputs
-       (("rust-alacritty-config-derive" ,rust-alacritty-config-derive-0.1)
+     `(#:cargo-inputs
+       (("rust-alacritty-config" ,rust-alacritty-config-0.1)
+        ("rust-alacritty-config-derive" ,rust-alacritty-config-derive-0.2)
         ("rust-base64" ,rust-base64-0.13)
         ("rust-bitflags" ,rust-bitflags-1)
-        ("rust-dirs" ,rust-dirs-2)
+        ("rust-dirs" ,rust-dirs-4)
         ("rust-libc" ,rust-libc-0.2)
         ("rust-log" ,rust-log-0.4)
         ("rust-mio" ,rust-mio-0.6)
         ("rust-mio-anonymous-pipes" ,rust-mio-anonymous-pipes-0.2)
         ("rust-mio-extras" ,rust-mio-extras-2)
         ("rust-miow" ,rust-miow-0.3)
-        ("rust-nix" ,rust-nix-0.23)
-        ("rust-parking-lot" ,rust-parking-lot-0.11)
+        ("rust-nix" ,rust-nix-0.24)
+        ("rust-parking-lot" ,rust-parking-lot-0.12)
         ("rust-regex-automata" ,rust-regex-automata-0.1)
         ("rust-serde" ,rust-serde-1)
         ("rust-serde-yaml" ,rust-serde-yaml-0.8)
-        ("rust-signal-hook" ,rust-signal-hook-0.1)
+        ("rust-signal-hook" ,rust-signal-hook-0.3)
+        ("rust-signal-hook-mio" ,rust-signal-hook-mio-0.2)
         ("rust-unicode-width" ,rust-unicode-width-0.1)
         ("rust-vte" ,rust-vte-0.10)
-        ("rust-winapi" ,rust-winapi-0.3))))
+        ("rust-windows-sys" ,rust-windows-sys-0.36))
+       #:cargo-development-inputs (("rust-serde-json" ,rust-serde-json-1))))
     (home-page "https://github.com/alacritty/alacritty")
     (synopsis "Library for writing terminal emulators")
     (description
