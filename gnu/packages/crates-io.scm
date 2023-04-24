eeeff4be50474f4aac9930432c4837c5647daafe
Efraim Flashner <efraim@flashner.co.il>
2023-04-24 16:16:31 +0300

    gnu: Add rust-sctk-adwaita-0.5.
    * gnu/packages/crates-io.scm (rust-sctk-adwaita-0.5): New variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -54124,6 +54124,30 @@ Pwrite traits from the scroll crate.")
        #:cargo-development-inputs
        (("rust-cc" ,rust-cc-1))))))
 
+(define-public rust-sctk-adwaita-0.5
+  (package
+    (name "rust-sctk-adwaita")
+    (version "0.5.4")
+    (source (origin
+              (method url-fetch)
+              (uri (crate-uri "sctk-adwaita" version))
+              (file-name (string-append name "-" version ".tar.gz"))
+              (sha256
+               (base32
+                "02gdwfc0dmx9azqd13sfnfczl0z8jjvci0df5b64q5zxw5xyk96d"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs
+       (("rust-ab-glyph" ,rust-ab-glyph-0.2)
+        ("rust-crossfont" ,rust-crossfont-0.5)
+        ("rust-log" ,rust-log-0.4)
+        ("rust-memmap2" ,rust-memmap2-0.5)
+        ("rust-smithay-client-toolkit" ,rust-smithay-client-toolkit-0.16)
+        ("rust-tiny-skia" ,rust-tiny-skia-0.8))))
+    (home-page "https://github.com/PolyMeilex/sctk-adwaita")
+    (synopsis "Adwaita-like SCTK Frame")
+    (description "Adwaita-like SCTK Frame")
+    (license license:expat)))
 
 (define-public rust-seahash-3
   (package
