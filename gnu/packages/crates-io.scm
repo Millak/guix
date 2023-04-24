4e4fca901f36a6ae729a36d4d5b8abdc4a018321
Efraim Flashner <efraim@flashner.co.il>
2023-04-24 16:14:43 +0300

    gnu: Add rust-ndk-0.7.
    * gnu/packages/crates-io.scm (rust-ndk-0.7): New variable.
    (rust-ndk-0.5): Inherit from rust-ndk-0.7.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -37241,8 +37241,38 @@ general elements and for numerics.")
         ("rust-quickcheck" ,rust-quickcheck-0.7)
         ("rust-rawpointer" ,rust-rawpointer-0.1))))))
 
+(define-public rust-ndk-0.7
+  (package
+    (name "rust-ndk")
+    (version "0.7.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "ndk" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "180sjpyf1ylqgqw4ni8jcg3kv96vvrddzamknp4730kiwjvj4525"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #true              ;XXX: Android only
+       #:cargo-inputs
+       (("rust-bitflags" ,rust-bitflags-1)
+        ("rust-jni" ,rust-jni-0.19)
+        ("rust-jni-glue" ,rust-jni-glue-0.0)
+        ("rust-jni-sys" ,rust-jni-sys-0.3)
+        ("rust-ndk-sys" ,rust-ndk-sys-0.4)
+        ("rust-num-enum" ,rust-num-enum-0.5)
+        ("rust-raw-window-handle" ,rust-raw-window-handle-0.5)
+        ("rust-thiserror" ,rust-thiserror-1))))
+    (home-page "https://github.com/rust-windowing/android-ndk-rs")
+    (synopsis "Safe Rust bindings to the Android NDK")
+    (description
+     "This package provides safe Rust bindings to the Android NDK.")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-ndk-0.5
   (package
+    (inherit rust-ndk-0.7)
     (name "rust-ndk")
     (version "0.5.0")
     (source
@@ -37252,7 +37282,6 @@ general elements and for numerics.")
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
         (base32 "17b9imdmv6cffr12bdpvxw1myxdyvaf6jwkmd3w7abn7akv6in4n"))))
-    (build-system cargo-build-system)
     (arguments
      `(#:skip-build? #true              ;XXX: Android only
        #:cargo-inputs
@@ -37262,12 +37291,7 @@ general elements and for numerics.")
         ("rust-jni-sys" ,rust-jni-sys-0.3)
         ("rust-ndk-sys" ,rust-ndk-sys-0.2)
         ("rust-num-enum" ,rust-num-enum-0.5)
-        ("rust-thiserror" ,rust-thiserror-1))))
-    (home-page "https://github.com/rust-windowing/android-ndk-rs")
-    (synopsis "Safe Rust bindings to the Android NDK")
-    (description
-     "This package provides safe Rust bindings to the Android NDK.")
-    (license (list license:expat license:asl2.0))))
+        ("rust-thiserror" ,rust-thiserror-1))))))
 
 (define-public rust-ndk-0.2
   (package
