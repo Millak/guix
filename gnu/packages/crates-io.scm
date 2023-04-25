abef54ef4dab43a362700629d00bea3bee8be39c
Efraim Flashner <efraim@flashner.co.il>
2023-04-25 14:36:11 +0300

    gnu: rust-webpki-0.19: Fix build.
    * gnu/packages/crates-io.scm (rust-webpki-0.19)[arguments]: Add a phase
    to build the curve25519 tables.
    [native-inputs]: Add clang, perl, python-2.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -71493,7 +71493,18 @@ Verification.")
        (("rust-ring" ,rust-ring-0.14)
         ("rust-untrusted" ,rust-untrusted-0.6))
        #:cargo-development-inputs
-       (("rust-base64" ,rust-base64-0.9))))))
+       (("rust-base64" ,rust-base64-0.9))
+       #:phases
+       (modify-phases %standard-phases
+         (add-before 'build 'build-curve25519-tables
+           (lambda* (#:key vendor-dir #:allow-other-keys)
+             (with-directory-excursion
+               (dirname (car (find-files vendor-dir "make_curve25519_tables.py")))
+               (with-output-to-file "curve25519_tables.h"
+                 (lambda _
+                   (invoke "python" "make_curve25519_tables.py")))))))))
+    (native-inputs
+     (list clang perl python-2))))
 
 (define-public rust-webpki-0.18
   (package
