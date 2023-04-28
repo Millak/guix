ad80b3fa5e57c174a7ba2a2d7a18810477eeeea3
Ricardo Wurmus <rekado@elephly.net>
2023-04-28 18:00:07 +0200

    gnu: r-karyoploter: Update to 1.26.0.
    * gnu/packages/bioconductor.scm (r-karyoploter): Update to 1.26.0.

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -11399,13 +11399,13 @@ gene and isoform level using RNA-seq data")
 (define-public r-karyoploter
   (package
     (name "r-karyoploter")
-    (version "1.24.0")
+    (version "1.26.0")
     (source (origin
               (method url-fetch)
               (uri (bioconductor-uri "karyoploteR" version))
               (sha256
                (base32
-                "1dcxq3651903wwzr7bww8c4wd8h5h7rklnqfgklqpss0psx59d2w"))))
+                "1nqcm2jlazm8wsa4p6a59m1j8y1qdwg0w3kii4dqnkann3q2q68x"))))
     (build-system r-build-system)
     (propagated-inputs
      (list r-annotationdbi
