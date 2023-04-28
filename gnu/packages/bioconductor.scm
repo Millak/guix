0bffa9d7e75b38fc949c3a91372a70266554043c
Ricardo Wurmus <rekado@elephly.net>
2023-04-28 19:01:11 +0200

    gnu: r-rbowtie2: Update to 2.6.0.
    * gnu/packages/bioconductor.scm (r-rbowtie2): Update to 2.6.0.

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -14421,14 +14421,14 @@ This algorithm is based on the publication by Hart et al., 2013 (Pubmed ID
 (define-public r-rbowtie2
   (package
     (name "r-rbowtie2")
-    (version "2.4.2")
+    (version "2.6.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "Rbowtie2" version))
        (sha256
         (base32
-         "0cbm6q4v9ddyx4gnw2p47ssyhnmjfxahvzbzwjzn0my3ixssy0aj"))))
+         "11h4irhi0pxd0l378im455amqfamqypyl0ri2cs8nk1lg184ridr"))))
     (properties `((upstream-name . "Rbowtie2")))
     (build-system r-build-system)
     (propagated-inputs
