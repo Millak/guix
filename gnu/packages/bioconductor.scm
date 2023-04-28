394c951ba281569044e453464c685a7e23382d7e
Ricardo Wurmus <rekado@elephly.net>
2023-04-28 17:59:57 +0200

    gnu: r-mzr: Update to 2.34.0.
    * gnu/packages/bioconductor.scm (r-mzr): Update to 2.34.0.

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -7909,14 +7909,14 @@ specific parser.")
 (define-public r-mzr
   (package
     (name "r-mzr")
-    (version "2.32.0")
+    (version "2.34.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "mzR" version))
        (sha256
         (base32
-         "0p7mkvvaf25si95lpwpr65jm3dzxmgs9i0wilyb2mbxkdcz9vm71"))
+         "0dz9wqaawhkvswv4035xknlicia0m79r8n666s1yf59cfpmdqgs3"))
        (modules '((guix build utils)))
        (snippet
         '(delete-file-recursively "src/boost"))))
