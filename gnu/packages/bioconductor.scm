19636f03416e288e3bf1f6cb7d7df89099caf52a
Ricardo Wurmus <rekado@elephly.net>
2023-04-28 17:59:43 +0200

    gnu: r-alphabeta: Update to 1.14.0.
    * gnu/packages/bioconductor.scm (r-alphabeta): Update to 1.14.0.

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -3093,13 +3093,13 @@ generated as HTML or PDF files, or as Shiny applications.")
 (define-public r-alphabeta
   (package
     (name "r-alphabeta")
-    (version "1.12.0")
+    (version "1.14.0")
     (source (origin
               (method url-fetch)
               (uri (bioconductor-uri "AlphaBeta" version))
               (sha256
                (base32
-                "000apg879li9wkbyrl8cm73z6h0xasqp41h9ir9hywy2v38rmc5b"))))
+                "11431d453xszzjyxr10npnblhlrfw8hl5jgabpxla7cj77w02wnr"))))
     (properties `((upstream-name . "AlphaBeta")))
     (build-system r-build-system)
     (propagated-inputs
