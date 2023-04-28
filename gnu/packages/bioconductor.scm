2ee15aee56af61f9ca21f6aa057659377b3a4b09
Ricardo Wurmus <rekado@elephly.net>
2023-04-28 19:01:11 +0200

    gnu: r-arrmnormalization: Update to 1.40.0.
    * gnu/packages/bioconductor.scm (r-arrmnormalization): Update to 1.40.0.

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -14480,14 +14480,14 @@ expression\".")
 (define-public r-arrmnormalization
   (package
     (name "r-arrmnormalization")
-    (version "1.38.0")
+    (version "1.40.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "ARRmNormalization" version))
        (sha256
         (base32
-         "14mffjsy7cwpa2xf8zdqwdk6mnfj3lf06y8s59ndh531b45nw5wc"))))
+         "001nwl605prg942vmnbb1rwjwgpscpjs27ssic3h10rlmpb65yzp"))))
     (properties
      `((upstream-name . "ARRmNormalization")))
     (build-system r-build-system)
