5d99ad4da459e8e4b2c5c6730ab3a820b509d2f9
Ricardo Wurmus <rekado@elephly.net>
2023-04-28 19:01:20 +0200

    gnu: r-seqbias: Update to 1.48.0.
    * gnu/packages/bioconductor.scm (r-seqbias): Update to 1.48.0.

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -16129,14 +16129,14 @@ with multiple R processes supported by the package @code{parallel}.")
 (define-public r-seqbias
   (package
     (name "r-seqbias")
-    (version "1.46.0")
+    (version "1.48.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "seqbias" version))
        (sha256
         (base32
-         "0mc6lv7vkch138n46gk1q1hfhv37mfxjij39q36wa0jy7s8cagfa"))))
+         "1sspl03m2hf6s5rw6nfqrycb236nvcgygql6apdlg3cjnikf090j"))))
     (properties `((upstream-name . "seqbias")))
     (build-system r-build-system)
     (propagated-inputs
