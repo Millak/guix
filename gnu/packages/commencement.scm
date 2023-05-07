8b855dc1f4a3e31c002c229ad4d91525f4ce97d3
Christopher Baines <mail@cbaines.net>
2023-05-07 12:15:04 +0100

    gnu: findutils: Update XFAIL_TESTS for the hurd.
    This could do with more investigating, but this reflects what I see locally.
    
    * gnu/packages/base.scm (findutils)[arguments]: Update XFAIL_TESTS for the
    hurd.
    * gnu/packages/commencement.scm (findutils-boot0): Stop clearing the
    make-flags, so the above change applies to findutils-boot0.

--- gnu/packages/commencement.scm
+++ gnu/packages/commencement.scm
@@ -2082,9 +2082,7 @@ exec " gcc "/bin/" program
                            (substitute* "gnulib-tests/Makefile"
                              (("^XFAIL_TESTS =")
                               "XFAIL_TESTS = test-fnmatch ")))))
-                     '())))
-           ((#:make-flags flags ''())
-            ''()))))))
+                     '()))))))))
 
 (define file
   (package
