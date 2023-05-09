b4e5844700b2304bfde451322feb5797bf0c6179
Eric Bavier <bavier@posteo.net>
2023-05-08 22:27:40 -0500

    gnu: kodi: Fix build against latest mesa.
    * gnu/packages/patches/kodi-mesa-eglchromium.patch: New file.
    * gnu/local.mk (dist_patch_DATA): Add it.
    * gnu/packages/kodi.scm (kodi)[source]: Use it.

--- gnu/packages/kodi.scm
+++ gnu/packages/kodi.scm
@@ -284,7 +284,8 @@ alternatives. In compilers, this can reduce the cascade of secondary errors.")
               (sha256
                (base32
                 "1pfd1ajivr865h0fkpbv778626c4czrvq8650bzqv9aqzh8f36my"))
-              (patches (search-patches "kodi-set-libcurl-ssl-parameters.patch"))
+              (patches (search-patches "kodi-set-libcurl-ssl-parameters.patch"
+                                       "kodi-mesa-eglchromium.patch"))
               (modules '((guix build utils)))
               (snippet
                '(begin
