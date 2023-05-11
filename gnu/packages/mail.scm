b9a87d02e80abbcedb449b711b98468e0fca2bcb
Efraim Flashner <efraim@flashner.co.il>
2023-05-11 21:33:05 +0300

    gnu: mutt: Update to 2.2.10.
    * gnu/packages/mail.scm (mutt): Update to 2.2.10.

--- gnu/packages/mail.scm
+++ gnu/packages/mail.scm
@@ -602,7 +602,7 @@ aliasing facilities to work just as they would on normal mail.")
 (define-public mutt
   (package
     (name "mutt")
-    (version "2.2.9")
+    (version "2.2.10")
     (source (origin
              (method url-fetch)
              (uri (list
@@ -612,7 +612,7 @@ aliasing facilities to work just as they would on normal mail.")
                                    version ".tar.gz")))
              (sha256
               (base32
-               "1yyg49sgghi7pgw7xr3cnzgypa9yd0kdc3nsrqq1zzjq3liinlzs"))
+               "121xrns9b0n5cdjb7nv5a6idjjk58pp7pdclgdphjy9g88i3yxsd"))
              (patches (search-patches "mutt-store-references.patch"))))
     (build-system gnu-build-system)
     (inputs
