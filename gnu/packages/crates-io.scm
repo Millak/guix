33d1c85630cfa3802468c56e146891173a77d73d
Efraim Flashner <efraim@flashner.co.il>
2023-05-11 17:30:28 +0300

    gnu: Add rust-orion-0.17.
    * gnu/packages/crates-io.scm (rust-orion-0.17): New variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -41430,6 +41430,40 @@ iteration.  NOTE: This crate was renamed to @code{indexmap}.  Please use it
 under its new name.")
     (license (list license:asl2.0 license:expat))))
 
+(define-public rust-orion-0.17
+  (package
+    (name "rust-orion")
+    (version "0.17.4")
+    (source (origin
+              (method url-fetch)
+              (uri (crate-uri "orion" version))
+              (file-name (string-append name "-" version ".tar.gz"))
+              (sha256
+               (base32
+                "0ri0b0vyd9vqwlzlcv0q4i7r9pga23q7nnnvd5z4zycjc9v4mryb"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs
+       (("rust-ct-codecs" ,rust-ct-codecs-1)
+        ("rust-fiat-crypto" ,rust-fiat-crypto-0.1)
+        ("rust-getrandom" ,rust-getrandom-0.2)
+        ("rust-serde" ,rust-serde-1)
+        ("rust-subtle" ,rust-subtle-2)
+        ("rust-zeroize" ,rust-zeroize-1))
+       #:cargo-development-inputs
+       (("rust-criterion" ,rust-criterion-0.4)
+        ("rust-hex" ,rust-hex-0.4)
+        ("rust-quickcheck" ,rust-quickcheck-1)
+        ("rust-quickcheck-macros" ,rust-quickcheck-macros-1)
+        ("rust-serde" ,rust-serde-1)
+        ("rust-serde-json" ,rust-serde-json-1))))
+    (home-page "https://github.com/orion-rs/orion")
+    (synopsis "Pure-Rust crypto")
+    (description
+     "Orion is a cryptography library written in pure Rust.  It aims to provide
+easy and usable crypto while trying to minimize the use of unsafe code.")
+    (license license:expat)))
+
 (define-public rust-os-info-3
   (package
     (name "rust-os-info")
