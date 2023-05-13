50ce21605abec4096730cd3d9b1d7b4c6e2e38d8
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-05-13 12:03:52 +0200

    gnu: texlive-latex-marginfix -> texlive-marginfix.
    * gnu/packages/tex.scm (texlive-marginfix): New variable.
    (texlive-latex-marginfix): Deprecate variable.

--- gnu/packages/tex.scm
+++ gnu/packages/tex.scm
@@ -6098,24 +6098,30 @@ always (re)defines a command.  There is also @code{\\makeenvironment} and
 @code{\\provideenvironment} for environments.")
     (license license:lppl1.3c+)))
 
-(define-public texlive-latex-marginfix
+(define-public texlive-marginfix
   (package
-    (inherit (simple-texlive-package
-              "texlive-latex-marginfix"
-              (list "doc/latex/marginfix/"
-                    "tex/latex/marginfix/")
-              (base32 "1qinf42mjry8bi5h3kgbi6hcymnk8fw0w43527wm95nc365dq0mf")
-              #:trivial? #t))
+    (name "texlive-marginfix")
+    (version (number->string %texlive-revision))
+    (source (texlive-origin
+             name version
+             (list "doc/latex/marginfix/"
+                   "source/latex/marginfix/"
+                   "tex/latex/marginfix/")
+             (base32
+              "0y6lmxm5ws2g0rqisvgdc8kg3whnvabrkl662sb4jvvaz02y3qdi")))
+    (outputs '("out" "doc"))
     (build-system texlive-build-system)
     (home-page "https://ctan.org/pkg/marginfix")
-    (synopsis "Patch \\marginpar to avoid overfull margins")
+    (synopsis "Patch @code{\\marginpar} to avoid overfull margins")
     (description
      "Authors using LaTeX to typeset books with significant margin material
 often run into the problem of long notes running off the bottom of the page.
-This package implements a solution to make marginpars ``just work'' by keeping
-a list of floating inserts and arranging them intelligently in the output
-routine.")
+This package implements a solution to make @code{marginpars} just work by
+keeping a list of floating inserts and arranging them intelligently in the
+output routine.")
     (license license:lppl)))
+
+(define-deprecated-package texlive-latex-marginfix texlive-marginfix)
 
 (define-public texlive-metalogo
   (package
