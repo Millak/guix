a1998bb56cabb2ecfe66bbdd9454f49cc6a14c50
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-05-14 23:02:28 +0200

    gnu: texlive-ifoddpage: Refresh package definition.
    * gnu/packages/tex.scm (texlive-ifoddpage): Remove SIMPLE-TEXLIVE-PACKAGE
    call.

--- gnu/packages/tex.scm
+++ gnu/packages/tex.scm
@@ -11312,30 +11312,30 @@ a physical page.")
       (license license:lppl))))
 
 (define-public texlive-ifoddpage
-  (let ((template (simple-texlive-package
-                   "texlive-ifoddpage"
-                   (list "/source/latex/ifoddpage/")
-                   (base32
-                    "14x0haj3xjsk9dn2djg117sl7x5nbwgbivhjj3ichnxlgrlf1bis"))))
-    (package
-      (inherit template)
-      (arguments
-       (substitute-keyword-arguments (package-arguments template)
-         ((#:tex-directory _ '())
-          "latex/ifoddpage")
-         ((#:build-targets _ '())
-          ''("ifoddpage.ins"))))
-      (native-inputs
-       (list texlive-ydoc))
-      (home-page "https://www.ctan.org/pkg/ifoddpage")
-      (synopsis "Determine if the current page is odd or even")
-      (description "This package provides an @code{\\ifoddpage} conditional to
-determine if the current page is odd or even.  The macro @code{\\checkoddpage}
-must be used directly before to check the page number using a label.  Two
-compiler runs are therefore required to achieve correct results.  In addition,
-the conditional @code{\\ifoddpageoronside} is provided which is also true in
-@code{oneside} mode where all pages use the odd page layout.")
-      (license license:lppl1.3))))
+  (package
+    (name "texlive-ifoddpage")
+    (version (number->string %texlive-revision))
+    (source (texlive-origin
+             name version
+             (list "doc/latex/ifoddpage/"
+                   "source/latex/ifoddpage/"
+                   "tex/latex/ifoddpage/")
+             (base32
+              "06xn3dwf6aa8j3lmvvgwfadw2ahw770jx91x8nyl8zir58aiys5s")))
+    (outputs '("out" "doc"))
+    (build-system texlive-build-system)
+    (native-inputs
+     (list texlive-ydoc))
+    (home-page "https://ctan.org/pkg/ifoddpage")
+    (synopsis "Determine if the current page is odd or even")
+    (description
+     "This package provides an @code{\\ifoddpage} conditional to determine if
+the current page is odd or even.  The macro @code{\\checkoddpage} must be used
+directly before to check the page number using a label.  Two compiler runs are
+therefore required to achieve correct results.  In addition, the conditional
+@code{\\ifoddpageoronside} is provided which is also true in @code{oneside}
+mode where all pages use the odd page layout.")
+    (license license:lppl1.3)))
 
 (define-public texlive-storebox
   (let ((template (simple-texlive-package
