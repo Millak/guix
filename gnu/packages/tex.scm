6886a807b6c00fa7b0a1727233241007d6f06f7b
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-05-15 19:28:58 +0200

    gnu: texlive-symbol: Refresh package definition.
    * gnu/packages/tex.scm (texlive-symbol): Remove SIMPLE-TEXLIVE-PACKAGE
    call.

--- gnu/packages/tex.scm
+++ gnu/packages/tex.scm
@@ -11003,24 +11003,35 @@ included in the bundle.")
 
 (define-public texlive-symbol
   (package
-    (inherit (simple-texlive-package
-              "texlive-symbol"
-              (list "/dvips/symbol/"
-                    "/fonts/afm/adobe/symbol/"
-                    "/fonts/afm/urw/symbol/"
-                    "/fonts/tfm/adobe/symbol/"
-                    "/fonts/tfm/urw35vf/symbol/"
-                    "/fonts/type1/urw/symbol/"
-                    "/fonts/map/dvips/symbol/"
-                    "/tex/latex/symbol/")
-              (base32
-               "01xiygb88xwi7rfvh1zrlxzi5pqb5fvylws5zzszg379iz4pyzwj")
-              #:trivial? #t))
+    (name "texlive-symbol")
+    (version (number->string %texlive-revision))
+    (source (texlive-origin
+             name version
+             (list "dvips/symbol/"
+                   "fonts/afm/adobe/symbol/"
+                   "fonts/afm/urw/symbol/"
+                   "fonts/map/dvips/symbol/"
+                   "fonts/tfm/adobe/symbol/"
+                   "fonts/tfm/monotype/symbol/"
+                   "fonts/tfm/urw35vf/symbol/"
+                   "fonts/type1/urw/symbol/"
+                   "tex/latex/symbol/")
+             (base32
+              "1pdkpr86bhia5hcmf7q3nhvklnsga4mqqrrirgl8a7al7x6q3ivs")))
+    (build-system texlive-build-system)
     (home-page "https://ctan.org/pkg/urw-base35")
     (synopsis "URW Base 35 font pack for LaTeX")
-    (description "This package provides a drop-in replacement for the
-Symbol font from Adobe's basic set.")
-    (license license:gpl2)))
+    (description
+     "This package provides a set of fonts for use as drop-in replacements for
+Adobe's basic set, comprising: Century Schoolbook (substituting for Adobe's
+New Century Schoolbook); Dingbats (substituting for Adobe's Zapf Dingbats);
+Nimbus Mono L (substituting for Abobe's Courier); Nimbus Roman No9
+L (substituting for Adobe's Times); Nimbus Sans L (substituting for Adobe's
+Helvetica); Standard Symbols L (substituting for Adobe's Symbol); URW Bookman;
+URW Chancery L Medium Italic (substituting for Adobe's Zapf Chancery); URW
+Gothic L Book (substituting for Adobe's Avant Garde); and URW Palladio
+L (substituting for Adobe's Palatino).")
+    (license license:gpl3+)))
 
 (define-public texlive-mathpazo
   (package
