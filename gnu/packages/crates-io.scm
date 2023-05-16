db023f207f5f7f25294f2f9f5c107bcc1b34a77e
Efraim Flashner <efraim@flashner.co.il>
2023-05-16 16:53:45 +0300

    gnu: rust-pear-codegen-0.1: Update to 0.1.5.
    * gnu/packages/crates-io.scm (rust-pear-codegen-0.1): Update to 0.1.5.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -43888,14 +43888,14 @@ pdqsort.")
 (define-public rust-pear-codegen-0.1
   (package
     (name "rust-pear-codegen")
-    (version "0.1.4")
+    (version "0.1.5")
     (source
      (origin
        (method url-fetch)
        (uri (crate-uri "pear_codegen" version))
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
-        (base32 "1jk8jxkgjd3pymldkckg2ligkpy4nnskgcj8qf3yzlf3zlvcihdz"))))
+        (base32 "19lcpkfspizd4ywwvca6rxgc311m070k3ndvwa9vrbw1snjqna60"))))
     (build-system cargo-build-system)
     (arguments
      `(#:skip-build? #t
