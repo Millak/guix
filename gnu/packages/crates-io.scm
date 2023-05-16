ca803ebdc1d8818ed569d2793752d81cea48f1a4
Efraim Flashner <efraim@flashner.co.il>
2023-05-16 16:50:16 +0300

    gnu: rust-devise-core-0.2: Update to 0.2.1.
    * gnu/packages/crates-io.scm (rust-devise-core-0.2): Update to 0.2.1.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -18660,14 +18660,14 @@ procedural macros.")
 (define-public rust-devise-core-0.2
   (package
     (name "rust-devise-core")
-    (version "0.2.0")
+    (version "0.2.1")
     (source
      (origin
        (method url-fetch)
        (uri (crate-uri "devise_core" version))
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
-        (base32 "11v4z5kljmpiyif3mmfnm3rl1lsqygjlfy2wll7frqxm4adwahfg"))))
+        (base32 "0wr3jdzzibpafz73hcca83wnzdgjinvm7axmxnyfkbasbnfkw1fi"))))
     (build-system cargo-build-system)
     (arguments
      `(#:skip-build? #t
