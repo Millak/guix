1638d1f0cecb52a7392d78534e9a0136878759e4
Leo Famulari <leo@famulari.name>
2023-05-16 19:32:16 -0400

    gnu: linux-libre: Update to 6.3.3.
    * gnu/packages/linux.scm (linux-libre-version, linux-libre-gnu-revision,
    linux-libre-pristine-source, linux-libre-source, linux-libre,
    linux-libre-with-bpf): Update to linux-libre 6.3.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -1121,11 +1121,11 @@ Linux kernel.  It has been modified to remove all non-free binary blobs.")
                        "aarch64-linux" "powerpc64le-linux" "riscv64-linux")
                      #:configuration-file kernel-config))
 
-(define-public linux-libre-version         linux-libre-6.2-version)
-(define-public linux-libre-gnu-revision    linux-libre-6.2-gnu-revision)
-(define-public linux-libre-pristine-source linux-libre-6.2-pristine-source)
-(define-public linux-libre-source          linux-libre-6.2-source)
-(define-public linux-libre                 linux-libre-6.2)
+(define-public linux-libre-version         linux-libre-6.3-version)
+(define-public linux-libre-gnu-revision    linux-libre-6.3-gnu-revision)
+(define-public linux-libre-pristine-source linux-libre-6.3-pristine-source)
+(define-public linux-libre-source          linux-libre-6.3-source)
+(define-public linux-libre                 linux-libre-6.3)
 
 (define-public linux-libre-6.1
   (make-linux-libre* linux-libre-6.1-version
@@ -1356,9 +1356,9 @@ Linux kernel.  It has been modified to remove all non-free binary blobs.")
 (define-public linux-libre-with-bpf
   (let ((base-linux-libre
          (make-linux-libre*
-          linux-libre-6.2-version
-          linux-libre-6.2-gnu-revision
-          linux-libre-6.2-source
+          linux-libre-6.3-version
+          linux-libre-6.3-gnu-revision
+          linux-libre-6.3-source
           '("x86_64-linux" "i686-linux" "armhf-linux"
             "aarch64-linux" "powerpc64le-linux" "riscv64-linux")
           #:extra-version "bpf"
