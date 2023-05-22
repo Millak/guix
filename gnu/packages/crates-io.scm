bd4a4caa09517e84be2f981451b95e45258a0c02
Efraim Flashner <efraim@flashner.co.il>
2023-05-22 17:19:39 +0300

    gnu: rust-thread-local-1: Update to 1.1.7.
    * gnu/packages/crates-io.scm (rust-thread-local-1): Update to 1.1.7.
    [arguments]: Don't skip build.
    [cargo-inputs]: Add rust-cfg-if-1. Remove rust-criterion-0.3.
    [cargo-development-inputs]: Add rust-criterion-0.3.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -65109,20 +65109,21 @@ different for every thread.")
 (define-public rust-thread-local-1
   (package
     (name "rust-thread-local")
-    (version "1.1.3")
+    (version "1.1.7")
     (source
      (origin
        (method url-fetch)
        (uri (crate-uri "thread_local" version))
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
-        (base32 "1gccp3grndpi6dyhzylz4hkqnkzc1xyri98n0xwwhnn90i7d4640"))))
+        (base32 "0lp19jdgvp5m4l60cgxdnl00yw1hlqy8gcywg9bddwng9h36zp9z"))))
     (build-system cargo-build-system)
     (arguments
-     `(#:skip-build? #t
-       #:cargo-inputs
-       (("rust-criterion" ,rust-criterion-0.3)
-        ("rust-once-cell" ,rust-once-cell-1))))
+     `(#:cargo-inputs
+       (("rust-cfg-if" ,rust-cfg-if-1)
+        ("rust-once-cell" ,rust-once-cell-1))
+       #:cargo-development-inputs
+       (("rust-criterion" ,rust-criterion-0.4))))
     (home-page "https://github.com/Amanieu/thread_local-rs")
     (synopsis "Per-object thread-local storage")
     (description "Per-object thread-local storage.")
