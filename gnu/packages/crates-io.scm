32465480adfe40725bab3675eec56c98888093b5
Efraim Flashner <efraim@flashner.co.il>
2023-05-22 16:35:03 +0300

    gnu: Add rust-anstyle-wincon-1.
    * gnu/packages/crates-io.scm (rust-anstyle-wincon-1): New variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -3082,6 +3082,29 @@ escape codes.")
     (description "This package provides ANSI text styling.")
     (license (list license:expat license:asl2.0))))
 
+(define-public rust-anstyle-wincon-1
+  (package
+    (name "rust-anstyle-wincon")
+    (version "1.0.1")
+    (source (origin
+              (method url-fetch)
+              (uri (crate-uri "anstyle-wincon" version))
+              (file-name (string-append name "-" version ".tar.gz"))
+              (sha256
+               (base32
+                "12714vwjf4c1wm3qf49m5vmd93qvq2nav6zpjc0bxbh3ayjby2hq"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs
+       (("rust-anstyle" ,rust-anstyle-1)
+        ("rust-windows-sys" ,rust-windows-sys-0.48))
+       #:cargo-development-inputs
+       (("rust-lexopt" ,rust-lexopt-0.3))))
+    (home-page "https://github.com/rust-cli/anstyle")
+    (synopsis "Styling legacy Windows terminals")
+    (description "Styling legacy Windows terminals")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-antidote-1
   (package
     (name "rust-antidote")
