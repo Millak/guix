c348b1be3891e6eb47bbdd9fc1587aba2b6ab0b7
Navid Afkhami <navid.afkhami@mdc-berlin.de>
2023-06-09 16:05:57 +0200

    gnu: Add python-mofax.
    * gnu/packages/bioinformatics.scm (python-mofax): New variable.
    
    Signed-off-by: Ricardo Wurmus <rekado@elephly.net>

--- gnu/packages/bioinformatics.scm
+++ gnu/packages/bioinformatics.scm
@@ -3312,6 +3312,51 @@ off-target reads for a capture method that targets CpG-rich region.")
 multiple sequence alignments.")
     (license license:expat)))
 
+(define-public python-mofax
+  ;; This is a recent commit from the "dev" branch, which is much more recent
+  ;; than the latest commit from the "master" branch.
+  (let ((commit "4d96f8f0a5d5251847353656f523684d66c3c47a")
+        (revision "0"))
+    (package
+      (name "python-mofax")
+      (version (git-version "0.4.0" revision commit))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                      (url "https://github.com/bioFAM/mofax")
+                      (commit commit)))
+                (file-name (git-file-name name version))
+                (sha256
+                 (base32
+                  "1lwrw0qyvvnyiqz1l20dhcf7dxidb80cqgvk78czvdgba87yxzqx"))
+                (modules '((guix build utils)))
+                ;; Prevent the pyproject-build-system from guessing that flit
+                ;; should be used as a builder.
+                (snippet '(substitute* "pyproject.toml"
+                            (("^#.*") "")))))
+      (build-system pyproject-build-system)
+      (arguments
+       (list
+        ;; This test is failing due to a bug. The bug has been reported to the
+        ;; developers. See https://github.com/bioFAM/mofax/issues/12 for more
+        ;; info.
+        #:test-flags '(list "-k" "not test_get_methods")))
+      (propagated-inputs (list python-h5py
+                               python-matplotlib
+                               python-pandas
+                               python-poetry-core
+                               python-scipy
+                               python-seaborn))
+      (native-inputs (list python-numpy python-pytest))
+      (home-page "https://github.com/bioFAM/mofax")
+      (synopsis
+       "Motif activity finder for transcription factor motif analysis")
+      (description
+       "MoFax is a Python package for transcription factor motif analysis.
+It provides convenience functions to load and visualize factor models trained
+with MOFA+ in Python.")
+      (license license:expat))))
+
 (define-public python-pyega3
   (package
     (name "python-pyega3")
