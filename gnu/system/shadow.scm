Millak/guix

commit 66ecffbeba0685ff2f1071db8aeb2393986afb97

services: Add default values.
* gnu/services.scm (boot-service-type, activation-service-type,
etc-service-type, profile-service-type): Add default-value.
* gnu/system/shadow.scm (account-service-type): Add default-value.

gnu/system/shadow.scm
@@ -458,6 +458,7 @@ the /etc/skel directory for those."
                                           (const '(user-homes)))
                        (service-extension etc-service-type
                                           etc-files)))
+                (default-value #f)
                 (description
                  "Ensure the specified user accounts and groups exist, as well
 as each account home directory.")))

gnu/services.scm
@@ -403,6 +403,7 @@ directory."
                                           boot-script-entry)))
                 (compose identity)
                 (extend compute-boot-script)
+                (default-value #f)
                 (description
                  "Produce the operating system's boot script, which is spawned
 by the initrd once the root file system is mounted.")))
@@ -661,6 +662,7 @@ system directory."
                                           activation-profile-entry)))
                 (compose identity)
                 (extend second-argument)
+                (default-value #f)
                 (description
                  "Run @dfn{activation} code at boot time and upon
 @command{guix system reconfigure} completion.")))
@@ -803,6 +805,7 @@ directory."
                   (service-extension system-service-type etc-entry)))
                 (compose concatenate)
                 (extend append)
+                (default-value '())
                 (description "Populate the @file{/etc} directory.")))
 
 (define-deprecated (etc-service files)
@@ -869,6 +872,7 @@ executables, making them setuid and/or setgid.")))
                                           packages->profile-entry)))
                 (compose concatenate)
                 (extend append)
+                (default-value '())
                 (description
                  "This is the @dfn{system profile}, available as
 @file{/run/current-system/profile}.  It contains packages that the sysadmin

commit 56ad923f47d352e1bee1a94da1a246ae21c03569

system: account: Fix default value.
Fixes a regression introduced in
66ecffbeba0685ff2f1071db8aeb2393986afb97.

* gnu/system/shadow.scm (account-service-type)[default-value]: Change to
the empty list.

gnu/system/shadow.scm
@@ -1,5 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2013-2020, 2022 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2013-2020, 2022, 2023 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2016 Alex Griffin <a@ajgrf.com>
 ;;; Copyright © 2020 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
 ;;; Copyright © 2020, 2023 Efraim Flashner <efraim@flashner.co.il>
@@ -458,7 +458,7 @@ the /etc/skel directory for those."
                                           (const '(user-homes)))
                        (service-extension etc-service-type
                                           etc-files)))
-                (default-value #f)
+                (default-value '())
                 (description
                  "Ensure the specified user accounts and groups exist, as well
 as each account home directory.")))