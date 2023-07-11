565d84170a85b7962a66f919b020d2983ca1ef26
Ricardo Wurmus <rekado@elephly.net>
2023-07-11 20:16:53 +0200

    gnu: r-affy: Update to 1.78.1.
    * gnu/packages/bioconductor.scm (r-affy): Update to 1.78.1.

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -5414,14 +5414,14 @@ performing parallel computations on multicore machines.")
 (define-public r-affy
   (package
     (name "r-affy")
-    (version "1.78.0")
+    (version "1.78.1")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "affy" version))
        (sha256
         (base32
-         "15hpxflygpy1sid0c4hlzmsc13nqyzs6j74md0ri478qysiqjnpf"))))
+         "1xj8pnaa782k1hxaiba6mcsqr21bk8xz31916836jz5l9848zjsw"))))
     (build-system r-build-system)
     (propagated-inputs
      (list r-affyio
