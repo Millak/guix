94a108d2717686bfa143dce819e76d1854ebbfb6
Efraim Flashner <efraim@flashner.co.il>
2023-07-13 19:11:01 +0300

    gnu: Add rust-scroll-0.11.
    * gnu/packages/crates-io.scm (rust-scroll-0.11): New variable.
    (rust-scroll-0.10): Inherit from rust-scroll-0.11.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -56387,8 +56387,34 @@ function.")
         ("rust-sha2" ,rust-sha2-0.9)
         ("rust-subtle" ,rust-subtle-2))))))
 
+(define-public rust-scroll-0.11
+  (package
+    (name "rust-scroll")
+    (version "0.11.0")
+    (source (origin
+              (method url-fetch)
+              (uri (crate-uri "scroll" version))
+              (file-name (string-append name "-" version ".tar.gz"))
+              (sha256
+               (base32
+                "1nhrhpzf95pxbcjjy222blwf8rl3adws6vsqax0yzyxsa6snbi84"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs
+       (("rust-scroll-derive" ,rust-scroll-derive-0.11))
+       #:cargo-development-inputs
+       (("rust-byteorder" ,rust-byteorder-1)
+        ("rust-rayon" ,rust-rayon-1))))
+    (home-page "https://github.com/m4b/scroll")
+    (synopsis "Endian-aware Read/Write traits for byte buffers")
+    (description
+     "This package provides a suite of powerful, extensible, generic,
+endian-aware Read/Write traits for byte buffers.")
+    (license license:expat)))
+
 (define-public rust-scroll-0.10
   (package
+    (inherit rust-scroll-0.11)
     (name "rust-scroll")
     (version "0.10.2")
     (source
@@ -56400,19 +56426,12 @@ function.")
         (sha256
          (base32
           "1v61drdig30qfx2xh8bn7qdk2xgqbmmhwyrznjl0gf1h915qv8px"))))
-    (build-system cargo-build-system)
     (arguments
      `(#:cargo-inputs
        (("rust-scroll-derive" ,rust-scroll-derive-0.10))
        #:cargo-development-inputs
        (("rust-byteorder" ,rust-byteorder-1)
-        ("rust-rayon" ,rust-rayon-1))))
-    (home-page "https://github.com/m4b/scroll")
-    (synopsis "Endian-aware Read/Write traits for byte buffers")
-    (description
-     "This package provides a suite of powerful, extensible, generic,
-endian-aware Read/Write traits for byte buffers.")
-    (license license:expat)))
+        ("rust-rayon" ,rust-rayon-1))))))
 
 (define-public rust-scroll-0.9
   (package
