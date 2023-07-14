21b718f4d6c3ded8ef50d12f6e9ae6474f74620f
Simon South <simon@simonsouth.net>
2023-07-14 15:45:35 -0400

    gnu: python-pyusb: Update to 1.2.1.
    * gnu/packages/libusb.scm (python-pyusb): Update to 1.2.1.
    [native-inputs]: Add python-setuptools-scm.
    
    Signed-off-by: 宋文武 <iyzsong@member.fsf.org>

--- gnu/packages/libusb.scm
+++ gnu/packages/libusb.scm
@@ -274,14 +274,14 @@ wrapper for accessing libusb-1.0.")
 (define-public python-pyusb
   (package
     (name "python-pyusb")
-    (version "1.0.2")
+    (version "1.2.1")
     (source
      (origin
        (method url-fetch)
        (uri (pypi-uri "pyusb" version))
        (sha256
         (base32
-         "0qkk2jn270jwwl1x26hmdhb14m9kkbrzzwzizdjcl1a29b6756sf"))))
+         "1fg7knfzybzija2b01pzrzhzsj989scl12sb2ra4f503l8279k54"))))
     (build-system python-build-system)
     (arguments
      `(#:tests? #f                      ; no tests
@@ -302,6 +302,9 @@ wrapper for accessing libusb-1.0.")
                                    "^libusb-.*\\.so\\..*"))
                  "\"")))
              #t)))))
+
+    (native-inputs
+     (list python-setuptools-scm))
     (inputs
      (list libusb))
     (home-page "https://pyusb.github.io/pyusb/")
