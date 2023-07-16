0bc890a5cb9153b27c99df8014f679d478cfd3d3
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-07-16 17:39:02 +0200

    gnu: Add texlive-collection-binextra.
    * gnu/packages/tex.scm (texlive-collection-binextra): New variable.

--- gnu/packages/tex.scm
+++ gnu/packages/tex.scm
@@ -89,6 +89,7 @@
   #:use-module (gnu packages perl)
   #:use-module (gnu packages perl-check)
   #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages plotutils)
   #:use-module (gnu packages python)
   #:use-module (gnu packages python-xyz)
   #:use-module (gnu packages qt)
@@ -21979,6 +21980,127 @@ The macros were designed for use within other macros.")
      "These files are regarded as basic for any TeX system, covering plain TeX
 macros, Computer Modern fonts, and configuration for common drivers; no
 LaTeX.")
+    (license (license:fsf-free "https://www.tug.org/texlive/copying.html"))))
+
+(define-public texlive-collection-binextra
+  (package
+    (name "texlive-collection-binextra")
+    (version (number->string %texlive-revision))
+    (source #f)
+    (build-system trivial-build-system)
+    (arguments (list #:builder #~(mkdir #$output)))
+    (propagated-inputs
+     (list asymptote
+           texlive-a2ping
+           texlive-adhocfilelist
+           texlive-arara
+           texlive-bibtex8
+           texlive-bibtexu
+           texlive-bundledoc
+           texlive-checklistings
+           texlive-chklref
+           texlive-chktex
+           texlive-clojure-pamphlet
+           texlive-cluttex
+           texlive-collection-basic
+           texlive-ctan-o-mat
+           texlive-ctan-chk
+           texlive-ctanbib
+           texlive-ctanify
+           texlive-ctanupload
+           texlive-ctie
+           texlive-cweb
+           texlive-de-macro
+           texlive-detex
+           texlive-digestif
+           texlive-dtl
+           texlive-dtxgen
+           texlive-dvi2tty
+           texlive-dviasm
+           texlive-dvicopy
+           texlive-dvidvi
+           texlive-dviinfox
+           texlive-dviljk
+           texlive-dviout-util
+           texlive-dvipng
+           texlive-dvipos
+           texlive-dvisvgm
+           texlive-findhyph
+           texlive-fragmaster
+           texlive-git-latexdiff
+           texlive-gsftopk
+           texlive-hook-pre-commit-pkg
+           texlive-installfont
+           texlive-ketcindy
+           texlive-lacheck
+           texlive-latex-git-log
+           texlive-latex-papersize
+           texlive-latex2man
+           texlive-latex2nemeth
+           texlive-latexdiff
+           texlive-latexfileversion
+           texlive-latexindent
+           texlive-latexmk
+           texlive-latexpand
+           texlive-light-latex-make
+           texlive-listings-ext
+           texlive-ltxfileinfo
+           texlive-ltximg
+           texlive-luajittex
+           texlive-make4ht
+           texlive-match-parens
+           texlive-mflua
+           texlive-mkjobtexmf
+           texlive-optexcount
+           texlive-patgen
+           texlive-pdfbook2
+           texlive-pdfcrop
+           texlive-pdfjam
+           texlive-pdflatexpicscale
+           texlive-pdftex-quiet
+           texlive-pdftosrc
+           texlive-pdfxup
+           texlive-pfarrei
+           texlive-pkfix
+           texlive-pkfix-helper
+           texlive-purifyeps
+           texlive-pythontex
+           texlive-seetexk
+           texlive-spix
+           texlive-srcredact
+           texlive-sty2dtx
+           texlive-synctex
+           texlive-tex4ebook
+           texlive-texaccents
+           texlive-texcount
+           texlive-texdef
+           texlive-texdiff
+           texlive-texdirflatten
+           texlive-texdoc
+           texlive-texdoctk
+           texlive-texfot
+           texlive-texliveonfly
+           texlive-texloganalyser
+           texlive-texlogfilter
+           texlive-texlogsieve
+           texlive-texosquery
+           texlive-texplate
+           texlive-texware
+           texlive-tie
+           texlive-tlcockpit
+           texlive-tpic2pdftex
+           texlive-typeoutfileinfo
+           texlive-upmendex
+           texlive-web
+           texlive-xindex
+           texlive-xindy
+           texlive-xpdfopen))
+    (home-page "https://www.tug.org/texlive/")
+    (synopsis "TeX auxiliary programs")
+    (description
+     "This package provides a myriad of additional TeX-related support
+programs.  It includes programs and macros for DVI file manipulation, literate
+programming, patgen, and plenty more.")
     (license (license:fsf-free "https://www.tug.org/texlive/copying.html"))))
 
 (define-public texlive-collection-context
