cf9a4d9dc357d92acf41c42c812722ff03549153
Ricardo Wurmus <rekado@elephly.net>
2023-07-18 14:48:59 +0200

    gnu: Add python-scm-sr-ht.
    * gnu/packages/sourcehut.scm (python-scm-sr-ht): New variable.

--- gnu/packages/sourcehut.scm
+++ gnu/packages/sourcehut.scm
@@ -79,3 +79,34 @@
     (description
      "This package contains code shared among all sr.ht projects.")
     (license license:bsd-3)))
+
+(define-public python-scm-sr-ht
+  (package
+    (name "python-scm-sr-ht")
+    (version "0.22.23")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://git.sr.ht/~sircmpwn/scm.sr.ht")
+             (commit version)))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32
+         "1p1nlwqfqfb89nky8sp4jh4vjnh8fm2mdx8inziqs3898qw1v7yk"))))
+    (build-system pyproject-build-system)
+    (arguments
+     (list
+      #:tests? #false                   ;there are none
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-after 'unpack 'set-version
+            (lambda _ (setenv "PKGVER" #$version))))))
+    (propagated-inputs
+     (list python-core-sr-ht))
+    (home-page "https://git.sr.ht/~sircmpwn/scm.sr.ht")
+    (synopsis "Shared support code for sr.ht source control services")
+    (description
+     "This package provides shared support code for sr.ht source control
+services.")
+    (license license:agpl3)))
