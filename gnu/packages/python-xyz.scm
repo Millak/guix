ebb54e6a5fba36a571ed239398ee3e0307a91c26
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-07-19 20:28:07 +0200

    gnu: python-nbconvert: Fix build.
    This fixes <https://issues.guix.gnu.org/64729>.
    
    * gnu/packages/python-xyz.scm (python-nbconvert)[propagated-inputs]: Add
    TEXLIVE-ENVIRON, TEXLIVE-PDFCOL, TEXLIVE-PGF, and TEXLIVE-XETEX.

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -15206,12 +15206,16 @@ time.")
            texlive-xcolor
            (texlive-updmap.cfg
             (list texlive-eurosym
+                  texlive-environ
                   texlive-jknapltx
                   texlive-lm
                   texlive-lm-math
                   texlive-mathpazo
+                  texlive-pdfcol
+                  texlive-pgf
                   texlive-rsfs
                   texlive-ucs
+                  texlive-xetex
                   texlive-zapfding))))
     (home-page "https://jupyter.org")
     (synopsis "Converting Jupyter Notebooks")
