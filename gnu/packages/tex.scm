f019ce027c280a7eb286c66a956ed8a5330a831c
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-07-20 11:10:33 +0200

    gnu: Add texlive-collection-plaingeneric.
    * gnu/packages/tex.scm (texlive-collection-plaingeneric): New variable.

--- gnu/packages/tex.scm
+++ gnu/packages/tex.scm
@@ -24839,6 +24839,131 @@ diagrams.  It includes TikZ, @code{pict}, etc.  However, MetaPost and
 PStricks are separate.")
     (license (license:fsf-free "https://www.tug.org/texlive/copying.html"))))
 
+(define-public texlive-collection-plaingeneric
+  (package
+    (name "texlive-collection-plaingeneric")
+    (version (number->string %texlive-revision))
+    (source #f)
+    (build-system trivial-build-system)
+    (arguments (list #:builder #~(mkdir #$output)))
+    (propagated-inputs
+     (list texlive-abbr
+           texlive-abstyles
+           texlive-apnum
+           texlive-autoaligne
+           texlive-barr
+           texlive-bitelist
+           texlive-borceux
+           texlive-c-pascal
+           texlive-catcodes
+           texlive-chronosys
+           texlive-collection-basic
+           texlive-colorsep
+           texlive-compare
+           texlive-crossrefenum
+           texlive-cweb-old
+           texlive-dinat
+           texlive-dirtree
+           texlive-docbytex
+           texlive-dowith
+           texlive-eijkhout
+           texlive-encxvlna
+           texlive-epigram
+           texlive-epsf
+           texlive-epsf-dvipdfmx
+           texlive-expex-acro
+           texlive-expkv-bundle
+           texlive-fenixpar
+           texlive-figflow
+           texlive-fixpdfmag
+           texlive-fltpoint
+           texlive-fntproof
+           texlive-font-change
+           texlive-fontch
+           texlive-fontname
+           texlive-gates
+           texlive-getoptk
+           texlive-gfnotation
+           texlive-gobble
+           texlive-graphics-pln
+           texlive-gtl
+           texlive-hlist
+           texlive-hyplain
+           texlive-inputnormalization
+           texlive-insbox
+           texlive-js-misc
+           texlive-kastrup
+           texlive-lambda-lists
+           texlive-langcode
+           texlive-lecturer
+           texlive-letterspacing
+           texlive-librarian
+           texlive-listofitems
+           texlive-localloc
+           texlive-mathdots
+           texlive-metatex
+           texlive-midnight
+           texlive-mkpattern
+           texlive-modulus
+           texlive-multido
+           texlive-namedef
+           texlive-navigator
+           texlive-newsletr
+           texlive-nth
+           texlive-ofs
+           texlive-olsak-misc
+           texlive-outerhbox
+           texlive-path
+           texlive-pdf-trans
+           texlive-pdfmsym
+           texlive-pitex
+           texlive-placeins-plain
+           texlive-plainpkg
+           texlive-plipsum
+           texlive-plnfss
+           texlive-plstmary
+           texlive-poormanlog
+           texlive-present
+           texlive-pwebmac
+           texlive-random
+           texlive-randomlist
+           texlive-resumemac
+           texlive-ruler
+           texlive-schemata
+           texlive-shade
+           texlive-simplekv
+           texlive-soul
+           texlive-swrule
+           texlive-systeme
+           texlive-tabto-generic
+           texlive-termmenu
+           texlive-tex-ps
+           texlive-tex4ht
+           texlive-texapi
+           texlive-texdate
+           texlive-texdimens
+           texlive-texinfo
+           texlive-timetable
+           texlive-tracklang
+           texlive-transparent-io
+           texlive-treetex
+           texlive-trigonometry
+           texlive-ulem
+           texlive-upca
+           texlive-varisize
+           texlive-xii
+           texlive-xii-lat
+           texlive-xintsession
+           texlive-xlop
+           texlive-yax
+           texlive-zztex))
+    (home-page "https://www.tug.org/texlive/")
+    (synopsis "Plain (La)TeX packages")
+    (description
+     "This consist of add-on packages and macros that work with plain TeX,
+often LaTeX, and occasionally other formats.")
+    (license (license:fsf-free "https://www.tug.org/texlive/copying.html"))))
+
 (define-public texlive-collection-xetex
   (package
     (name "texlive-collection-xetex")
