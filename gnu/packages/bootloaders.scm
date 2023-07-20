Millak/guix

commit ed5dc3a25d858a394bb7db937a51d866c3cdc6ed

gnu: u-boot: Add crust firmware to pinebook, pine64_plus and pine64-lts.
* gnu/packages/bootloaders.scm (make-u-boot-sunxi64-package): Take argument
for System Control Processor (SCP) firmware.
[native-inputs]: Add SCP firmware.
[phases]: Adjust 'set-environment to also set SCP variable.
(u-boot-pine64-plus, u-boot-pine64-lts, u-boot-pinebook): Pass appropriate
crust firmware as the SCP firmware.

Reviewed-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>

gnu/packages/bootloaders.scm
@@ -1010,7 +1010,7 @@ removed so that it fits within common partitioning schemes.")))
 (define-public u-boot-am335x-evm
   (make-u-boot-package "am335x_evm" "arm-linux-gnueabihf"))
 
-(define*-public (make-u-boot-sunxi64-package board triplet
+(define*-public (make-u-boot-sunxi64-package board triplet scp-firmware
                                              #:key defconfig configs)
   (let ((base (make-u-boot-package
                board triplet #:defconfig defconfig #:configs configs)))
@@ -1022,23 +1022,27 @@ removed so that it fits within common partitioning schemes.")))
           #~(modify-phases #$phases
               (add-after 'unpack 'set-environment
                 (lambda* (#:key native-inputs inputs #:allow-other-keys)
-                  ;; Avoid dependency on crust-firmware
-                  ;; https://issues.guix.gnu.org/48371
-                  (setenv "SCP" "/dev/null")
+                  (setenv "SCP" (search-input-file
+                                 native-inputs "libexec/scp.bin"))
                   (setenv "BL31" (search-input-file inputs "bl31.bin"))))))))
+      (native-inputs
+       (modify-inputs (package-native-inputs base)
+         (append scp-firmware)))
       (inputs
        (modify-inputs (package-inputs base)
          (append arm-trusted-firmware-sun50i-a64))))))
 
 (define-public u-boot-pine64-plus
-  (make-u-boot-sunxi64-package "pine64_plus" "aarch64-linux-gnu"))
+  (make-u-boot-sunxi64-package "pine64_plus" "aarch64-linux-gnu"
+                               crust-pine64-plus))
 
 (define-public u-boot-pine64-lts
-  (make-u-boot-sunxi64-package "pine64-lts" "aarch64-linux-gnu"))
+  (make-u-boot-sunxi64-package "pine64-lts" "aarch64-linux-gnu"
+                               crust-pine64-plus))
 
 (define-public u-boot-pinebook
   (make-u-boot-sunxi64-package
-   "pinebook" "aarch64-linux-gnu"
+   "pinebook" "aarch64-linux-gnu" crust-pinebook
    ;; Fix regression with LCD video output introduced in 2020.01
    ;; https://patchwork.ozlabs.org/patch/1225130/
    #:configs '("CONFIG_VIDEO_BPP32=y")))

commit 0ab46ef3f9719f03d9b191a16e5aa91619e95451

gnu: bootloaders: Avoid top-level circular references.
Fixes <https://issues.guix.gnu.org/64745>.

The regression was introduced in
ed5dc3a25d858a394bb7db937a51d866c3cdc6ed: at the top level,
bootloaders.scm and firmware.scm would depend on each other, leading to
unbound variables when loaded, depending on the order in which they got
loaded.

* gnu/packages/bootloaders.scm (make-u-boot-sunxi64-package): Expect
'scp-firmware' to be a promise; force it in 'native-inputs'.
(u-boot-pine64-plus, u-boot-pine64-lts, u-boot-pinebook): Wrap the 3rd
argument to 'make-u-boot-sunxi64-package' into 'delay'.

Reported-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>

gnu/packages/bootloaders.scm
@@ -1,5 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2021 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2013-2019, 2021, 2023 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2015, 2018 Mark H Weaver <mhw@netris.org>
 ;;; Copyright © 2015 Leo Famulari <leo@famulari.name>
 ;;; Copyright © 2016, 2020 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
@@ -1051,22 +1051,22 @@ removed so that it fits within common partitioning schemes.")))
                   (setenv "BL31" (search-input-file inputs "bl31.bin"))))))))
       (native-inputs
        (modify-inputs (package-native-inputs base)
-         (append scp-firmware)))
+         (append (force scp-firmware))))
       (inputs
        (modify-inputs (package-inputs base)
          (append arm-trusted-firmware-sun50i-a64))))))
 
 (define-public u-boot-pine64-plus
   (make-u-boot-sunxi64-package "pine64_plus" "aarch64-linux-gnu"
-                               crust-pine64-plus))
+                               (delay crust-pine64-plus)))
 
 (define-public u-boot-pine64-lts
   (make-u-boot-sunxi64-package "pine64-lts" "aarch64-linux-gnu"
-                               crust-pine64-plus))
+                               (delay crust-pine64-plus)))
 
 (define-public u-boot-pinebook
   (make-u-boot-sunxi64-package
-   "pinebook" "aarch64-linux-gnu" crust-pinebook
+   "pinebook" "aarch64-linux-gnu" (delay crust-pinebook)
    ;; Fix regression with LCD video output introduced in 2020.01
    ;; https://patchwork.ozlabs.org/patch/1225130/
    #:configs '("CONFIG_VIDEO_BPP32=y")))