7bb1bea4d7345cbe2ab9f6ea6d9194b23fedef63
Tobias Geerinckx-Rice <me@tobias.gr>
2023-07-23 02:00:00 +0200

    gnu: delta: Remove broken source URL.
    This does indeed serve infinite redirects, even from the Wayback
    Machine, so there is no value in keeping it here.
    
    * gnu/packages/debug.scm (delta)[source]: Drop tigris.org URL

--- gnu/packages/debug.scm
+++ gnu/packages/debug.scm
@@ -86,10 +86,7 @@
       (method url-fetch)
       (uri (list
             (string-append "mirror://debian/pool/main/d/delta/"
-                           "delta_" version ".orig.tar.gz")
-            ;; This uri seems to send guix download into an infinite loop
-            (string-append "http://delta.tigris.org/files/documents/3103/"
-                           "33566/delta-" version ".tar.gz")))
+                           "delta_" version ".orig.tar.gz")))
       (sha256
        (base32
         "184wh35pf2ddx97319s6sgkzpz48xxkbwzcjpycv009bm53lh61q"))))
