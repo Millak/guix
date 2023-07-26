Millak/guix

commit c4c08775a820868059b59c68d4dfd6be0c9d9010

gnu: guile-2.0: Skip failing tests on the Hurd.
* gnu/packages/guile.scm (guile-2.0)[arguments]: When building natively on the
Hurd, add stage 'disable-threads.tests'...
(guile-2.2): ...and when building natively on the Hurd, delete it again.

gnu/packages/guile.scm
@@ -212,8 +212,13 @@ without requiring the source code to be rewritten.")
                     (rename-file "test-suite/tests/srfi-18.test" "srfi-18.test")
                     ;; failed to remove 't-guild-compile-7215.go.tdL7yC
                     (substitute* "test-suite/standalone/Makefile.in"
-                      (("test-guild-compile ") ""))
-                    #t)))
+                      (("test-guild-compile ") "")))))
+              '())
+        ,@(if (system-hurd?)
+              '((add-after 'unpack 'disable-threads.tests
+                  (lambda _
+                    ;; Many tests hang, esp. (join-thread ..), also others.
+                    (rename-file "test-suite/tests/threads.test" "threads.test"))))
               '())
         (add-before 'configure 'pre-configure
           (lambda* (#:key inputs #:allow-other-keys)
@@ -286,7 +291,12 @@ without requiring the source code to be rewritten.")
         (if (target-x86-32?)            ;<https://issues.guix.gnu.org/49368>
             `(append '("--disable-static")
                  '("CFLAGS=-g -O2 -fexcess-precision=standard"))
-            flags))))
+            flags))
+       ((#:phases phases '%standard-phases)
+        #~(modify-phases #$phases
+            #$@(if (system-hurd?)
+                   #~((delete 'disable-threads.tests))
+                   '())))))
 
     (properties '((timeout . 72000)               ;20 hours
                   (max-silent-time . 36000)))     ;10 hours (needed on ARM

commit d979197eaf10883e02c53b87f8e0d14bf254c525

gnu: Fix guile-for-guile-emacs builder.
I think this broke in c4c08775a820868059b59c68d4dfd6be0c9d9010.

* gnu/packages/guile.scm (guile-for-guile-emacs)[arguments]: Switch to gexp
for phases.

gnu/packages/guile.scm
@@ -603,8 +603,8 @@ GNU@tie{}Guile.  Use the @code{(ice-9 readline)} module and call its
                   "1l7ik4q4zk7vq4m3gnwizc0b64b1mdr31hxqlzxs94xaf2lvi7s2"))))
       (arguments
        (substitute-keyword-arguments (package-arguments guile-2.2)
-         ((#:phases phases '%standard-phases)
-          `(modify-phases ,phases
+         ((#:phases phases)
+          #~(modify-phases #$phases
              (replace 'bootstrap
                (lambda _
                  ;; Disable broken tests.
@@ -617,8 +617,7 @@ GNU@tie{}Guile.  Use the @code{(ice-9 readline)} module and call its
                     (string-append "#;" m)))
 
                  (patch-shebang "build-aux/git-version-gen")
-                 (invoke "sh" "autogen.sh")
-                 #t))))))
+                 (invoke "sh" "autogen.sh")))))))
       (native-inputs
        (modify-inputs (package-native-inputs guile-2.2)
          (prepend autoconf