664e227b6769f56a9b9700016a7ab8e03f626bc5
Hilton Chain <hako@ultrarare.space>
2023-07-29 01:20:06 +0800

    gnu: go-github-com-spf13-cobra: Update to 1.8.0.
    * gnu/packages/golang.scm (go-github-com-spf13-cobra): Update to 1.8.0.
    [propagated-inputs]: Remove labels.
    
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>
    Change-Id: I3eff757674ff9eafa1bd1c32bfb12616faa080c6

--- gnu/packages/golang.scm
+++ gnu/packages/golang.scm
@@ -3835,7 +3835,7 @@ containers.")
 (define-public go-github-com-spf13-cobra
   (package
     (name "go-github-com-spf13-cobra")
-    (version "1.5.0")
+    (version "1.8.0")
     (source
      (origin
        (method git-fetch)
@@ -3845,12 +3845,12 @@ containers.")
        (file-name (git-file-name name version))
        (sha256
         (base32
-         "0qavvyljh509w5xb46lfkak670szp6v4jswwar1hx13cpid8gk5d"))))
+         "0mhnqfgcwwcknlhk7n07i02q3iqq6ihksj4dwz296zci8ry3w0d0"))))
     (build-system go-build-system)
     (arguments
      `(#:import-path "github.com/spf13/cobra"))
     (propagated-inputs
-     `(("github.com/spf13/pflag" ,go-github-com-spf13-pflag)))
+     (list go-github-com-spf13-pflag))
     (home-page "https://github.com/spf13/cobra")
     (synopsis "Go library for creating CLI applications")
     (description "Cobra is both a library for creating powerful modern CLI
