c3900978bc6087d707a8ea12b3bceb507e3a4a28
宋文武 <iyzsong@member.fsf.org>
2023-07-29 10:53:20 +0800

    gnu: bovo: Update to 23.04.3.
    * gnu/packages/kde-games.scm (bovo): Update to 23.04.3.

--- gnu/packages/kde-games.scm
+++ gnu/packages/kde-games.scm
@@ -1254,14 +1254,14 @@ This package is part of the KDE games module.")
 (define-public bovo
   (package
     (name "bovo")
-    (version "22.04.3")
+    (version "23.04.3")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://kde/stable/release-service/" version
                            "/src/bovo-" version ".tar.xz"))
        (sha256
-        (base32 "1yz4x7fmllmp57yxfipqkldgf38haldah81d2qmc5jlahvy3j23p"))))
+        (base32 "1zds6d4rmcf4sa2sfhyp1i5n7s7cgslikvbra4pz2kpi0hmvihi1"))))
     (build-system qt-build-system)
     (native-inputs
      (list extra-cmake-modules kdoctools))
