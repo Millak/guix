f195655ebc4849ac479646dd4547d416a174aded
宋文武 <iyzsong@member.fsf.org>
2023-07-29 10:53:19 +0800

    gnu: ktuberling: Update to 23.04.3.
    * gnu/packages/kde-games.scm (ktuberling): Update to 23.04.3.

--- gnu/packages/kde-games.scm
+++ gnu/packages/kde-games.scm
@@ -42,14 +42,14 @@
 (define-public ktuberling
   (package
     (name "ktuberling")
-    (version "22.04.3")
+    (version "23.04.3")
     (source
      (origin
       (method url-fetch)
       (uri (string-append "mirror://kde/stable/release-service/" version
                           "/src/ktuberling-" version ".tar.xz"))
       (sha256
-       (base32 "19kyk7r7lkzwcvhhn1jjgrcdkq8gfmzgyrvvcbm4qkx7jd27v1ia"))))
+       (base32 "0zk7wijp425657s2j7xq7mhybgyddpam88ih8i0sbpcs0sliyz7k"))))
     (build-system qt-build-system)
     (native-inputs
      (list extra-cmake-modules kdoctools perl))
