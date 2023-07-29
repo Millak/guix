3a975a52d47420ff71b2bba6e7c00f91a93c9ccf
宋文武 <iyzsong@member.fsf.org>
2023-07-29 10:53:20 +0800

    gnu: kspaceduel: Update to 23.04.3.
    * gnu/packages/kde-games.scm (kspaceduel): Update to 23.04.3.

--- gnu/packages/kde-games.scm
+++ gnu/packages/kde-games.scm
@@ -1219,14 +1219,14 @@ This package is part of the KDE games module.")
 (define-public kspaceduel
   (package
     (name "kspaceduel")
-    (version "22.04.3")
+    (version "23.04.3")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://kde/stable/release-service/" version
                            "/src/kspaceduel-" version ".tar.xz"))
        (sha256
-        (base32 "1yb7y1f5rcw4jacpf9vpzbragm2ywwb3mkjhq67x7x99q6r9iz55"))))
+        (base32 "1mpry73vvi332wxyw2wymh3lsrj2amblg50dym9ni1lj2af657ia"))))
     (build-system qt-build-system)
     (native-inputs
      (list extra-cmake-modules kdoctools))
