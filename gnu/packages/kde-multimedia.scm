3837d2bb64ff9848d564de5ebed1aa6406841bc3
宋文武 <iyzsong@member.fsf.org>
2023-07-31 18:31:00 +0800

    gnu: ffmpegthumbs: Update to 23.04.3.
    * gnu/packages/kde-multimedia.scm (ffmpegthumbs): Update to 23.04.3.

--- gnu/packages/kde-multimedia.scm
+++ gnu/packages/kde-multimedia.scm
@@ -228,14 +228,14 @@ its own database.  You can build and play your own playlist.")
 (define-public ffmpegthumbs
   (package
     (name "ffmpegthumbs")
-    (version "22.04.3")
+    (version "23.04.3")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://kde/stable/release-service/" version
                            "/src/ffmpegthumbs-" version ".tar.xz"))
        (sha256
-        (base32 "1jl7l3n2lfg71kbzwjn5nrm3qlgilhnvs67iil162mnv52gf3c0v"))))
+        (base32 "02wvhjqqird55xcs4kkj0s7nam4vqh3khjz5f1mjqfnk62sj1plx"))))
     (build-system qt-build-system)
     (native-inputs
      (list extra-cmake-modules pkg-config))
