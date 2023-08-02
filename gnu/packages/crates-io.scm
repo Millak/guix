f5b11523d65adf21a01d0ca0b68298159a6cc954
Efraim Flashner <efraim@flashner.co.il>
2023-08-02 20:14:20 +0300

    gnu: Add rust-async-recursion-1.
    * gnu/packages/crates-io.scm (rust-async-recursion-1): New variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -5244,6 +5244,32 @@ implementing async state machines that can later be wrapped in dedicated
 futures.")
     (license (list license:expat license:asl2.0))))
 
+(define-public rust-async-recursion-1
+  (package
+    (name "rust-async-recursion")
+    (version "1.0.4")
+    (source (origin
+              (method url-fetch)
+              (uri (crate-uri "async-recursion" version))
+              (file-name (string-append name "-" version ".tar.gz"))
+              (sha256
+               (base32
+                "1fhwz7jqgsakbjsr2nrsvgs245l1m5dkzir6f9fxw4ngwrywx5qf"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:tests? #f      ; TODO: Tests unexpectedly pass.
+       #:cargo-inputs
+       (("rust-proc-macro2" ,rust-proc-macro2-1)
+        ("rust-quote" ,rust-quote-1)
+        ("rust-syn" ,rust-syn-2))
+       #:cargo-development-inputs
+       (("rust-futures-executor" ,rust-futures-executor-0.3)
+        ("rust-trybuild" ,rust-trybuild-1))))
+    (home-page "https://github.com/dcchut/async-recursion")
+    (synopsis "Recursion for async functions")
+    (description "This package provides recursion for async functions in Rust.")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-async-std-1
   (package
     (name "rust-async-std")
