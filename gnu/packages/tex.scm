ece6f5ef43487feddf2cfa44d4b58304f29cafb9
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-08-04 12:13:24 +0200

    gnu: Add texlive-knuth-pdf.
    * gnu/packages/tex.scm (texlive-knuth-pdf): New variable.

--- gnu/packages/tex.scm
+++ gnu/packages/tex.scm
@@ -4549,6 +4549,34 @@ application.  The @code{knuth-hint} package is a showcase of HiTeX's
 capabilities.")
     (license (list license:public-domain license:knuth))))
 
+(define-public texlive-knuth-pdf
+  (package
+    (name "texlive-knuth-pdf")
+    (version (number->string %texlive-revision))
+    (source (texlive-origin
+             name version
+             (list "doc/generic/knuth-pdf/")
+             (base32
+              "14by59b7d1fi4jh5d0xvzb56g9pgd5zi1sdvfgi50vrpn28gfqfw")))
+    (outputs '("out" "doc"))
+    (build-system texlive-build-system)
+    (home-page "https://ctan.org/pkg/knuth-pdf")
+    (synopsis "PDF collection of typeset C/WEB sources in TeX Live")
+    (description
+     "Here you find a large collection of PDF documents for many C/WEB
+programs in TeX Live, both in their original form as written by their
+respective authors, and in the changed form as they are actually used in the
+TeX Live system.  Care has been taken to keep the section numbering intact, so
+that you can study the sources and their changes in parallel.
+
+Also included is the collection of errata for Donald Knuth's @emph{Computers &
+Typesetting series}.  Although not all the texts here are written or
+maintained by Donald Knuth, it is more convenient for everything to be
+collected in one place for reading and searching.  They all stem from the
+system that Knuth created.  The central entry point is the index file, with
+links to the individual documents, either in HTML or in PDF format.")
+    (license license:public-domain)))
+
 (define-public texlive-kvmap
   (package
     (name "texlive-kvmap")
