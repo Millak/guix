1c30d5a6bfc5d48137f4bdcc271189a06fdc6ed3
Ludovic Courtès <ludo@gnu.org>
2023-08-06 18:59:15 +0200

    home: services: mcron: Define as a mapping of the system service.
    * gnu/services/mcron.scm (list-of-gexps?): Remove.
    (<mcron-configuration>): Rewrite using 'define-record-type*'.
    [home-service?]: New field.
    [log-file]: Make thunked and changed default value.
    (mcron-shepherd-services): Honor 'home-service?' and remove use of
    'maybe-value-set?'.
    (mcron-service-type): Inherit 'home-service?' from CONFIG.
    (generate-doc): Remove.
    * gnu/home/services/mcron.scm (list-of-gexp?)
    (<home-mcron-configuration>, job-files, shepherd-schedule-action)
    (home-mcron-shepherd-services, home-mcron-profile)
    (home-mcron-extend, generate-doc): Remove.
    (home-mcron-configuration): Turn into a macro.
    (home-mcron-service-type): Define in terms of
    'system->home-service-type'.
    <top level>: Add service type mapping.

--- gnu/home/services/mcron.scm
+++ gnu/home/services/mcron.scm
@@ -2,6 +2,7 @@
 ;;; Copyright © 2021, 2023 Andrew Tropin <andrew@trop.in>
 ;;; Copyright © 2021 Xinglu Chen <public@yoctocell.xyz>
 ;;; Copyright © 2022 Maxim Cournoyer <maxim.cournoyer@gmail.com>
+;;; Copyright © 2023 Ludovic Courtès <ludo@gnu.org>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -19,16 +20,9 @@
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (gnu home services mcron)
-  #:use-module (gnu packages guile-xyz)
   #:use-module (gnu home services)
-  #:use-module (gnu services configuration)
-  #:use-module (gnu services shepherd)
   #:use-module (gnu home services shepherd)
-  #:use-module (guix records)
-  #:use-module (guix gexp)
-  #:use-module (srfi srfi-1)
-  #:use-module (ice-9 match)
-
+  #:use-module (gnu services mcron)               ;for the service mapping
   #:export (home-mcron-configuration
             home-mcron-service-type))
 
@@ -55,86 +49,16 @@
 ;;
 ;;; Code:
 
-(define list-of-gexps?
-  (list-of gexp?))
-
-(define-configuration/no-serialization home-mcron-configuration
-  (mcron (file-like mcron) "The mcron package to use.")
-  (jobs
-   (list-of-gexps '())
-   "This is a list of gexps (@pxref{G-Expressions}), where each gexp
-corresponds to an mcron job specification (@pxref{Syntax, mcron job
-specifications,, mcron, GNU@tie{}mcron}).")
-  (log? (boolean #t) "Log messages to standard output.")
-  (log-format
-   (string "~1@*~a ~a: ~a~%")
-   "@code{(ice-9 format)} format string for log messages.  The default value
-produces messages like \"@samp{@var{pid} @var{name}:
-@var{message}\"} (@pxref{Invoking mcron, Invoking,, mcron, GNU@tie{}mcron}).
-Each message is also prefixed by a timestamp by GNU Shepherd."))
-
-(define job-files (@@ (gnu services mcron) job-files))
-(define shepherd-schedule-action
-  (@@ (gnu services mcron) shepherd-schedule-action))
-
-(define (home-mcron-shepherd-services config)
-  (match-record config <home-mcron-configuration>
-    (mcron jobs log? log-format)
-    (if (null? jobs)
-        '()                                       ;no jobs to run
-        (let ((files (job-files mcron jobs)))
-          (list (shepherd-service
-                 (documentation "User cron jobs.")
-                 (provision '(mcron))
-                 (modules `((srfi srfi-1)
-                            (srfi srfi-26)
-                            (ice-9 popen)         ;for the 'schedule' action
-                            (ice-9 rdelim)
-                            (ice-9 match)
-                            ,@%default-modules))
-                 (start #~(make-forkexec-constructor
-                           (list (string-append #$mcron "/bin/mcron")
-                                 #$@(if log?
-                                        #~("--log" "--log-format" #$log-format)
-                                        #~())
-                                 #$@files)
-                           #:log-file (string-append
-                                       (or (getenv "XDG_STATE_HOME")
-                                           (format #f "~a/.local/state"
-                                                   (getenv "HOME")))
-                                       "/log/mcron.log")))
-                 (stop #~(make-kill-destructor))
-                 (actions
-                  (list (shepherd-schedule-action mcron files)))))))))
-
-(define home-mcron-profile (compose list home-mcron-configuration-mcron))
-
-(define (home-mcron-extend config jobs)
-  (home-mcron-configuration
-   (inherit config)
-   (jobs (append (home-mcron-configuration-jobs config)
-                 jobs))))
+(define-syntax-rule (home-mcron-configuration fields ...)
+  ;; Macro provided for backward compatibility.
+  (for-home (mcron-configuration fields ...)))
 
 (define home-mcron-service-type
-  (service-type (name 'home-mcron)
-                (extensions
-                 (list (service-extension
-                        home-shepherd-service-type
-                        home-mcron-shepherd-services)
-                       (service-extension
-                        home-profile-service-type
-                        home-mcron-profile)))
-                (compose concatenate)
-                (extend home-mcron-extend)
-                (default-value (home-mcron-configuration))
-                (description
-                 "Install and configure the GNU mcron cron job manager.")))
+  (service-type
+   (inherit (system->home-service-type mcron-service-type))
+   (default-value (for-home (mcron-configuration)))))
 
-
-;;;
-;;; Generate documentation.
-;;;
-(define (generate-doc)
-  (configuration->documentation 'home-mcron-configuration))
+(define-service-type-mapping
+  mcron-service-type => home-mcron-service-type)
 
 ;;; mcron.scm ends here
