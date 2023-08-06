72e2bbebf5f93467568dae2d9000022204dc941e
Tobias Geerinckx-Rice <me@tobias.gr>
2023-08-06 02:00:00 +0200

    gnu: openssh: Update to 9.4p1.
    * gnu/packages/ssh.scm (openssh): Update to 9.4p1.
    [source]: Remove upstreamed Hurd patch.
    * gnu/packages/patches/openssh-hurd.patch: Delete file.
    * gnu/local.mk (dist_patch_DATA): Remove it.

--- gnu/local.mk
+++ gnu/local.mk
@@ -1684,7 +1684,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/openjdk-15-xcursor-no-dynamic.patch	\
   %D%/packages/patches/openmpi-mtl-priorities.patch		\
   %D%/packages/patches/openmw-assume-nonconst-SIGSTKSZ.patch    \
-  %D%/packages/patches/openssh-hurd.patch			\
   %D%/packages/patches/openssh-trust-guix-store-directory.patch	\
   %D%/packages/patches/openresolv-restartcmd-guix.patch	\
   %D%/packages/patches/openrgb-unbundle-hueplusplus.patch	\
