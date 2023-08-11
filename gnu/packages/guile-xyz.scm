e28e76fc4ab2727a1bacbcfdead15298ea7d76fe
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2023-08-11 23:16:32 +0300

    gnu: guile-smc: Update to 0.6.2.
    * gnu/packages/guile-xyz.scm (guile-smc): Update to 0.6.2.
    
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/packages/guile-xyz.scm
+++ gnu/packages/guile-xyz.scm
@@ -5345,7 +5345,7 @@ GitLab instance.")
 (define-public guile-smc
   (package
     (name "guile-smc")
-    (version "0.6.1")
+    (version "0.6.2")
     (source
      (origin
        (method git-fetch)
@@ -5355,7 +5355,7 @@ GitLab instance.")
        (file-name (string-append name "-" version))
        (sha256
         (base32
-         "02729gkywzimmdlmnwzclvs3wl92f9mvp5dmcbp75kmwibxly288"))))
+         "11083lj048ab5zsdgwpkshxi8v5nfdr7kvmmslszbi7lq2pwfqig"))))
     (build-system gnu-build-system)
     (arguments
      `(#:make-flags '("GUILE_AUTO_COMPILE=0")     ;to prevent guild warnings
