2387c60815b54c7b007ed7e4bc3caecf420aa526
Andre A. Gomes <andremegafone@gmail.com>
2023-08-14 22:47:57 +0300

    gnu: nyxt: Update to 3.6.0.
    * gnu/packages/web-browsers.scm (nyxt): Update to 3.6.0.
    
    Signed-off-by: Guillaume Le Vaillant <glv@posteo.net>

--- gnu/packages/web-browsers.scm
+++ gnu/packages/web-browsers.scm
@@ -586,7 +586,7 @@ driven and does not detract you from your daily work.")
 (define-public nyxt
   (package
     (name "nyxt")
-    (version "3.5.0")
+    (version "3.6.0")
     (source
      (origin
        (method git-fetch)
@@ -595,7 +595,7 @@ driven and does not detract you from your daily work.")
              (commit version)))
        (sha256
         (base32
-         "13ldi191ccxyxr3hjxyhnjl2vw365v0fhb1pqia7rg1gl3id47gz"))
+         "1difs5hw4avzfn2f33gi0w8nz9ja9fvwsl5m9f8izr8zsqlw98qd"))
        (file-name (git-file-name "nyxt" version))
        (modules '((guix build utils)))
        (snippet
@@ -656,6 +656,7 @@ driven and does not detract you from your daily work.")
                   sbcl-cl-ppcre
                   sbcl-cl-prevalence
                   sbcl-cl-qrencode
+                  sbcl-cl-sqlite
                   sbcl-cl-str
                   sbcl-cl-tld
                   sbcl-closer-mop
