603db2ad71deef6c88e71ec53e5626c1d965fe46
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-08-15 20:16:48 +0200

    gnu: Add texlive-collection-langchinese.
    * gnu/packages/tex.scm (texlive-collection-langchinese): New variable.

--- gnu/packages/tex.scm
+++ gnu/packages/tex.scm
@@ -41675,6 +41675,54 @@ HiTeX engine and related.")
     (description "This collection provides support for Arabic and Persian.")
     (license (license:fsf-free "https://www.tug.org/texlive/copying.html"))))
 
+(define-public texlive-collection-langchinese
+  (package
+    (name "texlive-collection-langchinese")
+    (version (number->string %texlive-revision))
+    (source #f)
+    (build-system trivial-build-system)
+    (arguments (list #:builder #~(mkdir #$output)))
+    (propagated-inputs
+     (list texlive-arphic
+           texlive-arphic-ttf
+           texlive-asymptote-by-example-zh-cn
+           texlive-asymptote-faq-zh-cn
+           texlive-asymptote-manual-zh-cn
+           texlive-cns
+           texlive-collection-langcjk
+           texlive-ctex
+           texlive-ctex-faq
+           texlive-exam-zh
+           texlive-fandol
+           texlive-fduthesis
+           texlive-hanzibox
+           texlive-hyphen-complete
+           texlive-impatient-cn
+           texlive-install-latex-guide-zh-cn
+           texlive-latex-notes-zh-cn
+           texlive-lshort-chinese
+           texlive-nanicolle
+           texlive-njurepo
+           texlive-pgfornament-han
+           texlive-qyxf-book
+           texlive-texproposal
+           texlive-tlmgr-intro-zh-cn
+           texlive-upzhkinsoku
+           texlive-xpinyin
+           texlive-xtuthesis
+           texlive-zhlineskip
+           texlive-zhlipsum
+           texlive-zhmetrics
+           texlive-zhmetrics-uptex
+           texlive-zhnumber
+           texlive-zhspacing))
+    (home-page "https://www.tug.org/texlive/")
+    (synopsis "Support for Chinese")
+    (description
+     "This collection provides support for Chinese; additional packages are
+located in @code{collection-langcjk}.")
+    (license (license:fsf-free "https://www.tug.org/texlive/copying.html"))))
+
 (define-public texlive-collection-langcjk
   (package
     (name "texlive-collection-langcjk")
