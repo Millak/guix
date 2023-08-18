1fcf9c0d7de956e7029090bdcc5b921932e95082
Liliana Marie Prikler <liliana.prikler@gmail.com>
2023-08-19 01:05:22 +0200

    gnu: emacs-helpful: Update to 0.21.
    * gnu/packages/patches/emacs-helpful-fix-tests.patch: New file.
    * gnu/local.mk (dist_patch_DATA): Register it here.
    * gnu/packages/emacs-xyz.scm (emacs-helpful): Update to 0.21.
    [source]<patches>: Use it here.

--- gnu/local.mk
+++ gnu/local.mk
@@ -1097,6 +1097,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/emacs-fix-scheme-indent-function.patch	\
   %D%/packages/patches/emacs-json-reformat-fix-tests.patch	\
   %D%/packages/patches/emacs-haskell-mode-no-redefine-builtin.patch	\
+  %D%/packages/patches/emacs-helpful-fix-tests.patch	\
   %D%/packages/patches/emacs-highlight-stages-add-gexp.patch	\
   %D%/packages/patches/emacs-lispy-fix-thread-last-test.patch   \
   %D%/packages/patches/emacs-native-comp-driver-options.patch   \
