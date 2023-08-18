5a989fc417de48b64cb2ee6a5228c3af391acc25
Liliana Marie Prikler <liliana.prikler@gmail.com>
2023-08-19 00:52:53 +0200

    gnu: emacs-haskell-mode: Update to 17.4.
    * gnu/packages/patches/emacs-haskell-mode-no-redefine-builtin.patch: New file.
    * gnu/local.mk (dist_patch_DATA): Register it here.
    * gnu/packages/emacs-xyz.scm (emacs-haskell-mode): Update to 17.4.
    [source]<patches>: Use it here.

--- gnu/local.mk
+++ gnu/local.mk
@@ -1096,6 +1096,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/emacs-git-email-missing-parens.patch	\
   %D%/packages/patches/emacs-fix-scheme-indent-function.patch	\
   %D%/packages/patches/emacs-json-reformat-fix-tests.patch	\
+  %D%/packages/patches/emacs-haskell-mode-no-redefine-builtin.patch	\
   %D%/packages/patches/emacs-highlight-stages-add-gexp.patch	\
   %D%/packages/patches/emacs-lispy-fix-thread-last-test.patch   \
   %D%/packages/patches/emacs-native-comp-driver-options.patch   \
