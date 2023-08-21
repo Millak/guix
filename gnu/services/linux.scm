Millak/guix

commit 9c34b793c10cdb50235b876dea5be700ab5600dc

services: Add missing 'user-processes' requirements.
* gnu/services/guix.scm (guix-build-coordinator-agent-shepherd-services):
Add 'user-processes' requirement.
* gnu/services/linux.scm (earlyoom-shepherd-service): Likewise.

gnu/services/linux.scm
@@ -173,7 +173,7 @@ representation."
 (define (earlyoom-shepherd-service config)
   (shepherd-service
    (documentation "Run the Early OOM daemon.")
-   (provision '(earlyoom))
+   (provision '(earlyoom user-processes))
    (start #~(make-forkexec-constructor
              '#$(earlyoom-configuration->command-line-args config)
              #:log-file "/var/log/earlyoom.log"))

gnu/services/guix.scm
@@ -440,7 +440,7 @@
      (shepherd-service
       (documentation "Guix Build Coordinator Agent")
       (provision '(guix-build-coordinator-agent))
-      (requirement '(networking))
+      (requirement '(networking user-processes))
       (start
        #~(lambda _
            (parameterize ((%current-logfile-date-format ""))

commit 69d8d0a71964cc2f382da91090d61668189579e8

services: earlyoom: Move 'user-processes' to 'requirements'.
Fixes a regression introduced in
9c34b793c10cdb50235b876dea5be700ab5600dc.

* gnu/services/linux.scm (earlyoom-shepherd-service): Move
'user-processes' to 'requirements'.

Reported-by: Attila Lendvai <attila@lendvai.name>

gnu/services/linux.scm
@@ -173,7 +173,8 @@ representation."
 (define (earlyoom-shepherd-service config)
   (shepherd-service
    (documentation "Run the Early OOM daemon.")
-   (provision '(earlyoom user-processes))
+   (provision '(earlyoom))
+   (requirement '(user-processes))
    (start #~(make-forkexec-constructor
              '#$(earlyoom-configuration->command-line-args config)
              #:log-file "/var/log/earlyoom.log"))