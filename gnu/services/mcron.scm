Millak/guix

commit 552d0703776c532f25498d5cb852c3c497cb9252

services: mcron: Add module for %user-log-dir.
* gnu/services/mcron.scm (mcron-shepherd-services)[modules]: Add
(shepherd support) for home service.

gnu/services/mcron.scm
@@ -165,6 +165,9 @@ files."
                             (ice-9 popen) ;for the 'schedule' action
                             (ice-9 rdelim)
                             (ice-9 match)
+                            ,(if home-service?
+                                 '((shepherd support) #:select (%user-log-dir))
+                                 '())
                             ,@%default-modules))
                  (start #~(make-forkexec-constructor
                            (list #$(file-append mcron "/bin/mcron")

commit 0c4fb8f43f8aba2dcabe1bc97a8f6d8d41057704

services: mcron: Use (shepherd support) module unconditionally.
This fixes a issue introduced in the previous commit 552d0703776c (services:
mcron: Add module for %user-log-dir.)  which made the expression invalid when
using a '() module for the system service.

* gnu/services/mcron.scm (mcron-shepherd-services)[modules]: Use
(shepherd support) unconditionally.

gnu/services/mcron.scm
@@ -165,9 +165,7 @@ files."
                             (ice-9 popen) ;for the 'schedule' action
                             (ice-9 rdelim)
                             (ice-9 match)
-                            ,(if home-service?
-                                 '((shepherd support) #:select (%user-log-dir))
-                                 '())
+                            ((shepherd support) #:select (%user-log-dir))
                             ,@%default-modules))
                  (start #~(make-forkexec-constructor
                            (list #$(file-append mcron "/bin/mcron")