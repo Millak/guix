Millak/guix

commit 4cdbbfa48d87d9a500288249f228fd19d4371919

gnu: guix: Update to 0e6215a.
* gnu/packages/package-management.scm (guix): Update to 0e6215a.

gnu/packages/package-management.scm
@@ -174,8 +174,8 @@
   ;; Note: the 'update-guix-package.scm' script expects this definition to
   ;; start precisely like this.
   (let ((version "1.4.0")
-        (commit "44bbfc24e4bcc48d0e3343cd3d83452721af8c36")
-        (revision 7))
+        (commit "0e6215ac72cf642b0bafc4e1ea3120535bd7245a")
+        (revision 8))
     (package
       (name "guix")
 
@@ -191,7 +191,7 @@
                       (commit commit)))
                 (sha256
                  (base32
-                  "08gq04pphapr3i0gzdilp8l87rpxlh2p768qrn1fw92fmw727xf7"))
+                  "1c6bmv03vgng4g56xx7jpg0q0frfsvy9kv84qr04a1sk24s7zbxb"))
                 (file-name (string-append "guix-" version "-checkout"))))
       (build-system gnu-build-system)
       (arguments

commit 91bef86aa2bd7d94c60fc06f7527216b625b857a

gnu: guix: Update to 30355c1.
Fixes a test failure in 'tests/guix-system.sh' on aarch64-linux,
introduced by 4cdbbfa48d87d9a500288249f228fd19d4371919.

* gnu/packages/package-management.scm (guix): Update to 30355c1.

gnu/packages/package-management.scm
@@ -174,8 +174,8 @@
   ;; Note: the 'update-guix-package.scm' script expects this definition to
   ;; start precisely like this.
   (let ((version "1.4.0")
-        (commit "0e6215ac72cf642b0bafc4e1ea3120535bd7245a")
-        (revision 8))
+        (commit "30355c17d7b7eaeb16976c1432d660cb0783014a")
+        (revision 9))
     (package
       (name "guix")
 
@@ -191,7 +191,7 @@
                       (commit commit)))
                 (sha256
                  (base32
-                  "1c6bmv03vgng4g56xx7jpg0q0frfsvy9kv84qr04a1sk24s7zbxb"))
+                  "17fxwy3ssm6h38wapll7gvgc1c9gxp2ycz8s4v8k02ll1i5i1fc8"))
                 (file-name (string-append "guix-" version "-checkout"))))
       (build-system gnu-build-system)
       (arguments