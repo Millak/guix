71a1728332adc133d5f3449dadb2e8bfbf5e0a47
Leo Famulari <leo@famulari.name>
2023-08-27 17:56:29 -0400

    gnu: linux-libre 6.1: Update to 6.1.49.
    * gnu/packages/linux.scm (linux-libre-6.1-version): Update to 6.1.49.
    (linux-libre-6.1-pristine-source): Update hash.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -506,7 +506,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
 ;; The "longterm" kernels — the older releases with long-term upstream support.
 ;; Here are the support timelines:
 ;; <https://www.kernel.org/category/releases.html>
-(define-public linux-libre-6.1-version "6.1.47")
+(define-public linux-libre-6.1-version "6.1.49")
 (define-public linux-libre-6.1-gnu-revision "gnu")
 (define deblob-scripts-6.1
   (linux-libre-deblob-scripts
@@ -516,7 +516,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
    (base32 "1c73516nbhnz0cxjz38b5794dxygb8sznv9idiibw7ablmjbhd11")))
 (define-public linux-libre-6.1-pristine-source
   (let ((version linux-libre-6.1-version)
-        (hash (base32 "1azwvlzyp1s2adm17ic0jfmv3ph70wqzycb8s96z9987y1m8pmck")))
+        (hash (base32 "03vs0ncpxx12d2pm0glxa68lqkj17j69lcx9h8w6xjm43hii9sn9")))
    (make-linux-libre-source version
                             (%upstream-linux-source version hash)
                             deblob-scripts-6.1)))
