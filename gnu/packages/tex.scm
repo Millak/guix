f40ac7d95e0bd923a8fbfbe105da757f3a5caaf5
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-08-29 15:58:16 +0200

    gnu: Add texlive-phfextendedabstract.
    * gnu/packages/tex.scm (texlive-phfextendedabstract): New variable.

--- gnu/packages/tex.scm
+++ gnu/packages/tex.scm
@@ -92149,6 +92149,34 @@ automatically to each commenter by default, and the appearance of all comments
 is highly customizable.")
     (license license:lppl1.3+)))
 
+(define-public texlive-phfextendedabstract
+  (package
+    (name "texlive-phfextendedabstract")
+    (version (number->string %texlive-revision))
+    (source (texlive-origin
+             name version
+             (list "doc/latex/phfextendedabstract/"
+                   "source/latex/phfextendedabstract/"
+                   "tex/latex/phfextendedabstract/")
+             (base32
+              "1a9jrsmlafi2q67bqzzwmdxiivqnrzscz9nb99is08rkwnfwmj97")))
+    (outputs '("out" "doc"))
+    (build-system texlive-build-system)
+    (home-page "https://ctan.org/pkg/phfextendedabstract")
+    (synopsis "Typeset extended abstracts for conferences")
+    (description
+     "Several conferences in various fields require the submission of extended
+abstracts.  An extended abstract is a summary of a scientific result,
+presented at a high level, and consisting of at most a small handful of pages.
+The @code{phfextendedabstract} LaTeX class provides a simple style for such
+abstracts.  There are only two sectioning levels, sections and paragraphs, and
+the style is optimized to save space as well as to guide the reader's eye
+through the overall structure of the document.  An option will try to compress
+all vertical space to save some space, in case you need to satisfy page
+constraints.  The style builds upon the powerful RevTeX class, so you can use
+all of RevTeX's features such as author affiliations, etc.")
+    (license license:lppl1.3+)))
+
 ;;;
 ;;; Avoid adding new packages to the end of this file. To reduce the chances
 ;;; of a merge conflict, place them above by existing packages with similar
