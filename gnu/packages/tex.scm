814acac8de53f758e099b0a6992cbf0175471f1b
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-08-29 15:59:41 +0200

    gnu: Add texlive-regstats.
    * gnu/packages/tex.scm (texlive-regstats): New variable.

--- gnu/packages/tex.scm
+++ gnu/packages/tex.scm
@@ -93880,6 +93880,29 @@ provided, to convert the register specifications into structures suitable for,
 say, a pre-silicon test environment.")
     (license license:lppl1.3+)))
 
+(define-public texlive-regstats
+  (package
+    (name "texlive-regstats")
+    (version (number->string %texlive-revision))
+    (source (texlive-origin
+             name version
+             (list "doc/latex/regstats/"
+                   "source/latex/regstats/"
+                   "tex/latex/regstats/")
+             (base32
+              "1a7jzqs8iv9pmami1pihn1dp3a4dz69ywzgz20zppxsdlg4m01fx")))
+    (outputs '("out" "doc"))
+    (build-system texlive-build-system)
+    (home-page "https://ctan.org/pkg/regstats")
+    (synopsis "Information about register use")
+    (description
+     "The package will report number of used registers (@code{counter},
+@code{dimen}, @code{skip}, @code{muskip}, @code{box}, @code{token},
+@code{input}, @code{output}, @code{math families}, @code{languages},
+@code{insertions}), and will compare the number to the maximum available
+number of such registers.")
+    (license license:lppl1.3+)))
+
 ;;;
 ;;; Avoid adding new packages to the end of this file. To reduce the chances
 ;;; of a merge conflict, place them above by existing packages with similar
