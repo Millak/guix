db9372c7bc49b203aed0e07869fa1b1891fa2f16
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-08-29 15:46:36 +0200

    gnu: Add texlive-conditext.
    * gnu/packages/tex.scm (texlive-conditext): New variable.

--- gnu/packages/tex.scm
+++ gnu/packages/tex.scm
@@ -76920,6 +76920,42 @@ throughout the document.")
 (classical) concert programme; a sample is provided.")
     (license license:gpl3+)))
 
+(define-public texlive-conditext
+  (package
+    (name "texlive-conditext")
+    (version (number->string %texlive-revision))
+    (source (texlive-origin
+             name version
+             (list "doc/latex/conditext/"
+                   "tex/latex/conditext/")
+             (base32
+              "1b07ydmkqd4jfkqjvs073g9ansx5w378wv95qfpgrqqx7pc9215s")))
+    (outputs '("out" "doc"))
+    (build-system texlive-build-system)
+    (home-page "https://ctan.org/pkg/conditext")
+    (synopsis "Define and manage conditional content")
+    (description
+     "This package provides some commands to define and manage conditional content
+in a LaTeX source document.  A conditional content, in the sense within this
+is understood in this package, is a text (including mathematical or other
+formulas) and/or a graphical element (diagram, figure, image...) as
+substitutable forms, which, according to a condition test, may or may not
+appear in the generated document.  One of the most common forms of conditional
+content management is multilingual; but it can also include versioning,
+confidentiality levels, and so on.
+
+The philosophy of this package is based on the respective notions of condition
+field, condition property and condition space.  With this package, any
+substitutable form in a source document is identified by a condition field and
+a condition property.  The condition field is a functional theme that allows
+you to group together substitutable forms for the same conditional management.
+The condition property is a functional characterization specific to each
+substitutable form of a single condition domain.  The condition space is used
+to designate the substitutable form(s) that must appear in the generated
+document.  A condition space is defined by specifying a condition domain and
+a condition property to match with one or more substitutable forms.")
+    (license license:lppl1.3c)))
+
 ;;;
 ;;; Avoid adding new packages to the end of this file. To reduce the chances
 ;;; of a merge conflict, place them above by existing packages with similar
