004e9fe177c25b1aa924d6669fd93b92b7ebb0c6
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-08-29 15:47:34 +0200

    gnu: Add texlive-datetime2-french.
    * gnu/packages/tex.scm (texlive-datetime2-french): New variable.

--- gnu/packages/tex.scm
+++ gnu/packages/tex.scm
@@ -78274,6 +78274,26 @@ currently unmaintained.")
 @code{\\DTMsetstyle} provided by @file{datetime2.sty}.")
     (license license:lppl1.3+)))
 
+(define-public texlive-datetime2-french
+  (package
+    (name "texlive-datetime2-french")
+    (version (number->string %texlive-revision))
+    (source (texlive-origin
+             name version
+             (list "doc/latex/datetime2-french/"
+                   "source/latex/datetime2-french/"
+                   "tex/latex/datetime2-french/")
+             (base32
+              "1m5vkdhclrz2j25xjg0syj5xxa78vil164n1779vc68g68dm8mam")))
+    (outputs '("out" "doc"))
+    (build-system texlive-build-system)
+    (home-page "https://ctan.org/pkg/datetime2-french")
+    (synopsis "French language module for the @code{datetime2} package")
+    (description
+     "This module provides the @code{french} style that can be set using
+@code{\\DTMsetstyle} provided by @file{datetime2.sty}.")
+    (license license:lppl1.3+)))
+
 ;;;
 ;;; Avoid adding new packages to the end of this file. To reduce the chances
 ;;; of a merge conflict, place them above by existing packages with similar
