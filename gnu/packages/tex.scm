0cba654e237b3e1e884a396e8192e83374ff3506
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-08-29 15:51:33 +0200

    gnu: Add texlive-glossaries-french.
    * gnu/packages/tex.scm (texlive-glossaries-french): New variable.

--- gnu/packages/tex.scm
+++ gnu/packages/tex.scm
@@ -83784,6 +83784,26 @@ package.")
 package.")
     (license license:lppl1.3+)))
 
+(define-public texlive-glossaries-french
+  (package
+    (name "texlive-glossaries-french")
+    (version (number->string %texlive-revision))
+    (source (texlive-origin
+             name version
+             (list "doc/latex/glossaries-french/"
+                   "source/latex/glossaries-french/"
+                   "tex/latex/glossaries-french/")
+             (base32
+              "1q20vf43fbjbx3jq4w3d6jvk1z0rigdnl85bc6gwis7hxpj63ji9")))
+    (outputs '("out" "doc"))
+    (build-system texlive-build-system)
+    (home-page "https://ctan.org/pkg/glossaries-french")
+    (synopsis "French language module for @code{glossaries} package")
+    (description
+     "This package provides a French language module for @code{glossaries}
+package.")
+    (license license:lppl1.3+)))
+
 ;;;
 ;;; Avoid adding new packages to the end of this file. To reduce the chances
 ;;; of a merge conflict, place them above by existing packages with similar
