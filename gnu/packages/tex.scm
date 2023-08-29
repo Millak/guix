3495db0c330724ed5f4d34ab469b3118fb1b14d2
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-08-29 15:47:56 +0200

    gnu: Add texlive-datetime2-turkish.
    * gnu/packages/tex.scm (texlive-datetime2-turkish): New variable.

--- gnu/packages/tex.scm
+++ gnu/packages/tex.scm
@@ -78777,6 +78777,27 @@ currently unmaintained.")
 currently unmaintained.")
     (license license:lppl1.3+)))
 
+(define-public texlive-datetime2-turkish
+  (package
+    (name "texlive-datetime2-turkish")
+    (version (number->string %texlive-revision))
+    (source (texlive-origin
+             name version
+             (list "doc/latex/datetime2-turkish/"
+                   "source/latex/datetime2-turkish/"
+                   "tex/latex/datetime2-turkish/")
+             (base32
+              "1xhv08gkn4sj305szvvfflprag43qg437bfwhc19la93g79yx16k")))
+    (outputs '("out" "doc"))
+    (build-system texlive-build-system)
+    (home-page "https://ctan.org/pkg/datetime2-turkish")
+    (synopsis "Turkish language module for the @code{datetime2} package")
+    (description
+     "This module provides the @code{turkish} style that can be set using
+@code{\\DTMsetstyle} provided by @file{datetime2.sty}.  This package is
+currently unmaintained.")
+    (license license:lppl1.3+)))
+
 ;;;
 ;;; Avoid adding new packages to the end of this file. To reduce the chances
 ;;; of a merge conflict, place them above by existing packages with similar
