2fc6fc86384e01ef3aba69e68edc2ffe33c820e6
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-08-29 15:47:23 +0200

    gnu: Add texlive-datetime2-breton.
    * gnu/packages/tex.scm (texlive-datetime2-breton): New variable.

--- gnu/packages/tex.scm
+++ gnu/packages/tex.scm
@@ -78016,6 +78016,27 @@ currently unmaintained.")
 @code{\\DTMsetstyle} provided by @file{datetime2.sty}.")
     (license license:lppl1.3+)))
 
+(define-public texlive-datetime2-breton
+  (package
+    (name "texlive-datetime2-breton")
+    (version (number->string %texlive-revision))
+    (source (texlive-origin
+             name version
+             (list "doc/latex/datetime2-breton/"
+                   "source/latex/datetime2-breton/"
+                   "tex/latex/datetime2-breton/")
+             (base32
+              "10kgrdvr4psyhc3bjpgq0idj6zzfcfvsxrql2hvy5gqn4gzncbla")))
+    (outputs '("out" "doc"))
+    (build-system texlive-build-system)
+    (home-page "https://ctan.org/pkg/datetime2-breton")
+    (synopsis "Breton language module for the @code{datetime2} package")
+    (description
+     "This module provides the @code{breton} style that can be set using
+@code{\\DTMsetstyle} provided by @file{datetime2.sty}.  This package is
+currently unmaintained.")
+    (license license:lppl1.3+)))
+
 ;;;
 ;;; Avoid adding new packages to the end of this file. To reduce the chances
 ;;; of a merge conflict, place them above by existing packages with similar
