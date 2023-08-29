3636e80c6c4cfbc148cf434b9a71db5b1e74856d
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-08-29 15:47:51 +0200

    gnu: Add texlive-datetime2-scottish.
    * gnu/packages/tex.scm (texlive-datetime2-scottish): New variable.

--- gnu/packages/tex.scm
+++ gnu/packages/tex.scm
@@ -78650,6 +78650,27 @@ currently unmaintained.")
 currently unmaintained.")
     (license license:lppl1.3+)))
 
+(define-public texlive-datetime2-scottish
+  (package
+    (name "texlive-datetime2-scottish")
+    (version (number->string %texlive-revision))
+    (source (texlive-origin
+             name version
+             (list "doc/latex/datetime2-scottish/"
+                   "source/latex/datetime2-scottish/"
+                   "tex/latex/datetime2-scottish/")
+             (base32
+              "1p9zir361i5gwwiaw2fr9h5ckiilaaxc2p8gfy8xcg6q41lqa6rr")))
+    (outputs '("out" "doc"))
+    (build-system texlive-build-system)
+    (home-page "https://ctan.org/pkg/datetime2-scottish")
+    (synopsis "Scottish Gaelic Language Module for the @code{datetime2} Package")
+    (description
+     "This module provides the @code{scottish} style that can be set using
+@code{\\DTMsetstyle} provided by @file{datetime2.sty}.  This package is
+currently unmaintained.")
+    (license license:lppl1.3+)))
+
 ;;;
 ;;; Avoid adding new packages to the end of this file. To reduce the chances
 ;;; of a merge conflict, place them above by existing packages with similar
