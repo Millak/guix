078aa357517fc7e134e5469b2ffee570c3d33456
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-08-29 15:47:45 +0200

    gnu: Add texlive-datetime2-norsk.
    * gnu/packages/tex.scm (texlive-datetime2-norsk): New variable.

--- gnu/packages/tex.scm
+++ gnu/packages/tex.scm
@@ -78525,6 +78525,27 @@ currently unmaintained.")
 currently unmaintained.")
     (license license:lppl1.3+)))
 
+(define-public texlive-datetime2-norsk
+  (package
+    (name "texlive-datetime2-norsk")
+    (version (number->string %texlive-revision))
+    (source (texlive-origin
+             name version
+             (list "doc/latex/datetime2-norsk/"
+                   "source/latex/datetime2-norsk/"
+                   "tex/latex/datetime2-norsk/")
+             (base32
+              "1qan02np4jayqbs7ss92jlldsmmvwxjj7jjngipq3ik7dw035796")))
+    (outputs '("out" "doc"))
+    (build-system texlive-build-system)
+    (home-page "https://ctan.org/pkg/datetime2-norsk")
+    (synopsis "Norsk language module for the @code{datetime2} package")
+    (description
+     "This module provides the @code{norsk} style that can be set using
+@code{\\DTMsetstyle} provided by @file{datetime2.sty}.  This package is
+currently unmaintained.")
+    (license license:lppl1.3+)))
+
 ;;;
 ;;; Avoid adding new packages to the end of this file. To reduce the chances
 ;;; of a merge conflict, place them above by existing packages with similar
