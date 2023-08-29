9fa31c5ae6017d89059e38fcfa6e194110909bdb
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-08-29 15:55:22 +0200

    gnu: Add texlive-memexsupp.
    * gnu/packages/tex.scm (texlive-memexsupp): New variable.

--- gnu/packages/tex.scm
+++ gnu/packages/tex.scm
@@ -88578,6 +88578,26 @@ be produced for distribution prior to the meeting, with user-selected
 portions suppressed from printing.")
     (license license:lppl1.3+)))
 
+(define-public texlive-memexsupp
+  (package
+    (name "texlive-memexsupp")
+    (version (number->string %texlive-revision))
+    (source (texlive-origin
+             name version
+             (list "doc/latex/memexsupp/"
+                   "tex/latex/memexsupp/")
+             (base32
+              "1ckgin457bskn3iih0fzyy2jnh7w4xyyx7nspcpk2bc6ncslmbpj")))
+    (outputs '("out" "doc"))
+    (build-system texlive-build-system)
+    (home-page "https://ctan.org/pkg/memexsupp")
+    (synopsis "Experimental @code{memoir} support")
+    (description
+     "This package provides a package of code proposed as supporting material for
+@code{memoir}.  The package is intended as a test bed for such code, which may
+in the fullness of time be adopted into the main @code{memoir} release.")
+    (license license:lppl)))
+
 ;;;
 ;;; Avoid adding new packages to the end of this file. To reduce the chances
 ;;; of a merge conflict, place them above by existing packages with similar
