7bd4a8baee9b58b256811cf2209da7944c13673a
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-08-29 15:47:58 +0200

    gnu: Add texlive-datetime2-ukrainian.
    * gnu/packages/tex.scm (texlive-datetime2-ukrainian): New variable.

--- gnu/packages/tex.scm
+++ gnu/packages/tex.scm
@@ -78798,6 +78798,26 @@ currently unmaintained.")
 currently unmaintained.")
     (license license:lppl1.3+)))
 
+(define-public texlive-datetime2-ukrainian
+  (package
+    (name "texlive-datetime2-ukrainian")
+    (version (number->string %texlive-revision))
+    (source (texlive-origin
+             name version
+             (list "doc/latex/datetime2-ukrainian/"
+                   "source/latex/datetime2-ukrainian/"
+                   "tex/latex/datetime2-ukrainian/")
+             (base32
+              "19m3866mwm61r3chpil0xilzirql52igajwdz2v0d3m3x3g12ngs")))
+    (outputs '("out" "doc"))
+    (build-system texlive-build-system)
+    (home-page "https://ctan.org/pkg/datetime2-ukrainian")
+    (synopsis "Ukrainian language module for the @code{datetime2} package")
+    (description
+     "This module provides the @code{ukrainian} style that can be set using
+@code{\\DTMsetstyle} provided by @file{datetime2.sty}.")
+    (license license:lppl1.3+)))
+
 ;;;
 ;;; Avoid adding new packages to the end of this file. To reduce the chances
 ;;; of a merge conflict, place them above by existing packages with similar
