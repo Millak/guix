d02012653bd43e50229321b1b8fef87377583a76
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-08-29 16:01:24 +0200

    gnu: Add texlive-slantsc.
    * gnu/packages/tex.scm (texlive-slantsc): New variable.

--- gnu/packages/tex.scm
+++ gnu/packages/tex.scm
@@ -95935,6 +95935,26 @@ coursework and the like).  The class is small and straightforward; its design
 was inspired by that of the PracTeX journal style.")
     (license license:lppl1.3+)))
 
+(define-public texlive-slantsc
+  (package
+    (name "texlive-slantsc")
+    (version (number->string %texlive-revision))
+    (source (texlive-origin
+             name version
+             (list "doc/latex/slantsc/" "source/latex/slantsc/"
+                   "tex/latex/slantsc/")
+             (base32
+              "1m00h2bmgj5w3zd219s1l25ihr05kj1kpwilggbgh720bcvc3idp")))
+    (outputs '("out" "doc"))
+    (build-system texlive-build-system)
+    (home-page "https://ctan.org/pkg/slantsc")
+    (synopsis "Access different-shaped small-caps fonts")
+    (description
+     "This package enables the use of small capitals in different font shapes,
+e.g., slanted or bold slanted for all fonts that provide appropriate font
+shapes.")
+    (license license:lppl)))
+
 ;;;
 ;;; Avoid adding new packages to the end of this file. To reduce the chances
 ;;; of a merge conflict, place them above by existing packages with similar
