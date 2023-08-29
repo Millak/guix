7a5ef24572802e7a5ec4b1adebc48d1baf09ae0a
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-08-29 15:57:40 +0200

    gnu: Add texlive-parsa.
    * gnu/packages/tex.scm (texlive-parsa): New variable.

--- gnu/packages/tex.scm
+++ gnu/packages/tex.scm
@@ -91427,6 +91427,25 @@ running paragraphs; placement is at the end of each paragraph, or manually,
 using the @code{\\parnotes} command.")
     (license license:lppl1.3+)))
 
+(define-public texlive-parsa
+  (package
+    (name "texlive-parsa")
+    (version (number->string %texlive-revision))
+    (source (texlive-origin
+             name version
+             (list "doc/xelatex/parsa/" "tex/xelatex/parsa/")
+             (base32
+              "0g22q6frngl8mw1lhjzyfizw8394n0f8mnafxijyw0am40ipsvi4")))
+    (outputs '("out" "doc"))
+    (build-system texlive-build-system)
+    (home-page "https://ctan.org/pkg/parsa")
+    (synopsis
+     "XeLaTeX package for theses and dissertations at Iranian universities")
+    (description
+     "This package provides a package for preparing dissertations and theses for
+Iranian universities as fast and as efficiently as possible.")
+    (license license:lppl1.3c)))
+
 ;;;
 ;;; Avoid adding new packages to the end of this file. To reduce the chances
 ;;; of a merge conflict, place them above by existing packages with similar
