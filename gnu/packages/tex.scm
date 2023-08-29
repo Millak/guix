a755a32bb707099fa23acf8e107a18cab7840141
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-08-29 15:47:37 +0200

    gnu: Add texlive-datetime2-greek.
    * gnu/packages/tex.scm (texlive-datetime2-greek): New variable.

--- gnu/packages/tex.scm
+++ gnu/packages/tex.scm
@@ -78335,6 +78335,27 @@ currently unmaintained.")
 @code{\\DTMsetstyle} provided by @file{datetime2.sty}.")
     (license license:lppl1.3c)))
 
+(define-public texlive-datetime2-greek
+  (package
+    (name "texlive-datetime2-greek")
+    (version (number->string %texlive-revision))
+    (source (texlive-origin
+             name version
+             (list "doc/latex/datetime2-greek/"
+                   "source/latex/datetime2-greek/"
+                   "tex/latex/datetime2-greek/")
+             (base32
+              "1ai22nlgfsrqz5fz5diq5jjykrh2qqchw6r0cxmpmv9pv0vyba2s")))
+    (outputs '("out" "doc"))
+    (build-system texlive-build-system)
+    (home-page "https://ctan.org/pkg/datetime2-greek")
+    (synopsis "Greek language module for the @code{datetime2} package")
+    (description
+     "This module provides the @code{greek} style that can be set using
+@code{\\DTMsetstyle} provided by @file{datetime2.sty}.  This package is
+currently unmaintained.")
+    (license license:lppl1.3+)))
+
 ;;;
 ;;; Avoid adding new packages to the end of this file. To reduce the chances
 ;;; of a merge conflict, place them above by existing packages with similar
