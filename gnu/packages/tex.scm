737b7db2a3416c6ac5263e10050a062e299e16a3
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-08-29 15:47:28 +0200

    gnu: Add texlive-datetime2-danish.
    * gnu/packages/tex.scm (texlive-datetime2-danish): New variable.

--- gnu/packages/tex.scm
+++ gnu/packages/tex.scm
@@ -78120,6 +78120,27 @@ unmaintained.")
 currently unmaintained.")
     (license license:lppl1.3+)))
 
+(define-public texlive-datetime2-danish
+  (package
+    (name "texlive-datetime2-danish")
+    (version (number->string %texlive-revision))
+    (source (texlive-origin
+             name version
+             (list "doc/latex/datetime2-danish/"
+                   "source/latex/datetime2-danish/"
+                   "tex/latex/datetime2-danish/")
+             (base32
+              "0inz6afkhknmdm2g38hyzndkcfp5pkdkysw0hnv9ackavpgsa12c")))
+    (outputs '("out" "doc"))
+    (build-system texlive-build-system)
+    (home-page "https://ctan.org/pkg/datetime2-danish")
+    (synopsis "Danish language module for the @code{datetime2} package")
+    (description
+     "This module provides the @code{danish} style that can be set using
+@code{\\DTMsetstyle} provided by @file{datetime2.sty}.  This package is
+currently unmaintained.")
+    (license license:lppl1.3+)))
+
 ;;;
 ;;; Avoid adding new packages to the end of this file. To reduce the chances
 ;;; of a merge conflict, place them above by existing packages with similar
