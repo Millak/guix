ca60f2233fd9914b79f8619f78b5eb2c620a7ab0
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-08-29 15:47:24 +0200

    gnu: Add texlive-datetime2-bulgarian.
    * gnu/packages/tex.scm (texlive-datetime2-bulgarian): New variable.

--- gnu/packages/tex.scm
+++ gnu/packages/tex.scm
@@ -78037,6 +78037,27 @@ currently unmaintained.")
 currently unmaintained.")
     (license license:lppl1.3+)))
 
+(define-public texlive-datetime2-bulgarian
+  (package
+    (name "texlive-datetime2-bulgarian")
+    (version (number->string %texlive-revision))
+    (source (texlive-origin
+             name version
+             (list "doc/latex/datetime2-bulgarian/"
+                   "source/latex/datetime2-bulgarian/"
+                   "tex/latex/datetime2-bulgarian/")
+             (base32
+              "0s203lbaijjqji6kycni17vwmn5dy2az54r7bri5i7ys5ylff0cr")))
+    (outputs '("out" "doc"))
+    (build-system texlive-build-system)
+    (home-page "https://ctan.org/pkg/datetime2-bulgarian")
+    (synopsis "Bulgarian language module for the @code{datetime2} package")
+    (description
+     "This module provides the @code{bulgarian} style that can be set using
+@code{\\DTMsetstyle} provided by @file{datetime2.sty}.  This package is
+currently unmaintained.")
+    (license license:lppl1.3+)))
+
 ;;;
 ;;; Avoid adding new packages to the end of this file. To reduce the chances
 ;;; of a merge conflict, place them above by existing packages with similar
