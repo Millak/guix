d68c3666a7e75b542d010008b4f15a58f741c4e2
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-08-29 15:47:38 +0200

    gnu: Add texlive-datetime2-hebrew.
    * gnu/packages/tex.scm (texlive-datetime2-hebrew): New variable.

--- gnu/packages/tex.scm
+++ gnu/packages/tex.scm
@@ -78356,6 +78356,27 @@ currently unmaintained.")
 currently unmaintained.")
     (license license:lppl1.3+)))
 
+(define-public texlive-datetime2-hebrew
+  (package
+    (name "texlive-datetime2-hebrew")
+    (version (number->string %texlive-revision))
+    (source (texlive-origin
+             name version
+             (list "doc/latex/datetime2-hebrew/"
+                   "source/latex/datetime2-hebrew/"
+                   "tex/latex/datetime2-hebrew/")
+             (base32
+              "0n7kw2qmjvdxcnvvh2xv6n86dggmglz7ffvlg3ca2k95gzdw0z5d")))
+    (outputs '("out" "doc"))
+    (build-system texlive-build-system)
+    (home-page "https://ctan.org/pkg/datetime2-hebrew")
+    (synopsis "Hebrew language module for the @code{datetime2} package")
+    (description
+     "This module provides the @code{hebrew} style that can be set using
+@code{\\DTMsetstyle} provided by @file{datetime2.sty}.  This package is
+currently unmaintained.")
+    (license license:lppl1.3+)))
+
 ;;;
 ;;; Avoid adding new packages to the end of this file. To reduce the chances
 ;;; of a merge conflict, place them above by existing packages with similar
