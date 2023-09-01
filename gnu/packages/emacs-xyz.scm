5973ea8cd80c671ceb19f7c7473e1be99b2cdd45
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-09-01 21:20:10 +0200

    gnu: emacs-tramp: Update to 2.6.1.2.
    * gnu/packages/emacs-xyz.scm (emacs-tramp): Update to 2.6.1.2.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -31484,14 +31484,14 @@ well as an option for visually flashing evaluated s-expressions.")
 (define-public emacs-tramp
   (package
     (name "emacs-tramp")
-    (version "2.6.1")
+    (version "2.6.1.2")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://elpa.gnu.org/packages/"
                            "tramp-" version ".tar"))
        (sha256
-        (base32 "0rk2bkr2n6064170741wjparsshziwj6j6w6sfzpl7xxbldpsqxa"))))
+        (base32 "0nazcrpkwy59dxbyzarj75zvz7vh4pgq4yzqgf6zfbvqp7q73wbn"))))
     (build-system emacs-build-system)
     (arguments
      (list
