c72bc0bb71a7315241c174636f96faee46a6dfa8
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-09-01 22:11:53 +0200

    gnu: emacs-package-lint: Improve description.
    * gnu/packages/emacs-xyz.scm (emacs-package-lint)[synopsis]: Use proper capitalisation.
    [description]: Use Texinfo markup.  Remove part that belong to the manual
    instead of the description.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -26431,13 +26431,12 @@ image, rotate it, save modified images, and more.")
      '(#:include (cons "^data/" %default-include)))
     (build-system emacs-build-system)
     (home-page "https://github.com/purcell/package-lint")
-    (synopsis "Linting library for elisp package authors")
+    (synopsis "Linting library for Elisp package authors")
     (description
      "This provides a list of issues with the Emacs package metadata of a file,
-e.g. the package dependencies it requires.  See function
-@code{package-lint-buffer}.  Checks will currently be enabled only if a
-\"Package-Requires:\" or \"Package-Version:\" header is present in the
-file.")
+e.g., the package dependencies it requires.  Checks will currently be enabled
+only if a @samp{Package-Requires:} or @samp{Package-Version:} header is
+present in the file.")
     (license license:gpl3+)))
 
 (define-public emacs-packed
