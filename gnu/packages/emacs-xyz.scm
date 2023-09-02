434c44786e290428f864020726b04f905dc5a0ee
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-09-02 23:04:22 +0200

    gnu: emacs-hl-todo: Update to 3.6.0.
    * gnu/packages/emacs-xyz.scm (emacs-hl-todo): Update to 3.6.0.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -11787,7 +11787,7 @@ between symbols.")
 (define-public emacs-hl-todo
   (package
     (name "emacs-hl-todo")
-    (version "3.5.0")
+    (version "3.6.0")
     (source (origin
               (method git-fetch)
               (uri (git-reference
@@ -11796,7 +11796,7 @@ between symbols.")
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "1d9mkbd33zyx540ilp1vz8fgckvxv0qd2vyjgz11zh1qv1pfk8gh"))))
+                "1rq16zrdmq9j2jrsg136r5qbqvsgd2bkgpb2wmxca91kfx86r2qq"))))
     (build-system emacs-build-system)
     (propagated-inputs
      (list emacs-compat))
