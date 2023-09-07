9620b9d29858c05c774c15c3011bd610ce7ab671
Simon South <simon@simonsouth.net>
2023-09-07 12:37:50 -0400

    gnu: libftdi: Update package style.
    * gnu/packages/libftdi.scm: Re-order #:use-module statements.
    (libftdi): Wrap long lines; reformat gently.
    [license]: Use "license:" prefix.
    
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/packages/libftdi.scm
+++ gnu/packages/libftdi.scm
@@ -18,12 +18,12 @@
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (gnu packages libftdi)
-  #:use-module (guix licenses)
+  #:use-module (guix build-system cmake)
   #:use-module (guix download)
+  #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix packages)
   #:use-module (gnu packages)
-  #:use-module (gnu packages libusb)
-  #:use-module (guix build-system cmake))
+  #:use-module (gnu packages libusb))
 
 (define-public libftdi
   (package
@@ -32,18 +32,17 @@
     (source (origin
               (method url-fetch)
               (uri (string-append
-                    "http://www.intra2net.com/en/developer/libftdi/download/libftdi1-"
-                    version ".tar.bz2"))
+                    "http://www.intra2net.com/en/developer/libftdi/download/"
+                    "libftdi1-" version ".tar.bz2"))
               (sha256
                (base32
                 "0x0vncf6i92slgrn0h7ghkskqbglbs534220qa84d0qg114zndpc"))))
     (build-system cmake-build-system)
     (propagated-inputs
-     (list libusb)) ; required by libftdi1.pc
+     (list libusb))                     ; required by libftdi1.pc
     (home-page "https://www.intra2net.com/en/developer/libftdi/")
     (synopsis "FTDI USB driver with bitbang mode")
     (description
-     "libFTDI is a library to talk to FTDI chips: FT232BM,
-FT245BM, FT2232C, FT2232D, FT245R and FT232H including the popular
-bitbangmode.")
-    (license lgpl2.1)))
+     "libFTDI is a library to talk to FTDI chips: FT232BM, FT245BM, FT2232C,
+FT2232D, FT245R and FT232H including the popular bitbangmode.")
+    (license license:lgpl2.1)))
