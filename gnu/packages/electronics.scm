5c154cf2f03578bb27acac29ddbbfbb2c3fa7b2e
Simon South <simon@simonsouth.net>
2023-09-17 18:12:51 -0400

    gnu: sigrok-firmware-fx2lafw: Remove input labels.
    * gnu/packages/electronics.scm (sigrok-firmware-fx2lafw)[native-inputs]:
    Remove input labels.
    
    Signed-off-by: Christopher Baines <mail@cbaines.net>

--- gnu/packages/electronics.scm
+++ gnu/packages/electronics.scm
@@ -133,7 +133,7 @@ to take care of the OS-specific details when writing software that uses serial p
                (base32
                 "0fyfd82mvrcf55v5a3afq1mh1kfswk4c37qrbln6x92jm3b41x53"))))
     (native-inputs
-     `(("sdcc" ,sdcc)))
+     (list sdcc))
     (build-system gnu-build-system)
     (home-page "https://www.sigrok.org/wiki/Fx2lafw")
     (synopsis "Firmware for Cypress FX2 chips")
