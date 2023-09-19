b14f44710652c90e4a96ece966644021c877c0e9
Hendursaga <hendursaga@aol.com>
2023-09-19 16:42:15 -0400

    gnu: desmume: Update to 0.9.13.
    * gnu/packages/emulators.scm (desmume): Update to 0.9.13.
    [source]: Switch to git-fetch.  Remove patches.
    [build-system]: Switch to meson-build-system.
    [arguments]<#:configure-flags>: Replace “--enable-openal” with “-Dopenal=true”.
    Remove “--enable-wifi”.
    Add “-Dfrontend-cli=true”, “-Dfrontend-gtk=true”, and “-Dgdb-stub=true”.
    <#:phases>: Add ‘chdir’.
    [native-inputs]: Add glib:bin and gettext-minimal.
    [inputs]: Replace sdl with sdl2 and gtk+-2 with gtk+.
    Remove glu.
    Add agg, alsa-lib, libpcap, openal, and soundtouch.
    [home-page]: Use HTTPS.
    * gnu/packages/patches/desmume-gcc6-fixes.patch: Removed file.
    * gnu/packages/patches/desmume-gcc7-fixes.patch: Likewise.
    * gnu/local.mk (dist_patch_DATA): Adjust accordingly.
    
    Signed-off-by: Liliana Marie Prikler <liliana.prikler@gmail.com>

--- gnu/local.mk
+++ gnu/local.mk
@@ -1066,8 +1066,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/debops-constants-for-external-program-names.patch \
   %D%/packages/patches/debops-debops-defaults-fall-back-to-less.patch \
   %D%/packages/patches/dee-vapi.patch			\
-  %D%/packages/patches/desmume-gcc6-fixes.patch			\
-  %D%/packages/patches/desmume-gcc7-fixes.patch			\
   %D%/packages/patches/dfu-programmer-fix-libusb.patch		\
   %D%/packages/patches/diffutils-fix-signal-processing.patch	\
   %D%/packages/patches/directfb-davinci-glibc-228-compat.patch	\
