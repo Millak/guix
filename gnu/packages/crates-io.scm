429294d95a12d22e510b5e88dc80b315852cdafb
Andrew Jose <arnav.jose@gmail.com>
2023-09-21 18:14:49 +0530

    gnu: Add rust-snafu-0.7.
    * gnu/packages/crates-io.scm (rust-snafu-0.7): New variable.
    (rust-snafu-0.6): Inherit from rust-snafu-0.7.
    
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -62862,18 +62862,42 @@ package provides derive macros.")
         ("rust-quote" ,rust-quote-1)
         ("rust-syn" ,rust-syn-1))))))
 
-(define-public rust-snafu-0.6
+(define-public rust-snafu-0.7
   (package
     (name "rust-snafu")
-    (version "0.6.10")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (crate-uri "snafu" version))
-       (file-name (string-append name "-" version ".tar.gz"))
-       (sha256
-        (base32 "19wwqxwb85pl040qk5xylj0vlznib3xzy9hcv2q0h8qv4qy2vcga"))))
+    (version "0.7.5")
+    (source (origin
+              (method url-fetch)
+              (uri (crate-uri "snafu" version))
+              (file-name (string-append name "-" version ".tar.gz"))
+              (sha256
+               (base32
+                "1mj2j2gfbf8mm1hr02zrbrqrh2zp01f61xgkx0lpln2w0ankgpp4"))))
     (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs
+       (("rust-backtrace" ,rust-backtrace-0.3)
+        ("rust-doc-comment" ,rust-doc-comment-0.3)
+        ("rust-futures" ,rust-futures-0.3)
+        ("rust-futures-core" ,rust-futures-core-0.3)
+        ("rust-pin-project" ,rust-pin-project-1)
+        ("rust-snafu-derive" ,rust-snafu-derive-0.7))))
+    (home-page "https://github.com/shepmaster/snafu")
+    (synopsis "Ergonomic error handling library")
+    (description "Snafu aims to be an ergonomic error handling library.")
+    (license (list license:expat license:asl2.0))))
+
+(define-public rust-snafu-0.6
+  (package
+    (inherit rust-snafu-0.7)
+    (name "rust-snafu")
+    (version "0.6.10")
+    (source (origin
+              (method url-fetch)
+              (uri (crate-uri "snafu" version))
+              (file-name (string-append name "-" version ".tar.gz"))
+              (sha256
+               (base32 "19wwqxwb85pl040qk5xylj0vlznib3xzy9hcv2q0h8qv4qy2vcga"))))
     (arguments
      `(#:cargo-inputs
        (("rust-backtrace" ,rust-backtrace-0.3)
@@ -62882,11 +62906,7 @@ package provides derive macros.")
         ("rust-futures" ,rust-futures-0.1)
         ("rust-futures-core" ,rust-futures-core-0.3)
         ("rust-pin-project" ,rust-pin-project-0.4)
-        ("rust-snafu-derive" ,rust-snafu-derive-0.6))))
-    (home-page "https://github.com/shepmaster/snafu")
-    (synopsis "Ergonomic error handling library")
-    (description "Snafu aims to be an ergonomic error handling library.")
-    (license (list license:expat license:asl2.0))))
+        ("rust-snafu-derive" ,rust-snafu-derive-0.6))))))
 
 (define-public rust-snap-1
   (package
