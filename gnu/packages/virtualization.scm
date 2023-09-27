8fc1b0ccc8688e3e6ceae9d7004f95c88d08447a
Hilton Chain <hako@ultrarare.space>
2023-09-27 19:50:13 +0800

    gnu: hwdata: Update to 0.374.
    * gnu/packages/pciutils.scm (hwdata): Update to 0.374.
    [arguments]: Don't modify #:phases.
    [outputs]: Keep only "out" for pkg-config file.
    (pciutils)[native-inputs]: Replace hwdata to "out" output.
    * gnu/packages/graphics.scm (mangohub)[inputs]: Likewise.
    * gnu/packages/kde-plasma.scm (kwin)[inputs]: Likewise.
    * gnu/packages/virtualization.scm (libosinfo)[native-inputs]: Likewise.
    * gnu/packages/wm.scm (wlroots)[native-inputs]: Likewise.
    
    Signed-off-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>

--- gnu/packages/virtualization.scm
+++ gnu/packages/virtualization.scm
@@ -1330,8 +1330,7 @@ of one or more RISC-V harts.")
      (list `(,glib "bin")                ;glib-mkenums, etc.
            gobject-introspection
            gtk-doc/stable
-           `(,hwdata "pci")
-           `(,hwdata "usb")
+           hwdata
            vala
            intltool
            pkg-config))
