Millak/guix

commit 59587e2fda76c19b424fda9a987ff90d52ef217e

gnu: guix: Update to d0438fc.
This adds the “builtin:git-download” derivation builder to guix-daemon,
added in commit 13b0cf85eb31e1b1ea674debbbfb0f35a32d1374.

* gnu/packages/package-management.scm (guix): Update to d0438fc.

gnu/packages/package-management.scm
@@ -174,8 +174,8 @@
   ;; Note: the 'update-guix-package.scm' script expects this definition to
   ;; start precisely like this.
   (let ((version "1.4.0")
-        (commit "4dfdd822102690b5687acf28365ab707b68d9476")
-        (revision 10))
+        (commit "d0438fcf658cb8c95b32381fac44b0a6278459ed")
+        (revision 11))
     (package
       (name "guix")
 
@@ -191,7 +191,7 @@
                       (commit commit)))
                 (sha256
                  (base32
-                  "1p21gz2lr7iqvma1m83k2r04w201rzvk31d5kfn2qkr9l0gds4cx"))
+                  "05i32m7cxs7vhl54hvs0f9g84jhnn2dbc9qcgaqvqnqc4i8yhhxr"))
                 (file-name (string-append "guix-" version "-checkout"))))
       (build-system gnu-build-system)
       (arguments
@@ -514,6 +514,8 @@ $(prefix)/etc/openrc\n")))
 
          ("disarchive" ,disarchive)               ;for 'guix perform-download'
          ("guile-lzma" ,guile-lzma)               ;for Disarchive
+
+         ("git-minimal" ,git-minimal)             ;for 'guix perform-download'
 
          ("glibc-utf8-locales" ,glibc-utf8-locales)))
       (propagated-inputs

commit c4886c0cb608e1b36969f195eb2f7ea25a37ac89

gnu: guix: Fix cross-compilation.
Fixes a regression introduced in
59587e2fda76c19b424fda9a987ff90d52ef217e where ./configure would fail
when cross-compiling with “Git is missing”.

Reported by gabber on #guix.

* gnu/packages/package-management.scm (guix)[arguments]: Add
‘ac_cv_path_GIT’ to #:configure-flags.

gnu/packages/package-management.scm
@@ -215,6 +215,13 @@
                             ;; system installation image.)
                             "ac_cv_path_DOT_USER_PROGRAM=dot"
 
+                            ;; When cross-compiling, 'git' is not in $PATH
+                            ;; (because it's not a native input).  Thus,
+                            ;; always explicitly pass its file name.
+                            (string-append "ac_cv_path_GIT="
+                                           (search-input-file %build-inputs
+                                                              "/bin/git"))
+
                             ;; To avoid problems with the length of shebangs,
                             ;; choose a fixed-width and short directory name
                             ;; for tests.