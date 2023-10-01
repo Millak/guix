063bc94017b3c51836c1fbca601f92b61a08e56b
Efraim Flashner <efraim@flashner.co.il>
2023-10-01 16:14:51 +0300

    gnu: Add rust-pango-sys-0.17.
    * gnu/packages/crates-gtk.scm (rust-pango-sys-0.17): New variable.
    (rust-pango-sys-0.15): Inherit from rust-pango-sys-0.17.

--- gnu/packages/crates-gtk.scm
+++ gnu/packages/crates-gtk.scm
@@ -2071,22 +2071,22 @@ library.")
        #:cargo-development-inputs
        (("rust-gir-format-check" ,rust-gir-format-check-0.1))))))
 
-(define-public rust-pango-sys-0.15
+(define-public rust-pango-sys-0.17
   (package
     (name "rust-pango-sys")
-    (version "0.15.10")
+    (version "0.17.10")
     (source
      (origin
        (method url-fetch)
        (uri (crate-uri "pango-sys" version))
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
-        (base32 "1an3c931kbkr08n9d9d1dapsq3n26zs0xn4ixn11jrp4rn0h186j"))))
+        (base32 "15aindwdxsydzvqcvhc9ysamx5v1jmq8qbs61ncxic2h72grz9ix"))))
     (build-system cargo-build-system)
     (arguments
      `(#:cargo-inputs
-       (("rust-glib-sys" ,rust-glib-sys-0.15)
-        ("rust-gobject-sys" ,rust-gobject-sys-0.15)
+       (("rust-glib-sys" ,rust-glib-sys-0.17)
+        ("rust-gobject-sys" ,rust-gobject-sys-0.17)
         ("rust-libc" ,rust-libc-0.2)
         ("rust-system-deps" ,rust-system-deps-6))
        #:cargo-development-inputs
@@ -2100,6 +2100,28 @@ library.")
     (synopsis "FFI bindings to libpango-1.0")
     (description "This package provides FFI bindings to @code{libpango-1.0}.")
     (license license:expat)))
+
+(define-public rust-pango-sys-0.15
+  (package
+    (inherit rust-pango-sys-0.17)
+    (name "rust-pango-sys")
+    (version "0.15.10")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "pango-sys" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1an3c931kbkr08n9d9d1dapsq3n26zs0xn4ixn11jrp4rn0h186j"))))
+    (arguments
+     `(#:cargo-inputs
+       (("rust-glib-sys" ,rust-glib-sys-0.15)
+        ("rust-gobject-sys" ,rust-gobject-sys-0.15)
+        ("rust-libc" ,rust-libc-0.2)
+        ("rust-system-deps" ,rust-system-deps-6))
+       #:cargo-development-inputs
+       (("rust-shell-words" ,rust-shell-words-1)
+        ("rust-tempfile" ,rust-tempfile-3))))))
 
 (define-public rust-pango-sys-0.14
   (package
