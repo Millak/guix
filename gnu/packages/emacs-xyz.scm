026e28712b490ed1dfe0abc5bdd0f8abef753c02
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2023-10-03 13:36:37 +0200

    gnu: emacs-pyim: Update to 5.3.3.
    * gnu/packages/emacs-xyz.scm (emacs-pyim): Update to 5.3.3.
    [propagated-inputs]: Remove EMACS-POPUP.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -18491,16 +18491,16 @@ containing words from the Rime project.")
 (define-public emacs-pyim
   (package
     (name "emacs-pyim")
-    (version "5.3.2")
+    (version "5.3.3")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://elpa.gnu.org/packages/pyim-" version ".tar"))
        (sha256
-        (base32 "13irkmhlfq99glyy0vhj559si5672cqcysjxlxn7lvckxr298vzc"))))
+        (base32 "04dz3gdqq6pcxycpzkzzmhbg6lk629v41y64jlh6si21jwfr6wnx"))))
     (build-system emacs-build-system)
     (propagated-inputs
-     (list emacs-async emacs-popup emacs-posframe emacs-xr))
+     (list emacs-async emacs-posframe emacs-xr))
     (home-page "https://github.com/tumashu/pyim")
     (synopsis "Chinese input method")
     (description
