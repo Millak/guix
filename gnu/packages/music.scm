7722ca5f414386ca4964cd8bcf09424b6dde82ee
Bruno Victal <mirai@makinata.eu>
2023-10-09 21:06:10 +0100

    gnu: lilypond: Use dblatex/stable.
    * gnu/packages/music.scm (lilypond)[native-inputs]: Replace dblatex with
    dblatex/stable.
    
    Signed-off-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>

--- gnu/packages/music.scm
+++ gnu/packages/music.scm
@@ -1671,7 +1671,7 @@ and auto-mapping slices to MIDI note numbers.")
            python))
     (native-inputs
      (list bison
-           dblatex
+           dblatex/stable
            flex
            fontforge
            gettext-minimal
