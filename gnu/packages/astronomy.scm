4b1dce71855c8338e8045d31277db2a55cf3bddd
Sharlatan Hellseher <sharlatanus@gmail.com>
2023-10-11 02:39:33 +0100

    gnu: Add python-pyregion.
    * gnu/packages/astronomy.scm (python-pyregion): New variable.
    
    Signed-off-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>

--- gnu/packages/astronomy.scm
+++ gnu/packages/astronomy.scm
@@ -3494,6 +3494,46 @@ astrophysical simulations supporting PKDGRAV/Gasoline, Gadget, Gadget4/Arepo,
 N-Chilada and RAMSES AMR outputs.")
     (license license:gpl3+)))
 
+(define-public python-pyregion
+  (package
+    (name "python-pyregion")
+    (version "2.2.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (pypi-uri "pyregion" version))
+       (sha256
+        (base32 "0l7qb7r8fnv46mdih4m5b8jaxixgpw6m7v37dpikjkblgh0vigaw"))))
+    (build-system pyproject-build-system)
+    (arguments
+     (list
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-before 'check 'build-extensions
+            (lambda _
+              ;; Cython extensions have to be built before running the tests.
+              (invoke "python" "setup.py" "build_ext" "--inplace"))))))
+    (propagated-inputs
+     (list python-astropy python-numpy python-pyparsing))
+    (native-inputs
+     (list python-cython
+           python-pytest
+           python-pytest-astropy-header
+           python-setuptools-scm))
+    (home-page "https://github.com/astropy/pyregion")
+    (synopsis "Python parser for ds9 region files")
+    (description
+     "@code{pyregion} is a python module to parse ds9 region files.  It also
+supports ciao region files.
+Features:
+@itemize
+@item ds9 and ciao region files.
+@item (physical, WCS) coordinate conversion to the image coordinate.
+@item convert regions to matplotlib patches.
+@item convert regions to spatial filter (i.e., generate mask images)
+@end itemize")
+    (license license:expat)))
+
 (define-public python-pysynphot
   (package
     (name "python-pysynphot")
