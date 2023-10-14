b8caf74f299d68bff90da92e37c99051a3616846
Rostislav Svoboda <Rostislav.Svoboda@gmail.com>
2023-10-14 19:17:22 +0200

    gnu: Add emacs-color-theme-sanityinc-tomorrow.
    * gnu/packages/emacs-xyz.scm (emacs-color-theme-sanityinc-tomorrow): New variable.
    
    Signed-off-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>
    Change-Id: I10d82838e5127b2b826615365a3eec6b2a937531

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -12338,6 +12338,32 @@ It is built on top of the custom theme support in Emacs 24 or later.")
 variants.")
     (license license:gpl3+)))
 
+(define-public emacs-color-theme-sanityinc-tomorrow
+  ;; Use the latest commit as there are no recent release/tag; the version
+  ;; metadata in the Elisp source file is also defined as '0'.
+  (let ((commit "d34e8db507ee0c7b465ff18a8f220c33ed77cd79")
+        (revision "0"))
+    (package
+      (name "emacs-color-theme-sanityinc-tomorrow")
+      (version (git-version "0" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url
+                "https://github.com/purcell/color-theme-sanityinc-tomorrow")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "1mfildi7rav2j42avii7z4gp4ghl04cqv8wp1vyxzl8vkyj60nkb"))))
+      (build-system emacs-build-system)
+      (home-page "https://github.com/purcell/color-theme-sanityinc-tomorrow")
+      (synopsis "Emacs color themes based on Chris Kempson's 'tomorrow' themes")
+      (description
+       "An Emacs version of Chris Kempson's \"Tomorrow\" themes, with much more
+extensive face definitions than the \"official\" Emacs variant.")
+      (license license:gpl3+))))
+
 (define-public emacs-solarized-theme
   (package
     (name "emacs-solarized-theme")
