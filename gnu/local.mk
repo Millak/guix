6156bf9078e52483cad01ca3fad2ad793773d0bc
pukkamustard <pukkamustard@posteo.net>
2023-10-14 12:06:32 +0000

    gnu: unison: Update to 2.53.3.
    * gnu/packages/ocaml.scm (unison): Update to 2.53.0.
      [source]: Remove patch.
      [build-system]: Use dune-build-system.
      [outputs]: Remove "doc" output.
      [native-inputs]: Update package style.
      [propagated-inputs]: Add lablgtk3 and zlib.
      [arguments]: Enable tests.
      <#:phases>: Remove prepare-install and install-fsmonitor phases.
    * gnu/packages/patches/unison-fix-ocaml-4.08.patch: Remove file.
    * gnu/local.mk: Unregister.
    
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/local.mk
+++ gnu/local.mk
@@ -2059,7 +2059,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/ungoogled-chromium-RUNPATH.patch		\
   %D%/packages/patches/ungoogled-chromium-system-ffmpeg.patch	\
   %D%/packages/patches/ungoogled-chromium-system-nspr.patch	\
-  %D%/packages/patches/unison-fix-ocaml-4.08.patch		\
   %D%/packages/patches/unknown-horizons-python-3.8-distro.patch	\
   %D%/packages/patches/unzip-CVE-2014-8139.patch		\
   %D%/packages/patches/unzip-CVE-2014-8140.patch		\
