a5dd476b414c0779abf86a00e96352c149c1a721
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2023-10-20 01:18:47 -0400

    gnu: ephemeralpg: Add bash-minimal to inputs.
    * gnu/packages/databases.scm (ephemeralpg)
    [inputs]: Add bash-minimal.
    
    Change-Id: I7f1888b1413f14f1303829aa20e976ccf6d5b86e

--- gnu/packages/databases.scm
+++ gnu/packages/databases.scm
@@ -254,11 +254,10 @@ persistent, single-file databases with support for secondary indexes.")
                     ,(string-append (assoc-ref inputs "postgresql")
                                     "/bin")
                     ;; For getsocket.
-                    ,(string-append out "/bin")))))
-             #t)))
+                    ,(string-append out "/bin"))))))))
        #:test-target "test"))
     (inputs
-     (list postgresql util-linux))
+     (list bash-minimal postgresql util-linux))
     (native-inputs
      ;; For tests.
      (list ruby which))
