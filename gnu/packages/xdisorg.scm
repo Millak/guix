8264a5b3f6482bd3f7662d479d9c91cd3a62efe7
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2023-10-19 23:35:15 -0400

    gnu: gammastep: Add bash-minimal to inputs.
    * gnu/packages/xdisorg.scm (gammastep) [inputs]: Add bash-minimal.
    
    Change-Id: Icebb1b82b86af04eeca192d341faed7fcb155f5e

--- gnu/packages/xdisorg.scm
+++ gnu/packages/xdisorg.scm
@@ -1769,7 +1769,8 @@ to an arbitrary balanced color.")
            libtool
            pkg-config))
     (inputs
-     (list glib
+     (list bash-minimal
+           glib
            gtk+
            libappindicator
            libdrm
