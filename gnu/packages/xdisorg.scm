80c8f5b57aa3699445fab29e0f75f5955e697509
jgart <jgart@dismail.de>
2023-10-20 20:48:01 -0500

    gnu: fuzzel: Update to 1.9.2.
    * gnu/packages/xdisorg.scm (fuzzel): Update to 1.9.2.

--- gnu/packages/xdisorg.scm
+++ gnu/packages/xdisorg.scm
@@ -3248,7 +3248,7 @@ using @command{dmenu}.")
 (define-public fuzzel
   (package
     (name "fuzzel")
-    (version "1.9.1")
+    (version "1.9.2")
     (home-page "https://codeberg.org/dnkl/fuzzel")
     (source (origin
               (method git-fetch)
@@ -3256,7 +3256,7 @@ using @command{dmenu}.")
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "0k65nl2yifxnb95nv6nnikqxdanng2baw7gl47ji1av3gsdx3bsm"))))
+                "1pmxqdr00x9qbg0h23q3kywhd06qna0f6r5kfa8a1v7x2n1gylsz"))))
     (build-system meson-build-system)
     (arguments
      (list #:build-type "release"
