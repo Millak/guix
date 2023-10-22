25fc05fa8cc5058b9958f4b8c75fcb678fbb5389
Efraim Flashner <efraim@flashner.co.il>
2023-10-22 15:42:54 +0300

    gnu: Add rust-zbus-polkit-1.
    * gnu/packages/crates-io.scm (rust-zbus-polkit-1): New variable.

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -83882,6 +83882,30 @@ for locating fonts.")
     (description "This package provides a collection of D-Bus bus names types.")
     (license license:expat)))
 
+(define-public rust-zbus-polkit-1
+  (package
+    (name "rust-zbus-polkit")
+    (version "1.2.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "zbus-polkit" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1yzbs8sga4s3h97vb6n5nvdvlnmhws2vj46bn44hbncfm25f51mc"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs
+       (("rust-enumflags2" ,rust-enumflags2-0.6)
+        ("rust-serde" ,rust-serde-1)
+        ("rust-serde-repr" ,rust-serde-repr-0.1)
+        ("rust-zbus" ,rust-zbus-1)
+        ("rust-zvariant" ,rust-zvariant-2))))
+    (home-page "https://github.com/dbus2/zbus/")
+    (synopsis "PolicyKit binding")
+    (description "@code{PolicyKit} binding.")
+    (license license:expat)))
+
 (define-public rust-zerocopy-0.6
   (package
     (name "rust-zerocopy")
