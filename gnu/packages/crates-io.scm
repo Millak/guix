ea44597373a9e7e8a731456c7958132c8176d2cf
Efraim Flashner <efraim@flashner.co.il>
2023-10-23 22:36:53 +0300

    gnu: rust-serde-ignored-0.1: Update to 0.1.9.
    * gnu/packages/crates-io.scm (rust-serde-ignored-0.1): Update to 0.1.9.
    
    Change-Id: I674d7f3c88c367cd42aede973004134cc01a7cfd

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -64174,16 +64174,14 @@ derive macros.")
 (define-public rust-serde-ignored-0.1
   (package
     (name "rust-serde-ignored")
-    (version "0.1.7")
+    (version "0.1.9")
     (source
      (origin
        (method url-fetch)
        (uri (crate-uri "serde_ignored" version))
-       (file-name
-        (string-append name "-" version ".tar.gz"))
+       (file-name (string-append name "-" version ".tar.gz"))
        (sha256
-        (base32
-         "19j5mrngznhxa7yfvxwmc4dc0mdzvm7w92i0m4adz2xshx04mswl"))))
+        (base32 "199c91ddk6p132xavn6hm3idw55j1c5a5xbhww4g4fgxadf1vhw0"))))
     (build-system cargo-build-system)
     (arguments
      `(#:cargo-inputs
