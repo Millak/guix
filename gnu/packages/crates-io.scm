de5dae24ae052f7a601b064f4eaedc07745c1036
Efraim Flashner <efraim@flashner.co.il>
2023-10-24 09:10:36 +0300

    gnu: Add rust-prodash-25.
    * gnu/packages/crates-io.scm (rust-prodash-25): New variable.
    
    Change-Id: I22799b1b8b222fed0568bfe138af354b4f7f0929

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -53650,6 +53650,59 @@ macro use case.")
 @code{proc_macro_derive} pretend to be @code{proc_macro}.")
     (license (list license:expat license:asl2.0))))
 
+(define-public rust-prodash-25
+  (package
+    (name "rust-prodash")
+    (version "25.0.2")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "prodash" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1xva91iks58431asnsx3lfqyr4z4fvvkrc2b582llalr411fnrqx"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs
+       (("rust-async-io" ,rust-async-io-1)
+        ("rust-bytesize" ,rust-bytesize-1)
+        ("rust-compound-duration" ,rust-compound-duration-1)
+        ("rust-crosstermion" ,rust-crosstermion-0.11)
+        ("rust-ctrlc" ,rust-ctrlc-3)
+        ("rust-dashmap" ,rust-dashmap-5)
+        ("rust-futures-core" ,rust-futures-core-0.3)
+        ("rust-futures-lite" ,rust-futures-lite-1)
+        ("rust-human-format" ,rust-human-format-1)
+        ("rust-humantime" ,rust-humantime-2)
+        ("rust-is-terminal" ,rust-is-terminal-0.4)
+        ("rust-log" ,rust-log-0.4)
+        ("rust-parking-lot" ,rust-parking-lot-0.12)
+        ("rust-ratatui" ,rust-ratatui-0.20)
+        ("rust-signal-hook" ,rust-signal-hook-0.3)
+        ("rust-time" ,rust-time-0.3)
+        ("rust-tui-react" ,rust-tui-react-0.20)
+        ("rust-unicode-segmentation" ,rust-unicode-segmentation-1)
+        ("rust-unicode-width" ,rust-unicode-width-0.1))
+       #:cargo-development-inputs
+       (("rust-argh" ,rust-argh-0.1)
+        ("rust-async-executor" ,rust-async-executor-1)
+        ("rust-async-io" ,rust-async-io-1)
+        ("rust-blocking" ,rust-blocking-1)
+        ("rust-criterion" ,rust-criterion-0.5)
+        ("rust-env-logger" ,rust-env-logger-0.10)
+        ("rust-futures" ,rust-futures-0.3)
+        ("rust-futures-util" ,rust-futures-util-0.3)
+        ("rust-is-terminal" ,rust-is-terminal-0.4)
+        ("rust-once-cell" ,rust-once-cell-1)
+        ("rust-rand" ,rust-rand-0.8))))
+    (home-page "https://github.com/Byron/prodash")
+    (synopsis
+     "Dashboard for visualizing progress of asynchronous and possibly blocking tasks")
+    (description
+     "This package provides a dashboard for visualizing progress of asynchronous and
+possibly blocking tasks.")
+    (license license:expat)))
+
 (define-public rust-progress-0.2
   (package
     (name "rust-progress")
