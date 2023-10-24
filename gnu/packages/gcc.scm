1487b3f53c3426f634f286b3493197b4e9993176
Greg Hogan <code@greghogan.com>
2023-10-24 15:29:27 +0000

    gnu: gcc@11: Update to 11.4.0.
    * gnu/packages/gcc.scm (gcc-11): Update to 11.4.0.
    Remove ‘gcc-10-tree-sra-union-handling.patch’.
    
    Change-Id: Iff8c9151326c87cb13db9c5b1a00c44b8eef95e3
    Co-authored-by: Janneke Nieuwenhuizen <janneke@gnu.org>
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/packages/gcc.scm
+++ gnu/packages/gcc.scm
@@ -731,17 +731,16 @@ It also includes runtime support libraries for these languages.")
 (define-public gcc-11
   (package
    (inherit gcc-8)
-   (version "11.3.0")
+   (version "11.4.0")
    (source (origin
             (method url-fetch)
             (uri (string-append "mirror://gnu/gcc/gcc-"
                                 version "/gcc-" version ".tar.xz"))
             (sha256
              (base32
-              "0fdclcwf728wbq52vphfcjywzhpsjp3kifzj3pib3xcihs0z4z5l"))
+              "1ncd7akww0hl5kkmw1dj3qgqp3phdrr5dfnm7jia9s07n0ib4b9z"))
             (patches (search-patches "gcc-9-strmov-store-file-names.patch"
-                                     "gcc-5.0-libvtv-runpath.patch"
-                                     "gcc-10-tree-sra-union-handling.patch"))
+                                     "gcc-5.0-libvtv-runpath.patch"))
             (modules '((guix build utils)))
             (snippet gcc-canadian-cross-objdump-snippet)))
    (arguments
