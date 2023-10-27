817bbdfb1c59e70eced587d2af377aad0ce759aa
Efraim Flashner <efraim@flashner.co.il>
2023-10-27 20:40:46 +0300

    gnu: rust-color-print-proc-macro-0.3: Update to 0.3.5.
    * gnu/packages/crates-io.scm (rust-color-print-proc-macro-0.3): Update
    to 0.3.5.
    
    Change-Id: I27ad62dc6c16ead5aa4caf6eef7b6922cdf3d5c8

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -14229,14 +14229,14 @@ sets of intervals.")
 (define-public rust-color-print-proc-macro-0.3
   (package
     (name "rust-color-print-proc-macro")
-    (version "0.3.4")
+    (version "0.3.5")
     (source (origin
               (method url-fetch)
               (uri (crate-uri "color-print-proc-macro" version))
               (file-name (string-append name "-" version ".tar.gz"))
               (sha256
                (base32
-                "0jsijb8b1hiyrmz6hpgcn8544w7ijp00prsfycgjsgfp6yjyl6ym"))))
+                "0gq00lg9irnzag890m85w3j1p8rpmsns2fa48051swka8mk7iqsp"))))
     (build-system cargo-build-system)
     (arguments
      `(#:cargo-inputs
@@ -14246,7 +14246,7 @@ sets of intervals.")
         ("rust-syn" ,rust-syn-1))))
     (home-page "https://gitlab.com/yolenoyer/color-print")
     (synopsis "Implementation for the package color-print")
-    (description "Implementation for the package color-print")
+    (description "Implementation for the package color-print.")
     (license (list license:expat license:asl2.0))))
 
 (define-public rust-color-quant-1
