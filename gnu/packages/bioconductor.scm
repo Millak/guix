f2ab2bba68d9cde0ed3473a385a872a17ddd06f1
Ricardo Wurmus <rekado@elephly.net>
2023-11-03 17:19:08 +0100

    gnu: r-biocio: Update to 1.12.0.
    * gnu/packages/bioconductor.scm (r-biocio): Update to 1.12.0.
    
    Change-Id: Ie6e5aee0c1a0c122f574d801e3268b460f89be2f

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -21276,14 +21276,14 @@ the respective packages (such as e.g. @code{xcms}).")
 (define-public r-biocio
   (package
     (name "r-biocio")
-    (version "1.10.0")
+    (version "1.12.0")
     (source
       (origin
         (method url-fetch)
         (uri (bioconductor-uri "BiocIO" version))
         (sha256
           (base32
-            "03hslg7k6khchf54gmlbkvwbkfn5ppz0wp7lh75gsnr0licsjkwx"))))
+            "0bn2jknvj2ag1yv67kjqr7gsq771rmqv3my9njjkwcc9nzrxzhbp"))))
     (properties `((upstream-name . "BiocIO")))
     (build-system r-build-system)
     (propagated-inputs
