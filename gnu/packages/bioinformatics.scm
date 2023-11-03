4c17016019a1fdb2e49b944a594361d046bd1f00
Mădălin Ionel Patrașcu <madalinionel.patrascu@mdc-berlin.de>
2023-11-03 15:55:57 +0100

    gnu: Add r-ewastools.
    * gnu/packages/bioinformatics.scm (r-ewastools): New variable.
    
    Change-Id: I4117dbee188c7d9547fe63b2b89ec9258bbda60b
    Signed-off-by: Ricardo Wurmus <rekado@elephly.net>

--- gnu/packages/bioinformatics.scm
+++ gnu/packages/bioinformatics.scm
@@ -876,6 +876,38 @@ attributes of microbiome data - zero-inflation and over-dispersion, are
 simultaneously considered.")
       (license license:gpl3))))
 
+(define-public r-ewastools
+  (let ((commit "f7646cacd73266708479b3fea5d625054d179f95")
+        (revision "1"))
+    (package
+      (name "r-ewastools")
+      (version (git-version "1.7.2" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/hhhh5/ewastools/")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "0irarlnxfnasa755adxsn67rxsy01zwhjhw18g4cag08cqiyyw41"))))
+      (properties `((upstream-name . "ewastools")))
+      (build-system r-build-system)
+      (propagated-inputs
+       (list r-data-table
+             r-igraph
+             r-illuminaio
+             r-mblm
+             r-quadprog))
+      (native-inputs (list r-knitr))
+      (home-page "https://github.com/hhhh5/ewastools/")
+      (synopsis
+       "Quality control toolset for the Illumina Infinium DNA methylation")
+      (description
+       "This package provides a collection of useful functions for working
+with DNA methylation micro-array data.")
+      (license license:unlicense))))
+
 (define-public r-numbat
   (let ((commit "4ab7752e7d267a3f443756675728521a9b0a7295")
         (revision "1"))
