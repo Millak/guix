268cf54fbf8840319e8dd666dbc2baff0b2ee357
Ricardo Wurmus <rekado@elephly.net>
2023-11-03 16:40:28 +0100

    gnu: r-barcodetrackr: Update to 1.10.0.
    * gnu/packages/bioconductor.scm (r-barcodetrackr): Update to 1.10.0.
    
    Change-Id: Ia3ad0d4069441dd9a0b6a93f26e4791eae3ab408

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -4323,13 +4323,13 @@ estimates for each correlation element.")
 (define-public r-barcodetrackr
   (package
     (name "r-barcodetrackr")
-    (version "1.8.0")
+    (version "1.10.0")
     (source (origin
               (method url-fetch)
               (uri (bioconductor-uri "barcodetrackR" version))
               (sha256
                (base32
-                "155lxqqq2mgrz9i04xvv8y7gh2iacw6qd3mzijzbvrbm605qb3p8"))))
+                "1w5p6dqagf3g27ymqsxdim0qhnwm11rrs3nnpp4mj8jcxm8wjsh9"))))
     (properties `((upstream-name . "barcodetrackR")))
     (build-system r-build-system)
     (propagated-inputs
