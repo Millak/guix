1672a49c4e2b3664efea9fbf1bddf760db763b71
Ricardo Wurmus <rekado@elephly.net>
2023-11-03 17:18:43 +0100

    gnu: r-metapod: Update to 1.10.0.
    * gnu/packages/bioconductor.scm (r-metapod): Update to 1.10.0.
    
    Change-Id: I6ae84bb0ab8e68833206b214046a0144f60ae3cb

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -12920,14 +12920,14 @@ data.")
 (define-public r-metapod
   (package
     (name "r-metapod")
-    (version "1.8.0")
+    (version "1.10.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "metapod" version))
        (sha256
         (base32
-         "0lr2sgbgdbqq626nsm3vyhghs8nalg5cxycr4cwsqh9nqrmip3wa"))))
+         "1nhxwj6gwc3hqji7icp1q6n0hj1gnvv1y5zhd2myhm7kj3sic2qc"))))
     (properties `((upstream-name . "metapod")))
     (build-system r-build-system)
     (propagated-inputs
