2fa21de9c2bd52858c9187e2f4d556e76381860e
Ricardo Wurmus <rekado@elephly.net>
2023-11-03 17:19:01 +0100

    gnu: r-acde: Update to 1.32.0.
    * gnu/packages/bioconductor.scm (r-acde): Update to 1.32.0.
    
    Change-Id: I2f7fd79361be6326b307fbb7d3ef4c7bf6a585ed

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -18760,13 +18760,13 @@ on whole-genome tiling array experiments quite easily with enough memory.")
 (define-public r-acde
   (package
     (name "r-acde")
-    (version "1.30.0")
+    (version "1.32.0")
     (source (origin
               (method url-fetch)
               (uri (bioconductor-uri "acde" version))
               (sha256
                (base32
-                "088dq6mc9m1bx0dprcz8cdpfp447lnxq7420r139mhcik0z21vky"))))
+                "1rqyjj4lgqxs372qx8p3qx2napww0djh4jz8hzgv86x9r202kk9p"))))
     (properties `((upstream-name . "acde")))
     (build-system r-build-system)
     (propagated-inputs (list r-boot))
