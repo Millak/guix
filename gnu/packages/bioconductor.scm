31fef7c33129be03596bd48ba127bf4a05254ea7
Ricardo Wurmus <rekado@elephly.net>
2023-11-03 17:19:11 +0100

    gnu: r-minet: Update to 3.60.0.
    * gnu/packages/bioconductor.scm (r-minet): Update to 3.60.0.
    
    Change-Id: I4944d2e34aacc6d3ba8c5a2d2176ecf7b33ca6b8

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -22320,14 +22320,14 @@ mappings functionalities.")
 (define-public r-minet
   (package
     (name "r-minet")
-    (version "3.58.0")
+    (version "3.60.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "minet" version))
        (sha256
         (base32
-         "04ll8nqhmyj2v12n2xskia8dh5px8mzj8fbyw1pzahk9vp085gkr"))))
+         "17v2x7cyw74h0zfsz33pyl1kqk976pdzw7mrgprz9l7w5120niw5"))))
     (properties `((upstream-name . "minet")))
     (build-system r-build-system)
     (propagated-inputs
