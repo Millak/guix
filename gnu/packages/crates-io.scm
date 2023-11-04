ad3c174f39885ffba2a6bbf65fe918535cccdf41
Efraim Flashner <efraim@flashner.co.il>
2023-11-04 22:08:46 +0200

    gnu: rust-notify-5-pre.13: Fix build.
    * gnu/packages/crates-io.scm (rust-notify-5-pre.13)[cargo-inputs]: Add
    rust-bitflags-1, rust-crossbeam-channel-0.5, rust-filetime-0.2,
    rust-fsevent-sys-4, rust-inotify-0.9, rust-kqueue-1, rust-libc-0.2,
    rust-mio-0.7, rust-serde-1, rust-walkdir-2, rust-winapi-0.3.
    [cargo-development-inputs]: Add rust-futures-0.3, rust-serde-json-1,
    rust-tempfile-3.
    
    Change-Id: Ic5d9eae1e7deae40004d1a9ce39b489145e97a3d

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -47377,7 +47377,23 @@ notification library.")
               (file-name (string-append name "-" version ".tar.gz"))
               (sha256
                (base32
-                "0za8mpacxkr62fii5h7ny4h396y0m8myd3hf08njqdg2h21kap94"))))))
+                "0za8mpacxkr62fii5h7ny4h396y0m8myd3hf08njqdg2h21kap94"))))
+    (arguments
+     `(#:cargo-inputs (("rust-bitflags" ,rust-bitflags-1)
+                       ("rust-crossbeam-channel" ,rust-crossbeam-channel-0.5)
+                       ("rust-filetime" ,rust-filetime-0.2)
+                       ("rust-fsevent-sys" ,rust-fsevent-sys-4)
+                       ("rust-inotify" ,rust-inotify-0.9)
+                       ("rust-kqueue" ,rust-kqueue-1)
+                       ("rust-kqueue" ,rust-kqueue-1)
+                       ("rust-libc" ,rust-libc-0.2)
+                       ("rust-mio" ,rust-mio-0.7)
+                       ("rust-serde" ,rust-serde-1)
+                       ("rust-walkdir" ,rust-walkdir-2)
+                       ("rust-winapi" ,rust-winapi-0.3))
+       #:cargo-development-inputs (("rust-futures" ,rust-futures-0.3)
+                                   ("rust-serde-json" ,rust-serde-json-1)
+                                   ("rust-tempfile" ,rust-tempfile-3))))))
 
 (define-public rust-notify-4
   (package
