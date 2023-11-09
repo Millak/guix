8691f7059908c06b27bf307b75ce6545f0db3ef3
Santiago Payà i Miralta <santiagopim@gmail.com>
2023-11-09 07:43:57 +0100

    gnu: Add font-iosevka-ss15.
    * gnu/packages/fonts.scm (font-iosevka-ss15): New variable.
    
    Change-Id: I36e15a7997a32e3aa1515c7252045f6cf2570b08
    Signed-off-by: 宋文武 <iyzsong@member.fsf.org>

--- gnu/packages/fonts.scm
+++ gnu/packages/fonts.scm
@@ -2263,6 +2263,20 @@ programming.  Iosevka is completely generated from its source code.")
        (sha256
         (base32 "1liapgr528qd88y6brhskcniddxanqqmx2qww21rqfyv9wl110wj"))))))
 
+(define-public font-iosevka-ss15
+  (package
+    (inherit font-iosevka)
+    (name "font-iosevka-ss15")
+    (version (package-version font-iosevka))
+    (source
+     (origin
+       (method url-fetch/zipbomb)
+       (uri (string-append "https://github.com/be5invis/Iosevka"
+                           "/releases/download/v" version
+                           "/ttc-iosevka-ss15-" version ".zip"))
+       (sha256
+        (base32 "19mhdl6dzb4003m00chnj9918l3mxrwfvfxh3wmvp6h4sfa6hymk"))))))
+
 (define-public font-aporetic
   (package
     (name "font-aporetic")
