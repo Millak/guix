9f884b18940fa2fcd6d121da8a1e92d7ed86b18d
Steve George <steve@futurile.net>
2023-11-09 19:38:29 +0000

    gnu: Add rust-gix-transport-0.37.
    * gnu/packages/crates-io.scm (rust-gix-transport-0.37): New variable.
    (rust-gix-transport-0.32): Inherit from rust-gix-transport-0.37.
    
    Change-Id: Ie355057b6e0d97167ed171e29e26a65a1067107a
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -32022,8 +32022,53 @@ tempfile capability with a global registry to assure clean-up.")
 can be turned off to zero cost.")
     (license (list license:expat license:asl2.0))))
 
+(define-public rust-gix-transport-0.37
+  (package
+    (name "rust-gix-transport")
+    (version "0.37.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "gix-transport" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0kpv0aqx7c3hbsc14rk7c01yd7b73mm14d4swnn6hpi4d9p75v5r"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs
+       (("rust-async-std" ,rust-async-std-1)
+        ("rust-async-trait" ,rust-async-trait-0.1)
+        ("rust-base64" ,rust-base64-0.21)
+        ("rust-bstr" ,rust-bstr-1)
+        ("rust-curl" ,rust-curl-0.4)
+        ("rust-document-features" ,rust-document-features-0.2)
+        ("rust-futures-io" ,rust-futures-io-0.3)
+        ("rust-futures-lite" ,rust-futures-lite-1)
+        ("rust-gix-command" ,rust-gix-command-0.2)
+        ("rust-gix-credentials" ,rust-gix-credentials-0.20)
+        ("rust-gix-features" ,rust-gix-features-0.35)
+        ("rust-gix-packetline" ,rust-gix-packetline-0.16)
+        ("rust-gix-quote" ,rust-gix-quote-0.4)
+        ("rust-gix-sec" ,rust-gix-sec-0.10)
+        ("rust-gix-url" ,rust-gix-url-0.24)
+        ("rust-pin-project-lite" ,rust-pin-project-lite-0.2)
+        ("rust-reqwest" ,rust-reqwest-0.11)
+        ("rust-serde" ,rust-serde-1)
+        ("rust-thiserror" ,rust-thiserror-1))
+       #:cargo-development-inputs
+       (("rust-async-std" ,rust-async-std-1)
+        ("rust-blocking" ,rust-blocking-1)
+        ("rust-maybe-async" ,rust-maybe-async-0.2))))
+    (home-page "https://github.com/Byron/gitoxide")
+    (synopsis "Implements the Git transport layer for Gitoxide")
+    (description
+     "This package is part of Gitoxide a Rust implementation of Git.  It
+provides an implementation of the Git transport layer.")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-gix-transport-0.32
   (package
+    (inherit rust-gix-transport-0.37)
     (name "rust-gix-transport")
     (version "0.32.0")
     (source
@@ -32033,7 +32078,6 @@ can be turned off to zero cost.")
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
         (base32 "05fzh609x4d6djizmrv5m98axinp2m5hcpk003bqw1x9v7z9z8v4"))))
-    (build-system cargo-build-system)
     (arguments
      `(#:cargo-inputs
        (("rust-async-std" ,rust-async-std-1)
@@ -32058,13 +32102,7 @@ can be turned off to zero cost.")
        #:cargo-development-inputs
        (("rust-async-std" ,rust-async-std-1)
         ("rust-blocking" ,rust-blocking-1)
-        ("rust-maybe-async" ,rust-maybe-async-0.2))))
-    (home-page "https://github.com/Byron/gitoxide")
-    (synopsis "Implement the git transport layer")
-    (description
-     "This package provides a crate of the gitoxide project dedicated to
-implementing the git transport layer.")
-    (license (list license:expat license:asl2.0))))
+        ("rust-maybe-async" ,rust-maybe-async-0.2))))))
 
 (define-public rust-gix-traverse-0.33
   (package
