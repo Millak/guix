fd7c535d1b3264c131e3cbfcfa2089e618d397cc
Ricardo Wurmus <rekado@elephly.net>
2023-11-15 15:54:41 +0100

    gnu: r-matrixstats: Update to 1.1.0.
    * gnu/packages/statistics.scm (r-matrixstats): Update to 1.1.0.
    
    Change-Id: I27f701d83b7dbfcc54fde2701890c69dbc5b9384

--- gnu/packages/statistics.scm
+++ gnu/packages/statistics.scm
@@ -4543,13 +4543,13 @@ t-probabilities, quantiles, random deviates and densities.")
 (define-public r-matrixstats
   (package
     (name "r-matrixstats")
-    (version "1.0.0")
+    (version "1.1.0")
     (source (origin
               (method url-fetch)
               (uri (cran-uri "matrixStats" version))
               (sha256
                (base32
-                "0vxd7g1fm4x0f72bs1xfik1d4yklvpjdyg1hpx86pl8fwr86i1wl"))))
+                "0h85hjvsmc8s3hyjdj83fykb2vl8jc7pb9ynp2xsl0q9v1sihrxl"))))
     (properties `((upstream-name . "matrixStats")))
     (build-system r-build-system)
     (arguments
