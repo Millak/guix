d8943a3ddd4f3ab29e72d8d375895145f81ab815
Ricardo Wurmus <rekado@elephly.net>
2023-11-15 15:54:36 +0100

    gnu: r-nor1mix: Update to 1.3-2.
    * gnu/packages/cran.scm (r-nor1mix): Update to 1.3-2.
    
    Change-Id: I64511ea1988a3ee7e3616a4bcd1f01347e151ba7

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -21979,14 +21979,14 @@ numbers from ZIP and ZINB distributions.")
 (define-public r-nor1mix
   (package
     (name "r-nor1mix")
-    (version "1.3-0")
+    (version "1.3-2")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "nor1mix" version))
        (sha256
         (base32
-         "1817wcvlmxs70vs4db0jkxd7i037744zz8ay3c2a9949z29fxr4w"))))
+         "0252kqhdsp374isc59pm1c6dmsb16jibpnsp1bpyh4k5mg73cdj6"))))
     (build-system r-build-system)
     (home-page "https://cran.r-project.org/web/packages/nor1mix/")
     (synopsis "Normal (1-d) mixture models")
