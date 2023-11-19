3778f778c37110460dd78088200cbd05eb0c49e4
Tobias Geerinckx-Rice <me@tobias.gr>
2023-11-19 01:00:00 +0100

    gnu: fio: Update to 3.36.
    * gnu/packages/benchmark.scm (fio): Update to 3.36.
    
    Change-Id: I2a58cf03e51ff9ecbea3cd540bb2dde40b6bac1c

--- gnu/packages/benchmark.scm
+++ gnu/packages/benchmark.scm
@@ -79,14 +79,14 @@
 (define-public fio
   (package
     (name "fio")
-    (version "3.35")
+    (version "3.36")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://brick.kernel.dk/snaps/"
                                   "fio-" version ".tar.bz2"))
               (sha256
                (base32
-                "0dvxv771hzb72zs995wsq3i1kryv8vfzkndd79i0w2v7ssxnldb3"))))
+                "0ppg2rn57diz2mvbbps4cjxd903zn380hdkdsrbzal4l513w32h0"))))
     (build-system gnu-build-system)
     (arguments
      (list #:modules
