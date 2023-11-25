Millak/guix

commit 189525412e3d803f3f77e15ec4a62aaa57f65a2d

guix: Show better progress bars.
Style provides information on the characters to use before and after the
progress bar content (`[` and `]` for the ascii style), as well as the
character for filled step (`#` for ascii style).  When supported, it
provides intermediate steps.  This is used for unicode style, to show
better precision.

* guix/progress.scm (<progress-bar-style>): New record type.
(ascii-bar-style, unicode-bar-style): New variables.
(progress-bar): Draw progress depending on style.  When supported, use
unicode style.  Fall back to ascii style.

guix/progress.scm
@@ -166,16 +166,47 @@ INTERVAL (a time-duration object), otherwise does nothing and returns #f."
   ;; Number of columns of the terminal.
   (make-parameter 80))
 
+(define-record-type* <progress-bar-style>
+  progress-bar-style make-progress-bar-style progress-bar-style?
+  (start  progress-bar-style-start)
+  (stop   progress-bar-style-stop)
+  (filled progress-bar-style-filled)
+  (steps  progress-bar-style-steps))
+
+(define ascii-bar-style
+  (progress-bar-style
+    (start #\[)
+    (stop #\])
+    (filled #\#)
+    (steps '())))
+
+(define unicode-bar-style
+  (progress-bar-style
+    (start #\x2595)
+    (stop #\x258f)
+    (filled #\x2588)
+    (steps '(#\x258F #\x258E #\x258D #\x258C #\x258B #\x258A #\x2589))))
+
 (define* (progress-bar % #:optional (bar-width 20))
   "Return % as a string representing an ASCII-art progress bar.  The total
 width of the bar is BAR-WIDTH."
-  (let* ((bar-width (max 3 (- bar-width 2)))
-         (fraction (/ % 100))
-         (filled   (inexact->exact (floor (* fraction bar-width))))
-         (empty    (- bar-width filled)))
-    (format #f "[~a~a]"
-            (make-string filled #\#)
-            (make-string empty #\space))))
+  (let* ((bar-style (if (equal? (port-encoding (current-output-port)) "UTF-8")
+                        unicode-bar-style
+                        ascii-bar-style))
+         (bar-width (max 3 (- bar-width 2)))
+         (intermediates (+ (length (progress-bar-style-steps bar-style)) 1))
+         (step     (inexact->exact (floor (/ (* % bar-width intermediates) 100))))
+         (filled   (quotient step intermediates))
+         (intermediate
+           (list-ref (cons #f (progress-bar-style-steps bar-style))
+                     (modulo step intermediates)))
+         (empty    (- bar-width filled (if intermediate 1 0))))
+    (simple-format #f "~a~a~a~a~a"
+                   (string (progress-bar-style-start bar-style))
+                   (make-string filled (progress-bar-style-filled bar-style))
+                   (if intermediate (string intermediate) "")
+                   (make-string empty #\space)
+                   (string (progress-bar-style-stop bar-style)))))
 
 (define (erase-current-line port)
   "Write an ANSI erase-current-line sequence to PORT to erase the whole line and

commit 9b48cf8cdd5718bc35828b3c4c9abe84122dad96

tests: Import (guix build syscalls) when (guix build store-copy) is used.
Fixes a test failure introduced in
189525412e3d803f3f77e15ec4a62aaa57f65a2d.

* guix/progress.scm: Autoload (guix build syscalls).
* tests/gexp.scm ("gexp->derivation, store copy"): Add (guix build
syscalls) to the list of imported modules.  Use ‘with-imported-modules’
rather than #:modules.

Change-Id: I8d3fe90f564ef4b1a340f34cee6c08a741f7b836

guix/progress.scm
@@ -21,8 +21,7 @@
 
 (define-module (guix progress)
   #:use-module (guix records)
-  #:use-module ((guix build syscalls)
-                #:select (terminal-string-width))
+  #:autoload   (guix build syscalls) (terminal-string-width)
   #:use-module (srfi srfi-19)
   #:use-module (rnrs io ports)
   #:use-module (rnrs bytevectors)

tests/gexp.scm
@@ -1,5 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2014-2022 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2014-2023 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2021-2022 Maxime Devos <maximedevos@telenet.be>
 ;;;
 ;;; This file is part of GNU Guix.
@@ -826,38 +826,39 @@
                          (call-with-output-file (string-append #$output "/two")
                            (lambda (port)
                              (display "This is the second one." port))))))
-        (build-drv #~(begin
-                       (use-modules (guix build store-copy)
-                                    (guix build utils)
-                                    (srfi srfi-1))
+        (build-drv
+         (with-imported-modules '((guix build store-copy)
+                                  (guix build syscalls)
+                                  (guix progress)
+                                  (guix records)
+                                  (guix sets)
+                                  (guix build utils))
+           #~(begin
+               (use-modules (guix build store-copy)
+                            (guix build utils)
+                            (srfi srfi-1))
 
-                       (define (canonical-file? file)
-                         ;; Copied from (guix tests).
-                         (let ((st (lstat file)))
-                           (or (not (string-prefix? (%store-directory) file))
-                               (eq? 'symlink (stat:type st))
-                               (and (= 1 (stat:mtime st))
-                                    (zero? (logand #o222 (stat:mode st)))))))
+               (define (canonical-file? file)
+                 ;; Copied from (guix tests).
+                 (let ((st (lstat file)))
+                   (or (not (string-prefix? (%store-directory) file))
+                       (eq? 'symlink (stat:type st))
+                       (and (= 1 (stat:mtime st))
+                            (zero? (logand #o222 (stat:mode st)))))))
 
-                       (mkdir #$output)
-                       (populate-store '("graph") #$output
-                                       #:deduplicate? #f)
+               (mkdir #$output)
+               (populate-store '("graph") #$output
+                               #:deduplicate? #f)
 
-                       ;; Check whether 'populate-store' canonicalizes
-                       ;; permissions and timestamps.
-                       (unless (every canonical-file? (find-files #$output))
-                         (error "not canonical!" #$output)))))
+               ;; Check whether 'populate-store' canonicalizes
+               ;; permissions and timestamps.
+               (unless (every canonical-file? (find-files #$output))
+                 (error "not canonical!" #$output))))))
     (mlet* %store-monad ((one (gexp->derivation "one" build-one))
                          (two (gexp->derivation "two" (build-two one)))
                          (drv (gexp->derivation "store-copy" build-drv
                                                 #:references-graphs
-                                                `(("graph" ,two))
-                                                #:modules
-                                                '((guix build store-copy)
-                                                  (guix progress)
-                                                  (guix records)
-                                                  (guix sets)
-                                                  (guix build utils))))
+                                                `(("graph" ,two))))
                          (ok? (built-derivations (list drv)))
                          (out -> (derivation->output-path drv)))
       (let ((one (derivation->output-path one))