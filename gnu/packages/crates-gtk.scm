58b6ab04dbbf5500a4035d2f346152ca874aeef1
Sergio Pastor Pérez <sergio.pastorperez@outlook.es>
2023-11-28 22:13:01 +0100

    gnu: Add rust-gdk4-0.7.
    * gnu/packages/crates-gtk.scm (rust-gdk4-0.7): New variable.
    
    Change-Id: Ie4a07597abb084456809fa2d7746942a3ea5bd74
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/crates-gtk.scm
+++ gnu/packages/crates-gtk.scm
@@ -943,6 +943,37 @@
        (("rust-shell-words" ,rust-shell-words-0.1)
         ("rust-tempfile" ,rust-tempfile-3))))))
 
+(define-public rust-gdk4-0.7
+  (package
+    (name "rust-gdk4")
+    (version "0.7.3")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "gdk4" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1xiacc63p73apr033gjrb9dsk0y4yxnsljwfxbwfry41snd03nvy"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:tests? #f ; `Errors` doesn't implement `std::fmt::Display`
+       #:cargo-inputs (("rust-cairo-rs" ,rust-cairo-rs-0.18)
+                       ("rust-gdk-pixbuf" ,rust-gdk-pixbuf-0.18)
+                       ("rust-gdk4-sys" ,rust-gdk4-sys-0.7)
+                       ("rust-gio" ,rust-gio-0.18)
+                       ("rust-gl" ,rust-gl-0.14)
+                       ("rust-glib" ,rust-glib-0.18)
+                       ("rust-libc" ,rust-libc-0.2)
+                       ("rust-pango" ,rust-pango-0.18))
+       #:cargo-development-inputs
+       (("rust-gir-format-check" ,rust-gir-format-check-0.1))))
+    (native-inputs (list pkg-config))
+    (inputs (list cairo gdk-pixbuf gtk))
+    (home-page "https://gtk-rs.org/")
+    (synopsis "Rust bindings of the GDK 4 library")
+    (description "This package provides rust bindings of the GDK 4 library.")
+    (license license:expat)))
+
 (define-public rust-gdk4-sys-0.7
   (package
     (name "rust-gdk4-sys")
