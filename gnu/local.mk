d14389f10ec8c7e4c0a91c1c5094befc4445ddf0
Sharlatan Hellseher <sharlatanus@gmail.com>
2023-11-29 22:34:39 +0000

    gnu: Add (gnu packages golang-web) module.
    * gnu/packages/golang-web.scm: New file.
    * gnu/local.mk: Add to gnu/packages/golang-web.scm scope.

--- gnu/local.mk
+++ gnu/local.mk
@@ -306,6 +306,7 @@ GNU_SYSTEM_MODULES =				\
   %D%/packages/gobby.scm			\
   %D%/packages/golang.scm			\
   %D%/packages/golang-check.scm		\
+  %D%/packages/golang-web.scm	        	\
   %D%/packages/gperf.scm			\
   %D%/packages/gpodder.scm			\
   %D%/packages/gps.scm				\
