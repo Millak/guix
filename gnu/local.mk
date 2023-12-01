1b2505217cf222d98cc960b8510660976a01cfa1
Philip McGrath <philip@philipmcgrath.com>
2023-12-01 10:35:38 -0500

    gnu: racket: Update to 8.11.1.
    * gnu/packages/patches/racket-backport-8.10-rktboot.patch: Delete file.
    * gnu/packages/patches/racket-backport-8.11-layered-docs.patch: New file.
    * gnu/local.mk (dist_patch_DATA): Update accordingly.
    * gnu/packages/racket/scm (%racket-origin)[patches]: Likewise.
    (%racket-version): Update to 8.11.1.
    (%zuo-version): Update to 1.7.
    (racket)[inputs]<deinprogramm, draw, drracket, games, gui, htdp>
    <parser-tools, plot, rackunit, redex, scribble, slideshow, typed-racket>
    <srfi, string-constants, syntax-color, web-server, xrepl>: Update hashes.
    <quickscript>: Likewise, and add snippet.
    * gnu/packages/chez.scm (chez-scheme-for-racket): Update to
    9.9.9-pre-release.18.
    
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/local.mk
+++ gnu/local.mk
@@ -1942,7 +1942,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/rpcbind-CVE-2017-8779.patch		\
   %D%/packages/patches/rtags-separate-rct.patch			\
   %D%/packages/patches/racket-chez-scheme-bin-sh.patch		\
-  %D%/packages/patches/racket-backport-8.10-rktboot.patch	\
+  %D%/packages/patches/racket-backport-8.11-layered-docs.patch	\
   %D%/packages/patches/racket-rktio-bin-sh.patch		\
   %D%/packages/patches/racket-zuo-bin-sh.patch			\
   %D%/packages/patches/remake-impure-dirs.patch			\
