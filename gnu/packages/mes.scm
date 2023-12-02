a84bb5e60033c3b38ef88786ace79e49df848483
Janneke Nieuwenhuizen <janneke@gnu.org>
2023-12-02 09:12:14 +0100

    gnu: mescc-tools: Update to 1.5.2.
    * guix/git/mes.scm (mescc-tools): Update to 1.5.2.
    (arguments): Remove `patch-Kaem/test.sh' phase.
    
    Change-Id: I47d7f3159a1e6395ffbb9141147c9cbf0676f22b

--- gnu/packages/mes.scm
+++ gnu/packages/mes.scm
@@ -213,14 +213,14 @@ Guile.")
 (define-public mescc-tools
   (package
     (name "mescc-tools")
-    (version "1.5.0")
+    (version "1.5.2")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://savannah/" name "/"
                                   name "-" version ".tar.gz"))
               (sha256
                (base32
-                "1vjczlajyrbjcx9ld35vhdqbxfdwwy3axg0jray3iwnrf70qr700"))))
+                "1jak61gxab8bj8ddpgwfn9lqs917szq1phadmg8y5cjsndn1hv4k"))))
     (build-system gnu-build-system)
     (supported-systems '("i686-linux" "x86_64-linux"
                          "armhf-linux" "aarch64-linux"
@@ -230,11 +230,7 @@ Guile.")
       #:make-flags #~(list (string-append "PREFIX=" #$output))
       #:test-target "test"
       #:phases #~(modify-phases %standard-phases
-                   (delete 'configure)
-                   (add-after 'unpack 'patch-Kaem/test.sh
-                     (lambda _
-                       (substitute* "Kaem/test.sh"
-                         (("#/usr/") "#! /usr")))))))
+                   (delete 'configure))))
     (native-inputs (list which))
     (synopsis "Tools for the full source bootstrapping process")
     (description
