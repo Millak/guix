2e11caff39d502775d7c652972a441f5da3d9eec
Michael Ford <fanquake@gmail.com>
2023-12-05 10:49:29 +0000

    gnu: LLVM, Clang, libomp, lld: Update to 17.0.6.
    * gnu/packages/llvm.scm (llvm-17, clang-17, libomp-17, lld-17):
    Update to 17.0.6.
    
    Signed-off-by: Hilton Chain <hako@ultrarare.space>
    Change-Id: I7757b8e55e6a7ffc8185d366ba77706abaf5a798

--- gnu/packages/llvm.scm
+++ gnu/packages/llvm.scm
@@ -603,13 +603,13 @@ output), and Binutils.")
   '(("14.0.6" . "14f8nlvnmdkp9a9a79wv67jbmafvabczhah8rwnqrgd5g3hfxxxx")
     ("15.0.7" . "12sggw15sxq1krh1mfk3c1f07h895jlxbcifpwk3pznh4m1rjfy2")
     ("16.0.6" . "0jxmapg7shwkl88m4mqgfjv4ziqdmnppxhjz6vz51ycp2x4nmjky")
-    ("17.0.5" . "149flpr96vcn7a1ckya6mm93m9yp85l47w156fjd0r99ydxrw5kv")))
+    ("17.0.6" . "1a7rq3rgw5vxm8y39fyzr4kv7w97lli4a0c1qrkchwk8p0n07hgh")))
 
 (define %llvm-patches
   '(("14.0.6" . ("clang-14.0-libc-search-path.patch"))
     ("15.0.7" . ("clang-15.0-libc-search-path.patch"))
     ("16.0.6" . ("clang-16.0-libc-search-path.patch"))
-    ("17.0.5" . ("clang-17.0-libc-search-path.patch"))))
+    ("17.0.6" . ("clang-17.0-libc-search-path.patch"))))
 
 (define (llvm-monorepo version)
   (origin
@@ -1502,7 +1502,7 @@ Library.")
 (define-public llvm-17
   (package
     (inherit llvm-15)
-    (version "17.0.5")
+    (version "17.0.6")
     (source (llvm-monorepo version))))
 
 (define-public clang-runtime-17
@@ -1518,7 +1518,7 @@ Library.")
                     (package-version llvm-17)))
      (sha256
       (base32
-       "12dbp10bhq25a44qnvz978mf9y6pdycwpp7sgq8a93by0fpgb72r")))))
+       "1f8szx762c325916gjxb5lw7zxyidynwnvx6fxxqscsx8514cxxa")))))
 
 (define-public libomp-17
   (package
