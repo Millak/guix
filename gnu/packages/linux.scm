e3ac66bd8931250b15da40ec6a6ed07b89ad470b
Leo Famulari <leo@famulari.name>
2023-12-08 18:29:35 -0500

    gnu: linux-libre: Update to 6.6.5.
    * gnu/packages/linux.scm (linux-libre-6.6-version): Update to 6.6.5.
    (linux-libre-6.6-pristine-source): Update hash.
    
    Change-Id: I4c3e3923da171ff7edff412dd59f899edbd9ddb0

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -493,7 +493,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
 ;; The current "stable" kernels. That is, the most recently released major
 ;; versions that are still supported upstream.
 
-(define-public linux-libre-6.6-version "6.6.3")
+(define-public linux-libre-6.6-version "6.6.5")
 (define-public linux-libre-6.6-gnu-revision "gnu")
 (define deblob-scripts-6.6
   (linux-libre-deblob-scripts
@@ -503,7 +503,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
    (base32 "1hg3ck1j8288fhlhcvhgs1zzwh3i62nfvphw7x3vsaqr75kiwbjp")))
 (define-public linux-libre-6.6-pristine-source
   (let ((version linux-libre-6.6-version)
-        (hash (base32 "19wmjkyyv5glv1w647qisrv829hhhpba5x905a7p7kch9wyzrv98")))
+        (hash (base32 "17miac3h4kvj4yyf042qsmpsivpq243db5v0ay6233d6aic7k4kw")))
    (make-linux-libre-source version
                             (%upstream-linux-source version hash)
                             deblob-scripts-6.6)))
