8e0847e038c4bb68f2c23fc6d189f07f38d1e8e9
Hilton Chain <hako@ultrarare.space>
2023-12-11 22:34:16 +0800

    gnu: linux-libre 6.1: Update to 6.1.67.
    This kernel release fixes WiFi regressions introduced in 6.1.66:
    
    https://lore.kernel.org/lkml/2023121147-turban-polish-002d@gregkh/T/#t
    https://cdn.kernel.org/pub/linux/kernel/v6.x/ChangeLog-6.1.67
    
    * gnu/packages/linux.scm (linux-libre-6.1-version): Update to 6.1.67.
    (linux-libre-6.1-pristine-source): Update hash.
    
    Change-Id: Id2ef8f7f5d8c0af19284a3e6cd1283e029b3eb38

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -526,7 +526,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
 ;; The "longterm" kernels — the older releases with long-term upstream support.
 ;; Here are the support timelines:
 ;; <https://www.kernel.org/category/releases.html>
-(define-public linux-libre-6.1-version "6.1.66")
+(define-public linux-libre-6.1-version "6.1.67")
 (define-public linux-libre-6.1-gnu-revision "gnu")
 (define deblob-scripts-6.1
   (linux-libre-deblob-scripts
@@ -536,7 +536,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
    (base32 "1hdibv43xbn1lv83i6qjgfmf1bvqxvq17fryfsq4r4sjgs9212js")))
 (define-public linux-libre-6.1-pristine-source
   (let ((version linux-libre-6.1-version)
-        (hash (base32 "030sxwzqlf9jg57j1hvd46ffkc9yfplbk3b81faycfa2dk6n57j1")))
+        (hash (base32 "11cjqll3b7iq3mblwyzjrd5ph8avgk23f4mw4shm8j6ai5rdndvm")))
    (make-linux-libre-source version
                             (%upstream-linux-source version hash)
                             deblob-scripts-6.1)))
