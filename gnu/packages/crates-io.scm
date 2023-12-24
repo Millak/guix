85db3daf07329e2e667149c6fc2171ed4fffd8d6
Efraim Flashner <efraim@flashner.co.il>
2023-12-24 09:53:31 +0200

    gnu: Add rust-cookie-store-0.20.
    * gnu/packages/crates-io.scm (rust-cookie-store-0.20): New variable.
    (rust-cookie-store-0.19): Inherit from rust-cookie-store-0.20.
    
    Change-Id: Ia731a9b295e6926d60252ccce26b667b1f98b1fe

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -16573,8 +16573,37 @@ in @code{no_std}.  Alloc support is optional.")
          (base32 "1wzzy5iazdk5caadxvjfwrd312rbg7a55a1zpmsdrhk3kfpa77r3"))))
     (arguments `(#:cargo-inputs (("rust-memchr" ,rust-memchr-2))))))
 
+(define-public rust-cookie-store-0.20
+  (package
+    (name "rust-cookie-store")
+    (version "0.20.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "cookie_store" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1xkc7fl1jik9ki13j9pjgyw51d0qd613srz1lv1qb0blpjmn2x1q"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs (("rust-cookie" ,rust-cookie-0.17)
+                       ("rust-idna" ,rust-idna-0.3)
+                       ("rust-indexmap" ,rust-indexmap-1)
+                       ("rust-log" ,rust-log-0.4)
+                       ("rust-publicsuffix" ,rust-publicsuffix-2)
+                       ("rust-serde" ,rust-serde-1)
+                       ("rust-serde-derive" ,rust-serde-derive-1)
+                       ("rust-serde-json" ,rust-serde-json-1)
+                       ("rust-time" ,rust-time-0.3)
+                       ("rust-url" ,rust-url-2))))
+    (home-page "https://github.com/pfernie/cookie_store")
+    (synopsis "Cookie storage and retrieval")
+    (description "This package implements cookie storage and retrieval.")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-cookie-store-0.19
   (package
+    (inherit rust-cookie-store-0.20)
     (name "rust-cookie-store")
     (version "0.19.1")
     (source (origin
@@ -16584,7 +16613,6 @@ in @code{no_std}.  Alloc support is optional.")
               (sha256
                (base32
                 "19vy19rvp3a68sgxkrv6bxxlrr2ggqp0176yqb3zhmi0g4sqz8fm"))))
-    (build-system cargo-build-system)
     (arguments
      `(#:cargo-inputs
        (("rust-cookie" ,rust-cookie-0.16)
@@ -16596,11 +16624,7 @@ in @code{no_std}.  Alloc support is optional.")
         ("rust-serde-derive" ,rust-serde-derive-1)
         ("rust-serde-json" ,rust-serde-json-1)
         ("rust-time" ,rust-time-0.3)
-        ("rust-url" ,rust-url-2))))
-    (home-page "https://github.com/pfernie/cookie_store")
-    (synopsis "Cookie storage and retrieval")
-    (description "This package implements cookie storage and retrieval.")
-    (license (list license:expat license:asl2.0))))
+        ("rust-url" ,rust-url-2))))))
 
 (define-public rust-cookie-store-0.16
   (package
