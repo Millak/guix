5106fc899a5849102b2a9ac3e644a0090d4e635b
Ricardo Wurmus <rekado@elephly.net>
2023-12-28 00:44:41 +0100

    gnu: r-mgcv: Update to 1.9-1.
    * gnu/packages/statistics.scm (r-mgcv): Update to 1.9-1.
    
    Change-Id: I9275750902b6e2d2bd1c7394363bdbc66043c4de

--- gnu/packages/statistics.scm
+++ gnu/packages/statistics.scm
@@ -716,13 +716,13 @@ nonlinear mixed-effects models.")
 (define-public r-mgcv
   (package
    (name "r-mgcv")
-   (version "1.9-0")
+   (version "1.9-1")
    (source
     (origin
      (method url-fetch)
      (uri (cran-uri "mgcv" version))
      (sha256
-      (base32 "0w1v0hdswb332xz3br1fcgacib7ddr4hb96cmlycxcpqq5w01cdj"))))
+      (base32 "0cnvbdda243as2bxfsgnnk7xjmp1msgr9i4vbd84jfnxpqvvq3vh"))))
    (build-system r-build-system)
    (propagated-inputs
     (list r-matrix r-nlme))
