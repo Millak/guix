3bc167f42ed0924c51c2dc477cdce41a437ebc29
Efraim Flashner <efraim@flashner.co.il>
2024-01-02 09:44:24 +0200

    gnu: rust: Update to 1.75.0.
    * gnu/packages/rust.scm (rust-1.75): New variable.
    (rust): Update to 1.75.0.
    [arguments]: Adjust substitutions in custom phases for changes in source
    code.
    
    Change-Id: I0065d09a297e227e58998455bb90d18304fd6729

--- gnu/packages/rust.scm
+++ gnu/packages/rust.scm
@@ -828,6 +828,16 @@ safety and thread safety guarantees.")
                (("features = \\[\"fs\"" all)
                 (string-append all ", \"use-libc\""))))))))))
 
+(define rust-1.75
+  (let ((base-rust (rust-bootstrapped-package rust-1.74 "1.75.0"
+                    "1260mf3066ki6y55pvr35lnf54am6z96a3ap3hniwd4xpi2rywsv")))
+    (package
+      (inherit base-rust)
+      (source
+       (origin
+         (inherit (package-source base-rust))
+         (patches '()))))))
+
 (define (make-ignore-test-list strs)
   "Function to make creating a list to ignore tests a bit easier."
   (map (lambda (str)
@@ -842,7 +852,7 @@ safety and thread safety guarantees.")
 ;;; Here we take the latest included Rust, make it public, and re-enable tests
 ;;; and extra components such as rustfmt.
 (define-public rust
-  (let ((base-rust rust-1.74))
+  (let ((base-rust rust-1.75))
     (package
       (inherit base-rust)
       (properties (alist-delete 'hidden? (package-properties base-rust)))
@@ -982,7 +992,7 @@ safety and thread safety guarantees.")
                ;; different outputs while reusing the shared libraries.
                (lambda* (#:key outputs #:allow-other-keys)
                  (let ((out (assoc-ref outputs "out")))
-                   (substitute* "src/bootstrap/builder.rs"
+                   (substitute* "src/bootstrap/src/core/builder.rs"
                       ((" = rpath.*" all)
                        (string-append all
                                       "                "
@@ -1119,7 +1129,7 @@ exec -a \"$0\" \"~a\" \"$@\""
                          (("\\.ceil\\(\\)") ""))
                        ;; gcc doesn't recognize this flag.
                        (substitute*
-                         "compiler/rustc_target/src/spec/windows_gnullvm_base.rs"
+                         "compiler/rustc_target/src/spec/base/windows_gnullvm.rs"
                          ((", \"--unwindlib=none\"") "")))))
                  `())
              (replace 'set-env
