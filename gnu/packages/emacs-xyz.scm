49eef61db5969f9fc9beb807c910defc89be222e
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2024-01-04 11:46:37 +0100

    gnu: emacs-org-re-reveal: Update to 3.25.0.
    * gnu/packages/emacs-xyz.scm (emacs-org-re-reveal): Update to 3.25.0.
    
    Change-Id: Id3e3acaae62a4434c7a8354ae5d944268f8344f3

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -24783,7 +24783,7 @@ powerful Org contents.")
 (define-public emacs-org-re-reveal
   (package
     (name "emacs-org-re-reveal")
-    (version "3.24.2")
+    (version "3.25.0")
     (source
      (origin
        (method git-fetch)
@@ -24792,7 +24792,7 @@ powerful Org contents.")
              (commit version)))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "10x1cinn97wlm3dmv35dxrs78gfzgw59qf4j57m3vgss5q93mqq5"))))
+        (base32 "13q45c2a4y6pkaiixf931j4rq6sfpklxx854xwadik4a3mm06ipi"))))
     (build-system emacs-build-system)
     (propagated-inputs
      (list emacs-htmlize emacs-org))
