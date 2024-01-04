f39373c4550f888ae536e46716080067b8f314fd
Efraim Flashner <efraim@flashner.co.il>
2024-01-04 21:50:36 +0200

    gnu: rust-icrate-0.0.4: Move to (gnu packages crates-apple).
    * gnu/packages/crates-io.scm (rust-icrate-0.0.4): Move from here ...
    * gnu/packages/crates-apple.scm: ... to here.
    
    Change-Id: I567a637f64c4f5efc5f3cc73a28aee9f9a00c627

--- gnu/packages/crates-apple.scm
+++ gnu/packages/crates-apple.scm
@@ -723,6 +723,29 @@ macOS API for file changes notifications")
      `(#:skip-build? #t     ; only available on macOS
        #:cargo-inputs (("rust-libc" ,rust-libc-0.2))))))
 
+(define-public rust-icrate-0.0.4
+  (package
+    (name "rust-icrate")
+    (version "0.0.4")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "icrate" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "06d3g8n6xw3f0ai84mya0dlknp2n33zqdxpxp8272mslibzsmlwr"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs (("rust-block2" ,rust-block2-0.3)
+                       ("rust-dispatch" ,rust-dispatch-0.2)
+                       ("rust-objc2" ,rust-objc2-0.4))
+       #:cargo-development-inputs
+       (("rust-static-assertions" ,rust-static-assertions-1))))
+    (home-page "https://github.com/madsmtm/objc2")
+    (synopsis "Bindings to Apple's frameworks")
+    (description "This package provides bindings to Apple's frameworks.")
+    (license license:expat)))
+
 (define-public rust-objc-0.2
   (package
     (name "rust-objc")
