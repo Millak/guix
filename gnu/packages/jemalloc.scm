1807c9512d90f15785c90bd1bfa53560fbdfa98d
Roman Scherer <roman@burningswell.com>
2024-01-05 14:30:47 +0100

    gnu: jemalloc: Build with large page size.
    * gnu/packages/jemalloc.scm (jemalloc): Build with large page size.
    
    Change-Id: Ic813e7b0fe4c7ee79a1e703247abea77ad9d53f0
    Signed-off-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>

--- gnu/packages/jemalloc.scm
+++ gnu/packages/jemalloc.scm
@@ -62,6 +62,8 @@
       ;; https://github.com/jemalloc/jemalloc/issues/937
       #~'("--disable-initial-exec-tls"
           #$@(match (%current-system)
+               ("aarch64-linux"
+                (list "--with-lg-page=14"))
                ("powerpc-linux"
                 (list "CPPFLAGS=-maltivec"))
                (_
