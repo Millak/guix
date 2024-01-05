70a31359b2e82276e33b2b926ecec416cb04a926
Efraim Flashner <efraim@flashner.co.il>
2024-01-05 15:18:09 +0200

    gnu: rust-ci-info-0.3: Adjust tests.
    * gnu/packages/crates-io.scm (rust-ci-info-0.3)[arguments]: Add
    cargo-test-flags.
    
    Change-Id: I4bdbd6924c2d601eb1e7942ab55e2c6d3d8354a5

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -11358,7 +11358,13 @@ usage.")
          "00pr17g6q6i752acrkycg0hkq3lm0p634anm41g3m6lqg8q83s75"))))
     (build-system cargo-build-system)
     (arguments
-     `(#:cargo-inputs
+     `(#:cargo-test-flags
+       '("--release" "--"
+         "--skip=ci::ci_test::get_bamboo"
+         "--skip=ci::ci_test::get_taskcluster_runid"
+         "--skip=ci::ci_test::get_buildkite"
+         "--skip=ci::ci_test::get_tfs")
+       #:cargo-inputs
        (("rust-serde" ,rust-serde-1)
         ("rust-serde-derive" ,rust-serde-derive-1))))
     (home-page "https://github.com/sagiegurari/ci_info")
