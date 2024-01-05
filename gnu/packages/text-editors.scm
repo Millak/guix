d063e692e37ce74487b6545c2660a04f4fd29fab
Ludovic Courtès <ludo@gnu.org>
2024-01-05 17:56:22 +0100

    gnu: ed: Update to 1.19.
    * gnu/packages/text-editors.scm (ed): Update to 1.19.
    
    Change-Id: I69f4c9cbcf5c862cbffb6d82662c548591917d88

--- gnu/packages/text-editors.scm
+++ gnu/packages/text-editors.scm
@@ -115,14 +115,14 @@
 (define-public ed
   (package
     (name "ed")
-    (version "1.18")
+    (version "1.19")
     (source (origin
              (method url-fetch)
              (uri (string-append "mirror://gnu/ed/ed-"
                                  version ".tar.lz"))
              (sha256
               (base32
-               "0krb8rsb0cd8mgz0c5pqgnjbbrj7zjl7mf9099r8gi80k2nyza5c"))))
+               "0j6dfznfhll4afvrr714nrz0pp5zkcyvkb4xs2bam42789f2wbyf"))))
     (build-system gnu-build-system)
     (native-inputs (list lzip))
     (arguments
