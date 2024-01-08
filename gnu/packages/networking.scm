aa74e0bf17c052149baa6e030ed895239573ffd1
Tomas Volf <~@wolfsden.cz>
2024-01-08 20:38:16 +0100

    gnu: ngtcp2: Build libngtcp2_crypto_gnutls.
    * gnu/packages/networking.scm (ngtcp2)[arguments]<#:configure-flags>: Add
    --with-gnutls.
    [native-inputs]: Add pkg-config.
    [inputs]: Add gnutls.
    
    Change-Id: Id3a9210992d203a57a9b791f3c3752f2ebd3e634
    Signed-off-by: Mathieu Othacehe <othacehe@gnu.org>

--- gnu/packages/networking.scm
+++ gnu/packages/networking.scm
@@ -61,6 +61,7 @@
 ;;; Copyright © 2023 Yovan Naumovski <yovan@gorski.stream>
 ;;; Copyright © 2023 Zheng Junjie <873216071@qq.com>
 ;;; Copyright © 2023 Artyom V. Poptsov <poptsov.artyom@gmail.com>
+;;; Copyright © 2024 Tomas Volf <~@wolfsden.cz>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -4520,7 +4521,13 @@ network.")
        (sha256
         (base32 "1pppl6s25hz91w6321g1q7dqvfy4vccz9mmc5r8sfdvdc95fngl0"))))
     (build-system gnu-build-system)
-    (native-inputs (list cunit))
+    (arguments
+     (list
+      #:configure-flags
+      ;; openssl package does not support QUIC interface, so just gnutls
+      #~(list "--with-gnutls")))
+    (native-inputs (list cunit pkg-config))
+    (inputs (list gnutls))
     (home-page "https://nghttp2.org/ngtcp2/")
     (synopsis "QUIC protocol implementation")
     (description
