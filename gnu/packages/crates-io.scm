cbb6c33775885a6c57ccd683761228ed47c20d77
Efraim Flashner <efraim@flashner.co.il>
2024-01-14 16:34:28 +0200

    gnu: rust-lexical-parse-float-0.8: Update to 0.8.5.
    * gnu/packages/crates-io.scm (rust-lexical-parse-float-0.8): Update to
    0.8.5.
    [arguments]: Don't skip the build.  Skip the tests.
    [cargo-development-inputs]: Add rust-proptest-0.10.
    
    Change-Id: Ie8ae6586191dcce16f9bc80a3adbfe1b0501b78b

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -35533,21 +35533,22 @@ source audio format.")
 (define-public rust-lexical-parse-float-0.8
   (package
     (name "rust-lexical-parse-float")
-    (version "0.8.2")
+    (version "0.8.5")
     (source
      (origin
        (method url-fetch)
        (uri (crate-uri "lexical-parse-float" version))
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
-        (base32 "03mzjx8k2jzv2vbl1xb589p9zfs5i2m23vcmm6fjsxps0k86dl5l"))))
+        (base32 "0py0gp8hlzcrlvjqmqlpl2v1as65iiqxq2xsabxvhc01pmg3lfv8"))))
     (build-system cargo-build-system)
     (arguments
-     `(#:skip-build? #t
+     `(#:tests? #f      ; unresolved import `quickcheck`
        #:cargo-inputs
        (("rust-lexical-parse-integer" ,rust-lexical-parse-integer-0.8)
         ("rust-lexical-util" ,rust-lexical-util-0.8)
-        ("rust-static-assertions" ,rust-static-assertions-1))))
+        ("rust-static-assertions" ,rust-static-assertions-1))
+       #:cargo-development-inputs (("rust-proptest" ,rust-proptest-0.10))))
     (home-page "https://github.com/Alexhuszagh/rust-lexical")
     (synopsis "Efficient parsing of floats from strings")
     (description
