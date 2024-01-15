8e6b94c93485877598482f302e9e42e4bb1d7076
Josselin Poiret <dev@jpoiret.xyz>
2024-01-15 23:03:37 +0100

    gnu: ghc-9.2: Remove unneeded glibc 2.33 patch.
    * gnu/packages/patches/ghc-9.2-glibc-2.33-link-order.patch: Remove.
    * gnu/local.mk (dist_patch_DATA): Unregister.
    * gnu/packages/haskell.scm (ghc-9.2): Remove patch use.
    
    Change-Id: I7d305dadb627f6af55e4a5faabfc835e4a78a05b

--- gnu/local.mk
+++ gnu/local.mk
@@ -1355,7 +1355,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/gettext-libunicode-update.patch		\
   %D%/packages/patches/ghc-8.0-fall-back-to-madv_dontneed.patch	\
   %D%/packages/patches/ghc-9.2-cabal-support-package-path.patch \
-  %D%/packages/patches/ghc-9.2-glibc-2.33-link-order.patch \
   %D%/packages/patches/ghc-9.2-grep-warnings.patch \
   %D%/packages/patches/ghc-testsuite-dlopen-pie.patch		\
   %D%/packages/patches/ghc-testsuite-grep-compat.patch		\
