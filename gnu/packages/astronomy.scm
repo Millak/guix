324bc1ea8fa022f67f6f85636baa738de99add33
Sharlatan Hellseher <sharlatanus@gmail.com>
2024-01-15 18:44:10 +0000

    gnu: python-tweakwcs: Update to 0.8.6.
    * gnu/packages/astronomy.scm (python-tweakwcs): Update to 0.8.6.
    
    Change-Id: Icfbb7680a5e208d68f5b8da3fa3fdd35a358ecf5

--- gnu/packages/astronomy.scm
+++ gnu/packages/astronomy.scm
@@ -3966,13 +3966,13 @@ of the old packages.")
 (define-public python-tweakwcs
   (package
     (name "python-tweakwcs")
-    (version "0.8.5")
+    (version "0.8.6")
     (source (origin
               (method url-fetch)
               (uri (pypi-uri "tweakwcs" version))
               (sha256
                (base32
-                "15gc2rc2isx3va6wbn4k1hi5qh49gq15cgbb2qqkbznd7f7d5ifv"))))
+                "1j75i02xzd36gp7pf6gks6nmp4ivi9cbphya4l69py5p62iaab99"))))
     (build-system pyproject-build-system)
     (propagated-inputs (list python-astropy
                              python-gwcs
