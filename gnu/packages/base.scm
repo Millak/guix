a0c17878166db205def211cdaa6e79712057f883
Josselin Poiret <dev@jpoiret.xyz>
2024-01-15 16:55:01 +0100

    gnu: glibc-locales: Fix build.
    Change-Id: I34112114b76e75cb2e517d0965286ba8c11a5d6c
    
    * gnu/packages/base.scm (make-glibc-locales): Delete the new glibc
    'install-utf8-c-locale phase.
    
    Change-Id: I843ea0a4fb34dba42e3a2e5ceafb85db7060089f

--- gnu/packages/base.scm
+++ gnu/packages/base.scm
@@ -1423,6 +1423,7 @@ to the @code{share/locale} sub-directory of this package.")
                                                            normalized)))))
                              locales))))
              (delete 'install)
+             (delete 'install-utf8-c-locale)
              (delete 'move-static-libs)))
          ((#:configure-flags flags)
           `(append ,flags
