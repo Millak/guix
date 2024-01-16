b9c19c60327580970c3c5e9143bdb9ca0cde0c14
Josselin Poiret <dev@jpoiret.xyz>
2024-01-16 17:38:00 +0100

    gnu: python-matplotlib: Fix flaky Legend loc test.
    * gnu/packages/patches/python-matplotlib-fix-legend-loc-best-test.patch: New patch.
    * gnu/local.mk (dist_patch_DATA): Register it.
    * gnu/packages/python-xyz.scm (python-matplotlib): Use it.
    
    Change-Id: I0b488844d7b34a718b7294134b8c954492c9b697

--- gnu/local.mk
+++ gnu/local.mk
@@ -1877,6 +1877,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/python-docrepr-fix-tests.patch		\
   %D%/packages/patches/python-feedparser-missing-import.patch	\
   %D%/packages/patches/python-louvain-fix-test.patch		\
+  %D%/packages/patches/python-matplotlib-fix-legend-loc-best-test.patch	\
   %D%/packages/patches/python-random2-getrandbits-test.patch		\
   %D%/packages/patches/python-pillow-use-zlib-1.3.patch	\
   %D%/packages/patches/python-pyreadstat-link-libiconv.patch	\
