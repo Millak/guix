ea9204ab4ba02136cd70336a3a6c4253dd89b418
Sharlatan Hellseher <sharlatanus@gmail.com>
2024-01-16 23:14:04 +0000

    gnu: go-gitlab-com-yawning-edwards25519-extra: Move to (gnu packages golang-crypto).
    * gnu/packages/golang.scm (go-gitlab-com-yawning-edwards25519-extra):
    Move from here ...
    * gnu/packages/golang-crypto.scm: ... to here.
    
    Change-Id: I9c4aef2a0b58c482f838cc6a3a9e443dc05629b7

--- gnu/packages/golang-crypto.scm
+++ gnu/packages/golang-crypto.scm
@@ -294,6 +294,72 @@ library merely changes ClientHello part of it and provides low-level access.")
 tunnel proxy protocol.")
     (license license:asl2.0)))
 
+(define-public go-gitlab-com-yawning-edwards25519-extra
+  (let ((commit "2149dcafc266f66d2487f45b156f6397f9c4760b")
+        (revision "0"))
+    (package
+      (name "go-gitlab-com-yawning-edwards25519-extra")
+      (version (git-version "0.0.0" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://gitlab.com/yawning/edwards25519-extra")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "08mz1qyi8ig515hh5blnzxhiwsav564ah7mzyhvmr6i48ndhhv98"))))
+      (build-system go-build-system)
+      (arguments
+       '(#:unpack-path "gitlab.com/yawning/edwards25519-extra"
+         #:phases
+         (modify-phases %standard-phases
+           (replace 'build
+             (lambda arguments
+               (for-each
+                (lambda (directory)
+                  (apply (assoc-ref %standard-phases 'build)
+                         `(,@arguments #:import-path ,directory)))
+                (list
+                 "gitlab.com/yawning/edwards25519-extra/elligator2"
+                 "gitlab.com/yawning/edwards25519-extra/h2c"
+                 "gitlab.com/yawning/edwards25519-extra/internal/montgomery"
+                 "gitlab.com/yawning/edwards25519-extra/vrf"))))
+           (replace 'check
+             (lambda arguments
+               (for-each
+                (lambda (directory)
+                  (apply (assoc-ref %standard-phases 'check)
+                         `(,@arguments #:import-path ,directory)))
+                (list
+                 "gitlab.com/yawning/edwards25519-extra/elligator2"
+                 "gitlab.com/yawning/edwards25519-extra/h2c"
+                 "gitlab.com/yawning/edwards25519-extra/internal/montgomery"
+                 "gitlab.com/yawning/edwards25519-extra/vrf"))))
+           (replace 'install
+             (lambda arguments
+               (for-each
+                (lambda (directory)
+                  (apply (assoc-ref %standard-phases 'install)
+                         `(,@arguments #:import-path ,directory)))
+                (list
+                 "gitlab.com/yawning/edwards25519-extra/elligator2"
+                 "gitlab.com/yawning/edwards25519-extra/h2c"
+                 "gitlab.com/yawning/edwards25519-extra/internal/montgomery"
+                 "gitlab.com/yawning/edwards25519-extra/vrf")))))))
+      (propagated-inputs (list go-golang-org-x-crypto
+                               go-filippo-io-edwards25519))
+      (home-page "https://gitlab.com/yawning/edwards25519-extra")
+      (synopsis "edwards25519-extra")
+      (description "This package provides extensions to the Go standard
+library's Ed25519 and curve25519 implementations, primarily extracted from
+@@url{https://github.com/oasisprotocol/curve25519-voi,curve25519-voi}.  This
+package is intended for interoperability with the standard library and the
+@@url{https://filippo.io/edwards25519,edwards25519} package as much as
+possible.")
+      (license license:bsd-3))))
+
+
 ;;;
 ;;; Avoid adding new packages to the end of this file. To reduce the chances
 ;;; of a merge conflict, place them above by existing packages with similar
