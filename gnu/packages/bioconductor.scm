b19e208c174f6d5a1239e14c63fbb379ff2b20f8
Ricardo Wurmus <rekado@elephly.net>
2024-01-18 16:01:37 +0100

    gnu: r-basilisk: Update to 1.14.2.
    * gnu/packages/bioconductor.scm (r-basilisk): Update to 1.14.2.
    
    Change-Id: I9524ac5f52107c38b3c1172f3083836c7eabac99

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -23027,14 +23027,14 @@ package, primarily for creation of the underlying Conda instance.")
 (define-public r-basilisk
   (package
     (name "r-basilisk")
-    (version "1.14.1")
+    (version "1.14.2")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "basilisk" version))
        (sha256
         (base32
-         "1v67z53y2nnss6l6nk7f557kn0d5syg9n0zdgddpsz26rwdmp8ai"))))
+         "0fi3211p7fzab0bw2zqkc7gnqzpi24sf8655wghaw3yg9g39ksdg"))))
     (properties `((upstream-name . "basilisk")))
     (build-system r-build-system)
     (propagated-inputs
