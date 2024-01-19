0f1ed70bee69a18efa94ddb1007368e20cec6ee4
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-01-19 20:36:56 +0800

    gnu: python-pytz: Update to 2023.3.post1.
    * gnu/packages/time.scm (python-pytz): Update to 2023.3.post1.
    
    Change-Id: Ia8b4a58127be7acc9a188ae1eaffa61a05842e05
    Signed-off-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>

--- gnu/packages/time.scm
+++ gnu/packages/time.scm
@@ -24,6 +24,7 @@
 ;;; Copyright © 2023 Sharlatan Hellseher <sharlatanus@gmail.com>
 ;;; Copyright © 2024 Liliana Marie Prikler <liliana.prikler@gmail.com>
 ;;; Copyright © 2024 Sharlatan Hellseher <sharlatanus@gmail.com>
+;;; Copyright © 2024 Zheng Junjie <873216071@qq.com>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -167,14 +168,14 @@ expressions.")
   (package
     (name "python-pytz")
     ;; This package should be kept in sync with tzdata in (gnu packages base).
-    (version "2022.1")
+    (version "2023.3.post1")
     (source
      (origin
       (method url-fetch)
       (uri (pypi-uri "pytz" version))
       (sha256
        (base32
-        "19ya5sh7if819flgmszz585glailhi7rr8frng03n5m8wqphwxhy"))))
+        "0yrxykwhk18x22lp0hjlj98mgnzrzlclz8kxam5vl7jap6zdskvv"))))
     (build-system python-build-system)
     (home-page "http://pythonhosted.org/pytz")
     (synopsis "Python timezone library")
