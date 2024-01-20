75ee5202566cfa9710cae9379038e3b44da4e5e9
Jaeme Sifat <jaeme@runbox.com>
2024-01-20 12:55:24 -0500

    gnu: Add rust-owo-colors-4.
    * gnu/packages/crates-io.scm (rust-owo-colors-4): New variable.
    [description]: Fix description field.
    (rust-owo-colors-3): Inherit from rust-owo-colors-4.
    
    Change-Id: I6506cd5756f57c24fce929dd0c0fcb236f5a9d65
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -47629,8 +47629,30 @@ platform-native strings.")
      "This package provides a ttf-parser plus support for owned data.")
     (license license:asl2.0)))
 
+(define-public rust-owo-colors-4
+  (package
+    (name "rust-owo-colors")
+    (version "4.0.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "owo-colors" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0grsk47cllj0s4nc4qxvy4gdhj2lyiglbqx4lmw2m7grdmq59zya"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs (("rust-supports-color" ,rust-supports-color-2))))
+    (home-page "https://github.com/jam1garner/owo-colors")
+    (synopsis "Zero-allocation terminal colors that'll make people go owo")
+    (description
+     "This package provides zero-allocation terminal colors that'll make people go
+owo.")
+    (license license:expat)))
+
 (define-public rust-owo-colors-3
   (package
+    (inherit rust-owo-colors-4)
     (name "rust-owo-colors")
     (version "3.6.0")
     (source (origin
@@ -47639,14 +47661,7 @@ platform-native strings.")
               (file-name (string-append name "-" version ".tar.gz"))
               (sha256
                (base32
-                "0943lynkwz1glq3w7m9anv73lnrhd8yabs09krbh49g1wz4lxp39"))))
-    (build-system cargo-build-system)
-    (arguments
-     `(#:cargo-inputs (("rust-supports-color" ,rust-supports-color-2))))
-    (home-page "https://github.com/jam1garner/owo-colors")
-    (synopsis "Zero-allocation terminal colors that'll make people go owo")
-    (description "Zero-allocation terminal colors that'll make people go owo")
-    (license license:expat)))
+                "0943lynkwz1glq3w7m9anv73lnrhd8yabs09krbh49g1wz4lxp39"))))))
 
 (define-public rust-owned-ttf-parser-0.6
   (package
