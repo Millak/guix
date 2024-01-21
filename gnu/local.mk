be86f3de1eb175ba3e17f2146f2c96849f4f4fe7
Tomas Volf <~@wolfsden.cz>
2024-01-20 22:59:08 -0500

    gnu: python: Make the build reproducible.
    While Python build was reproducible on a single machine, once multiple file
    systems entered the picture, it was no longer true.  The solution adopted by
    the upstream (and Debian) was cherry-picked.
    
    More info: <https://github.com/python/cpython/pull/8226>.
    
    * gnu/packages/python.scm (python-3.10) [source]: Apply reproducibility patch.
    
    Signed-off-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>
    Modified-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>
    Change-Id: I0273dc0f8511a7acdcc2b462a26cc29a9756c801

--- gnu/local.mk
+++ gnu/local.mk
@@ -1970,6 +1970,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/python-3-hurd-configure.patch		\
   %D%/packages/patches/python-angr-addition-type-error.patch	\
   %D%/packages/patches/python-angr-check-exec-deps.patch	\
+  %D%/packages/patches/python-3-reproducible-build.patch	\
   %D%/packages/patches/python-aionotify-0.2.0-py3.8.patch	\
   %D%/packages/patches/python-argcomplete-1.11.1-fish31.patch	\
   %D%/packages/patches/python-cross-compile.patch		\
