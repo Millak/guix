b183cce3b0467a2d1ba3520cd3c6ecb9aed9f3bb
Efraim Flashner <efraim@flashner.co.il>
2024-01-21 19:41:39 +0200

    gnu: rust-block-cipher-trait: Move to (gnu packages crates-crypto).
    * gnu/packages/crates-io.scm (rust-block-cipher-trait-0.6,
    rust-block-cipher-trait-0.4): Move from here ...
    * gnu/packages/crates-crypto.scm: ... to here.
    
    Change-Id: I449e264f064dd389e0e44b57ba6d29ba376a0562

--- gnu/packages/crates-crypto.scm
+++ gnu/packages/crates-crypto.scm
@@ -790,6 +790,46 @@ based on Blake2s.")
 ciphers.")
     (license (list license:expat license:asl2.0))))
 
+(define-public rust-block-cipher-trait-0.6
+  (package
+    (name "rust-block-cipher-trait")
+    (version "0.6.2")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "block-cipher-trait" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0x273w6fwka0i48nrv428birdrs2jz6jdnmc0dhc1rq9pm4lv4hw"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs
+       (("rust-blobby" ,rust-blobby-0.1)
+        ("rust-generic-array" ,rust-generic-array-0.12))))
+    (home-page "https://github.com/RustCrypto/block-ciphers")
+    (synopsis "Block cipher algorithms")
+    (description "This package provides a collection of block cipher
+algorithms.  This package is deprecated.  Please use block-cipher instead.")
+    (license (list license:expat license:asl2.0))))
+
+(define-public rust-block-cipher-trait-0.4
+  (package
+    (inherit rust-block-cipher-trait-0.6)
+    (name "rust-block-cipher-trait")
+    (version "0.4.2")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "block-cipher-trait" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32
+         "10qmg8vphqmfllb9a2yx6s7r66jh1wh33clhsawq7ikg2wgz2p6q"))))
+    (arguments
+     `(#:cargo-inputs
+       (("rust-generic-array" ,rust-generic-array-0.8))))))
+
 (define-public rust-blowfish-0.9
   (package
     (name "rust-blowfish")
