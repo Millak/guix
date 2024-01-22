da5ce98e2c28cf57e6bde500fa09cb2f38bc585c
Efraim Flashner <efraim@flashner.co.il>
2024-01-22 22:23:07 +0200

    gnu: Remove rust-statrs-0.12.
    * gnu/packages/crates-io.scm (rust-statrs-0.12): Delete variable.
    
    Change-Id: I3f3061285c83d91d49de02e5d781b22a7bb55985

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -62518,29 +62518,6 @@ Rust.")
 Rust.")
     (license license:expat)))
 
-(define-public rust-statrs-0.12
-  (package
-    (name "rust-statrs")
-    (version "0.12.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (crate-uri "statrs" version))
-       (file-name (string-append name "-" version ".tar.gz"))
-       (sha256
-        (base32 "10hk9194ig21w08947yavf4l97g0106ph4xxlzn8ps2kwrnnzqfc"))))
-    (build-system cargo-build-system)
-    (arguments
-     `(#:cargo-test-flags
-       '("--release" "--lib" "--bins" "--tests")
-       #:cargo-inputs
-       (("rust-rand" ,rust-rand-0.7))))
-    (home-page "https://github.com/statrs-dev/statrs")
-    (synopsis "Statistical computing library for Rust")
-    (description "This package provides a statistical computing library for
-Rust.")
-    (license license:expat)))
-
 (define-public rust-stats-alloc-0.1
   (package
     (name "rust-stats-alloc")
