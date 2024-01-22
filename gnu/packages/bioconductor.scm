d2573f4e2896f939bf89e86d84b50f060836cab4
Ricardo Wurmus <rekado@elephly.net>
2024-01-22 13:44:10 +0100

    gnu: r-org-eck12-eg-db: Update to 3.18.0.
    * gnu/packages/bioconductor.scm (r-org-eck12-eg-db): Update to 3.18.0.
    
    Change-Id: Ic8b25521c9a735522c69e3d696d8a514f8d6d17c

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -290,13 +290,13 @@ even help to identify disease associations that are not obvious.")
 (define-public r-org-eck12-eg-db
   (package
     (name "r-org-eck12-eg-db")
-    (version "3.12.0")
+    (version "3.18.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "org.EcK12.eg.db" version 'annotation))
        (sha256
-        (base32 "0c4p6jr83k0gm6pvn760yr8xf33wggrfcr6fg7a42a96bcf817gs"))))
+        (base32 "12lqv06n49lwczv5l4h5h6lbfbcqyg9j4csffcdx495scmhc06mc"))))
     (properties
      `((upstream-name . "org.EcK12.eg.db")))
     (build-system r-build-system)
