4063fbf23df3d79d1e19f5653673e0093795558b
Efraim Flashner <efraim@flashner.co.il>
2024-01-22 08:33:35 +0200

    gnu: rust-oid-registry: Move to (gnu packages crates-tls).
    * gnu/packages/crates-io.scm (rust-oid-registry-0.6,
    rust-oid-registry-0.4, rust-oid-registry-0.3, rust-oid-registry-0.2):
    Move from here ...
    * gnu/packages/crates-tls.scm: ... to here.
    
    Change-Id: I81ae6f37fd22d328a168afd3fd5807e61538afdb

--- gnu/packages/crates-tls.scm
+++ gnu/packages/crates-tls.scm
@@ -426,6 +426,73 @@ targets")
 implementation.")
     (license (list license:expat license:asl2.0))))
 
+(define-public rust-oid-registry-0.6
+  (package
+    (name "rust-oid-registry")
+    (version "0.6.1")
+    (source (origin
+              (method url-fetch)
+              (uri (crate-uri "oid-registry" version))
+              (file-name (string-append name "-" version ".tar.gz"))
+              (sha256
+               (base32
+                "1zwvjp3ad6gzn8g8w2hcn9a2xdap0lkzckhlnwp6rabbzdpz7vcv"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs (("rust-asn1-rs" ,rust-asn1-rs-0.5))))
+    (home-page "https://github.com/rusticata/oid-registry")
+    (synopsis "Object Identifier (OID) database")
+    (description "This crate is a helper crate, containing a database of
+OID objects.  These objects are intended for use when manipulating ASN.1
+grammars and BER/DER encodings, for example.")
+    (license (list license:expat license:asl2.0))))
+
+(define-public rust-oid-registry-0.4
+  (package
+    (inherit rust-oid-registry-0.6)
+    (name "rust-oid-registry")
+    (version "0.4.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "oid-registry" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0akbah3j8231ayrp2l1y5d9zmvbvqcsj0sa6s6dz6h85z8bhgqiq"))))
+    (arguments
+     `(#:cargo-inputs (("rust-asn1-rs" ,rust-asn1-rs-0.3))))))
+
+(define-public rust-oid-registry-0.3
+  (package
+    (inherit rust-oid-registry-0.6)
+    (name "rust-oid-registry")
+    (version "0.3.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "oid-registry" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1jxr4nqpcsgirl53yndhfhch1gzddkjh27z0p7rbsr2xngcpyj9n"))))
+    (arguments
+     `(#:cargo-inputs (("rust-asn1-rs" ,rust-asn1-rs-0.3))))))
+
+(define-public rust-oid-registry-0.2
+  (package
+    (inherit rust-oid-registry-0.4)
+    (name "rust-oid-registry")
+    (version "0.2.0")
+    (source (origin
+              (method url-fetch)
+              (uri (crate-uri "oid-registry" version))
+              (file-name (string-append name "-" version ".tar.gz"))
+              (sha256
+               (base32
+                "058qip5j5y0i95ckmw67mp73372rq16ci0lcczyq9irv76r4qmgy"))))
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs (("rust-der-parser" ,rust-der-parser-6))))))
+
 (define-public rust-rcgen-0.11
   (package
     (name "rust-rcgen")
