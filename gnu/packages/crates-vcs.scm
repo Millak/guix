989d63e0b09f82daa71898a4790cdfdee68155ff
Efraim Flashner <efraim@flashner.co.il>
2024-01-22 10:59:22 +0200

    gnu: rust-gix-path-0.10: Update to 0.10.4.
    * gnu/packages/crates-vcs.scm (rust-gix-path-0.10): Update to 0.10.4.
    
    Change-Id: Ic7fd9ce27608bbb780e7d499197932fd7ef614e0

--- gnu/packages/crates-vcs.scm
+++ gnu/packages/crates-vcs.scm
@@ -2630,14 +2630,14 @@ duplicate of @code{gix-packetline} with the @code{blocking-io} feature pre-selec
 (define-public rust-gix-path-0.10
   (package
     (name "rust-gix-path")
-    (version "0.10.3")
+    (version "0.10.4")
     (source
      (origin
        (method url-fetch)
        (uri (crate-uri "gix-path" version))
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
-        (base32 "1dlz2y6z4v1l4ccw87dxbqc59zj2smc7w9m21ka36pr4mfc0kpdq"))))
+        (base32 "0h1fkwz2ll7ahyd3sy127w3bkcv4lncl1ml37z6bxldf44k2i9hl"))))
     (build-system cargo-build-system)
     (arguments
      `(#:cargo-inputs (("rust-bstr" ,rust-bstr-1)
