02ae4a51db259c45b2247737bdf53f1d903526d1
Efraim Flashner <efraim@flashner.co.il>
2024-01-24 10:37:15 +0200

    gnu: libbdplus: Update to 0.2.0.
    * gnu/packages/video.scm (libbdplus): Update to 0.2.0.
    
    Change-Id: Ic357c71649b8f07ddb37b0860f30d9ef247529d5

--- gnu/packages/video.scm
+++ gnu/packages/video.scm
@@ -4803,17 +4803,17 @@ and MPEG system streams.")
 (define-public libbdplus
   (package
     (name "libbdplus")
-    (version "0.1.2")
+    (version "0.2.0")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://ftp.videolan.org/pub/videolan/libbdplus/"
                            version "/" name "-" version ".tar.bz2"))
        (sha256
-        (base32 "02n87lysqn4kg2qk7d1ffrp96c44zkdlxdj0n16hbgrlrpiwlcd6"))))
+        (base32 "0n0ayjq2ld7lfhrfcdj9bam96m2hih34phyjan8nwggkmqzflgmr"))))
+    (build-system gnu-build-system)
     (inputs
      (list libgcrypt))
-    (build-system gnu-build-system)
     (home-page "https://www.videolan.org/developers/libbdplus.html")
     (synopsis "Library for decrypting certain Blu-Ray discs")
     (description "libbdplus is a library which implements the BD+ System
