9a7b0a0270f8378a780e0f2b66aa90f453777d83
Efraim Flashner <efraim@flashner.co.il>
2024-01-29 11:28:59 +0200

    gnu: Add rust-askama-derive-0.12.
    * gnu/packages/crates-io.scm (rust-askama-derive-0.12): New variable.
    (rust-askama-derive-0.11): Inherit from rust-askama-derive-0.12.
    
    Change-Id: I407b7714ed394216bca26337a391d2457c1aacce

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -2806,8 +2806,36 @@ extracted from Askama.")
     (description "This package provides shared code for Askama.")
     (license (list license:expat license:asl2.0))))
 
+(define-public rust-askama-derive-0.12
+  (package
+    (name "rust-askama-derive")
+    (version "0.12.5")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "askama_derive" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "10qxszzrwbabpd3jh6lvk3q1a81ryfba8bh75jb18irwn5n8vzhr"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs (("rust-askama-parser" ,rust-askama-parser-0.2)
+                       ("rust-basic-toml" ,rust-basic-toml-0.1)
+                       ("rust-mime" ,rust-mime-0.3)
+                       ("rust-mime-guess" ,rust-mime-guess-2)
+                       ("rust-proc-macro2" ,rust-proc-macro2-1)
+                       ("rust-quote" ,rust-quote-1)
+                       ("rust-serde" ,rust-serde-1)
+                       ("rust-syn" ,rust-syn-2))))
+    (home-page "https://github.com/djc/askama")
+    (synopsis "Procedural macro package for Askama")
+    (description
+     "This package provide the procedural macro package for Askama.")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-askama-derive-0.11
   (package
+    (inherit rust-askama-derive-0.12)
     (name "rust-askama-derive")
     (version "0.11.2")
     (source
@@ -2817,19 +2845,12 @@ extracted from Askama.")
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
         (base32 "0wbb5l1x1bx8x8vvz4ayw196l9y64mi3vrmxm7pn8wmlx3k8ggw7"))))
-    (build-system cargo-build-system)
     (arguments
      (list #:skip-build? #t
            #:cargo-inputs
            `(("rust-askama-shared" ,rust-askama-shared-0.12)
              ("rust-proc-macro2" ,rust-proc-macro2-1)
-             ("rust-syn" ,rust-syn-1))))
-    (home-page "https://github.com/djc/askama")
-    (synopsis "Procedural macro package for Askama")
-    (description
-     "This package provide the procedural macro package for
-Askama.")
-    (license (list license:expat license:asl2.0))))
+             ("rust-syn" ,rust-syn-1))))))
 
 (define-public rust-askama-0.11
   (package
