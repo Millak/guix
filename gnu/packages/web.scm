03aba34311978e5917eae0c0b614021a3e2091cb
Josselin Poiret <dev@jpoiret.xyz>
2024-02-02 19:06:14 +0100

    gnu: httpd: Add libxcrypt dependency.
    * gnu/packages/web.scm (httpd): Add libxcrypt dependency.
    
    Change-Id: I8ef010b0c37bae90493f2f19dd25987c32fd687e

--- gnu/packages/web.scm
+++ gnu/packages/web.scm
@@ -124,6 +124,7 @@
   #:use-module (gnu packages crates-gtk)
   #:use-module (gnu packages crates-tls)
   #:use-module (gnu packages crates-web)
+  #:use-module (gnu packages crypto)
   #:use-module (gnu packages curl)
   #:use-module (gnu packages cyrus-sasl)
   #:use-module (gnu packages databases)
@@ -300,7 +301,7 @@
                "1id45r2ccgkbjm9i998997ch32lvicpyynyx8x6aa4420wmdf5ps"))))
     (build-system gnu-build-system)
     (native-inputs (list `(,pcre "bin")))       ;for 'pcre-config'
-    (inputs (list apr apr-util openssl perl)) ; needed to run bin/apxs
+    (inputs (list apr apr-util libxcrypt openssl perl)) ; needed to run bin/apxs
     (arguments
      (list
       #:test-target "test"
