7734d25eac988fba39f45b4be73a215fb083816d
Josselin Poiret <dev@jpoiret.xyz>
2024-02-02 19:05:55 +0100

    gnu: tdb: Add libxcrypt dependency.
    * gnu/packages/databases.scm (tdb): Add libxcrypt dependency.
    
    Change-Id: If44722b61b9ed427665b5939a2c5f5ef35e78691

--- gnu/packages/databases.scm
+++ gnu/packages/databases.scm
@@ -2050,6 +2050,7 @@ changes.")
                        (string-append "--prefix=" out))))))))
     (native-inputs
      (list docbook-xsl
+           libxcrypt
            libxslt
            python ;for the Waf build system
            which))
