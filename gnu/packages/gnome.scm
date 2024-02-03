5cabec17d636c22e946d669be512279dd1506721
Vivien Kraus <vivien@planete-kraus.eu>
2024-02-03 13:46:08 +0100

    gnu: gnome-shell: Update to 44.9.
    * gnu/packages/gnome.scm (gnome-shell): Update to 44.9.
    
    Change-Id: I1160153fa4cbe3f93e9cfab983f518a405e32297
    Reviewed-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>
    Signed-off-by: Liliana Marie Prikler <liliana.prikler@gmail.com>

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -9257,7 +9257,7 @@ properties, screen resolution, and other GNOME parameters.")
 (define-public gnome-shell
   (package
     (name "gnome-shell")
-    (version "44.7")
+    (version "44.9")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://gnome/sources/" name "/"
@@ -9265,7 +9265,7 @@ properties, screen resolution, and other GNOME parameters.")
                                   name "-" version ".tar.xz"))
               (sha256
                (base32
-                "0gvhlvcw6rd8xa71qfxw39kyqgnv15x8xv8my6h5215kx3il1acb"))))
+                "0k5pxbwwqi1fqpvja77xl0fv7y46gaz17p29f74wpzyb61g16rhp"))))
     (build-system meson-build-system)
     (arguments
      (let ((disallowed-references
