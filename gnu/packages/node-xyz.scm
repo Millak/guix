ded5f62abe6ca8659066516f1dfa8041dc3de04d
Nicolas Graves <ngraves@ngraves.fr>
2024-02-04 13:40:16 +0100

    gnu: node-safe-buffer: Move package in alphabetical order.
    * gnu/packages/node-xyz.scm (node-safe-buffer): Move package in alphabetical order.
    
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/node-xyz.scm
+++ gnu/packages/node-xyz.scm
@@ -651,6 +651,35 @@ Optional custom circular values, deterministic behavior or strict JSON
 compatibility check.")
     (license license:expat)))
 
+(define-public node-safe-buffer
+  (package
+    (name "node-safe-buffer")
+    (version "5.2.1")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/feross/safe-buffer")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32
+         "0r26m0nl41h90ihnl2xf0cqs6z9z7jb87dl5j8yqb7887r9jlbpi"))))
+    (build-system node-build-system)
+    (arguments
+     '(#:phases
+       (modify-phases %standard-phases
+         (add-after 'patch-dependencies 'delete-dependencies
+           (lambda args
+             (delete-dependencies '("tape" "standard")))))
+       #:tests? #f))
+    (home-page "https://github.com/feross/safe-buffer")
+    (synopsis "Buffer creation with explicit semantics")
+    (description "This package provides a drop-in replacement for Node.js
+@code{Buffer} API, which provides newer, explicit constructors (such as
+@code{Buffer.alloc(SIZE)}) in older versions.")
+    (license license:expat)))
+
 (define-public node-stack-trace
   ;; There have been improvements since the last release.
   (let ((commit "4fd379ee78965ce7ce8820b436f1b1b590d5dbcf")
@@ -807,35 +836,6 @@ function with browser support.")
     (synopsis "Cross-platform utility to compute the PATH environment variable key")
     (description "@code{path-key} provides an implementation to compute the
 particular cross-platform spellings of the PATH environment variable key.")
-    (license license:expat)))
-
-(define-public node-safe-buffer
-  (package
-    (name "node-safe-buffer")
-    (version "5.2.1")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://github.com/feross/safe-buffer")
-             (commit (string-append "v" version))))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32
-         "0r26m0nl41h90ihnl2xf0cqs6z9z7jb87dl5j8yqb7887r9jlbpi"))))
-    (build-system node-build-system)
-    (arguments
-     '(#:phases
-       (modify-phases %standard-phases
-         (add-after 'patch-dependencies 'delete-dependencies
-           (lambda args
-             (delete-dependencies '("tape" "standard")))))
-       #:tests? #f))
-    (home-page "https://github.com/feross/safe-buffer")
-    (synopsis "Buffer creation with explicit semantics")
-    (description "This package provides a drop-in replacement for Node.js
-@code{Buffer} API, which provides newer, explicit constructors (such as
-@code{Buffer.alloc(SIZE)}) in older versions.")
     (license license:expat)))
 
 (define-public node-string-decoder
