d41d3d8fb2d210a7d14e1e31092e3ebc068ecc92
Nicolas Graves <ngraves@ngraves.fr>
2024-02-04 13:40:19 +0100

    gnu: node-irc and node-irc-colors: Move in alphabetical order.
    * gnu/packages/node-xyz.scm (node-irc and node-irc-colors): Move packages in alphabetical order.
    
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/node-xyz.scm
+++ gnu/packages/node-xyz.scm
@@ -357,6 +357,65 @@ Node's @code{inherits} constructor that can be used in browsers, while
 defaulting to Node's implementation otherwise.")
     (license license:isc)))
 
+(define-public node-irc-colors
+  (package
+    (name "node-irc-colors")
+    (version "1.5.0")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/fent/irc-colors.js")
+                    (commit (string-append "v" version))))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "0q3y34rbnlc55jcakmdxkicwazyvyph9r6gaf6hi8k7wj2nfwfli"))))
+    (build-system node-build-system)
+    (arguments
+     '(#:phases
+       (modify-phases %standard-phases
+         (add-after 'patch-dependencies 'delete-dependencies
+           (lambda args
+             (delete-dependencies `("istanbul" "vows")))))
+       #:tests? #f))
+    (home-page "https://github.com/fent/irc-colors.js")
+    (synopsis "Node.js module providing color and formatting for IRC")
+    (description "@code{node-irc-colors} is a Node.js module that
+allows you to easily use colored output and formatting in IRC bots.
+It contains functions for colours as well as more complex formatting
+such as rainbows.")
+    (license license:expat)))
+
+(define-public node-irc
+  (package
+    (name "node-irc")
+    (version "0.5.2")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/martynsmith/node-irc")
+                    (commit (string-append "v" version))))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "1ln4qfx20jbwg4cp8lp0vf27m5281z2sz16d15xd6150n26cbi4x"))))
+    (build-system node-build-system)
+    (arguments
+     '(#:phases
+       (modify-phases %standard-phases
+         (add-after 'patch-dependencies 'delete-dependencies
+           (lambda args
+             (delete-dependencies
+              `("ansi-color" "faucet" "jscs" "tape")))))
+       #:tests? #f))
+    (inputs
+     (list node-irc-colors))
+    (home-page "https://github.com/martynsmith/node-irc")
+    (synopsis "IRC client library for Node.js")
+    (description "@code{node-irc} is an IRC client library for Node.js.
+It has functions for joining, parting, talking, and many other IRC commands.")
+    (license license:gpl3+)))
+
 (define-public node-long-stack-traces
   (package
     (name "node-long-stack-traces")
@@ -919,65 +978,6 @@ function with browser support.")
     (description "@code{path-key} provides an implementation to compute the
 particular cross-platform spellings of the PATH environment variable key.")
     (license license:expat)))
-
-(define-public node-irc-colors
-  (package
-    (name "node-irc-colors")
-    (version "1.5.0")
-    (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                    (url "https://github.com/fent/irc-colors.js")
-                    (commit (string-append "v" version))))
-              (file-name (git-file-name name version))
-              (sha256
-               (base32
-                "0q3y34rbnlc55jcakmdxkicwazyvyph9r6gaf6hi8k7wj2nfwfli"))))
-    (build-system node-build-system)
-    (arguments
-     '(#:phases
-       (modify-phases %standard-phases
-         (add-after 'patch-dependencies 'delete-dependencies
-           (lambda args
-             (delete-dependencies `("istanbul" "vows")))))
-       #:tests? #f))
-    (home-page "https://github.com/fent/irc-colors.js")
-    (synopsis "Node.js module providing color and formatting for IRC")
-    (description "@code{node-irc-colors} is a Node.js module that
-allows you to easily use colored output and formatting in IRC bots.
-It contains functions for colours as well as more complex formatting
-such as rainbows.")
-    (license license:expat)))
-
-(define-public node-irc
-  (package
-    (name "node-irc")
-    (version "0.5.2")
-    (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                    (url "https://github.com/martynsmith/node-irc")
-                    (commit (string-append "v" version))))
-              (file-name (git-file-name name version))
-              (sha256
-               (base32
-                "1ln4qfx20jbwg4cp8lp0vf27m5281z2sz16d15xd6150n26cbi4x"))))
-    (build-system node-build-system)
-    (arguments
-     '(#:phases
-       (modify-phases %standard-phases
-         (add-after 'patch-dependencies 'delete-dependencies
-           (lambda args
-             (delete-dependencies
-              `("ansi-color" "faucet" "jscs" "tape")))))
-       #:tests? #f))
-    (inputs
-     (list node-irc-colors))
-    (home-page "https://github.com/martynsmith/node-irc")
-    (synopsis "IRC client library for Node.js")
-    (description "@code{node-irc} is an IRC client library for Node.js.
-It has functions for joining, parting, talking, and many other IRC commands.")
-    (license license:gpl3+)))
 
 (define-public node-nan
   (package
