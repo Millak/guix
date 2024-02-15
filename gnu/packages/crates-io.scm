7aa0a2e7082e67bf588460bf65d5d014fc9fd0df
Tomas Volf <~@wolfsden.cz>
2024-02-15 21:26:15 +0100

    gnu: Add rust-pulldown-cmark-to-cmark-10.
    * gnu/packages/crates-io.scm (rust-pulldown-cmark-to-cmark-10): New
    variable.
    
    Change-Id: I23b664947c2c31589f4a80aae89186834987a3b4
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -40,6 +40,7 @@
 ;;; Copyright © 2023 Daniel Ziltener <dziltener@lyrion.ch>
 ;;; Copyright © 2023, 2024 Troy Figiel <troy@troyfigiel.com>
 ;;; Copyright © 2024 Herman Rimm <herman@rimm.ee>
+;;; Copyright © 2024 Tomas Volf <~@wolfsden.cz>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -49820,6 +49821,36 @@ compliant email address validation.")
     (synopsis "Simple instrumentation profiler for games")
     (description "Simple instrumentation profiler for games.")
     (license (list license:expat license:asl2.0))))
+
+(define-public rust-pulldown-cmark-to-cmark-10
+  (package
+    (name "rust-pulldown-cmark-to-cmark")
+    (version "10.0.4")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "pulldown-cmark-to-cmark" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0gc366cmd5jxal9m95l17rvqsm4dn62lywc8v5gwq8vcjvhyd501"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs (("rust-pulldown-cmark" ,rust-pulldown-cmark-0.9))
+       #:cargo-development-inputs
+       (("rust-indoc" ,rust-indoc-1)
+        ("rust-pretty-assertions" ,rust-pretty-assertions-0.7))))
+    (home-page "https://github.com/Byron/pulldown-cmark-to-cmark")
+    (synopsis
+     "Convert pulldown-cmark Events back to the string they were parsed from")
+    (description
+     "A utility library which translates Event back to markdown.  It's the
+prerequisite for writing markdown filters which can work as
+@code{mdbook-preprocessors}.
+
+This library takes great pride in supporting everything that
+@code{pulldown-cmark} supports, including tables and footnotes and codeblocks
+in codeblocks, while assuring quality with a powerful test suite.")
+    (license license:asl2.0)))
 
 (define-public rust-pulldown-cmark-0.9
   (package
