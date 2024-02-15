6e51c32f9e9c8fc81fa11e30229d4dcc8bda1ebe
Tomas Volf <~@wolfsden.cz>
2024-02-15 21:26:37 +0100

    gnu: Add rust-netlink-packet-route-0.18.
    * gnu/packages/crates-io.scm (rust-netlink-packet-route-0.18): New variable.
    
    Change-Id: I33dd19fd8bd9376ece7dda55fc585242d464a1e2
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -39972,6 +39972,37 @@ that represent any netlink message for any sub-protocol.")
     (description "Generic netlink packet types.")
     (license license:expat)))
 
+(define-public rust-netlink-packet-route-0.18
+  (package
+    (name "rust-netlink-packet-route")
+    (version "0.18.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "netlink-packet-route" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1i9z89dy3p2jbs5rclvc6yxly81a4qwynrhyqv3hazj5pgxcv76x"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t     ; Cut the dependency tree
+       #:cargo-inputs (("rust-anyhow" ,rust-anyhow-1)
+                       ("rust-byteorder" ,rust-byteorder-1)
+                       ("rust-libc" ,rust-libc-0.2)
+                       ("rust-log" ,rust-log-0.4)
+                       ("rust-netlink-packet-core" ,rust-netlink-packet-core-0.7)
+                       ("rust-netlink-packet-utils" ,rust-netlink-packet-utils-0.5))
+       #:cargo-development-inputs
+       (("rust-netlink-sys" ,rust-netlink-sys-0.8)
+        ;("rust-pcap-file" ,rust-pcap-file-1)
+        ("rust-pretty-assertions" ,rust-pretty-assertions-0.7))))
+    (home-page "https://github.com/rust-netlink/netlink-packet-route")
+    (synopsis "Netlink packet types")
+    (description "The netlink-packet-route crate is designed to abstract Netlink
+route protocol (rtnetlink) packet into Rust data types.  The goal of this crate
+is saving netlink user from reading Kernel Netlink codes.")
+    (license license:expat)))
+
 (define-public rust-netlink-packet-utils-0.5
   (package
     (name "rust-netlink-packet-utils")
