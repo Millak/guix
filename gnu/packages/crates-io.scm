cae9b0ffaac9df16bf69e846a06922f76c2a7c93
Tomas Volf <~@wolfsden.cz>
2024-02-15 21:26:16 +0100

    gnu: Add rust-sync-wrapper-0.1.
    * gnu/packages/crates-io.scm (rust-sync-wrapper-0.1): New variable.
    
    Change-Id: I2eb340009dfb303130e421ae9f7bbde97e4286fa
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -66062,6 +66062,31 @@ a syntax tree of Rust source code.")
 standard library.")
     (license (list license:expat license:asl2.0))))
 
+(define-public rust-sync-wrapper-0.1
+  (package
+    (name "rust-sync-wrapper")
+    (version "0.1.2")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "sync_wrapper" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0q01lyj0gr9a93n10nxsn8lwbzq97jqd6b768x17c8f7v7gccir0"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs (("rust-futures-core" ,rust-futures-core-0.3))
+       #:cargo-development-inputs
+       (("rust-futures" ,rust-futures-0.3)
+        ("rust-pin-project-lite" ,rust-pin-project-lite-0.2))))
+    (home-page "https://docs.rs/sync_wrapper")
+    (synopsis
+     "Tool for enlisting the compiler's help in proving the absence of concurrency")
+    (description
+     "This package provides a tool for enlisting the compiler's help in proving
+the absence of concurrency.")
+    (license license:asl2.0)))
+
 (define-public rust-synom-0.11
   (package
     (name "rust-synom")
