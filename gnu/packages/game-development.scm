8d79ed2ec3efcb69c4dcba7faec689eff610865d
Liliana Marie Prikler <liliana.prikler@gmail.com>
2024-02-17 10:33:34 +0100

    gnu: python-pygame-sdl2: Update to 2.1.0+renpy8.2.0.
    * gnu/packages/game-development.scm (python-pygame-sdl2): Update to
    2.1.0+renpy8.2.0.

--- gnu/packages/game-development.scm
+++ gnu/packages/game-development.scm
@@ -1380,7 +1380,7 @@ and multimedia programs in the Python language.")
 
 (define-public python-pygame-sdl2
   (let ((real-version "2.1.0")
-        (renpy-version "8.1.3"))
+        (renpy-version "8.2.0"))
     (package
       (inherit python-pygame)
       (name "python-pygame-sdl2")
@@ -1390,7 +1390,7 @@ and multimedia programs in the Python language.")
          (method url-fetch)
          (uri (string-append "https://www.renpy.org/dl/" renpy-version
                              "/pygame_sdl2-" version ".tar.gz"))
-         (sha256 (base32 "0qlprs9n3w254ilizqzvr6s01zx72gh7an0bgwxsq4hm22qypdws"))
+         (sha256 (base32 "17mc39c7ha83kzv2wmq61a15mn6p8wh2y33ixhf5sb4bvyr48mhy"))
          (modules '((guix build utils)))
          (snippet
           '(begin
