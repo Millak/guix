2220d10226d4638a92ff131eba5fb6c94e8ec451
Herman Rimm <herman@rimm.ee>
2024-02-18 22:02:13 +0100

    gnu: Place rust-twox-hash-1 in alphabetical order.
    gnu/packages/crates-io.scm (rust-twox-hash-1): Move variable.
    
    Change-Id: Icb0d2a5f834af969f235aaef48b2b848277801b3
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -37775,35 +37775,6 @@ proofs.  It automates the Fiat-Shamir transform, so that by using Merlin,
 non-interactive protocols can be implemented as if they were interactive.")
     (license license:expat)))
 
-(define-public rust-twox-hash-1
-  (package
-    (name "rust-twox-hash")
-    (version "1.6.3")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (crate-uri "twox-hash" version))
-       (file-name (string-append name "-" version ".tar.gz"))
-       (sha256
-        (base32 "0xgn72j36a270l5ls1jk88n7bmq2dhlfkbhdh5554hbagjsydzlp"))))
-    (build-system cargo-build-system)
-    (arguments
-     `(#:cargo-inputs
-       (("rust-cfg-if" ,rust-cfg-if-0.1)
-        ("rust-digest" ,rust-digest-0.8)
-        ("rust-digest" ,rust-digest-0.9)
-        ("rust-digest" ,rust-digest-0.10)
-        ("rust-rand" ,rust-rand-0.8)
-        ("rust-serde" ,rust-serde-1)
-        ("rust-static-assertions" ,rust-static-assertions-1))
-       #:cargo-development-inputs
-       (("rust-serde-json" ,rust-serde-json-1))))
-    (home-page "https://github.com/shepmaster/twox-hash")
-    (synopsis "Rust implementation of the XXHash and XXH3 algorithms")
-    (description "This package provides a Rust implementation of the XXHash
-and XXH3 algorithms.")
-    (license license:expat)))
-
 (define-public rust-metrics-macros-0.2
   (package
     (name "rust-metrics-macros")
@@ -74864,6 +74835,35 @@ strings.")
        (sha256
         (base32
 		"1lbf64snscr3vz71jbl6i2c8zr2ndsiqbk6316z39fj1a8mipcar"))))))
+
+(define-public rust-twox-hash-1
+  (package
+    (name "rust-twox-hash")
+    (version "1.6.3")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "twox-hash" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0xgn72j36a270l5ls1jk88n7bmq2dhlfkbhdh5554hbagjsydzlp"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs
+       (("rust-cfg-if" ,rust-cfg-if-0.1)
+        ("rust-digest" ,rust-digest-0.8)
+        ("rust-digest" ,rust-digest-0.9)
+        ("rust-digest" ,rust-digest-0.10)
+        ("rust-rand" ,rust-rand-0.8)
+        ("rust-serde" ,rust-serde-1)
+        ("rust-static-assertions" ,rust-static-assertions-1))
+       #:cargo-development-inputs
+       (("rust-serde-json" ,rust-serde-json-1))))
+    (home-page "https://github.com/shepmaster/twox-hash")
+    (synopsis "Rust implementation of the XXHash and XXH3 algorithms")
+    (description "This package provides a Rust implementation of the XXHash
+and XXH3 algorithms.")
+    (license license:expat)))
 
 (define-public rust-typeable-0.1
   (package
