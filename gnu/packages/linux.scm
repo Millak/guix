b9b2cf3ccba52accdc7d86bb238727c0d7fe2074
Lars-Dominik Braun <lars@6xq.net>
2024-02-23 10:11:34 +0100

    gnu: Add missing import.
    Change-Id: I096914ef3980a99e553f512d1e83a0a1e01b4f7e

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -174,6 +174,7 @@
   #:use-module (gnu packages protobuf)
   #:use-module (gnu packages pulseaudio)
   #:use-module (gnu packages python)
+  #:use-module (gnu packages python-build)
   #:use-module (gnu packages python-xyz)
   #:use-module (gnu packages qt)
   #:use-module (gnu packages readline)
