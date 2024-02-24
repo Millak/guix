0a298e3c02683e5fda6fbdc8b41091478bfa813a
VÖRÖSKŐI András <voroskoi@gmail.com>
2024-02-24 21:39:30 +0100

    gnu: Add rust-ratatui-0.24.
    * gnu/packages/crates-io.scm (rust-ratatui-0.24): New variable.
    (rust-ratatui-0.20): Inherit from rust-ratatui-0.24.
    
    Change-Id: I92e6f2292676f7f36ea2cdca31b3bd5709709659
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -58116,8 +58116,60 @@ random generic types.")
     (license
      (list license:zlib license:asl2.0 license:expat))))
 
+(define-public rust-ratatui-0.24
+  (package
+    (name "rust-ratatui")
+    (version "0.24.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "ratatui" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "099433has5bhl6zbckpv2qskb66h7xz4rfgc6xn5cyjjzdy93g0f"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-test-flags
+       '("--release" "--"
+         "--skip=buffer::tests::index_of_panics_on_out_of_bounds"
+         "--skip=buffer::tests::pos_of_panics_on_out_of_bounds"
+         "--skip=buffer::Buffer::index_of"
+         "--skip=buffer::Buffer::pos_of")
+       #:cargo-inputs (("rust-bitflags" ,rust-bitflags-2)
+                       ("rust-cassowary" ,rust-cassowary-0.3)
+                       ("rust-crossterm" ,rust-crossterm-0.27)
+                       ("rust-document-features" ,rust-document-features-0.2)
+                       ("rust-indoc" ,rust-indoc-2)
+                       ("rust-itertools" ,rust-itertools-0.11)
+                       ("rust-lru" ,rust-lru-0.12)
+                       ("rust-paste" ,rust-paste-1)
+                       ("rust-serde" ,rust-serde-1)
+                       ("rust-strum" ,rust-strum-0.25)
+                       ("rust-termion" ,rust-termion-2)
+                       ("rust-termwiz" ,rust-termwiz-0.20)
+                       ("rust-time" ,rust-time-0.3)
+                       ("rust-unicode-segmentation" ,rust-unicode-segmentation-1)
+                       ("rust-unicode-width" ,rust-unicode-width-0.1))
+       #:cargo-development-inputs
+       (("rust-anyhow" ,rust-anyhow-1)
+        ("rust-argh" ,rust-argh-0.1)
+        ("rust-better-panic" ,rust-better-panic-0.3)
+        ("rust-cargo-husky" ,rust-cargo-husky-1)
+        ("rust-criterion" ,rust-criterion-0.5)
+        ("rust-fakeit" ,rust-fakeit-1)
+        ("rust-palette" ,rust-palette-0.7)
+        ("rust-pretty-assertions" ,rust-pretty-assertions-1)
+        ("rust-rand" ,rust-rand-0.8))))
+    (home-page "https://github.com/ratatui-org/ratatui")
+    (synopsis "Library for cooking up terminal user interfaces")
+    (description
+     "This package provides a library that's all about cooking up terminal user
+interfaces.")
+    (license license:expat)))
+
 (define-public rust-ratatui-0.20
   (package
+    (inherit rust-ratatui-0.24)
     (name "rust-ratatui")
     (version "0.20.1")
     (source
@@ -58127,7 +58179,6 @@ random generic types.")
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
         (base32 "0ab8w62dkp2c2l8s1xb904imqlv7aqkc07hm6bp01afbphrd1h6w"))))
-    (build-system cargo-build-system)
     (arguments
      `(#:cargo-test-flags
        '("--release" "--"
@@ -58143,15 +58194,8 @@ random generic types.")
         ("rust-termion" ,rust-termion-2)
         ("rust-unicode-segmentation" ,rust-unicode-segmentation-1)
         ("rust-unicode-width" ,rust-unicode-width-0.1))
-       #:cargo-development-inputs
-       (("rust-argh" ,rust-argh-0.1)
-        ("rust-rand" ,rust-rand-0.8))))
-    (home-page "https://github.com/ratatui-org/ratatui")
-    (synopsis "Library for cooking up terminal user interfaces")
-    (description
-     "This package provides a library that's all about cooking up terminal user
-interfaces.")
-    (license license:expat)))
+       #:cargo-development-inputs (("rust-argh" ,rust-argh-0.1)
+                                   ("rust-rand" ,rust-rand-0.8))))))
 
 (define-public rust-raw-cpuid-10
   (package
