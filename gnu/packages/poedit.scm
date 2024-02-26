6ca7b07a251739dfaefa639e74c01e3013c9454c
Marco Rimoldi <rimarko@libero.it>
2024-02-26 17:51:10 +0100

    gnu: poedit: Update to 3.5.2 and wrap executable.
    * gnu/packages/poedit.scm (poedit): Update to 3.5.2.
    [arguments]: Use G-expressions throughout.
    <phases>: Wrap the program with gettext binaries in PATH so that it does not
    crash.
    [inputs]: Add gnu-gettext (see above), nlohmann-json, python-minimal (needed
    for utility scripts).
    
    Change-Id: I09976a81ad93fa7850a4d414dbcbaa98eb778ece
    Signed-off-by: Nicolas Goaziou <mail@nicolasgoaziou.fr>

--- gnu/packages/poedit.scm
+++ gnu/packages/poedit.scm
@@ -21,16 +21,20 @@
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (gnu packages)
   #:use-module (guix packages)
+  #:use-module (guix gexp)
   #:use-module (guix git-download)
   #:use-module (guix utils)
   #:use-module (guix build-system gnu)
   #:use-module (gnu packages autotools)
+  #:use-module (gnu packages bash)
   #:use-module (gnu packages boost)
+  #:use-module (gnu packages cpp)
   #:use-module (gnu packages enchant)
   #:use-module (gnu packages gettext)
   #:use-module (gnu packages gtk)
   #:use-module (gnu packages icu4c)
   #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages python)
   #:use-module (gnu packages rdf)
   #:use-module (gnu packages wxwidgets)
   #:use-module (gnu packages xml))
@@ -38,41 +42,50 @@
 (define-public poedit
   (package
     (name "poedit")
-    (version "2.2.4")
-    (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                     (url "https://github.com/vslavik/poedit")
-                     (commit (string-append "v" version "-oss"))))
-              (file-name (git-file-name name version))
-              (sha256
-               (base32
-                "147jiiab4n0nbhzp1vw1jn8ykhy7qh6zf02654ppi0imdfvfnjss"))
-             (modules '((guix build utils)))
-             (snippet
-              '(begin
-                 (delete-file-recursively "deps")
-                 #t))))
+    (version "3.5.2")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/vslavik/poedit")
+             (commit (string-append "v" version "-oss"))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "119hhhjikzdvawldav99zzxnx7rj2ri5crq8smb2lprab8fx70hm"))
+       (modules '((guix build utils)))
+       (snippet #~(delete-file-recursively "deps"))))
     (build-system gnu-build-system)
     (arguments
-     `(#:configure-flags
-       (list (string-append "--with-boost-libdir="
-                            (assoc-ref %build-inputs "boost")
-                            "/lib"))))
-    (native-inputs
-     (list autoconf automake gettext-minimal pkg-config))
-    (inputs
-     (list boost
-           enchant
-           gtk+
-           gtkspell3
-           icu4c
-           lucene++
-           pugixml
-           wxwidgets))
+     (list
+      #:configure-flags
+      #~(list (string-append "--with-boost-libdir="
+                             #$(this-package-input "boost") "/lib"))
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-after 'install 'wrap-executable
+            (lambda _
+              (wrap-program (string-append #$output "/bin/poedit")
+                `("PATH" prefix
+                  ,(list (string-append #$(this-package-input "gettext")
+                                        "/bin")))))))))
+
+    (native-inputs (list autoconf automake pkg-config))
+    (inputs (list bash-minimal
+                  boost
+                  enchant
+                  gnu-gettext
+                  gtk+
+                  gtkspell3
+                  icu4c
+                  lucene++
+                  nlohmann-json
+                  python-minimal
+                  pugixml
+                  wxwidgets))
     (home-page "https://poedit.net/")
     (synopsis "Gettext catalog editing tool")
-    (description "Poedit is a GUI frontend to the GNU gettext utilities and
-a catalog editor/source code parser.  It helps with translating applications
-into other languages.")
+    (description
+     "Poedit is a GUI frontend to the GNU gettext utilities and a catalog
+editor/source code parser.  It helps with translating applications into other
+languages.")
     (license license:expat)))
