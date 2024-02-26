90d4cd54b7189c80d785a57cfe602f4f2ad16d43
Ricardo Wurmus <rekado@elephly.net>
2024-02-26 12:25:11 +0100

    gnu: r-secretbase: Update to 0.3.0.
    * gnu/packages/cran.scm (r-secretbase): Update to 0.3.0.
    
    Change-Id: Ic9e75a8746dd0a4d2c5a8ee64f4e242dfeda5fe5

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -3973,13 +3973,13 @@ scraping tasks, inspired by libraries like @code{BeautifulSoup}.")
 (define-public r-secretbase
   (package
     (name "r-secretbase")
-    (version "0.2.0")
+    (version "0.3.0")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "secretbase" version))
        (sha256
-        (base32 "1z0ddzdimma8rgd47fn5mma09d5c7vrl5b7gdlvfk8l047yihyb6"))))
+        (base32 "1wgbk16by9w933cff5ybb2sa0ndgj3sv9ahr2r1843vgqiyvx2xi"))))
     (properties `((upstream-name . "secretbase")))
     (build-system r-build-system)
     (home-page "https://shikokuchuo.net/secretbase/")
