44790c7c8c31e4f543c330c61a9b11175a380483
Sharlatan Hellseher <sharlatanus@gmail.com>
2024-03-06 23:15:47 +0000

    gnu: go-github-com-hashicorp-go-version: Update to 1.6.0.
    * gnu/packages/golang-xyz.scm (go-github-com-hashicorp-go-version): Update to 1.6.0.
    
    Change-Id: I964c450d42a436bf093222c90abadcaf84b2968d

--- gnu/packages/golang-xyz.scm
+++ gnu/packages/golang-xyz.scm
@@ -872,21 +872,18 @@ their component bytes.")
     (license license:mpl2.0)))
 
 (define-public go-github-com-hashicorp-go-version
-  (let ((commit
-         "03c5bf6be031b6dd45afec16b1cf94fc8938bc77")
-        (revision "0"))
     (package
       (name "go-github-com-hashicorp-go-version")
-      (version (git-version "0.0.0" revision commit))
+      (version "1.6.0")
       (source
        (origin
          (method git-fetch)
          (uri (git-reference
                (url "https://github.com/hashicorp/go-version")
-               (commit commit)))
+               (commit (string-append "v" version))))
          (file-name (git-file-name name version))
          (sha256
-          (base32 "0sjq57gpfznaqdrbyb2p0bn90g9h661cvr0jrk6ngags4pbw14ik"))))
+          (base32 "0fw6hwvjadpbfj10yk7f64ypw8lmv5s5ny3s4ria0nv6xam1wpai"))))
       (build-system go-build-system)
       (arguments
        '(#:import-path "github.com/hashicorp/go-version"))
@@ -898,7 +895,7 @@ constraints")
 constraints, and verifying versions against a set of constraints.  It can sort
 a collection of versions properly, handles prerelease/beta versions, can
 increment versions.")
-      (license license:mpl2.0))))
+      (license license:mpl2.0)))
 
 (define-public go-github-com-hhrutter-tiff
   (package
