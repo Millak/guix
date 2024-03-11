3675dc6123a23a72275fc37ad848d578cc7b2395
Efraim Flashner <efraim@flashner.co.il>
2024-03-11 13:49:57 +0200

    gnu: Add rust-gix-object-0.39.
    * gnu/packages/crates-vcs.scm (rust-gix-object-0.39): New variable.
    (rust-gix-object-0.38): Inherit from rust-gix-object-0.39.
    
    Change-Id: Iccad0b0e454e6c1d6bb0a9034b0afb5be9ecf249

--- gnu/packages/crates-vcs.scm
+++ gnu/packages/crates-vcs.scm
@@ -2531,8 +2531,46 @@ Git's negotiation algorithms.")
         ("rust-smallvec" ,rust-smallvec-1)
         ("rust-thiserror" ,rust-thiserror-1))))))
 
+(define-public rust-gix-object-0.39
+  (package
+    (name "rust-gix-object")
+    (version "0.39.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "gix-object" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0cqya07cwq0wcx2kzmxsm9acpl4mwsxwfx797z3c282phb2pkgzy"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs (("rust-bstr" ,rust-bstr-1)
+                       ("rust-btoi" ,rust-btoi-0.4)
+                       ("rust-document-features" ,rust-document-features-0.2)
+                       ("rust-gix-actor" ,rust-gix-actor-0.28)
+                       ("rust-gix-date" ,rust-gix-date-0.8)
+                       ("rust-gix-features" ,rust-gix-features-0.36)
+                       ("rust-gix-hash" ,rust-gix-hash-0.13)
+                       ("rust-gix-validate" ,rust-gix-validate-0.8)
+                       ("rust-itoa" ,rust-itoa-1)
+                       ("rust-serde" ,rust-serde-1)
+                       ("rust-smallvec" ,rust-smallvec-1)
+                       ("rust-thiserror" ,rust-thiserror-1)
+                       ("rust-winnow" ,rust-winnow-0.5))
+       #:cargo-development-inputs (("rust-criterion" ,rust-criterion-0.5)
+                                   ("rust-pretty-assertions" ,rust-pretty-assertions-1))))
+    (home-page "https://github.com/Byron/gitoxide")
+    (synopsis
+     "Immutable and mutable Git objects with decoding and encoding support")
+    (description
+     "Part of Gitoxide, a pure Rust Git implementation.  This package contains
+functions to handle immutable and mutable git objects with decoding and encoding
+support.")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-gix-object-0.38
   (package
+    (inherit rust-gix-object-0.39)
     (name "rust-gix-object")
     (version "0.38.0")
     (source
@@ -2542,7 +2580,6 @@ Git's negotiation algorithms.")
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
         (base32 "0lbaz0mzsg5vvm5qvi1nf6f0hyz62hfx18xk3h57fn3z4r22l3vl"))))
-    (build-system cargo-build-system)
     (arguments
      `(#:cargo-inputs (("rust-bstr" ,rust-bstr-1)
                        ("rust-btoi" ,rust-btoi-0.4)
@@ -2559,15 +2596,7 @@ Git's negotiation algorithms.")
                        ("rust-winnow" ,rust-winnow-0.5))
        #:cargo-development-inputs
        (("rust-criterion" ,rust-criterion-0.5)
-        ("rust-pretty-assertions" ,rust-pretty-assertions-1))))
-    (home-page "https://github.com/Byron/gitoxide")
-    (synopsis
-     "Immutable and mutable Git objects with decoding and encoding support")
-    (description
-     "Part of Gitoxide, a pure Rust Git implementation.  This package contains
-functions to handle immutable and mutable git objects with decoding and encoding
-support.")
-    (license (list license:expat license:asl2.0))))
+        ("rust-pretty-assertions" ,rust-pretty-assertions-1))))))
 
 (define-public rust-gix-object-0.37
   (package
