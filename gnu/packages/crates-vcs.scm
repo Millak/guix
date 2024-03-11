c9988562cca93eca1ffc009dc148fe7ca47a10be
Efraim Flashner <efraim@flashner.co.il>
2024-03-11 13:58:57 +0200

    gnu: Add rust-gix-submodule-0.6.
    * gnu/packages/crates-vcs.scm (rust-gix-submodule-0.6): New variable.
    (rust-gix-submodule-0.5): Inherit from rust-gix-submodule-0.6.
    
    Change-Id: I31e7bd9c68ea9bb46efd1cf2ea6be0f094a4add5

--- gnu/packages/crates-vcs.scm
+++ gnu/packages/crates-vcs.scm
@@ -3885,8 +3885,36 @@ Rust implementation of Git.")
                        ("rust-gix-pathspec" ,rust-gix-pathspec-0.3)
                        ("rust-thiserror" ,rust-thiserror-1))))))
 
+(define-public rust-gix-submodule-0.6
+  (package
+    (name "rust-gix-submodule")
+    (version "0.6.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "gix-submodule" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1y89k2c4isa9r4j9a5mim91dxx69s6ckqrl1i7mwmgcm1bvdg8q2"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs (("rust-bstr" ,rust-bstr-1)
+                       ("rust-gix-config" ,rust-gix-config-0.32)
+                       ("rust-gix-path" ,rust-gix-path-0.10)
+                       ("rust-gix-pathspec" ,rust-gix-pathspec-0.4)
+                       ("rust-gix-refspec" ,rust-gix-refspec-0.20)
+                       ("rust-gix-url" ,rust-gix-url-0.25)
+                       ("rust-thiserror" ,rust-thiserror-1))))
+    (home-page "https://github.com/Byron/gitoxide")
+    (synopsis "Functions for dealing with Git submodules")
+    (description
+     "Functions for dealing with Git submodules.  Part of Gitoxide a pure Rust
+implementation of Git.")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-gix-submodule-0.5
   (package
+    (inherit rust-gix-submodule-0.6)
     (name "rust-gix-submodule")
     (version "0.5.0")
     (source
@@ -3896,7 +3924,6 @@ Rust implementation of Git.")
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
         (base32 "1hm1d7a9qb3zylln44bxcnmdy27zfajc6gj5g00kf95a2a6qr9xv"))))
-    (build-system cargo-build-system)
     (arguments
      `(#:tests? #f  ; undeclared crate gix_testtools
        #:cargo-inputs (("rust-bstr" ,rust-bstr-1)
@@ -3905,13 +3932,7 @@ Rust implementation of Git.")
                        ("rust-gix-pathspec" ,rust-gix-pathspec-0.4)
                        ("rust-gix-refspec" ,rust-gix-refspec-0.19)
                        ("rust-gix-url" ,rust-gix-url-0.25)
-                       ("rust-thiserror" ,rust-thiserror-1))))
-    (home-page "https://github.com/Byron/gitoxide")
-    (synopsis "Functions for dealing with Git submodules")
-    (description
-     "Functions for dealing with Git submodules.  Part of Gitoxide a pure Rust
-implementation of Git.")
-    (license (list license:expat license:asl2.0))))
+                       ("rust-thiserror" ,rust-thiserror-1))))))
 
 (define-public rust-gix-submodule-0.4
   (package
