74066baa0bf29d4cbf59b013257782300d2b3464
Ricardo Wurmus <rekado@elephly.net>
2024-03-15 23:05:32 +0100

    gnu: r-gsva: Update to 1.50.1.
    * gnu/packages/bioconductor.scm (r-gsva): Update to 1.50.1.
    
    Change-Id: Ib298992cb9ea4615fe3888e799c332e778a7b5be

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -8221,13 +8221,13 @@ Enrichment Analysis} (GSEA).")
 (define-public r-gsva
   (package
     (name "r-gsva")
-    (version "1.50.0")
+    (version "1.50.1")
     (source (origin
               (method url-fetch)
               (uri (bioconductor-uri "GSVA" version))
               (sha256
                (base32
-                "01w5j7fmi3hsfd282kcn4v41mi58z2yfhapff2zgf9swdfps4m6z"))))
+                "0ld0bbl6ar3yhi6ncg9d8q60hg4m4v5kphl044fw63l19ixln7cf"))))
     (properties `((upstream-name . "GSVA")))
     (build-system r-build-system)
     (propagated-inputs (list r-biobase
