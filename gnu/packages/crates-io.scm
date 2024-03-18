c3701f079a6c5a253632bed1fdf760f7c07588ce
Efraim Flashner <efraim@flashner.co.il>
2024-03-18 14:42:38 +0200

    gnu: Add rust-cargo-toml-0.19.
    * gnu/packages/crates-io.scm (rust-cargo-toml-0.19): New variable.
    (rust-cargo-toml-0.16): Inherit from rust-cargo-toml-0.19.
    
    Change-Id: Id5e836bf33da064f3fc7ec2475c7dbe98db52b5c

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -10975,17 +10975,17 @@ optional dependency graph analysis.")
 supported by Cargo.")
     (license (list license:expat license:asl2.0))))
 
-(define-public rust-cargo-toml-0.16
+(define-public rust-cargo-toml-0.19
   (package
     (name "rust-cargo-toml")
-    (version "0.16.3")
+    (version "0.19.2")
     (source
      (origin
        (method url-fetch)
        (uri (crate-uri "cargo_toml" version))
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
-        (base32 "1lxgiwha2kqbk60iq5cvczbnd5xrqa4cj7bqk6k8wf64qsdn5yg3"))))
+        (base32 "1gljfkdjx07fisn5xkklv56ki3p49ppf8fkry7c1psx28bgmd0x9"))))
     (build-system cargo-build-system)
     (arguments
      `(#:cargo-inputs (("rust-serde" ,rust-serde-1)
@@ -10995,6 +10995,22 @@ supported by Cargo.")
     (description "This package provides `Cargo.toml` struct definitions for
 parsing with Serde.")
     (license (list license:asl2.0 license:expat))))
+
+(define-public rust-cargo-toml-0.16
+  (package
+    (inherit rust-cargo-toml-0.19)
+    (name "rust-cargo-toml")
+    (version "0.16.3")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "cargo_toml" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1lxgiwha2kqbk60iq5cvczbnd5xrqa4cj7bqk6k8wf64qsdn5yg3"))))
+    (arguments
+     `(#:cargo-inputs (("rust-serde" ,rust-serde-1)
+                       ("rust-toml" ,rust-toml-0.8))))))
 
 (define-public rust-cargo-util-0.2
   (package
