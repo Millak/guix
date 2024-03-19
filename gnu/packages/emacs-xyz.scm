2469b040a44c7a050933575326f9316e5ab7412c
Suhail Singh <suhailsingh247@gmail.com>
2024-03-19 08:25:00 -0400

    gnu: emacs-ox-tufte: Update to 4.2.0.
    * gnu/packages/emacs-xyz.scm (emacs-ox-tufte): Update to 4.2.0.
    
    Change-Id: Ib9584373cfed50c6c8195e9770a9953fb5b5a4e7
    Signed-off-by: Christopher Baines <mail@cbaines.net>

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -33695,7 +33695,7 @@ simple but powerful Org contents.")
 (define-public emacs-ox-tufte
   (package
     (name "emacs-ox-tufte")
-    (version "4.1.1")
+    (version "4.2.0")
     (source
      (origin
        (method git-fetch)
@@ -33704,7 +33704,7 @@ simple but powerful Org contents.")
              (commit version)))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "0nmi6p19sg2vl64znm356bchphbybq03jyjsnqhddb39c4pilxah"))))
+        (base32 "0rdainmx0xg6rfql7rai75x0v2r2s1snjy6b61jd53caif7aigbk"))))
     (build-system emacs-build-system)
     (arguments
      (list
