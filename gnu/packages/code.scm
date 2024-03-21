7f4cd6c50c7d92e90dc64bb0688e1be8ce19555e
Efraim Flashner <efraim@flashner.co.il>
2024-03-21 14:08:05 +0200

    gnu: global: Update to 6.6.12.
    * gnu/packages/code.scm (global): Update to 6.6.12.
    
    Change-Id: If073f74f55199bfbc9f29b95e9e3db3a6cf00548

--- gnu/packages/code.scm
+++ gnu/packages/code.scm
@@ -183,14 +183,14 @@ highlighting your own code that seemed comprehensible when you wrote it.")
 (define-public global                             ; a global variable
   (package
     (name "global")
-    (version "6.6.10")
+    (version "6.6.12")
     (source (origin
              (method url-fetch)
              (uri (string-append "mirror://gnu/global/global-"
                                  version ".tar.gz"))
              (sha256
               (base32
-               "1s6c9nzpp4jfq14l3mk9fnyipizljkka8hdr1wwh2g798nlydl9d"))))
+               "00ari03n0wdzz1ib73p64h8g1p0a9js61fxl92jyq50fhh35najl"))))
     (build-system gnu-build-system)
     (arguments
      (list #:configure-flags
