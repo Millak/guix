048e5a069279b619fad335f3f0ff69dfacfe2612
David Elsing <david.elsing@posteo.net>
2024-03-23 22:05:02 +0000

    gnu: xnnpack: Update to commit 51a9875.
    The tests which fail to build if building the shared library are removed by a
    patch. Autogenerated files are removed by a snippet and generated in a
    separate build phase.
    
    * gnu/packages/machine-learning.scm (xnnpack): Update to commit 51a9875.
    [source]: Add patch for tests. Add snippet to remove autogenerated files.
    [arguments]: New modules and phase arguments.
    [inputs]: Add clog and cpuinfo.
    [native-inputs]: New field.
    * gnu/packages/patches/xnnpack-remove-broken-tests.patch: Add file.
    * gnu/local.mk (dist_patch_DATA): Add it.
    * gnu/packages/patches/xnnpack-system-libraries.patch: Update patch.

--- gnu/local.mk
+++ gnu/local.mk
@@ -2253,6 +2253,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/xfce4-settings-defaults.patch		\
   %D%/packages/patches/xgboost-use-system-dmlc-core.patch       \
   %D%/packages/patches/xmonad-dynamic-linking.patch		\
+  %D%/packages/patches/xnnpack-remove-broken-tests.patch	\
   %D%/packages/patches/xnnpack-system-libraries.patch		\
   %D%/packages/patches/xnnpack-for-torch2-system-libraries.patch	\
   %D%/packages/patches/xplanet-1.3.1-cxx11-eof.patch		\
