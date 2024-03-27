9c60bff2b086da964ef685cdb38a457cfc6a7b32
Efraim Flashner <efraim@flashner.co.il>
2024-03-27 10:27:24 +0200

    gnu: Add rust-gix-attributes-0.21.
    * gnu/packages/crates-vcs.scm (rust-gix-attributes-0.21): New variable.
    (rust-gix-attributes-0.20): Inherit from rust-gix-attributes-0.21.
    
    Change-Id: I7344bcbefd31f75607865aab4a650eb6db7f8e96

--- gnu/packages/crates-vcs.scm
+++ gnu/packages/crates-vcs.scm
@@ -965,22 +965,22 @@ Rust implementation of Git.")
                        ("rust-time" ,rust-time-0.3)
                        ("rust-zip" ,rust-zip-0.6))))))
 
-(define-public rust-gix-attributes-0.20
+(define-public rust-gix-attributes-0.21
   (package
     (name "rust-gix-attributes")
-    (version "0.20.1")
+    (version "0.21.1")
     (source
      (origin
        (method url-fetch)
        (uri (crate-uri "gix-attributes" version))
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
-        (base32 "109kciz3cssfbx9zgslngdrkzwf3zd9mlv0srm3yqxlcsdlm8f8g"))))
+        (base32 "066y16rw994wrvvhv1xwf5gnc51bh727jxyql7wzrkkb7mhffvdx"))))
     (build-system cargo-build-system)
     (arguments
      `(#:cargo-inputs (("rust-bstr" ,rust-bstr-1)
                        ("rust-document-features" ,rust-document-features-0.2)
-                       ("rust-gix-glob" ,rust-gix-glob-0.14)
+                       ("rust-gix-glob" ,rust-gix-glob-0.15)
                        ("rust-gix-path" ,rust-gix-path-0.10)
                        ("rust-gix-quote" ,rust-gix-quote-0.4)
                        ("rust-gix-trace" ,rust-gix-trace-0.1)
@@ -995,6 +995,31 @@ Rust implementation of Git.")
      "This package provides a crate from the Gitoxide project dealing with
 @code{.gitattributes} files.")
     (license (list license:expat license:asl2.0))))
+
+(define-public rust-gix-attributes-0.20
+  (package
+    (inherit rust-gix-attributes-0.21)
+    (name "rust-gix-attributes")
+    (version "0.20.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "gix-attributes" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "109kciz3cssfbx9zgslngdrkzwf3zd9mlv0srm3yqxlcsdlm8f8g"))))
+    (arguments
+     `(#:cargo-inputs (("rust-bstr" ,rust-bstr-1)
+                       ("rust-document-features" ,rust-document-features-0.2)
+                       ("rust-gix-glob" ,rust-gix-glob-0.14)
+                       ("rust-gix-path" ,rust-gix-path-0.10)
+                       ("rust-gix-quote" ,rust-gix-quote-0.4)
+                       ("rust-gix-trace" ,rust-gix-trace-0.1)
+                       ("rust-kstring" ,rust-kstring-2)
+                       ("rust-serde" ,rust-serde-1)
+                       ("rust-smallvec" ,rust-smallvec-1)
+                       ("rust-thiserror" ,rust-thiserror-1)
+                       ("rust-unicode-bom" ,rust-unicode-bom-2))))))
 
 (define-public rust-gix-attributes-0.19
   (package
