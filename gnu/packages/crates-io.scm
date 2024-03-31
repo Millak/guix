8181d66d0329cd482f88e5524545c518565ad0d2
Efraim Flashner <efraim@flashner.co.il>
2024-03-31 14:40:43 +0300

    gnu: Add rust-uu-mv-0.0.23.
    * gnu/packages/crates-io.scm (rust-uu-mv-0.0.23): New variable.
    
    Change-Id: Id31032ea535ac9b57500f7b79bef047a488f2286

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -85045,6 +85045,28 @@ first byte.")
         (base32
          "0zamsj2986shm4x9zncjf2m5qy9scaw7qnxw4f89b2afpg6a8wl7"))))))
 
+(define-public rust-uu-mv-0.0.23
+  (package
+    (name "rust-uu-mv")
+    (version "0.0.23")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "uu_mv" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1z2cdcnf1q6zgzycimlbg0xyqh43v8x49vsr0sx1sbjdsdhpwc2y"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs (("rust-clap" ,rust-clap-4)
+                       ("rust-fs-extra" ,rust-fs-extra-1)
+                       ("rust-indicatif" ,rust-indicatif-0.17)
+                       ("rust-uucore" ,rust-uucore-0.0.25))))
+    (home-page "https://github.com/uutils/coreutils")
+    (synopsis "mv ~ (uutils) move (rename) SOURCE to DESTINATION")
+    (description "This package provides the @code{mv} command from uutils.")
+    (license license:expat)))
+
 (define-public rust-uu-whoami-0.0.23
   (package
     (name "rust-uu-whoami")
