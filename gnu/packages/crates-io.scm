579c0dfdfc761dd52c1c3e661337b5c535e88624
Efraim Flashner <efraim@flashner.co.il>
2024-03-31 20:45:52 +0300

    gnu: Add rust-embedded-io-0.6.
    * gnu/packages/crates-io.scm (rust-embedded-io-0.6): New variable.
    (rust-embedded-io-0.4): Inherit from rust-embedded-io-0.6.
    
    Change-Id: Ibabc9de0336c5e1b7e9f5cebec21d91525a20627

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -23033,8 +23033,28 @@ Emacs' support for dynamic modules.")
      "This crate provides an implementation of RFC 5322 email messages.")
     (license license:expat)))
 
+(define-public rust-embedded-io-0.6
+  (package
+    (name "rust-embedded-io")
+    (version "0.6.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "embedded-io" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0v901xykajh3zffn6x4cnn4fhgfw3c8qpjwbsk6gai3gaccg3l7d"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs (("rust-defmt" ,rust-defmt-0.3))))
+    (home-page "https://github.com/rust-embedded/embedded-hal")
+    (synopsis "Embedded IO traits")
+    (description "This package provides embedded IO traits.")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-embedded-io-0.4
   (package
+    (inherit rust-embedded-io-0.6)
     (name "rust-embedded-io")
     (version "0.4.0")
     (source
@@ -23044,16 +23064,11 @@ Emacs' support for dynamic modules.")
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
         (base32 "1v9wrc5nsgaaady7i3ya394sik5251j0iq5rls7mrx7fv696h6pg"))))
-    (build-system cargo-build-system)
     (arguments
      `(#:cargo-inputs (("rust-defmt" ,rust-defmt-0.3)
                        ("rust-futures" ,rust-futures-0.3)
                        ("rust-log" ,rust-log-0.4)
-                       ("rust-tokio" ,rust-tokio-1))))
-    (home-page "https://github.com/rust-embedded/embedded-hal")
-    (synopsis "Embedded IO traits")
-    (description "This package provides embedded IO traits.")
-    (license (list license:expat license:asl2.0))))
+                       ("rust-tokio" ,rust-tokio-1))))))
 
 (define-public rust-emojis-0.5
   (package
