a0ed61d7ff6ade9066983cdeb84c3608e4da33db
Ricardo Wurmus <rekado@elephly.net>
2024-04-02 11:01:51 +0200

    gnu: r-bionero: Update to 1.10.3.
    * gnu/packages/bioconductor.scm (r-bionero): Update to 1.10.3.
    
    Change-Id: I03b6f9259c05a6915d3e5425ab9165067c400f03

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -23974,14 +23974,14 @@ using aCGH or sequencing.")
 (define-public r-bionero
   (package
     (name "r-bionero")
-    (version "1.10.2")
+    (version "1.10.3")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "BioNERO" version))
        (sha256
         (base32
-         "1fk48h2v0snsg8y9pi0pav69wa95s8a8gk1rxsvrm7z1bcgi2j51"))))
+         "1vf3kq4lj4alvwzf0wl09lxb0ky6q0x94ajmrnihlj5saliqb73c"))))
     (properties `((upstream-name . "BioNERO")))
     (build-system r-build-system)
     (propagated-inputs
