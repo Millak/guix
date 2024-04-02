50ae661dca2b37ed5c5e38b50aab1aa977101186
Ricardo Wurmus <rekado@elephly.net>
2024-04-02 10:04:27 +0200

    gnu: r-collapse: Update to 2.0.12.
    * gnu/packages/cran.scm (r-collapse): Update to 2.0.12.
    
    Change-Id: Id30326eaddfb6d546524db366160ce39905527c8

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -537,13 +537,13 @@ labels on the node can also be achieved.")
 (define-public r-collapse
   (package
     (name "r-collapse")
-    (version "2.0.11")
+    (version "2.0.12")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "collapse" version))
        (sha256
-        (base32 "0aaqzipgnln6mcwgahlnvg0dhq82xnzrfn95mv4ywgpczaicwg3s"))))
+        (base32 "0dplfsj4lqafw0zkrqhph9sk22qpkm5i753pgwf9w9cq5d17f537"))))
     (properties `((upstream-name . "collapse")))
     (build-system r-build-system)
     (propagated-inputs (list r-rcpp))
