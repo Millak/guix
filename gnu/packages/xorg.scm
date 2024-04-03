e8dba8afffeb5cfc832b4898afbe1df2470450bf
John Kehayias <john.kehayias@protonmail.com>
2024-04-03 15:45:56 -0400

    gnu: xorg-server-xwayland: Update to 23.2.5 [security fixes].
    Fixes CVE-2024-31080, CVE-2024-31081, and CVE-2024-31083.  See the X.Org
    Security Advisory, <https://lists.x.org/archives/xorg/2024-April/061615.html>,
    for details.
    
    * gnu/packages/xorg.scm (xorg-server-xwayland): Update to 23.2.5.
    
    Change-Id: I8d6b8f65bb6d06374a1ea925b5ff2a399a3efeac

--- gnu/packages/xorg.scm
+++ gnu/packages/xorg.scm
@@ -36,7 +36,7 @@
 ;;; Copyright © 2021 Lu Hui <luhux76@gmail.com>
 ;;; Copyright © 2023 Zheng Junjie <873216071@qq.com>
 ;;; Copyright © 2023 Janneke Nieuwenhuizen <janneke@gnu.org>
-;;; Copyright © 2023 John Kehayias <john.kehayias@protonmail.com>
+;;; Copyright © 2023, 2024 John Kehayias <john.kehayias@protonmail.com>
 ;;; Copyright © 2023, 2024 Kaelyn Takata <kaelyn.alexi@protonmail.com>
 ;;;
 ;;; This file is part of GNU Guix.
@@ -5274,7 +5274,7 @@ EGLStream families of extensions.")
 (define-public xorg-server-xwayland
   (package
     (name "xorg-server-xwayland")
-    (version "23.2.4")
+    (version "23.2.5")
     (source
      (origin
        (method url-fetch)
@@ -5282,7 +5282,7 @@ EGLStream families of extensions.")
                            "/xserver/xwayland-" version ".tar.xz"))
        (sha256
         (base32
-         "0sxlh43cnpf56p2p5jnhp7427knfpy42mcka7f5hjcqddndib7m9"))))
+         "145xykwmyqkaa8zrbn5fnvnff67iral9mc5raamglnbsd3r7zv1k"))))
     (inputs (list font-dejavu
                   dbus
                   egl-wayland
