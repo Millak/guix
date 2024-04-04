9280f9f9b56f0debec10e2b688ba3e703484dd5b
Ricardo Wurmus <rekado@elephly.net>
2024-04-04 20:33:08 +0200

    gnu: python-mistletoe: Add missing inputs.
    * gnu/packages/markup.scm (python-mistletoe)[native-inputs]: Add
    python-setuptools and python-wheel.
    
    Change-Id: I01d0045855f85c2300ad987caf6d0345cf25599a

--- gnu/packages/markup.scm
+++ gnu/packages/markup.scm
@@ -14,6 +14,7 @@
 ;;; Copyright © 2023 Zheng Junjie <873216071@qq.com>
 ;;; Copyright © 2023 Nicolas Graves <ngraves@ngraves.fr>
 ;;; Copyright © 2024 Janneke Nieuwenhuizen <janneke@gnu.org>
+;;; Copyright © 2024 Ricardo Wurmus <rekado@elephly.net>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -57,6 +58,7 @@
   #:use-module (gnu packages perl-compression)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages python)
+  #:use-module (gnu packages python-build)
   #:use-module (gnu packages python-xyz)
   #:use-module (gnu packages tex)
   #:use-module (gnu packages web)
@@ -628,7 +630,11 @@ with a few extensions.")
     (arguments
      ;; FileNotFoundError (not distributed in PyPI).
      (list #:test-flags #~(list "-k" "not test_main")))
-    (native-inputs (list python-parameterized python-pytest))
+    (native-inputs
+     (list python-parameterized
+           python-pytest
+           python-setuptools
+           python-wheel))
     (home-page "https://github.com/miyuchina/mistletoe")
     (synopsis "Extensible Markdown parser in pure Python")
     (description
