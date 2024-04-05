45d2ee1bde272d196daab4a203a28f3b83d11fc1
Herman Rimm <herman@rimm.ee>
2024-04-05 21:35:45 +0200

    gnu: Add ipp-usb.
    * gnu/packages/printers.scm (ipp-usb): Add variable.
    
    Change-Id: I3abc86eb10c9b74ef073e1ead8bd51a91de5670f
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/printers.scm
+++ gnu/packages/printers.scm
@@ -1,6 +1,7 @@
 ;;; GNU Guix --- Functional package management for GNU
 ;;; Copyright © 2018 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2018 Tobias Geerinckx-Rice <me@tobias.gr>
+;;; Copyright © 2024 Herman Rimm <herman@rimm.ee>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -21,13 +22,51 @@
   #:use-module (guix packages)
   #:use-module (guix git-download)
   #:use-module (guix build-system gnu)
+  #:use-module (guix build-system go)
   #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (gnu packages avahi)
+  #:use-module (gnu packages golang-xyz)
   #:use-module (gnu packages libusb)
+  #:use-module (gnu packages man)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages qt))
 
 ;; This is a module for packages related to printer-like devices, but not
 ;; related to CUPS.
+
+(define-public ipp-usb
+  (package
+    (name "ipp-usb")
+    (version "0.9.25")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/OpenPrinting/ipp-usb")
+             (commit version)))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "003njvcxi0w97wrs52nm9b0f1d96624hjnv9ywsr1m7p6q7r08mg"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      ;; TODO: The project provides manpages and udev rules, review them and
+      ;; install in the next update cycle.
+      #:install-source? #f
+      #:import-path "github.com/OpenPrinting/ipp-usb"))
+    (inputs
+     (list avahi libusb))
+    (native-inputs
+     (list go-github-com-openprinting-goipp
+           pkg-config
+           ronn))
+    (home-page "https://github.com/OpenPrinting/ipp-usb")
+    (synopsis "HTTP reverse proxy, backed by the IPP-over-USB connection")
+    (description
+     "ipp-usb implements an HTTP reverse proxy, backed by the IPP-over-USB
+connection to the device. This is because IPP-over-USB implementations which
+simply relay a TCP connection to USB do not work.")
+    (license license:bsd-2)))
 
 (define-public robocut
   (package
