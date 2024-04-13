ada945251cd31dbffc4e8250f41343294cfaa1b4
Sharlatan Hellseher <sharlatanus@gmail.com>
2024-04-13 23:58:31 +0100

    gnu: Add go-github-com-montanaflynn-stats.
    * gnu/packages/golang-maths.scm (go-github-com-montanaflynn-stats): New variable.
    
    Change-Id: I1e131cb4ceece61e5b822178831014ed27189d9e

--- gnu/packages/golang-maths.scm
+++ gnu/packages/golang-maths.scm
@@ -1,4 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2024 Sharlatan Hellseher <sharlatanus@gmail.com>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -32,6 +33,29 @@
 ;;;
 ;;; Libraries:
 ;;;
+
+(define-public go-github-com-montanaflynn-stats
+  (package
+    (name "go-github-com-montanaflynn-stats")
+    (version "0.7.1")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/montanaflynn/stats")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0y38cvp7r6fb6291k82j781dbykx00mxw8ca0v9d0fijzc1x81fi"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:import-path "github.com/montanaflynn/stats"))
+    (home-page "https://github.com/montanaflynn/stats")
+    (synopsis "Statistics library for Golang")
+    (description
+     "This package provides a statistical library for Golang.")
+    (license license:expat)))
 
 ;;;
 ;;; Executables:
