2a622090fad2b65922669340b77bf470a4111da4
Janneke Nieuwenhuizen <janneke@gnu.org>
2024-04-14 09:45:06 +0200

    Revert "maint: Generate 'doc/version-LANG.texi' reproducibly."
    Using `build-aux/mdate-from-git.scm' makes this no longer necessary.
    
    This reverts commit 0e4ead187d83a958ca0deb54857c04967e84d68b.
    
    Change-Id: I9177828f90fa7f7e256bc72fdff35a2fab355780

--- doc/local.mk
+++ doc/local.mk
@@ -260,32 +260,26 @@ endif
 
 # Reproducible tarball
 
-# Define a rule to build `version[LANG].texi' reproducibly using metadata from
-# Git rather than using metadata from the filesystem.
-define version.texi-from-git
-$(srcdir)/doc/stamp-$(1): $(srcdir)/$(2) $(top_srcdir)/configure
-	$$(AM_V_GEN)set -e;						\
-	export LANG=C LANGUAGE=C LC_ALL=C LC_TIME=C;			\
-	export TZ=UTC0;							\
-	timestamp="$$$$(git log --pretty=format:%ct -n1 -- "$$<"	\
-		2>/dev/null						\
-		|| echo $$(SOURCE_DATE_EPOCH))"				\
-	dmy=$$$$(date --date="@$$$$timestamp" "+%-d %B %Y");		\
-	my=$$$$(date --date="@$$$$timestamp" "+%B %Y");			\
-	{ echo "@set UPDATED $$$$dmy";					\
-	  echo "@set UPDATED-MONTH $$$$my";				\
-	  echo "@set EDITION $$$(VERSION)";				\
-	  echo "@set VERSION $$$(VERSION)"; } > "$$@-t";		\
-	mv "$$@-t" "$$@";						\
-	cp -p "$$@" "$$(srcdir)/doc/version$(3).texi"
-endef
-
-# Generate rules for stamp-vti and stamp-N that create version.texi and
-# version-LANG.texi to override the Autotools versions that use timestamps
-# embedded in the file-system.  These are expected to generate warnings:
+# Generate 'version.texi' reproducibly using metadata from Git rather than
+# using metadata from the filesystem.  This is expected to generate warnings:
 #
 #   Makefile:7376: warning: overriding recipe for target 'doc/stamp-vti'
 #   Makefile:5098: warning: ignoring old recipe for target 'doc/stamp-vti'
+$(srcdir)/doc/stamp-vti: $(srcdir)/doc/guix.texi $(top_srcdir)/configure
+	$$(AM_V_GEN)set -e;						\
+	export LANG=C LANGUAGE=C LC_ALL=C LC_TIME=C; 			\
+	export TZ=UTC0;							\
+	timestamp=$$(git log --pretty=format:%ct -n1 -- $< 2>/dev/null	\
+		|| echo $(SOURCE_DATE_EPOCH))				\
+	dmy=$$(date --date="@$$timestamp" "+%-d %B %Y");		\
+	my=$$(date --date="@$$timestamp" "+%B %Y");			\
+	{ echo "@set UPDATED $$dmy";					\
+	  echo "@set UPDATED-MONTH $$my";				\
+	  echo "@set EDITION $(VERSION)";				\
+	  echo "@set VERSION $(VERSION)"; } > $@-t;			\
+	mv $@-t $@;							\
+	cp $@ $(srcdir)/doc/version.texi
+
 i:=0
 $(eval $(call version.texi-from-git,vti,doc/guix.texi,))
 $(foreach lang, $(MANUAL_LANGUAGES),							\
