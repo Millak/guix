a45b2c7c63c6af9c42698e12b6809c41c0926de3
Ashish SHUKLA <ashish.is@lostca.se>
2024-04-15 20:25:12 +0000

    gnu: Add fnc.
    * gnu/packages/version-control.scm (fnc): New variable.
    
    Change-Id: Idf1cb254c3ae18bacb86bf020511a5d34e2d3684
    Signed-off-by: Christopher Baines <mail@cbaines.net>

--- gnu/packages/version-control.scm
+++ gnu/packages/version-control.scm
@@ -2839,6 +2839,34 @@ output of the @code{git} command.")
 modification time.")
     (license license:bsd-2)))
 
+(define-public fnc
+  (package
+    (name "fnc")
+    (version "0.16")
+    (source (origin
+              (method url-fetch)
+              (uri
+               (string-append "https://fnc.bsdbox.org/uv/dl/fnc-"
+                              version ".tar.gz"))
+              (sha256
+               (base32
+                "1npnbdz5i4p61ri76vx6awggbc0q19y8b26l3sy4wxmaxkly7gwy"))))
+    (build-system gnu-build-system)
+    (arguments
+     (list
+      #:phases #~(modify-phases %standard-phases
+                   (delete 'configure))
+      #:tests? #f                       ; no tests
+      #:make-flags #~(list (string-append "CC=" #$(cc-for-target))
+                           (string-append "PREFIX=" #$output))))
+    (inputs (list ncurses zlib sqlite-next))
+    (home-page "https://fnc.bsdbox.org")
+    (synopsis "Interactive text-based user interface for Fossil")
+    (description "fnc uses ncurses and libfossil to create a fossil user
+interface in the terminal.  It can view local changes at the hunk level to
+prepare atomic commits.")
+    (license license:isc)))
+
 (define-public myrepos
   (package
     (name "myrepos")
