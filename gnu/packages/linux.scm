9bf19720b961c5f5461a2f24fe178b82e46c2b37
Wilko Meyer <w@wmeyer.eu>
2024-04-30 10:46:25 +0200

    gnu: linux-libre 6.1: Update to 6.1.89.
    * gnu/packages/linux.scm (linux-libre-6.1-version): Update to 6.1.89.
      (linux-libre-6.1-pristine-source): Update hashes.
    
    Change-Id: Id48a19cc0d59b11bf17179eb5dd860ed37ac35dc
    Signed-off-by: Leo Famulari <leo@famulari.name>

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -538,7 +538,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
                             (%upstream-linux-source version hash)
                             deblob-scripts-6.6)))
 
-(define-public linux-libre-6.1-version "6.1.88")
+(define-public linux-libre-6.1-version "6.1.89")
 (define-public linux-libre-6.1-gnu-revision "gnu")
 (define deblob-scripts-6.1
   (linux-libre-deblob-scripts
@@ -548,7 +548,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
    (base32 "0104m61mqhlmsjjprj51njwbffjcqgjln5bf1wknb6y3iiazl6ng")))
 (define-public linux-libre-6.1-pristine-source
   (let ((version linux-libre-6.1-version)
-        (hash (base32 "1lgqclvcxfs5c4my6fh041gxzgzz8i64cw43cf5ichy58pyh4sb9")))
+        (hash (base32 "06a74sq0isgh9zc6p5cqc64r8k05f1p1w87lx971v3b1jbhbifhj")))
    (make-linux-libre-source version
                             (%upstream-linux-source version hash)
                             deblob-scripts-6.1)))
