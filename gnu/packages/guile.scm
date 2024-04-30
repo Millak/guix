Millak/guix

commit 7cef6b7ba555a9dfaf6d09cb7e112b0df77d5141

gnu: guile-lzlib: Update to 0.3.0.
Fixes <https://issues.guix.gnu.org/69596>.

* gnu/packages/guile.scm (guile-lzlib): Update to 0.3.0.
[source]: Switch to ‘git-fetch’.

Change-Id: Ic9f8fe2d0ab24b66cfba618840773a3e89aea48c

gnu/packages/guile.scm
@@ -922,17 +922,16 @@ Guile's foreign function interface.")
 (define-public guile-lzlib
   (package
     (name "guile-lzlib")
-    (version "0.0.2")
+    (version "0.3.0")
+    (home-page "https://notabug.org/guile-lzlib/guile-lzlib")
     (source
      (origin
-       (method url-fetch)
-       (uri
-        (string-append "https://notabug.org/guile-lzlib/guile-lzlib/archive/"
-                       version ".tar.gz"))
-       (file-name (string-append name "-" version ".tar.gz"))
+       (method git-fetch)
+       (uri (git-reference (url home-page) (commit version)))
+       (file-name (git-file-name name version))
        (sha256
         (base32
-         "11sggvncyx08ssp1s5xii4d6nskh1qwqihnbpzzvkrs7sivxn8w6"))))
+         "19870njb3q5h6zy239gvra92ji077c6s8xm0hgcn42z74q5wqnk6"))))
     (build-system gnu-build-system)
     (arguments
      '(#:make-flags
@@ -944,7 +943,6 @@ Guile's foreign function interface.")
      "This package provides Guile bindings for lzlib, a C library for
 in-memory LZMA compression and decompression.  The bindings are written in
 pure Scheme by using Guile's foreign function interface.")
-    (home-page "https://notabug.org/guile-lzlib/guile-lzlib")
     (license license:gpl3+)))
 
 (define-public guile2.2-lzlib

commit df3d30819e650a490ef39dd6692740bb13263c75

gnu: guile-lzlib: Switch to ‘url-fetch’.
Fixes <https://issues.guix.gnu.org/70659>.

Fixes a regression introduced in
7cef6b7ba555a9dfaf6d09cb7e112b0df77d5141, which would create a circular
dependency on systems lacking “builtin:git-download”.

* gnu/packages/guile.scm (guile-lzlib)[source]: Switch to ‘git-fetch’.

Reported-by: Peacememories <peacememories@posteo.net>
Change-Id: Iad684564d0dc18f0a7a76e1c4c6464ba4f39f2e8

gnu/packages/guile.scm
@@ -926,12 +926,17 @@ Guile's foreign function interface.")
     (home-page "https://notabug.org/guile-lzlib/guile-lzlib")
     (source
      (origin
-       (method git-fetch)
-       (uri (git-reference (url home-page) (commit version)))
-       (file-name (git-file-name name version))
+       ;; Note: Until "builtin:git-download" can be taken for granted, this
+       ;; must be 'url-fetch', not 'git-fetch', to avoid a circular dependency
+       ;; with the 'git-fetch' derivation on systems that lack
+       ;; "builtin:git-download".
+       (method url-fetch)
+       (uri (string-append home-page "/archive/" version ".tar.gz"))
+       ;; content nar-sha256: 19870njb3q5h6zy239gvra92ji077c6s8xm0hgcn42z74q5wqnk6
+       (file-name (string-append "guile-lzlib-" version ".tar.gz"))
        (sha256
         (base32
-         "19870njb3q5h6zy239gvra92ji077c6s8xm0hgcn42z74q5wqnk6"))))
+         "1whgmwkr1v8m63p4aaqn8blwl9vcrswwhbfv4bm0aghl5a6rryd7"))))
     (build-system gnu-build-system)
     (arguments
      '(#:make-flags