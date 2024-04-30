07a814fdcfef39becbc105e2a849e551a65a2e16
Rodion Goritskov <rodion.goritskov@gmail.com>
2024-05-01 00:42:43 +0400

    gnu: Add go-github-com-x448-float16.
    * gnu/packages/golang-maths.scm (go-github-com-x448-float16): New variable.
    
    Change-Id: I02d8b8b6fea19f6a7b882cfa5d0165d7751f76bf
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/golang-maths.scm
+++ gnu/packages/golang-maths.scm
@@ -1,4 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2024 Rodion Goritskov <rodion.goritskov@gmail.com>
 ;;; Copyright © 2024 Sharlatan Hellseher <sharlatanus@gmail.com>
 ;;;
 ;;; This file is part of GNU Guix.
@@ -86,6 +87,33 @@ Features:
 @item database/sql serialization/deserialization
 @item JSON and XML serialization/deserialization
 @end itemize")
+    (license license:expat)))
+
+(define-public go-github-com-x448-float16
+  (package
+    (name "go-github-com-x448-float16")
+    (version "0.8.4")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/x448/float16")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0qg6ya30fra20hpa2qzqqzs8l95lvw9yzd87fdzq195xqi6crb2l"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:import-path "github.com/x448/float16"))
+    (home-page "https://github.com/x448/float16")
+    (synopsis "Float16 (Binary16) in Golang")
+    (description
+     "This package provides a
+@url{https://en.wikipedia.org/wiki/Half-precision_floating-point_format,IEEE
+754 half-precision floating-point format (binary16)} with IEEE 754 default
+rounding for conversions.  IEEE 754-2008 refers to this 16-bit floating-point
+format as binary16.")
     (license license:expat)))
 
 ;;;
