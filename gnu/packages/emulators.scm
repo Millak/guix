721e44d419cbea0a071d3b86ac2dedcc65b712e2
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2024-05-05 15:33:42 -0400

    gnu: mupen64plus-audio-sdl: Fix synopsis.
    * gnu/packages/emulators.scm (mupen64plus-audio-sdl): Fix synopsis.
    
    Change-Id: I08b2778cf625699d72d5fbab2b23d86ff06a1be0

--- gnu/packages/emulators.scm
+++ gnu/packages/emulators.scm
@@ -920,7 +920,7 @@ core library.")
        ;; There are no tests.
        #:tests? #f))
     (home-page "https://www.mupen64plus.org/")
-    (synopsis "Mupen64Plus SDL input plugin")
+    (synopsis "Mupen64Plus SDL audio plugin")
     (description
      "Mupen64Plus is a cross-platform plugin-based Nintendo 64 (N64) emulator
 which is capable of accurately playing many games.  This package contains the
