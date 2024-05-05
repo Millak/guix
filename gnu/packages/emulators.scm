376fe8201577e87288a65fc79ecb90da89ec26b9
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2024-05-05 15:34:55 -0400

    gnu: mupen64plus-rsp-hle: Fix synopsis.
    * gnu/packages/emulators.scm (mupen64plus-rsp-hle): Fix synopsis.
    
    Change-Id: I7235da472ecd1856fdc33ed08d92f9f05b29ba3a

--- gnu/packages/emulators.scm
+++ gnu/packages/emulators.scm
@@ -1006,7 +1006,7 @@ SDL input plugin.")
        ;; There are no tests.
        #:tests? #f))
     (home-page "https://www.mupen64plus.org/")
-    (synopsis "Mupen64Plus SDL input plugin")
+    (synopsis "Mupen64Plus RSP high-level emulation (HLE) plugin")
     (description
      "Mupen64Plus is a cross-platform plugin-based Nintendo 64 (N64) emulator
 which is capable of accurately playing many games.  This package contains the
