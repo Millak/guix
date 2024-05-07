c990a72c67bd22f2c2b77fb6b6694be4fbd07151
Ricardo Wurmus <rekado@elephly.net>
2024-05-07 13:06:50 +0200

    gnu: r-bumphunter: Update to 1.46.0.
    * gnu/packages/bioconductor.scm (r-bumphunter): Update to 1.46.0.
    
    Change-Id: I171983cd2e64539fcd2d8f9a07421ff2418382f2

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -15298,14 +15298,14 @@ Bayes Analyses of Microarrays} (EBAM).")
 (define-public r-bumphunter
   (package
     (name "r-bumphunter")
-    (version "1.44.0")
+    (version "1.46.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "bumphunter" version))
        (sha256
         (base32
-         "0vnm0m9abf8478f7lair58vw1v4mxj8sbmrxcpf8622ygf2na4qc"))))
+         "08b5d0grmpk5pryffdh6gdafhp1brana353q2kmwhz7x55aq3w68"))))
     (build-system r-build-system)
     (propagated-inputs
      (list r-annotationdbi
