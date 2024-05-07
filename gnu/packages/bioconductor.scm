7881ac326f0b9ccc44b85d95ea812b57478143c3
Ricardo Wurmus <rekado@elephly.net>
2024-05-07 12:33:01 +0200

    gnu: r-harshlight: Update to 1.76.0.
    * gnu/packages/bioconductor.scm (r-harshlight): Update to 1.76.0.
    
    Change-Id: I5e64edbe1204d588395dfc3de9d993a1b768f60e

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -8519,13 +8519,13 @@ details.")
 (define-public r-harshlight
   (package
     (name "r-harshlight")
-    (version "1.74.0")
+    (version "1.76.0")
     (source (origin
               (method url-fetch)
               (uri (bioconductor-uri "Harshlight" version))
               (sha256
                (base32
-                "1djs95l04b3qrqcb7jkzhfmxzbsn2riyydz8p2lmilg6z9rkdqx7"))))
+                "1nqi2ad9hnm6j8hn8f6xp9zav6rsa5wlzb2z5fjnm9q4xbjblnjl"))))
     (properties `((upstream-name . "Harshlight")))
     (build-system r-build-system)
     (propagated-inputs
