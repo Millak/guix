6aed2982eccb3960cf451d54eb9f6d2285f39553
Ricardo Wurmus <rekado@elephly.net>
2024-05-07 13:07:23 +0200

    gnu: r-megadepth: Update to 1.14.0.
    * gnu/packages/bioconductor.scm (r-megadepth): Update to 1.14.0.
    [inputs]: Remove megadepth.
    
    Change-Id: Ia0fc8a1e7266a996249f04447eabf9cf86b00f07

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -23652,17 +23652,17 @@ to annotation.")
 (define-public r-megadepth
   (package
     (name "r-megadepth")
-    (version "1.12.0")
+    (version "1.14.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "megadepth" version))
        (sha256
         (base32
-         "0g9d4q6vh2ys1la15fqgbc4ckfbjbzzaqa49x9liqrhpnrp59vc3"))))
+         "0394x1x81hgibvqb1dbrm8h3y72xfv9xi9fgqghjgnrw6farrkdk"))))
     (properties `((upstream-name . "megadepth")))
     (build-system r-build-system)
-    (inputs (list megadepth))
+    (inputs (list))
     (propagated-inputs
      (list r-cmdfun
            r-dplyr
