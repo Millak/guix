c25e80f57c2e09a2baddf3e28954bfc48af84f5e
Ricardo Wurmus <rekado@elephly.net>
2024-05-07 13:07:06 +0200

    gnu: r-genie3: Update to 1.26.0.
    * gnu/packages/bioconductor.scm (r-genie3): Update to 1.26.0.
    
    Change-Id: I7eb05930f49b4c44678e5069cda9083fa3b2d86a

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -19538,14 +19538,14 @@ cisTopics and explore the nature and regulatory proteins driving them.")
 (define-public r-genie3
   (package
     (name "r-genie3")
-    (version "1.24.0")
+    (version "1.26.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "GENIE3" version))
        (sha256
         (base32
-         "0bsid8qhcqgalqghr2b2592pzm4viyi7wq8h5dmhrrl7gky8l60k"))))
+         "0ijwyx42saqas52273c63c3yi948bh4pd4lifyqi3zjjs17bj1cg"))))
     (properties `((upstream-name . "GENIE3")))
     (build-system r-build-system)
     (propagated-inputs
