aec3a47c8c0f8ff9923f7b241fa1d9578690afc6
Ricardo Wurmus <rekado@elephly.net>
2024-05-07 12:32:44 +0200

    gnu: r-decomptumor2sig: Update to 2.20.0.
    * gnu/packages/bioconductor.scm (r-decomptumor2sig): Update to 2.20.0.
    
    Change-Id: I4f7cd015a500b904d725a9098907253b81e7d57c

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -5450,13 +5450,13 @@ mechanism based on the shared signatures.")
 (define-public r-decomptumor2sig
   (package
     (name "r-decomptumor2sig")
-    (version "2.18.0")
+    (version "2.20.0")
     (source (origin
               (method url-fetch)
               (uri (bioconductor-uri "decompTumor2Sig" version))
               (sha256
                (base32
-                "13vwrg82zprb9h72azdqd0rkm5k2xm4mw7viawbzwkkqvg6azsdj"))))
+                "1pnxnxz9zsxqs7cbxgkjapr57k2m1d57w5hak0dwcy5zcxjzmai8"))))
     (properties `((upstream-name . "decompTumor2Sig")))
     (build-system r-build-system)
     (inputs (list perl))                ;script/extractSpecColumns.pl
