b563dc47fda84c3277c1b4c234c7b32c49e60272
Ricardo Wurmus <rekado@elephly.net>
2024-05-07 12:32:50 +0200

    gnu: r-affycontam: Update to 1.62.0.
    * gnu/packages/bioconductor.scm (r-affycontam): Update to 1.62.0.
    
    Change-Id: Ia2688e906b16b11d96925beded22d47cbb3f67c0

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -6654,14 +6654,14 @@ Command Console} (AGCC)-compatible sample annotation files.")
 (define-public r-affycontam
   (package
     (name "r-affycontam")
-    (version "1.60.0")
+    (version "1.62.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "affyContam" version))
        (sha256
         (base32
-         "1jxp4qacclfl07ig8dfwy5gpnsqrfqnjbci1a7znc7acqg0kv0zv"))))
+         "0r4pqda9pv60b867c6yi328687kyxjglk5jmcal6xnlr75nf4w0y"))))
     (properties `((upstream-name . "affyContam")))
     (build-system r-build-system)
     (propagated-inputs
