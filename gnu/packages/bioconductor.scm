e99c0cb450e632ab6747bfad713335c6497a94d8
Ricardo Wurmus <rekado@elephly.net>
2024-05-07 13:06:55 +0200

    gnu: r-a4base: Update to 1.52.0.
    * gnu/packages/bioconductor.scm (r-a4base): Update to 1.52.0.
    
    Change-Id: Ib453338c9f5120152401b63316875563dcfa8a6f

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -16595,14 +16595,14 @@ provides reporting features.")
 (define-public r-a4base
   (package
     (name "r-a4base")
-    (version "1.50.0")
+    (version "1.52.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "a4Base" version))
        (sha256
         (base32
-         "0hin5lpwa7cxa862jh3y7d8kq3q9bv76ri08b27jxzyr5p7xwcr2"))))
+         "19vapjmyd3ly3s2gqa2xfg99gyc6n6lv3c6rpi0ch4hzb7wiinvm"))))
     (properties `((upstream-name . "a4Base")))
     (build-system r-build-system)
     (propagated-inputs
