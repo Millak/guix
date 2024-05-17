a1bb0afc7b74342bb0863f6dc7a15f8a75463b8b
Wilko Meyer <w@wmeyer.eu>
2024-05-17 14:38:28 +0200

    gnu: linux-libre 5.4: Update to 5.4.276.
    * gnu/packages/linux.scm (linux-libre-5.4-version): Update to 5.4.276.
    (linux-libre-5.4-pristine-source): Update hash.
    
    Change-Id: Ieea174e5ca46226f739cb9c092f7518bac44396f
    Signed-off-by: Leo Famulari <leo@famulari.name>

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -583,7 +583,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
                             (%upstream-linux-source version hash)
                             deblob-scripts-5.10)))
 
-(define-public linux-libre-5.4-version "5.4.275")
+(define-public linux-libre-5.4-version "5.4.276")
 (define-public linux-libre-5.4-gnu-revision "gnu1")
 (define deblob-scripts-5.4
   (linux-libre-deblob-scripts
@@ -593,7 +593,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
    (base32 "0x0xg0fcykpd117x3q0gim8jilhx922ashhckjvafxv2gk2zzjhj")))
 (define-public linux-libre-5.4-pristine-source
   (let ((version linux-libre-5.4-version)
-        (hash (base32 "0k1hyknx854k8z27j4rq1gcp8l0xc0bspmrhc41a033gjilb1lns")))
+        (hash (base32 "01vfx19n8rv9fgjjzvi78125md71zgn5jrinbarabzr18jyjwwg2")))
    (make-linux-libre-source version
                             (%upstream-linux-source version hash)
                             deblob-scripts-5.4)))
