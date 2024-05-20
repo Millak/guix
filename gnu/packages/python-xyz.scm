979249733f28dfad307eeec4c004654ec9f54e57
Sharlatan Hellseher <sharlatanus@gmail.com>
2024-05-20 23:12:52 +0100

    gnu: Add python-more-itertools-next.
    * gnu/packages/python-xyz.scm (python-more-itertools-next): New variable.
    
    Change-Id: Ia9deef0c19931356446e2731a315d27ce54e6ab0
    Signed-off-by: Andrew Tropin <andrew@trop.in>

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -26462,6 +26462,41 @@ and SML.  @code{more-itertools} includes additional building blocks for
 working with iterables.")
     (license license:expat)))
 
+;; Needed for python-yt, older version fails with exception: E ImportError:
+;; cannot import name 'mark_ends' from 'more_itertools'
+;; (<...>more_itertools/__init__.py)
+(define-public python-more-itertools-next
+  (package
+    (inherit python-more-itertools)
+    (name "python-more-itertools")
+    (version "10.2.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (pypi-uri "more-itertools" version))
+       (sha256
+        (base32
+         "1q9rq9g026m4wl6ki2q8pw7xbc02vl34qqw702h9jgixqj0b9k4g"))
+       (snippet
+        ;; distutils.errors.DistutilsOptionError: No configuration found for
+        ;; dynamic 'description'. Some dynamic fields need to be specified via
+        ;; `tool.setuptools.dynamic`others must be specified via the equivalent
+        ;; attribute in `setup.py`.
+        '(delete-file "setup.py"))))
+    (build-system pyproject-build-system)
+    (arguments
+     (list
+      #:phases
+      #~(modify-phases %standard-phases
+          (replace 'check
+            (lambda* (#:key tests? #:allow-other-keys)
+              (when tests?
+                (invoke "python" "-m" "unittest")))))))
+    (native-inputs
+     (list python-flit-core))
+    (propagated-inputs
+     (list python-six))))
+
 (define-public python-latexcodec
   (package
     (name "python-latexcodec")
