a1f1148dbd835ffa57077d67fd378098c35e2af1
Efraim Flashner <efraim@flashner.co.il>
2024-05-20 09:32:59 +0300

    gnu: enblend-enfuse: Build with librsvg-for-system.
    * gnu/packages/photo.scm (enblend-enfuse)[native-inputs]: Replace
    librsvg with librsvg-for-system.
    
    Change-Id: I04e046191abf1e36cdbb42d50a5c5abab029edf1

--- gnu/packages/photo.scm
+++ gnu/packages/photo.scm
@@ -2,7 +2,7 @@
 ;;; Copyright © 2014, 2015, 2017, 2019, 2021 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2015 Ricardo Wurmus <rekado@elephly.net>
 ;;; Copyright © 2015, 2017 Andreas Enge <andreas@enge.fr>
-;;; Copyright © 2016-2019, 2021, 2023 Efraim Flashner <efraim@flashner.co.il>
+;;; Copyright © 2016-2019, 2021, 2023, 2024 Efraim Flashner <efraim@flashner.co.il>
 ;;; Copyright © 2017 Roel Janssen <roel@gnu.org>
 ;;; Copyright © 2018–2022 Tobias Geerinckx-Rice <me@tobias.gr>
 ;;; Copyright © 2018 Leo Famulari <leo@famulari.name>
@@ -398,7 +398,7 @@ overlapping images, as well as some command line tools.")
            graphviz-minimal  ; for 'dot'
            font-ghostscript
            imagemagick/stable
-           librsvg
+           (librsvg-for-system)
            m4
            perl-readonly
            texlive-texloganalyser
