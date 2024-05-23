4ab8657b23ed951c6a95d18b4574e39f3aceca32
Efraim Flashner <efraim@flashner.co.il>
2024-05-23 14:22:54 +0300

    gnu: libbraille: Fix build on some architectures.
    * gnu/packages/accessibility.scm (libbraille)[arguments]: When building
    with config as a native-input replace the config.guess and config.sub
    files.
    [native-inputs]: When building for aarch64-linux, powerpc64le-linux or
    riscv64-linux add config.
    
    Change-Id: I7e7953a2b91868fa3f2698d3cb82da134564c06b

--- gnu/packages/accessibility.scm
+++ gnu/packages/accessibility.scm
@@ -33,6 +33,7 @@
   #:use-module (guix build-system glib-or-gtk)
   #:use-module (guix build-system meson)
   #:use-module (gnu packages)
+  #:use-module (gnu packages autotools)
   #:use-module (gnu packages lisp)
   #:use-module (gnu packages ocaml)
   #:use-module (gnu packages pcre)
@@ -77,11 +78,31 @@
     (arguments
      `(#:tests? #f                      ; Tests require drivers
        #:configure-flags
-       (list
-        "--disable-static"
-        "--enable-fake")))
+       (list "--disable-static"
+             "--enable-fake")
+       #:phases
+       (modify-phases %standard-phases
+         ,@(if (this-package-native-input "config")
+               `((add-after 'unpack 'update-config-scripts
+                   (lambda* (#:key native-inputs inputs #:allow-other-keys)
+                     (for-each
+                       (lambda (dir)
+                         (for-each (lambda (file)
+                                     (install-file
+                                       (search-input-file
+                                         (or native-inputs inputs)
+                                         (string-append "/bin/" file)) dir))
+                                   '("config.guess" "config.sub")))
+                       '("." "libltdl")))))
+               '()))))
     (native-inputs
-     (list latex2html pkg-config python-wrapper swig))
+     (append
+       (if (or (target-aarch64?)
+               (target-ppc64le?)
+               (target-riscv64?))
+           (list config)
+           '())
+       (list latex2html pkg-config python-wrapper swig)))
     (inputs
      (list glib gtk+-2 libusb-compat))
     (synopsis "Portable Braille Library")
