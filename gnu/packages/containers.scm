65bec2ffe82220b4d551d271337dabb8f9fd6640
Tomas Volf <~@wolfsden.cz>
2024-05-30 00:02:01 +0200

    gnu: podman: Update to 5.1.0.
    * gnu/packages/containers.scm (podman): Update to 5.1.0.
    
    Change-Id: I87b3e639f1eb01e93363a359d33c2aaceaee653b
    Signed-off-by: Zheng Junjie <zhengjunjie@iscas.ac.cn>

--- gnu/packages/containers.scm
+++ gnu/packages/containers.scm
@@ -454,7 +454,7 @@ Its main purpose is to support the key usage by @code{docker-init}:
 (define-public podman
   (package
     (name "podman")
-    (version "5.0.3")
+    (version "5.1.0")
     (source
      (origin
        (method git-fetch)
@@ -462,7 +462,7 @@ Its main purpose is to support the key usage by @code{docker-init}:
              (url "https://github.com/containers/podman")
              (commit (string-append "v" version))))
        (sha256
-        (base32 "1wgaiwh7m547j9irz6z6ckxcbw8fwzqixh55q1fk2g7kfclfc3iw"))
+        (base32 "0ldzrrz8jba6ka1xfs8msiy08iz4m674xhfxbcdsnc9lmxi3ys4f"))
        (file-name (git-file-name name version))))
     (build-system gnu-build-system)
     (arguments
