b6ef3cfdf0e948f3f5e291cbfefea5e5a13068a7
Trevor Arjeski <tmarjeski@gmail.com>
2024-06-02 23:20:42 +0300

    gnu: Add fzf-tab.
    * gnu/packages/shellutils.scm (fzf-tab): New variable.
    
    Change-Id: I821eb28c9ac12e8d6449ce7bc389538d260d6091
    Signed-off-by: Andreas Enge <andreas@enge.fr>

--- gnu/packages/shellutils.scm
+++ gnu/packages/shellutils.scm
@@ -912,3 +912,30 @@ with Guix Home:
                                      \"source \" liquidprompt \"/share/liquidprompt/themes/powerline/powerline.theme\"))))))
 @end example\n")
     (license license:agpl3+)))
+
+(define-public fzf-tab
+  (package
+  (name "fzf-tab")
+  (version "1.1.2")
+  (home-page "https://github.com/Aloxaf/fzf-tab")
+  (source (origin
+            (method git-fetch)
+            (uri (git-reference
+                  (url "https://github.com/Aloxaf/fzf-tab")
+                  (commit (string-append "v" version))))
+            (file-name (git-file-name name version))
+            (sha256
+             (base32
+              "061jjpgghn8d5q2m2cd2qdjwbz38qrcarldj16xvxbid4c137zs2"))))
+  (build-system copy-build-system)
+  (arguments
+   '(#:install-plan '(("lib" "/share/zsh/plugins/fzf-tab/")
+                      ("modules" "/share/zsh/plugins/fzf-tab/")
+                      ("fzf-tab.plugin.zsh" "/share/zsh/plugins/fzf-tab/")
+                      ("fzf-tab.zsh" "/share/zsh/plugins/fzf-tab/")
+                      ("README.md" "/share/doc/fzf-tab/"))))
+  (synopsis "Replace the zsh default completion menu with fzf")
+  (description
+   "The fzf-tab package replaces the default completion menu of the zsh
+shell with fzf, enabling fuzzy finding and multi-selection.")
+  (license license:expat)))
