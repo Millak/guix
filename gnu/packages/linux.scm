Millak/guix

commit ce78f9cb668971954add5473c8549ebb00424f66

gnu: e2fsprogs: Update to 1.47.0.
* gnu/packages/linux.scm (e2fsprogs): Update to 1.47.0.

Change-Id: I43832dfb0a8dd54d9a1348a07f959dbb0b46c080

gnu/packages/linux.scm
@@ -2564,7 +2564,7 @@ module.")
 (define-public e2fsprogs
   (package
     (name "e2fsprogs")
-    (version "1.46.4")
+    (version "1.47.0")
     (source (origin
              (method url-fetch)
              (uri (string-append
@@ -2573,7 +2573,7 @@ module.")
                    "e2fsprogs-" version ".tar.xz"))
              (sha256
               (base32
-               "04wp77fg842dhribgn0xvbd77idh0n7a839ga4bwy78v7i9l445i"))))
+               "1nymjw801lmlr5y6bg1zv9jsip5dkyxqislbdzpir4mx5czzajhl"))))
     (build-system gnu-build-system)
     (inputs (list `(,util-linux "lib")))
     (native-inputs (list pkg-config

commit 865bf63eb3025b978a74805ef0ea8325c97d88e9

gnu: e2fsprogs: Fix compilation for GNU/Hurd.
Fixes a regression introduced in
ce78f9cb668971954add5473c8549ebb00424f66.

* gnu/packages/linux.scm (e2fsprogs)[arguments]: When ‘system-hurd?’,
add ‘set-PATH_MAX’ phase.

Change-Id: Idc8c2a24d2518b44e42102db48a8ab484b855f47

gnu/packages/linux.scm
@@ -1,5 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2012-2021, 2021-2023 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2012-2021, 2021-2024 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2013, 2014, 2015, 2016 Andreas Enge <andreas@enge.fr>
 ;;; Copyright © 2012 Nikita Karetnikov <nikita@karetnikov.org>
 ;;; Copyright © 2014, 2015, 2016, 2017, 2018, 2019, 2020 Mark H Weaver <mhw@netris.org>
@@ -2710,7 +2710,12 @@ module.")
                                (chmod file #o666))
                              archives)))))
            ,@(if (system-hurd?)
-                 '((add-after 'unpack 'skip-tests
+                 '((add-after 'unpack 'set-PATH_MAX
+                     (lambda _
+                       ;; Shamelessly introduce an arbitrary limit.
+                       (substitute* "misc/tune2fs.c"
+                         (("PATH_MAX") "4096"))))
+                   (add-after 'unpack 'skip-tests
                      (lambda _
                        (with-directory-excursion "tests"
                          (for-each