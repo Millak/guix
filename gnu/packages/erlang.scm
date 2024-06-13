d39627d82403068b49c4300899553e427454d94c
Igor Goryachev via Guix-patches via <guix-patches@gnu.org>
2024-06-13 09:17:40 +0300

    gnu: erlang-certifi: Update to 2.13.0.
    * gnu/packages/erlang.scm (erlang-certifi): Update to 2.13.0.
    
    Change-Id: If1ea284809c6f581375140795ba2ca9e0deb39a8
    Signed-off-by: Andrew Tropin <andrew@trop.in>

--- gnu/packages/erlang.scm
+++ gnu/packages/erlang.scm
@@ -248,13 +248,13 @@ Mustache template engine")
 (define-public erlang-certifi
   (package
     (name "erlang-certifi")
-    (version "2.9.0")
+    (version "2.13.0")
     (source
      (origin
        (method url-fetch)
        (uri (hexpm-uri "certifi" version))
        (sha256
-        (base32 "0ha6vmf5p3xlbf5w1msa89frhvfk535rnyfybz9wdmh6vdms8v96"))))
+        (base32 "1887qmxp7qmha4c8k4m4cw35iii15srrdmfmvypp0q7hl0rragcg"))))
     (build-system rebar-build-system)
     (arguments
      `(#:tests? #f)) ;; have not been updated for latest cert bundle
