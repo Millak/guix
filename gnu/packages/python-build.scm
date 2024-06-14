3a0acc79cf3618a5be170fe74c88aceea530b308
Antero Mejr <mail@antr.me>
2024-06-14 20:47:25 +0000

    gnu: python-poetry-core: Update to 1.9.1.
    * gnu/packages/python-build.scm (python-poetry-core): Update to 1.9.1.
    
    Change-Id: Ic65feaa1efbd10368a715fbea1a974355d6fc96f
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/python-build.scm
+++ gnu/packages/python-build.scm
@@ -550,13 +550,13 @@ order to make bootstrapping easier.")
 (define-public python-poetry-core
   (package
     (name "python-poetry-core")
-    (version "1.5.2")
+    (version "1.9.1")
     (source
      (origin
        (method url-fetch)
        (uri (pypi-uri "poetry_core" version))
        (sha256
-        (base32 "053c8dw632p7jkhjb51k0wcx6hdw4r3lk97mds76df653qxnqmf6"))))
+        (base32 "1f31gwhnfyrdymlm0ym6k6c6r0x98zcr2s4xz4blz2zm9chljbbs"))))
     (build-system pyproject-build-system)
     (arguments
      `(#:tests? #f))                      ;disabled to avoid extra dependencies
