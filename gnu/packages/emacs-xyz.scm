5b332dd00576f13ec779d30cb2127ff6a28d0abe
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2024-06-18 16:03:40 +0200

    gnu: emacs-citre: Update to 0.4.1.
    * gnu/packages/emacs-xyz.scm (emacs-citre): Update to 0.4.1.
    
    Change-Id: Ic502e9dc4fbe5199fe859963096f105a98bdab67

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -39030,7 +39030,7 @@ project.el and xref.el.")
 (define-public emacs-citre
   (package
     (name "emacs-citre")
-    (version "0.4")
+    (version "0.4.1")
     (source
      (origin
        (method git-fetch)
@@ -39038,7 +39038,7 @@ project.el and xref.el.")
              (url "https://github.com/universal-ctags/citre/")
              (commit (string-append "v" version))))
        (sha256
-        (base32 "0fq1f03v706gj6p2fjz51kvmj1pwc9p64chqap2ajr0a9cxpwln6"))
+        (base32 "1x5kxlzhzr2x4cszcqaxcg2lc71nwmmfnm2vzx7iz7h74hn4f1ld"))
        (file-name (git-file-name name version))))
     (build-system emacs-build-system)
     (arguments
