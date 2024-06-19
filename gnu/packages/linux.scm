b25c1e7d03ab39f28669a8be2d7e014c4a15ab9f
宋文武 <iyzsong@member.fsf.org>
2024-06-19 15:34:22 +0800

    gnu: libnl-python2: Add libxcrypt dependency.
    * gnu/packages/linux.scm (libnl-python-package)[inputs]: Add libxcrypt.
    
    Change-Id: Id7293e6f1951bb525f5ad072bc7b12d18ac7c69c

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -3630,7 +3630,8 @@ configuration and monitoring interfaces.")
                    ((string=? python "python2")
                     `(("python-2" ,python-2)))
                    ((string=? python "python3")
-                    `(("python-3" ,python-3))))))
+                    `(("python-3" ,python-3))))
+                ("libxcrypt" ,libxcrypt))) ;required by Python.h
       (propagated-inputs (list libnl))
       (outputs '("out"))
       (arguments
