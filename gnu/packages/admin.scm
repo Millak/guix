6aaff90ad623232f6952fdbad1211eacfcc35219
Ludovic Courtès <ludo@gnu.org>
2024-06-26 23:27:09 +0200

    gnu: shepherd: Update to 0.10.5.
    * gnu/packages/admin.scm (shepherd-0.10): Update to 0.10.5.
    
    Change-Id: I4f195917e0103ba1da75a52665848bbc877b5d24

--- gnu/packages/admin.scm
+++ gnu/packages/admin.scm
@@ -392,14 +392,14 @@ interface and is based on GNU Guile.")
 (define-public shepherd-0.10
   (package
     (inherit shepherd-0.9)
-    (version "0.10.4")
+    (version "0.10.5")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://gnu/shepherd/shepherd-"
                                   version ".tar.gz"))
               (sha256
                (base32
-                "0206r2l914qjahzd1qill57r1qcg1x8faj0f6qv3x42wqx6x28ky"))))
+                "0k40n9qm5r5rqf94isa1857ghd4329zc5rjf3ll2572gpiw3ij4x"))))
     (native-inputs (modify-inputs (package-native-inputs shepherd-0.9)
                      (replace "guile-fibers"
                        ;; Work around
