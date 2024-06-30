c070c060fd5edd317fa838feff2f1df6fe29830a
Efraim Flashner <efraim@flashner.co.il>
2024-06-30 10:49:59 +0300

    gnu: openblas-ilp64: Enable building on all 64bit platforms.
    * gnu/packages/maths.scm (openblas-ilp64)[supported-systems]: Mark all
    64bit platforms as supported.
    
    Change-Id: I3956b0140798842bae23fc5f855f0953ec81b53d

--- gnu/packages/maths.scm
+++ gnu/packages/maths.scm
@@ -5204,8 +5204,7 @@ parts of it.")
 (define-public openblas-ilp64
   (package/inherit openblas
     (name "openblas-ilp64")
-    (supported-systems '("x86_64-linux" "aarch64-linux" "mips64el-linux"
-                         "powerpc64le-linux"))
+    (supported-systems %64bit-supported-systems)
     (arguments
      (substitute-keyword-arguments (package-arguments openblas)
        ((#:make-flags flags #~'())
