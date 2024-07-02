567f9dfbdd723fe7be23e309b1bc410f3a3eb553
Ricardo Wurmus <rekado@elephly.net>
2024-07-02 14:58:46 +0200

    gnu: r-lightgbm: Update to 4.4.0.
    * gnu/packages/cran.scm (r-lightgbm): Update to 4.4.0.
    
    Change-Id: Ie1c20daa0170515feb210da75905f7e839e1256f

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -35046,14 +35046,14 @@ input.")
 (define-public r-lightgbm
   (package
     (name "r-lightgbm")
-    (version "4.3.0")
+    (version "4.4.0")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "lightgbm" version))
        (sha256
         (base32
-         "1kk2jj7d1adqi9x1q3jij7fy8h9vc6bvzcz7jhi4bd9cybw8gpnh"))))
+         "082xmr33ki16i52a3z6lv9niv46pi4dbp0j3qn421sxml4115sf1"))))
     (properties `((upstream-name . "lightgbm")))
     (build-system r-build-system)
     (propagated-inputs
