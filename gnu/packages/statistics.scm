830f2108dd67c0b5f6248fdb1167d57f769d75e3
Ricardo Wurmus <rekado@elephly.net>
2024-07-02 14:58:54 +0200

    gnu: r-rsqlite: Update to 2.3.7.
    * gnu/packages/statistics.scm (r-rsqlite): Update to 2.3.7.
    
    Change-Id: I6065ee2dec5d0755400f8e9f14322d202b60a623

--- gnu/packages/statistics.scm
+++ gnu/packages/statistics.scm
@@ -3440,13 +3440,13 @@ a column in data frame.")
 (define-public r-rsqlite
   (package
     (name "r-rsqlite")
-    (version "2.3.6")
+    (version "2.3.7")
     (source (origin
               (method url-fetch)
               (uri (cran-uri "RSQLite" version))
               (sha256
                (base32
-                "1wpqr71wckxrh8ih5f01wkqqqnq5s3zaj0m8b40g76pliyjvgcgv"))))
+                "1kllna6hfnyy3rizr8wkpfn7p5sxv3sra12x9vz68hp6i4jmgq15"))))
     (properties `((upstream-name . "RSQLite")))
     (build-system r-build-system)
     (propagated-inputs
