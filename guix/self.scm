Millak/guix

commit 368e58afcb3b363098e52907c1ea9a385566f7d4

self: Use po4a instead of po4a-translate.
* guix/self.scm (translate-tmp-texi): Use po4a instead of po4a-translate.
This eliminates the po4a-translate warning; "po4a-translate is deprecated.
The unified po4a(1) program is more convenient and less error prone."

Change-Id: Id85c7478b1b237f31010994fcd2d38765993c1ad
Signed-off-by: Florian Pelz <pelzflorian@pelzflorian.de>

guix/self.scm
@@ -2,6 +2,7 @@
 ;;; Copyright © 2017-2023 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2020 Martin Becze <mjbecze@riseup.net>
 ;;; Copyright © 2023 Janneke Nieuwenhuizen <janneke@gnu.org>
+;;; Copyright © 2024 gemmaro <gemmaro.dev@gmail.com>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -317,9 +318,15 @@ DOMAIN, a gettext domain."
           (define (translate-tmp-texi po source output)
             "Translate Texinfo file SOURCE using messages from PO, and write
 the result to OUTPUT."
-            (invoke #+(file-append po4a-minimal "/bin/po4a-translate")
-              "-M" "UTF-8" "-L" "UTF-8" "-k" "0" "-f" "texinfo"
-              "-m" source "-p" po "-l" output))
+            (invoke #+(file-append po4a-minimal "/bin/po4a")
+              "--no-update"
+              "--variable" (string-append "localized=" output)
+              "--variable" (string-append "master=" source)
+              "--variable" (string-append "po=" po)
+              "--variable" (string-append "pot=" (string-append (tmpnam) ".pot"))
+              (string-append "--srcdir=" #$source)
+              "--destdir=."
+              #+(file-append documentation-po "/po4a.cfg")))
 
           (define (canonicalize-whitespace str)
             ;; Change whitespace (newlines, etc.) in STR to #\space.

commit 5f1e4e4c0242af6bcba656aedf8b49afbe7247b7

self: Remove reference to source.
A regression was introduced in 368e58afcb3b363098e52907c1ea9a385566f7d4 which
led to the guix-translated-texinfo derivation including a source file for the
entire checkout.

I'm not sure what the --srcdir option does, but the derivation still seems to
build when it's removed.

* guix/self.scm (translate-texi-manuals): Remove #$source reference.

Change-Id: Icfe1eaf7600f5708c9b3408d59d760b1edb3237b

guix/self.scm
@@ -324,7 +324,6 @@ the result to OUTPUT."
               "--variable" (string-append "master=" source)
               "--variable" (string-append "po=" po)
               "--variable" (string-append "pot=" (string-append (tmpnam) ".pot"))
-              (string-append "--srcdir=" #$source)
               "--destdir=."
               #+(file-append documentation-po "/po4a.cfg")))
 