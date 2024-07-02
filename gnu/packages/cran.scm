8b22bd95ff48d0c64bd5f537dcf25ea25556936d
Ricardo Wurmus <rekado@elephly.net>
2024-07-02 14:57:29 +0200

    gnu: r-mc2d: Update to 0.2.1.
    * gnu/packages/cran.scm (r-mc2d): Update to 0.2.1.
    
    Change-Id: Ia418219028c9f4e3b80ebd12e7cd1a61a3640b85

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -3933,13 +3933,13 @@ high-dimensional data.")
 (define-public r-mc2d
   (package
     (name "r-mc2d")
-    (version "0.2.0")
+    (version "0.2.1")
     (source (origin
               (method url-fetch)
               (uri (cran-uri "mc2d" version))
               (sha256
                (base32
-                "07h2gvmdfqjydikxksbqhikihg46fv6xyn8vk5kyni6x75gp9abk"))))
+                "06kva55vm2cldd95caw5hfx0xjadyhgkqi0y9pyz7hs3zg0zq9xy"))))
     (properties `((upstream-name . "mc2d")))
     (build-system r-build-system)
     (propagated-inputs (list r-ggplot2 r-ggpubr r-mvtnorm))
