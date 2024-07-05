0962e9f75fe986654c48bb331a4028ca390019f5
Wilko Meyer <w@wmeyer.eu>
2024-07-06 00:25:01 +0200

    gnu: linux-libre 6.9: Update to 6.9.8.
    * gnu/packages/linux.scm (linux-libre-6.9-version): Update to 6.9.8.
    (linux-libre-6.9-pristine-source): Update hash.
    
    Change-Id: Ied14fe931937fba95afc8330b60d5a51ee6b344f
    Signed-off-by: Leo Famulari <leo@famulari.name>

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -512,7 +512,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
 ;; The current "stable" kernels. That is, the most recently released major
 ;; versions that are still supported upstream.
 
-(define-public linux-libre-6.9-version "6.9.7")
+(define-public linux-libre-6.9-version "6.9.8")
 (define-public linux-libre-6.9-gnu-revision "gnu")
 (define deblob-scripts-6.9
   (linux-libre-deblob-scripts
@@ -522,7 +522,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
    (base32 "0b8hsr0s4f3hps27bmd5qj1yknhd73q4zplr4v3lmq7sr57mgly6")))
 (define-public linux-libre-6.9-pristine-source
   (let ((version linux-libre-6.9-version)
-        (hash (base32 "1y01w26sas7pl24l09yczdr8mzzy2nadykz1wmhx9ygfj76qixg4")))
+        (hash (base32 "109j8f69bsvk2a61ihs3r77jv05fm4aqvih8lyf6qcc8gdxjcj7h")))
    (make-linux-libre-source version
                             (%upstream-linux-source version hash)
                             deblob-scripts-6.9)))
