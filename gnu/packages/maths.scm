ae8b2caf115a8793511b3058cf1b775e61ebbc57
Vinicius Monego <monego@posteo.net>
2024-07-06 14:57:29 -0300

    gnu: cminpack: Update to 1.3.9.
    * gnu/packages/maths.scm (cminpack): Update to 1.3.9.
    
    Change-Id: I2b4039c4e8c313204f96773b1d0dd072dfca5d41

--- gnu/packages/maths.scm
+++ gnu/packages/maths.scm
@@ -1486,7 +1486,7 @@ C++ with a C API.  It contains a LU and LLt solver, and a few other things.")
 (define-public cminpack
   (package
     (name "cminpack")
-    (version "1.3.8")
+    (version "1.3.9")
     (source (origin
               (method git-fetch)
               (uri (git-reference
@@ -1495,7 +1495,7 @@ C++ with a C API.  It contains a LU and LLt solver, and a few other things.")
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "1bg0954mwry22izsvikpai16pkfp8srz4z34n267bhkmrvvb0zgy"))))
+                "05cjb54in7kks70rrnmvczwkg4nsxhwyf23abxqdj143zwbz4yyr"))))
     (build-system cmake-build-system)
     (arguments
      (list #:configure-flags #~(list "-DBUILD_SHARED_LIBS=ON")))
