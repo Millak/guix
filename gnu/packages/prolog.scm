0663668507377a849317b0fff3d72f609c3139c2
jgart <jgart@dismail.de>
2024-07-11 00:43:29 -0500

    gnu: trealla: Update to 2.53.48.
    * gnu/packages/prolog.scm (trealla): Update to 2.53.48.
    
    Change-Id: I2544e65003880fa5ec6d70a8ba814bb88722c3c9

--- gnu/packages/prolog.scm
+++ gnu/packages/prolog.scm
@@ -180,7 +180,7 @@ it.")
 (define-public trealla
   (package
     (name "trealla")
-    (version "2.53.47")
+    (version "2.53.48")
     (source
      (origin
        (method git-fetch)
@@ -189,7 +189,7 @@ it.")
          (url "https://github.com/trealla-prolog/trealla")
          (commit (string-append "v" version))))
        (sha256
-        (base32 "0h2lqy918i1qgymxyyswxb6pbnjw2rfq1rnclagiqnsz4ymzp9jp"))
+        (base32 "19gig7yn34qcw604ffpmwbn87kin5inaplb645b03bj25lr7h76b"))
        (file-name (git-file-name name version))))
     (build-system gnu-build-system)
     (native-inputs
