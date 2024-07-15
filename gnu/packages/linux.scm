92c323fdee7aa072761ed83654c49748e8be847f
Wilko Meyer <w@wmeyer.eu>
2024-07-15 17:29:06 +0200

    gnu: linux-libre 6.6: Update to 6.6.40.
    * gnu/packages/linux.scm (linux-libre-6.6-version): Update to 6.6.40.
    (linux-libre-6.6-pristine-source): Update hash.
    
    Change-Id: Ia5832159f507cde2bcc37c1a65a628f28d845acf
    Signed-off-by: Leo Famulari <leo@famulari.name>

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -532,7 +532,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
 ;; Here are the support timelines:
 ;; <https://www.kernel.org/category/releases.html>
 
-(define-public linux-libre-6.6-version "6.6.39")
+(define-public linux-libre-6.6-version "6.6.40")
 (define-public linux-libre-6.6-gnu-revision "gnu")
 (define deblob-scripts-6.6
   (linux-libre-deblob-scripts
@@ -542,7 +542,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
    (base32 "15xb4miirfmi1khlq4zhb8zmmh82f41jhsfbsfpv8v98yfka2nmb")))
 (define-public linux-libre-6.6-pristine-source
   (let ((version linux-libre-6.6-version)
-        (hash (base32 "0svpgakk81082mhhq17r8nlbz36zavq23d71232rapq928hx90r7")))
+        (hash (base32 "0f7mmw5pzd174376m7r928xbi9mdcla0vy6plq0xdf2mq01kqfjw")))
    (make-linux-libre-source version
                             (%upstream-linux-source version hash)
                             deblob-scripts-6.6)))
