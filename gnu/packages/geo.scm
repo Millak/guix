06d6dc2a95de03d59f53ba7e150fd084110cb9b3
Liliana Marie Prikler <liliana.prikler@gmail.com>
2024-07-15 23:02:14 +0200

    gnu: gnome-maps: Update to 46.11.
    * gnu/packages/geo.scm (gnome-maps): Update to 46.11.

--- gnu/packages/geo.scm
+++ gnu/packages/geo.scm
@@ -487,7 +487,7 @@ topology functions.")
 (define-public gnome-maps
   (package
     (name "gnome-maps")
-    (version "46.10")
+    (version "46.11")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://gnome/sources/" name "/"
@@ -495,7 +495,7 @@ topology functions.")
                                   name "-" version ".tar.xz"))
               (sha256
                (base32
-                "1br1ak0cwvvv8rszj9ffyvir7qcbxys940ygy22dzzn2l2byw9az"))))
+                "0daq2f96vh6fc3r9348c9vpa13yg2s3pa5irbqr1620bfjwl22wl"))))
     (build-system meson-build-system)
     (arguments
      (list
