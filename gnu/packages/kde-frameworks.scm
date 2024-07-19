c059dcec5c7989bce7ca189b82131a5163741c2d
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-07-20 00:24:44 +0800

    gnu: karchive: Update to 6.4.0.
    * gnu/packages/kde-frameworks.scm (karchive): Update to 6.4.0.
    
    Change-Id: Ie267602b71afc0c9b39b1a8f5b24145a8e1a9ae2

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -502,7 +502,7 @@ documentation.")
 (define-public karchive
   (package
     (name "karchive")
-    (version "6.3.0")
+    (version "6.4.0")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://kde/stable/frameworks/"
@@ -510,7 +510,7 @@ documentation.")
                                   "/" name "-" version ".tar.xz"))
               (sha256
                (base32
-                "0aafcxizxzh239sz9ffsgxbq6c4a368bm3l93jj9m3v60xbpz017"))))
+                "1fys0r43lc1055fg3ihn11hdh62csyr0hsf831y6q34nhiix1r5w"))))
     (build-system cmake-build-system)
     (arguments
      (list #:phases #~(modify-phases %standard-phases
