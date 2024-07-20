cef3176305f57013ebfea491afa716551697ef63
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-07-20 19:10:24 +0800

    gnu: kde-gtk-config: Update to 6.1.3.
    * gnu/packages/kde-plasma.scm (kde-gtk-config): Update to 6.1.3.
    
    Change-Id: I72df0e91f53c84f42edd03a999cf88e8803ee053

--- gnu/packages/kde-plasma.scm
+++ gnu/packages/kde-plasma.scm
@@ -468,14 +468,14 @@ RDP server.")
 (define-public kde-gtk-config
   (package
     (name "kde-gtk-config")
-    (version "6.1.2")
+    (version "6.1.3")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://kde/stable/plasma/" version
                                   "/kde-gtk-config-" version ".tar.xz"))
               (sha256
                (base32
-                "099n4dyyqirjm8ixgl3ifiral3jb2ivg74gzfv2xmfgj0cjwszza"))))
+                "0ijf80c6cms29i98hrssw1x0ysfp7zz7g9jlskb8lgd9axazk90g"))))
     (build-system qt-build-system)
     (arguments
      (list
