91b69f154db218834de002dcd013a8f47170e684
Evgeny Pisemsky <mail@pisemsky.site>
2024-07-22 11:00:12 +0300

    gnu: wireviz: Update to 0.4.1.
    * gnu/packages/engineering.scm (wireviz): Update to 0.4.1.
    
    Change-Id: Icdf85520272781d7a62867b6b10e111dcfda537d
    Signed-off-by: Nicolas Goaziou <mail@nicolasgoaziou.fr>

--- gnu/packages/engineering.scm
+++ gnu/packages/engineering.scm
@@ -4089,16 +4089,17 @@ G-code instructions for FFF printers or PNG layers for mSLA 3D printers.")
 (define-public wireviz
   (package
     (name "wireviz")
-    (version "0.3.2")
-    (source (origin
-              (method url-fetch)
-              (uri (pypi-uri "wireviz" version))
-              (sha256
-               (base32
-                "1dgnlxxlna2m1fh0ybivw0psym0sa5cqsl72mjl79bwfspnif61h"))))
+    (version "0.4.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (pypi-uri "wireviz" version))
+       (sha256
+        (base32 "1qbh0pknpymc42k4661b8ghbfk9him75xx57siyrl9is5s6as98f"))))
     (build-system python-build-system)
-    (propagated-inputs (list python-graphviz python-pillow python-pyyaml))
-    (home-page "https://github.com/formatc1702/WireViz")
+    (propagated-inputs
+     (list python-click python-graphviz python-pillow python-pyyaml))
+    (home-page "https://github.com/wireviz/WireViz")
     (synopsis "Easily document cables and wiring harnesses")
     (description
      "WireViz is a tool for easily documenting cables, wiring harnesses and
