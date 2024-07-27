f7f125ba5fbed09289b6b98673b709664d2a01ea
Jean SIMARD <woshilapin@tuziwo.info>
2024-07-27 13:07:52 +0000

    gnu: go-github-com-protonmail-go-crypto: Update to 1.0.0.
    * gnu/packages/golang-crypto.scm (go-github-com-protonmail-go-crypto):
    Update to 1.0.0.  Adjust package style.
    [inputs]: Add go-github-com-cloudflare-circl.
    
    Change-Id: I775c32f7da0e1983c085ddaf24f7767909bdaebc
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/golang-crypto.scm
+++ gnu/packages/golang-crypto.scm
@@ -1202,32 +1202,35 @@ algorithm.")
 (define-public go-github-com-protonmail-go-crypto
   (package
     (name "go-github-com-protonmail-go-crypto")
-    (version "0.0.0-20220623141421-5afb4c282135")
+    (version "1.0.0")
     (source
      (origin
        (method git-fetch)
        (uri (git-reference
              (url "https://github.com/ProtonMail/go-crypto")
-             (commit (go-version->git-ref version))))
+             (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "05qxdbn8wdk901z5kw2r3jdrag58nxlcsy0p8xd6rq0d71sw94wy"))))
+        (base32 "11q94983r6zjrdvflpikms4773a9s5vb9gg4qw1rj5800yhhah0n"))))
     (build-system go-build-system)
     (arguments
-     (list #:import-path "github.com/ProtonMail/go-crypto"
-           #:tests? #f ; Source-only package.
-           #:phases
-           #~(modify-phases %standard-phases
-               ;; Source-only package.
-               (delete 'build))))
+     (list
+      #:import-path "github.com/ProtonMail/go-crypto"
+      #:tests? #f ; Source-only package.
+      #:phases
+      #~(modify-phases %standard-phases
+          ;; Source-only package.
+          (delete 'build))))
     (propagated-inputs
-     (list go-golang-org-x-crypto))
+     (list go-github-com-cloudflare-circl
+           go-golang-org-x-crypto))
     (home-page "https://github.com/ProtonMail/go-crypto")
     (synopsis "Fork of x/crypto with up-to-date OpenPGP implementation")
-    (description "This package provides cryptography for Go.  This version of
-the package is a fork that adds a more up-to-date OpenPGP implementation.  It
-is completely backwards compatible with @code{golang.org/x/crypto}, the
-official package.")
+    (description
+     "This package provides cryptography for Go.  This version of the package
+is a fork that adds a more up-to-date OpenPGP implementation.  It is
+completely backwards compatible with @code{golang.org/x/crypto}, the official
+package.")
     (license license:bsd-3)))
 
 (define-public go-github-com-quic-go-qtls-go1-20
