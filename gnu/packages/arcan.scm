035d1ec7063b4cf29e99ede9a92d0e65e3f6fe03
Ahmad Draidi <a.r.draidi@redscript.org>
2024-08-03 11:55:54 +0400

    gnu: durden: Update to 0.6.1-2.43211ac.
    * gnu/packages/arcan.scm (durden): Update to 0.6.1-2.43211ac.
    
    Change-Id: I4a5f99a15a04995bc09603fedd8227d469308df1
    Signed-off-by: Andreas Enge <andreas@enge.fr>

--- gnu/packages/arcan.scm
+++ gnu/packages/arcan.scm
@@ -187,9 +187,8 @@ engine with a Lua scripting interface.")
     (synopsis "Combined display server, multimedia framework and game engine (SDL)")))
 
 (define-public durden
-  ;; Match Arcan 0.6.3
-  (let ((commit "a8938b9c835f55bedc2c42aec4ddc5c9739eb949")
-        (revision "1"))
+  (let ((commit "43211acd7994836d5c4ba039d7ecc939dcf8a5aa")
+        (revision "2"))
     (package
       (name "durden")
       (version (git-version "0.6.1" revision commit))
@@ -201,7 +200,7 @@ engine with a Lua scripting interface.")
                (url "https://github.com/letoram/durden")
                (commit commit)))
          (sha256
-          (base32 "1ybi6x2kwn597kjqycrqmlvp6z79yv2jfwzgx937wcckm55xlpvk"))
+          (base32 "1jns4lq959s8ydykm0yg9sddzh74k7fdvjrnbz3z91idb7abl92d"))
          (patches (search-patches "durden-shadow-arcan.patch"))))
       (build-system copy-build-system)
       (arguments
