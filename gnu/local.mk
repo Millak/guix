df485838b3a17511b258361428f1c9914bdc6185
Kaelyn Takata <kaelyn.alexi@protonmail.com>
2024-08-04 18:28:36 +0000

    gnu: webrtc-audio-processing: Fix build on i686 and powerpc64le.
    * gnu/packages/audio.scm (webrtc-audio-processing)[arguments]: New field.
    * gnu/packages/patches/webrtc-audio-processing-byte-order-pointer-size.patch,
    gnu/packages/patches/webrtc-audio-processing-x86-no-sse.patch: New files.
    * gnu/local.mk (dist_patch_DATA): Add them.
    
    Change-Id: I3a95e5e2794df0d51e005672b5d3e2d98f27b767
    Co-authored-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/local.mk
+++ gnu/local.mk
@@ -2283,6 +2283,8 @@ dist_patch_DATA =						\
   %D%/packages/patches/wcstools-extend-makefiles.patch	\
   %D%/packages/patches/wdl-link-libs-and-fix-jnetlib.patch	\
   %D%/packages/patches/webkitgtk-adjust-bubblewrap-paths.patch	\
+  %D%/packages/patches/webrtc-audio-processing-byte-order-pointer-size.patch \
+  %D%/packages/patches/webrtc-audio-processing-x86-no-sse.patch	\
   %D%/packages/patches/webrtc-for-telegram-desktop-unbundle-libsrtp.patch \
   %D%/packages/patches/websocketpp-fix-for-cmake-3.15.patch	\
   %D%/packages/patches/wlroots-hwdata-fallback.patch		\
