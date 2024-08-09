e8bad83362acde637062f7b8b5ebff7073593d2b
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-08-09 23:32:44 +0800

    gnu: plasma-activities-stats: Update to 6.1.4.
    * gnu/packages/kde-frameworks.scm (plasma-activities-stats): Update to 6.1.4.
    
    Change-Id: Ic488a0cc0e23d33d5a6d2766185cc10586073d5e

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -3143,7 +3143,7 @@ with other frameworks.")
 (define-public plasma-activities-stats
   (package
     (name "plasma-activities-stats")
-    (version "6.1.3")
+    (version "6.1.4")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://kde/stable/plasma/"
@@ -3151,7 +3151,7 @@ with other frameworks.")
                                   version ".tar.xz"))
               (sha256
                (base32
-                "0sfni5mraa6m39w6gm26la28vhlwk43xh4n3bhpp2vqzj5fj7qpv"))))
+                "0xr0gzncb4sf6asdd5hgfs9bkfjv9z4nn215jvlmyk2r56zydylj"))))
     (build-system cmake-build-system)
     (native-inputs
      (list extra-cmake-modules))
