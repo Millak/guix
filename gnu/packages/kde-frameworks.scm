1d9f92466137e399ad781956a9533623c2c42a02
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-08-13 14:54:53 +0800

    gnu: kfilemetadata: Update to 6.5.0.
    * gnu/packages/kde-frameworks.scm (kfilemetadata): Update to 6.5.0.
    
    Change-Id: I08ab40b06217406b85c4404edbcfbfcf840d7482

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -2526,7 +2526,7 @@ from DocBook files.")
 (define-public kfilemetadata
   (package
     (name "kfilemetadata")
-    (version "6.4.0")
+    (version "6.5.0")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -2535,7 +2535,7 @@ from DocBook files.")
                     name "-" version ".tar.xz"))
               (sha256
                (base32
-                "1vf5qj4777wqpbgpinygm8rdcy867hvl5a1a9bqmwk5yahq47zs5"))))
+                "1x60zl20qg19d615r62vf6kavzaij451n55wqvgqkqvz7n11ji2p"))))
     (build-system cmake-build-system)
     (arguments
      (list
