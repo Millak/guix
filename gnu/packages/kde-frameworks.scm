129a1ba3ad9f897566b423f987c73c3c93e22bb1
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-08-13 14:54:49 +0800

    gnu: kguiaddons: Update to 6.5.0.
    * gnu/packages/kde-frameworks.scm (kguiaddons): Update to 6.5.0.
    
    Change-Id: I932117a282572d0d04c71c2dc5cb20017c942534

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -1045,7 +1045,7 @@ replace the other outdated Graphviz tools.")
 (define-public kguiaddons
   (package
     (name "kguiaddons")
-    (version "6.4.0")
+    (version "6.5.0")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -1054,7 +1054,7 @@ replace the other outdated Graphviz tools.")
                     name "-" version ".tar.xz"))
               (sha256
                (base32
-                "04fgc8rgx1j3swf5pxh7cxdggsj3r0fffh90gm4y4nq9p3q5vhp1"))))
+                "01s8mjpazvl4j1fhb00f24zcsbin5zjz319sv9znxyl51f9zm4vi"))))
     (build-system qt-build-system)
     ;; TODO: Build packages for the Python bindings.  Ideally this will be
     ;; done for all versions of python guix supports.  Requires python,
