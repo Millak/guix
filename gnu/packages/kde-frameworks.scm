b18c2898d4107c4344538cb9b3a7ce02518b7057
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-08-13 14:54:56 +0800

    gnu: kparts: Update to 6.5.0.
    * gnu/packages/kde-frameworks.scm (kparts): Update to 6.5.0.
    
    Change-Id: I6309343d22f3876cd3dd94ac3c8ee37ae940e62a

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -4270,7 +4270,7 @@ notifications which can be embedded in your application.")
 (define-public kparts
   (package
     (name "kparts")
-    (version "6.4.0")
+    (version "6.5.0")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -4279,7 +4279,7 @@ notifications which can be embedded in your application.")
                     name "-" version ".tar.xz"))
               (sha256
                (base32
-                "13cr3q15nlmdasylr2ij6xcnnfmdc6d7rbzs41agjqvw7cfwac7z"))))
+                "07rp987bkzpvmb2735r3jm573dxmvyd92bs8hmnc2xvz4rxl3daf"))))
     (build-system qt-build-system)
     (arguments
      (list #:phases
