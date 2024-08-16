dc2c1bd1d922f0edb4cb98910134896ffdef76be
Ricardo Wurmus <rekado@elephly.net>
2024-08-16 18:36:18 +0200

    gnu: r-withr: Update to 3.0.1.
    * gnu/packages/statistics.scm (r-withr): Update to 3.0.1.
    
    Change-Id: Ic9302091cacb203a3d6e45183c871ba12276e3ba

--- gnu/packages/statistics.scm
+++ gnu/packages/statistics.scm
@@ -3090,13 +3090,13 @@ tools to simplify the devolpment of R packages.")
 (define-public r-withr
   (package
     (name "r-withr")
-    (version "3.0.0")
+    (version "3.0.1")
     (source (origin
               (method url-fetch)
               (uri (cran-uri "withr" version))
               (sha256
                (base32
-                "1rz0pgm1bg3cnc09s8q8yyg83lmf8bv9arlasciqqr0fdpgfwy4c"))))
+                "0h9qbaa4i63awk176i3pm7bia7wgssav9zk9jvc7sg3k5yng2wym"))))
     (build-system r-build-system)
     (native-inputs
      (list r-knitr))
