53e3619d92079a7b2a2ccd803d1c9c10a13bf608
Danny Milosavljevic <dannym@scratchpost.org>
2024-08-17 13:16:57 +0200

    import: pypi: make-pypi-sexp: Detect 'null description.
    * guix/import/pypi.scm (make-pypi-sexp): Detect 'null description.
    
    Change-Id: I8b2704852ecbd8de1bf7bf5598727e7a3a43932c

--- guix/import/pypi.scm
+++ guix/import/pypi.scm
@@ -544,8 +544,9 @@ name)))
                         'native-inputs)
         (home-page ,(project-info-home-page info))
         (synopsis ,(project-info-summary info))
-        (description ,(beautify-description
-                       (project-info-summary info)))
+        (description ,(and=> (non-empty-string-or-false
+                              (project-info-summary info))
+                             beautify-description))
         (license ,(license->symbol
                    (string->license
                     (project-info-license info)))))
