6c045f2c9eb7b8efe7c8a1002cda990abaa9be1a
Andrew Tropin <andrew@trop.in>
2024-08-18 08:31:25 +0400

    gnu: kmonad: Update to 0.4.2-1.07cd1cb.
    * gnu/packages/haskell-apps.scm (kmonad): Update to 0.4.2-1.07cd1cb.
    
    Change-Id: I8a8d36577804d2221b39b430d26e9fd17204c0de

--- gnu/packages/haskell-apps.scm
+++ gnu/packages/haskell-apps.scm
@@ -617,9 +617,9 @@ and mIRC chat codes.")
 
 (define-public kmonad
   ;; Project is active, but no new releases exist. Pick current master
-  ;; HEAD as of 2023-01-08.
-  (let ((commit "31c591b647d277fe34cb06fc70b0d053dd15f867")
-        (revision "0"))
+  ;; HEAD as of 2024-08-18.
+  (let ((commit "07cd1cb4fddb46a8d9de3bb9d06196d08b7a8ed2")
+        (revision "1"))
     (package
       (name "kmonad")
       (version (git-version "0.4.2" revision commit))
@@ -631,7 +631,7 @@ and mIRC chat codes.")
                (commit commit)))
          (file-name (git-file-name name version))
          (sha256
-          (base32 "0k1dfyy1q86l5yivv1jrclgvp9k48qm8pzk1j9wavq92li77y7r5"))))
+          (base32 "08ybif2lw0jy9h2hrlvx3469a3hkvih9gsg60kp9qnklzvqjdy5i"))))
       (build-system haskell-build-system)
       (arguments
        `(#:haddock? #f ; Haddock fails to generate docs
