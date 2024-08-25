59d3f1ab3fa63e6ccf7afbb5dcd397980b36f68a
Ricardo Wurmus <rekado@elephly.net>
2024-08-25 22:26:44 +0200

    gnu: r-tgp: Update to 2.4-22.1.
    * gnu/packages/cran.scm (r-tgp): Update to 2.4-22.1.
    
    Change-Id: I588869f684458957940e08efaa21f1d6c608d3f3

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -4187,13 +4187,13 @@ Chaitanya Swamy <doi:10.1145/2395116.2395117>.")
 (define-public r-tgp
   (package
     (name "r-tgp")
-    (version "2.4-22")
+    (version "2.4-22.1")
     (source (origin
               (method url-fetch)
               (uri (cran-uri "tgp" version))
               (sha256
                (base32
-                "0gpndjq2xiic6fb5187f4hgrqj3477pd83caajnw3yy9wan4vly6"))))
+                "1jf4mr59qp169z00pxb4kc0484mg73hgcyajg9hisdxl02qs1m4r"))))
     (properties `((upstream-name . "tgp")))
     (build-system r-build-system)
     (propagated-inputs (list r-maptree))
