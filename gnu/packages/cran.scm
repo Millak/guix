afb6f222f6bdc344ae45cb2eb33f0b44382eae22
Ricardo Wurmus <rekado@elephly.net>
2024-08-25 22:25:55 +0200

    gnu: r-arrow: Update to 17.0.0.1.
    * gnu/packages/cran.scm (r-arrow): Update to 17.0.0.1.
    
    Change-Id: I5a287b9488f40ea888d1faed15c2b3187520b56a

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -26529,14 +26529,14 @@ colored by the number of neighboring points.  This is useful to visualize the
 (define-public r-arrow
   (package
     (name "r-arrow")
-    (version "16.1.0")
+    (version "17.0.0.1")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "arrow" version))
        (sha256
         (base32
-         "1vz7vjm8j1k1ng2za3y1qdsrzhbx2p6zy48vsas6bkdywxp5ihb6"))))
+         "0pp7rllpmdwsx0idqhfg4lv96cvdj6pbnypnlrr835jqv7sxn502"))))
     (properties `((upstream-name . "arrow")))
     (build-system r-build-system)
     (inputs
