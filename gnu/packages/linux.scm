Millak/guix

commit 2a6ea3895bf554a70c699a1013d75ffa5407bcad

gnu: libfabric: Add support for extra providers.
* gnu/packages/linux.scm (libfabric) [arguments]: Add configure flags.

Change-Id: I57e43ffd92e8268112e48928af38b0fecc256a67
Signed-off-by: Ludovic Courtès <ludo@gnu.org>

gnu/packages/linux.scm
@@ -8933,7 +8933,13 @@ comparing system environments.")
                        ((member (package-supported-systems psm))
                         `(("psm" ,psm)))
                        (_ `()))
+              ("psm2" ,psm2)
               ("libnl" ,libnl)))
+    (arguments
+     (list #:configure-flags
+           #~(list "--enable-psm2"
+                   "--enable-efa"
+                   "--enable-verbs")))
     (home-page "https://ofiwg.github.io/libfabric/")
     (synopsis "Open Fabric Interfaces")
     (description

commit 1003d62790c32a5bf33fb09de590bbe459fb39d7

gnu: libfabric: Depend on PSM2 only on supported systems.
Fixes a regression introduced in
2a6ea3895bf554a70c699a1013d75ffa5407bcad, that would make ‘libfabric’
unsupported on non-x86_64 systems.

* gnu/packages/linux.scm (libfabric)[inputs]: Define ‘if-supported’.
Use it for ‘psm’ and ‘psm2, and remove labels.
[arguments]: Remove ‘--enable-psm2’, which is unnecessary.

Change-Id: I2a52ce6feb52b8773779a401088743491c3ad30b

gnu/packages/linux.scm
@@ -8903,17 +8903,20 @@ comparing system environments.")
        (sha256
         (base32 "1znmw83rmippv0fwz0x7lgylfk17dr9ckll8lrm4z7kclspnqpj8"))))
     (build-system gnu-build-system)
-    (inputs `(("rdma-core" ,rdma-core)
-              ,@(match (%current-system)
-                       ((member (package-supported-systems psm))
-                        `(("psm" ,psm)))
-                       (_ `()))
-              ("psm2" ,psm2)
-              ("libnl" ,libnl)))
+    (inputs
+     (let ((if-supported                          ;XXX: copied from openmpi
+            (lambda (package)
+              (if (and (not (%current-target-system))
+                       (member (%current-system)
+                               (package-supported-systems package)))
+                  (list package)
+                  '()))))
+       (append (list rdma-core libnl)
+               (if-supported psm)
+               (if-supported psm2))))
     (arguments
      (list #:configure-flags
-           #~(list "--enable-psm2"
-                   "--enable-efa"
+           #~(list "--enable-efa"
                    "--enable-verbs")))
     (home-page "https://ofiwg.github.io/libfabric/")
     (synopsis "Open Fabric Interfaces")