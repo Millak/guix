b5d3fd162408980742c0493146fbc0509aa86125
宋文武 <iyzsong@member.fsf.org>
2024-09-03 07:38:50 +0800

    gnu: thunar: Update to 4.18.11.
    * gnu/packages/xfce.scm (thunar): Update to 4.18.11.
    
    Change-Id: I80189eeef2f4d1278bffef7568e2894866eef3fd

--- gnu/packages/xfce.scm
+++ gnu/packages/xfce.scm
@@ -720,7 +720,7 @@ like appearance, display, keyboard and mouse settings.")
 (define-public thunar
   (package
     (name "thunar")
-    (version "4.18.10")                           ;stable version = even minor
+    (version "4.18.11")                           ;stable version = even minor
     (source (origin
               (method url-fetch)
               (uri (string-append "https://archive.xfce.org/src/xfce/"
@@ -728,7 +728,7 @@ like appearance, display, keyboard and mouse settings.")
                                   "thunar-" version ".tar.bz2"))
               (sha256
                (base32
-                "020xrwzdnk5b7cli8vmv3yn7sfq6pg7bz57m4p0xh0ln2cgqlc78"))))
+                "04nnfnm1913vva3d8515l3vs99q61rhbafj0gl9qqmka0zidl2vx"))))
     (build-system glib-or-gtk-build-system)
     (arguments
      '(#:configure-flags '("--with-custom-thunarx-dirs-enabled")))
