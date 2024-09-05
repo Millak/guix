bfb70cbe4955fa77536774371c6d750125f5277a
Mathieu Laparie <mathieu.laparie@inrae.fr>
2024-09-05 22:12:27 +0200

    gnu: libplacebo: Update to 7.349.0.
    * gnu/packages/video.scm (libplacebo): Update to 7.349.0.
    
    Signed-off-by: Nicolas Goaziou <mail@nicolasgoaziou.fr>
    Change-Id: Iacbde8cbd6e2008655f70fc009290cddb942f548

--- gnu/packages/video.scm
+++ gnu/packages/video.scm
@@ -1546,7 +1546,7 @@ libebml is a C++ library to read and write EBML files.")
 (define-public libplacebo
   (package
     (name "libplacebo")
-    (version "6.338.2")
+    (version "7.349.0")
     (source
      (origin
        (method git-fetch)
@@ -1555,7 +1555,7 @@ libebml is a C++ library to read and write EBML files.")
              (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "0d8pvv88bw21myf3pvnls2cv6bvdzsv76n5iy5hy3c3gf4mb4kl0"))))
+        (base32 "1q12yf04z570a2l3vkig2iqm7bgqr90hdxmn78sk2injrsyx124q"))))
     (build-system meson-build-system)
     (arguments
      (list #:configure-flags
