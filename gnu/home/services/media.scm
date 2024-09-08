7cd78468a214f262403c369f1b7a7aa8e8c73a8e
Ian Eure <ian@retrospec.tv>
2024-09-08 16:10:20 -0700

    gnu: Add home-jellyfin-mpv-shim-service-type.
    * gnu/home/services/media.scm (home-jellyfin-mpv-shim-service-type): New variable.
    * doc/guix.texi (Jellyfin Services): New section.
    * doc/guix.texi (Kodi Services): Add subheading.
    
    Change-Id: I037ab0602214fdaa1b032be51ff98ecf6b7ee16c

--- gnu/home/services/media.scm
+++ gnu/home/services/media.scm
@@ -1,5 +1,6 @@
 ;;; GNU Guix --- Functional package management for GNU
 ;;; Copyright © 2023 Janneke Nieuwenhuizen <janneke@gnu.org>
+;;; Copyright © 2024, 2025 Ian Eure <ian@retrospec.tv>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -19,14 +20,20 @@
 (define-module (gnu home services media)
   #:use-module (srfi srfi-26)
   #:use-module (gnu home services)
+  #:use-module (gnu home services desktop)
   #:use-module (gnu home services shepherd)
   #:use-module (gnu packages kodi)
+  #:use-module (gnu packages video)
   #:use-module (gnu services configuration)
   #:use-module (gnu services shepherd)
+  #:use-module (guix packages)
   #:use-module (guix records)
   #:use-module (guix gexp)
   #:export (home-kodi-configuration
-            home-kodi-service-type))
+            home-kodi-service-type
+
+            home-jellyfin-mpv-shim-configuration
+            home-jellyfin-mpv-shim-service-type))
 
 
 ;;;
@@ -66,3 +73,40 @@
    (description
     "Install and configure the Kodi media center so that it runs as a Shepherd
 service.")))
+
+(define-configuration home-jellyfin-mpv-shim-configuration
+  (package
+    (file-like jellyfin-mpv-shim)
+    "The Jellyfin MPV Shim package to use"))
+
+(define (jellyfin-mpv-shim-shepherd-service config)
+  (list (shepherd-service
+         (documentation "Jellyfin MPV Shim.")
+         (provision '(jellyfin-mpv-shim jellyfin-client))
+
+         ;; Depend on 'x11-display', which sets 'DISPLAY' if an X11 server is
+         ;; available, and fails to start otherwise.
+         (requirement '(x11-display))
+
+         (modules '((srfi srfi-1)
+                    (srfi srfi-26)
+                    (srfi srfi-98)))
+         (start
+          #~(make-forkexec-constructor
+             (list
+                #$(file-append
+                   (home-jellyfin-mpv-shim-configuration-package config)
+                   "/bin/jellyfin-mpv-shim"))))
+         (stop #~(make-kill-destructor)))))
+
+(define-public home-jellyfin-mpv-shim-service-type
+  (service-type
+   (name 'home-jellyfin-mpv-shim)
+   (extensions (list (service-extension home-shepherd-service-type
+                                        jellyfin-mpv-shim-shepherd-service)
+                     ;; Ensure 'home-x11-service-type' is instantiated so we
+                     ;; can depend on the Shepherd 'x11-display' service.
+                     (service-extension home-x11-service-type
+                                        (const #t))))
+   (default-value (home-jellyfin-mpv-shim-configuration))
+   (description "Run Jellyfin MPV Shim.")))
