ae58db59f85e23cf93e9e2e4cdbb84c0e31aa350
Tobias Geerinckx-Rice <me@tobias.gr>
2024-09-08 02:00:00 +0200

    gnu: synthv1: Update to 1.1.0.
    * gnu/packages/music.scm (synthv1): Update to 1.1.0.
    
    Change-Id: I0caa51355bb6ea09abd74bafc335c0ff86ba4d63

--- gnu/packages/music.scm
+++ gnu/packages/music.scm
@@ -2531,7 +2531,7 @@ perform creative live mixes with digital music files.")
 (define-public synthv1
   (package
     (name "synthv1")
-    (version "0.9.32")
+    (version "1.1.0")
     (source (origin
               (method url-fetch)
               (uri
@@ -2539,7 +2539,7 @@ perform creative live mixes with digital music files.")
                               "/synthv1-" version ".tar.gz"))
               (sha256
                (base32
-                "1i76d4w2anccyp70vylxxrmqgf1i3znl98ag82wz8lxb5sh34i4x"))))
+                "0szl3p9wgy7cdjb757yrkskr4sqs1gvqq2zk1cm7hvadc8r0pbp3"))))
     (build-system cmake-build-system)
     (arguments
      `(#:tests? #f))                    ; there are no tests
