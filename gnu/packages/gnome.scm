2a609ee48c21d9eb4971da6de3a388d4daa64d70
Liliana Marie Prikler <liliana.prikler@gmail.com>
2024-09-15 00:35:56 +0200

    gnu: libadwaita: Update to 1.6.0.
    * gnu/packages/gnome.scm (libadwaita): Update to 1.6.0.

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -14002,7 +14002,7 @@ Document Analysis and Recognition program.")
 (define-public libadwaita
   (package
     (name "libadwaita")
-    (version "1.5.2")
+    (version "1.6.0")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://gnome/sources/libadwaita/"
@@ -14010,7 +14010,7 @@ Document Analysis and Recognition program.")
                                   "libadwaita-" version ".tar.xz"))
               (sha256
                (base32
-                "05icswk84kf4pbcybv8j9r9n98q1dgr1m7zn6k72p4dlbh0fxyn9"))))
+                "1lyqvalqc09r4dkgrhpkp01r7c1c0zyf354icc0r375r9j9q7mwy"))))
     (build-system meson-build-system)
     (arguments
      `(#:phases
