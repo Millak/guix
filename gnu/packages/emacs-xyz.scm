1293259fe71d9c2928a78317c609137859f539ff
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2024-09-15 10:37:27 +0200

    gnu: emacs-oauth2: Update to 0.17.
    * gnu/packages/emacs-xyz.scm (emacs-oauth2): Update to 0.17.
    
    Change-Id: I0b73e8d48e3cd12c179351dfcc7cc92710b09242

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -22358,14 +22358,14 @@ write applications that use WebSockets, and is not useful by itself.")
 (define-public emacs-oauth2
   (package
     (name "emacs-oauth2")
-    (version "0.16")
+    (version "0.17")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://elpa.gnu.org/packages/"
                            "oauth2-" version ".tar"))
        (sha256
-        (base32 "1rzly2nwjywrfgcmp8zidbmjl2ahyd8l8507lb1mxm4xqryvf316"))))
+        (base32 "0ah0h3k6hiqm977414kyg96r6rrvnwvik3hz3ra3r0mxx7lksqha"))))
     (build-system emacs-build-system)
     (home-page "https://elpa.gnu.org/packages/oauth2.html")
     (synopsis "OAuth 2.0 authorization protocol implementation")
