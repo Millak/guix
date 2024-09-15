ecc95ecd399ce7917a19ae3e687ea1a92caf830a
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2024-09-15 16:10:22 +0200

    gnu: emacs-consult-denote: Update to 0.2.1.
    * gnu/packages/emacs-xyz.scm (emacs-consult-denote): Update to 0.2.1.
    
    Change-Id: Idbcecab439bf5384711aecb75b95ea9b47e193f4

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -12609,14 +12609,14 @@ completion using Consult.")
 (define-public emacs-consult-denote
   (package
     (name "emacs-consult-denote")
-    (version "0.1.2")
+    (version "0.2.1")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://elpa.gnu.org/packages/consult-denote-"
                            version ".tar"))
        (sha256
-        (base32 "0kvxjglh8cij22qzqbxaf8zrp75cph4536iqncp1rq789i0mjkzm"))))
+        (base32 "15hih62a6lyfgw3ha86hppc9wp468gslrdcnx8kcz0dlkvp0k1kx"))))
     (build-system emacs-build-system)
     (propagated-inputs (list emacs-consult emacs-denote))
     (home-page "https://github.com/protesilaos/consult-denote")
