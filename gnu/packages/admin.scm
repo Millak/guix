Millak/guix

commit be50b25b7b093f3879623f2252ebfae5db7fc5cd

gnu: sudo: Update to 1.9.16.
* gnu/packages/admin.scm (sudo): Update to 1.9.16.

Change-Id: I2079b4d83195cc6271320c10eee378c8cbd1ee02

gnu/packages/admin.scm
@@ -2041,7 +2041,7 @@ system administrator.")
 (define-public sudo
   (package
     (name "sudo")
-    (version "1.9.14p3")
+    (version "1.9.16")
     (source (origin
               (method url-fetch)
               (uri
@@ -2051,7 +2051,7 @@ system administrator.")
                                     version ".tar.gz")))
               (sha256
                (base32
-                "0qibg30d30gy85g83fj6gsg59g1sj3i9mkfl0k0851dwqjqii0x0"))
+                "0gd0pyycc3jnbgq5f8056fyc4a1ix257j2rxazy35dq6gxwlvn60"))
               (modules '((guix build utils)))
               (snippet
                '(begin

commit 79726a0507e09d31a838d1a42ab734dfa9fff3d0

gnu: sudo: Fix cross-compilation.
Fixes a regression introduced in
be50b25b7b093f3879623f2252ebfae5db7fc5cd.

* gnu/packages/admin.scm (sudo)[arguments]: Define ‘ac_cv_*’ when
cross-compiling.

Change-Id: Iaa6f856c902ff8e06d790f8b1bc49ecda66ce5f2

gnu/packages/admin.scm
@@ -2072,7 +2072,16 @@ system administrator.")
              ;; provide it.
              (string-append "CPPFLAGS=-D_PATH_MV=\\\""
                             (assoc-ref %build-inputs "coreutils")
-                            "/bin/mv\\\""))
+                            "/bin/mv\\\"")
+
+             ;; When cross-compiling, assume we have a working 'snprintf' and
+             ;; 'vsnprintf' (which we do, when using glibc).  The default
+             ;; choice fails with undefined references to 'sudo_snprintf' &
+             ;; co. when linking.
+             ,@(if (%current-target-system)
+                   '("ac_cv_have_working_snprintf=yes"
+                     "ac_cv_have_working_vsnprintf=yes")
+                   '()))
 
        ;; Avoid non-determinism; see <http://bugs.gnu.org/21918>.
        #:parallel-build? #f