Millak/guix

commit bfeccd3db9446d45d04d9144698506a61321e1c6

gnu: libgccjit: Track the default GCC version.
* gnu/packages/gcc.scm (libgccjit): Rewrite to use the default gcc
version.

gnu/packages/gcc.scm
@@ -1243,7 +1243,8 @@ misnomer.")))
 (define-public libgccjit-12 (make-libgccjit gcc-12))
 (define-public libgccjit-14 (make-libgccjit gcc-14))
 
-(define-public libgccjit libgccjit-10)
+;; Use the 'gcc' variable from above to track the same version.
+(define-public libgccjit (make-libgccjit gcc))
 
 (define (make-gccgo gcc)
   "Return a gccgo package based on GCC."

commit 552a18c24a9228ea6ee3a12d64366a15e3a0adbb

gnu: libgccjit: Remove duplicate libgccjit@11 package.
Fixes a regression introduced in
bfeccd3db9446d45d04d9144698506a61321e1c6 and caught by
‘tests/packages.scm’, whereby two equivalent but not ‘eq?’ libgccjit@11
packages would be exported.

* gnu/packages/gcc.scm (libgccjit): Make an alias for ‘libgccjit-11’.

Change-Id: I9aeacb7588af5f78af0941e80715665572567958

gnu/packages/gcc.scm
@@ -1225,8 +1225,9 @@ misnomer.")))
 (define-public libgccjit-12 (make-libgccjit gcc-12))
 (define-public libgccjit-14 (make-libgccjit gcc-14))
 
-;; Use the 'gcc' variable from above to track the same version.
-(define-public libgccjit (make-libgccjit gcc))
+;; This must match the 'gcc' variable, but it must also be 'eq?' to one of the
+;; libgccjit-* packages above.
+(define-public libgccjit libgccjit-11)
 
 (define (make-gccgo gcc)
   "Return a gccgo package based on GCC."