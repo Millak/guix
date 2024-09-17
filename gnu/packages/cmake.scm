e84e762a0364cfd1d9a343301a11b4e9c0daa1bf
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2024-09-17 14:32:54 +0900

    gnu: Add tinycmmc.
    * gnu/packages/cmake.scm (tinycmmc): New variable.
    
    Change-Id: Ie3a4e04f1ab3fb3ad6b6825f371d233a4ee905e2

--- gnu/packages/cmake.scm
+++ gnu/packages/cmake.scm
@@ -499,3 +499,24 @@ C/C++ projects.  It features:
 @item Support calling Qt Linguist Tools via CMake conveniently
 @end itemize")
       (license license:expat))))
+
+(define-public tinycmmc
+  (package
+    (name "tinycmmc")
+    (version "0.1.0")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/Grumbel/tinycmmc")
+                    (commit (string-append "v" version))))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "0chv7h6vnd8zpa6b69krxwdgvp3n5fd37355wa1zwi14x4nyyay5"))))
+    (build-system cmake-build-system)
+    (arguments (list #:tests? #f))      ;no test suite
+    (home-page "https://github.com/Grumbel/tinycmmc")
+    (synopsis "Tiny CMake Module Collections")
+    (description "The tinycmmc package contains a small collection of reusable
+CMake modules.")
+    (license license:zlib)))
