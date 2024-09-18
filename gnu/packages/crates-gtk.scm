3f51f27360cf75708962854031d05d7eefd198f7
Efraim Flashner <efraim@flashner.co.il>
2024-09-18 14:12:43 +0300

    gnu: Add rust-gobject-sys-0.19.
    * gnu/packages/crates-gtk.scm (rust-gobject-sys-0.19): New variable.
    (rust-gobject-sys-0.18): Inherit from rust-gobject-sys-0.19.
    
    Change-Id: I3808d54539fc7910d6c867d71d917c3248b2ac44

--- gnu/packages/crates-gtk.scm
+++ gnu/packages/crates-gtk.scm
@@ -2101,8 +2101,34 @@
        (("rust-shell-words" ,rust-shell-words-0.1)
         ("rust-tempfile" ,rust-tempfile-3))))))
 
+(define-public rust-gobject-sys-0.19
+  (package
+    (name "rust-gobject-sys")
+    (version "0.19.8")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "gobject-sys" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "17lb7dfbpcg8zchwlfbc08kckwf0a7d9n5ly3pyic13f5ljpws9f"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs (("rust-glib-sys" ,rust-glib-sys-0.19)
+                       ("rust-libc" ,rust-libc-0.2)
+                       ("rust-system-deps" ,rust-system-deps-6))
+       #:cargo-development-inputs (("rust-shell-words" ,rust-shell-words-1)
+                                   ("rust-tempfile" ,rust-tempfile-3))))
+    (native-inputs (list pkg-config))
+    (inputs (list glib))
+    (home-page "https://gtk-rs.org/")
+    (synopsis "FFI bindings to libgobject-2.0")
+    (description "This package provides FFI bindings to libgobject-2.0.")
+    (license license:expat)))
+
 (define-public rust-gobject-sys-0.18
   (package
+    (inherit rust-gobject-sys-0.19)
     (name "rust-gobject-sys")
     (version "0.18.0")
     (source
@@ -2112,7 +2138,6 @@
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
         (base32 "0i6fhp3m6vs3wkzyc22rk2cqj68qvgddxmpaai34l72da5xi4l08"))))
-    (build-system cargo-build-system)
     (arguments
      `(#:tests? #f ; `G_TYPE_FUNDAMENTAL_MAX` constant mismatch with gcc
        #:cargo-inputs
@@ -2121,13 +2146,7 @@
         ("rust-system-deps" ,rust-system-deps-6))
        #:cargo-development-inputs
        (("rust-shell-words" ,rust-shell-words-1)
-        ("rust-tempfile" ,rust-tempfile-3))))
-    (native-inputs (list pkg-config))
-    (inputs (list glib))
-    (home-page "https://gtk-rs.org/")
-    (synopsis "FFI bindings to libgobject-2.0")
-    (description "This package provides FFI bindings to libgobject-2.0.")
-    (license license:expat)))
+        ("rust-tempfile" ,rust-tempfile-3))))))
 
 (define-public rust-gobject-sys-0.17
   (package
