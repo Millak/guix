339d349d9b38139d0c033e5fc65645a27f9cde37
Efraim Flashner <efraim@flashner.co.il>
2024-09-29 15:38:16 +0300

    gnu: rust-encoding-rs-0.8: Update to 0.8.34.
    * gnu/packages/crates-io.scm (rust-encoding-rs-0.8): Update to 0.8.34.
    [cargo-inputs]: Add rust-any-all-workaround-0.1.  Remove
    rust-packed-simd-0.3.
    
    Change-Id: I13814c4fcac04036eb72f07f6aed15b5dfc3e8d7

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -23945,21 +23945,19 @@ encodings.")
 (define-public rust-encoding-rs-0.8
   (package
     (name "rust-encoding-rs")
-    (version "0.8.33")
+    (version "0.8.34")
     (source
      (origin
        (method url-fetch)
        (uri (crate-uri "encoding_rs" version))
-       (file-name
-        (string-append name "-" version ".tar.gz"))
+       (file-name (string-append name "-" version ".tar.gz"))
        (sha256
-        (base32
-         "1qa5k4a0ipdrxq4xg9amms9r9pnnfn7nfh2i9m3mw0ka563b6s3j"))))
+        (base32 "0nagpi1rjqdpvakymwmnlxzq908ncg868lml5b70n08bm82fjpdl"))))
     (build-system cargo-build-system)
     (arguments
      `(#:cargo-inputs
-       (("rust-cfg-if" ,rust-cfg-if-1)
-        ("rust-packed-simd" ,rust-packed-simd-0.3)
+       (("rust-any-all-workaround" ,rust-any-all-workaround-0.1)
+        ("rust-cfg-if" ,rust-cfg-if-1)
         ("rust-serde" ,rust-serde-1))
        #:cargo-development-inputs
        (("rust-bincode" ,rust-bincode-1)
