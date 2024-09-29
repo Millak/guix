724ace834fe91eba6ceab22e8362a2fb41c4dc48
Efraim Flashner <efraim@flashner.co.il>
2024-09-29 15:53:07 +0300

    gnu: rust-dtparse-1: Update to 1.5.0.
    * gnu/packages/crates-io.scm (rust-dtparse-1): Update to 1.5.0.
    [cargo-development-inputs]: Replace rust-base64-0.13 with 0.21.
    
    Change-Id: I2da31a13f75edad4a319eea8deca9c901ca66657

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -23072,20 +23072,20 @@ Rust.")
   (package
     (inherit rust-dtparse-2)
     (name "rust-dtparse")
-    (version "1.3.0")
+    (version "1.5.0")
     (source
      (origin
        (method url-fetch)
        (uri (crate-uri "dtparse" version))
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
-        (base32 "1awhyy58c28fhr5nvvfpikdzraihichkz3w1mzdg7smyffldi4w2"))))
+        (base32 "00g423c3nw7ffhnvxlryg5hl721aib4b4dq3alpfib8lq3ims8v8"))))
     (arguments
      `(#:cargo-inputs (("rust-chrono" ,rust-chrono-0.4)
                        ("rust-lazy-static" ,rust-lazy-static-1)
                        ("rust-num-traits" ,rust-num-traits-0.2)
                        ("rust-rust-decimal" ,rust-rust-decimal-1))
-       #:cargo-development-inputs (("rust-base64" ,rust-base64-0.13))))))
+       #:cargo-development-inputs (("rust-base64" ,rust-base64-0.21))))))
 
 (define-public rust-duct-0.13
   (package
