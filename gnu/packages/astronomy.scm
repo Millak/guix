2278733ac2190596407f935be5198a9900f19071
Sharlatan Hellseher <sharlatanus@gmail.com>
2024-10-01 19:10:58 +0100

    gnu: Add python-astrodendro.
    * gnu/packages/astronomy.scm (python-astrodendro): New variable.
    
    Change-Id: I23bee0e92f1a172c6688f79629c172ecb0b7dd62

--- gnu/packages/astronomy.scm
+++ gnu/packages/astronomy.scm
@@ -1549,6 +1549,48 @@ Astropy objects.")
 astronomical images, especially when there is no WCS information available.")
     (license license:expat)))
 
+(define-public python-astrodendro
+  ;; XXX: 0.2.0 was released 8 years ago and not compatible with Python 3.10,
+  ;; use the latest commit on master branch, see
+  ;; <https://github.com/dendrograms/astrodendro/issues/189>.
+  (let ((commit "3181c36489caa4c62b36b7ec8e06f4da65af6fda")
+        (revision "0"))
+    (package
+      (name "python-astrodendro")
+      (version (git-version "0.2.0" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/dendrograms/astrodendro")
+               (commit commit)))
+         (sha256
+          (base32 "1imahzl0g8r1lxyiz9wi9q4zk519yxgn6qs1nb0ybb34sipjs4kz"))
+         (file-name (git-file-name name version))))
+      (build-system pyproject-build-system)
+      (arguments
+       (list
+        #:phases
+        #~(modify-phases %standard-phases
+            (add-before 'build 'set-env-version
+              (lambda _
+                (setenv "SETUPTOOLS_SCM_PRETEND_VERSION"
+                        #$(version-major+minor+point version)))))))
+      (native-inputs
+       (list python-pytest
+             python-setuptools-scm))
+      (propagated-inputs
+       (list python-astropy
+             python-h5py
+             python-matplotlib
+             python-numpy))
+      (home-page "https://dendrograms.readthedocs.io/")
+      (synopsis "Astronomical dendrograms computation")
+      (description
+       "This package provides an way to compute dendrograms of observed or
+simulated Astronomical data in Python.")
+      (license license:expat))))
+
 (define-public python-astroml
   (package
     (name "python-astroml")
