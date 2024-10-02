3f328ab6ed3dd5f07727c9facde33f743e5d9308
Steve George <steve@futurile.net>
2024-10-02 19:24:13 +0100

    gnu: Add rust-windows-core-0.58.
    * gnu/packages/crates-windows.scm (rust-windows-core-0.58): New variable.
    (rust-windows-core-0.52): Inherit from rust-windows-core-0.58.
    
    Change-Id: I4c0366ea62eacb56441db3259409219ea572621d

--- gnu/packages/crates-windows.scm
+++ gnu/packages/crates-windows.scm
@@ -1696,8 +1696,33 @@ crate.")
                        ("rust-syn" ,rust-syn-2)
                        ("rust-windows-metadata" ,rust-windows-metadata-0.51))))))
 
+(define-public rust-windows-core-0.58
+  (package
+    (name "rust-windows-core")
+    (version "0.58.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "windows-core" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "16czypy425jzmiys4yb3pwsh7cm6grxn9kjp889iqnf2r17d99kb"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs (("rust-windows-implement" ,rust-windows-implement-0.58)
+                       ("rust-windows-interface" ,rust-windows-interface-0.58)
+                       ("rust-windows-result" ,rust-windows-result-0.2)
+                       ("rust-windows-strings" ,rust-windows-strings-0.1)
+                       ("rust-windows-targets" ,rust-windows-targets-0.52))))
+    (home-page "https://github.com/microsoft/windows-rs")
+    (synopsis "Rust for Windows")
+    (description "This package provides Rust for Windows.")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-windows-core-0.52
   (package
+    (inherit rust-windows-core-0.58)
     (name "rust-windows-core")
     (version "0.52.0")
     (source
