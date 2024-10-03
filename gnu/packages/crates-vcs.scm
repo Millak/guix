32e07730bad910d5e787f44635935db0208279c3
Steve George <steve@futurile.net>
2024-10-03 21:15:30 +0100

    gnu: Add rust-gix-revwalk-0.15.
    * gnu/packages/crates-vcs(rust-gix-revwalk-0.15): New variable.
    (rust-gix-revwalk-0.11): Inherit from rust-gix-revwalk-0.15.
    
    Change-Id: Idd8b7740b22ede3cfaca29bd6a6d6b071baced38

--- gnu/packages/crates-vcs.scm
+++ gnu/packages/crates-vcs.scm
@@ -4980,8 +4980,37 @@ handles finding names and parsing specifications.")
         ("rust-serde" ,rust-serde-1)
         ("rust-thiserror" ,rust-thiserror-1))))))
 
+(define-public rust-gix-revwalk-0.15
+  (package
+    (name "rust-gix-revwalk")
+    (version "0.15.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "gix-revwalk" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "111r3ddls12y1s884x01gai8ykdsn4qmnggghbk4w24k9da747ml"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs
+       (("rust-gix-commitgraph" ,rust-gix-commitgraph-0.24)
+         ("rust-gix-date" ,rust-gix-date-0.9)
+         ("rust-gix-hash" ,rust-gix-hash-0.14)
+         ("rust-gix-hashtable" ,rust-gix-hashtable-0.5)
+         ("rust-gix-object" ,rust-gix-object-0.44)
+         ("rust-smallvec" ,rust-smallvec-1)
+         ("rust-thiserror" ,rust-thiserror-1))))
+    (home-page "https://github.com/Byron/gitoxide")
+    (synopsis "Utilities for walking Git's revision graph")
+    (description
+     "Utilities for walking Git's revision graph.  This crate is part of
+Gitoxide, a pure Rust implementation of Git.")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-gix-revwalk-0.11
   (package
+    (inherit rust-gix-revwalk-0.15)
     (name "rust-gix-revwalk")
     (version "0.11.1")
     (source
