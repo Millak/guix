0af682970251e92a7042c087bb2c08816c00e83e
Steve George <steve@futurile.net>
2024-10-03 15:12:28 +0100

    gnu: Add rust-gix-filter-0.13.
    * gnu/packages/crates-vcs.scm (rust-gix-filter-0.13): New variable.
    (rust-gix-filter-0.8): Inherit from rust-gix-filter-0.13.
    
    Change-Id: Ie83676416c5b5cd773654f07e11482385aeb05ad

--- gnu/packages/crates-vcs.scm
+++ gnu/packages/crates-vcs.scm
@@ -2419,8 +2419,44 @@ compile-time feature flags.")
        #:cargo-development-inputs
        (("rust-bstr" ,rust-bstr-1))))))
 
+(define-public rust-gix-filter-0.13
+  (package
+    (name "rust-gix-filter")
+    (version "0.13.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "gix-filter" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "17hi8c47n1mmccpbwfcjs4w829wnfhpcgv1vjmdnw1j0w457j8a1"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs
+       (("rust-bstr" ,rust-bstr-1)
+        ("rust-encoding-rs" ,rust-encoding-rs-0.8)
+        ("rust-gix-attributes" ,rust-gix-attributes-0.22)
+        ("rust-gix-command" ,rust-gix-command-0.3)
+        ("rust-gix-hash" ,rust-gix-hash-0.14)
+        ("rust-gix-object" ,rust-gix-object-0.44)
+        ("rust-gix-packetline-blocking" ,rust-gix-packetline-blocking-0.17)
+        ("rust-gix-path" ,rust-gix-path-0.10)
+        ("rust-gix-quote" ,rust-gix-quote-0.4)
+        ("rust-gix-trace" ,rust-gix-trace-0.1)
+        ("rust-gix-utils" ,rust-gix-utils-0.1)
+        ("rust-smallvec" ,rust-smallvec-1)
+        ("rust-thiserror" ,rust-thiserror-1))
+       #:cargo-development-inputs (("rust-serial-test" ,rust-serial-test-3))))
+    (home-page "https://github.com/Byron/gitoxide")
+    (synopsis "Part of Gitoxide, this package implements Git filters")
+    (description
+     "This package provides a crate from the Gitoxide project implementing
+Git filters in Rust.")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-gix-filter-0.8
   (package
+    (inherit rust-gix-filter-0.13)
     (name "rust-gix-filter")
     (version "0.8.1")
     (source
@@ -2430,7 +2466,6 @@ compile-time feature flags.")
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
         (base32 "05p5yr0syd91k0yfn6pdqw66yvbq7q6ffzpd53s5g7dzi3bc367m"))))
-    (build-system cargo-build-system)
     (arguments
      `(#:cargo-inputs (("rust-bstr" ,rust-bstr-1)
                        ("rust-encoding-rs" ,rust-encoding-rs-0.8)
@@ -2445,13 +2480,7 @@ compile-time feature flags.")
                        ("rust-gix-utils" ,rust-gix-utils-0.1)
                        ("rust-smallvec" ,rust-smallvec-1)
                        ("rust-thiserror" ,rust-thiserror-1))
-       #:cargo-development-inputs (("rust-serial-test" ,rust-serial-test-2))))
-    (home-page "https://github.com/Byron/gitoxide")
-    (synopsis "Part of Gitoxide, this package implements Git filters in Rust")
-    (description
-     "This package provides a crate from the Gitoxide project implementing
-Git filters in Rust.")
-    (license (list license:expat license:asl2.0))))
+       #:cargo-development-inputs (("rust-serial-test" ,rust-serial-test-2))))))
 
 (define-public rust-gix-filter-0.7
   (package
