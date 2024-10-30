d16e008c0b262d4fb97d4b1b389328003e6b7206
Roman Scherer <roman@burningswell.com>
2024-10-30 19:55:53 +0100

    gnu: Add rust-urid-0.1.
    * gnu/packages/crates-io.scm (rust-urid-0.1): New variable.
    
    Change-Id: Ic5043d3f91bd4abb8a99c91b4d7108e03f54ada8

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -90067,6 +90067,25 @@ mile, ...).")
     (description "This package provides minimal request library in Rust.")
     (license (list license:expat license:asl2.0))))
 
+(define-public rust-urid-0.1
+  (package
+    (name "rust-urid")
+    (version "0.1.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "urid" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "195672gs136vczn1r4hkjg5vfa7vdzr26bzv6lwhk0z7cvbvaa38"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs (("rust-urid-derive" ,rust-urid-derive-0.1))))
+    (home-page "https://github.com/RustAudio/rust-lv2")
+    (synopsis "Library for idiomatic URID support")
+    (description "This package provides Library for idiomatic URID support.")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-urid-derive-0.1
   (package
     (name "rust-urid-derive")
