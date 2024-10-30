f0f1ebfc2ea0dece51d991c8472b28b6cbe2016d
Roman Scherer <roman@burningswell.com>
2024-10-30 19:55:56 +0100

    gnu: Add rust-lv2-units-0.1.
    * gnu/packages/crates-io.scm (rust-lv2-units-0.1): New variable.
    
    Change-Id: I90dac2f754fe870c8eefb5cad3bec6873861316d
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -43596,6 +43596,26 @@ Server Protocol.")
     (description "This package provides Rust LV2 C header bindings.")
     (license (list license:expat license:asl2.0))))
 
+(define-public rust-lv2-units-0.1
+  (package
+    (name "rust-lv2-units")
+    (version "0.1.3")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "lv2-units" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0fdamp3hxdr36hqi1j6y01rz1x17if1ibzr7rr4nrabidw74gf82"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs (("rust-lv2-sys" ,rust-lv2-sys-2)
+                       ("rust-urid" ,rust-urid-0.1))))
+    (home-page "https://github.com/RustAudio/rust-lv2")
+    (synopsis "Rust LV2 wrapper of LV2 unit types")
+    (description "This package provides a Rust LV2 wrapper of LV2 unit types.")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-lz4-1
   (package
     (name "rust-lz4")
