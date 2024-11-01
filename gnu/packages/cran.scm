b4c56467c033bcf0b6796b626f9270fbb51a0e92
Ricardo Wurmus <rekado@elephly.net>
2024-11-01 13:47:14 +0100

    gnu: Add r-mcr.
    * gnu/packages/cran.scm (r-mcr): New variable.
    
    Change-Id: I61388006a27120772e52c83ab48d37f4795432cb

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -4198,6 +4198,36 @@ the group sizes are small.")
 related models.  This is done using Markov chain Monte Carlo techniques.")
     (license license:gpl2+)))
 
+(define-public r-mcr
+  (package
+    (name "r-mcr")
+    (version "1.3.3.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (cran-uri "mcr" version))
+       (sha256
+        (base32 "0gbpnbs8mgqpyiayddcaf1xwh908gk1n375bqpnvfppyj0wz3yr9"))))
+    (properties `((upstream-name . "mcr")))
+    (build-system r-build-system)
+    (propagated-inputs (list r-robslopes))
+    (native-inputs (list gfortran))
+    (home-page "https://cran.r-project.org/package=mcr")
+    (synopsis "Method comparison regression")
+    (description
+     "Regression methods to quantify the relation between two measurement
+methods are provided by this package.  In particular it addresses regression
+problems with errors in both variables and without repeated measurements.  It
+implements the CLSI recommendations (see J. A. Budd et al. (2018,
+@url{https://clsi.org/standards/products/method-evaluation/documents/ep09/})
+for analytical method comparison and bias estimation using patient samples.
+Furthermore, algorithms for Theil-Sen and equivariant Passing-Bablok
+estimators are implemented, see F. Dufey (2020, <doi:10.1515/ijb-2019-0157>)
+and J. Raymaekers and F. Dufey (2022, <@code{arXiv:2202:08060>}).  A
+comprehensive overview over the implemented methods and references can be
+found in the manual pages @code{mcr-package} and @code{mcreg}.")
+    (license license:gpl3+)))
+
 (define-public r-shadowtext
   (package
     (name "r-shadowtext")
