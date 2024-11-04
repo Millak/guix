47f9017ab4f49373e26216dcdffdffe10b1a198c
Ricardo Wurmus <rekado@elephly.net>
2024-11-04 14:48:54 +0100

    gnu: r-genefilter: Update to 1.88.0.
    * gnu/packages/bioconductor.scm (r-genefilter): Update to 1.88.0.
    
    Change-Id: I8474fa2b50e5f7429c92001903846acd1a0be3b8

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -8134,14 +8134,14 @@ analysis using other methods.")
 (define-public r-genefilter
   (package
     (name "r-genefilter")
-    (version "1.86.0")
+    (version "1.88.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "genefilter" version))
        (sha256
         (base32
-         "0jp7jvgzb1bs0p76j9k8lhgliipjnif6bng7hdn5wi744jb5fmpr"))))
+         "1c5mi7g5l501x8l0cd27cvqpwfki740yxj9598sgvgmd8v8aczyd"))))
     (build-system r-build-system)
     (native-inputs
      (list gfortran r-knitr))
