a9d26157ba56cc1368d500f80e07d2d48b8c1d1b
Ricardo Wurmus <rekado@elephly.net>
2024-11-04 14:48:35 +0100

    gnu: r-agdex: Update to 1.54.0.
    * gnu/packages/bioconductor.scm (r-agdex): Update to 1.54.0.
    
    Change-Id: I7069ba379767d381879a43e7eb02f79c75424ca4

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -3036,13 +3036,13 @@ of samples that are affected by RNA degradation.")
 (define-public r-agdex
   (package
     (name "r-agdex")
-    (version "1.52.0")
+    (version "1.54.0")
     (source (origin
               (method url-fetch)
               (uri (bioconductor-uri "AGDEX" version))
               (sha256
                (base32
-                "088r2ahqjv61wnzp7by4c5v9aivahmyfkzr7prnkk3ff8zh6pznl"))))
+                "02kldgslgyx5xr5zn45vndai37j66wp4xdjpvasdv46b7rm7drx2"))))
     (properties `((upstream-name . "AGDEX")))
     (build-system r-build-system)
     (propagated-inputs (list r-biobase r-gseabase))
