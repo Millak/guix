fbf3a2ebcb16dbe37785b41a1e9c4f87467afa2d
Ricardo Wurmus <rekado@elephly.net>
2024-11-04 14:49:52 +0100

    gnu: r-bbcanalyzer: Update to 1.36.0.
    * gnu/packages/bioconductor.scm (r-bbcanalyzer): Update to 1.36.0.
    
    Change-Id: I549ff64e6fc31aadd651bf946b75614a706ba35e

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -24444,13 +24444,13 @@ posterior (3D array).")
 (define-public r-bbcanalyzer
   (package
     (name "r-bbcanalyzer")
-    (version "1.34.0")
+    (version "1.36.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "BBCAnalyzer" version))
        (sha256
-        (base32 "16zm6fix5f5277dkdaxyxl1q1n52960ma3vw8001a5bjxhic58rn"))))
+        (base32 "1021srhg6j3p0lwkn3d777iylrjqm8rvspfvj73hphk8l828cd53"))))
     (properties `((upstream-name . "BBCAnalyzer")))
     (build-system r-build-system)
     (propagated-inputs
