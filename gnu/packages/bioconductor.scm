81f1d6c5e86235eadbf4e7cbcd092bfc50b79737
Ricardo Wurmus <rekado@elephly.net>
2024-11-04 14:49:37 +0100

    gnu: r-bigmelon: Update to 1.32.0.
    * gnu/packages/bioconductor.scm (r-bigmelon): Update to 1.32.0.
    
    Change-Id: I09a0e0cd902eeda196fadf901ea44fa56b99350e

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -19933,14 +19933,14 @@ with multiple R processes supported by the package @code{parallel}.")
 (define-public r-bigmelon
   (package
     (name "r-bigmelon")
-    (version "1.30.0")
+    (version "1.32.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "bigmelon" version))
        (sha256
         (base32
-         "0fa1dwbk6hdiwg6dmbks7hy4cg3ydyd2sv94cyl0003awhf00nn9"))))
+         "05l7kgjgjx4rk3vgffmmh5jrg8qx9bvr9bmq9f70w4bxx4kj6xry"))))
     (properties `((upstream-name . "bigmelon")))
     (build-system r-build-system)
     (propagated-inputs
