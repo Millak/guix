2ee3d11e73d5ceb1998b7449c3c5935f536d90be
Ricardo Wurmus <rekado@elephly.net>
2024-11-04 14:49:16 +0100

    gnu: r-plyranges: Update to 1.26.0.
    * gnu/packages/bioconductor.scm (r-plyranges): Update to 1.26.0.
    
    Change-Id: I8dbb47dafa5957e03edb9fcd420bace436760d28

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -14194,13 +14194,13 @@ genes or proteins in these datasets.")
 (define-public r-plyranges
   (package
     (name "r-plyranges")
-    (version "1.24.0")
+    (version "1.26.0")
     (source (origin
               (method url-fetch)
               (uri (bioconductor-uri "plyranges" version))
               (sha256
                (base32
-                "0rh5l6bd80nhffqpnnc2c00mvp8p32ykld05h94c3y8c9mvmsl58"))))
+                "0ai6p7q4iva420wnm0008p1vivd72gc1anwhzzsmy14bxr52rwbx"))))
     (properties `((upstream-name . "plyranges")))
     (build-system r-build-system)
     (propagated-inputs (list r-biocgenerics
