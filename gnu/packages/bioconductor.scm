abe8d963e2b13076c8abe89285f19c52bcb649a1
Ricardo Wurmus <rekado@elephly.net>
2024-11-04 14:48:56 +0100

    gnu: r-gostats: Update to 2.72.0.
    * gnu/packages/bioconductor.scm (r-gostats): Update to 2.72.0.
    
    Change-Id: Ibc5f6d3471bef7298c9f34c37c5d6376c592c049

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -8497,14 +8497,14 @@ facilitate the use of this test for gene set testing of GO and KEGG terms.")
 (define-public r-gostats
   (package
     (name "r-gostats")
-    (version "2.70.0")
+    (version "2.72.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "GOstats" version))
        (sha256
         (base32
-         "1dygsr8nlqksfpk00lk67dhj4fcpjg1x4dbl2r4sfanlk4hq76cs"))))
+         "0zz2gmv2hg8cr0bxj4zb95p8cf8a19zx1v925cv5f4xh7m698bzj"))))
     (properties `((upstream-name . "GOstats")))
     (build-system r-build-system)
     (propagated-inputs
