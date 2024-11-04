32bedfd6e64fd27e19e5e934b3f6f1e0e29ed453
Ricardo Wurmus <rekado@elephly.net>
2024-11-04 14:49:45 +0100

    gnu: r-maftools: Update to 2.22.0.
    * gnu/packages/bioconductor.scm (r-maftools): Update to 2.22.0.
    [propagated-inputs]: Add r-pheatmap.
    
    Change-Id: Ia21c0fc80398c7f650a0e9134b9ae430d3a3cbc1

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -22553,18 +22553,19 @@ provides a wrapper of de novo motif discovery software.")
 (define-public r-maftools
   (package
     (name "r-maftools")
-    (version "2.20.0")
+    (version "2.22.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "maftools" version))
        (sha256
-        (base32 "0c2sf8l1wfgz3nh9ccm10y9wwh7rv55r09awavqlrr9glcbblp32"))))
+        (base32 "0vdqasrb4j7cvp66df2fvjbdi65480zrdzbbv057nlkvq1h4m3p3"))))
     (properties `((upstream-name . "maftools")))
     (build-system r-build-system)
     (propagated-inputs
      (list r-data-table
            r-dnacopy
+           r-pheatmap
            r-rcolorbrewer
            r-rhtslib
            r-survival
