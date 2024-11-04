cb765c421846a79d9dae8f0c090cc7e0b85e8d88
Janneke Nieuwenhuizen <janneke@gnu.org>
2024-11-04 19:58:01 +0100

    gnu: libstdc++: Support the 64bit Hurd.
    * gnu/packages/gcc.scm (make-libstdc++)[arguments]: When building for the
    64bit Hurd, add stage patch-hurd64.
    
    Change-Id: I795a591ef8282ee5b760fec43bd4ad849007f602

--- gnu/packages/gcc.scm
+++ gnu/packages/gcc.scm
@@ -1024,7 +1024,15 @@ using compilers other than GCC."
                   (("/lib64") "/lib")))))
           (add-before 'configure 'chdir
             (lambda _
-              (chdir "libstdc++-v3"))))
+              (chdir "libstdc++-v3")))
+          #$@(let ((version (package-version gcc)))
+               (if (target-hurd64?)
+                   #~((add-after 'unpack 'patch-hurd64
+                        (lambda _
+                          (substitute* "libstdc++-v3/src/c++20/tzdb.cc"
+                            (("#if ! defined _GLIBCXX_ZONEINFO_DIR")
+                             "#if __GNU__ || ! defined _GLIBCXX_ZONEINFO_DIR")))))
+                   '())))
 
       #:configure-flags '`("--disable-libstdcxx-pch"
                            ,(string-append "--with-gxx-include-dir="
