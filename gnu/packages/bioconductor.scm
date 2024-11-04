eff86e482b38c5768817a668b91de55e0f789437
Ricardo Wurmus <rekado@elephly.net>
2024-11-04 14:49:49 +0100

    gnu: r-parody: Update to 1.64.0.
    * gnu/packages/bioconductor.scm (r-parody): Update to 1.64.0.
    
    Change-Id: I1a8939ec820a766e41ed16c1dd96e3354b161001

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -23655,13 +23655,13 @@ other functional sequencing data.")
 (define-public r-parody
   (package
     (name "r-parody")
-    (version "1.62.0")
+    (version "1.64.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "parody" version))
        (sha256
-        (base32 "0k2gc8p9dk7j21n6hxdqjna09m6cggx47spkmxkqvwqm97mwqzrp"))))
+        (base32 "08vaqnrgkqxb99xjmgja3hs6r7n0z9chmykw6kjv6fgg914qlf2d"))))
     (properties `((upstream-name . "parody")))
     (build-system r-build-system)
     (native-inputs (list r-knitr))
