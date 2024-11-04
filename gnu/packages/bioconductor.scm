01a0374439e5c1a20390644dbf9b00806237f0cd
Ricardo Wurmus <rekado@elephly.net>
2024-11-04 14:48:27 +0100

    gnu: r-org-dm-eg-db: Update to 3.20.0.
    * gnu/packages/bioconductor.scm (r-org-dm-eg-db): Update to 3.20.0.
    
    Change-Id: Iff5df32995ccf1281b104533a3a34039885a2121

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -1004,13 +1004,13 @@ annotations for the genome of the model worm Caenorhabditis elegans.")
 (define-public r-org-dm-eg-db
   (package
     (name "r-org-dm-eg-db")
-    (version "3.19.1")
+    (version "3.20.0")
     (source (origin
               (method url-fetch)
               (uri (bioconductor-uri "org.Dm.eg.db" version 'annotation))
               (sha256
                (base32
-                "1sqw38z73sgq66x8qrb5xw0jxyimy7l1hs2iwm7xq9ylzrjw3yj0"))))
+                "1v21rx7kpdi30898jbnvg1cd3xgghvqhkiipkasn5wjz22z1lqna"))))
     (properties
      `((upstream-name . "org.Dm.eg.db")))
     (build-system r-build-system)
