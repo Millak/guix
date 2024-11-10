2d46a1b470563ff78f2220166f99239f14f18662
David Pflug <david@pflug.io>
2024-11-10 12:54:13 -0500

    gnu: postgresql-10: Replace deprecated dependency.
    * gnu/packages/databases.scm (postgresql-10): Inherit postgresql-13
    instead of 11.
    
    Using the deprecated postgresql-11 package was causing warnings to be
    printed on `guix pull`.
    
    Change-Id: I36225ff2a1889f94cd5aabe229ee638793594abf
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/packages/databases.scm
+++ gnu/packages/databases.scm
@@ -1416,17 +1416,17 @@ pictures, sounds, or video.")
 
 (define-deprecated/public postgresql-10 #f
   (package
-    (inherit postgresql-11)
+    (inherit postgresql-13)
     (version "10.23")
     (source (origin
-              (inherit (package-source postgresql-11))
+              (inherit (package-source postgresql-13))
               (uri (string-append "https://ftp.postgresql.org/pub/source/v"
                                   version "/postgresql-" version ".tar.bz2"))
               (sha256
                (base32
                 "1sgfssjc9lnzijhn108r6z26fri655k413f1c9b8wibjhd9b594l"))))
     (native-inputs
-     (modify-inputs (package-native-inputs postgresql-11)
+     (modify-inputs (package-native-inputs postgresql-13)
        (append opensp docbook-sgml-4.2)
        (delete "docbook-xml")))))
 
