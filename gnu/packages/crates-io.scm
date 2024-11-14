378f13af607675edda72a8e201e5ae15d143168d
Efraim Flashner <efraim@flashner.co.il>
2024-11-14 10:13:11 +0200

    gnu: Add rust-quick-xml-0.36.
    * gnu/packages/crates-io.scm (rust-quick-xml-0.36): New variable.
    (rust-quick-xml-0.32): Inherit from rust-quick-xml-0.36.
    
    Change-Id: Ia83d8cd249a9661883884bf53468e89cd99625a9

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -61468,8 +61468,42 @@ to write.")
          (base32
           "1q6za3v78hsspisc197bg3g7rpc989qycy8ypr8ap8igv10ikl51"))))))
 
+(define-public rust-quick-xml-0.36
+  (package
+    (name "rust-quick-xml")
+    (version "0.36.2")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "quick-xml" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1zj3sjcjk6sn544wb2wvhr1km5f9cy664vzclygfsnph9mxrlr7p"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs (("rust-arbitrary" ,rust-arbitrary-1)
+                       ("rust-document-features" ,rust-document-features-0.2)
+                       ("rust-encoding-rs" ,rust-encoding-rs-0.8)
+                       ("rust-memchr" ,rust-memchr-2)
+                       ("rust-serde" ,rust-serde-1)
+                       ("rust-tokio" ,rust-tokio-1))
+       #:cargo-development-inputs
+       (("rust-criterion" ,rust-criterion-0.4)
+        ("rust-pretty-assertions" ,rust-pretty-assertions-1)
+        ("rust-regex" ,rust-regex-1)
+        ("rust-serde-value" ,rust-serde-value-0.7)
+        ("rust-serde-derive" ,rust-serde-derive-1)
+        ("rust-tokio" ,rust-tokio-1)
+        ("rust-tokio-test" ,rust-tokio-test-0.4))))
+    (home-page "https://github.com/tafia/quick-xml")
+    (synopsis "High performance xml reader and writer")
+    (description
+     "This package provides a high performance XML reader and writer.")
+    (license license:expat)))
+
 (define-public rust-quick-xml-0.32
   (package
+    (inherit rust-quick-xml-0.36)
     (name "rust-quick-xml")
     (version "0.32.0")
     (source
@@ -61479,7 +61513,6 @@ to write.")
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
         (base32 "1hk9x4fij5kq1mnn7gmxz1hpv8s9wnnj4gx4ly7hw3mn71c6wfhx"))))
-    (build-system cargo-build-system)
     (arguments
      `(#:cargo-inputs (("rust-arbitrary" ,rust-arbitrary-1)
                        ("rust-document-features" ,rust-document-features-0.2)
@@ -61487,18 +61520,14 @@ to write.")
                        ("rust-memchr" ,rust-memchr-2)
                        ("rust-serde" ,rust-serde-1)
                        ("rust-tokio" ,rust-tokio-1))
-       #:cargo-development-inputs (("rust-criterion" ,rust-criterion-0.4)
-                                   ("rust-pretty-assertions" ,rust-pretty-assertions-1)
-                                   ("rust-regex" ,rust-regex-1)
-                                   ("rust-serde-value" ,rust-serde-value-0.7)
-                                   ("rust-serde-derive" ,rust-serde-derive-1)
-                                   ("rust-tokio" ,rust-tokio-1)
-                                   ("rust-tokio-test" ,rust-tokio-test-0.4))))
-    (home-page "https://github.com/tafia/quick-xml")
-    (synopsis "High performance xml reader and writer")
-    (description
-     "This package provides a high performance XML reader and writer.")
-    (license license:expat)))
+       #:cargo-development-inputs
+       (("rust-criterion" ,rust-criterion-0.4)
+        ("rust-pretty-assertions" ,rust-pretty-assertions-1)
+        ("rust-regex" ,rust-regex-1)
+        ("rust-serde-value" ,rust-serde-value-0.7)
+        ("rust-serde-derive" ,rust-serde-derive-1)
+        ("rust-tokio" ,rust-tokio-1)
+        ("rust-tokio-test" ,rust-tokio-test-0.4))))))
 
 (define-public rust-quick-xml-0.31
   (package
