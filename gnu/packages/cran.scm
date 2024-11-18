9668566767c91ea04abccc00890390449e46e54c
Ricardo Wurmus <rekado@elephly.net>
2024-11-18 13:14:24 +0100

    gnu: r-utf8: Add test inputs.
    * gnu/packages/cran.scm (r-utf8)[native-inputs]: Add r-testthat.
    
    Change-Id: If097749e8b42a76608abf9eb6404dd53f11b473b

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -12448,7 +12448,7 @@ packages maintained by Yihui Xie.")
          "0zbv2s5dkzxim3ydyk12479m28f68mak9m50v396in4wpm5q53s1"))))
     (build-system r-build-system)
     (native-inputs
-     (list r-knitr r-rmarkdown)) ; for vignettes
+     (list r-knitr r-rmarkdown r-testthat)) ; for vignettes
     (home-page "https://github.com/patperry/r-utf8")
     (synopsis "Unicode text processing")
     (description
