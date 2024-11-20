1007136bc39fc5516b1b8295f2d8c856323f2056
Efraim Flashner <efraim@flashner.co.il>
2024-11-20 20:33:55 +0200

    gnu: rust-rustls-pki-types-1: Update to 1.10.0.
    * gnu/packages/crates-tls.scm (rust-rustls-pki-types-1): Update to
    1.10.0.
    [source]: Add snippet to allow for newer versions of dependencies.
    [cargo-development-inputs]: Add rust-crabgrind-0.1.
    [native-inputs]: Add valgrind.
    
    Change-Id: I490a1acde3cae79b9ceb5f710111999c3667ac4f

--- gnu/packages/crates-tls.scm
+++ gnu/packages/crates-tls.scm
@@ -44,7 +44,8 @@
   #:use-module (gnu packages perl)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages rust-apps)
-  #:use-module (gnu packages tls))
+  #:use-module (gnu packages tls)
+  #:use-module (gnu packages valgrind))
 
 (define-public rust-asn1-0.15
   (package
@@ -1617,18 +1618,25 @@ PEM-encodings commonly used to store keys and certificates at rest.")
 (define-public rust-rustls-pki-types-1
   (package
     (name "rust-rustls-pki-types")
-    (version "1.7.0")
+    (version "1.10.0")
     (source
      (origin
        (method url-fetch)
        (uri (crate-uri "rustls-pki-types" version))
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
-        (base32 "0banlc9xzwqrx8n0h4bd0igmq3z5hc72rn941lf22cp3gkkraqlp"))))
+        (base32 "0jv78c32pgf1i0bn7rzf4xdr9qh5wsvigp6akc1yhzls7hdj1w8n"))
+       (snippet
+        #~(begin (use-modules (guix build utils))
+                 (substitute* "Cargo.toml"
+                   (("\"=([[:digit:]]+(\\.[[:digit:]]+)*)" _ version)
+                    (string-append "\"^" version)))))))
     (build-system cargo-build-system)
     (arguments
      `(#:cargo-inputs
-       (("rust-web-time" ,rust-web-time-1))))
+       (("rust-web-time" ,rust-web-time-1))
+       #:cargo-development-inputs (("rust-crabgrind" ,rust-crabgrind-0.1))))
+    (native-inputs (list valgrind))
     (home-page "https://github.com/rustls/pki-types")
     (synopsis "Shared types for the rustls PKI ecosystem")
     (description
