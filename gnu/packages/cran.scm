5068915a0c48d18c0414a097b540daea1b257de3
Ricardo Wurmus <rekado@elephly.net>
2024-11-22 09:49:44 +0100

    gnu: r-pan: Update source hash.
    * gnu/packages/cran.scm (r-pan)[source]: Update hash.
    
    Change-Id: I3bf50242ae61bde2957ebf44acf2b429fce5e41d

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -11491,7 +11491,7 @@ impute compatibly with the substantive model.")
        (method url-fetch)
        (uri (cran-uri "pan" version))
        (sha256
-        (base32 "0liwcym035174h6c4b9ddmqxa3vkpkk3alyr9psk8yhv7i61hzp3"))))
+        (base32 "1skfdh8mg3hs9h651shyn80maas71jlbmvn06izym0rpclnj74fd"))))
     (build-system r-build-system)
     (native-inputs (list gfortran))
     (home-page "https://cran.r-project.org/web/packages/pan/")
