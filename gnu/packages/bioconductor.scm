a172e37da40694566af72b6932b8eff7e59e1bbc
Ricardo Wurmus <rekado@elephly.net>
2024-11-22 11:58:08 +0100

    gnu: r-gostats: Add test inputs.
    * gnu/packages/bioconductor.scm (r-gostats)[native-inputs]: Add r-runit.
    
    Change-Id: I8344a9cfbef93309054b6d238fee7b11f83c86f4

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -8682,7 +8682,7 @@ facilitate the use of this test for gene set testing of GO and KEGG terms.")
            r-graph
            r-rbgl
            r-rgraphviz))
-    (native-inputs (list r-knitr))
+    (native-inputs (list r-knitr r-runit))
     (home-page "https://bioconductor.org/packages/GOstats")
     (synopsis "Tools for manipulating GO and microarrays")
     (description
