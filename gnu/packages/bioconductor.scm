deb134d429578f8869aa7bab2f48d543b7e981e2
Ricardo Wurmus <rekado@elephly.net>
2024-11-22 11:57:49 +0100

    gnu: r-alabaster-ranges: Add test inputs.
    * gnu/packages/bioconductor.scm (r-alabaster-ranges)[native-inputs]: Add r-testthat.
    
    Change-Id: I8c5404d20fb37cd51462ee347e4e607ef35576ff

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -3475,7 +3475,7 @@ can enrich this metadata with context-specific properties.")
                              r-iranges
                              r-rhdf5
                              r-s4vectors))
-    (native-inputs (list r-knitr))
+    (native-inputs (list r-knitr r-testthat))
     (home-page "https://bioconductor.org/packages/alabaster.ranges")
     (synopsis "Load and save Ranges-related artifacts from file")
     (description
