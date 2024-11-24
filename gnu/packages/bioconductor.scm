1b8e3750542cd760f2923fb65ee5fbe45926a14c
Ricardo Wurmus <rekado@elephly.net>
2024-11-24 10:32:06 +0100

    gnu: r-codedepends: Add missing input.
    * gnu/packages/bioconductor.scm (r-codedepends)[native-inputs]: Add
    r-rgraphviz.
    
    Change-Id: I1d122d4ac01b003ea6de5fb1df99399839fcea4d

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -13857,7 +13857,7 @@ signals directly in the Bruker format.")
     (build-system r-build-system)
     (propagated-inputs
      (list r-codetools r-graph r-xml))
-    (native-inputs (list r-knitr r-runit))
+    (native-inputs (list r-knitr r-rgraphviz r-runit))
     (home-page "https://cran.r-project.org/web/packages/CodeDepends")
     (synopsis "Analysis of R code for reproducible research and code comprehension")
     (description
