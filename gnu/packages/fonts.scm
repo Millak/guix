0ed38f3d9f71dc117646bfcc60093d4971f96406
8dcc <8dcc.git@gmail.com>
2024-11-24 15:53:23 +0100

    gnu: fonts: Add font-dina.
    * gnu/packages/fonts.scm (font-dina): New variable.
    
    Change-Id: I10f63e17754227145d72b1fc3380a532d74cef55
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/packages/fonts.scm
+++ gnu/packages/fonts.scm
@@ -3874,3 +3874,22 @@ well on screen or in print.  Each font contains 1090 glyphs, offering
 full support for the conjuncts and ligatures required by languages
 written with the Devanagari script.")
       (license license:silofl1.1))))
+
+(define-public font-dina
+  (package
+    (name "font-dina")
+    (version "2.92")
+    (source (origin
+              (method url-fetch/zipbomb)
+              (uri "https://www.dcmembers.com/jibsen/download/61/?wpdmdl=61")
+              (file-name "Dina.zip")
+              (sha256
+               (base32
+                "1kq86lbxxgik82aywwhawmj80vsbz3hfhdyhicnlv9km7yjvnl8z"))))
+    (build-system font-build-system)
+    (home-page "https://www.dcmembers.com/jibsen/download/61/")
+    (synopsis "Dina programming font")
+    (description "Dina is a monospace bitmap font, primarily aimed at programmers.
+It is relatively compact to allow a lot of code on screen, while (hopefully) clear
+enough to remain readable even at high resolutions.")
+    (license license:expat)))
