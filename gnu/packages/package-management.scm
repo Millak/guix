89252ddeac52c65d832a0fe048ca7a975e2dbf21
Christopher Baines <mail@cbaines.net>
2024-11-27 11:55:28 +0000

    gnu: guix-build-coordinator: Update to 0-111.037eac0.
    * gnu/packages/package-management.scm (guix-build-coordinator): Update to
    0-111.037eac0.
    
    Change-Id: I23a0f5e8667d127adfcd43b8ec10c9e0ab34de57

--- gnu/packages/package-management.scm
+++ gnu/packages/package-management.scm
@@ -1538,8 +1538,8 @@ environments.")
                   "0k9zkdyyzir3fvlbcfcqy17k28b51i20rpbjwlx2i1mwd2pw9cxc")))))))
 
 (define-public guix-build-coordinator
-  (let ((commit "492d4628887ea726cedc1c2b55743acfe32dfe0c")
-        (revision "110"))
+  (let ((commit "037eac0357baa448afe6aeeaf82d8f2e2665bbcb")
+        (revision "111"))
     (package
       (name "guix-build-coordinator")
       (version (git-version "0" revision commit))
@@ -1550,7 +1550,7 @@ environments.")
                       (commit commit)))
                 (sha256
                  (base32
-                  "0ssns2l4dl54igqrr874g6z32s9r9qdf7dzfrqar4mmjz343n8rp"))
+                  "0gvpbjjzig610i2rsdb0d6vjhaq8z507m481462y6vpxa55ri4yb"))
                 (file-name (string-append name "-" version "-checkout"))))
       (build-system gnu-build-system)
       (arguments
