e768fbd92a9cd4ce26c33cbcc371c24412bdea61
Andy Tai <atai@atai.org>
2024-11-27 22:43:53 -0800

    gnu: mtools: Update to 4.0.46.
    * gnu/packages/mtools.scm (mtools): Update to 4.0.46.
    
    Change-Id: Ice61bcb61fed69d9ae83c72cd078be3fcc3dc8d8
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/packages/mtools.scm
+++ gnu/packages/mtools.scm
@@ -29,14 +29,14 @@
 (define-public mtools
   (package
     (name "mtools")
-    (version "4.0.45")
+    (version "4.0.46")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://gnu/mtools/mtools-"
                                   version ".tar.bz2"))
               (sha256
                (base32
-                "066ncw51da1h6w80p6cl6flw8sznk933sf545xa0xzhfpnb8l00b"))
+                "1h0npkb90hl5ki841j6npbvsplwj05clgv14g5wbg3zqb7c8vbcs"))
               (patches
                (search-patches "mtools-mformat-uninitialized.patch"))))
     (build-system gnu-build-system)
