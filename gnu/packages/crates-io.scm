fa54c9ced23a8a3d2df755ba86be1bc1da84222e
Murilo <murilo@disroot.org>
2024-11-29 11:36:25 -0300

    gnu: Add rust-nucleo-0.2.
    * gnu/packages/crates-io.scm (rust-nucleo-0.2): New variable.
    
    Change-Id: I0ee23791f2569ecea3e30d689465da63fc8e881b
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -46,6 +46,7 @@
 ;;; Copyright © 2024 Nguyễn Gia Phong <mcsinyx@disroot.org>
 ;;; Copyright © 2024 Jordan Moore <lockbox@struct.foo>
 ;;; Copyright © 2024 Sharlatan Hellseher <sharlatanus@gmail.com>
+;;; Copyright © 2024 Murilo <murilo@disroot.org>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -43136,6 +43137,28 @@ Server Protocol.")
     (synopsis "Rust LZ4 sys package")
     (description "This is the Rust LZ4 sys package.")
     (license license:expat)))
+
+(define-public rust-nucleo-0.2
+  (package
+    (name "rust-nucleo")
+    (version "0.2.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "nucleo" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1wzx32bz4n68dcd3yw57195sz49hdhv9b75jikr5qiyfpks32lxf"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs (("rust-nucleo-matcher" ,rust-nucleo-matcher-0.2)
+                       ("rust-parking-lot" ,rust-parking-lot-0.12)
+                       ("rust-rayon" ,rust-rayon-1))))
+    (home-page "https://github.com/helix-editor/nucleo")
+    (synopsis "Plug and play high performance fuzzy matcher")
+    (description
+     "This package provides plug and play high performance fuzzy matcher.")
+    (license license:mpl2.0)))
 
 (define-public rust-rust-lzma-0.5
   (package
