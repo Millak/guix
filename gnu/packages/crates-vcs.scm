98ce410f93e112886a5dafb566c68f95f843bed9
Murilo <murilo@disroot.org>
2024-11-29 11:36:42 -0300

    gnu: Add rust-gix-discover-0.32.
    * gnu/packages/crates-vcs.scm (rust-gix-discover-0.32): New variable.
    
    Change-Id: I8e4e53adbc90ee78099ad387504056604ed0a0e3
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/crates-vcs.scm
+++ gnu/packages/crates-vcs.scm
@@ -2554,6 +2554,32 @@ This package is part of Gitoxide, a pure Rust implementation of Git.")
                                    ("rust-serial-test" ,rust-serial-test-3)
                                    ("rust-tempfile" ,rust-tempfile-3))))))
 
+(define-public rust-gix-discover-0.32
+  (package
+    (inherit rust-gix-discover-0.35)
+    (name "rust-gix-discover")
+    (version "0.32.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "gix-discover" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1grxby9mj5c9bs305wrf70x0rsdpq25nc00csm86p9ixnscwc9zw"))))
+    (arguments
+     `(#:cargo-inputs (("rust-bstr" ,rust-bstr-1)
+                       ("rust-dunce" ,rust-dunce-1)
+                       ("rust-gix-fs" ,rust-gix-fs-0.11)
+                       ("rust-gix-hash" ,rust-gix-hash-0.14)
+                       ("rust-gix-path" ,rust-gix-path-0.10)
+                       ("rust-gix-ref" ,rust-gix-ref-0.44)
+                       ("rust-gix-sec" ,rust-gix-sec-0.10)
+                       ("rust-thiserror" ,rust-thiserror-1))
+       #:cargo-development-inputs (("rust-defer" ,rust-defer-0.2)
+                                   ("rust-is-ci" ,rust-is-ci-1)
+                                   ("rust-serial-test" ,rust-serial-test-3)
+                                   ("rust-tempfile" ,rust-tempfile-3))))))
+
 (define-public rust-gix-discover-0.28
   (package
     (inherit rust-gix-discover-0.35)
