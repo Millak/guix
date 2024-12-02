496976f8ecf6cafc1f2e3473ec9a68a3ef41568b
Efraim Flashner <efraim@flashner.co.il>
2024-12-02 20:08:18 +0200

    gnu: rust-gix-config-value-0.14: Update to 0.14.9.
    * gnu/packages/crates-vcs.scm (rust-gix-config-value-0.14): Update to
    0.14.9.
    
    Change-Id: Ib2d692bf455b39220474b7f9662256ed28bff8af

--- gnu/packages/crates-vcs.scm
+++ gnu/packages/crates-vcs.scm
@@ -1966,14 +1966,14 @@ Gitoxide project.  Gitoxide is a pure Rust implementation of Git.")
 (define-public rust-gix-config-value-0.14
   (package
     (name "rust-gix-config-value")
-    (version "0.14.8")
+    (version "0.14.9")
     (source
      (origin
        (method url-fetch)
        (uri (crate-uri "gix-config-value" version))
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
-        (base32 "133gpqyrg22c2kxsiv4ncp0rwwyxzkbq63y6xaccbpm0z9ln3xq3"))))
+        (base32 "0q26nzp4rhj90whwvsw8n3k1v6x4bwk3sn3n7awa8py7m7f3zppk"))))
     (build-system cargo-build-system)
     (arguments
      `(#:cargo-inputs (("rust-bitflags" ,rust-bitflags-2)
