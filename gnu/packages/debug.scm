e7030fbfb71a826eabac5928e1eb3747a2e2161b
Nguyễn Gia Phong <mcsinyx@disroot.org>
2024-12-05 01:24:49 +0900

    gnu: aflplusplus: Update to 4.30c.
    * gnu/packages/debug.scm (aflplusplus): Update to 4.30c.
    
    Change-Id: Id7cbea11e53faf138e2d5691ff298a20ba6d63b0
    Signed-off-by: Hilton Chain <hako@ultrarare.space>

--- gnu/packages/debug.scm
+++ gnu/packages/debug.scm
@@ -519,7 +519,7 @@ server and embedded PowerPC, and S390 guests.")
   (package
     (inherit american-fuzzy-lop)
     (name "aflplusplus")
-    (version "4.21c")
+    (version "4.30c")
     (source (origin
               (method git-fetch)
               (uri (git-reference
@@ -528,7 +528,7 @@ server and embedded PowerPC, and S390 guests.")
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "0f6gd8r1fyc4lgdkldx4mg1b72h1xdvpdrb2c44r3y4f2i3hzb0c"))))
+                "1lmpyf1gwz78pqxw9iqxhxzanbrid33ihq9xzmnk8c4yv449sdks"))))
     (arguments
      (substitute-keyword-arguments (package-arguments american-fuzzy-lop)
        ((#:make-flags _ ''())
