1f3037e2fcf51385b75eb3c81c61e0e88e8cb95e
Janneke Nieuwenhuizen <janneke@gnu.org>
2024-12-09 11:04:37 +0100

    gnu: serf: Update to 1.3.10 and fix build with gcc-14.
    * gnu/packages/patches/serf-python3.patch: Remove file.N
    * gnu/local.mk (dist_patch_DATA): Unregister it.
    * gnu/packages/web.scm (serf): Update to 1.3.10 and remove it.
    [arguments]: Add CFLAGS to #:scons-flags to relax gcc-14's strictness.
    
    Change-Id: I2cb4d9bc7e22ab0bedacbd79b98d33a1b6a27bd0

--- gnu/local.mk
+++ gnu/local.mk
@@ -2290,7 +2290,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/seq24-rename-mutex.patch			\
   %D%/packages/patches/libsequoia-fix-ffi-Makefile.patch	\
   %D%/packages/patches/libsequoia-remove-store.patch		\
-  %D%/packages/patches/serf-python3.patch			\
   %D%/packages/patches/shakespeare-spl-fix-grammar.patch		\
   %D%/packages/patches/shared-mime-info-xdgmime-path.patch	\
   %D%/packages/patches/sharutils-CVE-2018-1000097.patch		\
