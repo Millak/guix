51263dc34dba5ea2a8cc6d1eb096a54efcae965b
Josep Bigorra <jjbigorra@gmail.com>
2024-12-13 14:03:39 +0100

    gnu: Add qt6ct.
    * gnu/packages/qt.scm (qt6ct): New variable.
    
    Signed-off-by: Zheng Junjie <zhengjunjie@iscas.ac.cn>
    Change-Id: Icc6807229a56d1638627bce3f1e60949f74a446c

--- gnu/packages/qt.scm
+++ gnu/packages/qt.scm
@@ -31,6 +31,7 @@
 ;;; Copyright © 2022, 2024 Zheng Junjie <873216071@qq.com>
 ;;; Copyright © 2023 Herman Rimm <herman@rimm.ee>
 ;;; Copyright © 2024 Foundation Devices, Inc. <hello@foundation.xyz>
+;;; Copyright © 2024 Josep Bigorra <jjbigorra@gmail.com>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -266,6 +267,43 @@ of C++20 coroutines in connection with certain asynchronous Qt actions.")
 settings (such as icons, themes, and fonts) in desktop environments or
 window managers, that don't provide Qt integration by themselves.")
     (home-page "https://qt5ct.sourceforge.io/")
+    (license license:bsd-2)))
+
+(define-public qt6ct
+  (package
+    (name "qt6ct")
+    (version "0.9")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/trialuser02/qt6ct")
+             (commit version)))
+       (sha256
+        (base32 "0dknk4qqzqc5wa763nclb1k6jkmvjh8kzz8kfp4iggy9jy0vnzgb"))))
+    (build-system qt-build-system)
+    (arguments
+     (list
+      #:qtbase qtbase
+      #:tests? #f                      ; No target
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-after 'unpack 'patch
+            (lambda _
+              (substitute* '("src/qt6ct-qtplugin/CMakeLists.txt"
+                             "src/qt6ct-style/CMakeLists.txt")
+                (("\\$\\{PLUGINDIR\\}")
+                 (string-append #$output "/lib/qt6/plugins"))))))))
+    (native-inputs
+     (list qttools))
+    (inputs
+     (list qtsvg
+           libxkbcommon))
+    (synopsis "Qt6 Configuration Tool")
+    (description "Qt6CT is a program that allows users to configure Qt6
+settings (such as icons, themes, and fonts) in desktop environments or
+window managers, that don't provide Qt integration by themselves.")
+    (home-page "https://github.com/trialuser02/qt6ct")
     (license license:bsd-2)))
 
 (define-public kddockwidgets
