6f7778e5da7e9a655109ca4cf021e7507f44e7d3
Leo Famulari <leo@famulari.name>
2024-12-18 16:34:56 -0500

    gnu: MPD: Update to 0.23.16.
    * gnu/packages/mpd.scm (mpd): Update to 0.23.16.
    
    Change-Id: I2efb423027848d3cc0a87470dffa179aff6f7c47

--- gnu/packages/mpd.scm
+++ gnu/packages/mpd.scm
@@ -117,7 +117,7 @@ interfacing MPD in the C, C++ & Objective C languages.")
 (define-public mpd
   (package
     (name "mpd")
-    (version "0.23.15")
+    (version "0.23.16")
     (source (origin
               (method url-fetch)
               (uri
@@ -126,7 +126,7 @@ interfacing MPD in the C, C++ & Objective C languages.")
                               "/mpd-" version ".tar.xz"))
               (sha256
                (base32
-                "06k60ych9crifyx6zfc98hdcihixq5nba1c9rwngib6ik8ik40am"))))
+                "1hdj68g40xpzzrxayhb1ishdh57kbf204bcn7il8br0dz1ny6s4n"))))
     (build-system meson-build-system)
     (arguments
      (list
