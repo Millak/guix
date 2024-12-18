9dbdda6c22c89bfe2331810108fefc15e356460d
Sharlatan Hellseher <sharlatanus@gmail.com>
2024-12-18 03:22:51 +0000

    gnu: Add go-modernc-org-fileutil.
    * gnu/packages/golang-xyz.scm (go-modernc-org-fileutil): New variable.
    
    Change-Id: I2b2f2bb682f17395104ec728d8d54e10f4e37df4

--- gnu/packages/golang-xyz.scm
+++ gnu/packages/golang-xyz.scm
@@ -10796,6 +10796,30 @@ kubernetes-independent packages supplementing the
 @url{https://pkg.go.dev/std#stdlib,Go standard libs}.")
     (license license:asl2.0)))
 
+(define-public go-modernc-org-fileutil
+  (package
+    (name "go-modernc-org-fileutil")
+    (version "1.3.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://gitlab.com/cznic/fileutil")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "10q5xbik9yk9jw2ziq1fw0hpjfbv5h3qm7rlxlkwj0qxyyb7b9bi"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:import-path "modernc.org/fileutil"))
+    (propagated-inputs
+     (list go-modernc-org-mathutil))
+    (home-page "https://gitlab.com/cznic/fileutil")
+    (synopsis "File utility functions")
+    (description "Package fileutil collects some file utility functions.")
+    (license license:bsd-3)))
+
 (define-public go-modernc-org-memory
   (package
     (name "go-modernc-org-memory")
