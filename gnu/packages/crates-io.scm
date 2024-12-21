45a8acd0f290291aae0b345662438ad98df17917
Danny Milosavljevic <dannym@friendly-machines.com>
2024-12-21 12:55:59 +0100

    gnu: Add rust-bindgen@0.65.1.
    * gnu/packages/crates-io.scm (rust-bindgen-0.65): New variable.
    
    Change-Id: Ic0f508434fe844e795da01578125d0b0f8479d5b

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -7771,6 +7771,37 @@ bindings to C and C++ libraries.")
                        ("rust-syn" ,rust-syn-2)
                        ("rust-which" ,rust-which-4))))))
 
+(define-public rust-bindgen-0.65
+  (package
+    (inherit rust-bindgen-0.69)
+    (name "rust-bindgen")
+    (version "0.65.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "bindgen" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1i9wci1h3xnk8hi7cf06capgifnmpk9dd59zqznh6jcsdx37ppyg"))))
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs (("rust-annotate-snippets" ,rust-annotate-snippets-0.9)
+                       ("rust-bitflags" ,rust-bitflags-1)
+                       ("rust-cexpr" ,rust-cexpr-0.6)
+                       ("rust-clang-sys" ,rust-clang-sys-1)
+                       ("rust-lazy-static" ,rust-lazy-static-1)
+                       ("rust-lazycell" ,rust-lazycell-1)
+                       ("rust-log" ,rust-log-0.4)
+                       ("rust-peeking-take-while" ,rust-peeking-take-while-0.1)
+                       ("rust-prettyplease" ,rust-prettyplease-0.2)
+                       ("rust-proc-macro2" ,rust-proc-macro2-1)
+                       ("rust-quote" ,rust-quote-1)
+                       ("rust-regex" ,rust-regex-1)
+                       ("rust-rustc-hash" ,rust-rustc-hash-1)
+                       ("rust-shlex" ,rust-shlex-1)
+                       ("rust-syn" ,rust-syn-2)
+                       ("rust-which" ,rust-which-4))))))
+
 (define-public rust-bindgen-0.64
   (package
     (inherit rust-bindgen-0.66)
