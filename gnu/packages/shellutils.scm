0d4af3c55c7fd75f03a0b6ecc059720657268ff3
Jordan Moore <lockbox@struct.foo>
2024-12-22 11:14:48 -0500

    gnu: direnv: Update to 2.35.0.
    * gnu/packages/shellutils.scm (direnv): Update to 2.35.0.
    
    Change-Id: I22b1769bb2c848b9f59f265d2cfc6f99224f158b
    Signed-off-by: 宋文武 <iyzsong@member.fsf.org>

--- gnu/packages/shellutils.scm
+++ gnu/packages/shellutils.scm
@@ -600,7 +600,7 @@ Latin script and other languages.")
 (define-public direnv
   (package
     (name "direnv")
-    (version "2.34.0")
+    (version "2.35.0")
     (source
      (origin (method git-fetch)
              (uri (git-reference
@@ -609,7 +609,7 @@ Latin script and other languages.")
              (file-name (git-file-name name version))
              (sha256
               (base32
-               "0g04yxsj78v664dhkfa6fhm0vi2gzx1agr3h47dnm2f55wnymz0j"))))
+               "0l9pziv5nxlq6dxbsqrfl6z4ibq171wfx6wmjs392cdn5w2n908b"))))
     (build-system go-build-system)
     (arguments
      '(#:import-path "github.com/direnv/direnv"
