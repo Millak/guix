6aae8a013e226aa3d51f02a34384058db0c29d82
Efraim Flashner <efraim@flashner.co.il>
2024-12-22 14:09:14 +0200

    gnu: rust-dbl-0.3: Update to 0.3.2.
    * gnu/packages/crates-io.scm (rust-dbl-0.3) Update to 0.3.2.
    [arguments]: Don't skip the build.
    
    Change-Id: I918499fd489cc5bed966f17aafbfc7ca7a20dcb4

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -21207,24 +21207,21 @@ and arithmetic.")
 (define-public rust-dbl-0.3
   (package
     (name "rust-dbl")
-    (version "0.3.0")
+    (version "0.3.2")
     (source
       (origin
         (method url-fetch)
         (uri (crate-uri "dbl" version))
-        (file-name
-          (string-append name "-" version ".tar.gz"))
+        (file-name (string-append name "-" version ".tar.gz"))
         (sha256
-          (base32 "1pihf6zrzncbs3lsyqkzxxxqmjf8rfpwvs1sg8nmz8cv7df18d97"))))
+         (base32 "1ng33ncai537xlxfy9r4v24parm9mf7zwiwkixq7d0qmj6kka9xx"))))
     (build-system cargo-build-system)
     (arguments
-      `(#:skip-build? #t
-        #:cargo-inputs
-        (("rust-generic-array" ,rust-generic-array-0.14))))
+     `(#:cargo-inputs (("rust-generic-array" ,rust-generic-array-0.14))))
     (home-page "https://docs.rs/dbl")
     (synopsis "Double operation in Galois Field")
     (description
-      "This package provides double and inverse double over Galois Field -
+     "This package provides double and inverse double over Galois Field -
 GF(2^n).  This trait is implemented for 64, 128 and 256 bit block
 sizes.  Big-endian order is used.  WARNING: Block must be aligned!")
     (license (list license:expat license:asl2.0))))
