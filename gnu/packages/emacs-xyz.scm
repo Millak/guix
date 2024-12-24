1b1697f09a7a9700273045e956f72e18cfc8df25
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2024-12-24 10:59:46 +0100

    gnu: emacs-meow: Update to 1.5.0.
    * gnu/packages/emacs-xyz.scm (emacs-meow): Update to 1.5.0.
    
    Change-Id: I5ee1ce5098c2a7dd6703e3ee04cd6b1271ee9503

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -20819,7 +20819,7 @@ code derived from Kelvin H's Org page.")
 (define-public emacs-meow
   (package
     (name "emacs-meow")
-    (version "1.4.5")
+    (version "1.5.0")
     (source
      (origin
        (method git-fetch)
@@ -20828,7 +20828,7 @@ code derived from Kelvin H's Org page.")
              (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "0xv6wg4lyi5bv68h5hk5hfxdwxa2g3ybxd8z0l420az4rnhr6zhq"))))
+        (base32 "1gfvqzp00vwbhxgp2wdcm4waba0r280dx0qbb7vpzyx93bqiplig"))))
     (build-system emacs-build-system)
     (home-page "https://github.com/meow-edit/meow")
     (synopsis "Yet another modal editing on Emacs")
