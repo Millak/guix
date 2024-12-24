4e39907478487f9232196919532b68576f8403f6
Herman Rimm <herman@rimm.ee>
2024-12-24 22:26:26 +0100

    gnu: rust-termini-1: Order alphabetically.
    * gnu/packages/crates-io.scm (rust-termini-1): Order.
    
    Change-Id: Ib43b5c24fd1974f0dc822318cbe0f06134738a38
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -86696,26 +86696,6 @@ color of the terminal.")
      "This package provides a handle to the terminal of the current process.")
     (license (list license:expat license:asl2.0))))
 
-(define-public rust-termini-1
-  (package
-    (name "rust-termini")
-    (version "1.0.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (crate-uri "termini" version))
-       (file-name (string-append name "-" version ".tar.gz"))
-       (sha256
-        (base32 "0n8dvbwkp2k673xqwivb01iqg5ir91zgpwhwngpcb2yrgpc43m1a"))))
-    (build-system cargo-build-system)
-    (arguments
-     `(#:tests? #f      ; Not all files included.
-       #:cargo-inputs (("rust-home" ,rust-home-0.5))))
-    (home-page "https://github.com/pascalkuthe/termini")
-    (synopsis "Minimal terminfo library")
-    (description "This package provides a minimal terminfo library.")
-    (license license:expat)))
-
 (define-public rust-terminfo-0.8
   (package
     (name "rust-terminfo")
@@ -86762,6 +86742,26 @@ color of the terminal.")
                        ("rust-nom" ,rust-nom-5)
                        ("rust-phf" ,rust-phf-0.8)
                        ("rust-phf-codegen" ,rust-phf-codegen-0.8))))))
+
+(define-public rust-termini-1
+  (package
+    (name "rust-termini")
+    (version "1.0.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "termini" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0n8dvbwkp2k673xqwivb01iqg5ir91zgpwhwngpcb2yrgpc43m1a"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:tests? #f      ; Not all files included.
+       #:cargo-inputs (("rust-home" ,rust-home-0.5))))
+    (home-page "https://github.com/pascalkuthe/termini")
+    (synopsis "Minimal terminfo library")
+    (description "This package provides a minimal terminfo library.")
+    (license license:expat)))
 
 (define-public rust-termion-3
   (package
