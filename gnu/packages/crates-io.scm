322795fbbdaa076ed134abca34dbb7faa0318b13
Herman Rimm <herman@rimm.ee>
2024-12-24 16:32:23 +0100

    gnu: rust-cookie-store-0.19: Order alphabetically.
    * gnu/packages/crates-io.scm (rust-cookie-store-0.19): Order.
    
    Change-Id: I2dc5ebfa1db216555f2443a10fd1237a790fd037
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -17399,6 +17399,31 @@ similar to the nom parser combinators library.")
                        ("rust-time" ,rust-time-0.3)
                        ("rust-url" ,rust-url-2))))))
 
+(define-public rust-cookie-store-0.19
+  (package
+    (inherit rust-cookie-store-0.20)
+    (name "rust-cookie-store")
+    (version "0.19.1")
+    (source (origin
+              (method url-fetch)
+              (uri (crate-uri "cookie_store" version))
+              (file-name (string-append name "-" version ".tar.gz"))
+              (sha256
+               (base32
+                "19vy19rvp3a68sgxkrv6bxxlrr2ggqp0176yqb3zhmi0g4sqz8fm"))))
+    (arguments
+     `(#:cargo-inputs
+       (("rust-cookie" ,rust-cookie-0.16)
+        ("rust-idna" ,rust-idna-0.3)
+        ("rust-indexmap" ,rust-indexmap-1)
+        ("rust-log" ,rust-log-0.4)
+        ("rust-publicsuffix" ,rust-publicsuffix-2)
+        ("rust-serde" ,rust-serde-1)
+        ("rust-serde-derive" ,rust-serde-derive-1)
+        ("rust-serde-json" ,rust-serde-json-1)
+        ("rust-time" ,rust-time-0.3)
+        ("rust-url" ,rust-url-2))))))
+
 (define-public rust-coolor-0.8
   (package
     (name "rust-coolor")
@@ -17670,31 +17695,6 @@ in @code{no_std}.  Alloc support is optional.")
         (sha256
          (base32 "1wzzy5iazdk5caadxvjfwrd312rbg7a55a1zpmsdrhk3kfpa77r3"))))
     (arguments `(#:cargo-inputs (("rust-memchr" ,rust-memchr-2))))))
-
-(define-public rust-cookie-store-0.19
-  (package
-    (inherit rust-cookie-store-0.20)
-    (name "rust-cookie-store")
-    (version "0.19.1")
-    (source (origin
-              (method url-fetch)
-              (uri (crate-uri "cookie_store" version))
-              (file-name (string-append name "-" version ".tar.gz"))
-              (sha256
-               (base32
-                "19vy19rvp3a68sgxkrv6bxxlrr2ggqp0176yqb3zhmi0g4sqz8fm"))))
-    (arguments
-     `(#:cargo-inputs
-       (("rust-cookie" ,rust-cookie-0.16)
-        ("rust-idna" ,rust-idna-0.3)
-        ("rust-indexmap" ,rust-indexmap-1)
-        ("rust-log" ,rust-log-0.4)
-        ("rust-publicsuffix" ,rust-publicsuffix-2)
-        ("rust-serde" ,rust-serde-1)
-        ("rust-serde-derive" ,rust-serde-derive-1)
-        ("rust-serde-json" ,rust-serde-json-1)
-        ("rust-time" ,rust-time-0.3)
-        ("rust-url" ,rust-url-2))))))
 
 (define-public rust-cookie-store-0.16
   (package
