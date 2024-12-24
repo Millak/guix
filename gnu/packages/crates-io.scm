fa025a19b4f9d35b84253cfde158af2a0561acaa
Herman Rimm <herman@rimm.ee>
2024-12-24 21:11:09 +0100

    gnu: rust-pretty-hex-0.4: Order alphabetically.
    * gnu/packages/crates-io.scm (rust-pretty-hex-0.4): Order.
    
    Change-Id: I78a9d145cdc0039ed9d3c84f27b24a2eec537ca7
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -61187,6 +61187,25 @@ replacements, adding colorful diffs.")
         ("rust-chrono" ,rust-chrono-0.4)
         ("rust-env-logger" ,rust-env-logger-0.6))))))
 
+(define-public rust-pretty-hex-0.4
+  (package
+    (name "rust-pretty-hex")
+    (version "0.4.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "pretty-hex" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0m0j8pqmh6gq1mq7yzp12z0ix159fw0di5lhiwv2y1j0m3j3xj5v"))))
+    (build-system cargo-build-system)
+    (arguments
+     (list #:cargo-development-inputs `(("rust-heapless" ,rust-heapless-0.8))))
+    (home-page "https://github.com/wolandr/pretty-hex")
+    (synopsis "Prettified output of hexadecimal byte slices")
+    (description "Pretty hex dump of byte slices in the common style.")
+    (license license:expat)))
+
 (define-public rust-prettyplease-0.2
   (package
     (name "rust-prettyplease")
@@ -61229,25 +61248,6 @@ replacements, adding colorful diffs.")
         ("rust-syn" ,rust-syn-1))
        #:cargo-development-inputs
        (("rust-syn" ,rust-syn-1))))))
-
-(define-public rust-pretty-hex-0.4
-  (package
-    (name "rust-pretty-hex")
-    (version "0.4.1")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (crate-uri "pretty-hex" version))
-       (file-name (string-append name "-" version ".tar.gz"))
-       (sha256
-        (base32 "0m0j8pqmh6gq1mq7yzp12z0ix159fw0di5lhiwv2y1j0m3j3xj5v"))))
-    (build-system cargo-build-system)
-    (arguments
-     (list #:cargo-development-inputs `(("rust-heapless" ,rust-heapless-0.8))))
-    (home-page "https://github.com/wolandr/pretty-hex")
-    (synopsis "Prettified output of hexadecimal byte slices")
-    (description "Pretty hex dump of byte slices in the common style.")
-    (license license:expat)))
 
 (define-public rust-pretty-hex-0.3
   (package
