a11f6ce29add6aec358d1b133ff7b15dbd5bc6a1
Herman Rimm <herman@rimm.ee>
2024-12-24 21:10:21 +0100

    gnu: rust-libssh2-sys-0.3: Order alphabetically.
    * gnu/packages/crates-io.scm (rust-libssh2-sys-0.3): Order.
    
    Change-Id: I03fa8f3d541bec19341d7e5568bf34bb876e8748
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -42691,6 +42691,39 @@ This package contains the protobuf logic.")
         ("rust-pkg-config" ,rust-pkg-config-0.3)
         ("rust-vcpkg" ,rust-vcpkg-0.2))))))
 
+(define-public rust-libssh2-sys-0.3
+  (package
+    (name "rust-libssh2-sys")
+    (version "0.3.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "libssh2-sys" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1vkidqw5ll71ynqc93hgcq62iqkklzb5268zffd13ql7nwqa1j1d"))
+       (modules '((guix build utils)))
+       (snippet
+        '(begin (delete-file-recursively "libssh2")))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs
+       (("rust-cc" ,rust-cc-1)
+        ("rust-libc" ,rust-libc-0.2)
+        ("rust-libz-sys" ,rust-libz-sys-1)
+        ("rust-openssl-sys" ,rust-openssl-sys-0.9)
+        ("rust-pkg-config" ,rust-pkg-config-0.3)
+        ("rust-vcpkg" ,rust-vcpkg-0.2))))
+    (native-inputs
+     (list pkg-config))
+    (inputs
+     (list libssh2 openssl zlib))
+    (home-page "https://github.com/alexcrichton/ssh2-rs")
+    (synopsis "Native bindings to the libssh2 library")
+    (description
+     "This package provides native rust bindings to the @code{libssh2} library.")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-libxml-0.3
   (package
     (name "rust-libxml")
@@ -43205,39 +43238,6 @@ Linux userspace APIs.")
            #:cargo-development-inputs
            `(("rust-libc" ,rust-libc-0.2)
              ("rust-static-assertions" ,rust-static-assertions-1))))))
-
-(define-public rust-libssh2-sys-0.3
-  (package
-    (name "rust-libssh2-sys")
-    (version "0.3.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (crate-uri "libssh2-sys" version))
-       (file-name (string-append name "-" version ".tar.gz"))
-       (sha256
-        (base32 "1vkidqw5ll71ynqc93hgcq62iqkklzb5268zffd13ql7nwqa1j1d"))
-       (modules '((guix build utils)))
-       (snippet
-        '(begin (delete-file-recursively "libssh2")))))
-    (build-system cargo-build-system)
-    (arguments
-     `(#:cargo-inputs
-       (("rust-cc" ,rust-cc-1)
-        ("rust-libc" ,rust-libc-0.2)
-        ("rust-libz-sys" ,rust-libz-sys-1)
-        ("rust-openssl-sys" ,rust-openssl-sys-0.9)
-        ("rust-pkg-config" ,rust-pkg-config-0.3)
-        ("rust-vcpkg" ,rust-vcpkg-0.2))))
-    (native-inputs
-     (list pkg-config))
-    (inputs
-     (list libssh2 openssl zlib))
-    (home-page "https://github.com/alexcrichton/ssh2-rs")
-    (synopsis "Native bindings to the libssh2 library")
-    (description
-     "This package provides native rust bindings to the @code{libssh2} library.")
-    (license (list license:expat license:asl2.0))))
 
 (define-public rust-libssh2-sys-0.2
   (package
