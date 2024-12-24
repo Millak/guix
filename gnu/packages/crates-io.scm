da236300df07b840c71ba0e6d21d4e4eda224eac
Herman Rimm <herman@rimm.ee>
2024-12-24 16:32:15 +0100

    gnu: rust-const-format-proc-macros-0.2: Order alphabetically.
    * gnu/packages/crates-io.scm (rust-const-format-proc-macros-0.2): Order.
    
    Change-Id: Ic781930a5275b801fe0d761d008d44deaccaa615
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -15814,29 +15814,6 @@ literals.")
     (description "This package provides compile-time string formatting.")
     (license license:zlib)))
 
-(define-public rust-const-format-proc-macros-0.2
-  (package
-    (name "rust-const-format-proc-macros")
-    (version "0.2.32")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (crate-uri "const_format_proc_macros" version))
-       (file-name (string-append name "-" version ".tar.gz"))
-       (sha256
-        (base32 "0015dzbjbd773nn6096dwqv11fm8m3gy4a4a56cz5x10zl4gzxn7"))))
-    (build-system cargo-build-system)
-    (arguments
-     `(#:cargo-inputs (("rust-proc-macro2" ,rust-proc-macro2-1)
-                       ("rust-quote" ,rust-quote-1)
-                       ("rust-syn" ,rust-syn-1)
-                       ("rust-unicode-xid" ,rust-unicode-xid-0.2))
-       #:cargo-development-inputs (("rust-fastrand" ,rust-fastrand-1))))
-    (home-page "https://github.com/rodrimati1992/const_format_crates/")
-    (synopsis "Implementation detail of the @code{const_format} crate")
-    (description "Implementation detail of the @code{const_format} crate.")
-    (license license:zlib)))
-
 ;; This package requires features which are unavailable
 ;; on the stable releases of Rust.
 (define-public rust-compiler-builtins-0.1
@@ -16773,6 +16750,29 @@ the browser's console.")
     (description "This package provides an attribute for easy generation of
 const functions with conditional compilations.")
     (license (list license:asl2.0 license:expat))))
+
+(define-public rust-const-format-proc-macros-0.2
+  (package
+    (name "rust-const-format-proc-macros")
+    (version "0.2.32")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "const_format_proc_macros" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0015dzbjbd773nn6096dwqv11fm8m3gy4a4a56cz5x10zl4gzxn7"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs (("rust-proc-macro2" ,rust-proc-macro2-1)
+                       ("rust-quote" ,rust-quote-1)
+                       ("rust-syn" ,rust-syn-1)
+                       ("rust-unicode-xid" ,rust-unicode-xid-0.2))
+       #:cargo-development-inputs (("rust-fastrand" ,rust-fastrand-1))))
+    (home-page "https://github.com/rodrimati1992/const_format_crates/")
+    (synopsis "Implementation detail of the @code{const_format} crate")
+    (description "Implementation detail of the @code{const_format} crate.")
+    (license license:zlib)))
 
 (define-public rust-const-oid-0.9
   (package
