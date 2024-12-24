d90e5dcf58b15335f9797386a432a993458fd908
Sharlatan Hellseher <sharlatanus@gmail.com>
2024-12-24 13:02:52 +0000

    gnu: packages/education: Remove golang module.
    * gnu/packages/time.scm: As all Golang dependencies were shifted to
    corresponded submodules, remove not required "golang" module from used
    ones.
    
    Change-Id: I9a647b3d6ff6aea4b77eff1d66fa18aa905dda10

--- gnu/packages/education.scm
+++ gnu/packages/education.scm
@@ -47,7 +47,6 @@
   #:use-module (gnu packages gettext)
   #:use-module (gnu packages glib)
   #:use-module (gnu packages gnome)
-  #:use-module (gnu packages golang)
   #:use-module (gnu packages golang-build)
   #:use-module (gnu packages golang-check)
   #:use-module (gnu packages golang-web)
