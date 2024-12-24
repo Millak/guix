88c6125fdf9444d715aab75ce529adae518d9f29
Herman Rimm <herman@rimm.ee>
2024-12-24 11:31:03 +0100

    gnu: rust-atk-0.8: Order alphabetically.
    * gnu/packages/crates-gtk.scm (rust-atk-0.8): Order.
    
    Change-Id: I70e6fff44123469c9a4e93bc44a4eb3ba0c0a591
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/crates-gtk.scm
+++ gnu/packages/crates-gtk.scm
@@ -101,6 +101,30 @@
     (description "Rust bindings for the ATK library")
     (license license:expat)))
 
+(define-public rust-atk-0.8
+  (package
+    (inherit rust-atk-0.14)
+    (name "rust-atk")
+    (version "0.8.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "atk" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0gk6ijqsd6kh8cki1wznw570499psbppg3d5bqaayagjapxawka4"))))
+    (arguments
+     `(#:cargo-inputs
+       (("rust-atk-sys" ,rust-atk-sys-0.9)
+        ("rust-bitflags" ,rust-bitflags-1)
+        ("rust-glib" ,rust-glib-0.9)
+        ("rust-glib-sys" ,rust-glib-sys-0.9)
+        ("rust-gobject-sys" ,rust-gobject-sys-0.9)
+        ("rust-gtk-rs-lgpl-docs" ,rust-gtk-rs-lgpl-docs-0.1)
+        ("rust-libc" ,rust-libc-0.2))
+       #:cargo-development-inputs
+       (("rust-gir-format-check" ,rust-gir-format-check-0.1))))))
+
 (define-public rust-atk-sys-0.18
   (package
     (name "rust-atk-sys")
@@ -202,30 +226,6 @@
        #:cargo-development-inputs
        (("rust-shell-words" ,rust-shell-words-0.1)
         ("rust-tempfile" ,rust-tempfile-3))))))
-
-(define-public rust-atk-0.8
-  (package
-    (inherit rust-atk-0.14)
-    (name "rust-atk")
-    (version "0.8.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (crate-uri "atk" version))
-       (file-name (string-append name "-" version ".tar.gz"))
-       (sha256
-        (base32 "0gk6ijqsd6kh8cki1wznw570499psbppg3d5bqaayagjapxawka4"))))
-    (arguments
-     `(#:cargo-inputs
-       (("rust-atk-sys" ,rust-atk-sys-0.9)
-        ("rust-bitflags" ,rust-bitflags-1)
-        ("rust-glib" ,rust-glib-0.9)
-        ("rust-glib-sys" ,rust-glib-sys-0.9)
-        ("rust-gobject-sys" ,rust-gobject-sys-0.9)
-        ("rust-gtk-rs-lgpl-docs" ,rust-gtk-rs-lgpl-docs-0.1)
-        ("rust-libc" ,rust-libc-0.2))
-       #:cargo-development-inputs
-       (("rust-gir-format-check" ,rust-gir-format-check-0.1))))))
 
 (define-public rust-cairo-rs-0.20
   (package
