3c1c6bdcbb496b0ae29a06b0a0e82e81e06319cc
Herman Rimm <herman@rimm.ee>
2024-12-24 22:25:46 +0100

    gnu: rust-pyo3-macros-0.21: Order alphabetically.
    * gnu/packages/crates-io.scm (rust-pyo3-macros-0.21): Order.
    
    Change-Id: Ib6e1578fc2a1424ce9619a8b72294b03cac40866
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -64473,6 +64473,25 @@ extension to python.")
                        ("rust-quote" ,rust-quote-1)
                        ("rust-syn" ,rust-syn-2))))))
 
+(define-public rust-pyo3-macros-0.21
+  (package
+    (inherit rust-pyo3-macros-0.22)
+    (name "rust-pyo3-macros")
+    (version "0.21.2")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "pyo3-macros" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0g65z9yj2iffjrkrkzan9hwhhj7rrchh7lfv64dy30h6zill1cvp"))))
+    (arguments
+     `(#:cargo-inputs
+       (("rust-proc-macro2" ,rust-proc-macro2-1)
+        ("rust-pyo3-macros-backend" ,rust-pyo3-macros-backend-0.21)
+        ("rust-quote" ,rust-quote-1)
+        ("rust-syn" ,rust-syn-2))))))
+
 (define-public rust-pyo3-macros-backend-0.23
   (package
     (name "rust-pyo3-macros-backend")
@@ -64629,25 +64648,6 @@ extension to python.")
         ("rust-quote" ,rust-quote-1)
         ("rust-syn" ,rust-syn-1))))
     (native-inputs (list python))))
-
-(define-public rust-pyo3-macros-0.21
-  (package
-    (inherit rust-pyo3-macros-0.22)
-    (name "rust-pyo3-macros")
-    (version "0.21.2")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (crate-uri "pyo3-macros" version))
-       (file-name (string-append name "-" version ".tar.gz"))
-       (sha256
-        (base32 "0g65z9yj2iffjrkrkzan9hwhhj7rrchh7lfv64dy30h6zill1cvp"))))
-    (arguments
-     `(#:cargo-inputs
-       (("rust-proc-macro2" ,rust-proc-macro2-1)
-        ("rust-pyo3-macros-backend" ,rust-pyo3-macros-backend-0.21)
-        ("rust-quote" ,rust-quote-1)
-        ("rust-syn" ,rust-syn-2))))))
 
 (define-public rust-pyo3-macros-0.20
   (package
