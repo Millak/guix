d01a8b804ebc1d39efe5a4ae82ae3c4f328d3069
Herman Rimm <herman@rimm.ee>
2024-12-24 21:11:22 +0100

    gnu: rust-pyo3-0.16: Order alphabetically.
    * gnu/packages/crates-io.scm (rust-pyo3-0.16): Order.
    
    Change-Id: I41dab63c475d004405429c231bfdc05a3e1d5756
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -63997,6 +63997,49 @@ Python code from a Rust binary is also supported.")
         ("rust-trybuild" ,rust-trybuild-1)
         ("rust-widestring" ,rust-widestring-0.5))))))
 
+(define-public rust-pyo3-0.16
+  (package
+    (inherit rust-pyo3-0.19)
+    (name "rust-pyo3")
+    (version "0.16.6")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "pyo3" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1jn6nhp9a8i5kfwds6fzy3hzb938qim8bajp8gfkkcn9892c8802"))))
+    (arguments
+     `(#:cargo-inputs
+       (("rust-anyhow" ,rust-anyhow-1)
+        ("rust-cfg-if" ,rust-cfg-if-1)
+        ("rust-eyre" ,rust-eyre-0.6)
+        ("rust-hashbrown" ,rust-hashbrown-0.11)
+        ("rust-indexmap" ,rust-indexmap-1.7)
+        ("rust-indoc" ,rust-indoc-1)
+        ("rust-inventory" ,rust-inventory-0.2)
+        ("rust-libc" ,rust-libc-0.2)
+        ("rust-num-bigint" ,rust-num-bigint-0.4)
+        ("rust-num-complex" ,rust-num-complex-0.4)
+        ("rust-parking-lot" ,rust-parking-lot-0.11)
+        ("rust-pyo3-build-config" ,rust-pyo3-build-config-0.16)
+        ("rust-pyo3-ffi" ,rust-pyo3-ffi-0.16)
+        ("rust-pyo3-macros" ,rust-pyo3-macros-0.16)
+        ("rust-serde" ,rust-serde-1)
+        ("rust-unindent" ,rust-unindent-0.1))
+       #:cargo-development-inputs
+       (("rust-assert-approx-eq" ,rust-assert-approx-eq-1)
+        ("rust-criterion" ,rust-criterion-0.3)
+        ("rust-proptest" ,rust-proptest-0.10)
+        ("rust-rustversion" ,rust-rustversion-1)
+        ("rust-send-wrapper" ,rust-send-wrapper-0.5)
+        ("rust-serde" ,rust-serde-1)
+        ("rust-serde-json" ,rust-serde-json-1)
+        ("rust-trybuild" ,rust-trybuild-1)
+        ("rust-widestring" ,rust-widestring-0.5))
+       ;; SIGSEGV: invalid memory reference
+       #:tests? #f))))
+
 (define-public rust-pyo3-build-config-0.23
   (package
     (name "rust-pyo3-build-config")
@@ -64634,49 +64677,6 @@ extension to python.")
         ("rust-quote" ,rust-quote-1)
         ("rust-syn" ,rust-syn-1))))
     (native-inputs (list python))))
-
-(define-public rust-pyo3-0.16
-  (package
-    (inherit rust-pyo3-0.19)
-    (name "rust-pyo3")
-    (version "0.16.6")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (crate-uri "pyo3" version))
-       (file-name (string-append name "-" version ".tar.gz"))
-       (sha256
-        (base32 "1jn6nhp9a8i5kfwds6fzy3hzb938qim8bajp8gfkkcn9892c8802"))))
-    (arguments
-     `(#:cargo-inputs
-       (("rust-anyhow" ,rust-anyhow-1)
-        ("rust-cfg-if" ,rust-cfg-if-1)
-        ("rust-eyre" ,rust-eyre-0.6)
-        ("rust-hashbrown" ,rust-hashbrown-0.11)
-        ("rust-indexmap" ,rust-indexmap-1.7)
-        ("rust-indoc" ,rust-indoc-1)
-        ("rust-inventory" ,rust-inventory-0.2)
-        ("rust-libc" ,rust-libc-0.2)
-        ("rust-num-bigint" ,rust-num-bigint-0.4)
-        ("rust-num-complex" ,rust-num-complex-0.4)
-        ("rust-parking-lot" ,rust-parking-lot-0.11)
-        ("rust-pyo3-build-config" ,rust-pyo3-build-config-0.16)
-        ("rust-pyo3-ffi" ,rust-pyo3-ffi-0.16)
-        ("rust-pyo3-macros" ,rust-pyo3-macros-0.16)
-        ("rust-serde" ,rust-serde-1)
-        ("rust-unindent" ,rust-unindent-0.1))
-       #:cargo-development-inputs
-       (("rust-assert-approx-eq" ,rust-assert-approx-eq-1)
-        ("rust-criterion" ,rust-criterion-0.3)
-        ("rust-proptest" ,rust-proptest-0.10)
-        ("rust-rustversion" ,rust-rustversion-1)
-        ("rust-send-wrapper" ,rust-send-wrapper-0.5)
-        ("rust-serde" ,rust-serde-1)
-        ("rust-serde-json" ,rust-serde-json-1)
-        ("rust-trybuild" ,rust-trybuild-1)
-        ("rust-widestring" ,rust-widestring-0.5))
-       ;; SIGSEGV: invalid memory reference
-       #:tests? #f))))
 
 (define-public rust-pyo3-macros-0.13
   (package
