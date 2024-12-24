8b5363cd91f93d86974c5616fb76bd847cdbbf79
Herman Rimm <herman@rimm.ee>
2024-12-24 21:10:55 +0100

    gnu: rust-ouroboros-0.15: Order alphabetically.
    * gnu/packages/crates-io.scm (rust-ouroboros-0.15): Order.
    
    Change-Id: Icba642afc9ab0dd75bf06953913d9978650c8a0b
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -54979,6 +54979,23 @@ platform-native strings.")
     (description "Easy, safe self-referential struct generation for Rust.")
     (license (list license:expat license:asl2.0))))
 
+(define-public rust-ouroboros-0.15
+  (package
+    (inherit rust-ouroboros-0.17)
+    (name "rust-ouroboros")
+    (version "0.15.6")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "ouroboros" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1nvjra9dana2g6kxv3397qrgpyw6lknzya6lzs1s1llbap8qndg1"))))
+    (arguments
+     `(#:cargo-inputs
+       (("rust-aliasable" ,rust-aliasable-0.1)
+        ("rust-ouroboros-macro" ,rust-ouroboros-macro-0.15))))))
+
 (define-public rust-ouroboros-macro-0.17
   (package
     (name "rust-ouroboros-macro")
@@ -55035,23 +55052,6 @@ platform-native strings.")
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
         (base32 "11cav04x82liknlrv50lpl1i1ln2jw4isdqzdjnjsg0pcpvwik9r"))))))
-
-(define-public rust-ouroboros-0.15
-  (package
-    (inherit rust-ouroboros-0.17)
-    (name "rust-ouroboros")
-    (version "0.15.6")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (crate-uri "ouroboros" version))
-       (file-name (string-append name "-" version ".tar.gz"))
-       (sha256
-        (base32 "1nvjra9dana2g6kxv3397qrgpyw6lknzya6lzs1s1llbap8qndg1"))))
-    (arguments
-     `(#:cargo-inputs
-       (("rust-aliasable" ,rust-aliasable-0.1)
-        ("rust-ouroboros-macro" ,rust-ouroboros-macro-0.15))))))
 
 (define-public rust-ouroboros-0.14
   (package
