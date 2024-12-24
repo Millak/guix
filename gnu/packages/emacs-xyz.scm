6c8e5fe634b4f97f589a5c469012a07aae03bf6e
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2024-12-24 10:59:53 +0100

    gnu: emacs-parsebib: Update to 6.3.
    * gnu/packages/emacs-xyz.scm (emacs-parsebib): Update to 6.3.
    
    Change-Id: I48f80ea23a171603f44f08e376b032432d5eea04

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -25247,7 +25247,7 @@ files to be expanded upon opening them.")
 (define-public emacs-parsebib
   (package
     (name "emacs-parsebib")
-    (version "4.3")
+    (version "6.3")
     (source
      (origin
        (method git-fetch)
@@ -25256,7 +25256,7 @@ files to be expanded upon opening them.")
              (commit version)))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "0vcl2wvxwpr62c9ym0fm3qaxzhjcrpk4r6r0zaqhkvlf8qr3rg8y"))))
+        (base32 "1rgawqwa7dqfiz74fsj1z6nsmjp7dszrxm9jjc9kgnr8w4s3yb2a"))))
     (build-system emacs-build-system)
     (home-page "https://github.com/joostkremers/parsebib")
     (synopsis "Library for parsing @file{.bib} files")
