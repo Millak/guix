d4dc0fde92f01386c72bc8592d4611d06a05957f
Efraim Flashner <efraim@flashner.co.il>
2024-12-26 21:20:05 +0200

    gnu: rust-basic-cookies-0.1: Move to (gnu packages crates-web).
    * gnu/packages/crates-io.scm (rust-basic-cookies-0.1): Move from here ...
    * gnu/packages/crates-web.scm: ... to here.
    
    Change-Id: Ia863a18a79fc55d5b7887e0e20ecf2a5f374e6d1

--- gnu/packages/crates-web.scm
+++ gnu/packages/crates-web.scm
@@ -2547,6 +2547,29 @@ built on the Actix ecosystem.")
     (description "Macros for @code{axum}.")
     (license license:expat)))
 
+(define-public rust-basic-cookies-0.1
+  (package
+    (name "rust-basic-cookies")
+    (version "0.1.5")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "basic-cookies" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1xwnmmcn32m18nis7azfxylkqyhirkqcag94i23b1g8n5ka8zgb7"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs (("rust-lalrpop" ,rust-lalrpop-0.20)
+                       ("rust-lalrpop-util" ,rust-lalrpop-util-0.20)
+                       ("rust-regex" ,rust-regex-1))))
+    (home-page "https://github.com/drjokepu/basic-cookies")
+    (synopsis "Low-level RFC 6265 compatible cookie handling library for Rust")
+    (description
+     "This package provides a low-level RFC 6265 compatible cookie handling
+library for Rust.")
+    (license license:expat)))
+
 (define-public rust-chardetng-0.1
   (package
     (name "rust-chardetng")
