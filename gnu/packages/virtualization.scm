e9f2c713e3a1c14eb9366068cf9660968b5bc743
Sharlatan Hellseher <sharlatanus@gmail.com>
2024-12-28 20:18:45 +0000

    gnu: umoci: Fix build.
    * gnu/packages/virtualization.scm (umoci) [arguments] <test-subdirs>:
    Limit to project's root.
    
    Change-Id: Ib002c142fefa7b8fa60c7d9c650615753bf03eba

--- gnu/packages/virtualization.scm
+++ gnu/packages/virtualization.scm
@@ -2290,6 +2290,7 @@ Open Container Initiative specification.")
   (package
     (name "umoci")
     (version "0.4.7")
+    ;; XXX: Source contain vendor, consider to pack all missing dependencies.
     (source
      (origin
        (method url-fetch)
@@ -2303,6 +2304,7 @@ Open Container Initiative specification.")
     (arguments
      '(#:import-path "github.com/opencontainers/umoci"
        #:install-source? #f
+       #:test-subdirs '(".")
        #:phases
        (modify-phases %standard-phases
          (replace 'unpack
