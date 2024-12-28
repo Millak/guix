fb21b316105ebba350eff8311c7b4ff682a7b63b
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2024-12-28 12:59:57 +0800

    gnu: compdb: Fix build.
    * gnu/packages/build-tools.scm (compdb): Fix build.
    [native-inputs]: Add python-setuptools and python-wheel.
    
    Change-Id: Ia8c94131dc0141c721de756f85b736ae5c1d123f

--- gnu/packages/build-tools.scm
+++ gnu/packages/build-tools.scm
@@ -657,6 +657,7 @@ be reached via direct API calls.")
        (sha256
         (base32 "0f4x0gm5n1mr87dx3gzn5da16a1qhd2y3kz22dl5xsd9pd720l4w"))))
     (build-system pyproject-build-system)
+    (native-inputs (list python-setuptools python-wheel))
     (propagated-inputs (list python-configparser))
     (home-page "https://github.com/Sarcasm/compdb")
     (synopsis "Compilation database Swiss army knife")
