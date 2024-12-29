4aa828cabed99dcbe6e614579c4c486491b31aa5
Roman Scherer <roman@burningswell.com>
2024-12-29 13:14:33 +0100

    gnu: Add go-github-com-yudai-golcs.
    * gnu/packages/golang-xyz.scm (go-github-com-yudai-golcs): New variable.
    
    Change-Id: I3ce09b85717bfccf1d4faf8ddc28542508be08ba
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/golang-xyz.scm
+++ gnu/packages/golang-xyz.scm
@@ -12832,6 +12832,30 @@ detection.")
 @url{https://github.com/yosuke-furukawa/json5, JSON5}.")
       (license license:bsd-3))))
 
+(define-public go-github-com-yudai-golcs
+  (package
+    (name "go-github-com-yudai-golcs")
+    (version "0.0.0-20170316035057-ecda9a501e82")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/yudai/golcs")
+             (commit (go-version->git-ref version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0mx6wc5fz05yhvg03vvps93bc5mw4vnng98fhmixd47385qb29pq"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:import-path "github.com/yudai/golcs"))
+    (home-page "https://github.com/yudai/golcs")
+    (synopsis "Calculate @acronym{LCS, longest common sequence} in Golang")
+    (description
+     "This package provides functions to calculate @acronym{LCS, longest
+common sequence} values from two arbitrary arrays.")
+    (license license:expat)))
+
 (define-public go-github-com-yuin-gopher-lua
   (package
     (name "go-github-com-yuin-gopher-lua")
