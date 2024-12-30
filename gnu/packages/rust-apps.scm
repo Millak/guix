6889794eee27118ed1200be4990378bb0502d3c5
Efraim Flashner <efraim@flashner.co.il>
2024-12-30 18:31:42 +0200

    gnu: speakersafetyd: Update to 1.0.2.
    * gnu/packages/rust-apps.scm (speakersafetyd): Update to 1.0.2.
    [cargo-inputs]: Replace rust-alsa-0.8 with 0.9, rust-simple-logger-1
    with 4.
    
    Change-Id: I5b2c11c31f24943165d983198ed8970c85a074f3

--- gnu/packages/rust-apps.scm
+++ gnu/packages/rust-apps.scm
@@ -2916,19 +2916,19 @@ of the project is to be runnable on untrusted networks without crashing.")
 (define-public speakersafetyd
   (package
     (name "speakersafetyd")
-    (version "1.0.1")
+    (version "1.0.2")
     (source
      (origin
        (method url-fetch)
        (uri (crate-uri "speakersafetyd" version))
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
-        (base32 "1dvyj194niz5i4rldsqvjmz8j7df9w9qpvf9rwg3vsnzc2mjh0zg"))))
+        (base32 "104xgyqhsg2rxa3ndkizrpndibmcbr25h63phcjswadbm8i790bz"))))
     (build-system cargo-build-system)
     (arguments
      (list
       #:install-source? #f
-      #:cargo-inputs `(("rust-alsa" ,rust-alsa-0.8)
+      #:cargo-inputs `(("rust-alsa" ,rust-alsa-0.9)
                        ("rust-chrono" ,rust-chrono-0.4)
                        ("rust-clap" ,rust-clap-4)
                        ("rust-clap-verbosity-flag" ,rust-clap-verbosity-flag-2)
@@ -2937,7 +2937,7 @@ of the project is to be runnable on untrusted networks without crashing.")
                        ("rust-libc" ,rust-libc-0.2)
                        ("rust-log" ,rust-log-0.4)
                        ("rust-signal-hook" ,rust-signal-hook-0.3)
-                       ("rust-simple-logger" ,rust-simple-logger-1))
+                       ("rust-simple-logger" ,rust-simple-logger-4))
       #:phases
       #~(modify-phases %standard-phases
           (add-after 'unpack 'fix-paths
