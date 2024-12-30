2804ca7cb7012dcb6c4226c5445b9a23c84f356b
Artyom V. Poptsov <poptsov.artyom@gmail.com>
2024-12-31 00:00:55 +0300

    gnu: qpdf: Update to 11.9.1.
    * gnu/packages/pdf.scm (qpdf): Update to 11.9.1.
    
    Change-Id: Iecb4c7d127ffcb943a7704947410d78a6746bcf0

--- gnu/packages/pdf.scm
+++ gnu/packages/pdf.scm
@@ -933,14 +933,14 @@ line tools for batch rendering @command{pdfdraw}, rewriting files
 (define-public qpdf
   (package
     (name "qpdf")
-    (version "11.8.0")
+    (version "11.9.1")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://sourceforge/qpdf/qpdf/" version
                                   "/qpdf-" version ".tar.gz"))
               (sha256
                (base32
-                "0n8jfk4yf0m36rs9lg82pj9lv6pdqpfh8mhacc1ih9ahpigiycnr"))))
+                "1l8k1ls5kclizrglay3r64i9dgckvksjwxxr8v0jfyjnz54d591b"))))
     (build-system cmake-build-system)
     (arguments
      (list #:configure-flags #~'("-DBUILD_STATIC_LIBS=OFF")))
