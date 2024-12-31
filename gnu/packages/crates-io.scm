8464352c9d21e6bc73bcdf4c600cd788544939a2
Efraim Flashner <efraim@flashner.co.il>
2024-12-31 19:05:00 +0200

    gnu: Remove rust-polars-io-0.17.
    * gnu/packages/crates-io.scm (rust-polars-io-0.17): Delete variable.
    
    Change-Id: I464c83434f7700e5fb1ddbf6ccc1d19205fbfe63

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -55700,41 +55700,6 @@ applications.")
      "This crate provides IO related logic for the Polars @code{DataFrame} library.")
     (license license:expat)))
 
-(define-public rust-polars-io-0.17
-  (package
-    (inherit rust-polars-io-0.37)
-    (name "rust-polars-io")
-    (version "0.17.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (crate-uri "polars-io" version))
-       (file-name (string-append name "-" version ".tar.gz"))
-       (sha256
-        (base32 "0mq921184vwmadxz3996g0ilb4wiws3fajgxpvkkw1152pszb98i"))))
-    (arguments
-     `(#:skip-build? #t
-       #:cargo-inputs
-       (("rust-ahash" ,rust-ahash-0.7)
-        ("rust-anyhow" ,rust-anyhow-1)
-        ("rust-arrow2" ,rust-arrow2-0.7)
-        ("rust-csv-core" ,rust-csv-core-0.1)
-        ("rust-dirs" ,rust-dirs-3)
-        ("rust-flate2" ,rust-flate2-1)
-        ("rust-lazy-static" ,rust-lazy-static-1)
-        ("rust-lexical" ,rust-lexical-6)
-        ("rust-memchr" ,rust-memchr-2)
-        ("rust-memmap2" ,rust-memmap2-0.5)
-        ("rust-num" ,rust-num-0.4)
-        ("rust-num-cpus" ,rust-num-cpus-1)
-        ("rust-polars-arrow" ,rust-polars-arrow-0.17)
-        ("rust-polars-core" ,rust-polars-core-0.17)
-        ("rust-rayon" ,rust-rayon-1)
-        ("rust-regex" ,rust-regex-1)
-        ("rust-simdutf8" ,rust-simdutf8-0.1))))
-    (native-inputs '())
-    (inputs '())))
-
 (define-public rust-polars-json-0.37
   (package
     (name "rust-polars-json")
