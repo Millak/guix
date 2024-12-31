101e24f241efd2c6b4b4530e61ca0febad4e15bd
Efraim Flashner <efraim@flashner.co.il>
2024-12-31 19:05:08 +0200

    gnu: Remove rust-chalk-recursive-0.75.
    * gnu/packages/crates-io.scm (rust-chalk-recursive-0.75): Delete variable.
    
    Change-Id: Ia5e453d7402347cf1ffb904ceecb2c8780e1f91d

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -12243,35 +12243,6 @@ macros.")
 clauses.")
     (license (list license:asl2.0 license:expat))))
 
-(define-public rust-chalk-recursive-0.75
-  (package
-    (name "rust-chalk-recursive")
-    (version "0.75.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (crate-uri "chalk-recursive" version))
-       (file-name
-        (string-append name "-" version ".tar.gz"))
-       (sha256
-        (base32
-         "11bnn0yn9np4mar4nryys6w1ijizj3y9dcz3xbldxzx6fplmk7y3"))))
-    (build-system cargo-build-system)
-    (arguments
-     `(#:skip-build? #t
-       #:cargo-inputs
-       (("rust-chalk-derive" ,rust-chalk-derive-0.75)
-        ("rust-chalk-ir" ,rust-chalk-ir-0.75)
-        ("rust-chalk-solve" ,rust-chalk-solve-0.75)
-        ("rust-rustc-hash" ,rust-rustc-hash-1)
-        ("rust-tracing" ,rust-tracing-0.1))))
-    (home-page "https://github.com/rust-lang/chalk")
-    (synopsis
-     "Recursive solver for the Chalk project")
-    (description
-     "Recursive solver for the Chalk project")
-    (license (list license:asl2.0 license:expat))))
-
 (define-public rust-chalk-solve-0.75
   (package
     (name "rust-chalk-solve")
