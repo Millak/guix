91a274a66f475e7cfe008c564952520abd355fca
Efraim Flashner <efraim@flashner.co.il>
2024-12-31 14:49:18 +0200

    gnu: Remove rust-addr2line-0.19.
    * gnu/packages/crates-io.scm (rust-addr2line-0.19): Delete variable.
    
    Change-Id: Ifaf4c70ce647c34d57b2978e097a8865f06ccc34

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -658,38 +658,6 @@ Rust, using gimli.")
                                    ("rust-libtest-mimic" ,rust-libtest-mimic-0.6)
                                    ("rust-typed-arena" ,rust-typed-arena-2))))))
 
-(define-public rust-addr2line-0.19
-  (package
-    (inherit rust-addr2line-0.21)
-    (name "rust-addr2line")
-    (version "0.19.0")
-    (source (origin
-              (method url-fetch)
-              (uri (crate-uri "addr2line" version))
-              (file-name (string-append name "-" version ".tar.gz"))
-              (sha256
-               (base32
-                "15ywmr5wx22q69ffnn79qp65ir5p1x0k2q06plcpv6v74c5xcvx7"))))
-    (arguments
-     `(#:tests? #f      ; use of undeclared crate or module `auxiliary`
-       #:cargo-inputs
-       (("rust-compiler-builtins" ,rust-compiler-builtins-0.1)
-        ("rust-cpp-demangle" ,rust-cpp-demangle-0.4)
-        ("rust-fallible-iterator" ,rust-fallible-iterator-0.2)
-        ("rust-gimli" ,rust-gimli-0.27)
-        ("rust-object" ,rust-object-0.30)
-        ("rust-rustc-demangle" ,rust-rustc-demangle-0.1)
-        ("rust-rustc-std-workspace-alloc" ,rust-rustc-std-workspace-alloc-1)
-        ("rust-rustc-std-workspace-core" ,rust-rustc-std-workspace-core-1)
-        ("rust-smallvec" ,rust-smallvec-1))
-       #:cargo-development-inputs
-       (("rust-backtrace" ,rust-backtrace-0.3)
-        ("rust-clap" ,rust-clap-3)
-        ("rust-findshlibs" ,rust-findshlibs-0.10)
-        ("rust-memmap2" ,rust-memmap2-0.5)
-        ("rust-rustc-test" ,rust-rustc-test-0.3)
-        ("rust-typed-arena" ,rust-typed-arena-2))))))
-
 (define-public rust-adler-1
   (package
     (name "rust-adler")
