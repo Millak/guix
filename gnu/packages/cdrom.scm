3dd7b6cfab83afd03428432c0327fd7b88d56180
Janneke Nieuwenhuizen <janneke@gnu.org>
2024-12-31 09:52:16 +0100

    gnu: libcddb: Fix build with gcc-14.
    * gnu/packages/cdrom.scm (libcddb)[arguments]: Add #:configure-flags to relax
    gcc-14's strictness.
    
    Change-Id: I463872e31d6f8c42136e6e04cf4013c2261d43cc

--- gnu/packages/cdrom.scm
+++ gnu/packages/cdrom.scm
@@ -17,6 +17,7 @@
 ;;; Copyright © 2023 Zheng Junjie <873216071@qq.com>
 ;;; Copyright © 2024 Julian Flake <flake@uni-koblenz.de>
 ;;; Copyright © 2025 Yovan Naumovski <yovan@gorski.stream>
+;;; Copyright © 2024 Janneke Nieuwenhuizen <janneke@gnu.org>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -90,7 +91,13 @@
               (base32
                "0fr21a7vprdyy1bq6s99m0x420c9jm5fipsd63pqv8qyfkhhxkim"))))
     (build-system gnu-build-system)
-    (arguments '(#:tests? #f))      ; tests rely on access to external servers
+    (arguments
+     (list
+      #:tests? #f                    ;tests rely on access to external servers
+      #:configure-flags #~(list
+                           #$(string-append
+                              "CFLAGS=-g -O2"
+                              " -Wno-error=incompatible-pointer-types"))))
     (home-page "https://libcddb.sourceforge.net/")
     (synopsis "C library to access data on a CDDB server")
     (description
