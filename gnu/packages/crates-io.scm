ae1002e2fbb8b835221dfb88561a8df94757b193
Efraim Flashner <efraim@flashner.co.il>
2024-12-31 14:49:15 +0200

    gnu: Remove rust-calamine-0.18.
    * gnu/packages/crates-io.scm (rust-calamine-0.18): Delete variable.
    
    Change-Id: Ie11f74bd376a63624181e6ad249f5f5df535a4c9

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -11118,31 +11118,6 @@ spreadsheet file.")
                                    ("rust-serde-derive" ,rust-serde-derive-1)
                                    ("rust-sha256" ,rust-sha256-1))))))
 
-(define-public rust-calamine-0.18
-  (package
-    (inherit rust-calamine-0.24)
-    (name "rust-calamine")
-    (version "0.18.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (crate-uri "calamine" version))
-       (file-name (string-append name "-" version ".tar.gz"))
-       (sha256
-        (base32 "1fssrwkwar1z4d2f0xl3cqg2y8qlmlc05g5xy305mkmxlj6sfv5q"))))
-    (arguments
-     `(#:skip-build? #t
-       #:cargo-inputs (("rust-byteorder" ,rust-byteorder-1)
-                       ("rust-chrono" ,rust-chrono-0.4)
-                       ("rust-codepage" ,rust-codepage-0.1)
-                       ("rust-encoding-rs" ,rust-encoding-rs-0.8)
-                       ("rust-log" ,rust-log-0.4)
-                       ("rust-quick-xml" ,rust-quick-xml-0.19)
-                       ("rust-serde" ,rust-serde-1)
-                       ("rust-zip" ,rust-zip-0.5))
-       #:cargo-development-inputs (("rust-env-logger" ,rust-env-logger-0.7)
-                                   ("rust-glob" ,rust-glob-0.3))))))
-
 (define-public rust-calibright-0.1
   (package
     (name "rust-calibright")
