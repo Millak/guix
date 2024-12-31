c78cce6cb8f5f61887dda6d4744b42759af78054
Efraim Flashner <efraim@flashner.co.il>
2024-12-31 19:05:08 +0200

    gnu: Remove rust-clircle-0.3.
    * gnu/packages/crates-io.scm (rust-clircle-0.3): Delete variable.
    
    Change-Id: Ie1fe1b58dc137b800455f819684d7b347ab84337

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -13725,28 +13725,6 @@ a file (from a path) and for all three stdio streams, if they are piped from
 or to a file as well.")
     (license (list license:expat license:asl2.0))))
 
-(define-public rust-clircle-0.3
-  (package
-    (inherit rust-clircle-0.4)
-    (name "rust-clircle")
-    (version "0.3.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (crate-uri "clircle" version))
-       (file-name (string-append name "-" version ".tar.gz"))
-       (sha256
-        (base32 "15p36klnsgjimfswi0mjc7wsh4b662v7gbfinh56ipk3bacbv2z6"))))
-    (arguments
-     `(#:cargo-inputs
-       (("rust-cfg-if" ,rust-cfg-if-1)
-        ("rust-libc" ,rust-libc-0.2)
-        ("rust-serde" ,rust-serde-1)
-        ("rust-winapi" ,rust-winapi-0.3))
-       #:cargo-development-inputs
-       (("rust-nix" ,rust-nix-0.19)
-        ("rust-tempfile" ,rust-tempfile-3))))))
-
 (define-public rust-close-fds-0.3
   (package
     (name "rust-close-fds")
