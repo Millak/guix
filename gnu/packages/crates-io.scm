d280f2ec795e560598a5847739e77964177d0c08
Efraim Flashner <efraim@flashner.co.il>
2024-12-31 19:07:00 +0200

    gnu: Remove rust-askama-escape-0.10.
    * gnu/packages/crates-io.scm (rust-askama-escape-0.10): Delete variable.
    
    Change-Id: Ib2b1e53005ab367167a7edad3ca6c12cfb09efdf

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -4076,26 +4076,6 @@ standard library.")
      "This library provides utilities to handle ASCII characters.")
     (license license:mpl2.0)))
 
-(define-public rust-askama-escape-0.10
-  (package
-    (name "rust-askama-escape")
-    (version "0.10.3")
-    (source
-     (origin
-       (method url-fetch)
-       (uri (crate-uri "askama_escape" version))
-       (file-name (string-append name "-" version ".tar.gz"))
-       (sha256
-        (base32 "0hg3rz0cma5f6385z7qmqw3jbir76jndwd5s7dqfk92v9gil75v1"))))
-    (build-system cargo-build-system)
-    (arguments (list #:skip-build? #t))
-    (home-page "https://github.com/djc/askama")
-    (synopsis "HTML escaping extracted from Askama")
-    (description
-     "This package provides an optimized HTML escaping library,
-extracted from Askama.")
-    (license (list license:expat license:asl2.0))))
-
 (define-public rust-askama-parser-0.2
   (package
     (name "rust-askama-parser")
