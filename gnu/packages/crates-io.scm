89edfe7dceb90011f7408b673b727a92c2de4765
Efraim Flashner <efraim@flashner.co.il>
2024-12-31 15:46:10 +0200

    gnu: Remove rust-subplotlib-0.7.
    * gnu/packages/crates-io.scm (rust-subplotlib-0.7): Delete variable.
    
    Change-Id: Iac8c7214fe6a0b306f4541e840a6608102df3779

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -78164,39 +78164,6 @@ another project's @code{build.rs} module.")
 based test suites.  Relies on @code{subplotlib-derive} for associated macros.")
     (license license:expat-0)))
 
-(define-public rust-subplotlib-0.7
-  (package
-    (inherit rust-subplotlib-0.11)
-    (name "rust-subplotlib")
-    (version "0.7.1")
-    (source (origin
-              (method url-fetch)
-              (uri (crate-uri "subplotlib" version))
-              (file-name (string-append name "-" version ".tar.gz"))
-              (sha256
-               (base32
-                "0cbsrsk9pkyhs3fqzmxgf19x2bhr22pic5wqwvphv3sk4nc1ysiw"))))
-    (arguments
-     `(#:tests? #f      ; Not all files included.
-       #:cargo-inputs
-       (("rust-base64" ,rust-base64-0.21)
-        ("rust-fehler" ,rust-fehler-1)
-        ("rust-filetime" ,rust-filetime-0.2)
-        ("rust-fs2" ,rust-fs2-0.4)
-        ("rust-glob" ,rust-glob-0.3)
-        ("rust-lazy-static" ,rust-lazy-static-1)
-        ("rust-regex" ,rust-regex-1)
-        ("rust-remove-dir-all" ,rust-remove-dir-all-0.8)
-        ("rust-shell-words" ,rust-shell-words-1)
-        ("rust-state" ,rust-state-0.5)
-        ("rust-subplot-build" ,rust-subplot-build-0.7)
-        ("rust-subplotlib-derive" ,rust-subplotlib-derive-0.7)
-        ("rust-tempfile" ,rust-tempfile-3)
-        ("rust-time" ,rust-time-0.3)
-        ("rust-unescape" ,rust-unescape-0.1))
-       #:cargo-development-inputs
-       (("rust-serde-json" ,rust-serde-json-1))))))
-
 (define-public rust-subplotlib-derive-0.11
   (package
     (name "rust-subplotlib-derive")
