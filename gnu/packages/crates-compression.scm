491c38de5412eb830c0ca0a4312aef7e1dd336c4
Efraim Flashner <efraim@flashner.co.il>
2024-12-31 15:50:07 +0200

    gnu: Remove rust-libdeflater-0.11.
    * gnu/packages/crates-compression.scm (rust-libdeflater-0.11): Delete variable.
    
    Change-Id: I11ebc37f4b0c2b106450f80425be7c067f07f341

--- gnu/packages/crates-compression.scm
+++ gnu/packages/crates-compression.scm
@@ -791,24 +791,6 @@ for raw deflate, zlib, and gzip data.")
        (("rust-criterion" ,rust-criterion-0.3)
         ("rust-flate2" ,rust-flate2-1))))))
 
-(define-public rust-libdeflater-0.11
-  (package
-    (inherit rust-libdeflater-1)
-    (name "rust-libdeflater")
-    (version "0.11.0")
-    (source (origin
-              (method url-fetch)
-              (uri (crate-uri "libdeflater" version))
-              (file-name (string-append name "-" version ".tar.gz"))
-              (sha256
-               (base32 "0385hpai7fsnpfvxd3hki43v1cj8w6z1cb2rn8wd6vq4dam8bqnq"))))
-    (arguments
-     `(#:cargo-inputs
-       (("rust-libdeflate-sys" ,rust-libdeflate-sys-0.11))
-       #:cargo-development-inputs
-       (("rust-criterion" ,rust-criterion-0.3)
-        ("rust-flate2" ,rust-flate2-1))))))
-
 (define-public rust-libflate-2
   (package
     (name "rust-libflate")
