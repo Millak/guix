a854592fecf65c0768fbc354fa368f866dc0c4b3
Efraim Flashner <efraim@flashner.co.il>
2024-12-31 19:05:09 +0200

    gnu: Remove rust-askama-shared-0.12.
    * gnu/packages/crates-io.scm (rust-askama-shared-0.12): Delete variable.
    
    Change-Id: Ibda6205fdae65c2216ba8dbf996a19bc13d33e95

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -4227,42 +4227,6 @@ extracted from Askama.")
     (description "Parser for Askama templates.")
     (license (list license:expat license:asl2.0))))
 
-(define-public rust-askama-shared-0.12
-  (package
-    (name "rust-askama-shared")
-    (version "0.12.2")
-    (source (origin
-              (method url-fetch)
-              (uri (crate-uri "askama_shared" version))
-              (file-name (string-append name "-" version ".tar.gz"))
-              (sha256
-               (base32
-                "1l4fycmw65zyvfabf672sj2pc0ilfcj0y6a0csygq1wa26a2nwmz"))))
-    (build-system cargo-build-system)
-    (arguments
-     (list #:skip-build? #t
-           #:cargo-inputs
-           `(("rust-askama-escape" ,rust-askama-escape-0.10)
-             ("rust-comrak" ,rust-comrak-0.12)
-             ("rust-humansize" ,rust-humansize-1)
-             ("rust-mime" ,rust-mime-0.3)
-             ("rust-mime-guess" ,rust-mime-guess-2)
-             ("rust-nom" ,rust-nom-7)
-             ("rust-num-traits" ,rust-num-traits-0.2)
-             ("rust-percent-encoding"
-              ,rust-percent-encoding-2)
-             ("rust-proc-macro2" ,rust-proc-macro2-1)
-             ("rust-quote" ,rust-quote-1)
-             ("rust-serde" ,rust-serde-1)
-             ("rust-serde-json" ,rust-serde-json-1)
-             ("rust-serde-yaml" ,rust-serde-yaml-0.8)
-             ("rust-syn" ,rust-syn-1)
-             ("rust-toml" ,rust-toml-0.5))))
-    (home-page "https://github.com/djc/askama")
-    (synopsis "Shared code for Askama")
-    (description "This package provides shared code for Askama.")
-    (license (list license:expat license:asl2.0))))
-
 (define-public rust-assert-0.7
   (package
     (name "rust-assert")
