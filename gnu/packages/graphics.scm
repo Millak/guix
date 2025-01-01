5ea435beaa8b39fc30e28f494400d2f5891ce887
Janneke Nieuwenhuizen <janneke@gnu.org>
2025-01-01 14:30:42 +0100

    gnu: openexr: Update to 3.3.2; fixes build with gcc-14.
    * gnu/packages/graphics.scm (openexr): Update to 3.3.2.
    
    Change-Id: I5ae0205e4bc1c1ec6514d0c7278ce08d22d77907

--- gnu/packages/graphics.scm
+++ gnu/packages/graphics.scm
@@ -39,7 +39,7 @@
 ;;; Copyright © 2023, 2024 Artyom V. Poptsov <poptsov.artyom@gmail.com>
 ;;; Copyright © 2024 Ivan Vilata-i-Balaguer <ivan@selidor.net>
 ;;; Copyright © 2024 James Smith <jsubuntuxp@disroot.org>
-;;; Copyright © 2024 Janneke Nieuwenhuizen <janneke@gnu.org>
+;;; Copyright © 2024, 2025 Janneke Nieuwenhuizen <janneke@gnu.org>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -1379,7 +1379,7 @@ graphics.")
 (define-public openexr
   (package
     (name "openexr")
-    (version "3.2.4")
+    (version "3.3.2")
     (source (origin
               (method git-fetch)
               (uri (git-reference
@@ -1389,7 +1389,7 @@ graphics.")
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "00s1a05kggk71vfbnsvykyjc2j7y6yyzgl63sy4yiddshz2k2mcr"))))
+                "1vhjnhsgjaabg2rmqbkqpf3ikrbfm5qivl562yalllysll4gbimk"))))
     (build-system cmake-build-system)
     (arguments
      (list #:configure-flags
