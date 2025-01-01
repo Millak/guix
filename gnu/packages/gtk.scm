b2825430296ad5435cc484adb1ac5b4c508254c8
Janneke Nieuwenhuizen <janneke@gnu.org>
2025-01-01 17:26:02 +0100

    gnu: pangox-compat: Fix build with gcc-14.
    * gnu/packages/gtk.scm (pangox-compat)[arguments]: New field to relax gcc-14's
    strictness.
    
    Change-Id: I1f1c0ce20052b59a6d2e25490e3fcacf099f0757

--- gnu/packages/gtk.scm
+++ gnu/packages/gtk.scm
@@ -35,7 +35,7 @@
 ;;; Copyright © 2022 Petr Hodina <phodina@protonmail.com>
 ;;; Copyright © 2023 Sergiu Ivanov <sivanov@colimite.fr>
 ;;; Copyright © 2023, 2024 Zheng Junjie <873216071@qq.com>
-;;; Copyright © 2023 Janneke Nieuwenhuizen <janneke@gnu.org>
+;;; Copyright © 2023, 2025 Janneke Nieuwenhuizen <janneke@gnu.org>
 ;;; Copyright © 2024 John Kehayias <john.kehayias@protonmail.com>
 ;;; Copyright © 2025 Florian Pelz <pelzflorian@pelzflorian.de>
 ;;; Copyright © 2025 Remco van 't Veer <remco@remworks.net>
@@ -525,6 +525,10 @@ g_test_add_func \\(\"/layout/gravity-metrics2\", test_gravity_metrics2\\);")
               (base32
                "0ip0ziys6mrqqmz4n71ays0kf5cs1xflj1gfpvs4fgy2nsrr482m"))))
     (build-system gnu-build-system)
+    (arguments
+     (list
+      #:configure-flags
+      #~(list "CFLAGS=-g -O2 -Wno-error=incompatible-pointer-types")))
     (inputs
      (list glib pango-1.42))
     (native-inputs
