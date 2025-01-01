5e7570acc938e2b1cce0dc3cce7e2204c6ddf40b
Janneke Nieuwenhuizen <janneke@gnu.org>
2025-01-01 12:10:27 +0100

    gnu: cryptsetup-static: Update to latest json-c (0.18).
    * gnu/packages/cryptsetup.scm (static-library): Also cater for the cmake build
    system.
    (cryptsetup-static)[inputs]: Update to use json-c instead of json-c-0.13.
    
    Change-Id: Ic1d3644227d34d72204b4f670f1c84bfcea19702

--- gnu/packages/cryptsetup.scm
+++ gnu/packages/cryptsetup.scm
@@ -3,7 +3,7 @@
 ;;; Copyright © 2016 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2019–2021 Tobias Geerinckx-Rice <me@tobias.gr>
 ;;; Copyright © 2024 Efraim Flashner <efraim@flashner.co.il>
-;;; Copyright © 2024 Janneke Nieuwenhuizen <janneke@gnu.org>
+;;; Copyright © 2024, 2025 Janneke Nieuwenhuizen <janneke@gnu.org>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -24,6 +24,7 @@
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix packages)
   #:use-module (guix download)
+  #:use-module (guix build-system cmake)
   #:use-module (guix build-system gnu)
   #:use-module (guix gexp)
   #:use-module (guix utils)
@@ -121,8 +122,13 @@ files).  This assumes LIBRARY uses Libtool."
     (arguments
      (substitute-keyword-arguments (package-arguments library)
        ((#:configure-flags flags #~'())
-        #~(append '("--disable-shared" "--enable-static")
-                  #$flags))))))
+        (let* ((build-system (package-build-system library))
+               (static-flags (cond ((eq? build-system cmake-build-system)
+                                    '("-DBUILD_SHARED_LIBS=OFF"))
+                                   (else
+                                    '("--disable-shared" "--enable-static")))))
+          #~(append '#$static-flags
+                    #$flags)))))))
 
 (define-public cryptsetup-static
   ;; Stripped-down statically-linked 'cryptsetup' command for use in initrds.
@@ -179,7 +185,7 @@ files).  This assumes LIBRARY uses Libtool."
               (propagated-inputs
                `(("libgpg-error-host" ,(static-library libgpg-error)))))))
        `(("argon2" ,(static-library argon2))
-         ("json-c" ,(static-library json-c-0.13))
+         ("json-c" ,(static-library json-c))
          ("libgcrypt" ,libgcrypt-static)
          ("lvm2" ,lvm2-static)
          ("util-linux" ,util-linux "static")
