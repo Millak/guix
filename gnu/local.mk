cf9ff623ee3cbcbc4169d2a61422b6e9b3dd59ff
Janneke Nieuwenhuizen <janneke@gnu.org>
2025-01-02 19:46:42 +0100

    gnu: doc++: Fix build with gcc-14.
    * gnu/packages/patches/doc++-gcc-14.patch: New fdile.
    * gnu/local.mk (dist_patch_DATA): Register it.
    * gnu/packages/documentation.scm (doc++)[source]: Use it.
    
    Change-Id: Ifb388a5e45c1a4a5031b327e06de97774dc761a1

--- gnu/local.mk
+++ gnu/local.mk
@@ -1197,6 +1197,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/dicedb-remove-init-from-config-subpkg.patch	\
   %D%/packages/patches/directfb-davinci-glibc-228-compat.patch	\
   %D%/packages/patches/dkimproxy-add-ipv6-support.patch		\
+  %D%/packages/patches/doc++-gcc-14.patch			\
   %D%/packages/patches/docbook-utils-documentation-edits.patch	\
   %D%/packages/patches/docbook-utils-escape-characters.patch	\
   %D%/packages/patches/docbook-utils-remove-jade-sp.patch	\
