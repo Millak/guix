57e86ad335d7c2536ae41f737b8950519cb1e04b
Janneke Nieuwenhuizen <janneke@gnu.org>
2025-01-02 16:37:36 +0100

    gnu: tbb: Fix build with gcc-14.
    * gnu/packages/patches/tbb-gcc-14.patch: New file.
    * gnu/local.mk (dist_patch_DATA): Register it.
    * gnu/packages/tbb.scm (tbb)[source]: Use it.
    
    Change-Id: Ia8a5a609af4b0f7273912f1f3de1bd2f44b2f160

--- gnu/local.mk
+++ gnu/local.mk
@@ -2330,6 +2330,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/tao-fix-parser-types.patch		\
   %D%/packages/patches/tar-remove-wholesparse-check.patch	\
   %D%/packages/patches/tar-skip-unreliable-tests.patch		\
+  %D%/packages/patches/tbb-gcc-14.patch				\
   %D%/packages/patches/tbb-other-arches.patch			\
   %D%/packages/patches/tclxml-3.2-install.patch			\
   %D%/packages/patches/tcsh-fix-autotest.patch			\
