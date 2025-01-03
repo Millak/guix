20d0eb4c658f22ae05be61185aeabfafd7d9b102
Janneke Nieuwenhuizen <janneke@gnu.org>
2025-01-03 14:39:21 +0100

    gnu: r-rcppparallel: Fix build with gcc-14.
    * gnu/packages/cran.scm (r-rcppparallel)[arguments]: Use G-Expressions.  Add
    phase "relax-gcc-14-strictness".
    
    Change-Id: I3d6c0d0986405dcc327bddb1eccaf95c298aec1d

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -29676,10 +29676,22 @@ package provides a minimal R interface by relying on the Rcpp package.")
     (arguments
      (list
       #:phases
-      '(modify-phases %standard-phases
-         (add-after 'unpack 'use-system-tbb
-           (lambda* (#:key inputs #:allow-other-keys)
-             (setenv "TBB_ROOT" (assoc-ref inputs "tbb")))))))
+      #~(modify-phases %standard-phases
+          (add-after 'unpack 'use-system-tbb
+            (lambda* (#:key inputs #:allow-other-keys)
+              (setenv "TBB_ROOT" (assoc-ref inputs "tbb"))))
+          (add-before 'install 'relax-gcc-14-strictness
+            (lambda _
+              ;; XXX FIXME: $HOME/.R/Makevars seems to be the only way to
+              ;; set custom CFLAGS for R?
+              (setenv "HOME" (getcwd))
+              (mkdir-p ".R")
+              (with-directory-excursion ".R"
+                (with-output-to-file "Makevars"
+                  (lambda _
+                    (display (string-append
+                              "CXXFLAGS=-g -O2"
+                              " -Wno-error=changes-meaning\n"))))))))))
     (inputs (list tbb-2020))
     (native-inputs (list r-rcpp r-runit))
     (home-page "https://rcppcore.github.io/RcppParallel/")
