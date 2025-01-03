d6899e4c51b546fd1a5c55887b393888858df660
Janneke Nieuwenhuizen <janneke@gnu.org>
2025-01-03 13:43:45 +0100

    gnu: r-acme: Fix build with gcc-14.
    * gnu/packages/bioconductor.scm (r-acme)[arguments]: New field to relax
    gcc-14's strictness.
    
    Change-Id: Ib71324b6ac3d475bcdd9cd4bc868acb680122eba

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -14,6 +14,7 @@
 ;;; Copyright © 2021 Nicolas Vallet <nls.vallet@gmail.com>
 ;;; Copyright © 2023 Navid Afkhami <Navid.Afkhami@mdc-berlin.de>
 ;;; Copyright © 2024 Spencer King <spencer.king@geneoscopy.com>
+;;; Copyright © 2024, 2025 Janneke Nieuwenhuizen <janneke@gnu.org>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -22161,6 +22162,23 @@ printing and plotting @code{aCGH} objects.")
                 "05lvljiq3wgn0xqax8nhrsfwfxkslch9fyzk8v7qdvi72j0j3g9b"))))
     (properties `((upstream-name . "ACME")))
     (build-system r-build-system)
+    (arguments
+     (list
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-before 'install 'relax-gcc-14-strictness
+            (lambda _
+              ;; XXX FIXME: $HOME/.R/Makevars seems to be the only way to
+              ;; set custom CFLAGS for R?
+              (setenv "HOME" (getcwd))
+              (mkdir-p ".R")
+              (with-directory-excursion ".R"
+                (with-output-to-file "Makevars"
+                  (lambda _
+                    (display
+                     (string-append
+                      "CFLAGS=-g -O2"
+                      " -Wno-error=incompatible-pointer-types\n"))))))))))
     (propagated-inputs (list r-biobase r-biocgenerics))
     (home-page "https://bioconductor.org/packages/aCGH/")
     (synopsis "Calculating microarray enrichment")
