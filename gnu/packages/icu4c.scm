2a8e07cd04a20fb2a9b7e69cb059a384c4356288
Janneke Nieuwenhuizen <janneke@gnu.org>
2025-01-03 20:06:12 +0100

    gnu: icu4c: Revert to 71.
    It seems that there's some incompatibility, many packages, such as guix, are
    failing after the update to 76.1.
    
    * gnu/packages/icu4c.scm (icu4c): Rename to...
    (icu4c-76): ...this.
    (icu4c): Set to icu4c-71.
    
    Change-Id: Ibbe9154ade830cdbd0f1f0b8a870dc8d792b3e4c

--- gnu/packages/icu4c.scm
+++ gnu/packages/icu4c.scm
@@ -44,7 +44,7 @@
   #:use-module (guix build-system ant)
   #:use-module (guix build-system gnu))
 
-(define-public icu4c
+(define-public icu4c-76
   (package
     (name "icu4c")
     (version "76.1")
@@ -122,7 +122,7 @@ C/C++ part.")
 
 (define-public icu4c-71
   (package
-    (inherit icu4c)
+    (inherit icu4c-76)
     (name "icu4c")
     (version "71.1")
     (source (origin
@@ -135,6 +135,8 @@ C/C++ part.")
                     "-src.tgz"))
               (sha256
                (base32 "1gqywaqj9jmdwrng9lm6inyqmi5j2cz36db9dcqg3yk13zjyd9v7"))))))
+
+(define-public icu4c icu4c-71)
 
 (define-public icu4c-73
   (package
