555a73bfa852191e66cabf6687b7da40b4ede608
Efraim Flashner <efraim@flashner.co.il>
2025-01-03 13:53:29 +0200

    gnu: Add rust-thrussh-0.33.
    * gnu/packages/crates-io.scm (rust-thrussh-0.33): New variable.
    
    Change-Id: I846a3823f6802c8af9955270bfe4bdc7c88dede6

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -80532,6 +80532,46 @@ system.")
     (description "This package provides a client and server SSH library.")
     (license license:asl2.0)))
 
+(define-public rust-thrussh-0.33
+  (package
+    (inherit rust-thrussh-0.34)
+    (name "rust-thrussh")
+    (version "0.33.5")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "thrussh" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "1y1nhpg9l8jwjv59am9kmf9gib2j58c1qm36dvgq7pwsi8il0rcf"))
+       (snippet
+        #~(begin (use-modules (guix build utils))
+                 ;; README.md not included in the release tarball.
+                 (substitute* "Cargo.toml"
+                   (("\\.\\./README\\.md") "Cargo.toml"))))))
+    (arguments
+     `(#:cargo-test-flags '("--"
+                            "--skip=test_compress::compress_local_test"
+                            "--skip=src/lib.rs - (line 152)")
+       #:cargo-inputs (("rust-bitflags" ,rust-bitflags-1)
+                       ("rust-byteorder" ,rust-byteorder-1)
+                       ("rust-cryptovec" ,rust-cryptovec-0.6)
+                       ("rust-digest" ,rust-digest-0.9)
+                       ("rust-flate2" ,rust-flate2-1)
+                       ("rust-futures" ,rust-futures-0.3)
+                       ("rust-generic-array" ,rust-generic-array-0.14)
+                       ("rust-log" ,rust-log-0.4)
+                       ("rust-openssl" ,rust-openssl-0.10)
+                       ("rust-rand" ,rust-rand-0.8)
+                       ("rust-sha2" ,rust-sha2-0.9)
+                       ("rust-thiserror" ,rust-thiserror-1)
+                       ("rust-thrussh-keys" ,rust-thrussh-keys-0.21)
+                       ("rust-thrussh-libsodium" ,rust-thrussh-libsodium-0.2)
+                       ("rust-tokio" ,rust-tokio-1))
+       #:cargo-development-inputs (("rust-anyhow" ,rust-anyhow-1)
+                                   ("rust-env-logger" ,rust-env-logger-0.7)
+                                   ("rust-tokio" ,rust-tokio-1))))))
+
 (define-public rust-thrussh-config-0.6
   (package
     (name "rust-thrussh-config")
