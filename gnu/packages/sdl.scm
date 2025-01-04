beb40d81a88c14e5ba2322995afe1ece71fdeb77
Janneke Nieuwenhuizen <janneke@gnu.org>
2025-01-04 18:17:58 +0100

    gnu: sdl-image-1.2.12: Fix build with gcc-14.
    * gnu/packages/sdl.scm (sdl-image): Add CFLAGS to #:configure-flags to relax
    gcc-14's strictness.
    
    Change-Id: Ib589af2325488cc9e96333fd3046983714357508
    Modified-by: Zheng Junjie <z572@z572.online>

--- gnu/packages/sdl.scm
+++ gnu/packages/sdl.scm
@@ -18,6 +18,7 @@
 ;;; Copyright © 2023, 2024 dan <i@dan.games>
 ;;; Copyright © 2025 Zheng Junjie <873216071@qq.com>
 ;;; Copyright © 2025 Eric Bavier <bavier@posteo.net>
+;;; Copyright © 2025 Janneke Nieuwenhuizen <janneke@gnu.org>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -378,11 +379,15 @@ other supporting functions for SDL.")
     (build-system gnu-build-system)
     (outputs '("out" "debug"))
     (arguments
-     ;; Explicitly link against shared libraries instead of dlopening them.
-     '(#:configure-flags '("--disable-jpg-shared"
-                           "--disable-png-shared"
-                           "--disable-tif-shared"
-                           "--disable-webp-shared")))
+     (list
+      #:configure-flags
+      ;; Relax gcc-14's strictness.
+      #~(list "CFLAGS=-g -O2 -Wno-error=incompatible-pointer-types"
+              ;; Explicitly link against shared libraries instead of dlopening them.
+              "--disable-jpg-shared"
+              "--disable-png-shared"
+              "--disable-tif-shared"
+              "--disable-webp-shared")))
     (native-inputs (list pkg-config))
     ;; libjpeg, libpng, and libtiff are propagated inputs because the
     ;; SDL_image headers include the headers of these libraries.  SDL is a
