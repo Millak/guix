73963e8f889d0717d40ddaad3f1b1e368cf3cafa
Liliana Marie Prikler <liliana.prikler@gmail.com>
2025-01-05 08:42:52 +0100

    teams: gnome: Add gnome-extension-manifest.
    * etc/teams/gnome/gnome-extension-manifest.scm: New file.

---
+++ etc/teams/gnome/gnome-extension-manifest.scm
@@ -0,0 +1,31 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2024 Liliana Marie Prikler <liliana.prikler@gmail.com>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(use-modules (guix packages)
+             (guix profiles)
+             (gnu packages))
+
+(manifest
+  (map package->manifest-entry
+       (fold-packages
+         (lambda (package extensions)
+           (if (string-prefix? "gnome-shell-extension"
+                               (package-name package))
+               (cons package extensions)
+               extensions))
+         (list))))
