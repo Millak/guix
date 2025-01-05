f0b839ee9688e43170f6fd306941b6c51d226a2a
Janneke Nieuwenhuizen <janneke@gnu.org>
2025-01-05 09:59:31 +0100

    gnu: telepathy-logger: Fix build with gcc-14.
    * gnu/packages/freedesktop.scm (telepathy-logger)[arguments]: Use
    G-Expressions.  Add #:configure-flags to relax gcc-14's strictness.
    
    Change-Id: I72e7a045cc29f704777f599933f9fceb25f3f6bb

--- gnu/packages/freedesktop.scm
+++ gnu/packages/freedesktop.scm
@@ -2293,15 +2293,17 @@ between protocols to provide a unified interface for applications.")
                 "1bjx85k7jyfi5pvl765fzc7q2iz9va51anrc2djv7caksqsdbjlg"))))
     (build-system gnu-build-system)
     (arguments
-     '(#:parallel-tests? #f
-       #:phases
-       (modify-phases %standard-phases
-         (add-before 'check 'pre-check
-          (lambda _
-            (setenv "HOME" (getenv "TMPDIR"))
-            #t)))))
+     (list
+      #:parallel-tests? #f
+      #:configure-flags
+      #~(list "CFLAGS=-g -O2 -Wno-error=incompatible-pointer-types")
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-before 'check 'pre-check
+            (lambda _
+              (setenv "HOME" (getenv "TMPDIR")))))))
     (native-inputs
-     `(("glib:bin" ,glib "bin") ; for glib-genmarshal, etc.
+     `(("glib:bin" ,glib "bin")         ;for glib-genmarshal, etc.
        ("gobject-introspection" ,gobject-introspection)
        ("intltool" ,intltool)
        ("pkg-config" ,pkg-config)
