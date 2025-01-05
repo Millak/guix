3b31ef750affe3a5dcb82437647cb98f607648a2
Gabriel Wicki <gabriel@erlikon.ch>
2025-01-05 20:26:17 +0100

    gnu: hunspell: Add Austrian German dictionary.
    * gnu/packages/hunspell.scm (hunspell-dict-de-at): New variable.
    
    Change-Id: I34e8fd0b04443efa5e06fe54d6b57dff666c10a0
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/packages/hunspell.scm
+++ gnu/packages/hunspell.scm
@@ -219,6 +219,39 @@ spell-checking library.")
     (home-page "https://www.j3e.de/ispell/igerman98/")
     (license (list license:gpl2 license:gpl3))))
 
+(define-public hunspell-dict-de-at
+  (package
+    (name "hunspell-dict-de-at")
+    (version "20161207")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "https://www.j3e.de/ispell/igerman98/dict/"
+                           "igerman98-" version ".tar.bz2"))
+       (sha256
+        (base32 "1a3055hp2bc4q4nlg3gmg0147p3a1zlfnc65xiv2v9pyql1nya8p"))))
+    (build-system gnu-build-system)
+    (arguments
+     `(#:make-flags '("hunspell/de_AT.dic")
+       #:phases
+       (modify-phases %standard-phases
+         (delete 'configure)
+         (replace 'install              ;no install target
+           (lambda* (#:key outputs #:allow-other-keys)
+             (let* ((out (assoc-ref outputs "out"))
+                    (share (string-append out "/share/hunspell/")))
+               (install-file "hunspell/de_AT.aff" share)
+               (install-file "hunspell/de_AT.dic" share)
+               #t))))
+       #:tests? #f))        ; no tests
+    (native-inputs
+     (list hunspell ispell perl))
+    (synopsis "Hunspell dictionary for Austrian German (de_AT)")
+    (description "This package provides a dictionary for the Hunspell
+spell-checking library.")
+    (home-page "https://www.j3e.de/ispell/igerman98/")
+    (license (list license:gpl2 license:gpl3))))
+
 (define-public hunspell-dict-de-ch
   (package
     (name "hunspell-dict-de-ch")
