72e25861ed622f29d645c169aa580ee715b69105
Ricardo Wurmus <rekado@elephly.net>
2025-01-08 17:54:07 +0100

    gnu: python-mizani: Add missing input.
    * gnu/packages/python-xyz.scm (python-mizani)[native-inputs]: Add
    python-wheel.
    
    Change-Id: I9a1a7a4ee38db7a48e14069efb2b7b97dad32a80

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -5634,7 +5634,7 @@ processing tasks.")
      (list python-matplotlib python-numpy python-pandas python-scipy))
     (native-inputs
      (list python-setuptools python-setuptools-scm
-           python-pytest python-pytest-cov tzdata))
+           python-pytest python-pytest-cov python-wheel tzdata))
     (home-page "https://github.com/has2k1/mizani")
     (synopsis "Create data visualizations in Python")
     (description
