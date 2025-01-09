4d0de227e067cd60a4a7eb851adc0bc8966d0f39
Sharlatan Hellseher <sharlatanus@gmail.com>
2025-01-09 20:34:40 +0000

    gnu: go-github-com-git-lfs-go-netrc: Update to 0.0.0-20230601124202-e96144b9a966.
    * gnu/packages/golang.scm (go-github-com-git-lfs-go-netrc): Update to 0.0.0-20230601124202-e96144b9a966.
    
    Change-Id: I6214604e21b2d564c17dce5f8f276f559a6d111b

--- gnu/packages/golang.scm
+++ gnu/packages/golang.scm
@@ -2475,31 +2475,31 @@ of building man pages.")
     (license license:expat)))
 
 (define-public go-github-com-git-lfs-go-netrc
-  (let ((commit "f0c862dd687a9d9a7e15b3cd7cb3fd3e81cdd5ef")
-        (revision "0"))
     (package
       (name "go-github-com-git-lfs-go-netrc")
-      (version (git-version "0.0.0" revision commit))
+      (version "0.0.0-20230601124202-e96144b9a966")
       (source
        (origin
          (method git-fetch)
          (uri
           (git-reference
            (url "https://github.com/git-lfs/go-netrc")
-           (commit commit)))
+           (commit (go-version->git-ref version))))
          (file-name (git-file-name name version))
          (sha256
-          (base32 "0xvnjyg54gm3m3qszkfp12id0jmpg3583nqvv2llza1nr18w1sqi"))))
+          (base32 "1rb8xk0gp7yvw10zs122p48qhj3xc9csaykbflr7wdfx5xqpbr2f"))))
       (build-system go-build-system)
-      (arguments `(#:import-path "github.com/git-lfs/go-netrc/netrc"
-                   #:unpack-path "github.com/git-lfs/go-netrc"))
+      (arguments
+       (list
+        #:skip-build? #t
+        #:import-path "github.com/git-lfs/go-netrc"))
       (home-page "https://github.com/git-lfs/go-netrc")
       (synopsis "Netrc file parser for Go")
       (description "This package is for reading and writing netrc files.  This
 package can parse netrc files, make changes to them, and then serialize them
 back to netrc format, while preserving any whitespace that was present in the
 source file.")
-      (license license:expat))))
+      (license license:expat)))
 
 (define-public go-github-com-rubyist-tracerx
   (let ((commit "787959303086f44a8c361240dfac53d3e9d53ed2")
