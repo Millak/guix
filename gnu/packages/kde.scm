c384befdc5463e2bf473d026134403a8059c969b
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2025-01-10 11:52:07 +0800

    gnu: kdeconnect: Update to 24.12.1.
    * gnu/packages/kde.scm (kdeconnect): Update to 24.12.1.
    [inputs]: Add kcrash and qtconnectivity.
    
    Change-Id: Ie0a1951bac64e81c1d4bedb33e5fed435b055c10

--- gnu/packages/kde.scm
+++ gnu/packages/kde.scm
@@ -1416,7 +1416,7 @@ different notification systems.")
 (define-public kdeconnect
   (package
     (name "kdeconnect")
-    (version "24.05.2")
+    (version "24.12.1")
     (source
      (origin
        (method url-fetch)
@@ -1425,7 +1425,7 @@ different notification systems.")
                            version ".tar.xz"))
        (sha256
         (base32
-         "0jh5rx6amg7mxgy0n0hv23wj3qqmb37212996ssm41bvxnmjgn03"))))
+         "0b6n5bh071ww4nmpwyhgrm1wlkjpvr61s52xfd7b6bzz8v8ilanz"))))
     (build-system qt-build-system)
     (arguments
      (list #:qtbase qtbase
@@ -1457,6 +1457,7 @@ different notification systems.")
      (list dbus
            kcmutils
            kconfigwidgets
+           kcrash
            kdbusaddons
            kguiaddons
            ki18n
@@ -1476,6 +1477,7 @@ different notification systems.")
            qca-qt6
            qqc2-desktop-style
            qtbase
+           qtconnectivity
            qtdeclarative
            qtmultimedia
            qtwayland
