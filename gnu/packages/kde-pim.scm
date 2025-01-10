0d99537d6a67a1ea45d10080f65c95456ca7a8b0
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2025-01-10 11:51:46 +0800

    gnu: keventviews: Update to 24.12.1.
    * gnu/packages/kde-pim.scm (keventviews): Update to 24.12.1.
    
    Change-Id: Id2053a8592b8c7802af2bce90e759477274d1376

--- gnu/packages/kde-pim.scm
+++ gnu/packages/kde-pim.scm
@@ -806,14 +806,14 @@ package.")
 (define-public keventviews
   (package
     (name "keventviews")
-    (version "24.05.2")
+    (version "24.12.1")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://kde/stable/release-service/" version
                            "/src/eventviews-" version ".tar.xz"))
        (sha256
-        (base32 "0kqkd8dqh8plmxngajr8266nad2sm7qf711h2jpiav753p0xas5z"))))
+        (base32 "11f2k7mszik3f05rn6gv7a6ihhdl8vb9g97ynqdbc8qvik2x8frq"))))
     (properties `((upstream-name . "eventviews")))
     (build-system qt-build-system)
     (native-inputs
