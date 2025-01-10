90f433c1b2672397d829e15fd31314eeac873a99
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2025-01-10 11:51:59 +0800

    gnu: print-manager: Update to 6.2.5.
    * gnu/packages/kde-plasma.scm (print-manager): Update to 6.2.5.
    
    Change-Id: I0cda8abfe36cd8c46c44303057155134259cf3bc

--- gnu/packages/kde-plasma.scm
+++ gnu/packages/kde-plasma.scm
@@ -3014,14 +3014,14 @@ hardware management, and a high degree of customizability.")
 (define-public print-manager
   (package
     (name "print-manager")
-    (version "6.1.4")
+    (version "6.2.5")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://kde/stable/plasma/" version
                            "/print-manager-" version ".tar.xz"))
        (sha256
-        (base32 "1pqfs1v3ll8plb6950jn8s0fslkfvpzl89ix20hs2jw08kspnx65"))))
+        (base32 "1k7abx5x0bjw6hs6wryw1g10s33ch1wsdgvi5qzjmq2znzmicq05"))))
     (build-system qt-build-system)
     (native-inputs
      (list extra-cmake-modules))
