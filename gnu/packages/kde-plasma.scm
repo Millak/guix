9f2994652c354ac56d6939de84af6d9e827b72ff
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2025-01-10 11:51:54 +0800

    gnu: kpipewire: Update to 6.2.5.
    * gnu/packages/kde-plasma.scm (kpipewire): Update to 6.2.5.
    
    Change-Id: I8b4a28bc0781634c664bff16bd46fb930bf8cd2d

--- gnu/packages/kde-plasma.scm
+++ gnu/packages/kde-plasma.scm
@@ -897,14 +897,14 @@ the schedule and venue information.")
 (define-public kpipewire
   (package
     (name "kpipewire")
-    (version "6.1.4")
+    (version "6.2.5")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://kde/stable/plasma/"
                                   version "/" name "-" version ".tar.xz"))
               (sha256
                (base32
-                "0d3i6d2lcykvlvf2brpqf78qwg9qyiy5jpsrdgd3cswazmb6s4db"))))
+                "06b37sbvvv1cm9zyx6kc4qa13w6fm4gpv9p61vc7nhnay20xahnv"))))
     (build-system cmake-build-system)
     (native-inputs (list extra-cmake-modules pkg-config))
     (propagated-inputs (list qtbase qtdeclarative
