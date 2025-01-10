f6e9fcafa70159c13a907b9d2c6bbf43a44b8b8f
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2025-01-10 11:51:55 +0800

    gnu: libksysguard: Update to 6.2.5.
    * gnu/packages/kde-plasma.scm (libksysguard): Update to 6.2.5.
    [inputs]: Remove kcompletion, kconfigwidgets, kdeclarative, kglobalaccel,
    kiconthemes, kio, kwidgetsaddons, kwindowsystem, libplasma, qtwebchannel and
    qtwebengine. Add kpackage and solid.
    
    Change-Id: I7c2b241de6ff2df0a9b3b966a7eca712f85f0615

--- gnu/packages/kde-plasma.scm
+++ gnu/packages/kde-plasma.scm
@@ -1261,39 +1261,30 @@ basic needs and easy to configure for those who want special setups.")
 (define-public libksysguard
   (package
     (name "libksysguard")
-    (version "6.1.4")
+    (version "6.2.5")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://kde/stable/plasma/" version
                            "/libksysguard-" version ".tar.xz"))
        (sha256
-        (base32 "1cf5ar63l81q41324g9h9712j66rl1s5n2brras83kkijnyax9yj"))))
+        (base32 "1avn3krk5g20lrclq8y2syg10b8fn97d6vlfxf14v2q7npbg754n"))))
     (native-inputs
      (list bash-minimal extra-cmake-modules pkg-config qttools))
     (inputs
      (list kauth
-           kcompletion
            kconfig
-           kconfigwidgets
            kcoreaddons
-           kdeclarative
-           kglobalaccel
            ki18n
-           kiconthemes
-           kio
            knewstuff
+           kpackage
            kservice
-           kwidgetsaddons
-           kwindowsystem
            libnl
            libcap
            libpcap
            `(,lm-sensors "lib")
-           libplasma
            qtdeclarative
-           qtwebchannel
-           qtwebengine
+           solid
            zlib))
     (build-system qt-build-system)
     (arguments
