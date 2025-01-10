8943af583d4af757ac6b656d719d5cef9eebf9a1
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2025-01-10 11:51:52 +0800

    gnu: krdp: Update to 6.2.5.
    * gnu/packages/kde-plasma.scm (krdp): Update to 6.2.5.
    [inputs]: Add kcrash.
    
    Change-Id: I5f4d83e1370e04bbdc3c8367f09ea4edc333e17a

--- gnu/packages/kde-plasma.scm
+++ gnu/packages/kde-plasma.scm
@@ -429,7 +429,7 @@ concept.")
 (define-public krdp
   (package
     (name "krdp")
-    (version "6.1.4")
+    (version "6.2.5")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://kde/stable/plasma/"
@@ -437,7 +437,7 @@ concept.")
                                   version ".tar.xz"))
               (sha256
                (base32
-                "1jwiqmmwhcslj6zcjgm3jj3xkr3zkp3r8hbassykazg6bcc8xpkl"))))
+                "1z3k9b1jbiw1qixvlmi4vrwl4p4qhrcirgr2ksp0rs4ivgikg38s"))))
     (build-system qt-build-system)
     (arguments (list #:qtbase qtbase
                      #:phases
@@ -457,6 +457,7 @@ concept.")
                          wayland))
     (inputs (list
              kconfig
+             kcrash
              kdbusaddons
              kcmutils
              ki18n
