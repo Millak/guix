b3543b5d688dd1b1575ea385c9e52cb1e0ae067a
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2025-01-10 11:51:30 +0800

    gnu: kded: Update to 6.10.0.
    * gnu/packages/kde-frameworks.scm (kded): Update to 6.10.0.
    
    Change-Id: I0b41b05d9078ea9e8cb1b52c3cab019e1f7b3f14

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -3524,7 +3524,7 @@ that offer bindings to some of the Frameworks.")
 (define-public kded
   (package
     (name "kded")
-    (version "6.5.0")
+    (version "6.10.0")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -3533,7 +3533,7 @@ that offer bindings to some of the Frameworks.")
                     name "-" version ".tar.xz"))
               (sha256
                (base32
-                "1c2lk418lazlynclw3zy79bzs21v962afpl98wzl9766mi0sjb19"))))
+                "13ka1vzid6k3hywyhjkj35z8rlqjgnxp8ivzy7m7yl69zpdxj0an"))))
     (build-system cmake-build-system)
     (native-inputs
      (list extra-cmake-modules kdoctools))
