9562474ebca28c95f1d053046787f2e8cff0e9d4
Ricardo Wurmus <rekado@elephly.net>
2025-01-10 21:59:52 +0100

    gnu: python-xdoctest: Update to 1.2.0.
    * gnu/packages/python-xyz.scm (python-xdoctest): Update to 1.2.0.
    [build-system]: Use pyproject-build-system.
    [arguments]: Remove custom 'check phase; add 'pre-check phase.
    [native-inputs]: Add cmake-minimal, ninja, pybind11, python-pytest-cov,
    python-scikit-build, python-setuptools, and python-wheel.
    [propagated-inputs]: Remove python-six.
    
    Change-Id: Ia12eaa96751191efc2d6aa23ec460b3892cc6428

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -20434,26 +20434,31 @@ python-xdo for newer bindings.)")
 (define-public python-xdoctest
   (package
     (name "python-xdoctest")
-    (version "1.0.0")
+    (version "1.2.0")
     (source
      (origin
        (method url-fetch)
        (uri (pypi-uri "xdoctest" version))
        (sha256
-        (base32 "0bgbmb9nqv95f9gfxqifqff1qaz5fnanjqy4hv7ygrjp2kksgfvy"))))
-    (build-system python-build-system)
+        (base32 "0qsqxp9bpjzg8kb4k8n3sppzbzdr6l6n0vkm7z9qir4ii5nwmkyq"))))
+    (build-system pyproject-build-system)
     (arguments
-     (list #:phases
-           #~(modify-phases %standard-phases
-               (replace 'check
-                 (lambda* (#:key tests? #:allow-other-keys)
-                   (when tests?
-                     ;; A writable HOME is needed by the
-                     ;; 'import_module_from_path' test.
-                     (setenv "HOME" "/tmp")
-                     (invoke "pytest" "-vv")))))))
-    (propagated-inputs (list python-six))
-    (native-inputs (list python-pytest which))
+     (list
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-before 'check 'pre-check
+            (lambda _
+              ;; A writable HOME is needed by the 'import_module_from_path'
+              ;; test.
+              (setenv "HOME" "/tmp"))))))
+    (native-inputs (list cmake-minimal
+                         ninja
+                         pybind11
+                         python-pytest
+                         python-pytest-cov
+                         python-scikit-build
+                         python-setuptools
+                         python-wheel))
     (home-page "https://github.com/Erotemic/xdoctest")
     (synopsis "Rewrite of the Python builtin doctest module")
     (description "This package provides a rewrite of the builtin doctest
