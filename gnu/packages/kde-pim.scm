2cade63db5301f6755692cd9368e6269fdcd1f5d
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2025-01-10 11:51:50 +0800

    gnu: libkgapi: Update to 24.12.1.
    * gnu/packages/kde-pim.scm (libkgapi): Update to 24.12.1.
    
    Change-Id: Ifb9366d3748f12affee0975dd569d24deb2270dd

--- gnu/packages/kde-pim.scm
+++ gnu/packages/kde-pim.scm
@@ -2113,14 +2113,14 @@ and allows one to view/extract message formatted text in Rich Text Format.")
 (define-public libkgapi
   (package
     (name "libkgapi")
-    (version "24.05.2")
+    (version "24.12.1")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://kde/stable/release-service/" version
                            "/src/libkgapi-" version ".tar.xz"))
        (sha256
-        (base32 "0j0rbzwcjq4wjrrk0vhkifa8ahmmrpfy039fpf3gy237k5ncj5y3"))))
+        (base32 "1k508grqzw60xjvjx8679ha8gsfvvvdyi9dn6xvz3zi34rhlqiim"))))
     (build-system qt-build-system)
     (native-inputs
      (list extra-cmake-modules qttools))
