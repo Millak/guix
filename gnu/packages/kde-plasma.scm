24b96fe0303c50039a6226b14f1a9deb3f3ad0b8
Zheng Junjie <zhengjunjie@iscas.ac.cn>
2025-01-10 11:51:54 +0800

    gnu: ksystemstats: Update to 6.2.5.
    * gnu/packages/kde-plasma.scm (ksystemstats): Update to 6.2.5.
    [inputs]: Add kcrash.
    
    Change-Id: Ib60796e36731e7ccaffb76f8d9594a368c8a9582

--- gnu/packages/kde-plasma.scm
+++ gnu/packages/kde-plasma.scm
@@ -1002,7 +1002,7 @@ call it if it is not associated to a terminal.")
 (define-public ksystemstats
   (package
     (name "ksystemstats")
-    (version "6.1.4")
+    (version "6.2.5")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://kde/stable/plasma/"
@@ -1010,7 +1010,7 @@ call it if it is not associated to a terminal.")
                                   version ".tar.xz"))
               (sha256
                (base32
-                "1743q0xyx1b465qhv7bg1xyblfwm515xfvifgzc3qxwhkyci64d4"))))
+                "18cmjp6jykzg8iiihkfp23ylcgcds3448ym4651fvrbgbvsmdzzg"))))
     (build-system qt-build-system)
     (arguments
      (list #:qtbase qtbase
@@ -1022,6 +1022,7 @@ call it if it is not associated to a terminal.")
     (native-inputs (list extra-cmake-modules pkg-config))
     (inputs (list glib
                   kcoreaddons
+                  kcrash
                   kdbusaddons
                   solid
                   networkmanager-qt
