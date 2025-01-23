95504f4821dca398ea71b5e99a1d7ffe6069b4b2
Felix Gruber <felgru@posteo.net>
2025-01-23 19:47:31 +0000

    gnu: python-pygments: Update to 2.19.1.
    * gnu/packages/python-xyz.scm (python-pygments): Update to 2.19.1.
      [source]: Fix PyPI package name; with capital P, the download doesn't
      work.
      [arguments] <test-flags>: Ignore just some problematic tests; drop
      "--ignore-glob" option.
      [native-inputs]: Add python-hatchling; replace python-pytest by
      python-pytest-bootstrap; remove python-setuptools and python-wheel.
    
    Change-Id: I85bf3cb2a8c4a996586e820bd582dea777ef930d
    Modified-by: Sharlatan Hellseher <sharlatanus@gmail.com>
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -8704,23 +8704,37 @@ with Python.")
 (define-public python-pygments
   (package
     (name "python-pygments")
-    (version "2.15.1")
+    (version "2.19.1")
     (source
      (origin
        (method url-fetch)
-       (uri (pypi-uri "Pygments" version))
+       (uri (pypi-uri "pygments" version))
        (sha256
         (base32
-         "0p3p28fif7m2w5mkd0z99zk9xwgrs3m61x85415qk0fl3ly4vkla"))))
+         "07qm8mx3y5r8ri6zpn0hp9zx5g02bydhi7pkv54hdp3nhlm6vhb1"))))
     (build-system pyproject-build-system)
     (arguments
      (list
       #:test-flags
-      ;; Some tests require sphinx, which depends on this, so we only run
-      ;; basic tests.
-      '(list "--ignore-glob=tests/*/*")))
+      ;; 4568 passed, 16 skipped, 597 deselected
+      ;;
+      ;; Ignore tests requiring "wcag_contrast_ratio"
+      #~(list "--ignore=tests/contrast/test_contrasts.py"
+              ;; Tests fail with not matched diff.
+              "--ignore=tests/examplefiles/awk/test.awk"
+              "--ignore=tests/examplefiles/bash/example.sh"
+              "--ignore=tests/examplefiles/make/Makefile"
+              "--ignore=tests/examplefiles/phix/example.exw"
+              "--ignore=tests/examplefiles/sed/all_sorts_of_syntax.sed"
+              "--ignore=tests/examplefiles/sed/count_words.sed"
+              "--ignore=tests/examplefiles/sed/increment_number.sed"
+              "--ignore=tests/examplefiles/sed/reverse.sed"
+              "--ignore=tests/examplefiles/slurm/example.sl"
+              ;; Assertion error to find example file by following symlink:
+              ;; assert p.is_file(), f"Example file {p} not found"
+              "--deselect=tests/test_basic_api.py::test_lexer_classes")))
     (native-inputs
-     (list python-pytest python-setuptools python-wheel))
+     (list python-hatchling python-pytest-bootstrap))
     (home-page "https://pygments.org/")
     (synopsis "Syntax highlighting")
     (description
