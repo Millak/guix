Millak/guix

commit 5c2bcafd281fdd31b3dfec5a67ba85084c58cf60

gnu: Remove linux-libre 6.11.
This kernel series is no longer supported upstream.

* gnu/packages/linux.scm (linux-libre-6.11-version,
linux-libre-6.11-gnu-revision, deblob-scripts-6.11,
linux-libre-6.11-pristine-source, linux-libre-6.11-source,
linux-libre-headers-6.11, linux-libre-6.11): Remove variables.
the commit message for your changes. Lines starting
* gnu/packages/aux-files/linux-libre/6.11-arm.conf,
gnu/packages/aux-files/linux-libre/6.11-arm64.conf,
gnu/packages/aux-files/linux-libre/6.11-i686.conf,
gnu/packages/aux-files/linux-libre/6.11-riscv.conf,
gnu/packages/aux-files/linux-libre/6.11-x86_64.conf: Delete files.
* Makefile.am (AUX_FILES): Remove them.

Change-Id: I71ae7696534b2096de5ffb650bf8a8313e907edb

gnu/packages/linux.scm
@@ -529,21 +529,6 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
                             (%upstream-linux-source version hash)
                             deblob-scripts-6.12)))
 
-(define-public linux-libre-6.11-version "6.11.11")
-(define-public linux-libre-6.11-gnu-revision "gnu")
-(define deblob-scripts-6.11
-  (linux-libre-deblob-scripts
-   linux-libre-6.11-version
-   linux-libre-6.11-gnu-revision
-   (base32 "0igjsnlnvzl9q2cb5ypavbfirldqw86v7krmxljivshh765xzykd")
-   (base32 "0y5s6yr50vilmi095argqikq1inng9rsaf5r8b3j2zz3brk8d17j")))
-(define-public linux-libre-6.11-pristine-source
-  (let ((version linux-libre-6.11-version)
-        (hash (base32 "1z2913y38clnlmhvwj49h7p4pic24s4d8np7nmd4lk7m2xz8w532")))
-   (make-linux-libre-source version
-                            (%upstream-linux-source version hash)
-                            deblob-scripts-6.11)))
-
 ;; The "longterm" kernels — the older releases with long-term upstream support.
 ;; Here are the support timelines:
 ;; <https://www.kernel.org/category/releases.html>
@@ -653,11 +638,6 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
 
 (define-public linux-libre-6.12-source
   (source-with-patches linux-libre-6.12-pristine-source
-                       (list %boot-logo-patch
-                             %linux-libre-arm-export-__sync_icache_dcache-patch)))
-
-(define-public linux-libre-6.11-source
-  (source-with-patches linux-libre-6.11-pristine-source
                        (list %boot-logo-patch
                              %linux-libre-arm-export-__sync_icache_dcache-patch)))
 
@@ -781,11 +761,6 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
   (make-linux-libre-headers* linux-libre-6.12-version
                              linux-libre-6.12-gnu-revision
                              linux-libre-6.12-source))
-
-(define-public linux-libre-headers-6.11
-  (make-linux-libre-headers* linux-libre-6.11-version
-                             linux-libre-6.11-gnu-revision
-                             linux-libre-6.11-source))
 
 (define-public linux-libre-headers-6.6
   (make-linux-libre-headers* linux-libre-6.6-version
@@ -1147,14 +1122,6 @@ Linux kernel.  It has been modified to remove all non-free binary blobs.")
   (make-linux-libre* linux-libre-6.12-version
                      linux-libre-6.12-gnu-revision
                      linux-libre-6.12-source
-                     '("x86_64-linux" "i686-linux" "armhf-linux"
-                       "aarch64-linux" "powerpc64le-linux" "riscv64-linux")
-                     #:configuration-file kernel-config))
-
-(define-public linux-libre-6.11
-  (make-linux-libre* linux-libre-6.11-version
-                     linux-libre-6.11-gnu-revision
-                     linux-libre-6.11-source
                      '("x86_64-linux" "i686-linux" "armhf-linux"
                        "aarch64-linux" "powerpc64le-linux" "riscv64-linux")
                      #:configuration-file kernel-config))

Makefile.am
@@ -446,11 +446,6 @@ AUX_FILES =						\
   gnu/packages/aux-files/linux-libre/6.12-i686.conf	\
   gnu/packages/aux-files/linux-libre/6.12-x86_64.conf	\
   gnu/packages/aux-files/linux-libre/6.12-riscv.conf	\
-  gnu/packages/aux-files/linux-libre/6.11-arm.conf	\
-  gnu/packages/aux-files/linux-libre/6.11-arm64.conf	\
-  gnu/packages/aux-files/linux-libre/6.11-i686.conf	\
-  gnu/packages/aux-files/linux-libre/6.11-x86_64.conf	\
-  gnu/packages/aux-files/linux-libre/6.11-riscv.conf	\
   gnu/packages/aux-files/linux-libre/6.6-arm.conf	\
   gnu/packages/aux-files/linux-libre/6.6-arm64.conf	\
   gnu/packages/aux-files/linux-libre/6.6-i686.conf	\

commit 9f9e89a8ca0c18d0ca0292a3eb58d1da9a8cc119

gnu: linux-libre: Remove duplicate “linux-libre-arm64-generic@6.12” package.
Fixes a regression in 5c2bcafd281fdd31b3dfec5a67ba85084c58cf60, which
led to having two “linux-libre-arm64-generic@6.12” packages on
aarch64-linux, thus leading to a failure of the “fold-available-packages
with/without cache” in ‘tests/packages.scm’.

* gnu/packages/linux.scm (linux-libre-lts-arm64-generic): Check whether
‘linux-libre-lts-version’ is equal to ‘linux-libre-version’ and, if so,
make it an alias for ‘linux-libre-arm64-generic’.

Change-Id: I01e4d4ff9c66ffe1cb33dd781fba9f7f6b40e93e

gnu/packages/linux.scm
@@ -1,5 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2012-2021, 2021-2024 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2012-2021, 2021-2025 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2013, 2014, 2015, 2016 Andreas Enge <andreas@enge.fr>
 ;;; Copyright © 2012 Nikita Karetnikov <nikita@karetnikov.org>
 ;;; Copyright © 2014, 2015, 2016, 2017, 2018, 2019, 2020 Mark H Weaver <mhw@netris.org>
@@ -1268,32 +1268,38 @@ Linux kernel.  It has been modified to remove all non-free binary blobs.")
                       (default-extra-linux-options linux-libre-version))))
 
 (define-public linux-libre-lts-arm64-generic
-  (make-linux-libre* linux-libre-lts-version
-                     linux-libre-lts-gnu-revision
-                     linux-libre-lts-source
-                     '("aarch64-linux")
-                     #:defconfig "defconfig"
-                     #:extra-version "arm64-generic"
-                     #:extra-options
-                     (append
-                      `(;; Provide support for ath9k wireless
-                        ("CONFIG_ATH9K_HTC" . m)
-                        ;; needed to fix the RTC on rockchip platforms
-                        ("CONFIG_RTC_DRV_RK808" . #t)
-                        ;; Pinebook display, battery, charger and usb
-                        ("CONFIG_DRM_ANALOGIX_ANX6345" . m)
-                        ("CONFIG_CHARGER_AXP20X" . m)
-                        ("CONFIG_INPUT_AXP20X_PEK" . m)
-                        ("CONFIG_CHARGER_AXP20X" . m)
-                        ("CONFIG_BATTERY_AXP20X" . m)
-                        ("CONFIG_PINCTRL_AXP209" . m)
-                        ("CONFIG_AXP20X_POWER" . m)
-                        ("CONFIG_AXP20X_ADC" . m)
-                        ;; Pinebook PRO battery and sound support
-                        ("CONFIG_BATTERY_CW2015" . m)
-                        ("CONFIG_CHARGER_GPIO" . m)
-                        ("CONFIG_SND_SOC_ES8316" . m))
-                      (default-extra-linux-options linux-libre-lts-version))))
+  (if (string=? linux-libre-lts-version linux-libre-version)
+
+      ;; Make it a strict alias so that the user interface does not see two
+      ;; packages with the exact same name and version.
+      linux-libre-arm64-generic
+
+      (make-linux-libre* linux-libre-lts-version
+                         linux-libre-lts-gnu-revision
+                         linux-libre-lts-source
+                         '("aarch64-linux")
+                         #:defconfig "defconfig"
+                         #:extra-version "arm64-generic"
+                         #:extra-options
+                         (append
+                          `( ;; Provide support for ath9k wireless
+                            ("CONFIG_ATH9K_HTC" . m)
+                            ;; needed to fix the RTC on rockchip platforms
+                            ("CONFIG_RTC_DRV_RK808" . #t)
+                            ;; Pinebook display, battery, charger and usb
+                            ("CONFIG_DRM_ANALOGIX_ANX6345" . m)
+                            ("CONFIG_CHARGER_AXP20X" . m)
+                            ("CONFIG_INPUT_AXP20X_PEK" . m)
+                            ("CONFIG_CHARGER_AXP20X" . m)
+                            ("CONFIG_BATTERY_AXP20X" . m)
+                            ("CONFIG_PINCTRL_AXP209" . m)
+                            ("CONFIG_AXP20X_POWER" . m)
+                            ("CONFIG_AXP20X_ADC" . m)
+                            ;; Pinebook PRO battery and sound support
+                            ("CONFIG_BATTERY_CW2015" . m)
+                            ("CONFIG_CHARGER_GPIO" . m)
+                            ("CONFIG_SND_SOC_ES8316" . m))
+                          (default-extra-linux-options linux-libre-lts-version)))))
 
 (define-public linux-libre-arm64-generic-5.10
   (make-linux-libre* linux-libre-5.10-version