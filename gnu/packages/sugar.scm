333799c38f5d91347088757757468ad23ce09b13
Tomas Volf <~@wolfsden.cz>
2025-01-24 12:48:26 +0100

    gnu: sugar-toolkit-gtk3: Dehardcode #:modules.
    * gnu/packages/sugar.scm (sugar-toolkit-gtk3)[arguments]<#:modules>: Replace
    the hardcoded list with %glib-or-gtk-build-system-default-modules.
    
    Change-Id: I6a5e4511d2e696a673f7d5b49f75285ee488223d
    Signed-off-by: Andreas Enge <andreas@enge.fr>

--- gnu/packages/sugar.scm
+++ gnu/packages/sugar.scm
@@ -320,9 +320,8 @@ and metadata, and the journal with querying and full text search.")
       `(,@%glib-or-gtk-build-system-modules
         (guix build python-build-system))
       #:modules
-      '((guix build glib-or-gtk-build-system)
-        ((guix build python-build-system) #:prefix python:)
-        (guix build utils))
+      `(((guix build python-build-system) #:prefix python:)
+        ,@%glib-or-gtk-build-system-default-modules)
       #:phases
       #~(modify-phases %standard-phases
           (add-after 'unpack 'patch-build-system
