b5ff25fc070bdcec22f0fcba3fee83045bc7b688
Sharlatan Hellseher <sharlatanus@gmail.com>
2025-01-24 13:06:01 +0000

    gnu: packages/syncthing: Remove (gnu packages golang).
    * gnu/packages/syncthing.scm: Remove (gnu packages golang) as no longer
    required.
    
    Change-Id: I884595ec431b153868902271885e179bb4cf812e

--- gnu/packages/syncthing.scm
+++ gnu/packages/syncthing.scm
@@ -38,7 +38,6 @@
   #:use-module (gnu packages freedesktop)
   #:use-module (gnu packages glib)
   #:use-module (gnu packages gnome)
-  #:use-module (gnu packages golang)
   #:use-module (gnu packages golang-build)
   #:use-module (gnu packages golang-compression)
   #:use-module (gnu packages gtk)
