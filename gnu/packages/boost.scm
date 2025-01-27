be6f1d0fe4abb3bd8f3239db33b7b44a50de8bbc
Ada Stevenson <adanskana@gmail.com>
2025-01-27 14:16:54 +0800

    gnu: boost: Update source URL.
    * gnu/packages/boost.scm (boost) [source]: Update URL.
    
    Change-Id: Iffca22ac6a7b44a9e0b3a227a0c7265466ba2066
    Signed-off-by: Ian Eure <ian@retrospec.tv>

--- gnu/packages/boost.scm
+++ gnu/packages/boost.scm
@@ -70,7 +70,7 @@
     (version "1.83.0")
     (source (origin
               (method url-fetch)
-              (uri (string-append "https://boostorg.jfrog.io/artifactory/main/release/"
+              (uri (string-append "https://archives.boost.io/release/"
                                   version "/source/boost_"
                                   (version-with-underscores version) ".tar.bz2"))
               (patches
