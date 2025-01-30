409c99f4b9e5f7902efd59de0b27e727cc8908d9
Nicolas Graves <ngraves@ngraves.fr>
2025-01-30 23:40:47 +0100

    gnu: python-doubletdetection: Remove python-black native-input.
    * gnu/packages/bioinformatics.scm (python-doubletdetection)
    [native-inputs]: Remove python-black.
    
    Change-Id: Iaa12a1b73bb2f97a053c7b6365b3e1bc7c53080d
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/bioinformatics.scm
+++ gnu/packages/bioinformatics.scm
@@ -2899,8 +2899,7 @@ demultiplexing step.")
            python-scipy
            python-tqdm))
     (native-inputs
-     (list python-black
-           python-flake8
+     (list python-flake8
            python-poetry-core
            python-pytest))
     (home-page "https://github.com/JonathanShor/DoubletDetection")
