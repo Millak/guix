dbdcb9e86bdebe2672a52fafc365045e11a2807a
Herman Rimm <herman@rimm.ee>
2025-01-31 22:09:30 +0100

    gnu: rust-http-body-0.4: Add cargo-inputs.
    * gnu/packages/crates-web.scm (rust-http-body-0.4)[cargo-inputs]: Add
    rust-pin-project-lite-0.2.
    
    Change-Id: Ibe08c807c1b0b7f6065cbf1e7ea4f10a052c31a7
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/crates-web.scm
+++ gnu/packages/crates-web.scm
@@ -3970,7 +3970,8 @@ HTTP request or response body.")
      `(#:skip-build? #t
        #:cargo-inputs
        (("rust-bytes" ,rust-bytes-1)
-        ("rust-http" ,rust-http-0.2))))))
+        ("rust-http" ,rust-http-0.2)
+        ("rust-pin-project-lite" ,rust-pin-project-lite-0.2))))))
 
 (define-public rust-http-body-0.3
   (package
