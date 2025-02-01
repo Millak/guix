7bac8e1317a2a383ab0453975d658e0e3049d4ed
Danny Milosavljevic <dannym@friendly-machines.com>
2025-02-02 00:59:24 +0100

    gnu: Register patch for llama-cpp.
    * gnu/local.mk (dist_patch_DATA): Add llama-cpp-vulkan-optional.patch .
    
    Change-Id: If6d189aef3b40b28d5ccbdb9435ead510f014ec0

--- gnu/local.mk
+++ gnu/local.mk
@@ -1825,6 +1825,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/mcrypt-CVE-2012-4527.patch			\
   %D%/packages/patches/libmemcached-build-with-gcc7.patch	\
   %D%/packages/patches/libmhash-hmac-fix-uaf.patch		\
+  %D%/packages/patches/llama-cpp-vulkan-optional.patch	\
   %D%/packages/patches/lvm2-no-systemd.patch    		\
   %D%/packages/patches/maturin-no-cross-compile.patch		\
   %D%/packages/patches/mecab-variable-param.patch		\
