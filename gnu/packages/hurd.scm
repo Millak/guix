Millak/guix

commit beb9ad2cf7e83b747781b47cdde2f75a19cd3a1b

gnu: hurd: Add libpciaccess-static.
This is a followup to 20a8d796f1e8ed3f2ff3344fcf4ab40a45bc3942, which broke
the Hurd.

* gnu/packages/hurd.scm (libpciaccess-static): New variable.
(hurd, netdde) [inputs]: Replace libpciaccess with libpciaccess-static.

Change-Id: Ifb2cc3d210cf4ce6f4798eac1e9643f1c75ba6fa
Suggested-by: yelninei on IRC.

gnu/packages/hurd.scm
@@ -311,6 +311,17 @@ Hurd-minimal package which are needed for both glibc and GCC.")
 (define %add-to-hurd-subdirs
   (list "libmachdevdde" "libddekit"))
 
+;;; A static libpciaccess is required by hurd and netdde.
+(define libpciaccess-static
+  (package
+    (inherit libpciaccess)
+    (arguments
+     (substitute-keyword-arguments (package-arguments libpciaccess)
+       ((#:configure-flags flags)
+        #~(cons "-Ddefault_library=static" #$flags))))
+    (propagated-inputs
+     (list zlib (list zlib "static")))))
+
 (define-public hurd
   (package
     (name "hurd")
@@ -572,7 +583,7 @@ exec ${system}/rc \"$@\"
      `(("libgcrypt" ,libgcrypt)                  ;for /hurd/random
        ("libdaemon" ,libdaemon)                  ;for /bin/console --daemonize
        ("unifont" ,unifont)
-       ("libpciaccess" ,libpciaccess)
+       ("libpciaccess" ,libpciaccess-static)
 
        ;; For NFS support
        ("libtirpc" ,libtirpc/hurd)
@@ -700,7 +711,7 @@ implementing them.")
                  (install-file "netdde" hurd)
                  (install-file "netdde.static" hurd)))))))
       (inputs
-       (list hurd libpciaccess zlib `(,zlib "static")))
+       (list hurd libpciaccess-static zlib `(,zlib "static")))
       (native-inputs
        `(("coreutils" ,coreutils)
          ("gawk" ,gawk)

commit 9a6a7c9ea0328382e35f8df6d95fbb22e77c2c99

gnu: libpciaccess-static: Keep in same module as ‘libpciaccess’.
Fixes a bug introduced in beb9ad2cf7e83b747781b47cdde2f75a19cd3a1b where
‘guix pull’ would fail to build guix-system-tests.drv:

  gnu/packages/hurd.scm:317:13: error: libpciaccess: unbound variable

* gnu/packages/hurd.scm (libpciaccess-static): Move to…
* gnu/packages/xorg.scm (libpciaccess-static): … here.
Add ‘name’ field.

Change-Id: I4ec6a800360e82a5bc28f4e0b6c51772108fc3b1

gnu/packages/hurd.scm
@@ -311,17 +311,6 @@ Hurd-minimal package which are needed for both glibc and GCC.")
 (define %add-to-hurd-subdirs
   (list "libmachdevdde" "libddekit"))
 
-;;; A static libpciaccess is required by hurd and netdde.
-(define libpciaccess-static
-  (package
-    (inherit libpciaccess)
-    (arguments
-     (substitute-keyword-arguments (package-arguments libpciaccess)
-       ((#:configure-flags flags)
-        #~(cons "-Ddefault_library=static" #$flags))))
-    (propagated-inputs
-     (list zlib (list zlib "static")))))
-
 (define-public hurd
   (package
     (name "hurd")

gnu/packages/xorg.scm
@@ -1206,6 +1206,17 @@ themselves.")
     (description "Xorg Generic PCI access library.")
     (license license:x11)))
 
+;;; A static libpciaccess is required by hurd and netdde.
+(define-public libpciaccess-static
+  (package/inherit libpciaccess
+    (name "libpciaccess-static")
+    (arguments
+     (substitute-keyword-arguments (package-arguments libpciaccess)
+       ((#:configure-flags flags)
+        #~(cons "-Ddefault_library=static" #$flags))))
+    (propagated-inputs
+     (list zlib (list zlib "static")))))
+
 (define-public libpthread-stubs
   (package
     (name "libpthread-stubs")