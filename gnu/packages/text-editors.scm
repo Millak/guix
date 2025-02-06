01aed29f92c90464aa04bf7b173631c57282589c
Andy Tai <atai@atai.org>
2025-02-05 22:47:25 -0800

    gnu: ed: Update to 1.21.
    * gnu/packages/text-editors.scm (ed): Update to 1.21.
    
    Change-Id: If4adc3d92cf14b5bf5acbb43c1c9d65e15824139
    Signed-off-by: Zheng Junjie <z572@z572.online>

--- gnu/packages/text-editors.scm
+++ gnu/packages/text-editors.scm
@@ -133,14 +133,14 @@
 (define-public ed
   (package
     (name "ed")
-    (version "1.20.1")
+    (version "1.21")
     (source (origin
              (method url-fetch)
              (uri (string-append "mirror://gnu/ed/ed-"
                                  version ".tar.lz"))
              (sha256
               (base32
-               "1jmvpbs2mnrmk870js11v7g5qr3z8w0ws7sbdj3zjhd1jyr6795i"))))
+               "0xddpcdsdhf1cx7izk9l5dmf8vakjx5ncm1cy1f3libxfac4kqk0"))))
     (build-system gnu-build-system)
     (native-inputs (list lzip))
     (arguments
