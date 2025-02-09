36f57d0718b907325662b23fb1f1514a6d1bbd1d
Danny Milosavljevic <dannym@friendly-machines.com>
2025-02-09 17:43:26 +0100

    gnu: Add emacs-window-tool-bar.
    * gnu/packages/emacs-xyz.scm (emacs-window-tool-bar): New variable.
    
    Change-Id: Ic48813c85db25fe1d1272611d40465477777bc27

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -42509,6 +42509,45 @@ a vertical completion UI.")
 @code{transient} popups of @code{magit} in mind.")
       (license license:gpl3+))))
 
+(define-public emacs-window-tool-bar
+  (package
+    (name "emacs-window-tool-bar")
+    (version "0.3")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "https://elpa.gnu.org/packages/window-tool-bar-"
+                           version ".tar"))
+       (sha256
+        (base32 "00kggfpfi1nj05mzy5zig0fs4as7qh99wqgvya3xj2kw8141cvd6"))))
+    (build-system emacs-build-system)
+    (propagated-inputs (list emacs-compat))
+    (home-page "http://github.com/chaosemer/window-tool-bar")
+    (synopsis "Add tool bars inside Emacs windows")
+    (description
+     "This package puts a tool bar in each Emacs window.  This allows you to see
+multiple tool bars simultaneously directly next to the buffer it acts on which
+feels much more intuitive.
+Emacs \"browsing\" modes generally have sensible tool bars, for example:
+*info*, *help*, and *eww* have them.
+
+It does this while being mindful of screen real estate.
+If `tool-bar-map is nil, then this package will not take up any space for an
+empty tool bar.
+
+Most modes do not define a custom tool bar, so calling (setq tool-bar-map nil)
+in your init file will make most buffers not take up space for a tool bar.
+
+To get the default behavior, run (global-window-tool-bar-mode 1) or enable
+via M-x customize-group RET window-tool-bar RET.
+
+This uses the per-window tab line to show the tool bar.
+If you want to share space with an existing tab line, mode line, or header line,
+add (:eval (window-tool-bar-string)) to `tab-line-format', `mode-line-format',
+or `header-line-format'.  For additional documentation, see info node
+`(emacs)Window Tool Bar'.")
+    (license license:gpl3+)))
+
 (define-public emacs-tintin-mode
   (let ((commit "82e71e1db92ee3d94c7d0208bafc5de337193de8")
 	(revision "1"))
