27ee55714b364b38f48501ef226b1c0a3fdbe01a
Sharlatan Hellseher <sharlatanus@gmail.com>
2025-02-15 18:16:29 +0000

    gnu: go-gopkg-in-tomb-v1: Move to golang-xyz.
    * gnu/packages/golang.scm (go-gopkg-in-tomb-v1): Move from here ...
    * gnu/packages/golang-xyz.scm: ... to here.
    
    Change-Id: I9b5e36505b924c2ae69b95201c2d81312ae16310

--- gnu/packages/golang-xyz.scm
+++ gnu/packages/golang-xyz.scm
@@ -17488,6 +17488,37 @@ machine readable.  It is modeled after the Go standard library's @code{io} and
          (package-arguments go-github-com-op-go-logging)
        ((#:import-path _) "gopkg.in/op/go-logging.v1")))))
 
+(define-public go-gopkg-in-tomb-v1
+  (package
+    (name "go-gopkg-in-tomb-v1")
+    (version "1.0.0-20141024135613-dd632973f1e7")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://gopkg.in/tomb.v1")
+             (commit (go-version->git-ref version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "1lqmq1ag7s4b3gc3ddvr792c5xb5k6sfn0cchr3i2s7f1c231zjv"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:import-path "gopkg.in/tomb.v1"
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-after 'unpack 'fix-test
+            (lambda* (#:key import-path #:allow-other-keys)
+              (substitute* (string-append "src/" import-path "/tomb_test.go")
+                (("t.Fatalf\\(`Killf\\(\"BO%s")
+                 "t.Fatalf(`Killf(\"BO%%s")))))))
+    (home-page "https://gopkg.in/tomb.v1")
+    (synopsis "@code{tomb} handles clean goroutine tracking and termination")
+    (description
+     "The @code{tomb} package handles clean goroutine tracking and
+termination.")
+    (license license:bsd-3)))
+
 (define-public go-gopkg-in-vmihailenco-msgpack-v2
   (package
     (name "go-gopkg-in-vmihailenco-msgpack-v2")
--- gnu/packages/golang.scm
+++ gnu/packages/golang.scm
@@ -1618,37 +1618,6 @@ management interface.  It can be used to monitor and control an OpenVPN process
 running with its management port enabled.")
       (license license:expat))))
 
-(define-public go-gopkg-in-tomb-v1
-  (package
-    (name "go-gopkg-in-tomb-v1")
-    (version "1.0.0-20141024135613-dd632973f1e7")
-    (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                    (url "https://gopkg.in/tomb.v1")
-                    (commit (go-version->git-ref version))))
-              (file-name (git-file-name name version))
-              (sha256
-               (base32
-                "1lqmq1ag7s4b3gc3ddvr792c5xb5k6sfn0cchr3i2s7f1c231zjv"))))
-    (build-system go-build-system)
-    (arguments
-     (list #:import-path "gopkg.in/tomb.v1"
-           #:phases
-           #~(modify-phases %standard-phases
-               (add-after 'unpack 'fix-test
-                 (lambda* (#:key import-path #:allow-other-keys)
-                   (substitute* (string-append "src/" import-path
-                                               "/tomb_test.go")
-                     (("t.Fatalf\\(`Killf\\(\"BO%s")
-                      "t.Fatalf(`Killf(\"BO%%s")))))))
-    (home-page "https://gopkg.in/tomb.v1")
-    (synopsis "@code{tomb} handles clean goroutine tracking and termination")
-    (description
-     "The @code{tomb} package handles clean goroutine tracking and
-termination.")
-    (license license:bsd-3)))
-
 (define-public go-github-com-google-cadvisor
   (let ((commit "2ed7198f77395ee9a172878a0a7ab92ab59a2cfd")
         (revision "0"))
