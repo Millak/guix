a7ee9359da87c9624aed095ce575c168ff609801
Ricardo Wurmus <rekado@elephly.net>
2025-02-16 20:59:39 +0100

    gnu: r-biocneighbors: Add test input.
    * gnu/packages/bioconductor.scm (r-biocneighbors)[native-inputs]: Add
    r-biocparallel.
    
    Change-Id: Iae4a0be70263f60e4c8700047f15640760c12d62

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -16044,7 +16044,7 @@ samples.")
     (propagated-inputs
      (list r-assorthead r-rcpp))
     (native-inputs
-     (list r-knitr r-testthat))
+     (list r-biocparallel r-knitr r-testthat))
     (home-page "https://bioconductor.org/packages/BiocNeighbors")
     (synopsis "Nearest Neighbor Detection for Bioconductor packages")
     (description
