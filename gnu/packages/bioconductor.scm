0ccd8f222ad2c311383b25abd75e4b9f1a2cdf7d
Ricardo Wurmus <rekado@elephly.net>
2025-02-16 20:58:31 +0100

    gnu: r-mbkmeans: Add test input.
    * gnu/packages/bioconductor.scm (r-mbkmeans)[properties]: Record
    updater-extra-native-inputs.
    [native-inputs]: Add r-hdf5array.
    
    Change-Id: I719f38814ce5bd79a509db2b1daf9133501fdb69

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -14952,9 +14952,11 @@ before and after correcting for batch effects.")
               (sha256
                (base32
                 "0alm36lj0p541wyrk85ddyk8g4sscip54532i8pkb6a8mclp9cp3"))))
+    (properties
+     '((updater-extra-native-inputs . ("r-hdf5array"))))
     (build-system r-build-system)
     (native-inputs
-     (list r-knitr r-testthat))
+     (list r-hdf5array r-knitr r-testthat))
     (propagated-inputs
      (list r-beachmat
            r-benchmarkme
