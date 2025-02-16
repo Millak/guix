6f420c0febe855234b37974970c7dab0d6d7530e
Nicolas Graves <ngraves@ngraves.fr>
2025-02-16 01:33:48 +0100

    gnu: ruby-latex-decode: Remove uneeded ruby-ritex propagated-input.
    * gnu/packages/ruby.scm (ruby-latex-decode)[propagated-inputs]: Remove
    ruby-ritex.
    
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/ruby.scm
+++ gnu/packages/ruby.scm
@@ -17177,7 +17177,6 @@ into HTML pages easy.  It supports most TeX math syntax as well as macros.")
     (build-system ruby-build-system)
     (native-inputs
      (list ruby-cucumber
-           ruby-ritex
            ruby-rspec))
     (arguments
      (list
