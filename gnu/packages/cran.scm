26363c4e84f41b70ff2b0bfd6cbfae552003a210
Ricardo Wurmus <rekado@elephly.net>
2025-02-17 12:17:26 +0100

    gnu: r-lmodel2: Update to 1.7-4.
    * gnu/packages/cran.scm (r-lmodel2): Update to 1.7-4.
    
    Change-Id: I0f60aa76aaffa1165ac5cd00489893d95421498e

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -4323,13 +4323,13 @@ between a set of landmarks and the samples are calculated.")
 (define-public r-lmodel2
   (package
     (name "r-lmodel2")
-    (version "1.7-3")
+    (version "1.7-4")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "lmodel2" version))
        (sha256
-        (base32 "1fiin2nkffbihz1s8ixmw9lgf8mn3j9krr8iiflizk10mdv54hnb"))))
+        (base32 "1rclwaq2hqn6qpkzl686sikkwc5lrmbbdgpsf8qxwqkn2f0xia4f"))))
     (properties `((upstream-name . "lmodel2")))
     (build-system r-build-system)
     (home-page "https://cran.r-project.org/package=lmodel2")
