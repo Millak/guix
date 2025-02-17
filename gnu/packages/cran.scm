d916efc707493eff8833b7b6e9c25eca7b597b9a
Ricardo Wurmus <rekado@elephly.net>
2025-02-17 12:17:28 +0100

    gnu: r-secretbase: Update to 1.0.4.
    * gnu/packages/cran.scm (r-secretbase): Update to 1.0.4.
    
    Change-Id: I04063a1d792108d7d591f15002448728a648eb6e

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -6208,13 +6208,13 @@ scraping tasks, inspired by libraries like @code{BeautifulSoup}.")
 (define-public r-secretbase
   (package
     (name "r-secretbase")
-    (version "1.0.3")
+    (version "1.0.4")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "secretbase" version))
        (sha256
-        (base32 "0bmnbjnwwb7s697f9pjy905x8pj6jmsg4rldgmrzpbhazlzwxbn6"))))
+        (base32 "19n7d2f02a4iswmzgh8r7sq2gbbdq9ddwbh9d43b1ikslpsxwvz8"))))
     (properties `((upstream-name . "secretbase")))
     (build-system r-build-system)
     (arguments
