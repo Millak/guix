5f3e979c7cce1e9f9dd346131ff3c219d6b82682
Sharlatan Hellseher <sharlatanus@gmail.com>
2025-02-23 22:34:46 +0000

    gnu: go-github-com-git-lfs-pktline: 0.0.0-1.ca444d5.
    * gnu/packages/version-control.scm (go-github-com-git-lfs-pktline): 0.0.0-1.ca444d5.
    [propagated-inputs]: Remove go-github-com-stretchr-testify,
    go-github-com-pmezard-go-difflib, and go-github-com-davecgh-go-spew.
    [native-inputs]: Add go-github-com-stretchr-testify.
    
    Change-Id: I6741785f8d1a91c9b446c5d37f7948379b550b5e

--- gnu/packages/version-control.scm
+++ gnu/packages/version-control.scm
@@ -4042,8 +4042,8 @@ interrupted, published, and collaborated on while in progress.")
     (license license:gpl2+)))
 
 (define-public go-github-com-git-lfs-pktline
-  (let ((commit "06e9096e28253ba5c7825cbba43f469e4efd10f0")
-        (revision "0"))
+  (let ((commit "ca444d533ef1e474d0aab99cdbeed9b048d65241")
+        (revision "1"))
     (package
       (name "go-github-com-git-lfs-pktline")
       (version (git-version "0.0.0" revision commit))
@@ -4056,17 +4056,16 @@ interrupted, published, and collaborated on while in progress.")
            (commit commit)))
          (file-name (git-file-name name version))
          (sha256
-          (base32 "02sn3v8vrl7qjnagbnrbrjnyjvzq8cwkxmc922zyc9b2hg187kpz"))))
+          (base32 "0mxp4c59g5b3y20ilf90b6cagbk2b473vsmdz159y4hssvi1ac7r"))))
       (build-system go-build-system)
       (arguments `(#:import-path "github.com/git-lfs/pktline"))
-      (propagated-inputs (list go-github-com-stretchr-testify
-                               go-github-com-pmezard-go-difflib
-                               go-github-com-davecgh-go-spew))
+      (native-inputs
+       (list go-github-com-stretchr-testify))
       (home-page "https://github.com/git-lfs/pktline")
       (synopsis "Git pkt-line Go toolkit")
       (description "This package is a Go language toolkit for reading and
 writing files using the Git pkt-line format used in various Git operations.")
-(license license:expat))))
+      (license license:expat))))
 
 (define-public go-github-com-git-lfs-wildmatch-v2
   (package
