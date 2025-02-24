c2fd9bf11626000e1b3ebc217dcfc321c67c9341
Sharlatan Hellseher <sharlatanus@gmail.com>
2025-02-24 07:54:37 +0000

    gnu: git-lfs: Fix indentation.
    * gnu/packages/version-control.scm (git-lfs): Fix indentation.
    
    Change-Id: Ic5034d877a547369e5caa40ccad1a53574f698d6

--- gnu/packages/version-control.scm
+++ gnu/packages/version-control.scm
@@ -4123,31 +4123,31 @@ will reconstruct the object along its delta-base chain and return it.")
                  (search-input-file inputs "bin/sh")))))
           ;; Only build the man pages if ruby-asciidoctor is available.
           #$@(if (this-package-native-input "ruby-asciidoctor")
-               #~((add-before 'build 'man-gen
-                    ;; Without this, the binary generated in 'build
-                    ;; phase won't have any embedded usage-text.
-                    (lambda _
-                      (with-directory-excursion "src/github.com/git-lfs/git-lfs"
-                        (invoke "make" "mangen"))))
-                  (add-after 'build 'build-man-pages
-                    (lambda _
-                      (with-directory-excursion "src/github.com/git-lfs/git-lfs"
-                        (invoke "make" "man"))))
-                  (add-after 'install 'install-man-pages
-                    (lambda* (#:key outputs #:allow-other-keys)
-                      (with-directory-excursion "src/github.com/git-lfs/git-lfs/man"
-                        (for-each
-                         (lambda (manpage)
-                           (install-file manpage
-                                         (string-append #$output "/share/man/man1")))
-                         (find-files "." "^git-lfs.*\\.1$"))))))
-               #~()))))
+                 #~((add-before 'build 'man-gen
+                      ;; Without this, the binary generated in 'build
+                      ;; phase won't have any embedded usage-text.
+                      (lambda _
+                        (with-directory-excursion "src/github.com/git-lfs/git-lfs"
+                          (invoke "make" "mangen"))))
+                    (add-after 'build 'build-man-pages
+                      (lambda _
+                        (with-directory-excursion "src/github.com/git-lfs/git-lfs"
+                          (invoke "make" "man"))))
+                    (add-after 'install 'install-man-pages
+                      (lambda* (#:key outputs #:allow-other-keys)
+                        (with-directory-excursion "src/github.com/git-lfs/git-lfs/man"
+                          (for-each
+                           (lambda (manpage)
+                             (install-file manpage
+                                           (string-append #$output "/share/man/man1")))
+                           (find-files "." "^git-lfs.*\\.1$"))))))
+                 #~()))))
     ;; make `ronn` available during build for man page generation
     (native-inputs
      (append (list git-minimal)
              (if (supported-package? ruby-asciidoctor)
-               (list ronn-ng ruby-asciidoctor)
-               '())))
+                 (list ronn-ng ruby-asciidoctor)
+                 '())))
     (propagated-inputs
      (list go-github-com-avast-retry-go
            go-github-com-dpotapov-go-spnego
