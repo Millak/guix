9ef5446bb780280ae37f8214c90231905b3dc4e2
Ricardo Wurmus <rekado@elephly.net>
2025-02-27 17:05:18 +0100

    gnu: Add r-bsgenome-drerio-ucsc-danrer10.
    * gnu/packages/bioconductor.scm (r-bsgenome-drerio-ucsc-danrer10): New variable.
    
    Change-Id: I7d8ec96702e1d370547e91bb168f5621a87906d3

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -97,6 +97,29 @@ as provided by UCSC (@code{danRer7}, Jul. 2010) and stored in Biostrings
 objects.")
     (license license:artistic2.0)))
 
+(define-public r-bsgenome-drerio-ucsc-danrer10
+  (package
+    (name "r-bsgenome-drerio-ucsc-danrer10")
+    (version "1.4.2")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (bioconductor-uri "BSgenome.Drerio.UCSC.danRer10" version
+                              'annotation))
+       (sha256
+        (base32 "1grlxihdx65fwgb9gbp1jibbqan9a5667w4jgswhg0qsia608lzs"))))
+    (properties `((upstream-name . "BSgenome.Drerio.UCSC.danRer10")))
+    (build-system r-build-system)
+    (propagated-inputs (list r-bsgenome))
+    (home-page
+     "https://bioconductor.org/packages/BSgenome.Drerio.UCSC.danRer10")
+    (synopsis "Full genome sequences for Danio rerio (UCSC version danRer10)")
+    (description
+     "This package provides full genome sequences for Danio rerio (Zebrafish)
+as provided by UCSC (@code{danRer10}, Sep. 2014) and stored in
+@code{Biostrings} objects.")
+    (license license:artistic2.0)))
+
 (define-public r-bsgenome-hsapiens-ucsc-hg38-masked
   (package
     (name "r-bsgenome-hsapiens-ucsc-hg38-masked")
