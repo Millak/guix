9a2145042ddea6e4f2f3e759a4a1c477eebcdd4f
Liliana Marie Prikler <liliana.prikler@gmail.com>
2025-03-02 14:59:10 +0100

    gnu: emacs-elquery: Use implicit “ert-runner”.
    * gnu/packages/emacs-xyz.scm (emacs-elquery)[arguments]: Drop #:test-command.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -12575,7 +12575,6 @@ issues from Emacs.")
     (arguments
      (list
       #:emacs emacs-no-x
-      #:test-command #~(list "ert-runner")
       #:phases #~(modify-phases %standard-phases
                    (add-after 'unpack 'fix-native-compilation
                      (lambda _
