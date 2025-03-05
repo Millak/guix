20e352a12e43de0d094f916797e42308aad469af
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2025-03-05 21:13:12 +0900

    gnu: keepassxc: Fix indentation.
    * gnu/packages/password-utils.scm (keepassxc): Fix indentation.
    
    Change-Id: I807fe602177596a11b01aa4f740849f33a74789c

--- gnu/packages/password-utils.scm
+++ gnu/packages/password-utils.scm
@@ -243,12 +243,12 @@ human.")
      (list
       #:configure-flags
       #~(append
-          (list "-DWITH_XC_ALL=YES"
-                "-DWITH_XC_UPDATECHECK=NO")
-          #$(if (member (%current-system)
-                        (package-transitive-supported-systems ruby-asciidoctor))
-              #~'()
-              #~(list "-DWITH_XC_DOCS=NO")))
+         (list "-DWITH_XC_ALL=YES"
+               "-DWITH_XC_UPDATECHECK=NO")
+         #$(if (member (%current-system)
+                       (package-transitive-supported-systems ruby-asciidoctor))
+               #~'()
+               #~(list "-DWITH_XC_DOCS=NO")))
       #:phases
       #~(modify-phases %standard-phases
           (add-after 'unpack 'record-clipboard-programs
@@ -279,11 +279,11 @@ human.")
                 (invoke "ctest" "--exclude-regex" "testcli")))))))
     (native-inputs
      (append
-       (list qttools-5)
-       (if (member (%current-system)
-                   (package-transitive-supported-systems ruby-asciidoctor))
-         (list ruby-asciidoctor)
-         '())))
+      (list qttools-5)
+      (if (member (%current-system)
+                  (package-transitive-supported-systems ruby-asciidoctor))
+          (list ruby-asciidoctor)
+          '())))
     (inputs
      (list argon2
            botan
