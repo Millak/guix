5380b35772eb548850d28d42074735233069d523
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2025-03-07 10:22:52 +0100

    gnu: emacs-auctex: Update to 14.0.9.
    * gnu/packages/emacs-xyz.scm (emacs-auctex): Update to 14.0.9.
    
    Change-Id: I07743036332363060963ecd44fbeaa36d514ff95

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -5018,14 +5018,14 @@ as a library for other Emacs packages.")
 (define-public emacs-auctex
   (package
     (name "emacs-auctex")
-    (version "14.0.8")
+    (version "14.0.9")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://elpa.gnu.org/packages/"
                            "auctex-" version ".tar"))
        (sha256
-        (base32 "0bcjkbwhbkmm0r7pbh44j7vw9b39g2iw1jgw4sq54qp7387j6lmy"))))
+        (base32 "1mhzrqln0fjj9nn04dhgzpcyv2wzysz4l85wmp4fw620jksf8j7n"))))
     (build-system emacs-build-system)
     ;; We use 'emacs' because AUCTeX requires dbus at compile time
     ;; ('emacs-minimal' does not provide dbus).
