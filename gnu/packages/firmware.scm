375aa8d2daa75da7377d56b78047a6ce58777df7
Danny Milosavljevic <dannym@friendly-machines.com>
2025-03-07 23:42:31 +0100

    gnu: Add qmk-firmware-keychron-q4-ansi-default.
    * gnu/packages/firmware.scm (qmk-firmware-keychron-q4-ansi-default):
    New variable.
    
    Change-Id: I8215ae0a707841ae4b48b401c9c44a8cc609d548

--- gnu/packages/firmware.scm
+++ gnu/packages/firmware.scm
@@ -2024,6 +2024,11 @@ or passthrough board.")
    #:description
    "This package provides the firmware for the Keychron Q3 JIS with default keymap and with encoder."))
 
+(define-public qmk-firmware-keychron-q4-ansi-default
+  (make-qmk-firmware-keychron "keychron/q4/ansi" "default"
+   #:description
+   "This package provides the firmware for the Keychron Q4 ANSI with default keymap."))
+
 (define-public qmk-firmware-keychron-v3-ansi-default
   (make-qmk-firmware-keychron "keychron/v3/ansi" "default"
    #:description
