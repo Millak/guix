6694d19b5dc80cfb2e7c4d887de5fbf7db867296
Danny Milosavljevic <dannym@friendly-machines.com>
2025-03-07 23:54:55 +0100

    gnu: Add qmk-firmware-keychron-v5-ansi-encoder-default.
    * gnu/packages/firmware.scm (qmk-firmware-keychron-v5-ansi-encoder-default):
    New variable.
    
    Change-Id: Idc89d44c1bc4cf110c37f5afb25b94a863303343

--- gnu/packages/firmware.scm
+++ gnu/packages/firmware.scm
@@ -2264,6 +2264,11 @@ or passthrough board.")
    #:description
    "This package provides the firmware for the Keychron V5 ANSI with default keymap."))
 
+(define-public qmk-firmware-keychron-v5-ansi-encoder-default
+  (make-qmk-firmware-keychron "keychron/v5/ansi_encoder" "default"
+   #:description
+   "This package provides the firmware for the Keychron V5 ANSI with default keymap and with encoder."))
+
 (define-public firmware-senoko
   (package
     (name "firmware-senoko")
