6decb7c86c9183d26382de889ea33140ab672cd3
Danny Milosavljevic <dannym@friendly-machines.com>
2025-03-07 23:41:29 +0100

    gnu: Add qmk-firmware-keychron-q3-iso-encoder-default.
    * gnu/packages/firmware.scm (qmk-firmware-keychron-q3-iso-encoder-default):
    New variable.
    
    Change-Id: I2e7b7a8cfda3544c22629e77b34941092e7ce8f2

--- gnu/packages/firmware.scm
+++ gnu/packages/firmware.scm
@@ -2009,6 +2009,11 @@ or passthrough board.")
    #:description
    "This package provides the firmware for the Keychron Q3 ISO with default keymap."))
 
+(define-public qmk-firmware-keychron-q3-iso-encoder-default
+  (make-qmk-firmware-keychron "keychron/q3/iso_encoder" "default"
+   #:description
+   "This package provides the firmware for the Keychron Q3 ISO with default keymap and with encoder."))
+
 (define-public qmk-firmware-keychron-v3-ansi-default
   (make-qmk-firmware-keychron "keychron/v3/ansi" "default"
    #:description
