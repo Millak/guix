afd5eea43d912bd536ddea1754ec1ce1b92e3d8f
Danny Milosavljevic <dannym@friendly-machines.com>
2025-03-07 23:43:27 +0100

    gnu: Add qmk-firmware-keychron-q5-iso-default.
    * gnu/packages/firmware.scm (qmk-firmware-keychron-q5-iso-default):
    New variable.
    
    Change-Id: Ib72c4b0d3262ebc1cfd60af44ae9618bf7f5e839

--- gnu/packages/firmware.scm
+++ gnu/packages/firmware.scm
@@ -2044,6 +2044,11 @@ or passthrough board.")
    #:description
    "This package provides the firmware for the Keychron Q5 ANSI with default keymap and with encoder."))
 
+(define-public qmk-firmware-keychron-q5-iso-default
+  (make-qmk-firmware-keychron "keychron/q5/iso" "default"
+   #:description
+   "This package provides the firmware for the Keychron Q5 ISO with default keymap."))
+
 (define-public qmk-firmware-keychron-v3-ansi-default
   (make-qmk-firmware-keychron "keychron/v3/ansi" "default"
    #:description
