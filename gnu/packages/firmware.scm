bf1df3f9ac085f98a8d4a58f38370e6b55c4de4e
Danny Milosavljevic <dannym@friendly-machines.com>
2025-03-07 23:44:31 +0100

    gnu: Add qmk-firmware-keychron-q6-ansi-default.
    * gnu/packages/firmware.scm (qmk-firmware-keychron-q6-ansi-default):
    New variable.
    
    Change-Id: Ic7586c1dc0b79c5c6ba872d3491e259b692e07a4

--- gnu/packages/firmware.scm
+++ gnu/packages/firmware.scm
@@ -2064,6 +2064,11 @@ or passthrough board.")
    #:description
    "This package provides the firmware for the Keychron Q65 ANSI with default keymap and with encoder."))
 
+(define-public qmk-firmware-keychron-q6-ansi-default
+  (make-qmk-firmware-keychron "keychron/q6/ansi" "default"
+   #:description
+   "This package provides the firmware for the Keychron Q6 ANSI with default keymap."))
+
 (define-public qmk-firmware-keychron-v3-ansi-default
   (make-qmk-firmware-keychron "keychron/v3/ansi" "default"
    #:description
