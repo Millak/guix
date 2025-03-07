88f20e34275e8021178a43c1d5ae090f9e84315d
Nicolas Graves <ngraves@ngraves.fr>
2025-03-07 14:07:57 +0100

    gnu: zchunk: Update to 1.5.1.
    * gnu/packages/compression.scm (zchunk): Update to 1.5.1.
    
    Signed-off-by: Zheng Junjie <z572@z572.online>

--- gnu/packages/compression.scm
+++ gnu/packages/compression.scm
@@ -2546,7 +2546,7 @@ reading from and writing to ZIP archives.")
 (define-public zchunk
   (package
     (name "zchunk")
-    (version "1.3.1")
+    (version "1.5.1")
     (source (origin
               (method git-fetch)
               (uri (git-reference
@@ -2555,7 +2555,7 @@ reading from and writing to ZIP archives.")
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "19rw870150w1c730wzg2pn68ixmscq8cwa3vricqhwxs5l63r5wr"))))
+                "08ngp6d54fllk19qhdz6r1s9832781g322gn7524akbr3v1v5jjz"))))
     (build-system meson-build-system)
     (arguments
      (list
