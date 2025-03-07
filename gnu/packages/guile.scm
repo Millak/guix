Millak/guix

commit 98f894e05fdbc6bc90887820b0abc2a443a8b676

gnu: guile-next: Update to 3.0.10-0.5d3f561.
* gnu/packages/guile.scm (guile-next): Update to 3.0.10-0.5d3f561.

Change-Id: I07f5fb59327940d8c797770aff7b825788d6315b

gnu/packages/guile.scm
@@ -480,9 +480,9 @@ without requiring the source code to be rewritten.")
                                                 ;  when heavily loaded)
 
 (define-public guile-next
-  (let ((version "3.0.9")
+  (let ((version "3.0.10")
         (revision "0")
-        (commit "3b76a30e3ca1f0b7ee7944836c2fc5660596b3bd"))
+        (commit "5d3f561d7dbcee370dc764cd5ba4210c62ce13de"))
     (package
       (inherit guile-3.0)
       (name "guile-next")
@@ -496,7 +496,7 @@ without requiring the source code to be rewritten.")
                 (file-name (git-file-name name version))
                 (sha256
                  (base32
-                  "1cgyjz1x8zpfhsw8gsqkak1lnmi780gps6874ks0bi8dwk0lwx6f"))))
+                  "1k2brrff9ygikcm7s6bp5yii72a0jihxd67wgszzqkwbjhyk9acb"))))
       (arguments
        (substitute-keyword-arguments (package-arguments guile-3.0)
          ((#:phases phases '%standard-phases)
@@ -511,9 +511,6 @@ without requiring the source code to be rewritten.")
                   (call-with-output-file ".tarball-version"
                     (lambda (port)
                       (display #$version port)))))
-              (add-before 'check 'skip-failing-tests
-                (lambda _
-                  (delete-file "test-suite/tests/version.test")))
               #$@(if (target-hurd?)
                      #~((delete 'patch-posix-spawn-usage))
                      #~())))))

commit 535efb3c9add7e32530aa0dced40b18b12b8ed95

gnu: guile-for-guile-emacs: Fix build.
Got broken in commit 98f894e05fdbc6bc90887820b0abc2a443a8b676 which updated
guile-next and removed a no longer needed phase.  This package still needed
that phase though.

* gnu/packages/guile.scm (guile-for-guile-emacs)[arguments]: Add phase to
disable a failing test.

Change-Id: Ia45e33f65187e3fdb02dc6a9560d04479e32a017
Signed-off-by: Zheng Junjie <z572@z572.online>

gnu/packages/guile.scm
@@ -625,7 +625,14 @@ GNU@tie{}Guile.  Use the @code{(ice-9 readline)} module and call its
                 (file-name (git-file-name name version))
                 (sha256
                  (base32
-                  "0fgkcv29581kqkxqq6y48xly72970qs7016qhs6c4ilygg0gyfqb")))))))
+                  "0fgkcv29581kqkxqq6y48xly72970qs7016qhs6c4ilygg0gyfqb"))))
+      (arguments
+       (substitute-keyword-arguments (package-arguments guile-next)
+         ((#:phases phases '%standard-phases)
+          #~(modify-phases #$phases
+              (add-before 'check 'skip-failing-tests
+                (lambda _
+                  (delete-file "test-suite/tests/version.test"))))))))))
 
 
 ;;;