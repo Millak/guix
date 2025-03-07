35730a1aa2d00dde15da663335dd117c550bf5bf
Danny Milosavljevic <dannym@friendly-machines.com>
2025-03-07 23:50:16 +0100

    gnu: Add qmk-firmware-keychron-v10-ansi-encoder-default.
    * gnu/packages/firmware.scm (qmk-firmware-keychron-v10-ansi-encoder-default):
    New variable.
    
    Change-Id: I880e9b322ba1d987b8c8db6b755719472dd53a70

--- gnu/packages/firmware.scm
+++ gnu/packages/firmware.scm
@@ -2149,6 +2149,11 @@ or passthrough board.")
    #:description
    "This package provides the firmware for the Keychron S1 ANSI white with default keymap."))
 
+(define-public qmk-firmware-keychron-v10-ansi-encoder-default
+  (make-qmk-firmware-keychron "keychron/v10/ansi_encoder" "default"
+   #:description
+   "This package provides the firmware for the Keychron V10 ANSI with default keymap and with encoder."))
+
 (define-public qmk-firmware-keychron-v3-ansi-default
   (make-qmk-firmware-keychron "keychron/v3/ansi" "default"
    #:description
