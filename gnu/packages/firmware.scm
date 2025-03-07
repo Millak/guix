0b0bede7b391a3a6ae5d5f75ac36c52134e98b12
Danny Milosavljevic <dannym@friendly-machines.com>
2025-03-07 23:57:43 +0100

    gnu: Add qmk-firmware-keychron-v8-iso-encoder-default.
    * gnu/packages/firmware.scm (qmk-firmware-keychron-v8-iso-encoder-default):
    New variable.
    
    Change-Id: I4fc6e0e058923aef245e15a695ed3b582522fa6d

--- gnu/packages/firmware.scm
+++ gnu/packages/firmware.scm
@@ -2324,6 +2324,11 @@ or passthrough board.")
    #:description
    "This package provides the firmware for the Keychron V8 ISO with default keymap."))
 
+(define-public qmk-firmware-keychron-v8-iso-encoder-default
+  (make-qmk-firmware-keychron "keychron/v8/iso_encoder" "default"
+   #:description
+   "This package provides the firmware for the Keychron V8 ISO with default keymap and with encoder."))
+
 (define-public firmware-senoko
   (package
     (name "firmware-senoko")
