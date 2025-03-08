7bea040382401ea3a985ab8cf70f94f0df18de6d
Arun Isaac <arunisaac@systemreboot.net>
2025-03-08 19:57:57 +0000

    gnu: mumi: Update to 0.6.0.
    * gnu/packages/mail.scm (mumi): Update to 0.6.0.
    [inputs]: Remove guile-fibers.

--- gnu/packages/mail.scm
+++ gnu/packages/mail.scm
@@ -4265,7 +4265,7 @@ It is a replacement for the @command{urlview} program.")
 (define-public mumi
   (package
     (name "mumi")
-    (version "0.5.0")
+    (version "0.6.0")
     (source (origin
               (method git-fetch)
               (uri (git-reference
@@ -4274,7 +4274,7 @@ It is a replacement for the @command{urlview} program.")
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "0r9kbxn97si74swppfyznn3s17ng9x1clq6w9fi9m1ml2blmi5dv"))))
+                "056amwn59wfl8vzl3vfxf6hizyrxlvnsv6bsry4q0zv1gy4q9rs0"))))
     (build-system gnu-build-system)
     (arguments
      (list
@@ -4310,7 +4310,6 @@ It is a replacement for the @command{urlview} program.")
      (list bash-minimal
            guile-avatar
            guile-email
-           guile-fibers
            guile-gcrypt
            guile-gnutls
            guile-kolam
