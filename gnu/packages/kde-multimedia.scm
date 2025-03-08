e981ad758b5fd54891006ce3ed8a4baecfe9ae11
Zheng Junjie <z572@z572.online>
2025-03-08 13:54:50 +0800

    gnu: elisa: Update to 24.12.3.
    * gnu/packages/kde-multimedia.scm (elisa): Update to 24.12.3.
    
    Change-Id: If3a157bfca8012e482143fcdac7798aa6f964a84

--- gnu/packages/kde-multimedia.scm
+++ gnu/packages/kde-multimedia.scm
@@ -195,14 +195,14 @@ This package is part of the KDE multimedia module.")
 (define-public elisa
   (package
     (name "elisa")
-    (version "24.12.1")
+    (version "24.12.3")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://kde/stable/release-service/" version
                            "/src/elisa-" version ".tar.xz"))
        (sha256
-        (base32 "0af2n1ji7qjsi8yi5hqlx0s9dv0zq9qhqxvrx5h5r3d2p123k3s5"))))
+        (base32 "1hzzi12j8kz04psdd8w0iqib50lyninni3qrqlx17chwnhysn6ax"))))
     (build-system qt-build-system)
     (native-inputs
      (list extra-cmake-modules pkg-config dbus kdoctools
