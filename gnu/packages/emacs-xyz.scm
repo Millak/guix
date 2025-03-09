44fe00d02a2fffbef89ed73cd225a68c712e5e05
Liliana Marie Prikler <liliana.prikler@gmail.com>
2025-03-09 14:39:26 +0100

    gnu: emacs-emms: Add missing test inputs.
    * gnu/packages/emacs-xyz.scm (emacs-emms)[native-inputs]: Add
    ‘emacs-ert-runner’.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -4393,7 +4393,8 @@ light user interface.")
            mp3info
            python-mutagen
            perl-image-exiftool
-           opus-tools))))
+           opus-tools))
+    (native-inputs (list emacs-ert-runner))))
 
 (define-public emacs-emms-mode-line-cycle
   (package
