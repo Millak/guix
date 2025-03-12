758702209e9b17fa6da63f9df664d1755a148289
Liliana Marie Prikler <liliana.prikler@gmail.com>
2025-03-12 09:30:35 +0100

    gnu: emacs-casual-calc: Skip tests.
    * gnu/packages/emacs-xyz.scm (emacs-casual-calc)[arguments]: Add #:tests? #f.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -36052,7 +36052,9 @@ all of your projects, then override or add variables on a per-project basis.")
     (build-system emacs-build-system)
     (arguments
      (list
-      #:lisp-directory "lisp"))
+      #:lisp-directory "lisp"
+      ;; Tests require “casual-lib-test-utils”, which are not installed.
+      #:tests? #f))
     (propagated-inputs (list emacs-casual-lib))
     (home-page "https://github.com/kickingvegas/casual-calc")
     (synopsis "Transient-based porcelain for calc")
