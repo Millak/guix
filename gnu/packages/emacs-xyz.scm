e2658f2ec12ee06481ae89c6de0ca8102ad79da4
Liliana Marie Prikler <liliana.prikler@gmail.com>
2025-03-13 20:42:56 +0100

    gnu: emacs-consult-eglot: Skip tests.
    * gnu/packages/emacs-xyz.scm (emacs-consult-eglot)[arguments]: Add #:tests? #f.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -14268,6 +14268,7 @@ versions utilizing Consult's internal API.")
                 "1xx5g4z4l0kanf2mh3f798gw8ydfzbx15wfyqrnhwhiljz796xis"))
               (file-name (git-file-name name version))))
     (build-system emacs-build-system)
+    (arguments (list #:tests? #f))      ; no tests
     (propagated-inputs (list emacs-consult emacs-eglot))
     (home-page "https://github.com/mohkale/consult-eglot")
     (synopsis "Consulting-read interface for eglot")
