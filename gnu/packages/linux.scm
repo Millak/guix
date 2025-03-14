f366441a0e91e9d8709e0d783cd2dd1d5fe68a44
Leo Famulari <leo@famulari.name>
2025-03-14 14:54:49 -0400

    gnu: linux-libre 5.10: Update to 5.10.235.
    * gnu/packages/linux.scm (linux-libre-5.10-version): Update to 5.10.235.
    (linux-libre-5.10-pristine-source): Update hash.
    
    Change-Id: I489c423c86a270c4df19db2675e8c7d5eaf47b78

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -592,7 +592,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
                             (%upstream-linux-source version hash)
                             deblob-scripts-5.15)))
 
-(define-public linux-libre-5.10-version "5.10.234")
+(define-public linux-libre-5.10-version "5.10.235")
 (define-public linux-libre-5.10-gnu-revision "gnu1")
 (define deblob-scripts-5.10
   (linux-libre-deblob-scripts
@@ -602,7 +602,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
    (base32 "14jdl5rk3w7h97svl6j6ngbywj5qixmpv6ifhqkzjxpp07xngvs4")))
 (define-public linux-libre-5.10-pristine-source
   (let ((version linux-libre-5.10-version)
-        (hash (base32 "1rgb4v6dvqlw1mgzsli0hxaj2d5d4m1nylgcrwm4bkpiwbzc95wm")))
+        (hash (base32 "1k7iq4np3pflkq3d71ya8xs5czhslhy2iha4ls9lma81269y6fwm")))
    (make-linux-libre-source version
                             (%upstream-linux-source version hash)
                             deblob-scripts-5.10)))
