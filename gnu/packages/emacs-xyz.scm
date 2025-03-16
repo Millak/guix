8b762e556b3db3963cef34fb074122619388e6de
Liliana Marie Prikler <liliana.prikler@gmail.com>
2025-03-16 16:43:31 +0100

    gnu: emacs-jabber: Fix tests.
    * gnu/packages/emacs-xyz.scm (emacs-jabber)[arguments]: Add #:test-command.
    [native-inputs]: Add emacs-ert-runner.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -22848,6 +22848,7 @@ with Eglot.")
       (arguments
        (list
         #:emacs emacs                   ;requires gnutls
+        #:test-command #~(list "ert-runner" "tests")
         #:phases
         #~(modify-phases %standard-phases
             (add-after 'build 'make-info
@@ -22855,7 +22856,7 @@ with Eglot.")
                 (invoke "makeinfo" "jabber.texi")
                 (install-file "jabber.info"
                               (string-append #$output "/share/info")))))))
-      (native-inputs (list texinfo))
+      (native-inputs (list emacs-ert-runner texinfo))
       (propagated-inputs (list emacs-fsm emacs-hexrgb emacs-srv gnutls))
       (home-page "https://codeberg.org/emacs-jabber/emacs-jabber")
       (synopsis "XMPP (Jabber) client for Emacs")
