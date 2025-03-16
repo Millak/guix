86f6e8af4f6862416331301a96d7aa2aceecf016
Liliana Marie Prikler <liliana.prikler@gmail.com>
2025-03-16 08:00:23 +0100

    gnu: emacs-elpher: Skip tests.
    * gnu/packages/emacs-xyz.scm (emacs-elpher)[arguments]: Add #:tests? #f.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -37373,6 +37373,7 @@ it forcibly
     (arguments
      (list
       #:emacs emacs-no-x                ;need libxml support
+      #:tests? #f                       ;no tests
       #:phases
       #~(modify-phases %standard-phases
           (add-before 'install 'build-doc
