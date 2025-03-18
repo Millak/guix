Millak/guix

commit 766ad072ad90d4ffc161cb83e29910b7bb3a9d54

services: configuration: Document multiple configurations per field.
* gnu/services/configuration.scm (generate-documentation): Filter for all
configurations associated with each field-name.
* doc/guix.texi (Complex Configurations): Describe use case for multiple
field-names.

Change-Id: I6d1e7b8858231aa05fda884b31a9bd0a48f07293
Signed-off-by: Ludovic Courtès <ludo@gnu.org>

gnu/services/configuration.scm
@@ -7,6 +7,7 @@
 ;;; Copyright © 2021 Andrew Tropin <andrew@trop.in>
 ;;; Copyright © 2022 Maxime Devos <maximedevos@telenet.be>
 ;;; Copyright © 2023 Bruno Victal <mirai@makinata.eu>
+;;; Copyright © 2024 Herman Rimm <herman@rimm.ee>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -460,8 +461,12 @@ DEFAULT."
                            (para ,@field-docs)
                            ,@(append-map
                               generate
-                              (or (assq-ref sub-documentation field-name)
-                                  '())))))
+                              (filter-map
+                                (match-lambda
+                                  ((name config)
+                                   (and (eq? name field-name)
+                                        config)))
+                                sub-documentation)))))
                fields)))))))
   (stexi->texi `(*fragment* . ,(generate documentation-name))))
 

doc/guix.texi
@@ -44702,7 +44702,9 @@ available for the configuration record.
 @var{sub-documentation} is a @code{(@var{field-name}
 @var{configuration-name})} tuple.  @var{field-name} is the name of the
 field which takes another configuration record as its value, and
-@var{configuration-name} is the name of that configuration record.
+@var{configuration-name} is the name of that configuration record.  The
+same value may be used for multiple @var{field-name}s, in case a field
+accepts different types of configurations.
 
 @var{sub-documentation} is only needed if there are nested configuration
 records.  For example, the @code{getmail-configuration} record

commit 8b6e9acc268501223a231d50932192f6bb69d34d

services/configuration: Fix case where sub-documentation is nonexistent.
Commit 766ad072ad90d4ffc161cb83e29910b7bb3a9d54 introduced a regression
where (configuration->documentation 'libvirt-configuration) would raise:

  (configuration->documentation 'libvirt-configuration)
  ice-9/boot-9.scm:1685:16: In procedure raise-exception:
  In procedure symbol->string: Wrong type argument in position 1 (expecting symbol): #f

* gnu/services/configuration.scm (generate-documentation): Re-introduce
previous handling of sub-documentation.

Change-Id: If2dbb59db5d22da221abfa2ffe7889854528a49c

gnu/services/configuration.scm
@@ -462,11 +462,12 @@ DEFAULT."
                            ,@(append-map
                               generate
                               (filter-map
-                                (match-lambda
-                                  ((name config)
-                                   (and (eq? name field-name)
-                                        config)))
-                                sub-documentation)))))
+                               (match-lambda
+                                 ((name config)
+                                  (and (eq? name field-name)
+                                       config)))
+                               (or (assq-ref sub-documentation field-name)
+                                   '()))))))
                fields)))))))
   (stexi->texi `(*fragment* . ,(generate documentation-name))))
 