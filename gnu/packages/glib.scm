Millak/guix

commit 17d9803374657ef4ed7cf26b8695805c5790df6c

gnu: gobject-introspection: Update to 1.82.0.
* gnu/packages/glib.scm (gobject-introspection): Update to 1.82.0.

Reviewed-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>

gnu/packages/glib.scm
@@ -612,14 +612,14 @@ be used when cross-compiling."
 (define gobject-introspection
   (package
     (name "gobject-introspection")
-    (version "1.78.1")
+    (version "1.82.0")
     (source (origin
              (method url-fetch)
              (uri (string-append "mirror://gnome/sources/"
                    "gobject-introspection/" (version-major+minor version)
                    "/gobject-introspection-" version ".tar.xz"))
              (sha256
-              (base32 "1d0vhi83q0xc7kg3zn32wy7n16f3dd5blicyh5v8w9gpkbcsnyxx"))
+              (base32 "029gr80q8749dhcpmf5x1w48adinihb634qyqimz4js210clqnhg"))
              (patches (search-patches
                        "gobject-introspection-cc.patch"
                        "gobject-introspection-girepository.patch"

commit 6a6b1dbe01dd7bd0cf976b64a6b1b3b48ce5d608

gnu: cppgir-for-telegram-desktop: Update to 2e96cab to fix telegram.
This fixes the telegram-desktop build, which was broken since in commit
17d9803374 ("gnu: gobject-introspection: Update to 1.82.0").
* gnu/packages/glib.scm (cppgir-for-telegram-desktop): Update to the latest
commit, 2e96cab8ed40df326815b87b1e4b449e0c1a5947.

Change-Id: I336168d49c5ac96ce5205f9824ce42701edb058c
Signed-off-by: Maxim Cournoyer <maxim.cournoyer@gmail.com>

gnu/packages/glib.scm
@@ -1649,7 +1649,7 @@ namespaces, classes and other types that together form a C++ binding.")
 
 ;; telegram-desktop requires a more recent version of cppgir
 (define-public cppgir-for-telegram-desktop
-  (let ((commit "9c4f5820d94d62ab451501f016bfea97156518f4")
+  (let ((commit "2e96cab8ed40df326815b87b1e4b449e0c1a5947")
         (revision "0"))
     (package
       (inherit cppgir)
@@ -1664,4 +1664,4 @@ namespaces, classes and other types that together form a C++ binding.")
            (commit commit)))
          (file-name (git-file-name name version))
          (sha256
-          (base32 "1fa9nf4yljfarihaqj5kd98yysrky7q316mh6l5b1rq39ga15k9b")))))))
+          (base32 "0rdcgnriw8s5fqyx2v4218ii647l4fl1s9crnm9ihzf9bpl2p5p9")))))))