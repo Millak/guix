38b2fe74e5d859a08d1b41c0eb4ad3e1b346f75e
Steve George <steve@futurile.net>
2025-03-24 23:04:18 +0000

    gnu: Add rust-tree-sitter-c-0.23.
    * gnu/packages/tree-sitter.scm (rust-tree-sitter-c-0.23): New variable.
    
    Change-Id: Ib916019c51c766523d3c253703b8b6c47a4b638e
    Signed-off-by: Steve George <steve@futurile.net>

--- gnu/packages/tree-sitter.scm
+++ gnu/packages/tree-sitter.scm
@@ -122,6 +122,27 @@ Tree-sitter parsing library.")
     (description "This package provides a Bash grammar for tree-sitter.")
     (license license:expat)))
 
+(define-public rust-tree-sitter-c-0.23
+  (package
+    (name "rust-tree-sitter-c")
+    (version "0.23.4")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "tree-sitter-c" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0wcwdvp8k9qsyfb5zpa9cq05kc5dp0fx11wysvv2xp452nzv3lmg"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs (("rust-cc" ,rust-cc-1)
+                       ("rust-tree-sitter-language" ,rust-tree-sitter-language-0.1))
+       #:cargo-development-inputs (("rust-tree-sitter" ,rust-tree-sitter-0.24))))
+    (home-page "https://github.com/tree-sitter/tree-sitter-c")
+    (synopsis "C grammar for tree-sitter")
+    (description "This package provides C grammar for tree-sitter.")
+    (license license:expat)))
+
 (define-public tree-sitter
   (package
     (name "tree-sitter")
