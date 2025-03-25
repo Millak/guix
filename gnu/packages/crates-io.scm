c1040adb7d7f990dc858ab69863fb1fff2ee2441
Steve George <steve@futurile.net>
2025-03-25 13:59:00 +0000

    gnu: Add rust-wit-parser-0.215.
    * gnu/packages/crates-io.scm (rust-wit-parser-0.215): New variable.
    
    Change-Id: I4e3c7b89c23a51f55b2b2849d6d7c9dc553bc687
    Signed-off-by: Steve George <steve@futurile.net>

--- gnu/packages/crates-io.scm
+++ gnu/packages/crates-io.scm
@@ -99384,6 +99384,40 @@ file format.")
      "This package provides tooling for parsing and handling wit files.")
     (license (list license:asl2.0 license:asl2.0 license:expat))))
 
+(define-public rust-wit-parser-0.215
+  (package
+    (inherit rust-wit-parser-0.217)
+    (name "rust-wit-parser")
+    (version "0.215.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "wit-parser" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "07rjc2wdr0wsp3c9rrsrgpzak92hnpqgh455790knz6mzzm9fnlk"))))
+    (arguments
+     `(#:cargo-test-flags
+         '("--release" "--"
+           "--skip=tests/ui/kinds-of-deps"
+           "--skip=tests/ui/simple-wasm-text")
+       #:cargo-inputs (("rust-anyhow" ,rust-anyhow-1)
+                       ("rust-id-arena" ,rust-id-arena-2)
+                       ("rust-indexmap" ,rust-indexmap-2)
+                       ("rust-log" ,rust-log-0.4)
+                       ("rust-semver" ,rust-semver-1)
+                       ("rust-serde" ,rust-serde-1)
+                       ("rust-serde-derive" ,rust-serde-derive-1)
+                       ("rust-serde-json" ,rust-serde-json-1)
+                       ("rust-unicode-xid" ,rust-unicode-xid-0.2)
+                       ("rust-wasmparser" ,rust-wasmparser-0.215)
+                       ("rust-wat" ,rust-wat-1))
+       #:cargo-development-inputs
+         (("rust-env-logger" ,rust-env-logger-0.11)
+          ("rust-libtest-mimic" ,rust-libtest-mimic-0.7)
+          ("rust-pretty-assertions" ,rust-pretty-assertions-1)
+          ("rust-serde-json" ,rust-serde-json-1))))))
+
 (define-public rust-wl-clipboard-rs-0.8
   (package
     (name "rust-wl-clipboard-rs")
