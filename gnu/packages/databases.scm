5737570c2a06e16f08283ce2c32055150018468e
Sharlatan Hellseher <sharlatanus@gmail.com>
2025-03-27 12:39:06 +0000

    gnu: python-fastparquet: Update to 2024.11.0, fix build.
    * gnu/packages/databases.scm (python-fastparquet): Update to 2024.11.0.
    [native-inputs]: Remove python-pytest-runner; add python-pytest, python-setuptools,
    and python-wheel.
    
    Change-Id: I4a798972077cd1646e48cea28b4bc369a7a68fd1

--- gnu/packages/databases.scm
+++ gnu/packages/databases.scm
@@ -5364,7 +5364,7 @@ other traditional Python scientific computing packages.")
 (define-public python-fastparquet
   (package
     (name "python-fastparquet")
-    (version "2024.2.0")
+    (version "2024.11.0")
     (source
      (origin
        ;; Fastparquet uses setuptools-scm to find the current version. This
@@ -5377,7 +5377,7 @@ other traditional Python scientific computing packages.")
              (commit version)))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "0f32dj1xvd11l0siznqd33dpjlhg9siylcjcfkcdlqfcy45jfj3v"))))
+        (base32 "0dhmyag06d073g1q58npbcikr9hjd6jgf05721gkl6m1gsprv7hq"))))
     (build-system pyproject-build-system)
     (arguments
      (list
@@ -5403,6 +5403,13 @@ other traditional Python scientific computing packages.")
             ;; Cython extensions need to be built for the check phase.
             (lambda _
               (invoke "python" "setup.py" "build_ext" "--inplace"))))))
+    (native-inputs
+     (list python-cython
+           python-pytest
+           python-pytest-xdist
+           python-setuptools
+           python-setuptools-scm
+           python-wheel))
     (propagated-inputs
      (list python-cramjam
            python-fsspec
@@ -5410,11 +5417,6 @@ other traditional Python scientific computing packages.")
            python-numpy
            python-packaging
            python-pandas))
-    (native-inputs
-     (list python-cython
-           python-pytest-runner
-           python-pytest-xdist
-           python-setuptools-scm))
     (home-page "https://github.com/dask/fastparquet")
     (synopsis "Python implementation of the Parquet file format")
     (description
