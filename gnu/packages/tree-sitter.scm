283e3f4bdc85ae0eb922902600669b1d003071b3
Steve George <steve@futurile.net>
2025-03-28 15:07:35 +0000

    gnu: Add rust-tree-sitter-yaml-0.7.
    * gnu/packages/tree-sitter.scm (rust-tree-sitter-yaml-0.7): New variable.
    
    Change-Id: Iff5b17ed41921207abcb87558d1f2256b017fe0d
    Signed-off-by: Steve George <steve@futurile.net>

--- gnu/packages/tree-sitter.scm
+++ gnu/packages/tree-sitter.scm
@@ -691,6 +691,29 @@ Tree-sitter parsing library.")
     (description "This package provides XML & DTD grammars for tree-sitter.")
     (license license:expat)))
 
+(define-public rust-tree-sitter-yaml-0.7
+  (package
+    (name "rust-tree-sitter-yaml")
+    (version "0.7.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "tree-sitter-yaml" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0phdym735blwnb8aff4225c5gyws6aljy8vbifhz2xxnj8mrzjfh"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs
+         (("rust-cc" ,rust-cc-1)
+          ("rust-tree-sitter-language" ,rust-tree-sitter-language-0.1))
+       #:cargo-development-inputs
+         (("rust-tree-sitter" ,rust-tree-sitter-0.24))))
+    (home-page "https://github.com/tree-sitter-grammars/tree-sitter-yaml")
+    (synopsis "YAML grammar for tree-sitter")
+    (description "This package provides a YAML grammar for tree-sitter.")
+    (license license:expat)))
+
 (define-public tree-sitter
   (package
     (name "tree-sitter")
