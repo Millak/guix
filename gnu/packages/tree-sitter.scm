8fd7691218afb810a70a7dc22bf31b7a8aefbc07
Steve George <steve@futurile.net>
2025-03-28 13:02:54 +0000

    gnu: Add rust-tree-sitter-json-0.24.
    * gnu/packages/tree-sitter.scm (rust-tree-sitter-json-0.24): New variable.
    
    Change-Id: I3169be241c37415903d5c5654e6623ee4c4a628a
    Signed-off-by: Steve George <steve@futurile.net>

--- gnu/packages/tree-sitter.scm
+++ gnu/packages/tree-sitter.scm
@@ -344,6 +344,29 @@ Tree-sitter parsing library.")
      "This package provides @code{JavaScript} grammar for tree-sitter.")
     (license license:expat)))
 
+(define-public rust-tree-sitter-json-0.24
+  (package
+    (name "rust-tree-sitter-json")
+    (version "0.24.8")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "tree-sitter-json" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0wf4gsa5mcrcprg8wh647n76rwv4cx8kbky6zw605h06lk67lwjd"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs
+         (("rust-cc" ,rust-cc-1)
+          ("rust-tree-sitter-language" ,rust-tree-sitter-language-0.1))
+       #:cargo-development-inputs
+         (("rust-tree-sitter" ,rust-tree-sitter-0.24))))
+    (home-page "https://github.com/tree-sitter/tree-sitter-json")
+    (synopsis "JSON grammar for tree-sitter")
+    (description "This package provides a JSON grammar for tree-sitter.")
+    (license license:expat)))
+
 (define-public tree-sitter
   (package
     (name "tree-sitter")
