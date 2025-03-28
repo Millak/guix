354ad815610fa457fff001c6e8221ee5ef17ec63
Steve George <steve@futurile.net>
2025-03-28 13:13:52 +0000

    gnu: Add rust-tree-sitter-make-1.
    * gnu/packages/tree-sitter.scm (rust-tree-sitter-make-1): New variable.
    
    Change-Id: I5bc95fd291ee717d6f835d64a3f9f2f8f07b6a1a
    Signed-off-by: Steve George <steve@futurile.net>

--- gnu/packages/tree-sitter.scm
+++ gnu/packages/tree-sitter.scm
@@ -413,6 +413,29 @@ Tree-sitter parsing library.")
     (description "This package provides a Lua grammar for tree-sitter.")
     (license license:expat)))
 
+(define-public rust-tree-sitter-make-1
+  (package
+    (name "rust-tree-sitter-make")
+    (version "1.1.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "tree-sitter-make" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0101h5ilrv2aqjffdlnq2d2m9mpj5fcfzvwamsgv3nnbrg3qv6f5"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:cargo-inputs
+         (("rust-cc" ,rust-cc-1)
+          ("rust-tree-sitter-language" ,rust-tree-sitter-language-0.1))
+       #:cargo-development-inputs
+         (("rust-tree-sitter" ,rust-tree-sitter-0.24))))
+    (home-page "https://github.com/tree-sitter-grammars/tree-sitter-make")
+    (synopsis "Makefile grammar for tree-sitter")
+    (description "This package provides a Makefile grammar for tree-sitter.")
+    (license license:expat)))
+
 (define-public tree-sitter
   (package
     (name "tree-sitter")
