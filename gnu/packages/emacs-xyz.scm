033903a07bd2e2ad6e9f1757157c379d072b8b7b
Liliana Marie Prikler <liliana.prikler@gmail.com>
2025-04-05 11:04:29 +0200

    gnu: emacs-rime: Fix build.
    * gnu/packages/emacs-xyz.scm (emacs-rime)[arguments]: Add #:test-command.
    <#:phases>: Add ‘pre-check’.

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -39210,6 +39210,7 @@ REPL appropriate to the current major mode.")
     (build-system emacs-build-system)
     (arguments
      '(#:include (cons "\\.so$" %default-include)
+       #:test-command (list "emacs" "--batch" "-l" "test.el")
        #:phases
        (modify-phases %standard-phases
          (add-after 'unpack 'patch-rime-data-path
@@ -39221,7 +39222,12 @@ REPL appropriate to the current major mode.")
                                         "share/rime-data")))))
          (add-before 'install 'build-emacs-module
            (lambda _
-             (invoke "make" "lib"))))))
+             (invoke "make" "lib")))
+         (add-before 'check 'pre-check
+           (lambda _
+             (setenv "HOME" (getenv "TMPDIR"))
+             (mkdir-p (string-append (getenv "HOME")
+                                     "/.emacs.d/rime")))))))
     (inputs
      (list librime rime-data))
     (propagated-inputs
