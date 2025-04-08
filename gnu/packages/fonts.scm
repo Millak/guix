06481c3e441268191bb8cada146b8f372ad88239
Kurome <hunt31999@gmail.com>
2025-04-08 10:08:48 +0900

    gnu: font-adobe-source-code-pro: Update to 2.042R-u-1.062R-i-1.026R-vf.
    * gnu/packages/fonts.scm (font-adobe-source-code-pro): Update to
    2.042R-u-1.062R-i-1.026R-vf.
    [source]<commit>: Update substitution pattern for version string.
    
    Change-Id: I40b0e44d867341dbc3ef32829619c83621d06c11
    Signed-off-by: Hilton Chain <hako@ultrarare.space>
    Modified-by: Hilton Chain <hako@ultrarare.space>

--- gnu/packages/fonts.scm
+++ gnu/packages/fonts.scm
@@ -1572,21 +1572,21 @@ Powerline support.")
 (define-public font-adobe-source-code-pro
   (package
     (name "font-adobe-source-code-pro")
-    (version "2.032R-ro-1.052R-it-1.012R-VAR")
+    (version "2.042R-u-1.062R-i-1.026R-vf")
     (source
      (origin
        (method git-fetch)
        (uri (git-reference
              (url "https://github.com/adobe-fonts/source-code-pro")
              (commit (regexp-substitute/global
-                      ;; The upstream tag uses "/" between the roman and italic
-                      ;; versions, so substitute our "-" separator here.
-                      #f "((R-ro)|(R-it))(-)" version
-                      'pre 1 "/" 'post
-                      ))))
+                      ;; The upstream tag uses "/" among its upright, italic and
+                      ;; variable versions, so substitute our "-" separator
+                      ;; here.
+                      #f "((R-u)|(R-i))(-)" version
+                      'pre 1 "/" 'post))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "1lqchm8z0ah5y675ycmciqvr8y1v1gcj22ysfs443gm291vy0z4v"))))
+        (base32 "0mir6yi5i0h1kk6fr8dsqmv4m8cwrbldadpy7rnlyvkd26wdqpiy"))))
     (build-system font-build-system)
     (home-page "https://github.com/adobe-fonts/source-code-pro")
     (synopsis
