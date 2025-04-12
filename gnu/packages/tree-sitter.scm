49e99d6b4b4f691a8328eb30ada3dc8f98ed6a6b
Hilton Chain <hako@ultrarare.space>
2025-04-12 13:13:29 +0800

    gnu: tree-sitter-verilog: Update to 1.0.3
    * gnu/packages/tree-sitter.scm (tree-sitter-verilog): Update to 1.0.3
    
    Change-Id: I0027eff544a9d6f85420d9327e9f8bb7e818c636

--- gnu/packages/tree-sitter.scm
+++ gnu/packages/tree-sitter.scm
@@ -790,20 +790,10 @@ which will be used as a snippet in origin."
    #:repository-url "https://github.com/Beaglefoot/tree-sitter-awk"))
 
 (define-public tree-sitter-verilog
-  (let ((version "1.0.0") ; In package.json, but untagged
-        (commit "075ebfc84543675f12e79a955f79d717772dcef3")
-        (revision "0"))
-    (tree-sitter-grammar
-     "verilog" "Verilog"
-     "0j5iycqm5dmvzy7dssm8km1djhr7hnfgk26zyzcxanhrwwq3wi4k"
-     (git-version version revision commit)
-     #:commit commit
-     #:get-cleanup-snippet
-     (lambda _
-       #~(begin
-           (use-modules (guix build utils))
-           (delete-file "binding.gyp")
-           (delete-file-recursively "bindings"))))))
+  (tree-sitter-grammar
+   "verilog" "Verilog"
+   "1mk8waij5lbj1wbayvqs0cxk003dssdic13h14gd5fi1ckfvflja"
+   "1.0.3"))
 
 (define-public tree-sitter-vhdl
   (let ((version "0.1.1") ; In package.json, but untagged
