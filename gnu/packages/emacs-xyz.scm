57b187f12d6129868395f9e8f10b4e8f8075c114
jgart <jgart@dismail.de>
2025-04-23 14:14:00 -0500

    gnu: Add emacs-denote-silo.
    * gnu/packages/emacs-xyz.scm (emacs-denote-silo): New variable.
    
    Change-Id: Id2bfbeffac7d821d892d882c0f8665a66c672093

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -19807,6 +19807,26 @@ journaling.  While it is possible to use the generic @code{denote} command
 to streamline the journaling workflow.")
     (license license:gpl3+)))
 
+(define-public emacs-denote-silo
+  (package
+    (name "emacs-denote-silo")
+    (version "0.1.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "https://elpa.gnu.org/packages/denote-silo-"
+                           version ".tar"))
+       (sha256
+        (base32 "1jxr52npjiwisambwav6rasndjdxhll8x278q8cr7giq71am7c8b"))))
+    (build-system emacs-build-system)
+    (propagated-inputs (list emacs-denote))
+    (home-page "https://github.com/protesilaos/denote-silo")
+    (synopsis "Convenience functions for using Denote in multiple silos")
+    (description "The denote-silo package makes it easier to work with
+multiple silos.  A silo is a localised denote-directory that is not connected
+to the default/global denote-directory and other silos.")
+    (license license:gpl3+)))
+
 (define-public emacs-denote-menu
   (package
     (name "emacs-denote-menu")
