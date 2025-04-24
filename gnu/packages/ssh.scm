Millak/guix

commit b12d44dd5e35ac236bf3fbb5619b9c8c2f42c902

gnu: libssh: Enable tests.
* gnu/packages/ssh.scm (libssh) [arguments]
<#:configure-flags>: Add "-DUNIT_TESTING=ON".
<#:phases>: New argument.
[native-inputs]: Add cmocka.

Change-Id: Ibe0f8f7b9bff969c9483dcd8c6b7635d91e62feb

gnu/packages/ssh.scm
@@ -15,7 +15,7 @@
 ;;; Copyright © 2019, 2020 Mathieu Othacehe <m.othacehe@gmail.com>
 ;;; Copyright © 2020, 2023, 2024 Janneke Nieuwenhuizen <janneke@gnu.org>
 ;;; Copyright © 2020 Oleg Pykhalov <go.wigust@gmail.com>
-;;; Copyright © 2020, 2021, 2022 Maxim Cournoyer <maxim.cournoyer@gmail.com>
+;;; Copyright © 2020, 2021, 2022, 2025 Maxim Cournoyer <maxim.cournoyer@gmail.com>
 ;;; Copyright © 2021 Brice Waegeneire <brice@waegenei.re>
 ;;; Copyright © 2023 Simon Streit <simon@netpanic.org>
 ;;; Copyright © 2024 Zheng Junjie <873216071@qq.com>
@@ -158,16 +158,30 @@ file names.
     (build-system cmake-build-system)
     (outputs '("out" "debug"))
     (arguments
-     `(#:configure-flags '("-DWITH_GCRYPT=ON"
-                           ,@(if (and (%current-target-system)
-                                      (not (target-64bit?)))
-                                 (list (string-append
-                                        "-DCMAKE_C_FLAGS=-g -O2"
-                                        " -Wno-incompatible-pointer-types"))
-                                 '()))
-
-       ;; TODO: Add 'CMockery' and '-DWITH_TESTING=ON' for the test suite.
-       #:tests? #f))
+     (list
+      #:configure-flags
+      #~(list "-DWITH_GCRYPT=ON"
+              "-DUNIT_TESTING=ON"
+              #$@(if (and (%current-target-system)
+                          (not (target-64bit?)))
+                     #~(list (string-append
+                              "-DCMAKE_C_FLAGS=-g -O2"
+                              " -Wno-incompatible-pointer-types"))
+                     #~()))
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-after 'unpack 'disable-problematic-tests
+            (lambda _
+              ;; XXX: There is no finer-grain control on skipping tests using
+              ;; cmocka, short of patching sources, which isn't trivial with
+              ;; substitute*/sed.
+              (substitute* "tests/unittests/CMakeLists.txt"
+                ;; Some torture tests fail due to assuming the user directory
+                ;; (from the passwd database) matches HOME, and other fail for
+                ;; unknown reasons (see:
+                ;; https://gitlab.com/libssh/libssh-mirror/-/issues/302).
+                (("^    torture_(config|misc|options).*$") "")))))))
+    (native-inputs (list cmocka))
     (inputs (list zlib libgcrypt mit-krb5))
     (synopsis "SSH client library")
     (description

commit f88be0a5b39ac70972041b717cf55f03b5084fc5

gnu: libssh: Fix #:configure-flags for cross-compilation to 32-bit.
Partly fixes <https://issues.guix.gnu.org/78029>.

Fixes a build-side syntax error introduced in
b12d44dd5e35ac236bf3fbb5619b9c8c2f42c902.

* gnu/packages/ssh.scm (libssh)[arguments]: Fix syntax error
in #:configure-flags.

Reported-by: Yelninei <yelninei@tutamail.com>
Change-Id: Ic50731806be7f0106928a9f8a2ec4b09c496f44a

gnu/packages/ssh.scm
@@ -1,5 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2012-2023 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2012-2023, 2025 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2013, 2014 Andreas Enge <andreas@enge.fr>
 ;;; Copyright © 2014, 2015, 2016 Mark H Weaver <mhw@netris.org>
 ;;; Copyright © 2015, 2016, 2018-2022, 2024 Efraim Flashner <efraim@flashner.co.il>
@@ -164,9 +164,9 @@ file names.
               "-DUNIT_TESTING=ON"
               #$@(if (and (%current-target-system)
                           (not (target-64bit?)))
-                     #~(list (string-append
-                              "-DCMAKE_C_FLAGS=-g -O2"
-                              " -Wno-incompatible-pointer-types"))
+                     #~((string-append
+                         "-DCMAKE_C_FLAGS=-g -O2"
+                         " -Wno-incompatible-pointer-types"))
                      #~()))
       #:phases
       #~(modify-phases %standard-phases