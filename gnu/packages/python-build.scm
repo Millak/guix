76e10bcc5a1bd24352abb43fa276a87857a8eaa5
Sharlatan Hellseher <sharlatanus@gmail.com>
2025-04-24 12:36:30 +0100

    gnu: Add python-setuptools-next.
    * gnu/packages/python-build.scm (python-setuptools-next): New variable.
    
    Change-Id: I2d295cddaa8ced23dd62a3526c4ed09aa03d8e69

--- gnu/packages/python-build.scm
+++ gnu/packages/python-build.scm
@@ -344,6 +344,24 @@ facilitate packaging Python projects, where packaging includes:
                    license:asl2.0       ;packaging is dual ASL2/BSD-2
                    license:bsd-2))))
 
+(define-public python-setuptools-next
+  (package
+    (inherit python-setuptools)
+    (name "python-setuptools")
+    (version "79.0.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (pypi-uri "setuptools" version))
+       (sha256
+        (base32 "127svm8cdpvmq37gcrbvdr9fhrhs0nscnzh63gypjc1wyfwfg30j"))
+       (modules '((guix build utils)))
+       (snippet
+        #~(begin
+            (for-each delete-file
+                      (find-files "setuptools" "^(cli|gui).*\\.exe$"))))))
+    (build-system pyproject-build-system)))
+
 (define-public python-wheel
   (package
     (name "python-wheel")
