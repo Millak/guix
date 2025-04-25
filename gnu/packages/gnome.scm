6e6335e0502336640e40444674eec73b4f782f82
Grigory Shepelev <shegeley@gmail.com>
2025-04-25 16:34:29 +0300

    gnu: Add network-manager-l2tp.
    * gnu/packages/gnome.scm (network-manager-l2tp): New variable.
    
    Change-Id: I90a45795e702fe04c64e02331de02104f083582c
    Signed-off-by: Maxim Cournoyer <maxim@guixotic.coop>

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -8860,6 +8860,73 @@ to virtual private networks (VPNs) via Fortinet SSLVPN.")
       (license license:gpl2+)
       (properties `((upstream-name . "NetworkManager-fortisslvpn"))))))
 
+(define-public network-manager-l2tp
+  (package
+    (name "network-manager-l2tp")
+    (version "1.20.20")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/nm-l2tp/NetworkManager-l2tp")
+                    (commit version)))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "0znv57fkg2l7pjbgdn9g3issvf5mb252d8hfm3pxf6wl21cc6rh2"))))
+    (build-system gnu-build-system)
+    (arguments
+     (list
+      #:configure-flags
+      #~(list
+         "--enable-absolute-paths"
+         "--localstatedir=/var"
+         "--with-gtk4=yes")
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-after 'configure 'patch-path
+            (lambda* (#:key inputs #:allow-other-keys #:rest args)
+              (let* ((xl2tpd (search-input-file inputs "/sbin/xl2tpd"))
+                     (ipsec
+                      (search-input-file inputs "/sbin/ipsec"))
+                     (modprobe
+                      (search-input-file inputs "/bin/modprobe")))
+                (for-each
+                 (lambda (file)
+                   (substitute* file
+                     (("/usr/bin/xl2tpd") xl2tpd)
+                     (("/usr/bin/ipsec") ipsec)
+                     (("/sbin/modprobe") modprobe)))
+                 '("shared/utils.c"
+                   "src/nm-l2tp-service.c"))))))))
+    (native-inputs
+     (list gettext-minimal
+           intltool
+           autoconf
+           automake
+           libtool
+           (list glib "bin")
+           pkg-config))
+    (inputs
+     (list gtk+
+           gtk
+           (list gtk "bin")
+           kmod
+           ppp
+           openssl
+           nss
+           libnma
+           libsecret
+           network-manager
+           strongswan
+           xl2tpd))
+    (home-page "https://github.com/nm-l2tp/NetworkManager-l2tp")
+    (synopsis "NetworkManager plugin for L2TP")
+    (description "NetworkManager-l2tp is a VPN plugin for NetworkManager
+and later which provides support for L2TP and L2TP/IPsec (i.e. L2TP over
+IPsec) connections.")
+    (license license:gpl2+)
+    (properties `((upstream-name . "NetworkManager-l2tp")))))
+
 (define-public mobile-broadband-provider-info
   (package
     (name "mobile-broadband-provider-info")
