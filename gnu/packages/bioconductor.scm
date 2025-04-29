f05cc56f22258279f5288347d2fde2e85ae60001
Ricardo Wurmus <rekado@elephly.net>
2025-04-29 18:56:37 +0200

    gnu: r-wiggleplotr: Update to 1.32.0.
    * gnu/packages/bioconductor.scm (r-wiggleplotr): Update to 1.32.0.
    
    Change-Id: I0f725e809a6603111997a5e20bd8ac0d40081b00

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -20644,14 +20644,14 @@ that arising from 16s metagenomic surveys.")
 (define-public r-wiggleplotr
   (package
     (name "r-wiggleplotr")
-    (version "1.30.0")
+    (version "1.32.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "wiggleplotr" version))
        (sha256
         (base32
-         "1p0x3zrp5hvrfh7cyxdxgniij3yjawrsm1pnz0sw75ps00kw3kpn"))))
+         "0ncldwkyy0pd97bgv9zps9xh4b2a6apzvy9jhm5n6wgrr0i01zq2"))))
     (build-system r-build-system)
     (propagated-inputs
      (list r-assertthat
