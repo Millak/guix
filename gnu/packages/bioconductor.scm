0e442f052080d9d8e458dda62d3fc001d663f45d
Ricardo Wurmus <rekado@elephly.net>
2025-04-29 22:40:01 +0200

    gnu: r-msmseda: Update to 1.46.0.
    * gnu/packages/bioconductor.scm (r-msmseda): Update to 1.46.0.
    
    Change-Id: I6628e82ab5cde3594b616b43b3f7a978f6705d08

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -26132,14 +26132,14 @@ as well as local access.  Developers can register a file extension, e.g.,
 (define-public r-msmseda
   (package
     (name "r-msmseda")
-    (version "1.44.0")
+    (version "1.46.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "msmsEDA" version))
        (sha256
         (base32
-         "0i3ivq14jjc2v9d8v536s55w211bh67v39qzl2prringrwncq418"))))
+         "1qxksn69zp1whxajwzdprpns9r1aq66hkgp9knc8nd2jk5y8arla"))))
     (properties `((upstream-name . "msmsEDA")))
     (build-system r-build-system)
     (propagated-inputs
