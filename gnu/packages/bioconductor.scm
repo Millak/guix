1377add8f8632a0fea3bc647243ba820404674c1
Ricardo Wurmus <rekado@elephly.net>
2025-04-29 22:40:15 +0200

    gnu: r-tcgabiolinks: Update to 2.36.0.
    * gnu/packages/bioconductor.scm (r-tcgabiolinks): Update to 2.36.0.
    
    Change-Id: I9569c4178e12c45a1b07e2baed3137de3be531ce

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -28358,13 +28358,13 @@ estimates, etc.")
 (define-public r-tcgabiolinks
   (package
     (name "r-tcgabiolinks")
-    (version "2.34.0")
+    (version "2.36.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "TCGAbiolinks" version))
        (sha256
-        (base32 "0rrk97956kyd17apjm29fxnqjhbh5mllpfr3y4yf3a3ras9chgw4"))))
+        (base32 "1ahpj0609x2wa2rzvkfdaia6026flh011lg53lrnagxh1wi55qkc"))))
     (properties
      '((upstream-name . "TCGAbiolinks")
        (updater-extra-native-inputs . ("r-edaseq" "r-edger" "r-survminer"))))
