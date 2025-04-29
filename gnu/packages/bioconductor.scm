5f85c18e5bbbeaf204924c946a4b5b7352619d7a
Ricardo Wurmus <rekado@elephly.net>
2025-04-29 22:40:08 +0200

    gnu: r-bayesknockdown: Update to 1.34.0.
    * gnu/packages/bioconductor.scm (r-bayesknockdown): Update to 1.34.0.
    
    Change-Id: I69faad00a359c1fff392e2cffc57db64f8fc1d93

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -27456,13 +27456,13 @@ Python environments in a single R session.")
 (define-public r-bayesknockdown
   (package
     (name "r-bayesknockdown")
-    (version "1.32.0")
+    (version "1.34.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "BayesKnockdown" version))
        (sha256
-        (base32 "149ksni85zcms0kiz0as30jji9w55djzv7isnc69nd0xxp54gnyj"))))
+        (base32 "18jmsgdyvg5m3kndzcszjgfwfh0bwmi3nvfyg4nl9l0skp478cld"))))
     (properties `((upstream-name . "BayesKnockdown")))
     (build-system r-build-system)
     (propagated-inputs (list r-biobase))
