b927cbe70df202db788aa9a443acc78ebf1ee7cb
Ricardo Wurmus <rekado@elephly.net>
2025-05-11 10:18:45 +0200

    gnu: Remove r-snapcgh.
    This package has been removed with the release of Bioconductor 3.19, and it
    now fails to build.
    
    * gnu/packages/bioconductor.scm (r-snapcgh): Remove variable.
    
    Change-Id: Id4408ea2d17d07913fea359292eb3e5d7329b13d

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -26810,34 +26810,6 @@ integrates with pathway and gene set (enrichment) analysis tools for
 large-scale and fully automated analysis.")
     (license license:gpl3+)))
 
-(define-public r-snapcgh
-  (package
-    (name "r-snapcgh")
-    (version "1.72.0")
-    (source (origin
-              (method url-fetch)
-              (uri (bioconductor-uri "snapCGH" version))
-              (sha256
-               (base32
-                "0knjspxzc3z5dhx0a6kx9rkic85w65l84vy552x5bv8jzxkcwrsj"))))
-    (properties `((upstream-name . "snapCGH")))
-    (build-system r-build-system)
-    (propagated-inputs
-     (list r-acgh
-           r-cluster
-           r-dnacopy
-           r-glad
-           r-limma
-           r-tilingarray))
-    (home-page "https://bioconductor.org/packages/snapCGH")
-    (synopsis "Segmentation, normalisation and processing of the aCGH data")
-    (description
-     "This package provides methods for segmenting, normalising and processing
-aCGH data.  snapCGH also includes plotting functions for visualising raw and
-segmented data for individual and multiple arrays.")
-    ;; Expanded from GPL
-    (license (list license:gpl2+ license:gpl3+))))
-
 (define-public r-snprelate
   (package
     (name "r-snprelate")
