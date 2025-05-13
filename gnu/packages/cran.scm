0d534a2b9b004cc3b18a26efdd4176680a1880a6
Ricardo Wurmus <rekado@elephly.net>
2025-05-13 10:25:50 +0200

    gnu: r-tarchetypes: Update to 0.13.1.
    * gnu/packages/cran.scm (r-tarchetypes): Update to 0.13.1.
    
    Change-Id: I9bac9658fcc86d9f5e5757f9407c0909cbb5b61c

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -27022,13 +27022,13 @@ handle data from simple random samples as well as complex surveys.")
 (define-public r-tarchetypes
   (package
     (name "r-tarchetypes")
-    (version "0.13.0")
+    (version "0.13.1")
     (source (origin
               (method url-fetch)
               (uri (cran-uri "tarchetypes" version))
               (sha256
                (base32
-                "05h0cgz6j45d4spzddlb5p68i0q9yflln1rpfn1aim7ck1rrxp16"))))
+                "0h1d658crp717iwpc8q2ly1yybj4l33jr5gzgncx2b8mm14ykiij"))))
     (properties `((upstream-name . "tarchetypes")))
     (build-system r-build-system)
     ;; Many tests fail, because r-targets looks up the system's partitions,
