3f1f9af4ddb350173f0d4765bdae82aa542c7ca4
Ricardo Wurmus <rekado@elephly.net>
2025-05-13 10:25:48 +0200

    gnu: r-rcppspdlog: Update to 0.0.22.
    * gnu/packages/cran.scm (r-rcppspdlog): Update to 0.0.22.
    
    Change-Id: Ie5716c8ea4adf1f99716195f0b0eea1e6695859e

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -14833,13 +14833,13 @@ factorization and divisive clustering for large sparse and dense matrices.")
 (define-public r-rcppspdlog
   (package
     (name "r-rcppspdlog")
-    (version "0.0.21")
+    (version "0.0.22")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "RcppSpdlog" version))
        (sha256
-        (base32 "0s9wacp7kf5z4xrgh69z2rip2j89a3c4j56qhff4qcw79ly2vxkf"))))
+        (base32 "0ihh0g0amq69ws166p85zszm9slhr5n7i124i7k92ay394r14wh7"))))
     (properties `((upstream-name . "RcppSpdlog")))
     (build-system r-build-system)
     (propagated-inputs (list r-rcpp))
