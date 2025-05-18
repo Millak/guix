99f0ecc96f3da427b30f275103f71f0080d3e9f2
Nicolas Graves <ngraves@ngraves.fr>
2025-05-18 10:16:00 +0200

    gnu: emacs-emms: Fix build.
    * gnu/packages/emacs-xyz.scm (emacs-emms): Fix double [native-inputs]
    field shadowing texinfo.
    
    Signed-off-by: Ian Eure <ian@retrospec.tv>

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -4438,7 +4438,6 @@ light user interface.")
                   (string-append "\"" vorbiscomment "\"")))
                (substitute* "emms-info-exiftool.el"
                  (("\"exiftool\"") (string-append "\"" exiftool "\"")))))))))
-    (native-inputs (list texinfo))
     (inputs
      (list emacs-emms-print-metadata
            alsa-utils
@@ -4449,7 +4448,7 @@ light user interface.")
            python-mutagen
            perl-image-exiftool
            opus-tools))
-    (native-inputs (list emacs-ert-runner))))
+    (native-inputs (list emacs-ert-runner texinfo))))
 
 (define-public emacs-emms-mode-line-cycle
   (package
