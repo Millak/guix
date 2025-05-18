3be3aca6c714291c6c8b7ed7e5349a6943034e00
Nicolas Graves <ngraves@ngraves.fr>
2025-05-18 10:16:43 +0200

    gnu: emacs-spamfilter-el: Rename package to emacs-spamfilter.
    * gnu/packages/emacs-xyz.scm (emacs-spamfilter-el): Define in terms of
    'deprecated-package'.
    (emacs-spamfilter): New variable, formerly known as "emacs-spamfilter-el".
    (emacs-wanderlust)[inputs]: Replace accordingly.
    
    Signed-off-by: Ian Eure <ian@retrospec.tv>

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -42266,9 +42266,9 @@ border, the padding of the mode line, and more.  The idea is to produce a more
 comfortable presentation.")
     (license license:gpl3+)))
 
-(define-public emacs-spamfilter-el
+(define-public emacs-spamfilter
   (package
-    (name "emacs-spamfilter-el")
+    (name "emacs-spamfilter")
     (version "1.13")
     (source (origin
               (method url-fetch)
@@ -42310,6 +42310,9 @@ supports Japanese and has the following features:
 @item @url{https://github.com/naota/navi2ch, Navi2ch} integration.
 @end itemize\n")
     (license license:gpl2+)))
+
+(define-deprecated/public emacs-spamfilter-el emacs-spamfilter
+  (deprecated-package "emacs-spamfilter-el" emacs-spamfilter))
 
 (define-public emacs-promise
   ;; XXX: Last stable release fails to build with "(wrong-number-of-arguments
@@ -44344,7 +44347,7 @@ EasyPG and latest Emacs.")
                      (invoke "make" "install")
                      (invoke "make" "install-info"))))))
       (propagated-inputs (list emacs-semi-epg))
-      (inputs (list emacs-spamfilter-el))
+      (inputs (list emacs-spamfilter))
       (home-page "https://www.emacswiki.org/emacs/WanderLust")
       (synopsis "Yet Another Message Interface on Emacsen")
       (description
