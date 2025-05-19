7ba6e6f090b329bdc750ddba404a92ceb7cdc5b2
Danny Milosavljevic <dannym@friendly-machines.com>
2025-05-19 20:54:03 +0200

    gnu: Add rust-gix-worktree@0.39.0.
    * gnu/packages/crates-vcs.scm (rust-gix-worktree-0.39): New variable.
    
    Change-Id: I3f5d5f47dd7a867590523cbb635ec9077db3d217

--- gnu/packages/crates-vcs.scm
+++ gnu/packages/crates-vcs.scm
@@ -9571,6 +9571,41 @@ package contains validation functions for various kinds of names in Git.")
        (("rust-bstr" ,rust-bstr-1)
         ("rust-thiserror" ,rust-thiserror-1))))))
 
+(define-public rust-gix-worktree-0.39
+  (package
+    (name "rust-gix-worktree")
+    (version "0.39.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "gix-worktree" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0n49fywzh1f4gmv7gwd4d5jnq7ahiabsdv6wda3scmxagqpm2wv6"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t
+       #:cargo-inputs (("rust-bstr" ,rust-bstr-1)
+                       ("rust-document-features" ,rust-document-features-0.2)
+                       ("rust-gix-attributes" ,rust-gix-attributes-0.24)
+                       ("rust-gix-features" ,rust-gix-features-0.40)
+                       ("rust-gix-fs" ,rust-gix-fs-0.13)
+                       ("rust-gix-glob" ,rust-gix-glob-0.18)
+                       ("rust-gix-hash" ,rust-gix-hash-0.16)
+                       ("rust-gix-ignore" ,rust-gix-ignore-0.13)
+                       ("rust-gix-index" ,rust-gix-index-0.38)
+                       ("rust-gix-object" ,rust-gix-object-0.47)
+                       ("rust-gix-path" ,rust-gix-path-0.10)
+                       ("rust-gix-validate" ,rust-gix-validate-0.9)
+                       ("rust-serde" ,rust-serde-1))))
+    (home-page "https://github.com/GitoxideLabs/gitoxide")
+    (synopsis
+     "Gitoxide functions for handling shared Git worktree types and utilities")
+    (description
+     "This package provides functions for handling shared Git worktree related
+types and utilities.  Part of Gitoxide a pure Rust implementation of Git.")
+    (license (list license:expat license:asl2.0))))
+
 (define-public rust-gix-worktree-0.36
   (package
     (name "rust-gix-worktree")
