ad7ef0d9dd61b40b9846d2cab58d63489c726193
Leo Famulari <leo@famulari.name>
2025-05-20 20:36:53 -0400

    gnu: linux-libre: Update to 6.14.7.
    * gnu/packages/linux.scm (linux-libre-6.14-version): Update to 6.14.7.
    (linux-libre-6.14-pristine-source, deblob-scripts-6.14): Update hashes.
    
    Change-Id: I0ecad6647af84701233467f6d1bb0de514442e47

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -513,17 +513,17 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
 ;; The current "stable" kernels. That is, the most recently released major
 ;; versions that are still supported upstream.
 
-(define-public linux-libre-6.14-version "6.14.6")
+(define-public linux-libre-6.14-version "6.14.7")
 (define-public linux-libre-6.14-gnu-revision "gnu")
 (define deblob-scripts-6.14
   (linux-libre-deblob-scripts
    linux-libre-6.14-version
    linux-libre-6.14-gnu-revision
-   (base32 "1r6p7wnf78i5fgdw1gc2ax8lqqizp58ms48b00xs82hcrkik1v5v")
-   (base32 "1wx8f0fx71sr506j655i7mr9xrc4fpw96d3fdgfivjsxlcrwfpnk")))
+   (base32 "00b6axy07ykdxk4qy3dnx5mvhz2dvbf78qxax3zq81bg11wbfvay")
+   (base32 "076x15yp1qjhvv81si3aj3n6ny4l6yl1rcj7f7l12rlbl9p64vl2")))
 (define-public linux-libre-6.14-pristine-source
   (let ((version linux-libre-6.14-version)
-        (hash (base32 "1acpjxscw5nvgp19jzd29zhl1c6wdzx0bxp4yy0hy8z2k0cpz091")))
+        (hash (base32 "0wj9yl0ymzjx3ig0l3wdl5dqqiylvjk0j8adm5bnj23dq8mj04l1")))
    (make-linux-libre-source version
                             (%upstream-linux-source version hash)
                             deblob-scripts-6.14)))
