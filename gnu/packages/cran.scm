0fae8ee14a6bf15e511595305286e7fc04a31428
Ricardo Wurmus <rekado@elephly.net>
2025-05-22 08:12:22 +0200

    gnu: r-isocodes: Update to 2025.05.18.
    * gnu/packages/cran.scm (r-isocodes): Update to 2025.05.18.
    
    Change-Id: I5beab5d033c6bec4c536f408dbd1c3275d13a4d8

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -43969,14 +43969,14 @@ and Euclidean distance is particularly fast.")
 (define-public r-isocodes
   (package
     (name "r-isocodes")
-    (version "2024.02.12")
+    (version "2025.05.18")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "ISOcodes" version))
        (sha256
         (base32
-         "1rp2gasiggqfaqbag8wqz1rlhlajpji2klx0rgzms21lh28ppjq2"))))
+         "03av713l5szg9cp7kmhp2yplsi91l089yx2bb2xhjfxgpn9p3nnv"))))
     (properties `((upstream-name . "ISOcodes")))
     (build-system r-build-system)
     (home-page "https://cran.r-project.org/package=ISOcodes")
