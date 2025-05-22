268945d42b7eaf4e3ff2a3fbf713a59052681391
Ashish SHUKLA <ashish.is@lostca.se>
2025-05-22 08:14:50 +0200

    gnu: snac: Update to 2.77.
    * gnu/packages/fediverse.scm (snac2): Update to 2.77.
    
    Change-Id: Ia5a025f113aea0abba2ae434b23085c0082476ca
    Signed-off-by: Christopher Baines <mail@cbaines.net>

--- gnu/packages/fediverse.scm
+++ gnu/packages/fediverse.scm
@@ -205,7 +205,7 @@ seamlessly with your desktop environment.")
 (define-public snac2
   (package
     (name "snac")
-    (version "2.75")
+    (version "2.77")
     (source
      (origin
        (method git-fetch)
@@ -214,7 +214,7 @@ seamlessly with your desktop environment.")
              (commit version)))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "11dnsc3ml0rmjqwf01mwiggf5xdg762k6dciis17frrx9grb8bn7"))))
+        (base32 "1mibrqjdv68lxgvhly46v6i04bv05bm06qp7qp1kjr3mph7wahs1"))))
     (build-system gnu-build-system)
     (inputs (list curl openssl))
     (arguments
