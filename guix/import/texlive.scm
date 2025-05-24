Millak/guix

commit f13f0769688493271f43f31a016957355dbecb30

refresh: Add support for partial target versions.
* guix/import/utils.scm (find-version): New procedure.
* guix/scripts/refresh.scm (<update-spec>) [partial?]: New field.
(update-spec-partial?): New accessor.
(update-spec): Add a PARTIAL? optional argument.
(update-specification->update-spec) <update-spec>: Call with its new PARTIAL?
optional argument when FALLBACK-VERSION is provided, i.e. when
'--target-version' was used.
(update-package): Remove the PACKAGE and VERSION positional arguments, and
replace them with UPDATE-SPEC.  Update doc.  Call `package-update' with its
new #:partial-version? argument.
(check-for-package-update) <package-latest-release>: Pass the new
 #:partial-version? argument to it.
(guix-refresh) <update-package>: Adjust call accordingly.
(show-help): Udate doc.
* guix/upstream.scm (package-latest-release): Add #:partial-version? argument,
and apply it to the importer call.
(package-update): Add #:partial-version?> argument.  Update doc.  Pass it to
the `package-latest-release' call.
* guix/gnu-maintenance.scm (rewrite-url): Add #:partial-version? argument.
Update doc.  Crawl URL for newer compatible versions when provided.
(import-html-release): Add #:partial-version? argument, and pass it to the
`rewrite-url' call.  Use `find-version' to find the best version.
(import-release, import-ftp-release, import-gnu-release)
(import-release*): Add #:partial-version? argument and honor it.
(import-html-updatable-release): Add #:partial-version? argument, and pass it
to the `import-html-release' call.
* guix/import/gnome.scm (import-gnome-release)
<#:partial-version?>: Add new argument and honor it.
* guix/import/texlive.scm (latest-texlive-tag): Rename to...
(texlive-tags): ... this, and have it return all tags.
(texlive->guix-package): Adjust accordingly.
(latest-release): Add a #:partial-version? argument.  Update doc.
* guix/import/stackage.scm (latest-lts-release): New #:partial-version?
argument.

guix/import/texlive.scm
@@ -1,6 +1,6 @@
 ;;; GNU Guix --- Functional package management for GNU
 ;;; Copyright © 2017, 2021, 2022, 2023 Ricardo Wurmus <rekado@elephly.net>
-;;; Copyright © 2021 Maxim Cournoyer <maxim.cournoyer@gmail.com>
+;;; Copyright © 2021, 2024 Maxim Cournoyer <maxim.cournoyer@gmail.com>
 ;;; Copyright © 2024 Nicolas Goaziou <mail@nicolasgoaziou.fr>
 ;;;
 ;;; This file is part of GNU Guix.
@@ -35,7 +35,8 @@
   #:use-module (guix store)
   #:use-module (guix svn-download)
   #:use-module (guix upstream)
-  #:use-module ((guix utils) #:select (downstream-package-name))
+  #:use-module ((guix utils) #:select (downstream-package-name
+                                       version>? version-prefix?))
   #:use-module (ice-9 ftw)
   #:use-module (ice-9 match)
   #:use-module (ice-9 popen)
@@ -261,17 +262,21 @@ not succeed."
   "Return number of days since Epoch."
   (floor (/ (time-second (current-time)) (* 24 60 60))))
 
-(define latest-texlive-tag
-  ;; Return the latest TeX Live tag in repository.  The argument refers to
-  ;; current day, so memoization is only active a single day, as the
-  ;; repository may have been updated between two calls.
+(define texlive-tags
   (memoize
    (lambda* (#:key (day (current-day)))
-     (let ((output
-            (svn-command "ls" (string-append %texlive-repository "tags") "-v")))
-       ;; E.g. "70951 karl april 15 18:11 texlive-2024.2/\n\n"
-       (and=> (string-match "texlive-([^/]+)/\n*$" output)
-              (cut match:substring <> 1))))))
+     "Return all tags found in for the TeX Live tags in repository, from
+latest to oldest.  The argument refers to current day, so memoization is only
+active a single day, as the repository may have been updated between two
+calls."
+     (let* ((output (svn-command
+                     "ls" (string-append %texlive-repository "tags") "-v"))
+            (lines (string-split output #\newline)))
+       ;; Each line look like "70951 karl april 15 18:11 texlive-2024.2/\n\n".
+       (filter-map (lambda (l)
+                     (and=> (string-match "texlive-([^/]+)/\n*$" l)
+                            (cut match:substring <> 1)))
+                   lines)))))
 
 (define string->license
   (match-lambda
@@ -761,7 +766,7 @@ associated Guix package, or #f on failure.  Fetch metadata for a specific
 version whenever VERSION keyword is specified.  Otherwise, grab package latest
 release.  When DATABASE is provided, fetch metadata from there, ignoring
 VERSION."
-    (let ((version (or version (latest-texlive-tag))))
+    (let ((version (or version (first (texlive-tags)))))
       (tlpdb->package name version (or database (tlpdb/cached version))))))
 
 (define* (texlive-recursive-import name #:key repo version)
@@ -785,13 +790,14 @@ VERSION."
              (eq? 'texlive
                   (build-system-name (package-build-system package)))))))
 
-(define* (latest-release package #:key version)
+(define* (latest-release package #:key version partial-version?)
   "Return an <upstream-source> for the latest release of PACKAGE.  Optionally
-include a VERSION string to fetch a specific version."
-  (let* ((version (or version (latest-texlive-tag)))
+include a VERSION string to fetch a specific version, which may be a partial
+prefix when PARTIAL-VERSION? is #t."
+  (let* ((version (find-version (texlive-tags) version partial-version?))
          (database (tlpdb/cached version))
          (upstream-name (package-upstream-name* package)))
-    (and (assoc-ref database upstream-name)
+    (and version (assoc-ref database upstream-name)
          (upstream-source
           (package upstream-name)
           (version version)

doc/guix.texi
@@ -14975,6 +14975,7 @@ gnu/packages/guile.scm:147:2: guile: updating from version 2.0.10 to version 2.0
 @dots{}
 @end example
 
+@cindex target version, guix refresh
 In some specific cases, you may have many packages specified via a
 manifest or a module selection which should all be updated together; for
 these cases, the @option{--target-version} option can be provided to have
@@ -14993,6 +14994,17 @@ gnu/packages/qt.scm:1202:13: qtdeclarative would be upgraded from 5.15.8 to 5.15
 gnu/packages/qt.scm:1762:13: qtserialbus would be upgraded from 5.15.8 to 5.15.10
 gnu/packages/qt.scm:2070:13: qtquickcontrols2 would be upgraded from 5.15.8 to 5.15.10
 @dots{}
+@end example
+
+@cindex partial target version, guix refresh
+The @option{--target-version} option accepts partial version prefixes,
+which can be useful to update to the latest major or major-minor
+prefixed version:
+
+@example
+$ guix refresh qtbase@@5 qtdeclarative@@5 --target-version=5
+gnu/packages/qt.scm:1472:13: qtdeclarative would be upgraded from 5.15.8 to 5.15.10
+gnu/packages/qt.scm:452:13: qtbase would be upgraded from 5.15.8 to 5.15.10
 @end example
 
 Sometimes the upstream name differs from the package name used in Guix,

guix/gnu-maintenance.scm
@@ -3,7 +3,7 @@
 ;;; Copyright © 2012, 2013 Nikita Karetnikov <nikita@karetnikov.org>
 ;;; Copyright © 2021 Simon Tournier <zimon.toutoune@gmail.com>
 ;;; Copyright © 2022 Maxime Devos <maximedevos@telenet.be>
-;;; Copyright © 2023 Maxim Cournoyer <maxim.cournoyer@gmail.com>
+;;; Copyright © 2023, 2025 Maxim Cournoyer <maxim.cournoyer@gmail.com>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -44,7 +44,7 @@
   #:use-module (guix records)
   #:use-module (guix upstream)
   #:use-module (guix packages)
-  #:autoload   (guix import utils) (false-if-networking-error)
+  #:autoload   (guix import utils) (false-if-networking-error find-version)
   #:autoload   (zlib) (call-with-gzip-input-port)
   #:autoload   (htmlprag) (html->sxml)            ;from Guile-Lib
   #:export (gnu-package-name
@@ -346,12 +346,15 @@ name/directory pairs."
 
 (define* (import-ftp-release project
                              #:key
-                             (version #f)
+                             version
+                             partial-version?
                              (server "ftp.gnu.org")
                              (directory (string-append "/gnu/" project))
                              (file->signature (cut string-append <> ".sig")))
   "Return an <upstream-source> for the latest release of PROJECT on SERVER
-under DIRECTORY, or #f. Optionally include a VERSION string to fetch a specific version.
+under DIRECTORY, or #f.  Optionally include a VERSION string to fetch a
+specific version, which may be marked as partially specified via
+PARTIAL-VERSION?.
 
 Use FTP-OPEN and FTP-CLOSE to open (resp. close) FTP connections; this can be
 useful to reuse connections.
@@ -417,7 +420,9 @@ return the corresponding signature URL, or #f it signatures are unavailable."
                                     (and (release-file? project file)
                                          (file->source directory file)))
                                    (_ #f))
-                                 entries)))
+                                 entries))
+           (versions (map upstream-source-version releases))
+           (version (find-version versions version partial-version?)))
 
       ;; Assume that SUBDIRS correspond to versions, and jump into the
       ;; one with the highest version number.
@@ -440,14 +445,17 @@ return the corresponding signature URL, or #f it signatures are unavailable."
 
 (define* (import-release package
                          #:key
-                         (version #f)
+                         version
+                         partial-version?
                          (server "ftp.gnu.org")
                          (directory (string-append "/gnu/" package)))
   "Return the <upstream-source> for the latest version of PACKAGE or #f.
 PACKAGE must be the canonical name of a GNU package. Optionally include a
-VERSION string to fetch a specific version."
+VERSION string to fetch a specific version, which may be marked as partially
+specified via PARTIAL-VERSION?."
   (import-ftp-release package
                       #:version version
+                      #:partial-version? partial-version?
                       #:server server
                       #:directory directory))
 
@@ -463,7 +471,7 @@ of EXP otherwise."
           (close-port port))
       #f)))
 
-(define* (import-release* package #:key (version #f))
+(define* (import-release* package #:key version partial-version?)
   "Like 'import-release', but (1) take a <package> object, and (2) ignore FTP
 errors that might occur when PACKAGE is not actually a GNU package, or not
 hosted on ftp.gnu.org, or not under that name (this is the case for
@@ -474,6 +482,7 @@ hosted on ftp.gnu.org, or not under that name (this is the case for
      (false-if-ftp-error
       (import-release (package-upstream-name package)
                       #:version version
+                      #:partial-version? partial-version?
                       #:server server
                       #:directory directory)))))
 
@@ -561,16 +570,23 @@ URL is a directory instead of a file, it should be suffixed with a slash (/)."
 ;;; TODO: Extend to support the RPM and GNOME version schemes?
 (define %version-rx "[0-9.]+")
 
-(define* (rewrite-url url version #:key to-version)
+(define* (rewrite-url url version #:key to-version partial-version?)
   "Rewrite URL so that the URL path components matching the current VERSION or
 VERSION-MAJOR.VERSION-MINOR are updated with that of the latest version found
 by crawling the corresponding URL directories.  Alternatively, when TO-VERSION
-is specified, rewrite version matches directly to it without crawling URL.
+is specified, rewrite version matches directly to it without crawling URL.  If
+TO-VERSION is provided and PARTIAL-VERSION? set to #t, then crawl URL to find
+the newest compatible release (one that is prefixed by TO-VERSION).
 
 For example, the URL
 \"https://dist.libuv.org/dist/v1.45.0/libuv-v1.45.0.tar.gz\" could be
 rewritten to something like
-\"https://dist.libuv.org/dist/v1.46.0/libuv-v1.46.0.tar.gz\"."
+\"https://dist.libuv.org/dist/v1.46.0/libuv-v1.46.0.tar.gz\".
+
+With TO-VERSION set to \"1.49\" and PARTIAL-VERSION? set to #t, the URL
+\"https://dist.libuv.org/dist/v1.45.0/libuv-v1.45.0.tar.gz\" could be
+rewritten to something like
+\"https://dist.libuv.org/dist/v1.49.2/libuv-v1.49.2.tar.gz\"."
   ;; XXX: major-minor may be #f if version is not a triplet but a single
   ;; number such as "2".
   (let* ((major-minor (false-if-exception (version-major+minor version)))
@@ -590,14 +606,15 @@ rewritten to something like
      (reverse
       (fold
        (lambda (s parents)
-         (if to-version
+         (if (and to-version (not partial-version?))
              ;; Direct rewrite case; the archive is assumed to exist.
              (let ((u (string-replace-substring s version to-version)))
                (cons (if (and major-minor to-major-minor)
                          (string-replace-substring u major-minor to-major-minor)
                          u)
                      parents))
-             ;; More involved HTML crawl case.
+             ;; More involved HTML crawl case to get the latest version or a
+             ;; partial to-version.
              (let* ((pattern (if major-minor
                                  (format #f "(~a|~a)" version major-minor)
                                  (format #f "(~a)" version)))
@@ -620,15 +637,14 @@ rewritten to something like
                                               (m (string-match pattern l))
                                               (v (match:substring m 1)))
                                            (cons v l)))
-                                       links)))
-                     ;; Retrieve the item having the largest version.
-                     (if (null? candidates)
-                         parents
-                         (cons (cdr (first (sort candidates
-                                                 (lambda (x y)
-                                                   (version>? (car x)
-                                                              (car y))))))
-                               parents)))
+                                       links))
+                          (versions (map car candidates))
+                          (version (find-version versions to-version
+                                                 partial-version?)))
+                     ;; Retrieve the item having the greatest version.
+                     (if version
+                         (cons (assoc-ref candidates version) parents)
+                         parents))      ;XXX: bogus case; throw an error?
                    ;; No version found in path component; continue.
                    (cons s parents)))))
        (reverse url-prefix-components)
@@ -639,12 +655,14 @@ rewritten to something like
                               #:key
                               rewrite-url?
                               version
+                              partial-version?
                               (directory (string-append
                                           "/" (package-upstream-name package)))
                               file->signature)
   "Return an <upstream-source> for the latest release of PACKAGE under
 DIRECTORY at BASE-URL, or #f.  Optionally include a VERSION string to fetch a
-specific version.
+specific version, which may be marked as partially specified via
+PARTIAL-VERSION?.
 
 BASE-URL should be the URL of an HTML page, typically a directory listing as
 found on 'https://kernel.org/pub'.
@@ -663,7 +681,8 @@ also updated to the latest version, as explained in the doc of the
                   base-url
                   (string-append base-url directory "/")))
          (url (if rewrite-url?
-                  (rewrite-url url current-version #:to-version version)
+                  (rewrite-url url current-version #:to-version version
+                               #:partial-version? partial-version?)
                   url))
          (links (map (cut canonicalize-url <> url) (url->links url))))
 
@@ -695,23 +714,18 @@ else #f.  URL is assumed to fully specified."
                         (lambda (url) (list (uri-mirror-rewrite url))))))))))
 
     (define candidates
-      (filter-map url->release links))
+      (coalesce-sources (filter-map url->release links)))
 
-    (match candidates
-      (() #f)
-      ((first . _)
-       (if version
-           ;; Find matching release version and return it.
-           (find (lambda (upstream)
-                   (string=? (upstream-source-version upstream) version))
-                 (coalesce-sources candidates))
-           ;; Select the most recent release and return it.
-           (reduce (lambda (r1 r2)
-                     (if (version>? (upstream-source-version r1)
-                                    (upstream-source-version r2))
-                         r1 r2))
-                   first
-                   (coalesce-sources candidates)))))))
+    (define versions
+      (map upstream-source-version candidates))
+
+    (define new-version
+      (find-version versions version partial-version?))
+
+    (and new-version
+         (find (compose (cut string=? new-version <>)
+                        upstream-source-version)
+               candidates))))
 
 
 ;;;
@@ -743,7 +757,7 @@ else #f.  URL is assumed to fully specified."
            (call-with-gzip-input-port port
              (compose string->lines get-string-all))))))
 
-(define* (import-gnu-release package #:key (version #f))
+(define* (import-gnu-release package #:key version partial-version?)
   "Return the latest release of PACKAGE, a GNU package available via
 ftp.gnu.org. Optionally include a VERSION string to fetch a specific version.
 
@@ -776,12 +790,15 @@ list available from %GNU-FILE-LIST-URI over HTTP(S)."
                                     (string-contains file directory)
                                     (release-file? name (basename file))))
                              files))
-           ;; find latest version
-           (version (or version
-                        (and (not (null? relevant))
-                             (tarball->version
-                              (find-latest-tarball-version relevant)))))
-           ;; find tarballs matching this version
+           (versions (delay (sort (delete-duplicates
+                                   (map tarball->version relevant))
+                                  version>?)))
+           (version (or (and version partial-version?
+                             (find (cut version-prefix? version <>)
+                                   (force versions)))
+                        version
+                        (first (force versions))))
+           ;; Find tarballs matching this version.
            (tarballs (filter (lambda (file)
                                (string=? version (tarball->version file)))
                              relevant)))
@@ -998,11 +1015,11 @@ updater."
   (or (assoc-ref (package-properties package) 'release-monitoring-url)
       ((url-predicate http-url?) package)))
 
-(define* (import-html-updatable-release package #:key (version #f))
+(define* (import-html-updatable-release package #:key version partial-version?)
   "Return the latest release of PACKAGE else #f.  Do that by crawling the HTML
 page of the directory containing its source tarball.  Optionally include a
-VERSION string to fetch a specific version."
-
+VERSION string to fetch a specific version; which may be partially provided
+when PARTIAL-VERSION? is #t."
   (define (expand-uri uri)
     (match uri
       ((and (? string?) (? (cut string-prefix? "mirror://" <>) url))
@@ -1029,6 +1046,7 @@ VERSION string to fetch a specific version."
      (import-html-release base package
                           #:rewrite-url? #t
                           #:version version
+                          #:partial-version? partial-version?
                           #:directory directory))))
 
 (define %gnu-updater

guix/import/composer.scm
@@ -1,5 +1,6 @@
 ;;; GNU Guix --- Functional package management for GNU
 ;;; Copyright © 2019 Julien Lepiller <julien@lepiller.eu>
+;;; Copyright © 2024 Maxim Cournoyer <maxim.cournoyer@gmail.com>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -36,6 +37,7 @@
   #:use-module (guix upstream)
   #:use-module (guix utils)
   #:use-module (srfi srfi-1)
+  #:use-module (srfi srfi-2)
   #:use-module (srfi srfi-11)
   #:use-module (srfi srfi-26)
   #:export (composer->guix-package
@@ -57,10 +59,6 @@
     ((string-prefix? "v" version)
      (substring version 1))
     (else version)))
-
-(define (latest-version versions)
-  (fold (lambda (a b) (if (version>? (fix-version a) (fix-version b)) a b))
-        (car versions) versions))
 
 (define (json->require dict)
   (if dict
@@ -102,31 +100,25 @@
          (not (string-contains d "beta"))
          (not (string-contains d "rc")))))
 
-(define* (composer-fetch name #:key (version #f))
+(define* (composer-fetch name #:key version partial-version?)
   "Return a composer-package representation of the Composer metadata for the
-package NAME with optional VERSION, or #f on failure."
-  (let* ((url (string-append (%composer-base-url) "/p/" name ".json"))
-         (packages (and=> (json-fetch url)
-                          (lambda (pkg)
-                            (let ((pkgs (assoc-ref pkg "packages")))
-                              (or (assoc-ref pkgs name) pkg))))))
-    (if packages
-        (json->composer-package
-         (if version
-             (assoc-ref packages version)
-             (cdr
-              (fold
-               (lambda (new cur-max)
-                 (match new
-                   (((? valid-version? version) . tail)
-                    (if (version>? (fix-version version)
-                                   (fix-version (car cur-max)))
-                        (cons* version tail)
-                        cur-max))
-                   (_ cur-max)))
-               (cons* "0.0.0" #f)
-               packages))))
-        #f)))
+package NAME with optional VERSION, or #f on failure.  VERSION may be gien as
+version prefix if PARTIAL-VERSION? is #t."
+  (and-let* ((url (string-append (%composer-base-url) "/p/" name ".json"))
+             (packages (and=> (json-fetch url)
+                              (lambda (pkg)
+                                (let ((pkgs (assoc-ref pkg "packages")))
+                                  (or (assoc-ref pkgs name) pkg)))))
+             (all-versions (map car packages))
+             (valid-versions (filter valid-version? all-versions))
+             (version (or (find-version valid-versions version partial-version?)
+                          (and version
+                               ;; If the user-provided VERSION could not be
+                               ;; found, fallback to look through all
+                               ;; versions.
+                               (find-version all-versions version
+                                             partial-version?)))))
+    (json->composer-package (assoc-ref packages version))))
 
 (define (php-package-name name)
   "Given the NAME of a package on Packagist, return a Guix-compliant name for
@@ -246,10 +238,15 @@ package in Packagist."
    (downstream-name (php-package-name dependency))
    (type type)))
 
-(define* (import-release package #:key (version #f))
-  "Return an <upstream-source> for VERSION or the latest release of PACKAGE."
+(define* (import-release package #:key version partial-version?)
+  "Return an <upstream-source> for VERSION or the latest release of PACKAGE.
+If PARTIAL-VERSION? is #t, the provided VERSION may be a partial version
+prefix."
   (let* ((php-name (guix-package->composer-name package))
-         (composer-package (composer-fetch php-name #:version version)))
+         (composer-package (composer-fetch php-name
+                                           #:version version
+                                           #:partial-version?
+                                           partial-version?)))
     (if composer-package
         (upstream-source
          (package (composer-package-name composer-package))

guix/import/cpan.scm
@@ -328,7 +328,7 @@ in RELEASE, a <cpan-release> record."
                                              ")"))))
     (url-predicate (cut regexp-exec cpan-rx <>))))
 
-(define* (latest-release package #:key (version #f))
+(define* (latest-release package #:key version partial-version?)
   "Return an <upstream-source> for the latest release of PACKAGE."
   (when version
     (raise

guix/import/cran.scm
@@ -1028,7 +1028,7 @@ s-expression corresponding to that package, or #f on failure."
              (_ #f)))
           (_ #f)))))
 
-(define* (latest-cran-release pkg #:key (version #f))
+(define* (latest-cran-release pkg #:key version partial-version?)
   "Return an <upstream-source> for the latest release of the package PKG."
   (when version
     (error
@@ -1051,7 +1051,7 @@ s-expression corresponding to that package, or #f on failure."
           (urls (cran-uri upstream-name version))
           (inputs (cran-package-inputs meta 'cran))))))
 
-(define* (latest-bioconductor-release pkg #:key (version #f))
+(define* (latest-bioconductor-release pkg #:key version partial-version?)
   "Return an <upstream-source> for the latest release of the package PKG."
   (when version
     (error

commit 096dedd0bb13523002c814b001429c2f65b6f10d

import: texlive: Import the given version when specified.
Fixes a regression in ‘tests/texlive.scm’ introduced in commit
f13f0769688493271f43f31a016957355dbecb30 whereby ‘texlive-tags’ would be
called unconditionally, leading to an attempt to build Subversion.

* guix/import/texlive.scm (texlive->guix-package): Honor VERSION when it
is true.

Change-Id: I05e158fe55c65f7547c7150f00a589b4f600a40a

guix/import/texlive.scm
@@ -769,7 +769,8 @@ associated Guix package, or #f on failure.  Fetch metadata for a specific
 version whenever VERSION keyword is specified.  Otherwise, grab package latest
 release.  When DATABASE is provided, fetch metadata from there, ignoring
 VERSION."
-    (let ((version (find-version (texlive-tags) version #t)))
+    (let ((version (or version
+                       (find-version (texlive-tags) version #t))))
       (tlpdb->package name version (or database (tlpdb/cached version))))))
 
 (define* (texlive-recursive-import name #:key repo version)