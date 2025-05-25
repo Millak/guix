f96412be856e7346d3b17f78b7c8b6aefb955d0f
Alexis Praga <alexis@praga.dev>
2025-05-25 21:36:48 +0200

    gnu: emacs-dash: Update to 2.20.0.
    Change-Id: I3bea56c283c64c40495120d4fdd0994bb5e329db
    Signed-off-by: Ian Eure <ian@retrospec.tv>

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -6880,26 +6880,27 @@ next matching page.")
 (define-public emacs-dash
   (package
     (name "emacs-dash")
-    (version "2.19.1")
-    (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                    (url "https://github.com/magnars/dash.el")
-                    (commit version)))
-              (file-name (git-file-name name version))
-              (sha256
-               (base32
-                "0z6f8y1m9amhg427iz1d4xcyr6n0kj5w7kmiz134p320ixsdnzd8"))))
+    (version "2.20.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/magnars/dash.el")
+             (commit version)))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "092kf61bi6dwl42yng69g3y55ni8afycqbpaqx9wzf8frx9myg6m"))))
     (build-system emacs-build-system)
     (arguments
-     (list #:tests? #t
-           #:phases
-           #~(modify-phases %standard-phases
-               (add-after 'unpack 'disable-byte-compile-error-on-warn
-                 (lambda _
-                   (substitute* "Makefile"
-                     (("\\(setq byte-compile-error-on-warn t\\)")
-                      "(setq byte-compile-error-on-warn nil)")))))))
+     (list
+      #:tests? #t
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-after 'unpack 'disable-byte-compile-error-on-warn
+            (lambda _
+              (substitute* "Makefile"
+                (("\\(setq byte-compile-error-on-warn t\\)")
+                 "(setq byte-compile-error-on-warn nil)")))))))
     (home-page "https://github.com/magnars/dash.el")
     (synopsis "Modern list library for Emacs")
     (description "This package provides a modern list API library for Emacs.")
