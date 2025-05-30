9b253deb5fa0f32fba364f1e5ee0d826dc98d419
Danny Milosavljevic <dannym@friendly-machines.com>
2025-05-31 01:34:10 +0200

    gnu: Add rust-askama-escape@0.10.3.
    * gnu/packages/crates-web.scm (rust-askama-escape-0.10): New variable.
    
    Change-Id: Ibe9979908cfac916ee4b62cc11cd6dfacdb08d8f

--- gnu/packages/crates-web.scm
+++ gnu/packages/crates-web.scm
@@ -8998,3 +8998,22 @@ GUIs as desktop applications.")
     (description "This package provides HTTP mocking to test Rust
 applications.")
     (license (list license:expat license:asl2.0))))
+
+(define-public rust-askama-escape-0.10
+  (package
+    (name "rust-askama-escape")
+    (version "0.10.3")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (crate-uri "askama_escape" version))
+       (file-name (string-append name "-" version ".tar.gz"))
+       (sha256
+        (base32 "0hg3rz0cma5f6385z7qmqw3jbir76jndwd5s7dqfk92v9gil75v1"))))
+    (build-system cargo-build-system)
+    (arguments
+     `(#:skip-build? #t))
+    (home-page "https://askama.readthedocs.io/")
+    (synopsis "HTML escaping, extracted from Askama")
+    (description "This package provides HTML escaping, extracted from Askama.")
+    (license (list license:expat license:asl2.0))))
