acf0086a1937f30730102d87bfc07b264e44c3e8
Sharlatan Hellseher <sharlatanus@gmail.com>
2025-06-10 22:57:39 +0100

    gnu: Add go-go-opentelemetry-io-contrib-propagators-b3.
    * gnu/packages/golang-web.scm (go-go-opentelemetry-io-contrib-propagators-b3): New variable.
    
    Change-Id: Ide2a6193931bca50898cb4b872a4a50a503765ba

--- gnu/packages/golang-web.scm
+++ gnu/packages/golang-web.scm
@@ -11529,6 +11529,38 @@ defaults (a combined @code{TraceContext} and Baggage
 OTEL_PROPAGATORS environment variable.")
     (license license:asl2.0)))
 
+(define-public go-go-opentelemetry-io-contrib-propagators-b3
+  (package
+    (name "go-go-opentelemetry-io-contrib-propagators-b3")
+    (version "1.36.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/open-telemetry/opentelemetry-go-contrib")
+             (commit (go-version->git-ref version
+                                          #:subdir "propagators/b3"))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "15k2xps4qv37pw3ml6pj98kihl6a04bbr005px5hhckl23s40w2c"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:import-path "go.opentelemetry.io/contrib/propagators/b3"
+      #:unpack-path "go.opentelemetry.io/contrib"))
+    (native-inputs
+     (list go-github-com-google-go-cmp
+           go-github-com-stretchr-testify))
+    (propagated-inputs
+     (list go-go-opentelemetry-io-otel
+           go-go-opentelemetry-io-otel-trace))
+    (home-page "https://go.opentelemetry.io/contrib")
+    (synopsis "OpenTracing B3 propagator library")
+    (description
+     "Package b3 implements the B3 propagator specification as defined at
+@@url{https://github.com/openzipkin/b3-propagation,}.")
+    (license license:asl2.0)))
+
 (define-public go-go-opentelemetry-io-contrib-propagators-jaeger
   (package
     (name "go-go-opentelemetry-io-contrib-propagators-jaeger")
