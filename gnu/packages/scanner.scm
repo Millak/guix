1745a6aae6e736829f053b8e312cb0f55a1ecdd1
Sergey Trofimov <sarg@sarg.org.ru>
2025-06-12 14:40:30 +0200

    gnu: scanbd: Format inputs.
    * gnu/packages/scanner.scm (scanbd)[inputs]: Use new format.
    
    Change-Id: I82d4d013b93cf445056bfe3a0c78addef367b0db

--- gnu/packages/scanner.scm
+++ gnu/packages/scanner.scm
@@ -348,11 +348,7 @@ standard.")
     (native-inputs
      (list pkg-config))
     (inputs
-     `(("dbus" ,dbus)
-       ("libconfuse" ,libconfuse)
-       ("sane-backends" ,sane-backends)
-       ("udev" ,eudev)
-       ("zlib" ,zlib)))
+     (list dbus libconfuse sane-backends eudev zlib))
     (home-page "https://scanbd.sourceforge.io")
     (synopsis "Configurable scanner button monitor")
     (description "Scanbd stands for scanner button daemon.  It regularly polls
