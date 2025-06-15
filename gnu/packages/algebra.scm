f51ac16d2a897935454dee0ea9e48da4b17ae91b
Sharlatan Hellseher <sharlatanus@gmail.com>
2025-06-15 20:53:16 +0100

    gnu: packages/algebra: Select only pypi-uri from python build-system.
    python-build-system is no longer in use in this module but
    pyproject-build-system misses PYPI-URI symbol yet, this change load
    just it.
    
    * gnu/packages/algebra.scm: Load just PYPI-URI from python-build-system.
    
    Change-Id: Ic394f3ea3e9e068abc108fe2280582c855819cdf

--- gnu/packages/algebra.scm
+++ gnu/packages/algebra.scm
@@ -82,7 +82,7 @@
   #:use-module (guix build-system gnu)
   #:use-module (guix build-system cmake)
   #:use-module (guix build-system pyproject)
-  #:use-module (guix build-system python)
+  #:use-module ((guix build-system python) #:select (pypi-uri))
   #:use-module (guix build-system r)
   #:use-module (guix download)
   #:use-module (guix gexp)
