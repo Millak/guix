b3f412a5f26ef411f7da29a3b6304c05c847a632
fanquake <fanquake@gmail.com>
2025-06-16 11:50:55 +0100

    gnu: Remove unused bazaar patch.
    * gnu/packages/patches/bazaar-CVE-2017-14176.patch: Remove file.
    * gnu/local.mk (dist_patch_DATA): Remove it.
    
    Change-Id: Iec644995544afb79ff80aa6b5466da083e6f0b86
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/local.mk
+++ gnu/local.mk
@@ -1043,7 +1043,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/bash-completion-directories.patch	\
   %D%/packages/patches/bash-linux-pgrp-pipe.patch		\
   %D%/packages/patches/bastet-change-source-of-unordered_set.patch	\
-  %D%/packages/patches/bazaar-CVE-2017-14176.patch		\
   %D%/packages/patches/bc-fix-cross-compilation.patch		\
   %D%/packages/patches/bdb-5.3-atomics-on-gcc-9.patch		\
   %D%/packages/patches/biboumi-cmake-ignore-git.patch		\
