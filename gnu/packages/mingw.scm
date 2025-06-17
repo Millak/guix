8e874ded5c89091581a100c3150a084b9ae18f8f
fanquake <fanquake@gmail.com>
2025-06-17 09:19:16 +0100

    gnu: mingw-w64-tools: Update to 13.0.0.
    * gnu/packages/mingw.scm (mingw-w64-tools): Update to 13.0.0.
    
    Change-Id: Ie83c32f24eb6028864c96267b48a795a266269cf
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/mingw.scm
+++ gnu/packages/mingw.scm
@@ -153,7 +153,7 @@ several new APIs such as DirectX and DDK, and 64-bit support.")
 (define-public mingw-w64-tools
   (package
     (name "mingw-w64-tools")
-    (version "12.0.0")
+    (version "13.0.0")
     (source
      (origin
        (method url-fetch)
@@ -161,7 +161,7 @@ several new APIs such as DirectX and DDK, and 64-bit support.")
              "mirror://sourceforge/mingw-w64/mingw-w64/"
              "mingw-w64-release/mingw-w64-v" version ".tar.bz2"))
        (sha256
-        (base32 "0bzdprdrb8jy5dhkl2j2yhnr2nsiv6wk2wzxrzaqsvjbmj58jhfc"))))
+        (base32 "1bwchaannz49ckg295diyvp4k3sk3p3fwidgv9kvzvf4ylm85zjs"))))
     (build-system gnu-build-system)
     (arguments
      (list
