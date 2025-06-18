a694de3938f0cf783a4db098521cfd0821d4ffe3
Kjartan Oli Agustsson <kjartanoli@outlook.com>
2025-06-18 00:39:12 +0000

    gnu: clipper-peak: Use pyproject-build-system.
    * gnu/packages/bioinformatics.scm (clipper-peak): [build-system]: Use
    pyproject.
    [native-inputs]: Add python-setuptools and python-wheel.
    
    Change-Id: I6cae7d463a412b6736413e80346e377ccb14c290
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/bioinformatics.scm
+++ gnu/packages/bioinformatics.scm
@@ -6093,7 +6093,7 @@ databases.")
                '(begin
                   ;; Delete pre-compiled files.
                   (delete-file "clipper/src/peaks.so")))))
-    (build-system python-build-system)
+    (build-system pyproject-build-system)
     (arguments
      `(#:tests? #false
        #:phases
@@ -6133,6 +6133,8 @@ setup"))))
            python-scipy))
     (native-inputs
      (list python-setuptools-git
+           python-setuptools
+           python-wheel
            python-mock ; for tests
            python-nose ; for tests
            python-pytz)) ; for tests
