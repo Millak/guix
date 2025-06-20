98a3f273accd27ba25016fde8d06a51a0a70566e
Igorj Gorjaĉev <igor@goryachev.org>
2025-06-20 11:46:39 +0300

    gnu: erlang: Update to 27.3.4.1.
    * gnu/packages/erlang.scm (erlang): Update to 27.3.4.1.
    
    Change-Id: I0dbc21ee1d83b88ba1a7491fa8e9bc1c817e62bd
    Signed-off-by: Andreas Enge <andreas@enge.fr>

--- gnu/packages/erlang.scm
+++ gnu/packages/erlang.scm
@@ -49,7 +49,7 @@
 (define-public erlang
   (package
     (name "erlang")
-    (version "27.3.4")
+    (version "27.3.4.1")
     (source (origin
               (method git-fetch)
               ;; The tarball from http://erlang.org/download contains many
@@ -61,7 +61,7 @@
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "0ha9xyhgwgcv1ln2hx84znjpvl702c0wsdwfiq442f06qx9bskvn"))
+                "0psayp39priibc0clkhx4h593iar2sp0lg9zls4l123854cq8m8m"))
               (patches (search-patches "erlang-man-path.patch"))))
     (build-system gnu-build-system)
     (native-inputs
@@ -74,7 +74,7 @@
                                "/OTP-" version "/otp_doc_man_" version ".tar.gz"))
            (sha256
             (base32
-             "0d07bg7q8xlhbvf6k7xdy4xic5gq9g3dkxn4vsssg9pljs0zagq8"))))))
+             "03333khh6qqkp7rfagb9jqdgcywc2vz2305fva9rn50h3i6v9cnk"))))))
     (inputs
      (list ncurses openssl wxwidgets))
     (propagated-inputs
