b7edabbad8b8a176dcab7a2a5dbfdd0c543831bd
jgart <jgart@dismail.de>
2025-06-25 16:50:41 -0400

    gnu: Add clasp-lisp-critic.
    * gnu/packages/lisp-check.scm (clasp-lisp-critic): New variable.
    
    Change-Id: I6daa32db5e3992cf8b95bdade54a9ff021668d87

--- gnu/packages/lisp-check.scm
+++ gnu/packages/lisp-check.scm
@@ -1233,3 +1233,6 @@ on the XPTest package by Craig Brozensky and the JUnit package by Kent Beck.")
 
 (define-public ecl-lisp-critic
   (sbcl-package->ecl-package sbcl-lisp-critic))
+
+(define-public clasp-lisp-critic
+  (sbcl-package->clasp-package sbcl-lisp-critic))
