21e95b37889c61ee7e597e13bc6e1afc72187036
jgart <jgart@dismail.de>
2025-06-25 16:05:13 -0400

    gnu: Add clasp-charje.documentation.
    * gnu/packages/lisp-xyz.scm (clasp-charje.documentation): New variable.
    
    Change-Id: I42ba0247459eae4683f4d3cfceff4ac8350da495

--- gnu/packages/lisp-xyz.scm
+++ gnu/packages/lisp-xyz.scm
@@ -3451,6 +3451,9 @@ initialization methods.")
 (define-public ecl-charje.documentation
   (sbcl-package->ecl-package sbcl-charje.documentation))
 
+(define-public clasp-charje.documentation
+  (sbcl-package->clasp-package sbcl-charje.documentation))
+
 (define-public sbcl-charje.loop
   (package
     (name "sbcl-charje.loop")
