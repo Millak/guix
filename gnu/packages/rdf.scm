3dcd1307d123838424d5b296bd7ab10130283b8a
Nicolas Graves <ngraves@ngraves.fr>
2025-06-26 11:53:53 +0200

    gnu: redland: Update to 1.0.17-0.3ec9bda.
    * gnu/packages/rdf.scm (redland): Update to 1.0.17-0.3ec9bda.
      [source]: Switch to git-fetch.
      [arguments]{phases}: Add phases 'bootstrap-libtool and
      'disable-problematic-test.
      [native-inputs]: Add autoconf, automake, libtool, gtk-doc.
    
    Change-Id: I388b4f396eb0fee8949927dce5982b665d46be31
    Signed-off-by: Andreas Enge <andreas@enge.fr>

--- gnu/packages/rdf.scm
+++ gnu/packages/rdf.scm
@@ -50,6 +50,7 @@
   #:use-module (gnu packages documentation)
   #:use-module (gnu packages dbm)
   #:use-module (gnu packages gnupg)
+  #:use-module (gnu packages gtk)
   #:use-module (gnu packages kerberos)
   #:use-module (gnu packages linux)
   #:use-module (gnu packages multiprecision)
@@ -255,29 +256,58 @@ Turtle/N3 and read them in SPARQL XML, RDF/XML and Turtle/N3.")
     (license license:lgpl2.1+))) ; or any choice of gpl2+ or asl2.0
 
 (define-public redland
-  (package
-    (name "redland")
-    (version "1.0.17")
-    (source (origin
-             (method url-fetch)
-             (uri (string-append "http://download.librdf.org/source/" name
-                                 "-" version ".tar.gz"))
-             (sha256
-              (base32
-               "109n0kp39p966dpiasad2bb7q66rwbcb9avjvimw28chnpvlf66y"))))
-    (build-system gnu-build-system)
-    (native-inputs
-     (list perl ; needed for installation
-           pkg-config))
-    (propagated-inputs
-     (list rasqal)) ; in Requires.private field of .pc
-    (inputs
-     (list bdb))
-    (home-page "https://librdf.org/")
-    (synopsis "RDF library")
-    (description "The Redland RDF Library (librdf) provides the RDF API
-and triple stores.")
-    (license license:lgpl2.1+))) ; or any choice of gpl2+ or asl2.0
+  ;; XXX: No tags for the last release, altough it's clear from
+  ;; Github and logs a new release has been made.
+  (let ((commit "3ec9bda623107f9b1c86c0a3f261ffd3f8a40965")
+        (revision "0"))
+    (package
+      (name "redland")
+      (version (git-version "1.0.17" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/dajobe/librdf")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "0qwxmwi96lqjqi1mdx0mfbw23a5xg20xbv8bv18pmr4h4fxr51q7"))))
+      (build-system gnu-build-system)
+      (arguments
+       (list
+        #:phases
+        #~(modify-phases %standard-phases
+            ;; XXX: Copied files from libtool have been generated with
+            ;; another version of autoconf.
+            (add-after 'bootstrap 'bootstrap-libtool
+              (lambda _
+                (with-directory-excursion "libltdl"
+                  (delete-file "aclocal.m4")
+                  (invoke "autoreconf" "-vfi"))))
+            ;; XXX: 1/17 fails with recent compilers. No patch seems to have
+            ;; been done for Nix nor Debian.
+            (add-after 'unpack 'disable-problematic-test
+              (lambda _
+                (substitute* "src/Makefile.am"
+                  (("test rdf_parser_test")
+                   "test")))))))
+      (native-inputs
+       (list autoconf
+             automake
+             libtool
+             gtk-doc
+             perl ; needed for installation
+             pkg-config))
+      (propagated-inputs
+       (list rasqal)) ; in Requires.private field of .pc
+      (inputs
+       (list bdb))
+      (home-page "https://librdf.org/")
+      (synopsis "RDF library")
+      (description
+       "The Redland RDF Library (librdf) provides the RDF API and triple
+stores.")
+      (license license:lgpl2.1+)))) ; or any choice of gpl2+ or asl2.0
 
 (define-public serd
   (package
