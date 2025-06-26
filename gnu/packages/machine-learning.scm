Millak/guix

commit 95ca1a54493279a146ace2410d78062fa4e091de

gnu: xnnpack: Update to 0.0-4.51a0103.
* gnu/packages/machine-learning.scm (xnnpack): Update to 0.0-4.51a0103.
[source]: Remove deprecated -a flag from update-microkernels.py invocation.
[snippet]: Adjust directory list for autogenerated file removal.

Signed-off-by: Ayan Das <bvits@riseup.net>
Signed-off-by: Ludovic Courtès <ludo@gnu.org>

gnu/packages/machine-learning.scm
@@ -4513,8 +4513,8 @@ on quantized 8-bit tensors.")
 (define-public xnnpack
   ;; There's currently no tag on this repo.
   (let ((version "0.0")
-        (commit "08f1489fc815e8f121d4d2676c4863d2b51bfe73")
-        (revision "3"))
+        (commit "51a0103656eff6fc9bfd39a4597923c4b542c883")
+        (revision "4"))
     (package
       (name "xnnpack")
       (version (git-version version revision commit))
@@ -4526,7 +4526,7 @@ on quantized 8-bit tensors.")
          (file-name (git-file-name name version))
          (sha256
           (base32
-           "00jjhz0nfggbdnqqvcznba03pcyy7gssd24yhhzjhincnz9qh8jr"))
+           "0j4smj8yaxw6r14caqn0dgb8j8mnsq34zp8ixxn11zmgbab306ly"))
          (modules '((guix build utils)
                     (ice-9 ftw)
                     (ice-9 textual-ports)
@@ -4548,7 +4548,7 @@ on quantized 8-bit tensors.")
                        (delete-file path))))
                  (scandir dir (negate (cut member <> '("." ".." "simd"))))))
               (cons*
-               "test" "bench" "eval" "models" "src/enums" "src/xnnpack"
+               "test" "bench/models" "src/enums" "src/xnnpack"
                "gen" "cmake/gen"
                (filter
                 identity
@@ -4625,7 +4625,7 @@ on quantized 8-bit tensors.")
                 ;; These need to run after the above scripts
                 (display "Remaining files\n")
                 (invoke "python3" "tools/update-microkernels.py")
-                (invoke "python3" "tools/update-microkernels.py" "-a")
+                ;; The -a flag is no longer supported in newer versions
                 (invoke "python3" "tools/generate-lut-norm-test.py"
                         "--spec" "test/u8-lut32norm.yaml"
                         "--output" "test/u8-lut32norm.cc")

commit 4fec118cdd7be2c3604d162788da6a9d4d357118

gnu: xnnpack: Remove remaining autogenerated files.
Fixes a regression introduced in 95ca1a54493279a146ace2410d78062fa4e091de.

* gnu/packages/machine-learning.scm (xnnpack)[source]<snippet>: Remove
autogenerated files in "bench".

Change-Id: I3874fe1fe4b6dcad7255d75313bc8639d483bf31
Signed-off-by: Ludovic Courtès <ludo@gnu.org>

gnu/packages/machine-learning.scm
@@ -4533,6 +4533,7 @@ on quantized 8-bit tensors.")
                     (srfi srfi-26)))
          (snippet
           '(begin
+             (delete-file-recursively "bench/models")
              ;; Remove autogenerated files, which contain the string
              ;; "Auto-generated file"
              (for-each
@@ -4548,7 +4549,7 @@ on quantized 8-bit tensors.")
                        (delete-file path))))
                  (scandir dir (negate (cut member <> '("." ".." "simd"))))))
               (cons*
-               "test" "bench/models" "src/enums" "src/xnnpack"
+               "test" "bench" "src/enums" "src/xnnpack"
                "gen" "cmake/gen"
                (filter
                 identity
@@ -4625,7 +4626,6 @@ on quantized 8-bit tensors.")
                 ;; These need to run after the above scripts
                 (display "Remaining files\n")
                 (invoke "python3" "tools/update-microkernels.py")
-                ;; The -a flag is no longer supported in newer versions
                 (invoke "python3" "tools/generate-lut-norm-test.py"
                         "--spec" "test/u8-lut32norm.yaml"
                         "--output" "test/u8-lut32norm.cc")