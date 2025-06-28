3ed66191d11ef4bb51629b3304f6a1f8288a5122
Christopher Baines <mail@cbaines.net>
2025-06-28 09:51:53 +0200

    gnu: guix-data-service: Update to 0.0.1-70.40212ea.
    * gnu/packages/web.scm (guix-data-service): Update to 0.0.1-70.40212ea.
    
    Change-Id: I818a47a6817841149dbb8da0261da5d7bd712d83

--- gnu/packages/web.scm
+++ gnu/packages/web.scm
@@ -5419,8 +5419,8 @@ Cloud.")
     (license license:expat)))
 
 (define-public guix-data-service
-  (let ((commit "9b79543a734857ccf10fdb099b31ee6e5156aaea")
-        (revision "69"))
+  (let ((commit "40212ea053309341e5b35cc962e9404ff0ccea6b")
+        (revision "70"))
     (package
       (name "guix-data-service")
       (version (string-append "0.0.1-" revision "." (string-take commit 7)))
@@ -5432,7 +5432,7 @@ Cloud.")
                 (file-name (git-file-name name version))
                 (sha256
                  (base32
-                  "06nq1ql3r146rlajsihxljf7rs2wkcszfx756cdckyrdi4nwm563"))))
+                  "1lxq7kfrih2h19r2gvpbvvcdbqfpqlgkm2r8hq5v1dj8bwqb02mj"))))
       (build-system gnu-build-system)
       (arguments
        (list
