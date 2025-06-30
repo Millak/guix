4f03b88b8ee530064e3a5f43c3925a7619f2a431
Matthias Riße <matrss@0px.xyz>
2025-06-30 16:44:14 +0200

    gnu: git-annex: Update to 10.20250630.
    * gnu/packages/haskell-apps.scm (git-annex): Update to 10.20250630.
    
    Change-Id: I7a6bf5d8828ff832e4793787fb7bc39587e425b4
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/packages/haskell-apps.scm
+++ gnu/packages/haskell-apps.scm
@@ -297,7 +297,7 @@ to @code{cabal repl}).")
 (define-public git-annex
   (package
     (name "git-annex")
-    (version "10.20250605")
+    (version "10.20250630")
     (source
      (origin
        ;; hackage release doesn't include everything needed for extra bits.
@@ -307,7 +307,7 @@ to @code{cabal repl}).")
               (commit version)))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "1y1q5mh0sglwan40gvwvn87yb5aqvkmizbhz4hsgm4x6d1r1ckvd"))))
+        (base32 "1livig1x1a929qagisjx33pzmgv38d6fzf11bmy2jfpgvvlk9mfx"))))
     (build-system haskell-build-system)
     (properties '((upstream-name . "git-annex")))
     (arguments
