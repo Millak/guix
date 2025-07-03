da957db871ff245306612f8ce88518c143e76b93
fanquake <fanquake@gmail.com>
2025-07-03 14:53:29 +0100

    gnu: cross-base: mingw: use compressed debug sections
    I can no-longer recreate the mentioned failure.
    
    * gnu/packages/cross-base.scm (cross-binutils-package): Remove the usage
      of --enable-compressed-debug-sections=no.
    
    Change-Id: Iff64696a3a9630c026fc72689fc927aa33e26c4b
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>
    Merges: #985

--- gnu/packages/cross-base.scm
+++ gnu/packages/cross-base.scm
@@ -135,20 +135,10 @@
                                         (search-patches
                                          "ath9k-htc-firmware-binutils.patch")))
                  ((target-mingw? target)
-                  (package-with-extra-patches
-                   (package-with-extra-configure-variable
-                    ;; mingw binutils does not work correctly when configured
-                    ;; with `--enable-compressed-debug-sections`. An error
-                    ;; like the following will occur whenever you try to link:
-                    ;;
-                    ;;   x86_64-w64-mingw32-ld: final link failed: bad value
-                    ;;
-                    ;; TODO: This seems like a deeper problem that warrants
-                    ;; deeper investigation.
-                    binutils "--enable-compressed-debug-sections" "no")
-                   (search-patches
-                    "binutils-cross-windres.patch"
-                    "binutils-mingw-w64-deterministic.patch")))
+                  (package-with-extra-patches binutils
+                                              (search-patches
+                                               "binutils-cross-windres.patch"
+                                               "binutils-mingw-w64-deterministic.patch")))
                  (else binutils))
            target)))
 
