868825bc86d44b0927b46b8dd621d85c0ca09dcb
Nigko Yerden <nigko.yerden@gmail.com>
2025-07-03 18:31:33 +0500

    gnu: Add cdk.
    * gnu/packages/ncurses.scm (cdk): New variable.
    
    Change-Id: Ic2f1fd7241a48a09657700a0466d1e042bc52a55
    
    Change-Id: Iad54af59d7197958d300db4f78a043ae0dbb0e4f
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/ncurses.scm
+++ gnu/packages/ncurses.scm
@@ -254,6 +254,33 @@ ncursesw library provides wide character support.")
        ((#:configure-flags cf)
         #~(cons "--with-termlib=tinfo" #$cf))))))
 
+(define-public cdk
+  (package
+    (name "cdk")
+    (version "5.0-20250116")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append
+                    "https://invisible-mirror.net/archives/cdk/cdk-"
+                    version ".tgz"))
+              (sha256
+               (base32
+                "10ywvbnxwk6sfvlbkzfnbx9whsq9xv227znaiir742ym4h9d800m"))))
+    (build-system gnu-build-system)
+    ;; NOTE: This package does not have automated tests.
+    (arguments
+     (list #:configure-flags #~'("--with-shared" "--enable-rpath")))
+    (inputs
+     (list ncurses))
+    (synopsis "Curses widgets")
+    (description "Curses Development Kit (CDK) is a C library of widgets
+which allow a programmer to quickly create TUI applications.  Widgets are
+objects whose appearance and behavior can be customized, e.g. a pulldown
+menu or a file-viewer.")
+    (home-page "https://invisible-island.net/cdk/cdk.html")
+    ;; The license is identified as MIT-X11 (Expat) by the author.
+    (license (non-copyleft "file://share/doc/cdk/COPYING"))))
+
 (define-public dialog
   (package
     (name "dialog")
