fc2726355c79906d6d501aa949ad24f451884b6a
Sharlatan Hellseher <sharlatanus@gmail.com>
2025-07-06 22:58:04 +0100

    gnu: Remove python-nose-random.
    * gnu/packages/check.scm (python-nose-random): Delete variable.
    
    Change-Id: I208301de2f83e914cfacb0888165a115b2d00acf

--- gnu/packages/check.scm
+++ gnu/packages/check.scm
@@ -3531,32 +3531,6 @@ JSON APIs with Behave.")
 directories to be excluded from testing.")
     (license license:lgpl2.1+)))
 
-(define-public python-nose-random
-  (package
-    (name "python-nose-random")
-    (version "1.0.0")
-    (source
-     (origin
-      (method git-fetch)
-      (uri (git-reference
-            (url "https://github.com/fzumstein/nose-random")
-            (commit version)))
-      (file-name (git-file-name name version))
-      (sha256
-       (base32
-        "1dvip61r2frjv35mv6mmfjc07402z73pjbndfp3mhxyjn2zhksw2"))))
-    (build-system python-build-system)
-    (native-inputs
-     (list python-nose))
-    (home-page "https://github.com/fzumstein/nose-random")
-    (synopsis "Nose plugin to facilitate randomized unit testing with
-Python")
-    (description "Python nose-random is designed to facilitate
-Monte-Carlo style unit testing.  The idea is to improve testing by
-running your code against a large number of randomly generated input
-scenarios.")
-    (license license:expat)))
-
 (define-public python-nose-timer
   (package
     (name "python-nose-timer")
