230ad0e3370e7a7a927d54dff33d2cee8b6300f9
Mathieu Othacehe <othacehe@gnu.org>
2025-07-07 10:20:33 +0200

    Revert "gnu: mate: Replace 'union-build' with 'propagated-inputs'."
    This reverts commit b7a8f886f503e88b339886c97bc392d9d3848092 because it breaks
    system tests as discussed here: https://issues.guix.gnu.org/48265.
    
    Change-Id: Ib00696db10475e049ada892004c1164933a78066

--- gnu/packages/mate.scm
+++ gnu/packages/mate.scm
@@ -1586,8 +1586,19 @@ menu specification.")
     (version (package-version mate-desktop))
     (source #f)
     (build-system trivial-build-system)
-    (arguments '(#:builder (mkdir %output)))
-    (propagated-inputs
+    (arguments
+     `(#:modules ((guix build union))
+       #:builder
+       (begin
+         (use-modules (ice-9 match)
+                      (guix build union))
+         (match %build-inputs
+           (((names . directories) ...)
+            (union-build (assoc-ref %outputs "out")
+                         directories)
+            #t)))))
+    (native-inputs (list desktop-file-utils))
+    (inputs
      ;; TODO: Add more packages
      (append (if (or (%current-target-system)
                      (supported-package? gnome-keyring))
@@ -1599,11 +1610,9 @@ menu specification.")
                    dbus
                    dconf
                    dconf-editor
-                   desktop-file-utils
                    engrampa
                    eom
                    font-abattis-cantarell
-                   font-dejavu          ;default font
                    glib-networking
                    gvfs
                    hicolor-icon-theme
@@ -1637,6 +1646,9 @@ menu specification.")
                    shared-mime-info
                    yelp
                    zenity)))
+    (propagated-inputs
+     ;; Default font that applications such as IceCat require.
+     (list font-dejavu))
     (synopsis "The MATE desktop environment")
     (home-page "https://mate-desktop.org/")
     (description
