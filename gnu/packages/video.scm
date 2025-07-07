1b1fb4693ece6f129e85dd9f4d989f4863968015
Antoine Côté <antoine.cote@posteo.net>
2025-07-07 16:16:29 -0400

    gnu: dav1d: Update to 1.5.1.
    * gnu/packages/video.scm (dav1d): Update to 1.5.1.
    
    Change-Id: I2a2cbf458a867f3e0c98328e567d2f4b194abe14
    Signed-off-by: Andreas Enge <andreas@enge.fr>

--- gnu/packages/video.scm
+++ gnu/packages/video.scm
@@ -45,7 +45,7 @@
 ;;; Copyright © 2020 Brett Gilio <brettg@gnu.org>
 ;;; Copyright © 2020 Alexandru-Sergiu Marton <brown121407@posteo.ro>
 ;;; Copyright © 2020 Ivan Kozlov <kanichos@yandex.ru>
-;;; Copyright © 2020 Antoine Côté <antoine.cote@posteo.net>
+;;; Copyright © 2020, 2025 Antoine Côté <antoine.cote@posteo.net>
 ;;; Copyright © 2020 Hartmut Goebel <h.goebel@crazy-compilers.com>
 ;;; Copyright © 2021 Alexey Abramov <levenson@mmer.org>
 ;;; Copyright © 2021, 2022, 2023 Andrew Tropin <andrew@trop.in>
@@ -6161,7 +6161,7 @@ and audio capture, network stream playback, and many more.")
 (define-public dav1d
   (package
     (name "dav1d")
-    (version "1.5.0")
+    (version "1.5.1")
     (source
       (origin
         (method git-fetch)
@@ -6170,7 +6170,7 @@ and audio capture, network stream playback, and many more.")
                (commit version)))
         (file-name (git-file-name name version))
         (sha256
-         (base32 "0rn8zvmqapjq4r9s9hlpz1866war4ap7hzp5h8qhm5igry7i1qvq"))))
+         (base32 "1n0b5m4srdjsqg15plc9fng5kjv14ygqahjxy8863fbzhr13vjx9"))))
     (build-system meson-build-system)
     (native-inputs
      (if (target-x86?)
