6c53b11e1cd9a4a25a6a43b8514dd5ec37b77c80
Cayetano Santos <csantosb@inventati.org>
2025-07-11 21:51:29 +0200

    gnu: symbiyosys: Update to 0.55.
    * gnu/packages/electronics.scm (symbiyosys): Update to 0.55.
    
    Change-Id: Ib1a8277b765283697136ee22f4e9ca15dd933123
    Signed-off-by: Maxim Cournoyer <maxim@guixotic.coop>

--- gnu/packages/electronics.scm
+++ gnu/packages/electronics.scm
@@ -861,7 +861,7 @@ them usable as simple logic analyzer and/or oscilloscope hardware.")
 (define-public symbiyosys
   (package
     (name "symbiyosys")
-    (version "0.54")
+    (version "0.55")
     (source
      (origin
        (method git-fetch)
@@ -870,7 +870,7 @@ them usable as simple logic analyzer and/or oscilloscope hardware.")
              (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "072j4i9d4wn56w8vzdwgqhwf0q0jswlyf6sa21qkw0drwawyg0hm"))))
+        (base32 "1nxaijz7afpa1y8i4pbpadgv7kpz8rk02j42kxjpv117lxd3g9za"))))
     (build-system gnu-build-system)
     (arguments
      (list
