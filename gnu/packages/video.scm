7ea1bcbd43d201b06df80d29f1757fb4527d15fd
Maxim Cournoyer <maxim@guixotic.coop>
2025-07-12 23:08:16 +0900

    gnu: ffmpeg-7: Enable vulkan support.
    * gnu/packages/video.scm (ffmpeg-7) [native-inputs]: Add vulkan-headers.
    [inputs]: Add shaderc, spirv-tools and vulkan-loader.
    [configure-flags]: Add --enable-libshaderc and --enable-vulkan.
    
    Change-Id: I469b013f6fc4d85a86004615948d3a03c9c3efde

--- gnu/packages/video.scm
+++ gnu/packages/video.scm
@@ -1655,7 +1655,7 @@ operate properly.")
                                   version ".tar.xz"))
               (sha256
                (base32
-                "12dkv17mrsdqrm70c30azjw7qi1lfxca7xisw81x6flacddm2il6"))))
+                "1c837agaw8ljhjx6ndp2w7hffi2mkb22vnmb8v0fbfqdbqwq8fbk"))))
     (outputs '("out" "debug"))
     (build-system gnu-build-system)
     (inputs
@@ -1686,13 +1686,16 @@ operate properly.")
             mesa
             openal
             pulseaudio
+            shaderc
             sdl2
             soxr
             speex
+            spirv-tools
             srt
             svt-av1
             twolame
             vidstab
+            vulkan-loader
             x265
             xvid
             zlib)))
@@ -1702,6 +1705,7 @@ operate properly.")
            pkg-config
            texinfo
            speex
+           vulkan-headers
            yasm))
     (arguments
      (list
@@ -1776,6 +1780,7 @@ operate properly.")
          #$@(if (this-package-input "rav1e")
                 '("--enable-librav1e")
                 '())
+         "--enable-libshaderc"
          "--enable-libsoxr"
          "--enable-libspeex"
          "--enable-libsrt"
@@ -1793,6 +1798,7 @@ operate properly.")
          "--enable-opengl"
          "--enable-libdrm"
          "--enable-vaapi"
+         "--enable-vulkan"
 
          "--enable-runtime-cpudetect"
 
