110c96a5162d3a9d139eb68907013c6fc0aad0e5
Zheng Junjie <z572@z572.online>
2025-07-12 17:54:10 +0800

    gnu: capnproto: Fix tests.
    * gnu/packages/patches/capnproto-fix-test.patch: New patch.
    * gnu/local.mk (dist_patch_DATA): Register it.
    * gnu/packages/serialization.scm (capnproto)[source]: Use it.
    
    Change-Id: I216563e5263f6d4e61e382b8d1eb5687c82b0cc1

--- gnu/local.mk
+++ gnu/local.mk
@@ -1089,6 +1089,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/calls-disable-application-test.patch	\
   %D%/packages/patches/calls-disable-sip-test.patch		\
   %D%/packages/patches/camlboot-dynamically-allocate-stack-signal.patch	\
+  %D%/packages/patches/capnproto-fix-test.patch		\
   %D%/packages/patches/capstone-fix-python-constants.patch	\
   %D%/packages/patches/catdoc-CVE-2017-11110.patch		\
   %D%/packages/patches/ccextractor-add-missing-header.patch	\
