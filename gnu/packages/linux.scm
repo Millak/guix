4a2e94791405de6cf4bc559339f8f910b949d48e
Leo Famulari <leo@famulari.name>
2025-07-16 10:45:38 -0400

    gnu: linux-libre 6.12: Update to 6.12.38.
    * gnu/packages/linux.scm (linux-libre-6.12-version): Update to 6.12.38.
    (linux-libre-6.12-pristine-source, deblob-scripts-6.12): Update hashes.
    
    Change-Id: Ia4e2625ccdf7534885204b8e4bba017f1b617b9e

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -549,17 +549,17 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
 ;; Here are the support timelines:
 ;; <https://www.kernel.org/category/releases.html>
 
-(define-public linux-libre-6.12-version "6.12.36")
+(define-public linux-libre-6.12-version "6.12.38")
 (define-public linux-libre-6.12-gnu-revision "gnu")
 (define deblob-scripts-6.12
   (linux-libre-deblob-scripts
    linux-libre-6.12-version
    linux-libre-6.12-gnu-revision
    (base32 "0y6i9ifax1a8bxih3rr9xzy3mj61ssgx9yfsy6fh94mni6wl51gp")
-   (base32 "0g08l17f57wkh2jfj4n2k7f7ikm87aj55npljvwd1lns7v5p90xr")))
+   (base32 "1hc3fgc0wajlwbsjkvfcxvmjw4bcxfbrnbfbcasbczz7if4f0fvf")))
 (define-public linux-libre-6.12-pristine-source
   (let ((version linux-libre-6.12-version)
-        (hash (base32 "135s057ya63zw4v1jr9lzjdxk60ahr9v38hbv6nima755pnql5ja")))
+        (hash (base32 "1k0gcwavn5iws3z1as39227i2hnc62qnfddjfqy7k7ymhf6zldgh")))
    (make-linux-libre-source version
                             (%upstream-linux-source version hash)
                             deblob-scripts-6.12)))
