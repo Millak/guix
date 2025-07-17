e15771f1fca5e94702167b45759b78b3c79925a9
Sharlatan Hellseher <sharlatanus@gmail.com>
2025-07-17 17:15:37 +0100

    gnu: python-matplotlib: Reduce closure size.
    * gnu/packages/python-xyz.scm (python-matplotlib):
      [native-inputs]: Remove python-setuptools-scm-git-archive.
    
    Change-Id: I0db74e40f935b76e49d9cb8f0214513fb41fa8b8

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -11488,7 +11488,6 @@ tests = True~%" #$(this-package-input "tcl") #$(this-package-input "tk"))))))
            python-pytest-xdist
            python-setuptools
            python-setuptools-scm
-           python-setuptools-scm-git-archive
            python-wheel))
     (home-page "https://matplotlib.org/")
     (synopsis "2D plotting library for Python")
