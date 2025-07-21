ad7136046a762c95967c44242777112dcd16a29b
Zheng Junjie <z572@z572.online>
2025-07-21 23:26:16 +0800

    gnu: qtsensors: Update to 6.9.2.
    * gnu/packages/qt.scm (qtsensors): Update to 6.9.2.
    
    Change-Id: I77428d0af8d42acba2cf237299184b48802fc827

--- gnu/packages/qt.scm
+++ gnu/packages/qt.scm
@@ -1948,13 +1948,13 @@ consume data received from the server, or both.")
   (package
     (inherit qtsvg)
     (name "qtsensors")
-    (version "6.8.2")
+    (version "6.9.2")
     (source (origin
               (method url-fetch)
               (uri (qt-url name version))
               (sha256
                (base32
-                "1jplvcnpp7xc8d1lpw7qzrk6pvm8lrn84r2hy7dspl0s02dpr0ca"))))
+                "0qj4674vim2p34mq3kp99spjyf82qvs75w625namzqp274pshk4n"))))
     (native-inputs (list qtdeclarative))
     (inputs (list qtbase))
     (synopsis "Qt Sensors module")
