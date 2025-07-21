e6cc749f318d4248365b96edf9812560b0aa40cd
Andreas Enge <andreas@enge.fr>
2025-07-21 10:23:20 +0200

    gnu: wxsvg: Update to 1.5.25.
    * gnu/packages/wxwidgets.scm (wxsvg): Update to 1.5.25.
    
    Change-Id: Ief2b2064e2ba539c1172897b9d6c00edd196224c

--- gnu/packages/wxwidgets.scm
+++ gnu/packages/wxwidgets.scm
@@ -405,14 +405,14 @@ provide a 100% native look and feel for the application.")
 (define-public wxsvg
   (package
     (name "wxsvg")
-    (version "1.5.24")
+    (version "1.5.25")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://sourceforge/wxsvg/wxsvg/"
                             version "/wxsvg-" version ".tar.bz2"))
        (sha256
-        (base32 "10i4bv1bfbfgrrpxvfdjrr5svgn64v471lkcl2pzx9fhz28k4ixf"))))
+        (base32 "0jsk4vrv7rkb7wrdbsdf60zvdnvj0pxfika8xydd2jxm65larxjv"))))
     (build-system glib-or-gtk-build-system)
     (inputs
      (list wxwidgets cairo ffmpeg))
