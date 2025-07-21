e7a87d59b84f20307f22f6878b39e0210496104e
Brennan Vincent <brennan@umanwizard.com>
2025-07-21 14:23:43 -0700

    gnu: qtbase: Backport fix for flaky test.
    * gnu/packages/qt.scm (qtbase)[#:phases]: Add 'patch-aarch64-tests, which
    backports an upstream commit improving a flaky test.
    * gnu/packages/patches/qtbase-fix-thread-test.patch: New file.
    * gnu/local.mk (dist_patch_DATA): Register patch.
    
    Change-Id: Ibf9b95d7225ed42c9edad03a550d1c6e6f86be54

--- gnu/local.mk
+++ gnu/local.mk
@@ -2205,6 +2205,7 @@ dist_patch_DATA =						\
   %D%/packages/patches/qemu-fix-agent-paths.patch 		\
   %D%/packages/patches/qrcodegen-cpp-cmake.patch		\
   %D%/packages/patches/qtbase-absolute-runpath.patch		\
+  %D%/packages/patches/qtbase-fix-thread-test.patch             \
   %D%/packages/patches/qtbase-qmake-fix-includedir.patch 	\
   %D%/packages/patches/qtbase-qmlimportscanner-qml-import-path.patch 	\
   %D%/packages/patches/qtbase-moc-ignore-gcc-macro.patch	\
