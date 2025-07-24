0d96a5e0bb22f35dda98ee4fe6e14d6a0d07df0b
Murilo <murilo@disroot.org>
2025-07-24 08:59:16 -0300

    gnu: libinput: Update to 1.28.1.
    * gnu/packages/freedesktop.scm (libinput): Update to 1.28.1.
    
    Signed-off-by: John Kehayias <john.kehayias@protonmail.com>

--- gnu/packages/freedesktop.scm
+++ gnu/packages/freedesktop.scm
@@ -586,7 +586,7 @@ freedesktop.org project.")
   ;; Updating this will rebuild over 700 packages through libinput-minimal.
   (package
     (name "libinput")
-    (version "1.26.2")
+    (version "1.28.1")
     (source (origin
               (method git-fetch)
               (uri (git-reference
@@ -595,7 +595,7 @@ freedesktop.org project.")
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "1zwwq7a0a6yznc6jxhp6gb50yw5vpfkvgbrabrpc5pwldpckfbrg"))))
+                "1kgdjxgx5dbrx21kp4x2azfffd96lhcry77pbddbpkw4643vkmwj"))))
     (build-system meson-build-system)
     (arguments
      `(#:configure-flags '("-Ddocumentation=false")
