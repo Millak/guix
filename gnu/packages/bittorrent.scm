edd96a2b39eebe470ece5a697b85df40cd2a9a8b
Andreas Enge <andreas@enge.fr>
2025-07-28 00:33:40 +0200

    gnu: qbittorrent-enhanced: Update to 5.1.2.10.
    * gnu/packages/bittorrent.scm (qbittorrent-enhanced): Update to 5.1.2.10.
    
    Change-Id: Ie457949f39612c585609ed752213671fd5e4dbac

--- gnu/packages/bittorrent.scm
+++ gnu/packages/bittorrent.scm
@@ -565,7 +565,7 @@ features.")
   (package
     (inherit qbittorrent)
     (name "qbittorrent-enhanced")
-    (version "5.0.4.10")
+    (version "5.1.2.10")
     (source
      (origin
        (method git-fetch)
@@ -575,7 +575,7 @@ features.")
        (file-name (git-file-name name version))
        (sha256
         (base32
-         "1y5pm4ljzdl06sacz63dl2cjfwfvx5c808f73rh9c19q90y870d9"))))
+         "17yvx8k1fnawfkyhhzhl458l1bjg2dhnc6a456hkh2qr32jj4y23"))))
     (home-page "https://github.com/c0re100/qBittorrent-Enhanced-Edition")
     (description
      "qBittorrent Enhanced is a bittorrent client based on qBittorrent with
