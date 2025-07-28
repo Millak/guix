66178e64e0e14e73bb6d607ce7e07ff5be2f32dd
Zheng Junjie <z572@z572.online>
2025-07-28 16:16:12 +0800

    gnu: system-settings: Update to 6.4.3.
    * gnu/packages/kde-plasma.scm (system-settings): Update to 6.4.3.
    
    Change-Id: Ic87279d2c90b925dd78e780506d348be244a9c47

--- gnu/packages/kde-plasma.scm
+++ gnu/packages/kde-plasma.scm
@@ -3082,14 +3082,14 @@ of a Plasma shell.")
 (define-public system-settings
   (package
     (name "system-settings")
-    (version "6.3.6")
+    (version "6.4.3")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://kde/stable/plasma/" version
                                   "/systemsettings-" version ".tar.xz"))
               (sha256
                (base32
-                "116p2agask93pgsb65ppnv8n6xb65wkhqjdyhh7zhpmzgywp5j61"))))
+                "0mjm62r7wqn7k9ifs0vq87dlag8rsyacmvc64hjgvxgk23bxq76c"))))
     (build-system qt-build-system)
     (native-inputs (list extra-cmake-modules))
     (inputs (list kaccounts-integration
