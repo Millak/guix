c04c7442f658982702e92a78c296ee8e22d46f5f
Cayetano Santos <csantosb@inventati.org>
2025-08-01 18:16:57 +0200

    gnu: adaptivecpp: Update to 25.02.0.
    * gnu/packages/sycl.scm (adaptivecpp): Update to 25.02.0.
    [arguments]<#:phases>{create-symlinks}: New phase.
    [inputs]: Add spirv-tools, numactl and libffi.
    [native-inputs]: Add python-minimal, clang-19, llvm-19; remove python,
    clang-15, llvm-15, spirv-tools.
    [synopsys]: Fix max column.
    
    Change-Id: I175287b08a409d4c5a495cd71f81ae11e23950a2
    Signed-off-by: Andreas Enge <andreas@enge.fr>

--- gnu/packages/sycl.scm
+++ gnu/packages/sycl.scm
@@ -18,12 +18,15 @@
 
 (define-module (gnu packages sycl)
   #:use-module (guix build-system cmake)
+  #:use-module (guix gexp)
   #:use-module (guix git-download)
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix packages)
   #:use-module (gnu packages)
   #:use-module (gnu packages boost)
+  #:use-module (gnu packages linux)
   #:use-module (gnu packages llvm)
+  #:use-module (gnu packages libffi)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages python)
   #:use-module (gnu packages rocm)
@@ -37,7 +40,7 @@
 (define-public adaptivecpp
   (package
     (name "adaptivecpp")
-    (version "24.10.0")
+    (version "25.02.0")
     (source (origin
               (method git-fetch)
               (uri (git-reference
@@ -46,13 +49,50 @@
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "1gha90zkvg6nabyj1y55rflxzygdkznkjqj8v6zb1jgm1f5w60b7"))))
+                "01wajw1vvbic1hiyz4rj7in09js3kl0xvaa2qpcg1pv7xkrz0xxx"))))
     (build-system cmake-build-system)
-    (native-inputs (list clang-15 llvm-15 python spirv-tools))
-    (inputs (list boost rocm-opencl-runtime spirv-headers))
-    (arguments `(#:tests? #f)) ; no tests
+    ;; Sync with llvm-for-rocm llvm release.
+    (native-inputs (list clang-19 llvm-19 python-minimal))
+    (inputs
+     (list boost
+           libffi
+           numactl
+           rocm-opencl-runtime
+           spirv-headers
+           spirv-tools))
+    (arguments
+     (list
+      #:phases
+      #~(modify-phases %standard-phases
+          ;; FIXME: There is probably a much better way.
+          ;; This file: bin/hipSYCL/llvm-to-backend/llvm-to-host-tool
+          ;; requires libacpp-common.so and libllvm-to-{backend,host}.so
+          ;; in RUNPATH
+          (add-before 'validate-runpath 'create-symlinks
+            (lambda _
+              (symlink
+               (string-append #$output "/lib/libacpp-common.so")
+               (string-append
+                #$output
+                "/bin/hipSYCL/llvm-to-backend/libacpp-common.so"))
+              (symlink
+               (string-append
+                #$output
+                "/lib/hipSYCL/llvm-to-backend/libllvm-to-backend.so")
+               (string-append
+                #$output
+                "/bin/hipSYCL/llvm-to-backend/libllvm-to-backend.so"))
+              (symlink
+               (string-append
+                #$output
+                "/lib/hipSYCL/llvm-to-backend/libllvm-to-host.so")
+               (string-append
+                #$output
+                "/bin/hipSYCL/llvm-to-backend/libllvm-to-host.so")))))
+      #:tests? #f)) ; no tests
     (home-page "https://adaptivecpp.github.io/")
-    (synopsis "Implementation of the SYCL programming language for accelerators")
+    (synopsis
+     "Implementation of the SYCL programming language for accelerators")
     (description
      "AdaptiveCpp is aImplementation of SYCL and C++ standard parallelism
 for CPUs and GPUs from all vendors, with independent, community-driven
