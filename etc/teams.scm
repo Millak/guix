a300e6feb1c664d4fc5c09a0937e8ac2eed3fb0e
Cayetano Santos <csantosb@inventati.org>
2025-08-02 13:11:53 +0200

    etc: teams: Add hpc team.
    * etc/teams.scm (hpc): New team.
    [Cayetano Santos]: Add to new team.
    * CODEOWNERS: Rebuild.
    
    Change-Id: I5b08aed33ea89acaa770dd2ac5dd8b991d43989a
    Signed-off-by: Andreas Enge <andreas@enge.fr>

--- etc/teams.scm
+++ etc/teams.scm
@@ -690,6 +690,16 @@ the haskell-build-system."
                       "tests/home-import.scm"
                       "tests/home-services.scm")))
 
+(define-team hpc
+  (team 'hpc
+        #:name "Hpc team"
+        #:description "High performance computing related packages."
+        #:scope (list "gnu/packages/mpi.scm"
+                      "gnu/packages/rocm.scm"
+                      "gnu/packages/sycl.scm"
+                      "gnu/packages/tbb.scm"
+                      "gnu/packages/vulkan.scm")))
+
 (define-team hurd
   (team 'hurd
         #:name "Team for the Hurd"
@@ -1244,7 +1254,7 @@ the \"texlive\" importer."
 (define-member (person "Cayetano Santos"
                        "csantosb@inventati.org"
                        "csantosb")
-  ai emacs electronics)
+  ai emacs electronics hpc)
 
 (define-member (person "Greg Hogan"
                        "code@greghogan.com"
