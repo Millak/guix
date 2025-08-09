9527466146a4681d016869f7691d4499aa53710c
Sharlatan Hellseher <sharlatanus@gmail.com>
2025-08-09 23:57:08 +0100

    gnu: python-debugpy: Update to 1.8.16.
    * gnu/packages/python-xyz.scm (python-debugpy): Update to 1.8.16.
    [native-inputs]: Add python-setuptools.
    
    Change-Id: I14bb3ecc5b2f15cb9e60f6ec68db17918dc01493

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -18895,7 +18895,7 @@ and other @acronym{IDEs, Integrated Development Environments}.")
 (define-public python-debugpy
   (package
     (name "python-debugpy")
-    (version "1.8.9")
+    (version "1.8.16")
     (source
      (origin
        (method git-fetch)
@@ -18910,8 +18910,8 @@ and other @acronym{IDEs, Integrated Development Environments}.")
        (patches (search-patches "python-debugpy-unbundle-pydevd.patch"))
        (sha256
         (base32
-         "0rq9ndsg4za0np5lnlkdwaqlizay8ndm8ki2m7r7awji262dzzlx"))))
-    (build-system python-build-system)
+         "18dkzw2cvsvjrn6ly08pqwrn04fmvqnjjv0gff6795fnl3xzjvfd"))))
+    (build-system pyproject-build-system)
     (arguments
      (list
       #:tests? #f ; Fail on systems with YAMA LSM’s ptrace scope > 0.
@@ -18958,7 +18958,8 @@ and other @acronym{IDEs, Integrated Development Environments}.")
            python-pytest-cov
            python-pytest-timeout
            python-pytest-xdist
-           python-requests))
+           python-requests
+           python-setuptools))
     (propagated-inputs (list python-pydevd))
     (home-page "https://aka.ms/debugpy")
     (synopsis "Debug Adapter Protocol Python implementation")
