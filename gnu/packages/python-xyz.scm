28f820746cfa73997bb91e4b707af059c1e84b12
Sharlatan Hellseher <sharlatanus@gmail.com>
2025-08-10 12:09:19 +0100

    gnu: python-types-colorama: Update to 0.4.15.20250801.
    * gnu/packages/python-xyz.scm (python-types-colorama): Update to 0.4.15.20250801.
      [arguments] <tests?>: Disable as no provided with PyPI archive.
      [native-inputs]: Remove python-wheel.
    
    Change-Id: I1e4f5cb1a3f1a5a8ba6c50b902089b19b27f015b

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -6674,15 +6674,17 @@ any Python package.")
 (define-public python-types-colorama
   (package
     (name "python-types-colorama")
-    (version "0.4.15.20240311")
+    (version "0.4.15.20250801")
     (source
      (origin
        (method url-fetch)
-       (uri (pypi-uri "types-colorama" version))
+       (uri (pypi-uri "types_colorama" version))
        (sha256
-        (base32 "0ylwp6r6k69icsd96zbsan7i1x0rwj427lv5jpxi8avxs6c7z3m2"))))
+        (base32 "1xapqdq6450ngzp18nvvkwbs68nnxksk1wyk6wid2qw9sq9msmh2"))))
     (build-system pyproject-build-system)
-    (native-inputs (list python-setuptools python-wheel))
+    (arguments (list #:tests? #f))      ;no tests in PyPI archive
+    (native-inputs
+     (list python-setuptools))
     (home-page "https://github.com/python/typeshed")
     (synopsis "Typing stubs for colorama")
     (description "This package provides typing stubs for colorama.")
