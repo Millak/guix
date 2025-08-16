399c40b211cf17a71742c9321f3e378afc46eb28
Cayetano Santos <csantosb@inventati.org>
2025-08-16 20:05:20 +0200

    gnu: emacs-eldoc-box: Update to 1.14.1.
    * gnu/packages/emacs-xyz.scm (emacs-eldoc-box): Update to 1.14.1.
    [arguments]: Disable tests.
    
    Change-Id: I381e53a89de0e6e40a1b76bdaa5c2d5094a566b1
    Reviewed-by: Liliana Marie Prikler <liliana.prikler@gmail.com>
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -23512,7 +23512,7 @@ variable instead, to remind you of that variable's meaning.")
 (define-public emacs-eldoc-box
   (package
     (name "emacs-eldoc-box")
-    (version "1.13.2")
+    (version "1.14.1")
     (source (origin
               (method git-fetch)
               (uri (git-reference
@@ -23521,8 +23521,9 @@ variable instead, to remind you of that variable's meaning.")
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "1136rgi7qy5kmciqrz88lxvk01fdcpry2srsvv3h2rfqwmiccpl0"))))
+                "0hk716pym15crmnviwikwrdqzhs74widjlz5zrp5107klqgxicwn"))))
     (build-system emacs-build-system)
+    (arguments (list #:tests? #f))      ;no tests
     (home-page "https://github.com/casouri/eldoc-box")
     (synopsis "Childframe doc for eglot and anything that uses eldoc")
     (description
