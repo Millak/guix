a4e8b4334e863fc9d1deac46e7c99fee6cce1b9e
Brett Schalin <brettschalin@gmail.com>
2025-08-16 11:22:09 -0700

    gnu: frei0r-plugins: Update source URL.
    * gnu/packages/video.scm (frei0r-plugins):
    [source]: Update URL.
    
    Fixes: guix/guix#1440.
    Reported-by: Mazin AlHaddad <codeberg@getstate.dev>
    Change-Id: I16e28031e8ab8684f5b4571b0a9dd0947fe01c50
    Reviewed-by: Simon Tournier <zimon.toutoune@gmail.com>
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/video.scm
+++ gnu/packages/video.scm
@@ -5750,8 +5750,10 @@ alpha blending etc).")
     (source
      (origin
        (method url-fetch)
-       (uri (string-append "https://files.dyne.org/frei0r/"
-                           "frei0r-plugins-" version ".tar.gz"))
+       (uri (string-append "https://files.dyne.org/frei0r"
+                            "?file=frei0r/old-releases/frei0r-plugins-"
+                            version
+                            ".tar.gz"))
        (sha256
         (base32
          "0fjji3060r4fwr7vn91lwfzl80lg3my9lkp94kbyw8xwz7qgh7qv"))))
