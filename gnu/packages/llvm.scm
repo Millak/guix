c95dd5cef164d4e1c81cb07a94789ac39ab7172e
Andreas Enge <andreas@enge.fr>
2025-08-19 21:28:26 +0200

    gnu: Remove lld-11.
    * gnu/packages/llvm.scm (lld-11): Delete variable.
    
    Change-Id: Id22f52bdee1f5a3a57ef52a3ffeaa6ae2e0ea387

--- gnu/packages/llvm.scm
+++ gnu/packages/llvm.scm
@@ -1571,19 +1571,6 @@ components which highly leverage existing libraries in the larger LLVM Project."
     (inputs (modify-inputs (package-inputs lld)
               (replace "llvm" llvm-12)))))
 
-(define-public lld-11
-  (package
-    (inherit lld-12)
-    (version "11.0.0")
-    (source (origin
-              (method url-fetch)
-              (uri (llvm-uri "lld" version))
-              (sha256
-               (base32
-                "077xyh7sij6mhp4dc4kdcmp9whrpz332fa12rwxnzp3wgd5bxrzg"))))
-    (inputs (modify-inputs (package-inputs lld)
-              (replace "llvm" llvm-11)))))
-
 (define-public lld-16
   (package
     (inherit lld-15)
