29d364498ee87e134cb53ee15350001417eaedc6
Baptiste Strazzulla <baptiste.strazzulla@protonmail.com>
2025-08-20 18:33:19 +0200

    gnu: git-lfs: Fix build.
    This change is to bypass combination of go-build-system and custom make
    targets where the import path is set manually, see discussion in
    guix/guix#2133.
    
    * gnu/packages/version-control.scm (git-lfs): Rename binary and home page
    
    Change-Id: Ic87627412f715ebed07cf79ddd298e777746975f
    Reviewed-by: Nicolas Graves <ngraves@ngraves.fr>
    Reviewed-by: Maxim Cournoyer <maxim@guixotic.coop>
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/version-control.scm
+++ gnu/packages/version-control.scm
@@ -4224,7 +4224,11 @@ will reconstruct the object along its delta-base chain and return it.")
                              (install-file manpage
                                            (string-append #$output "/share/man/man1")))
                            (find-files "." "^git-lfs.*\\.1$"))))))
-                 #~()))))
+                 #~())
+          (add-after 'install 'rename-binary
+            (lambda _
+              (with-directory-excursion (string-append #$output "/bin")
+                (rename-file "v3" "git-lfs")))))))
     (native-inputs
      (append (list git-minimal
                    go-github-com-avast-retry-go
@@ -4251,7 +4255,7 @@ will reconstruct the object along its delta-base chain and return it.")
              (if (supported-package? ruby-asciidoctor/minimal)
                  (list ronn-ng ruby-asciidoctor/minimal)
                  '())))
-    (home-page "https://git-lfs.github.com/")
+    (home-page "https://git-lfs.com/")
     (synopsis "Git extension for versioning large files")
     (description
      "Git Large File Storage (LFS) replaces large files such as audio samples,
