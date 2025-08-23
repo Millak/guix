e074f5e26b851e426616fe0348ebc14a3408d26b
宋文武 <iyzsong@member.fsf.org>
2025-08-23 12:36:47 +0800

    gnu: lxqt-about: Update to 2.2.0.
    * gnu/packages/lxqt.scm (lxqt-about): Update to 2.2.0.
    
    Change-Id: Icc803d7a38ffa9d09ba8fa5fcbc9c7a00c263a0d

--- gnu/packages/lxqt.scm
+++ gnu/packages/lxqt.scm
@@ -277,14 +277,14 @@ and memory usage or network traffic.")
 (define-public lxqt-about
   (package
     (name "lxqt-about")
-    (version "2.1.0")
+    (version "2.2.0")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://github.com/lxqt/" name "/releases/download/"
                            version "/" name "-" version ".tar.xz"))
        (sha256
-        (base32 "0js94q82rpaqkdg6vl3p4w4kw091dp6wixgkw5dbvqsnaw2v8nd0"))))
+        (base32 "0vypdk0nf2rr7riv8xp7wy1x9694i73rgk0dj9x39gh53jm4xjy5"))))
     (build-system cmake-build-system)
     (inputs
      (list liblxqt))
