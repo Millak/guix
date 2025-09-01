105757fac1c0ef430919363ee51ac1d758d341a2
Ashish SHUKLA <ashish.is@lostca.se>
2025-09-01 23:14:31 +0200

    gnu: janet: Update to 1.39.1
    * gnu/packages/lisp.scm (janet): Update to 1.39.1.
    
    Change-Id: I9421f93374f6ffe261b8bf93e63742aac82849bb
    Signed-off-by: jgart <jgart@dismail.de>

--- gnu/packages/lisp.scm
+++ gnu/packages/lisp.scm
@@ -925,7 +925,7 @@ interface to the Tk widget system.")
 (define-public janet
   (package
     (name "janet")
-    (version "1.38.0")
+    (version "1.39.1")
     (source
      (origin
        (method git-fetch)
@@ -934,7 +934,7 @@ interface to the Tk widget system.")
              (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "1ym852ns8s1m4br33yzw4ak475j5y8svh7bpzd22mhbaqqbc3drw"))))
+        (base32 "03mnza3fbgngr389mri12vq1jn7cf7a2a1a45gkbnzzxwjwh7pqx"))))
     (build-system gnu-build-system)
     (arguments
      (list #:make-flags
