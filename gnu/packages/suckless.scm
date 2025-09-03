Millak/guix

commit e03f2316e45e4aea7501ff29aa174222bdd2e93c

gnu: st: Improve style.
* gnu/packages/suckless.scm (st)[arguments]: Use G-Expressions.
[inputs]: Modernize style.

Change-Id: I790c43e81950ea82e8bbd7581b5cbc772b5f94b3
Signed-off-by: jgart <jgart@dismail.de>

gnu/packages/suckless.scm
@@ -377,21 +377,21 @@ numbers of user-defined menu items efficiently.")
         (base32 "0js9z5kn8hmpxzfmb2g6zsy28zkpg88j3wih5wixc89b8x7ms8bb"))))
     (build-system gnu-build-system)
     (arguments
-     `(#:tests? #f                      ; no tests
-       #:make-flags
-       (list (string-append "CC=" ,(cc-for-target))
-             (string-append "TERMINFO="
-                            (assoc-ref %outputs "out")
-                            "/share/terminfo")
-             (string-append "PREFIX=" %output))
-       #:phases
-       (modify-phases %standard-phases
-         (delete 'configure))))
+     (list
+      #:tests? #f                      ;no tests
+      #:make-flags
+      #~(list
+         (string-append "CC=" #$(cc-for-target))
+         (string-append "TERMINFO=" #$output "/share/terminfo")
+         (string-append "PREFIX=" #$output))
+      #:phases
+      #~(modify-phases %standard-phases
+          (delete 'configure))))
     (inputs
-     `(("libx11" ,libx11)
-       ("libxft" ,libxft)
-       ("fontconfig" ,fontconfig)
-       ("freetype" ,freetype)))
+     (list libx11
+           libxft
+           fontconfig
+           freetype))
     (native-inputs
      (list ncurses ;provides tic program
            pkg-config))

commit 2f6319cf94713ec2377ce309c2fc87ce6d5e09aa

gnu: lukesmithxyz-st: Fix broken builder script.
I believe broken in e03f2316e45e4aea7501ff29aa174222bdd2e93c with the switch
to a G-exp for the st package phases.

* gnu/packages/suckless.scm (lukesmithxyz-st)[arguments]: Change phases to a
gexp.

Change-Id: I0cb47d07dd40e9d15ffebee089ccac7750f28bad

gnu/packages/suckless.scm
@@ -458,12 +458,12 @@ drawing.")
       (arguments
        (substitute-keyword-arguments (package-arguments st)
          ((#:phases phases)
-          `(modify-phases ,phases
-             (add-after 'unpack 'remove-calls-to-git
-               (lambda _
-                 (substitute* "Makefile"
-                   (("git submodule init") "")
-                   (("git submodule update") ""))))))))
+          #~(modify-phases #$phases
+              (add-after 'unpack 'remove-calls-to-git
+                (lambda _
+                  (substitute* "Makefile"
+                    (("git submodule init") "")
+                    (("git submodule update") ""))))))))
       (inputs (modify-inputs (package-inputs st)
                 (prepend libxext harfbuzz)))
       (home-page "https://github.com/LukeSmithxyz/st")