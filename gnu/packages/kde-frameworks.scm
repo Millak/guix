bb31d07db0a4eebdea3788713e39eab211f94cdf
Maxim Cournoyer <maxim@guixotic.coop>
2025-09-04 21:51:33 +0900

    gnu: extra-cmake-modules: Update to 6.17.0.
    * gnu/packages/kde-frameworks.scm (extra-cmake-modules): Update to 6.17.0.
    
    Change-Id: Ifb901bbcee974654a9f98d3f1b63a5b0746c2a3a

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -104,7 +104,7 @@
 (define-public extra-cmake-modules
   (package
     (name "extra-cmake-modules")
-    (version "6.13.0")
+    (version "6.17.0")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -113,7 +113,7 @@
                     name "-" version ".tar.xz"))
               (sha256
                (base32
-                "00plifdda75cdkm3k39rg94d348p4xni95b90m6gy5y801y021kh"))))
+                "0h0axhm0zzh70kj4r7kvqslx8dv1c2k94ns8scfxxx1q09yv3v6z"))))
     (build-system cmake-build-system)
     (native-inputs
      ;; Add test dependency, except on armhf where building it is too
