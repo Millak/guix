fd12ed9997a30dda0032d1019efcaf510501bf86
Sergey Trofimov <sarg@sarg.org.ru>
2025-09-09 10:43:56 +0200

    gnu: iwd: Update to 3.9.
    * gnu/packages/networking.scm (iwd): Update to 3.9.
    
    Change-Id: I3e73595f534d63b6d4bd4b45d7eb9954630c5efb
    Signed-off-by: Maxim Cournoyer <maxim@guixotic.coop>

--- gnu/packages/networking.scm
+++ gnu/packages/networking.scm
@@ -4236,77 +4236,75 @@ powerful route filtering syntax and an easy-to-use configuration interface.")
     (license license:gpl2+)))
 
 (define-public iwd
-  (let ((commit "c4718a53553b8c13cbdd713d3783072589dd1620")
-        (revision "1"))
-    (package
-      (name "iwd")
-      (version (git-version "3.8" revision commit))
-      (source (origin
-                (method git-fetch)
-                (uri (git-reference
-                      (url "https://git.kernel.org/pub/scm/network/wireless/iwd.git")
-                      (commit commit)))
-                (file-name (git-file-name name version))
-                (sha256
-                 (base32
-                  "1dqmqr63srgr8y5x2nb07hami7vva1zkg9lh1a1ca2pfspjpa8q3"))))
-      (build-system gnu-build-system)
-      (inputs
-       (list dbus ell (package-source ell) openresolv readline))
-      (native-inputs
-       (list autoconf
-             automake
-             libtool
-             pkg-config
-             python
-             python-docutils
-             openssl))
-      (arguments
-       (list #:configure-flags
-             #~(list "--disable-systemd-service"
-                     "--enable-external-ell"
-                     "--enable-hwsim"
-                     "--enable-tools"
-                     "--enable-wired"
-                     "--localstatedir=/var"
-                     (string-append "--with-dbus-datadir=" #$output "/share/")
-                     (string-append "--with-dbus-busdir="
-                                    #$output "/share/dbus-1/system-services"))
-             #:phases
-             #~(modify-phases %standard-phases
-                 (add-after 'unpack 'copy-ell-header-files
-                   ;; Copy into the source tree two of ell's private header files
-                   ;; that it shares with iwd, as is required to build with the
-                   ;; "--enable-external-ell" configure option.  See the
-                   ;; definition of "ell_shared" in iwd's Makefile.am.
-                   (lambda* (#:key inputs #:allow-other-keys)
-                     (let ((ell-header-dir (search-input-directory inputs "/ell"))
-                           (target-dir "ell"))
-                       (mkdir target-dir)
-                       (for-each
-                        (lambda (file-name)
-                          (copy-file (string-append ell-header-dir "/" file-name)
-                                     (string-append target-dir "/" file-name)))
-                        '("asn1-private.h" "useful.h")))))
-                 (add-after 'unpack 'patch-resolvconf-path
-                   (lambda* (#:key inputs #:allow-other-keys)
-                     (substitute* "src/resolve.c"
-                       (("getenv\\(\"PATH\"\\)")
-                        (format #f "\"~a\""
-                                (dirname (search-input-file
-                                          inputs "sbin/resolvconf")))))))
-                 (add-after 'configure 'patch-Makefile
-                   (lambda _
-                     (substitute* "Makefile"
-                       ;; Don't try to 'mkdir /var'.
-                       (("\\$\\(MKDIR_P\\) -m 700") "true")))))))
-      (home-page "https://iwd.wiki.kernel.org/")
-      (synopsis "iNet Wireless Daemon")
-      (description "iwd is a wireless daemon for Linux that aims to replace WPA
+  (package
+    (name "iwd")
+    (version "3.9")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                     (url "https://git.kernel.org/pub/scm/network/wireless/iwd.git")
+                     (commit version)))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "0kk88ipi901gibz7275rh6p9hplh5xg00b4bxf3i51wyml3id39m"))))
+    (build-system gnu-build-system)
+    (inputs
+     (list dbus ell (package-source ell) openresolv readline))
+    (native-inputs
+     (list autoconf
+           automake
+           libtool
+           pkg-config
+           python
+           python-docutils
+           openssl))
+    (arguments
+     (list #:configure-flags
+           #~(list "--disable-systemd-service"
+                   "--enable-external-ell"
+                   "--enable-hwsim"
+                   "--enable-tools"
+                   "--enable-wired"
+                   "--localstatedir=/var"
+                   (string-append "--with-dbus-datadir=" #$output "/share/")
+                   (string-append "--with-dbus-busdir="
+                                  #$output "/share/dbus-1/system-services"))
+           #:phases
+           #~(modify-phases %standard-phases
+               (add-after 'unpack 'copy-ell-header-files
+                 ;; Copy into the source tree two of ell's private header files
+                 ;; that it shares with iwd, as is required to build with the
+                 ;; "--enable-external-ell" configure option.  See the
+                 ;; definition of "ell_shared" in iwd's Makefile.am.
+                 (lambda* (#:key inputs #:allow-other-keys)
+                   (let ((ell-header-dir (search-input-directory inputs "/ell"))
+                         (target-dir "ell"))
+                     (mkdir target-dir)
+                     (for-each
+                      (lambda (file-name)
+                        (copy-file (string-append ell-header-dir "/" file-name)
+                                   (string-append target-dir "/" file-name)))
+                      '("asn1-private.h" "useful.h")))))
+               (add-after 'unpack 'patch-resolvconf-path
+                 (lambda* (#:key inputs #:allow-other-keys)
+                   (substitute* "src/resolve.c"
+                     (("getenv\\(\"PATH\"\\)")
+                      (format #f "\"~a\""
+                              (dirname (search-input-file
+                                        inputs "sbin/resolvconf")))))))
+               (add-after 'configure 'patch-Makefile
+                 (lambda _
+                   (substitute* "Makefile"
+                     ;; Don't try to 'mkdir /var'.
+                     (("\\$\\(MKDIR_P\\) -m 700") "true")))))))
+    (home-page "https://iwd.wiki.kernel.org/")
+    (synopsis "iNet Wireless Daemon")
+    (description "iwd is a wireless daemon for Linux that aims to replace WPA
 Supplicant.  It optimizes resource utilization by not depending on any external
 libraries and instead utilizing features provided by the Linux kernel to the
 maximum extent possible.")
-      (license license:lgpl2.1+))))
+    (license license:lgpl2.1+)))
 
 (define-public iwgtk
   (package
