8257848a46f1b189143364062f7e8f4f0176e5f9
Giacomo Leidi <goodoldpaul@autistici.org>
2025-09-12 18:50:37 +0200

    gnu: Add erlang-telemetry.
    * gnu/packages/erlang-xyz.scm (erlang-telemetry): New variable.
    
    Change-Id: If0bb373fcd914aa286093aaf9b1c92c053ac172b
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/packages/erlang-xyz.scm
+++ gnu/packages/erlang-xyz.scm
@@ -458,6 +458,26 @@ implementation.")
     (home-page "https://hex.pm/packages/tdiff")
     (license license:lgpl2.0)))
 
+(define-public erlang-telemetry
+  (package
+    (name "erlang-telemetry")
+    (version "1.3.0")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (hexpm-uri "telemetry" version))
+       (sha256
+        (base32 "1rkrbf3z7my9dsz9km7077anp6f0nyapmf5lyij3grnv364zq5bh"))))
+    (build-system rebar-build-system)
+    (synopsis "Dynamic dispatching library for metrics and instrumentations")
+    (description
+     "@code{Telemetry} is a lightweight library for dynamic dispatching of
+events, with a focus on metrics and instrumentation.  Any Erlang or Elixir
+library can use @code{telemetry} to emit events.  Application code and other
+libraries can then hook into those events and run custom handlers.")
+    (home-page "https://hexdocs.pm/telemetry/")
+    (license license:asl2.0)))
+
 (define-public erlang-cache-tab
   (package
     (name "erlang-cache-tab")
