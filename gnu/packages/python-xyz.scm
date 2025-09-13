096733b92053058458b40cd0b5157b1749f338f0
Nicolas Graves <ngraves@ngraves.fr>
2025-09-13 16:30:50 +0200

    gnu: python-streamtracer: Extend with (pyproject-guile-json).
    * gnu/packages/python-xyz.scm (python-steamtracer)[arguments]
    <#:phases>: Extend with (pyproject-guile-json).
    
    Change-Id: I968fd19a792bc09635a4bb991e4db8eb789523c9
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -36537,6 +36537,7 @@ functionality and customization to your projects with their own plugins.")
                   ((guix build pyproject-build-system) #:prefix py:)
                   (guix build utils))
       #:phases
+      (with-extensions (list (pyproject-guile-json))
       #~(modify-phases %standard-phases
           (add-after 'build 'build-python-module
             (assoc-ref py:%standard-phases 'build))
@@ -36548,7 +36549,7 @@ functionality and customization to your projects with their own plugins.")
             (lambda* (#:key tests? #:allow-other-keys)
               (when tests?
                 (with-directory-excursion #$output
-                  (invoke "pytest" "-vv"))))))
+                  (invoke "pytest" "-vv")))))))
       #:install-source? #false))
     (native-inputs
      (list maturin
