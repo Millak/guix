32305c0543b44029ecb5c03b1ac57583e9da8dbb
Nicolas Graves <ngraves@ngraves.fr>
2025-09-13 16:56:33 +0200

    gnu: python-orjson: Extend with (pyproject-guile-json).
    * gnu/packages/python-xyz.scm (python-orjson)[arguments]
    <#:phases>: Extend with (pyproject-guile-json).
    
    Change-Id: If76da1021cd035828bf5c365c01ac0f94e96d2c1
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -5910,11 +5910,12 @@ help formatter.")
                   ((guix build pyproject-build-system) #:prefix py:)
                   (guix build utils))
       #:phases
+      (with-extensions (list (pyproject-guile-json))
       #~(modify-phases %standard-phases
           (add-after 'build 'build-python-module
             (assoc-ref py:%standard-phases 'build))
           (add-after 'build-python-module 'install-python-module
-            (assoc-ref py:%standard-phases 'install)))
+            (assoc-ref py:%standard-phases 'install))))
       #:install-source? #false))
     (inputs
      (cons maturin (cargo-inputs 'python-orjson)))
