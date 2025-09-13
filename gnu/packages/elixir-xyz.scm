d6c3f39d3a825504a7f855d73dc26bc693dadf45
Giacomo Leidi <goodoldpaul@autistici.org>
2025-09-13 12:26:51 +0200

    gnu: Add elixir-nimble-options.
    * gnu/packages/elixir-xyz.scm (elixir-nimble-options): New variable.
    
    Change-Id: If9039e3dc818cd57c0b39da34d6fe3f63e19965a
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/packages/elixir-xyz.scm
+++ gnu/packages/elixir-xyz.scm
@@ -369,6 +369,28 @@ literals, variables, or parametrically defined matchers.")
     (home-page "https://hexdocs.pm/machete/")
     (license license:expat)))
 
+(define-public elixir-nimble-options
+  (package
+    (name "elixir-nimble-options")
+    (version "1.1.1")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (hexpm-uri "nimble_options" version))
+       (sha256
+        (base32 "0i0bcmpsc02ga2llakgcvnw734rqn2dzx0j8k2vc8hllr9q286w2"))))
+    (build-system mix-build-system)
+    (native-inputs
+     (list elixir-castore
+           elixir-excoveralls))
+    (synopsis
+     "Validates and documents high-level options")
+    (description
+     "This package provides a tiny library for validating and documenting high-level
+options.")
+    (home-page "https://hexdocs.pm/nimble_options/")
+    (license license:asl2.0)))
+
 (define-public elixir-nimble-ownership
   (package
     (name "elixir-nimble-ownership")
