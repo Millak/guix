2df248991be673b1712239e11d77b8554d2a046d
Nicolas Graves <ngraves@ngraves.fr>
2025-09-13 16:59:05 +0200

    gnu: onnxruntime: Extend with (pyproject-guile-json).
    * gnu/packages/machine-learning.scm (onnxruntime)[arguments]
    <#:phases>: Extend with (pyproject-guile-json).
    
    Change-Id: Ib498a4697642d59784cf699230478ba23fb41ff6
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/machine-learning.scm
+++ gnu/packages/machine-learning.scm
@@ -1837,6 +1837,7 @@ with a single function call.")
               ;; XXX: Fixes build with gcc@14.
               "-DCMAKE_CXX_FLAGS=-Wl,-z,noexecstack")
       #:phases
+      (with-extensions (list (pyproject-guile-json))
       #~(modify-phases %standard-phases
           (add-after 'unpack 'chdir
             (lambda _
@@ -1899,7 +1900,7 @@ with a single function call.")
               ((assoc-ref py:%standard-phases 'sanity-check)
                #:inputs `(("sanity-check.py" . ,#$(default-sanity-check.py))
                           ,@inputs)
-               #:outputs `(("out" . ,#$output:python))))))))
+               #:outputs `(("out" . ,#$output:python)))))))))
     (outputs (list "out" "python"))
     (inputs
      (list abseil-cpp
