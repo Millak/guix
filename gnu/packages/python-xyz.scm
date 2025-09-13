12dd89da97273b5d274b0c991b8b46b4157ca4f7
Nicolas Graves <ngraves@ngraves.fr>
2025-09-13 16:57:44 +0200

    gnu: python-tiktoken: Extend with (pyproject-guile-json).
    * gnu/packages/python-xyz.scm (python-tiktoken)[arguments]
    <#:phases>: Extend with (pyproject-guile-json).
    
    Change-Id: I94c11b8e85c41f8da8158d691114952df823d8de
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -37666,6 +37666,7 @@ to TIFF, BigTIFF, and ImageJ hyperstack compatible files.")
         ((guix build pyproject-build-system) #:prefix py:)
         (guix build utils))
       #:phases
+      (with-extensions (list (pyproject-guile-json))
       #~(modify-phases %standard-phases
           (replace 'build
             (assoc-ref py:%standard-phases 'build))
@@ -37679,7 +37680,7 @@ to TIFF, BigTIFF, and ImageJ hyperstack compatible files.")
               (assoc-ref py:%standard-phases 'create-entrypoints)
               (assoc-ref py:%standard-phases 'compile-bytecode)))
           (replace 'install
-            (assoc-ref py:%standard-phases 'install)))))
+            (assoc-ref py:%standard-phases 'install))))))
     (propagated-inputs (list python-regex python-requests))
     (inputs (cargo-inputs 'python-tiktoken))
     (native-inputs
