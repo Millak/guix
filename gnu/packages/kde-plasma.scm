b260b32f091c4c777c22eac2335d3cd89567aa05
Maxim Cournoyer <maxim@guixotic.coop>
2025-09-15 16:47:48 +0900

    gnu: powerdevil: Update to 6.3.6.
    * gnu/packages/kde-plasma.scm (powerdevil): Update to 6.3.6.
    
    Change-Id: I7e34fa7c578c1b328746803c97de8b194180a643

--- gnu/packages/kde-plasma.scm
+++ gnu/packages/kde-plasma.scm
@@ -3100,14 +3100,14 @@ UI for Plasma")
 (define-public powerdevil
   (package
     (name "powerdevil")
-    (version "6.3.4")
+    (version "6.3.6")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://kde/stable/plasma/" version
                                   "/" name "-" version ".tar.xz"))
               (sha256
                (base32
-                "0pibn9m0jx6p506arcm6jr9168pqp0i0nwq7cwwr6ir1zlvv73xv"))))
+                "1wma65rjaix94mc3jng8i0yrcbwl3indcgmf7y0ix6f09n6gxn68"))))
     (build-system qt-build-system)
     (native-inputs (list extra-cmake-modules qttools pkg-config))
     (inputs (list bluez-qt
