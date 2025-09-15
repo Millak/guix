7cec81ba7824709bdaf84b1bd14a4ab1b3e0437b
Nicolas Graves <ngraves@ngraves.fr>
2025-09-15 09:47:31 +0200

    gnu: gnome-authenticator: Extend with (cargo-guile-json).
    * gnu/packages/gnome.scm (gnome-authenticator)[arguments]
    <#:phases>: Extend with (cargo-guile-json).
    
    Change-Id: I80c827a7f967e460bf3c49329bc0ab007b6a1c19
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -10555,6 +10555,7 @@ specified duration and save it as a GIF encoded animated image file.")
                   (guix build meson-build-system)
                   (guix build utils))
       #:phases
+      (with-extensions (list (cargo-guile-json))
       #~(modify-phases %standard-phases
           (add-after 'unpack 'prepare-for-build
             (lambda _
@@ -10581,7 +10582,7 @@ specified duration and save it as a GIF encoded animated image file.")
                '(unpack-rust-crates
                  configure
                  check-for-pregenerated-files
-                 patch-cargo-checksums)))))))
+                 patch-cargo-checksums))))))))
     (native-inputs
      (append
       (list gettext-minimal
