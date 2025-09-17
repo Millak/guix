2f3805e3d1e2ba0069ee1620a2d14c5f019b592d
Giacomo Leidi <goodoldpaul@autistici.org>
2025-09-17 15:59:15 +0200

    gnu: Add elixir-x509.
    * gnu/packages/elixir-web.scm (elixir-x509): New variable.
    
    Change-Id: Ic48ecc55662cb28f8389b2f40d73bf90181bc0da
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/packages/elixir-web.scm
+++ gnu/packages/elixir-web.scm
@@ -46,3 +46,22 @@
      "This package provides a specification for @code{WebSocket} connections.")
     (home-page "https://hexdocs.pm/websock/")
     (license license:expat)))
+
+(define-public elixir-x509
+  (package
+    (name "elixir-x509")
+    (version "0.9.2")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (hexpm-uri "x509" version))
+       (sha256
+        (base32 "1iyg91719bkxpps3l97aj2hd67xvf4xlrq2v1x5msmkyd5sxwpjc"))))
+    (build-system mix-build-system)
+    (synopsis
+     "Elixir package for working with X.509 certificates")
+    (description
+     "Elixir package for working with X.509 certificates, Certificate Signing Requests
+(CSRs), Certificate Revocation Lists (CRLs) and RSA/ECC key pairs.")
+    (home-page "https://hexdocs.pm/x509/")
+    (license license:bsd-3)))
