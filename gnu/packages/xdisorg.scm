75e243cc15db88cc39b14d0a4337a39232137f15
λx.x <case_lambda@disroot.org>
2025-09-17 11:42:09 +0200

    gnu: scrot: Update to to 1.12.1.
    * gnu/packages/xdisorg.scm (scrot): Update to 1.12.1.
    
    Closes: #2752
    Change-Id: Ifd1ff93479288669f882f2e9d5a4d03f6cdb352b
    Signed-off-by: 宋文武 <iyzsong@member.fsf.org>

--- gnu/packages/xdisorg.scm
+++ gnu/packages/xdisorg.scm
@@ -1145,7 +1145,7 @@ move windows, switch between desktops, etc.).")
 (define-public scrot
   (package
     (name "scrot")
-    (version "1.11.1")
+    (version "1.12.1")
     (source
      (origin
        (method git-fetch)
@@ -1155,7 +1155,7 @@ move windows, switch between desktops, etc.).")
          (commit version)))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "0pvp44lb2lrvm11x92vzxwl21hmcbld55kk8wjqa5k1kjg6syj9i"))))
+        (base32 "080wh1rb11xzzmwpmb0qxn7zhabkgqaklg4395dzf4p9p3wlf5hk"))))
     (build-system gnu-build-system)
     (native-inputs
      (list autoconf autoconf-archive automake pkg-config))
