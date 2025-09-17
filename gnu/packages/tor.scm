5f3d9a5662350a4a80fd5554cb1538bd0285d13a
Efraim Flashner <efraim@flashner.co.il>
2025-09-17 12:27:10 +0300

    gnu: tor: Update to 0.4.8.18.
    * gnu/packages/tor.scm (tor): Update to 0.4.8.18.
    
    Change-Id: Id7cb3084ce00dcd088f7ca6714eb3c2f984998a2

--- gnu/packages/tor.scm
+++ gnu/packages/tor.scm
@@ -1,7 +1,7 @@
 ;;; GNU Guix --- Functional package management for GNU
 ;;; Copyright © 2013, 2014, 2015, 2021 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2014, 2015 Mark H Weaver <mhw@netris.org>
-;;; Copyright © 2016-2018, 2020-2023 Efraim Flashner <efraim@flashner.co.il>
+;;; Copyright © 2016-2018, 2020-2023, 2025 Efraim Flashner <efraim@flashner.co.il>
 ;;; Copyright © 2016, 2017 Nikita <nikita@n0.is>
 ;;; Copyright © 2017–2021 Tobias Geerinckx-Rice <me@tobias.gr>
 ;;; Copyright © 2017, 2018, 2019, 2021, 2023 Eric Bavier <bavier@posteo.net>
@@ -69,14 +69,14 @@
 (define-public tor
   (package
     (name "tor")
-    (version "0.4.8.17")
+    (version "0.4.8.18")
     (source (origin
              (method url-fetch)
              (uri (string-append "https://dist.torproject.org/tor-"
                                  version ".tar.gz"))
              (sha256
               (base32
-               "0mnvijfnzkrvb0wfawfl7k7daxrp4k9b02gxd2g7p22b3mg75d3r"))))
+               "1y1sv6s4d46d5yq18ql5zqa5v5haddz5m47vpai4xzsfkl86rsja"))))
     (build-system gnu-build-system)
     (arguments
      (list #:configure-flags
