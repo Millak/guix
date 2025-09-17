5bc29e79acf2f985f74d5da62fcd9f5e31774759
Andreas Enge <andreas@enge.fr>
2025-09-17 17:06:56 +0200

    gnu: Remove abseil-cpp-20200923.3.
    * gnu/packages/cpp.scm (abseil-cpp-20200923.3): Delete variable.
    * gnu/packages/patches/abseil-cpp-20200923.3-adjust-sysinfo.patch,
    gnu/packages/patches/abseil-cpp-20200923.3-duration-test.patch: Remove
    files.
    * gnu/local.mk (dist_patch_DATA): Unregister patches.
    
    Change-Id: I52970fe693c0127850d7e7588836141b22ef0213

--- gnu/local.mk
+++ gnu/local.mk
@@ -961,8 +961,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/abcl-fix-build-xml.patch	\
   %D%/packages/patches/ableton-link-system-libraries-debian.patch	\
   %D%/packages/patches/abiword-explictly-cast-bools.patch	\
-  %D%/packages/patches/abseil-cpp-20200923.3-adjust-sysinfo.patch	\
-  %D%/packages/patches/abseil-cpp-20200923.3-duration-test.patch	\
   %D%/packages/patches/abseil-cpp-20220623.1-no-kepsilon-i686.patch	\
   %D%/packages/patches/abseil-cpp-fix-strerror_test.patch	\
   %D%/packages/patches/adb-add-libraries.patch			\
