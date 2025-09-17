036e8fada3bd7ede8551ba5a369f2005cc303340
jgart <jgart@dismail.de>
2025-09-17 09:51:06 -0500

    gnu: trealla: Update to 2.82.36.
    * gnu/packages/prolog.scm (trealla): Update to 2.82.36.
    
    Change-Id: Ib70c28036151de9dc229a0bd8e90ebaec5f455a6

--- gnu/packages/prolog.scm
+++ gnu/packages/prolog.scm
@@ -185,7 +185,7 @@ it.")
 (define-public trealla
   (package
     (name "trealla")
-    (version "2.82.35")
+    (version "2.82.36")
     (source
      (origin
        (method git-fetch)
@@ -194,7 +194,7 @@ it.")
          (url "https://github.com/trealla-prolog/trealla")
          (commit (string-append "v" version))))
        (sha256
-        (base32 "11p9wbv83xhlipzsjpfkqbz0ym8hgfj0azzfkc018by6f2vkqjsy"))
+        (base32 "0absabqhm9f69x8n8wmw8qszqnls8bqld4r8m6as2kwlhr5l00fr"))
        (file-name (git-file-name name version))))
     (build-system gnu-build-system)
     (native-inputs
