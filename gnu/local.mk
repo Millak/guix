6f8e11ad4897ff2414a75e64b23c3ea0de3ecceb
Andreas Enge <andreas@enge.fr>
2025-09-19 20:54:25 +0200

    gnu: Remove (gnu packages presentation).
    * gnu/packages/presentation.scm: Delete file.
    * gnu/local.mk (GNU_SYSTEM_MODULES), po/packages/POTFILES.in: Unregister
    file.
    
    Change-Id: I43b5ed8409bc79d84b929d9f00f57ab7ae9d4889

--- gnu/local.mk
+++ gnu/local.mk
@@ -562,7 +562,6 @@ GNU_SYSTEM_MODULES =				\
   %D%/packages/pth.scm				\
   %D%/packages/pulseaudio.scm			\
   %D%/packages/pumpio.scm			\
-  %D%/packages/presentation.scm		\
   %D%/packages/pretty-print.scm			\
   %D%/packages/protobuf.scm			\
   %D%/packages/pure.scm				\
