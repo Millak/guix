11dcc36d5ec606a8522e4500ee9511030081db87
Andreas Enge <andreas@enge.fr>
2025-09-19 21:37:00 +0200

    gnu: Unregister deleted file.
    This is a follow-up to commit 3dc53ee3e70bd109978548bd43f26386f5c93582.
    
    * gnu/packages/llvm.scm (dist_patch_DATA): Really unregister
    gnu/packages/patches/llvm-3.5-fix-clang-build-with-gcc5.patch.
    
    Change-Id: Ib49c1b506decfa1c5b0b11947964cfc9af2b61b6

--- gnu/local.mk
+++ gnu/local.mk
@@ -1805,7 +1805,6 @@ dist_patch_DATA =						\
   %D%/packages/patches/linux-pam-unix_chkpwd.patch		\
   %D%/packages/patches/lirc-localstatedir.patch			\
   %D%/packages/patches/lirc-reproducible-build.patch		\
-  %D%/packages/patches/llvm-3.5-fix-clang-build-with-gcc5.patch	\
   %D%/packages/patches/llvm-3.x.1-fix-build-with-gcc.patch	\
   %D%/packages/patches/llvm-13-gcc-14.patch			\
   %D%/packages/patches/lm-sensors-hwmon-attrs.patch		\
