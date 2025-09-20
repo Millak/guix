548538a66d14cdaf8ec9900127a59acbe9bd2950
Nicolas Graves <ngraves@ngraves.fr>
2025-09-20 15:11:52 +0200

    gnu: python-mercantile: Switch to pyproject.
    * gnu/packages/python-xyz.scm (python-mercantile):
    [source]: Switch to git-fetch.
    [build-system]: Switch to pyproject-build-system.
    [native-inputs]: Add python-setuptools.
    [description]: Improve style.
    
    Change-Id: Id6c1a3750489947935ac32c6826969a5a83f00d8
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -31510,19 +31510,23 @@ supports Image, Video, GeoJSON and TopoJSON overlays.")
     (version "1.2.1")
     (source
      (origin
-       (method url-fetch)
-       (uri (pypi-uri "mercantile" version))
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/mapbox/mercantile")
+             (commit version)))
+       (file-name (git-file-name name version))
        (sha256
-        (base32 "0sxmndhzzrvss5irsgzfrk51k6jihwcb7661992mizdgbnqnsg7s"))))
-    (build-system python-build-system)
+        (base32 "1b7kglzkjz1gmgjgk2y3n4gg70b8zdfg6kyqx2g7a3z7clxxf80f"))))
+    (build-system pyproject-build-system)
     (propagated-inputs (list python-click))
-    (native-inputs
-     (list python-check-manifest python-hypothesis python-pytest))
+    (native-inputs (list python-check-manifest python-hypothesis python-pytest
+                         python-setuptools))
     (home-page "https://github.com/mapbox/mercantile")
     (synopsis "Web mercator XYZ tile utilities")
-    (description "The mercantile module provides @code{ul(xtile, ytile, zoom)}
-and @code{bounds(xtile, ytile, zoom)} functions that respectively return the
-upper left corner and bounding longitudes and latitudes for XYZ tiles, a
+    (description
+     "The mercantile module provides @code{ul(xtile, ytile, zoom)} and
+@code{bounds(xtile, ytile, zoom)} functions that respectively return the upper
+left corner and bounding longitudes and latitudes for XYZ tiles, a
 @code{xy(lng, lat)} function that returns spherical mercator x and y
 coordinates, a @code{tile(lng, lat, zoom)} function that returns the tile
 containing a given point, and quadkey conversion functions
