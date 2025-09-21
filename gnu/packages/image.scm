24701a21e812acdbb9cad67d0af390528b572bdc
Trevor Arjeski <tmarjeski@gmail.com>
2025-09-21 16:32:19 +0300

    gnu: libicns: Fix build with gcc-14.
    * gnu/packages/image.scm (libicns)[arguments]<#:configure-flags>:
    New field, relaxing errors to warnings.
    
    Change-Id: Ie609d4acd2714f91a3675f7c60b3d13d86c39f49
    Signed-off-by: Andreas Enge <andreas@enge.fr>

--- gnu/packages/image.scm
+++ gnu/packages/image.scm
@@ -760,19 +760,26 @@ maximum quality factor.")
                 "1hjm8lwap7bjyyxsyi94fh5817xzqhk4kb5y0b7mb6675xw10prk"))))
     (build-system gnu-build-system)
     (arguments
-     (if (and (target-riscv64?)
-              (%current-target-system))
-       (list #:phases
-             #~(modify-phases %standard-phases
-                 (add-after 'unpack 'update-config-scripts
-                   (lambda* (#:key native-inputs inputs #:allow-other-keys)
-                     (for-each (lambda (file)
-                                 (install-file
+     (append
+      (list
+       #:configure-flags
+       #~(list (string-append "CFLAGS= "
+                              ;; Relax GCC 14's checks.
+                              "-Wno-error=builtin-declaration-mismatch "
+                              "-Wno-error=implicit-function-declaration")))
+      (if (and (target-riscv64?)
+               (%current-target-system))
+          (list #:phases
+                (modify-phases %standard-phases
+                  (add-after 'unpack 'update-config-scripts
+                    (lambda* (#:key native-inputs inputs #:allow-other-keys)
+                      (for-each (lambda (file)
+                                  (install-file
                                    (search-input-file
-                                     (or native-inputs inputs)
-                                     (string-append "/bin/" file)) "."))
-                               '("config.guess" "config.sub"))))) )
-       '()))
+                                    (or native-inputs inputs)
+                                    (string-append "/bin/" file)) "."))
+                                '("config.guess" "config.sub"))))))
+          '())))
     (native-inputs
      (if (and (target-riscv64?)
               (%current-target-system))
