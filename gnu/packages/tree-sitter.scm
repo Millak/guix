c6ca9ea159885160ca6d0ba9b43fe926cf838666
Evgenii Klimov <eugene.dev@lipklim.org>
2025-09-22 18:19:21 +0100

    gnu: Add python-tree-sitter-json.
    * gnu/packages/tree-sitter.scm (python-tree-sitter-json): New variable.
    
    Change-Id: I290268e402cbcd351a6a4a15222b587ec1ef2ccf
    Signed-off-by: Liliana Marie Prikler <liliana.prikler@gmail.com>

--- gnu/packages/tree-sitter.scm
+++ gnu/packages/tree-sitter.scm
@@ -924,3 +924,9 @@ to generate the package name.  When TESTS? is true, tests are enabled."
    tree-sitter-javascript
    ;; TODO: Enable tests once python-tree-sitter >= 0.22 is packaged
    #:tests? #f))
+
+(define-public python-tree-sitter-json
+  (python-tree-sitter-grammar
+   tree-sitter-json
+   ;; TODO: Enable tests once python-tree-sitter >= 0.22 is packaged
+   #:tests? #f))
