925d10cc642d60c3f67a8423b4636da3c618e983
Alexey Abramov <levenson@mmer.org>
2025-09-24 11:46:09 +0200

    gnu: linux-libre-6.16: Add armhf-linux support.
    * gnu/packages/aux-files/linux-libre/6.16-arm.conf: New file.
    * gnu/packages/linux.scm (linux-libre-6.16): Add "armhf-linux" to
    supported systems.
    * Makefile.am (AUX_FILES): Add 6.16-arm.conf.
    
    Change-Id: I2e4208bac0539c09d0f7ced1d5c978cef6534f06

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -1165,8 +1165,8 @@ Linux kernel.  It has been modified to remove all non-free binary blobs.")
   (make-linux-libre* linux-libre-6.16-version
                      linux-libre-6.16-gnu-revision
                      linux-libre-6.16-source
-                     '("x86_64-linux" "i686-linux" "aarch64-linux"
-                       "powerpc64le-linux" "riscv64-linux")
+                     '("x86_64-linux" "i686-linux" "armhf-linux"
+                       "aarch64-linux" "powerpc64le-linux" "riscv64-linux")
                      #:configuration-file kernel-config))
 
 (define-public linux-libre-version         linux-libre-6.16-version)
