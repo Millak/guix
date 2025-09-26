aa7fa7aa5f2a60af136f33615a4994ea75ee351f
Rutherther <rutherther@ditigal.xyz>
2025-09-26 19:11:44 +0200

    gnu: sway: Use wlroots-0.18.
    * gnu/packages/wm.scm (sway)[inputs]: Remove wlroots; add wlroots-0.18.
    
    Change-Id: I3f59098af9f128269733f99a80daff2b6f7484a7
    Signed-off-by: John Kehayias <john@guixotic.coop>

--- gnu/packages/wm.scm
+++ gnu/packages/wm.scm
@@ -2480,7 +2480,7 @@ narrow the items to those matching the tokens in the input.")
                   pcre2
                   swaybg
                   wayland
-                  wlroots))
+                  wlroots-0.18))
     (native-inputs
      (cons* linux-pam mesa pkg-config scdoc wayland-protocols
             (if (%current-target-system)
