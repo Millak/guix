3c422c6111afb679bcdce74e310525b112b905cc
Arun Isaac <arunisaac@systemreboot.net>
2025-09-26 21:49:33 +0100

    gnu: mumi: Update source URI.
    * gnu/packages/mail.scm (mumi)[source]: Update URI.

--- gnu/packages/mail.scm
+++ gnu/packages/mail.scm
@@ -4357,7 +4357,7 @@ It is a replacement for the @command{urlview} program.")
     (source (origin
               (method git-fetch)
               (uri (git-reference
-                    (url "https://git.savannah.gnu.org/git/guix/mumi.git/")
+                    (url "https://codeberg.org/guix/mumi.git")
                     (commit version)))
               (file-name (git-file-name name version))
               (sha256
