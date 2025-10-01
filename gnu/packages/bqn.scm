9e53e17d196f7996f6dce94d9822bba4ebc3f6a6
Lee Thompson <lee.p.thomp@gmail.com>
2025-10-01 21:03:46 +0100

    gnu: Add singeli-sources
    * gnu/packages/bqn.scm (%singeli-commit, singeli-sources): New variables.
    
    Change-Id: Ie818dcfd4d9f326d57f172baefd4c0081e886bb7
    Signed-off-by: Liliana Marie Prikler <liliana.prikler@gmail.com>

--- gnu/packages/bqn.scm
+++ gnu/packages/bqn.scm
@@ -148,6 +148,20 @@ the same author.")
       (sha256
        (base32 "0440xjvdkrbpxqjrd6nsrnaxki0mgyinsb0b1dcshjj3h3jr1yy4")))))
 
+(define %singeli-commit "53f42ce4331176d281fa577408ec5a652bdd9127")
+
+(define singeli-sources
+  (let ((commit %singeli-commit))
+    (origin
+      (method git-fetch)
+      (uri
+       (git-reference
+         (url "https://github.com/mlochbaum/Singeli")
+         (commit commit)))
+      (file-name (git-file-name "singeli" commit))
+      (sha256
+       (base32 "1dzg4gk74lhy6pwvxzhk4zj1qinc83l7i6x6zpvdajdlz5vqvc1m")))))
+
 (define cbqn-bootstrap
   (let* ((revision "2")
          (commit "66584ce1491d300746963b8ed17170348b2a03e6"))
