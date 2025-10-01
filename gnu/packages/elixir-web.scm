947c3bb9319bce8cf0cc4872c532f927f50246c7
Giacomo Leidi <goodoldpaul@autistici.org>
2025-10-01 17:34:48 +0200

    gnu: elixir-phoenix-live-favicon: Update to 1.0.0.
    * gnu/packages/elixir-web.scm (elixir-phoenix-live-favicon): Update to 1.0.0.
    
    Change-Id: I6f990243b563a2514dd7ffe5734775c704bcf538

--- gnu/packages/elixir-web.scm
+++ gnu/packages/elixir-web.scm
@@ -460,13 +460,13 @@ framework.")
 (define-public elixir-phoenix-live-favicon
   (package
     (name "elixir-phoenix-live-favicon")
-    (version "0.2.0")
+    (version "1.0.0")
     (source
      (origin
        (method url-fetch)
        (uri (hexpm-uri "phoenix_live_favicon" version))
        (sha256
-        (base32 "17m7gpx6qw0cr0d2acwajjhz2qzfi2zywzyk2i139y9higb22q8k"))))
+        (base32 "1wakzjn4z0skmmymywng2n5jqcfbfccawzcd03a5gcyy3m215h7w"))))
     (build-system mix-build-system)
     (arguments
      ;; Tests depend on makeup_diff from hex.pm but it is not packaged yet.
