396e7fb8df63f32ed859c2e9cb02cbf5b88ca5e0
宋文武 <iyzsong@member.fsf.org>
2025-10-01 17:03:47 +0800

    gnu: mpv: Use pipewire-minimal.
    * gnu/packages/video.scm (mpv)[propagated-inputs]: Replace pipewire with
    pipewire-minimal.
    
    Change-Id: I45781ec304c892ba30ff5c30a4ae7f8a2c311476
    Signed-off-by: Gabriel Wicki <gabriel@erlikon.ch>

--- gnu/packages/video.scm
+++ gnu/packages/video.scm
@@ -2745,7 +2745,7 @@ SVCD, DVD, 3ivx, DivX 3/4/5, WMV and H.264 movies.")
            ;; XXX: lua > 5.2 is not currently supported; see meson.build
            lua-5.2
            mesa
-           pipewire
+           pipewire-minimal
            pulseaudio
            shaderc
            wayland
