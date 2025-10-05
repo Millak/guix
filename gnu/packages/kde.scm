97d46cc0f93d28877bde3fb6ddf3b5f42cca8984
Sughosha <sughosha@disroot.org>
2025-10-05 19:12:18 +0530

    gnu: kosmindoormap: Update to 25.08.1.
    * gnu/packages/kde.scm (kosmindoormap): Update to 25.08.1.
    [inputs]: Add kirigami-addons.
    
    Change-Id: Ib51c6a096161ecdfcf0909203847836cdf255e6a

--- gnu/packages/kde.scm
+++ gnu/packages/kde.scm
@@ -782,17 +782,18 @@ opening hours expressions.")
 (define-public kosmindoormap
   (package
     (name "kosmindoormap")
-    (version "24.12.1")
+    (version "25.08.1")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://kde/stable/release-service/"
                                   version "/src/" name "-" version ".tar.xz"))
               (sha256
                (base32
-                "1ik8mi2ljhlj0sv1pshk6a97flagrpj40mlv2l0j6lya5ngn7mw9"))))
+                "0ilia0ydm78s7nvrh8ph5865x52zc1pd9k1qgad767sr91z5r15h"))))
     (build-system cmake-build-system)
     (native-inputs (list bison extra-cmake-modules flex python-minimal))
     (inputs (list ki18n
+                  kirigami-addons
                   kopeninghours
                   kpublictransport
                   qtbase
