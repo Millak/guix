51af33af844018774f0c199dc6524380244aaa20
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2025-10-05 22:45:00 +0200

    gnu: python-youtube-search: Fix build by switching repository.
    * gnu/packages/video.scm (python-youtube-search): Fix build by switching
    source repository to a fork. The old repository is no longer maintained.
    [source]: Switch to git-fetch.
    [native-inputs]: Remove python-wheel.
    
    Fixes: guix/guix#3754
    Change-Id: Ie7744a16fc99f419060713e870655aa5dccad209
    Reviewed-by: Nicolas Graves <ngraves@ngraves.fr>
    Modified-by: Sharlatan Hellseher <sharlatanus@gmail.com>
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/video.scm
+++ gnu/packages/video.scm
@@ -6605,17 +6605,31 @@ downloader.  It does not require a Youtube API key.")
 (define-public python-youtube-search
   (package
     (name "python-youtube-search")
-    (version "1.6.6")
+    ;; Original repository is archived, see
+    ;; https://github.com/alexmercerind/youtube-search-python/issues/189
+    ;; The most promising fork (in the sense that it works) is
+    ;; https://github.com/ahmedayyad-dev/youtube-search-python-fork,
+    ;; which is therefor packaged in Guix.
+    (properties '((commit . "6d7e16ebcdc90032392749c2f30f8e29ea0956ae")
+                  (revision . "0")))
+    (version (git-version "1.6.6"
+                          (assoc-ref properties 'revision)
+                          (assoc-ref properties 'commit)))
     (source
      (origin
-       (method url-fetch)
-       (uri (pypi-uri "youtube-search-python" version))
+       (method git-fetch)
+       (uri
+        (git-reference
+          (url "https://github.com/ahmedayyad-dev/youtube-search-python-fork")
+          (commit (assoc-ref properties 'commit))))
+       (file-name (git-file-name name version))
        (sha256
-        (base32 "1xgw6nqypnj3ymjkfyzc1vvwar73qvp08prnp15ypmzcd7bx2s25"))))
+        (base32 "1b2sc0v0xw8kqrqy1844xkdsss70npkyk26fprpw3yhz09az5k3q"))))
+    (arguments (list #:tests? #f)) ;no tests in repository
     (build-system pyproject-build-system)
     (propagated-inputs (list python-httpx))
-    (native-inputs (list python-setuptools python-wheel))
-    (home-page "https://github.com/alexmercerind/youtube-search-python")
+    (native-inputs (list python-setuptools))
+    (home-page "https://github.com/ahmedayyad-dev/youtube-search-python-fork")
     (synopsis "Search for YouTube videos, channels & playlists")
     (description
      "This package provides tools to search for @code{YouTube} videos,
