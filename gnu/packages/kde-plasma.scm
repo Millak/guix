af2738ad01e292c5cf21e990549a5e440dd2f4dd
Sughosha <sughosha@disroot.org>
2025-10-07 20:13:07 +0530

    gnu: plasma-phonebook: Add qtwayland to inputs.
    * gnu/packages/kde-plasma.scm (plasma-phonebook)[inputs]: Add qtwayland.
    
    Change-Id: If7e96568743147f632775b7750da8ff8bba8e773

--- gnu/packages/kde-plasma.scm
+++ gnu/packages/kde-plasma.scm
@@ -2658,7 +2658,8 @@ PulseAudio.")
                   kcoreaddons
                   kcontacts
                   qtdeclarative
-                  qtsvg))
+                  qtsvg
+                  qtwayland))
     (home-page "https://plasma-mobile.org/")
     (synopsis "Phonebook for Plasma Mobile devices")
     (description "This package provides contacts application which allows
