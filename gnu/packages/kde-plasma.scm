172fb3aae64dbc496522de807b85ce88cc2d625c
Sughosha <sughosha@disroot.org>
2025-10-07 19:03:20 +0530

    gnu: kinfocenter: Enable qtwayland.
    * gnu/packages/kde-plasma.scm (kinfocenter)[build-system]: Switch to
    qt-build-system.
    [arguments]<#:qtbase>: Set to qtbase.
    [inputs]: Reorder; remove qtbase; add qtwayland.
    
    Change-Id: Iffb4a18adb18cb58cf249f1095a2149425289b26

--- gnu/packages/kde-plasma.scm
+++ gnu/packages/kde-plasma.scm
@@ -862,9 +862,10 @@ functionality")
               (sha256
                (base32
                 "0ycaljixrn43hj5ladid5gn15zdfnzz7dxi9yas78akpf2a4jpsd"))))
-    (build-system cmake-build-system)
+    (build-system qt-build-system)
     (arguments
      (list
+      #:qtbase qtbase
       #:phases
       #~(modify-phases %standard-phases
           (add-after 'unpack 'set-path
@@ -924,6 +925,7 @@ functionality")
     ;; * vulkaninfo
     ;; Wayland KCM
     (inputs (list bash-minimal
+                  clinfo
                   dmidecode
                   eudev
                   fwupd
@@ -945,13 +947,12 @@ functionality")
                   pciutils
                   libplasma
                   qttools
-                  qtbase
+                  qtwayland
                   solid
                   util-linux
                   vulkan-tools
                   wayland-utils
-                  xdpyinfo
-                  clinfo))
+                  xdpyinfo))
     (propagated-inputs (list system-settings))
     (home-page "https://invent.kde.org/plasma/kinfocenter")
     (synopsis "View information about computer's hardware")
