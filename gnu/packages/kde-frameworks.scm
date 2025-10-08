f0235a2228768e3c7707731511ddd012a7ca3f44
Sughosha <sughosha@disroot.org>
2025-10-08 19:36:55 +0530

    gnu: kquickcharts: Update to 6.18.0.
    * gnu/packages/kde-frameworks.scm (kquickcharts): Update to 6.18.0.
    
    Change-Id: If1e8e6342cb6749a70a9919fc0e1024cee96dbc1

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -197,7 +197,7 @@ common build settings used in software produced by the KDE community.")
 (define-public kquickcharts
   (package
     (name "kquickcharts")
-    (version "6.16.0")
+    (version "6.18.0")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://kde/stable/frameworks/"
@@ -205,7 +205,7 @@ common build settings used in software produced by the KDE community.")
                                   "/" name "-" version ".tar.xz"))
               (sha256
                (base32
-                "0g8qkyivgxbr9frx3k77iffrj2wc5dwx2382rjxriw5innjhz9q3"))))
+                "1jvlgyy72bvq8s3933fldj7q2fa38z7wffb6f0dmiq02ilvss7ll"))))
     (build-system qt-build-system)
     (arguments (list #:qtbase qtbase))
     (native-inputs (list extra-cmake-modules glslang pkg-config))
