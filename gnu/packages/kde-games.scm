1b45488ee7facdad21e425d12296c8ff289e39f6
Sughosha <sughosha@disroot.org>
2025-10-10 13:54:41 +0530

    gnu: kblocks: Update to 25.08.2.
    * gnu/packages/kde-games.scm (kblocks): Update to 25.08.2.
    
    Change-Id: I9e709c9041c52849db8cc6d9aa775b28ef332f60

--- gnu/packages/kde-games.scm
+++ gnu/packages/kde-games.scm
@@ -608,14 +608,14 @@ This package is part of the KDE games module.")
 (define-public kblocks
   (package
     (name "kblocks")
-    (version "25.08.1")
+    (version "25.08.2")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://kde/stable/release-service/"
                            version "/src/kblocks-" version ".tar.xz"))
        (sha256
-        (base32 "1zscip75qjhxbq3lf4ysz0cwbvq009wid93lk4gdkybs7ydd1r4i"))))
+        (base32 "1zcy1rk37vrk3kg1iggvgp0xvq5jd2cvvib89k5v6vzrm6j26v2l"))))
     (build-system qt-build-system)
     (arguments (list #:qtbase qtbase))
     (native-inputs
