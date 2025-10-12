b2260f3aba2fe357ac5e8ae2bd118f29dc9c40d3
Sharlatan Hellseher <sharlatanus@gmail.com>
2025-10-12 21:37:54 +0100

    gnu: python-poetry-dynamic-versioning: Update to 1.9.1.
    * gnu/packages/python-xyz.scm (python-poetry-dynamic-versioning): Update to 1.9.1.
    [arguments] <test-flags>: Rework skipped tests.
    [propagated-inputs]: Remove poetry.
    
    Change-Id: I0d7b519fd02c7175c865c3a1e9aae892176b30ab

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -3259,34 +3259,33 @@ scatter plots, histograms and heatmaps in the terminal using braille dots.")
 (define-public python-poetry-dynamic-versioning
   (package
     (name "python-poetry-dynamic-versioning")
-    (version "1.4.0")
+    (version "1.9.1")
     (source
      (origin
        (method url-fetch)
        (uri (pypi-uri "poetry_dynamic_versioning" version))
        (sha256
-        (base32 "19v9jrawbx1nwnwrd6giyzwfq50y2mjzkrsx0ga2sbx2a2yphlbj"))))
+        (base32 "0j6sm60x0w31kf5003v4dhp9sdz1i5n5qsfd8r4wm8ksh7gvkryn"))))
     (build-system pyproject-build-system)
     (arguments
      (list
       #:test-flags
       #~(list "--ignore=tests/test_integration.py"
-              ;; E RuntimeError: Unable to detect version control system. Not
-              ;; installed: Git, Mercurial, Darcs, Subversion, Bazaar, Fossil,
-              ;; Pijul.
-              "-k"
-              (string-append
-               "not test__get_version__defaults"
-               " and not test__get_version__format_jinja"
-               " and not test__get_version__format_jinja_with_enforced_style"
-               " and not test__get_version__format_jinja_imports_with_module_only"
-               " and not test__get_version__format_jinja_imports_with_module_and_item"))))
+              "-k" (string-join
+                    ;; Tests searching for VCS back end.
+                    (list "not test__get_version__defaults"
+                          "test__enable_in_doc__project_with_dynamic"
+                          "test__enable_in_doc__project_with_version"
+                          "test__get_version__format_jinja"
+                          "test__get_version__format_jinja_imports_with_module_and_item"
+                          "test__get_version__format_jinja_imports_with_module_only"
+                          "test__get_version__format_jinja_with_enforced_style")
+                    " and not "))))
     (native-inputs
      (list python-poetry-core
            python-pytest))
     (propagated-inputs
-     (list poetry
-           python-dunamai
+     (list python-dunamai
            python-jinja2
            python-tomlkit))
     (home-page "https://github.com/mtkennerly/poetry-dynamic-versioning")
