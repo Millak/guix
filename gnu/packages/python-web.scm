68d7bd6ed1f1262c2786fba6c8cf5663a2c75adf
Danny Milosavljevic <dannym@friendly-machines.com>
2025-10-12 19:38:44 +0200

    gnu: python-ajsonrpc: Fix version.
    * gnu/packages/python-web.scm (python-ajsonrpc)[arguments] <#:phases>{set-version}:
    New phase.
    [native-inputs]: Remove python-wheel.
    
    Change-Id: If28164fcb53e91698c947d764efdc42391bc65f0
    Modified-by: Sharlatan Hellseher <sharlatanus@gmail.com>
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/python-web.scm
+++ gnu/packages/python-web.scm
@@ -11411,7 +11411,18 @@ standard library and supported by default in Quart.")
        (sha256
         (base32 "0c7jxfkv5q2m95j54dn650gcvdbpag2qcki7phvmrwsgb36w09kd"))))
     (build-system pyproject-build-system)
-    (native-inputs (list python-pytest python-setuptools python-wheel))
+    (arguments
+     (list
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-after 'unpack 'set-version
+            (lambda _
+              (substitute* "ajsonrpc/__init__.py"
+               (("^__version__ = .*")
+                (string-append "__version__ = \"" #$version "\"\n"))))))))
+    (native-inputs
+     (list python-pytest
+           python-setuptools))
     (propagated-inputs
      (list python-quart
            python-sanic
