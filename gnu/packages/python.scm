a2590694ae0350f9d7400f6f6f41fdbac2fa5340
Hartmut Goebel <h.goebel@crazy-compilers.com>
2025-10-12 20:09:14 +0200

    gnu: python: Graft secure package.
    * gnu/packages/python.scm (python/fixed): Add new variable.
    (python): Graft python/fixed.
    
    Change-Id: I8b1537a64be1984c52170ba3f2fbdca5737290e8

--- gnu/packages/python.scm
+++ gnu/packages/python.scm
@@ -678,6 +678,7 @@ def contents() -> str:
     (inherit python-3.10)
     (name "python")
     (version "3.11.11")
+    (replacement python-3.11/fixed)
     (source (origin
               (method url-fetch)
               (uri (string-append "https://www.python.org/ftp/python/"
@@ -1079,6 +1080,16 @@ def contents() -> str:
            (search-path-specification
             (variable "PYTHONTZPATH")
             (files (list "share/zoneinfo")))))))
+
+(define-public python-3.11/fixed
+  (package/inherit python-3.11
+    (version "3.11.14")
+    (source (origin
+              (inherit (package-source python-3.11))
+              (uri (string-append "https://www.python.org/ftp/python/" version
+                                  "/Python-" version ".tar.xz"))
+              (sha256 (base32
+                       "0y4v42qm66nvizjxbnixh59283a54nki51jmbrgwkhc8bkndhgld"))))))
 
 (define-public python-3.12
   (package
