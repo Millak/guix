283b437bf2d82795f1bb12228594fca439bb9c98
Sughosha <sughosha@disroot.org>
2025-10-13 20:46:36 +0530

    gnu: kguiaddons: Update to 6.19.0.
    * gnu/packages/kde-frameworks.scm (kguiaddons): Update to 6.19.0.
    
    Change-Id: I8b5adaed5507e50fdf4c67b3eeb2ab1123cf4646

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -1093,7 +1093,7 @@ infrastructure.")
 (define-public kguiaddons
   (package
     (name "kguiaddons")
-    (version "6.18.0")
+    (version "6.19.0")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -1102,7 +1102,7 @@ infrastructure.")
                     name "-" version ".tar.xz"))
               (sha256
                (base32
-                "0rn4prmj3y6gdj23gwfb70samrq6b64skzqjalc7pcni08l8farf"))))
+                "1rm1m0n566nc511mmipcfbgl2nz4mdmnr01vrncci457bdn42hxc"))))
     (build-system qt-build-system)
     ;; TODO: Build packages for the Python bindings.  Ideally this will be
     ;; done for all versions of python guix supports.  Requires python,
