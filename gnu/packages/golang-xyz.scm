7605b6257237c240f8bffff91b1bcc3de6b76a2f
Arthur Rodrigues <arthurhdrodrigues@proton.me>
2025-10-14 01:39:02 +0000

    gnu: Add go-github-com-tonistiigi-vt100.
    * gnu/packages/golang-xyz.scm (go-github-com-tonistiigi-vt100): New variable.
    
    Change-Id: I038dabe021e57e434b8138c9db71db16fdab92ea
    Modified-by: Sharlatan Hellseher <sharlatanus@gmail.com>
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/golang-xyz.scm
+++ gnu/packages/golang-xyz.scm
@@ -20744,6 +20744,32 @@ recommended.")
     (description "This package provides a simple byte size formatting in Go.")
     (license license:expat)))
 
+(define-public go-github-com-tonistiigi-vt100
+  (package
+    (name "go-github-com-tonistiigi-vt100")
+    (version "0.0.0-20240514184818-90bafcd6abab")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+              (url "https://github.com/tonistiigi/vt100")
+              (commit (go-version->git-ref version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "1vjk3yam610kc600h3hd3glsygr3m863765m9q7c0gsaj0vd38y0"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:import-path "github.com/tonistiigi/vt100"))
+    (native-inputs
+     (list go-github-com-stretchr-testify))
+    (home-page "https://github.com/tonistiigi/vt100")
+    (synopsis "Raw-mode vt100 screen reader in Golang")
+    (description
+     "Package vt100 implements a quick-and-dirty programmable ANSI terminal
+emulator.")
+    (license license:expat)))
+
 (define-public go-github-com-twpayne-go-shell
   (package
     (name "go-github-com-twpayne-go-shell")
