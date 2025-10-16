afb9c65cf3a4145d475d3a60f54a78a604681194
Sharlatan Hellseher <sharlatanus@gmail.com>
2025-10-16 10:05:03 +0100

    gnu: aerich: Update to 0.9.2.
    As it's a final program all propagated inputs moved to inputs.
    
    * gnu/packages/databases.scm (aerich): Update to 0.9.2.
    [arguments] <test-flags>: Skip one tests searching for "uvx".
    [inputs]: Add python-anyio, python-asyncclick, python-dictdiffer,
    python-tortoise-orm, python-aiomysql, python-asyncmy, python-asyncpg,
    python-psycopg, and python-tomli-w.
    [propagated-inputs]: Remove python-asyncclick, python-asyncmy,
    python-asyncpg, python-dictdiffer, python-pydantic, python-tomli-w, and
    python-tortoise-orm.
    [native-inputs]: Remove python-poetry-core; add python-pdm-backend,
    python-pydantic-2, python-pydantic-settings, and python-tortoise-vector.
    
    Change-Id: I9bcb13f0fc9f70428b5f794893d1a4bddf611a5d

--- gnu/packages/databases.scm
+++ gnu/packages/databases.scm
@@ -3962,7 +3962,7 @@ similarity distances for scementic search using embeddings.")
 (define-public aerich
   (package
     (name "aerich")
-    (version "0.8.1")
+    (version "0.9.2")
     (source
      (origin
        (method git-fetch)
@@ -3971,22 +3971,34 @@ similarity distances for scementic search using embeddings.")
               (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "1cln1ik7519n6k4lnh06w956lp8xjb0khkkpsmaj8wqlm0jbvdbi"))))
+        (base32 "07h02lv6r1hf4jjzfsd8g9yxn5df6i5qj8gmrvm2php97x45v84j"))))
     (build-system pyproject-build-system)
+    (arguments
+     (list
+      ;; tests: 56 passed, 7 skipped, 1 deselected, 1 warning
+      #:test-flags
+      ;; FileNotFoundError: [Errno 2] No such file or directory: 'uvx'
+      #~(list "--deselect=tests/test_python_m.py::test_poetry_add")))
     (native-inputs
      (list python-cryptography
+           python-pdm-backend
+           python-pydantic-2
+           python-pydantic-settings
            python-pytest
-           python-poetry-core
            python-pytest-asyncio
-           python-pytest-mock))
-    (propagated-inputs
-     (list python-asyncclick
+           python-pytest-mock
+           python-tortoise-vector))
+    (inputs
+     (list python-anyio
+           python-asyncclick
+           python-dictdiffer
+           python-tortoise-orm
+           ;; [optional]
+           python-aiomysql
            python-asyncmy
            python-asyncpg
-           python-dictdiffer
-           python-pydantic
-           python-tomli-w
-           python-tortoise-orm))
+           python-psycopg
+           python-tomli-w))
     (home-page "https://github.com/tortoise/aerich")
     (synopsis "Database migrations tool for Tortoise @acronym{ORM, Object Relational
 Mapper}")
