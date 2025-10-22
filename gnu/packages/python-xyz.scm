131a0ada332be25b3fe12fb1840ee155edc18ec3
Nicolas Graves <ngraves@ngraves.fr>
2025-10-22 22:27:26 +0200

    gnu: python-asteval: Update to 1.0.6.
    * gnu/packages/python-xyz.scm (python-asteval): Update to 1.0.6.
    [source]: Switch to git-fetch.
    [build-system]: Switch to pyproject-build-system.
    [native-inputs]: Add python-pytest-cov, python-setuptools, and
    python-setuptools-scm.
    
    Change-Id: Id721ed72211cf63128ed58fe0f8bb527dd49c081
    Modified-by: Sharlatan Hellseher <sharlatanus@gmail.com>
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -20799,22 +20799,24 @@ manipulation library.")
 (define-public python-asteval
   (package
     (name "python-asteval")
-    (version "0.9.23")
+    (version "1.0.6")
     (source
      (origin
        (method url-fetch)
        (uri (pypi-uri "asteval" version))
        (sha256
-        (base32
-         "0f54sd4w1a72ij1bcxs2x7dk9xf8bzclawijf1z18bqx9f96l2gm"))))
-    (build-system python-build-system)
+        (base32 "17x1qk07cixj1nn59pp5dzjc9jna92v6kljdss81l5rf9cqfga0s"))))
+    (build-system pyproject-build-system)
     (native-inputs
-     (list python-pytest))
+     (list python-pytest
+           python-pytest-cov
+           python-setuptools
+           python-setuptools-scm))
     (home-page "https://github.com/newville/asteval")
     (synopsis "Minimalistic evaluator of Python expressions")
     (description
      "This package provides a minimalistic evaluator of Python expression
-using the @code{ast} module")
+using the @code{ast} module.")
     (license license:expat)))
 
 (define-public python-lmfit
