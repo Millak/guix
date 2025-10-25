6d22f7d27c879afd84310b702e060d4cefda2b1f
Nicolas Graves <ngraves@ngraves.fr>
2025-10-25 13:23:04 +0200

    gnu: python-py3dns: Update to 4.0.2.
    * gnu/packages/python-xyz.scm (python-py3dns): Update to 4.0.2.
    [build-system]: Switch to pyproject-build-system.
    [arguments]: Drop <#:phases>.
    [native-inputs]: Add python-flit-core.
    [description]: Improve style.
    
    Change-Id: I714b1248e68369e0562d8875b7bb478553ef830f
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -22438,34 +22438,25 @@ It supports TSIG authenticated messages and EDNS0.")
 (define-public python-py3dns
   (package
     (name "python-py3dns")
-    (version "3.2.1")
+    (version "4.0.2")
     (source
       (origin
         (method url-fetch)
         (uri (pypi-uri "py3dns" version))
         (sha256
-         (base32
-          "1r25f0ys5p37bhld7m7n4gb0lrysaym3w318w2f8bncq7r3d81qz"))))
-    (build-system python-build-system)
-    ;; This package wants to read /etc/resolv.conf. We can't patch it without
-    ;; removing functionality so we copy from Nix and "just don't build it".
+         (base32 "0fcac412kh34r3gxvn7dc1srmjii2qs6n3lgyxh3q57cxj02wrcq"))))
+    (build-system pyproject-build-system)
     (arguments
-     `(#:phases
-       (modify-phases %standard-phases
-         (add-after 'unpack 'patch-source
-           (lambda _
-             (substitute* "setup.py"
-               (("import DNS") "")
-               (("DNS.__version__") (string-append "\"" ,version "\"")))))
-         ;; For the same reason, drop the sanity check because the library
-         ;; fails to load without /etc/resolv.conf.
-         (delete 'sanity-check))
-       #:tests? #f)) ; Also skip the tests.
+     ;; This package wants to read /etc/resolv.conf. We can't patch it without
+     ;; removing functionality so we don't run tests.
+     (list #:tests? #f))
+    (native-inputs (list python-flit-core))
     (home-page "https://launchpad.net/py3dns")
     (synopsis "Python 3 DNS library")
-    (description "This Python 3 module provides a DNS API for looking up DNS
-entries from within Python 3 modules and applications.  This module is a
-simple, lightweight implementation.")
+    (description
+     "This Python 3 module provides a DNS API for looking up DNS entries from
+within Python 3 modules and applications.  This module is a simple,
+lightweight implementation.")
     (license license:psfl)))
 
 (define-public python-email-validator
