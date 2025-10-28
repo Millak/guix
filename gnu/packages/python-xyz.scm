73502e8c4a006770d67431d89a4b288de3a907e2
Nicolas Graves <ngraves@ngraves.fr>
2025-10-28 23:07:19 +0100

    gnu: python-babel: Remove python-pytest-cov input.
    * gnu/packages/python-xyz.scm (python-babel)[native-inputs]:
    Remove python-pytest-cov and python-wheel.
    
    Change-Id: Ibec8055f67600839948f6db9fa12bef671bcf8f8
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -4708,9 +4708,7 @@ and function call return values in a human-readable way.")
     (native-inputs
      (list python-freezegun
            python-pytest
-           python-pytest-cov
            python-setuptools
-           python-wheel
            tzdata-for-tests))
     (propagated-inputs
      (list python-pytz))
