9e6abc08ed928405ee44c63b869837a6d47a0454
Simon Tournier <zimon.toutoune@gmail.com>
2025-10-28 16:19:49 +0100

    gnu: r-aneufinder: Adjust R@4.5.0 and GCC@14.
    * gnu/packages/bioconductor.scm (r-aneufinder)[arguments]: Add phases that
    relax GCC@14 strictness when using R@4.5.0 C-level facilities.
    
    Change-Id: If524806121e7b6f697b0cdee75cbe1aef3bb7b9a

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -4992,6 +4992,23 @@ create an alternative mapping from sequences.")
                (base32
                 "0zx0brvcyi9id7xli9h5nk9an7j46p7zgjj3qmwr3jm4b95qahpl"))))
     (build-system r-build-system)
+    (arguments
+     (list
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-before 'install 'fix-r-4.5.0
+            ;; Changes in R 4.5.0: C-Level Facilities.
+            ;; Strict R headers are now the default. This removes the legacy
+            ;; definitions of PI, Calloc, Realloc and Free: use M_PI,
+            ;; R_Calloc, R_Realloc or R_Free instead.
+            ;; https://cran.r-project.org/doc/manuals/r-release/NEWS.html
+            (lambda _
+              (substitute* '("src/densities.cpp"
+                             "src/loghmm.cpp"
+                             "src/scalehmm.cpp"
+                             "src/utility.cpp")
+                (("Calloc\\(") "R_Calloc(")
+                (("Free\\(") "R_Free(")))))))
     (native-inputs
      (list r-bsgenome-hsapiens-ucsc-hg19 r-knitr r-testthat))
     (propagated-inputs
