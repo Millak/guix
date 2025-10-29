305680af4a057f45542d88df48c9160ce363dc2e
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2025-10-29 11:45:05 +0100

    gnu: emacs-denote-markdown: Update to 0.2.0.
    * gnu/packages/emacs-xyz.scm (emacs-denote-markdown): Update to 0.2.0.
    
    Change-Id: I4e93c631d747d8396daf9bb900ea222d2ae88923

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -20813,14 +20813,14 @@ to streamline the journaling workflow.")
 (define-public emacs-denote-markdown
   (package
     (name "emacs-denote-markdown")
-    (version "0.1.1")
+    (version "0.2.0")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://elpa.gnu.org/packages/denote-markdown-"
                            version ".tar"))
        (sha256
-        (base32 "0ic8kqfw56xsm9s0rlq7cgnh0dzjsbbcx7kdk55dggpvxv67jj62"))))
+        (base32 "0y1lnzv2x6wnw06gx9jb38wwpd4cyn1f8r430wg26zc6grb4fmsz"))))
     (build-system emacs-build-system)
     (propagated-inputs (list emacs-denote))
     (home-page "https://github.com/protesilaos/denote-markdown")
