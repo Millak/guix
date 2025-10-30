4760733922052bf4cc1534200332c5629b336456
Cayetano Santos <csantosb@inventati.org>
2025-10-30 08:24:26 +0100

    gnu: fdroidserver: Update to 2.4.2.
    * gnu/packages/android.scm (fdroidserver): Update to 2.4.2.
    [build-system]: Switch to pyproject-build-system.
    [arguments]: Disable #:tests?.
    <#:phases>: Remove fix-versioning; add set-env.
    [propagated-inputs]: Remove python-ruamel.yaml; add python-argcomplete,
    python-asn1crypto, python-oscrypto, python-platformdirs,
    python-puremagic, python-ruamel.yaml-0.16, python-yamllint, and
    sdkmanager.
    [native-inputs]: Remove python-bcrypt, python-docker-pycreds,
    python-pynacl, and python-websocket-client; add python-setuptools.
    
    Fixes: guix/guix#3929
    Change-Id: Idb302961009ea96667f713396e7d0922c1a98201
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/android.scm
+++ gnu/packages/android.scm
@@ -55,6 +55,7 @@
   #:use-module (gnu packages golang-xyz)
   #:use-module (gnu packages graphviz)
   #:use-module (gnu packages image)
+  #:use-module (gnu packages iso-codes)
   #:use-module (gnu packages java)
   #:use-module (gnu packages linux)
   #:use-module (gnu packages pcre)
@@ -1191,49 +1192,49 @@ for communicating with Xiaomi smart appliances over miIO and MIoT protocols.")
 (define-public fdroidserver
   (package
     (name "fdroidserver")
-    (version "1.1.9")
+    (version "2.4.2")
     (source
      (origin
        (method url-fetch)
        (uri (pypi-uri "fdroidserver" version))
        (sha256
         (base32
-         "0m07f791z45w7r2dzx4yb6s54b3c3wykm3w9hn25p2jcyax082a2"))))
-    (build-system python-build-system)
+         "06xybginrwi5c7bw000wz5s5hzi0aqrxskzwh8qc6wv463w2djax"))))
+    (build-system pyproject-build-system)
     (arguments
-     `(#:phases
-       (modify-phases %standard-phases
-         (add-after 'unpack 'fix-versioning
-           (lambda _
-             (substitute* "setup.py"
-               (("0.2.1") ,(package-version python-pyasn1-modules))
-               ;; The dependency on docker has been removed upstream by
-               ;; a fairly large patch:
-               ;; https://gitlab.com/fdroid/fdroidserver/-/commit/89614851250c79a05db84070feca6dea033af334
-               ;; that is not in a release yet. It appears we can compile with
-               ;; a newer version.
-               (("docker-py >= 1.9, < 2.0") "docker >= 1.9"))
-             #t)))))
+     (list
+      #:tests? #f                       ;requires Android SDK
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-before 'check 'set-env
+            (lambda _
+              (setenv "HOME" "/tmp"))))))
     (propagated-inputs
      (list python-androguard
            python-apache-libcloud
+           python-argcomplete
+           python-asn1crypto
            python-clint
            python-defusedxml
            python-docker
            python-gitpython
            python-mwclient
+           python-oscrypto
            python-paramiko
            python-pillow
+           python-platformdirs
+           python-puremagic
            python-pyasn1
            python-pyasn1-modules
            python-pyyaml
            python-qrcode
-           python-ruamel.yaml
            python-requests
-           python-vagrant))
+           python-ruamel.yaml-0.16
+           python-vagrant
+           python-yamllint
+           sdkmanager))
     (native-inputs
-     (list python-babel python-bcrypt python-docker-pycreds python-pynacl
-           python-websocket-client))
+     (list python-babel python-setuptools))
     (home-page "https://f-droid.org")
     (synopsis "F-Droid server tools")
     (description
