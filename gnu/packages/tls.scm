b4df8b64f29d3f46be4cbe74fc4dd5c66b1bfb85
Ashish SHUKLA <ashish.is@lostca.se>
2025-11-02 14:07:39 +0000

    gnu: libressl: Update to 4.2.1.
    * gnu/packages/tls.scm (libressl): Update to 4.2.1.
    
    Closes guix/guix#4009
    
    Change-Id: I5e7f2746e98dcf19416abcf1eeab948750c732fc
    Signed-off-by: Cayetano Santos <csantosb@inventati.org>
    Signed-off-by: Rutherther <rutherther@ditigal.xyz>

--- gnu/packages/tls.scm
+++ gnu/packages/tls.scm
@@ -675,14 +675,14 @@ kilobytes of RAM.")
 (define-public libressl
   (package
     (name "libressl")
-    (version "4.2.0")
+    (version "4.2.1")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://openbsd/LibreSSL/"
                                   "libressl-" version ".tar.gz"))
               (sha256
                (base32
-                "1ch8hkpgmb1gp8d5ync902715g2laaaz3yrc7zazi3fbsx2blz8g"))))
+                "02ypmfsixjh6l24gfnsaapx0vl3i8004b1jc3xwym21mb1c2yp3d"))))
     (outputs (list "out" "debug"))
     (build-system gnu-build-system)
     (arguments
