33bca998f472dc407024e18d7dc24af4e21f5031
Sughosha <sughosha@disroot.org>
2025-11-09 09:31:54 +0530

    gnu: kde-gtk-config: Update to 6.5.2.
    * gnu/packages/kde-plasma.scm (kde-gtk-config): Update to 6.5.2.
    
    Change-Id: Ib09e47db8b11cccd4f7a12bf22b32cf24d5557a8

--- gnu/packages/kde-plasma.scm
+++ gnu/packages/kde-plasma.scm
@@ -598,14 +598,14 @@ RDP server.")
 (define-public kde-gtk-config
   (package
     (name "kde-gtk-config")
-    (version "6.5.0")
+    (version "6.5.2")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://kde/stable/plasma/" version
                                   "/kde-gtk-config-" version ".tar.xz"))
               (sha256
                (base32
-                "03if8vz046mslbagkw39hff6da26hrxpj2yyvzjxydvpq2x5ixzd"))))
+                "0acd9vg2m9kjs4i4ys1vqwrgp8zp2caa52rwcrvfc35y04scsdyc"))))
     (build-system qt-build-system)
     (arguments
      (list
