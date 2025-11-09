05a9303d3809faf8a8f078a433acf68133a6159a
Sughosha <sughosha@disroot.org>
2025-11-09 10:10:28 +0530

    gnu: dolphin-plugins: Update to 25.08.3.
    * gnu/packages/kde-systemtools.scm (dolphin-plugins): Update to 25.08.3.
    
    Change-Id: Ib25893ba4ab9f314e1fa87398d1d83b05c4c132b

--- gnu/packages/kde-systemtools.scm
+++ gnu/packages/kde-systemtools.scm
@@ -146,14 +146,14 @@ The main features of Dolphin are:
 (define-public dolphin-plugins
   (package
     (name "dolphin-plugins")
-    (version "25.08.2")
+    (version "25.08.3")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://kde/stable/release-service/" version
                            "/src/dolphin-plugins-" version ".tar.xz"))
        (sha256
-        (base32 "1j0g2p7hj6ghg84bbchwayr0s0gcy3a8frgvwwjjc0aap5cxs4a3"))))
+        (base32 "1idsm1qgc0zwhwiz8p8718b58s50ggs16vrv7xb7gkmaw0arsi18"))))
     (build-system qt-build-system)
     (native-inputs
      (list extra-cmake-modules))
