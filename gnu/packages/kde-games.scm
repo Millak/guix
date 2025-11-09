6cfb5b10b2f84ee025b5c75255450f02f14f8b2b
Sughosha <sughosha@disroot.org>
2025-11-09 10:09:31 +0530

    gnu: klines: Update to 25.08.3.
    * gnu/packages/kde-games.scm (klines): Update to 25.08.3.
    
    Change-Id: I915361818f949d3555ae7c4c264a20249845b4d2

--- gnu/packages/kde-games.scm
+++ gnu/packages/kde-games.scm
@@ -790,14 +790,14 @@ This package is part of the KDE games module.")
 (define-public klines
   (package
     (name "klines")
-    (version "25.08.2")
+    (version "25.08.3")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://kde/stable/release-service/" version
                            "/src/klines-" version ".tar.xz"))
        (sha256
-        (base32 "0pgfk1ksxv7a2677gk12rc1vv3a0jfx2j4g22x22gls398ba0w09"))))
+        (base32 "0xwsr63p37pdk8232c856qpymjwvlr5jydvjli0mlq3nj6wka489"))))
     (build-system qt-build-system)
     (arguments
      (list #:qtbase qtbase
