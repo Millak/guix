bc48d6a040a510d0be2373393d55bf8a0ba2721b
Sughosha <sughosha@disroot.org>
2025-11-09 10:10:20 +0530

    gnu: ktnef: Update to 25.08.3.
    * gnu/packages/kde-pim.scm (ktnef): Update to 25.08.3.
    
    Change-Id: I96062bdc1d1901f0119362586b74f258a7759729

--- gnu/packages/kde-pim.scm
+++ gnu/packages/kde-pim.scm
@@ -2436,14 +2436,14 @@ standard protocols for e-mail transmission.")
 (define-public ktnef
   (package
     (name "ktnef")
-    (version "25.08.2")
+    (version "25.08.3")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://kde/stable/release-service/" version
                            "/src/ktnef-" version ".tar.xz"))
        (sha256
-        (base32 "1ivq6aqc5a8xcjz90i0vvgfj7gzk1r3wczcjhjh98c4nba1lbsdz"))))
+        (base32 "04z20lksck42pvz9dx048rpqgdmi0r1vkrmlv6wxv70ia1wlf5bh"))))
     (build-system qt-build-system)
     (native-inputs
      (list extra-cmake-modules))
