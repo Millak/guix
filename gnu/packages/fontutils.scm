Millak/guix

commit ad461613d97f974e9e1aa30dc27753aa360476de

gnu: Use self-references to inputs for #:disallowed-references.
This allows ‘package-input-rewriting’ to operate as expected on these
packages.  Failing that, rewritten packages would still carry references to
non-rewritten packages in #:disallowed-references.

* gnu/packages/admin.scm (wpa-supplicant-gui): Use ‘this-package-native-input’
for #:disallowed-references.
* gnu/packages/fontutils.scm (freetype): Likewise.
* gnu/packages/glib.scm (glib-minimal): Likewise.
* gnu/packages/gnome.scm (cogl, gnome-shell): Likewise.
* gnu/packages/groff.scm (groff-minimal): Likewise.
* gnu/packages/gstreamer.scm (gstreamer): Likewise.
* gnu/packages/gtk.scm (gtk+): Likewise.
* gnu/packages/inkscape.scm (inkscape/pinned): Likewise.
* gnu/packages/java.scm (icedtea-8, openjdk9, openjdk10)
(openjdk11): Likewise.
* gnu/packages/linux.scm (f2fs-tools/static): Likewise.
* gnu/packages/man.scm (man-db): Likewise.
* gnu/packages/openldap.scm (389-ds-base): Likewise.
* gnu/packages/qt.scm (qtbase-5): Likewise.
* gnu/packages/rrdtool.scm (rrdtool): Likewise.
* gnu/packages/rust.scm (rust-bootstrapped-package): Likewise.
* gnu/packages/statistics.scm (r-with-tests): Likewise.
(r-minimal): Likewise.
* gnu/packages/texinfo.scm (info-reader): Likewise.
* gnu/packages/tls.scm (gnutls): Likewise.
(openssl-1.1): Likewise.
* gnu/packages/version-control.scm (git-minimal): Likewise.
(git): Likewise.
(gource): Likewise.
* gnu/packages/web.scm (mod-wsgi): Likewise.
* gnu/packages/xfce.scm (xfdesktop): Likewise.
* gnu/packages/perl.scm (perl-libtime-parsedate): Update comment.
* gnu/packages/video.scm (v4l-utils-minimal): Add FIXME comment.

Change-Id: I118ecd5894c24c53ec52c9208b0025ccfa3dd68a
Signed-off-by: Ludovic Courtès <ludo@gnu.org>

gnu/packages/fontutils.scm
@@ -123,7 +123,7 @@
                                 #$@(if (target-arm32?)
                                        (list "CFLAGS=-g -O2 -fno-tree-slp-vectorize")
                                        '()))
-      #:disallowed-references (list pkg-config)
+      #:disallowed-references (list (this-package-native-input "pkg-config"))
       #:phases
       #~(modify-phases %standard-phases
           (add-after 'install 'remove-reference-to-pkg-config

gnu/packages/admin.scm
@@ -2718,7 +2718,7 @@ command.")
       #:tests? #f ; no tests
       ;; Make sure the (rarely updated) package 'imagemagick/stable'
       ;; does not end up in the closure.
-      #:disallowed-references (list imagemagick/stable)
+      #:disallowed-references (list (this-package-native-input "imagemagick"))
       #:modules '((guix build qt-build-system)
                   ((guix build gnu-build-system) #:prefix gnu:)
                   (guix build utils))

gnu/packages/glib.scm
@@ -1,5 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2013, 2014, 2015, 2016, 2019, 2020, 2021 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2013-2016, 2019-2021, 2025 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2013, 2015 Andreas Enge <andreas@enge.fr>
 ;;; Copyright © 2013 Nikita Karetnikov <nikita@karetnikov.org>
 ;;; Copyright © 2014, 2015, 2016, 2017, 2018, 2021 Mark H Weaver <mhw@netris.org>
@@ -272,7 +272,7 @@ information, refer to the @samp{dbus-daemon(1)} man page.")))
     (arguments
      (list
       #:disallowed-references
-      (cons tzdata-for-tests
+      (cons (this-package-native-input "tzdata")
             ;; Verify glib-mkenums, gtester, ... use the cross-compiled
             ;; python.
             (if (%current-target-system)

gnu/packages/gnome.scm
@@ -6285,7 +6285,7 @@ throughout GNOME for API documentation).")
            pango
            wayland))
     (arguments
-     `(#:disallowed-references (,xorg-server-for-tests)
+     `(#:disallowed-references ,(list (this-package-native-input "xorg-server"))
        #:configure-flags (list ,(string-append
                                  "CFLAGS=-g -O2"
                                  " -Wno-error=implicit-function-declaration")
@@ -9432,10 +9432,10 @@ properties, screen resolution, and other GNOME parameters.")
     (build-system meson-build-system)
     (arguments
      (let ((disallowed-references
-            (list (gexp-input glib "bin")
-                  (gexp-input libxslt)
-                  (gexp-input meson)
-                  (gexp-input ruby-sass))))
+            (list (gexp-input (this-package-native-input "glib") "bin")
+                  (this-package-native-input "libxslt")
+                  meson
+                  (this-package-native-input "ruby-sass"))))
        (list
         #:glib-or-gtk? #t
         #:disallowed-references disallowed-references

gnu/packages/groff.scm
@@ -2,7 +2,7 @@
 ;;; Copyright © 2013 Andreas Enge <andreas@enge.fr>
 ;;; Copyright © 2014 Mark H Weaver <mhw@netris.org>
 ;;; Copyright © 2016 Ricardo Wurmus <rekado@elephly.net>
-;;; Copyright © 2017 Ludovic Courtès <ludo@gnu.org>
+;;; Copyright © 2017, 2025 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2019, 2020 Efraim Flashner <efraim@flashner.co.il>
 ;;; Copyright © 2019 Eric Bavier <bavier@member.fsf.org>
 ;;; Copyright © 2019 Mathieu Othacehe <m.othacehe@gmail.com>
@@ -143,7 +143,7 @@ is usually the formatter of \"man\" documentation pages.")
                      ("groff" ,groff)))
 
     (arguments
-     `(#:disallowed-references (,perl)
+     `(#:disallowed-references ,(list (this-package-native-input "perl"))
 
        #:configure-flags '("--with-doc=no")
 

gnu/packages/gstreamer.scm
@@ -446,7 +446,7 @@ the GStreamer multimedia framework.")
          "1016z0znl2j43n08rl19zdhfvfsgnmg44qh8fk6dr4qj481icrnw"))))
     (build-system meson-build-system)
     (arguments
-     (list #:disallowed-references (list python)
+     (list #:disallowed-references (list (this-package-native-input "python"))
            #:phases
            #~(modify-phases %standard-phases
                #$@%common-gstreamer-phases

gnu/packages/gtk.scm
@@ -1083,7 +1083,7 @@ application suites.")
       #:modules '((guix build utils)
                   (guix build meson-build-system)
                   ((guix build glib-or-gtk-build-system) #:prefix glib-or-gtk:))
-      #:disallowed-references (list xorg-server-for-tests)
+      #:disallowed-references (list (this-package-native-input "xorg-server"))
       #:configure-flags
       #~(list "-Dcloudproviders=true"   ;for cloud-providers support
               "-Dcolord=yes"            ;for color printing support

gnu/packages/inkscape.scm
@@ -162,7 +162,8 @@ endif()~%~%"
      (build-system cmake-build-system)
      (arguments
       (list
-       #:disallowed-references (list imagemagick/stable)
+       #:disallowed-references (list (this-package-native-input "imagemagick"))
+
        #:imported-modules `(,@%cmake-build-system-modules
                             (guix build glib-or-gtk-build-system))
        #:modules '((guix build cmake-build-system)

commit eb6b1aed25a61af5bc489bc5d0b69750f5abdff3

gnu: freetype: Pass valid #:disallowed-references when cross-compiling.
Fixes a regression introduced in ad461613d97f974e9e1aa30dc27753aa360476de
whereby ‘this-package-native-input’ would return #f when cross-compiling.

* gnu/packages/fontutils.scm (freetype)[arguments]: Adjust name passed to
‘this-package-native-input’ based on (%current-target-system).

Change-Id: I5487c9ef9cf760148aefbaa8c11f4bf417482bec

gnu/packages/fontutils.scm
@@ -123,7 +123,11 @@
                                 #$@(if (target-arm32?)
                                        (list "CFLAGS=-g -O2 -fno-tree-slp-vectorize")
                                        '()))
-      #:disallowed-references (list (this-package-native-input "pkg-config"))
+      #:disallowed-references (list (this-package-native-input
+                                     (if (%current-target-system)
+                                         (string-append "pkg-config-"
+                                                        (%current-target-system))
+                                         "pkg-config")))
       #:phases
       #~(modify-phases %standard-phases
           (add-after 'install 'remove-reference-to-pkg-config