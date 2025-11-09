c878f16c52f78ad1237669e09b82b1d2254624ca
Sughosha <sughosha@disroot.org>
2025-11-09 10:10:27 +0530

    gnu: baloo-widgets: Update to 25.08.3.
    * gnu/packages/kde-systemtools.scm (baloo-widgets): Update to 25.08.3.
    
    Change-Id: I6d3521f77123de1872e00e1f1150a6986ad4a7e3

--- gnu/packages/kde-systemtools.scm
+++ gnu/packages/kde-systemtools.scm
@@ -62,14 +62,14 @@
 (define-public baloo-widgets
   (package
     (name "baloo-widgets")
-    (version "25.08.2")
+    (version "25.08.3")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://kde/stable/release-service/" version
                            "/src/baloo-widgets-" version ".tar.xz"))
        (sha256
-        (base32 "1k8v6jkz7mdwnpzr8hvig24iggy6i90warr3qjgnh2sddz5x6v2w"))))
+        (base32 "1jz77ky8v9il4s070lbmsn89n0bpfn7csrgiw6q4ngqv6b2qw2pw"))))
     (build-system qt-build-system)
     (native-inputs
      (list extra-cmake-modules))
