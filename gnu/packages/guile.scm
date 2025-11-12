86ffb6b089d80fcb9b9da8a3bdb20160a8c57fca
Christopher Baines <mail@cbaines.net>
2025-11-12 13:16:56 +0000

    gnu: guile-sqlite3: Use sqlite-next.
    * gnu/packages/guile.scm (guile-sqlite3)[inputs]: Use sqlite-next.
    
    Change-Id: If7e0a281638c5255281df8bf03f6c79a0a33a821

--- gnu/packages/guile.scm
+++ gnu/packages/guile.scm
@@ -792,7 +792,7 @@ Guile's foreign function interface.")
               (file-name (string-append name "-" version "-checkout"))))
     (build-system gnu-build-system)
     (native-inputs (list autoconf automake guile-3.0 pkg-config))
-    (inputs (list guile-3.0 sqlite))
+    (inputs (list guile-3.0 sqlite-next))
     (synopsis "Access SQLite databases from Guile")
     (description
      "This package provides Guile bindings to the SQLite database system.")
