b1c1e42215190b67db55117509dc9ee4af434246
Sughosha <sughosha@disroot.org>
2025-11-12 12:09:48 +0530

    gnu: kde-games: Update to 25.08.2.
    * gnu/packages/kde-games.scm (kde-games)[version]: Use the version number of
    libkdegames.
    
    Change-Id: I9b30677b875a56c30fe326aee6fe590a69b1f64a

--- gnu/packages/kde-games.scm
+++ gnu/packages/kde-games.scm
@@ -1841,7 +1841,7 @@ This package is part of the KDE games module.")
 (define-public kde-games
   (package
     (name "kde-games")
-    (version "25.08.1")
+    (version (package-version libkdegames))
     (source #f)
     (build-system trivial-build-system)
     (arguments
