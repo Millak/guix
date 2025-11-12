f3d7ab3fbdd39b7bc698adf740807a94991d5d75
Sughosha <sughosha@disroot.org>
2025-11-12 16:09:04 +0530

    gnu: Add katomic.
    * gnu/packages/kde-games.scm (katomic): New variable.
    
    Change-Id: I3d048dd216b512ecaa59a394ef5bcfdbf00bc418

--- gnu/packages/kde-games.scm
+++ gnu/packages/kde-games.scm
@@ -93,6 +93,44 @@
     (description "Runtime library for kdegames")
     (license (list license:gpl2+  license:fdl1.2+))))
 
+(define-public katomic
+  (package
+    (name "katomic")
+    (version "25.08.2")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "mirror://kde/stable/release-service/" version
+                           "/src/katomic-" version ".tar.xz"))
+       (sha256
+        (base32 "0srxgmbsd6wgzbi72bjhnvp3jj1k6z80fvrr84nrvzlc6r4jpfi5"))))
+    (build-system qt-build-system)
+    (arguments
+     (list #:qtbase qtbase
+           #:tests? #f )) ;no tests
+    (native-inputs
+     (list extra-cmake-modules kdoctools))
+    (inputs
+     (list kcompletion
+           kconfig
+           kcoreaddons
+           kcrash
+           kdbusaddons
+           ki18n
+           kiconthemes
+           knewstuff
+           kwidgetsaddons
+           kxmlgui
+           libkdegames))
+    (home-page "https://apps.kde.org/katomic/")
+    (synopsis "Molecular game")
+    (description "KAtomic is a fun educational game built around molecular
+geometry.  It employs simplistic two-dimensional looks at different chemical
+elements.
+
+This package is part of the KDE games module.")
+    (license license:gpl2+)))
+
 (define-public ktuberling
   (package
     (name "ktuberling")
