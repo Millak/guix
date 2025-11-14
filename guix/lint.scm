Millak/guix

commit 7752580a33f416119d5ee2e9c553c8578a8ca23f

guix: lint: Check end of sentences for a period.
* guix/lint.scm (check-description-style): Check that sentences end with a period.

Change-Id: Ic10161f8093351dce440e19802a71f03bc849638
Signed-off-by: jgart <jgart@dismail.de>

guix/lint.scm
@@ -396,6 +396,15 @@ superfluous when building natively and incorrect when cross-compiling."
                        #:field 'description))
         '()))
 
+  (define (check-ends-with-period description)
+    "Checks that a description field ends with a period."
+    (if (not (string-suffix? "." description))
+        (list
+         (make-warning package
+                       (G_ "description should end with a period")
+                       #:field 'description))
+        '()))
+
   (define (check-texinfo-markup description)
     "Check that DESCRIPTION can be parsed as a Texinfo fragment.  If the
 markup is valid return a plain-text version of DESCRIPTION, otherwise #f."
@@ -515,6 +524,7 @@ by two spaces; possible infraction~p at ~{~a~^, ~}")
          (check-not-empty description)
          (check-quotes description)
          (check-trademarks description)
+         (check-ends-with-period description)
          (check-description-typo description
           '(("Infrastucture" . "Infrastructure")    ; codespell:ignore
             ("This packages" . "This package")      ; codespell:ignore

tests/lint.scm
@@ -108,6 +108,13 @@
    (check-description-style
     (dummy-package "x" (description "")))))
 
+(test-equal "description: does not end with a period"
+  "description should end with a period"
+  (single-lint-warning-message
+   (let ((pkg (dummy-package "x"
+                             (description "This package provides a bad description"))))
+     (check-description-style pkg))))
+
 (test-equal "description: invalid Texinfo markup"
   "Texinfo markup in description is invalid"
   (single-lint-warning-message

commit 4932444387a3ea656e3d64829b3da174c98006ea

lint: Fix regressions in tests.
This fixes a regression introduced with commit 7752580a33 ("guix: lint: Check
end of sentences for a period.").

* guix/lint.scm (check-description-style): Do not emit warning when the
description is empty or ends with trailing whitespace.
* tests/lint.scm ("description: invalid Texinfo markup")
("description: may start with texinfo markup")
("description: may not contain trademark signs: ™")
("description: may not contain trademark signs: ®"): Add period.

Fixes: #4222
Change-Id: Ib10bcf324cb55b39a88c0c67875a40a59c1d4c18

guix/lint.scm
@@ -398,7 +398,9 @@ superfluous when building natively and incorrect when cross-compiling."
 
   (define (check-ends-with-period description)
     "Checks that a description field ends with a period."
-    (if (not (string-suffix? "." description))
+    (if (not (or (string-null? description)       ;check-not-empty
+                 (string-suffix? " " description) ;check-no-trailing-whitespace
+                 (string-suffix? "." description)))
         (list
          (make-warning package
                        (G_ "description should end with a period")

tests/lint.scm
@@ -119,7 +119,7 @@
   "Texinfo markup in description is invalid"
   (single-lint-warning-message
    (check-description-style
-    (dummy-package "x" (description (identity "f{oo}b@r"))))))
+    (dummy-package "x" (description (identity "f{oo}b@r."))))))
 
 (test-equal "description: does not start with an upper-case letter"
   "description should start with an upper-case letter or digit"
@@ -131,7 +131,7 @@
 (test-equal "description: may start with texinfo markup"
   '()
   (check-description-style
-   (dummy-package "x" (description "@emph{Maxwell Equations of Software}"))))
+   (dummy-package "x" (description "@emph{Maxwell Equations of Software}."))))
 
 (test-equal "description: may start with a digit"
   '()
@@ -187,14 +187,14 @@
   "description should not contain trademark sign '™' at 20"
   (single-lint-warning-message
    (let ((pkg (dummy-package "x"
-                             (description "Does The Right Thing™"))))
+                             (description "Does The Right Thing™."))))
      (check-description-style pkg))))
 
 (test-equal "description: may not contain trademark signs: ®"
   "description should not contain trademark sign '®' at 17"
   (single-lint-warning-message
    (let ((pkg (dummy-package "x"
-                             (description "Works with Format®"))))
+                             (description "Works with Format®."))))
      (check-description-style pkg))))
 
 (test-equal "description: suggest ornament instead of quotes"