bf6f94b1eeb917301f3d5a3f6a3309e377b4fce3
Ludovic Courtès <ludo@gnu.org>
2025-11-18 13:06:58 +0100

    gnu: cuirass: Update to 1.3.3.
    * gnu/packages/ci.scm (cuirass): Update to 1.3.3.
    
    Change-Id: I32c9ef63278f94254e25c3160e9ab73611e83df0

--- gnu/packages/ci.scm
+++ gnu/packages/ci.scm
@@ -72,7 +72,7 @@
 (define-public cuirass
   (package
     (name "cuirass")
-    (version "1.3.2")
+    (version "1.3.3")
     (source
      (origin
        (method git-fetch)
@@ -82,7 +82,7 @@
        (file-name (git-file-name name version))
        (sha256
         (base32
-         "0d6ngj51q3pixi2qakc6asvaigqg0cw1qvbwvpkx285zm9csx1vd"))))
+         "0zq56wdjyxnmhr93358db95cc9i7qkffk98vscjr26pb4ab3ym21"))))
     (build-system gnu-build-system)
     (arguments
      (list #:modules `((guix build utils)
