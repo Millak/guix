7fb6d6c65e060c0f87a27c4d1acd7bd3889263ca
Ricardo Wurmus <rekado@elephly.net>
2025-11-20 14:46:40 +0100

    gnu: r-future: Update to 1.68.0.
    * gnu/packages/cran.scm (r-future): Update to 1.68.0.
    
    Change-Id: I8b89791ac957821c83bf68f57bbb35265194e686

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -34307,14 +34307,14 @@ port-forwarding to your local computer.")
 (define-public r-future
   (package
     (name "r-future")
-    (version "1.67.0")
+    (version "1.68.0")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "future" version))
        (sha256
         (base32
-         "15wlavph1gnk7cskp34jjlx4ngdsh8wm3fik5q6sypjvbh4q9jzq"))))
+         "07fbxfc51my5j6flym8v2xbyq6vw6mh99h353873fxsj0zaghi9x"))))
     (build-system r-build-system)
     (arguments
      `(#:phases
