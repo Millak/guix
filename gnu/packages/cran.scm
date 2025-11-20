be0151783c9235af77cb536cbc122241360c42f6
Ricardo Wurmus <rekado@elephly.net>
2025-11-20 10:38:36 +0100

    gnu: r-compositions: Update to 2.0-9.
    * gnu/packages/cran.scm (r-compositions): Update to 2.0-9.
    
    Change-Id: I7b2f5af86e8c91b0aa458e85758388e6076ccc4b

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -34845,14 +34845,14 @@ users of rARPACK are advised to switch to the RSpectra package.")
 (define-public r-compositions
   (package
     (name "r-compositions")
-    (version "2.0-8")
+    (version "2.0-9")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "compositions" version))
        (sha256
         (base32
-         "147j3sbqcy3hfja1zs8s8p9zzp9a68vk52s545c2p6anyj4381n5"))))
+         "1zwpw85fvnsixqmfzkg6id04yymqbp23s7l8aks3s3d23hyg59gd"))))
     (build-system r-build-system)
     (propagated-inputs
      (list r-bayesm r-mass r-robustbase r-tensora))
