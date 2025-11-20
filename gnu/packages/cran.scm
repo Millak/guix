7896ee84eea6511fd1967f048cf5de2c79011c82
Ricardo Wurmus <rekado@elephly.net>
2025-11-20 10:48:42 +0100

    gnu: r-spatstat: Update to 3.4-1.
    * gnu/packages/cran.scm (r-spatstat): Update to 3.4-1.
    
    Change-Id: I10c28f4ad9bfd6c71bd438f0d6c686df5deea096

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -48817,14 +48817,14 @@ sampler).")
 (define-public r-spatstat
   (package
     (name "r-spatstat")
-    (version "3.3-3")
+    (version "3.4-1")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "spatstat" version))
        (sha256
         (base32
-         "1sg09hdzr9a327d3nwsvbz35s3mcdxy6gxbchmwwzqs6yagb1gh6"))))
+         "1rkrrwzs9ywhj2i40kamjkrich87r7z8rzp5n4ca4bvrk3bqjn2a"))))
     (properties `((upstream-name . "spatstat")))
     (build-system r-build-system)
     (propagated-inputs
