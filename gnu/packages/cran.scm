d86dd26a1acb8a5e2ef88607996c7cce67b77067
Ricardo Wurmus <rekado@elephly.net>
2025-11-20 10:48:42 +0100

    gnu: r-rfast: Update to 2.1.5.2.
    * gnu/packages/cran.scm (r-rfast): Update to 2.1.5.2.
    
    Change-Id: I3a9a8476c28e2e6187c74734071fbec5272eb040

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -48962,14 +48962,14 @@ aggregation for comparing different implementations in order to provide a
 (define-public r-rfast
   (package
     (name "r-rfast")
-    (version "2.1.5.1")
+    (version "2.1.5.2")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "Rfast" version))
        (sha256
         (base32
-         "1fhjvabxq1r9zbkch36f5x0xf844a1z8l9njvyac5i9bxlmj9fzi"))))
+         "15qnvj3hpmlkzwl0wsmlgpywlbz9nk9ndsnkp1iiwvjvavijq0yj"))))
     (properties `((upstream-name . "Rfast")))
     (build-system r-build-system)
     (arguments
