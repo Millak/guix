79ec6628b9b7fa90035e7c36f154958ef8c6ccf0
Patrick Norton <patrick.147.norton@gmail.com>
2025-11-24 11:10:04 -0500

    gnu: Add go-github-com-a8m-tree.
    * gnu/packages/golang-xyz.scm (go-github-com-a8m-tree): New variable.
    
    Change-Id: I6b42ead8419634568034a6aa08cf2583f280b987
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/golang-xyz.scm
+++ gnu/packages/golang-xyz.scm
@@ -69,6 +69,7 @@
 ;;; Copyright © 2025 Arthur Rodrigues <arthurhdrodrigues@proton.me>
 ;;; Copyright © 2025 Tomás Ortín Fernández <quanrong@mailbox.org>
 ;;; Copyright © 2025 Allan Adair <allan@adair.no>
+;;; Copyright © 2025 Patrick Norton <patrick.147.norton@gmail.com>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -806,6 +807,34 @@ Golang.")
     (description
      "This package provides a library for environment variables
 substitution.")
+    (license license:expat)))
+
+(define-public go-github-com-a8m-tree
+  (package
+    (name "go-github-com-a8m-tree")
+    (version "0.0.0-20240104212747-2c8764a5f17e")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/a8m/tree")
+             (commit (go-version->git-ref version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0alkk8ghgmdzx4585r5562igsj39g89zlzdw109hdm5c1vmrm8dw"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      ;; "non-constant format string in call to fmt.Fprintf"
+      #:test-flags
+      #~(list "-vet=off")
+      #:import-path "github.com/a8m/tree"))
+    (home-page "https://github.com/a8m/tree")
+    (synopsis
+     "Implementation of the tree command that can be used programmatically")
+    (description
+     "This package implements the @code{tree} command in Go, in a way such that it
+can be used programatically.")
     (license license:expat)))
 
 (define-public go-github-com-abadojack-whatlanggo
