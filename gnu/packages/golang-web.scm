439c30b17b17673547f338da8c48eacf4cfd2cfe
Patrick Norton <patrick.147.norton@gmail.com>
2025-11-24 17:03:30 -0500

    gnu: Add go-github-com-putdotio-go-putio-for-rclone.
    * gnu/packages/golang-web.scm
    (go-github-com-putdotio-go-putio-for-rclone): New variable.
    
    Change-Id: I794ecf340e90c2046267ce5e2035d38318b23825
    Modified-by: Sharlatan Hellseher <sharlatanus@gmail.com>
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/golang-web.scm
+++ gnu/packages/golang-web.scm
@@ -11943,6 +11943,28 @@ characters incorrectly escaped by net/url.")
 fork of https://github.com/igungor/go-putio.")
     (license license:expat)))
 
+(define-public go-github-com-putdotio-go-putio-for-rclone
+  (hidden-package
+   (let ((commit "16d982cac2b8cbae19cc4dd129dacee91f8c1447")
+         (revision "0"))
+     (package
+       (inherit go-github-com-putdotio-go-putio)
+       (name "go-github-com-putdotio-go-putio")
+       (version (git-version "0.0.0" revision commit))
+       (source
+        (origin
+          (method git-fetch)
+          (uri (git-reference
+                 (url "https://github.com/putdotio/go-putio")
+                 (commit commit)))
+          (file-name (git-file-name name version))
+          (sha256
+           (base32 "1z4rfrhzrm6kbx7hpg00g02q88n3k6hvixd4drdadak13dsncipb"))))
+       (arguments
+        (substitute-keyword-arguments
+            (package-arguments go-github-com-putdotio-go-putio)
+          ((#:skip-build? _ #t) #t)))))))
+
 (define-public go-github-com-quic-go-qpack
   (package
     (name "go-github-com-quic-go-qpack")
