bcaea13d04e37fd0003016238c3956cbd472002c
Ricardo Wurmus <rekado@elephly.net>
2025-11-25 13:33:13 +0100

    gnu: r-bags: Update to 2.50.0.
    * gnu/packages/bioconductor.scm (r-bags): Update to 2.50.0.
    
    Change-Id: Ic2107fde1fbc63f582837064001e9f1b648ac6cd

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -5706,13 +5706,13 @@ alleles, a known phenotypical feature of cancer samples.")
 (define-public r-bags
   (package
     (name "r-bags")
-    (version "2.48.0")
+    (version "2.50.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "BAGS" version))
        (sha256
-        (base32 "1s0fqs4chr8ixxlq3a5scnncrq3bma6vz1s1isdwwxf57vp6d1v2"))))
+        (base32 "0813s292xszlw4r8xy0hnm0jkawz6l7cafv2xsa7k005lzm6dhbx"))))
     (properties `((upstream-name . "BAGS")))
     (build-system r-build-system)
     (propagated-inputs (list r-biobase r-breastcancervdx))
