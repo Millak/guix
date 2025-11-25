796c99aa4f815f2cdc17d4cabfc6e3b37efaa757
Ricardo Wurmus <rekado@elephly.net>
2025-11-25 13:33:20 +0100

    gnu: r-delayedarray: Update to 0.36.0.
    * gnu/packages/bioconductor.scm (r-delayedarray): Update to 0.36.0.
    
    Change-Id: Iab6de74e59d0c227b29bf5726d30929092759110

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -7102,13 +7102,13 @@ fold changes mean and variability for each selected gene.")
 (define-public r-delayedarray
   (package
     (name "r-delayedarray")
-    (version "0.34.1")
+    (version "0.36.0")
     (source (origin
               (method url-fetch)
               (uri (bioconductor-uri "DelayedArray" version))
               (sha256
                (base32
-                "09fx4gq0yx9k0qfxj6596dw4kn9h1wnzxwn142ld429vsn4gp3z7"))))
+                "1x5qhgnwf89hmra4lvs0sa5mqz9rm45bmw4q1469zw1brazcp3gv"))))
     (properties
      '((upstream-name . "DelayedArray")
        ;; These are needed for tests, which we don't run, because we cannot
