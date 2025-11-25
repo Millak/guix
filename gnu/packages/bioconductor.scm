cc1e429a69088b2964849f767fe5546d3191ac37
Ricardo Wurmus <rekado@elephly.net>
2025-11-25 13:33:29 +0100

    gnu: r-chipseq: Update to 1.60.0.
    * gnu/packages/bioconductor.scm (r-chipseq): Update to 1.60.0.
    
    Change-Id: Iddb48e8e5a0766f7d475ca4ce28f6c2caee62370

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -9030,14 +9030,14 @@ annotation, distance to TSS, and overlap of peaks or genes.")
 (define-public r-chipseq
   (package
     (name "r-chipseq")
-    (version "1.58.0")
+    (version "1.60.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "chipseq" version))
        (sha256
         (base32
-         "1wav01504azcp1ghcm51nxz41nf1xz7yqfdd94yxnbfq24x9k6cw"))))
+         "15vxg462svh33460xfas5kxc96nnlkprq442gplmspsiwb0jhjf3"))))
     (build-system r-build-system)
     (propagated-inputs
      (list r-biocgenerics
