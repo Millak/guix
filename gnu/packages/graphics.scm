5981d7c0b1b0c77f80fb393664e840eaf0feb782
Sharlatan Hellseher <sharlatanus@gmail.com>
2025-12-09 13:44:29 +0000

    gnu: blender: Use python-numpy@1.
    * gnu/packages/graphics.scm (blender)[inputs]: Remove python-numpy;
    add python-numpy-1.
    
    Change-Id: I0da2e589de939417d147875b909287d6d6d7ea40
    Signed-off-by: Rutherther <rutherther@ditigal.xyz>

--- gnu/packages/graphics.scm
+++ gnu/packages/graphics.scm
@@ -725,7 +725,7 @@ typically encountered in feature film production.")
            openvdb
            pugixml
            python
-           python-numpy
+           python-numpy-1
            tbb
            zlib
            `(,zstd "lib")))
