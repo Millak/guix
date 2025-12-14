ae36f472bc979bd55c555c2b68f45a2f720896f7
jgart <jgart@dismail.de>
2025-12-14 04:40:32 -0600

    teams: lisp: Add manifest.
    * etc/teams/lisp/lisp-manifest.scm: New file.
    
    Change-Id: I9e23464197978bc6236eb13d42bd9bc3069bddeb
    Signed-off-by: jgart <jgart@dismail.de>

---
+++ etc/teams/lisp/lisp-manifest.scm
@@ -0,0 +1,40 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2025 jgart <jgart@dismail.de>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; This file returns a manifest of packages built using any one of the 
+;;; asdf build systems. It is used to assist continuous integration of 
+;;; the lisp-team branch.
+
+(use-modules (guix packages)
+             (guix profiles)
+             (guix build-system))
+
+(manifest
+ (map package->manifest-entry
+      (fold-packages
+       (lambda (package lst)
+         (if (or (eq? 'asdf/sbcl (build-system-name (package-build-system package)))
+                 (eq? 'asdf/ecl (build-system-name (package-build-system package)))
+                 (eq? 'asdf/clasp (build-system-name (package-build-system package)))
+                 (string=? "clasp-cl" (package-name package))
+                 (string=? "ecl" (package-name package))
+                 (string=? "sbcl" (package-name package)))
+             (cons package lst)
+             lst))
+       '())))
+
