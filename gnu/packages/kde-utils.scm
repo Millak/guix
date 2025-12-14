e72eaab054b1bf8ebce5c1624fbbe9689219d5a7
Sughosha <sughosha@disroot.org>
2025-12-14 19:27:05 +0530

    gnu: kmousetool: Update to 25.12.0.
    * gnu/packages/kde-utils.scm (kmousetool): Update to 25.12.0.
    
    Change-Id: If8bbb0bc9bb7a1e91ff48a9fba0076e73393ce7a

--- gnu/packages/kde-utils.scm
+++ gnu/packages/kde-utils.scm
@@ -1029,14 +1029,14 @@ artists to web-designers to people with low vision.")
 (define-public kmousetool
   (package
     (name "kmousetool")
-    (version "25.08.3")
+    (version "25.12.0")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://kde/stable/release-service/" version
                            "/src/kmousetool-" version ".tar.xz"))
        (sha256
-        (base32 "13z04iqf71krpx1p48xapvb3b1y51g8k3dap6l7g2r6g441y1lvr"))))
+        (base32 "178ck5alyv585imm8nkcwpvzc43qmp28q27h6srj27f3wn6i3pwn"))))
     (build-system qt-build-system)
     (native-inputs
      (list extra-cmake-modules kdoctools))
