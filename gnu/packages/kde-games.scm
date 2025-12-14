00c402f04877fceefe8101032052d9953ba7a0b5
Sughosha <sughosha@disroot.org>
2025-12-14 19:26:21 +0530

    gnu: kiriki: Update to 25.12.0.
    * gnu/packages/kde-games.scm (kiriki): Update to 25.12.0.
    
    Change-Id: I0f0a45dd52e8eecce41eded45b0637161d62a7de

--- gnu/packages/kde-games.scm
+++ gnu/packages/kde-games.scm
@@ -1251,14 +1251,14 @@ This package is part of the KDE games module.")
 (define-public kiriki
   (package
     (name "kiriki")
-    (version "25.08.3")
+    (version "25.12.0")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://kde/stable/release-service/" version
                            "/src/kiriki-" version ".tar.xz"))
        (sha256
-        (base32 "05b345krj5i5m5f6mrmgqbxhcxq2djy3vyapyn9zcysvyxhj8qc2"))))
+        (base32 "0zsf15i4zqya9n2abb187lr2ayrykjd5h9s5pxqalg0zj7vpsnan"))))
     (build-system qt-build-system)
     (native-inputs
      (list extra-cmake-modules kdoctools))
