1e498bff0b0cd00e11ffcea44e40eb0d48672935
Sughosha <sughosha@disroot.org>
2025-12-14 19:26:28 +0530

    gnu: libkdcraw: Update to 25.12.0.
    * gnu/packages/kde-graphics.scm (libkdcraw): Update to 25.12.0.
    
    Change-Id: I5b2d304dd0d473fe3247299e1b3d92d583b6c142

--- gnu/packages/kde-graphics.scm
+++ gnu/packages/kde-graphics.scm
@@ -668,14 +668,14 @@ expression library, that is used in Krita.")
 (define-public libkdcraw
   (package
     (name "libkdcraw")
-    (version "25.08.3")
+    (version "25.12.0")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://kde/stable/release-service/" version
                            "/src/" name "-" version ".tar.xz"))
        (sha256
-        (base32 "1prclxx92zk42m2d59j8k7c1330ay678dmml5z374g712g87iyjj"))))
+        (base32 "1m2gz0md29jra8ac73q2mzq1xhi76daxrh7fc2k16hvqb4d40mji"))))
     (build-system cmake-build-system)
     (native-inputs
      (list pkg-config extra-cmake-modules))
