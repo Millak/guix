aaefa622306d3060d3d65ff246319849179e177d
Sughosha <sughosha@disroot.org>
2025-12-24 14:40:36 +0530

    gnu: solid: Update to 6.21.0.
    * gnu/packages/kde-frameworks.scm (solid): Update to 6.21.0.
    
    Change-Id: I13f67428b056362dd2de39aa6a67930a626f6c2e

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -1989,7 +1989,7 @@ feel.")
 (define-public solid
   (package
     (name "solid")
-    (version "6.19.0")
+    (version "6.21.0")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -1998,7 +1998,7 @@ feel.")
                     name "-" version ".tar.xz"))
               (sha256
                (base32
-                "1jx37iwm2rs4p8dwiifkx0mnfmvml8ha7cc20ypf3v8y425skj9s"))))
+                "0a06i5r6px1zzbqgqaaif896b49w70l7n8vbqf1lqdi3nvl0gz9z"))))
     (build-system cmake-build-system)
     (native-inputs
      (list bison dbus extra-cmake-modules flex pkg-config qttools))
