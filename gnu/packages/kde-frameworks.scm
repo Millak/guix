bc8f4ccbb21b585a3672b544c1b0d6bcb4c2bbec
Sughosha <sughosha@disroot.org>
2025-12-24 14:40:28 +0530

    gnu: kapidox: Update to 6.21.0.
    * gnu/packages/kde-frameworks.scm (kapidox): Update to 6.21.0.
    
    Change-Id: If74833c42487f8f4e6f934972f3255deec6ef5f8

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -502,7 +502,7 @@ It is the default icon theme for the KDE Plasma desktop.")
 (define-public kapidox
   (package
     (name "kapidox")
-    (version "6.19.0")
+    (version "6.21.0")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -511,7 +511,7 @@ It is the default icon theme for the KDE Plasma desktop.")
                     name "-" version ".tar.xz"))
               (sha256
                (base32
-                "0lwcabw7f91br6irwlwy4qx4929s81f9dyz4b9r8rkwis2p8a3kx"))))
+                "0pq8687qhfz9sxlg0kpv5szzfxak31v7bym1viifadfs37sbhkbf"))))
     (build-system pyproject-build-system)
     (arguments
      (list #:tests? #f ; test need network
