fc50d219a009eee4298e61e54a0e0e776baa8618
Sughosha <sughosha@disroot.org>
2025-12-24 14:40:29 +0530

    gnu: kcodecs: Update to 6.21.0.
    * gnu/packages/kde-frameworks.scm (kcodecs): Update to 6.21.0.
    
    Change-Id: I1fa2b9863cf184f0bb6b2f57327fe697303d4eb8

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -641,7 +641,7 @@ and the older vCalendar.")
 (define-public kcodecs
   (package
     (name "kcodecs")
-    (version "6.19.0")
+    (version "6.21.0")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -650,7 +650,7 @@ and the older vCalendar.")
                     name "-" version ".tar.xz"))
               (sha256
                (base32
-                "1ldfcklymq4ypagmhmwp9hsp1b4im0q6vkz22vs3lkxwpilc4q9s"))))
+                "05kyarripvbf5d3jq1rhgha8jijj0b8ns8n7w87d3ybc9vlib86r"))))
     (build-system cmake-build-system)
     (native-inputs (list extra-cmake-modules gperf qttools))
     (inputs (list qtbase))
