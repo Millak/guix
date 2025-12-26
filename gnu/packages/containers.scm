38a67bf99155970501e55f93684f2692eb35cc75
Foster Hangdaan <foster@hangdaan.email>
2025-12-26 13:35:32 -0500

    gnu: podman: Update to 5.7.1.
    * gnu/packages/containers.scm (podman): Update to 5.7.1.
    
    Closes: guix/guix#5148
    Signed-off-by: 宋文武 <iyzsong@member.fsf.org>

--- gnu/packages/containers.scm
+++ gnu/packages/containers.scm
@@ -572,7 +572,7 @@ Its main purpose is to support the key usage by @code{docker-init}:
 (define-public podman
   (package
     (name "podman")
-    (version "5.7.0")
+    (version "5.7.1")
     (outputs '("out" "docker"))
     (properties
      `((output-synopsis "docker" "docker alias for podman")
@@ -585,7 +585,7 @@ Its main purpose is to support the key usage by @code{docker-init}:
              (url "https://github.com/containers/podman")
              (commit (string-append "v" version))))
        (sha256
-        (base32 "0p1hh3sy5m1l1151wsda3q346pd4dm9hgpwjqakdha8yixyicwj8"))
+        (base32 "16lb58r8vkngics6fn1b3six8bl3nrl9awrnq6ivdv1grfgy9z61"))
        (file-name (git-file-name name version))))
     (build-system gnu-build-system)
     (arguments
