c09c950658f0d542e71af9cd577cd3f32af459de
Andreas Enge <andreas@enge.fr>
2026-01-05 21:58:17 +0100

    gnu: Remove hdf-eos2.
    * gnu/packages/maths.scm (hdf-eos2): Delete variable.
    
    Change-Id: I71e3ba4106b2d800439e4affc7b65d7b428bb160
    Signed-off-by: Rutherther <rutherther@ditigal.xyz>

--- gnu/packages/maths.scm
+++ gnu/packages/maths.scm
@@ -2215,44 +2215,6 @@ Interface to wrap the HDF5 library, which is implemented in C.")
 (define-deprecated-package hdf-java
   hdf5-java)
 
-(define-public hdf-eos2
-  (package
-    (name "hdf-eos2")
-    (version "19.1.0")
-    (source
-     (origin
-       (method url-fetch)
-       (uri "ftp://edhs1.gsfc.nasa.gov\
-/edhs/hdfeos/latest_release/HDF-EOS2.19v1.00.tar.Z")
-       (sha256
-        (base32 "0c9fcz25s292ldap12wxmlrvnyz99z24p63d8fwx51bf8s0s1zrz"))
-       (patches (search-patches "hdf-eos2-remove-gctp.patch"
-                                "hdf-eos2-build-shared.patch"
-                                "hdf-eos2-fortrantests.patch"))))
-    (build-system gnu-build-system)
-    (native-inputs
-     (list gfortran))
-    (inputs
-     `(("hdf4" ,hdf4-alt) ; assume most HDF-EOS2 users won't use the HDF4 netCDF API
-       ;; XXX: These inputs are really dependencies of hdf4.
-       ("zlib" ,zlib)
-       ("libjpeg" ,libjpeg-turbo)
-       ("libtirpc" ,libtirpc)
-
-       ("gctp" ,gctp)))
-    (arguments
-     `( #:configure-flags '("--enable-install-include" "--enable-shared"
-                            "CC=h4cc -Df2cFortran" "LIBS=-lgctp")
-        #:parallel-tests? #f))
-    (home-page "https://hdfeos.org/software/library.php#HDF-EOS2")
-    (synopsis "HDF4-based data format for NASA's Earth Observing System")
-    (description "HDF-EOS2 is a software library built on HDF4 which supports
-the construction of data structures used in NASA's Earth Observing
-System (Grid, Point and Swath).")
-
-    ;; Source files carry a permissive license header.
-    (license (license:non-copyleft home-page))))
-
 (define-public hdf-eos5
   (package
     (name "hdf-eos5")
