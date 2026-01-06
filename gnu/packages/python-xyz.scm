0415cedcb3364e77ea89da986152929b2d192879
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2026-01-06 18:50:58 +0100

    gnu: python-celery: Fix build on CI.
    The issue with "Too many open files" is known by upstream and has been
    reported several times already:
    
    - high concurrency results in OSError,
    <https://github.com/celery/celery/issues/4541>, (2018).
    - Autoreload doesn't reload if modules imported by the tasks module
    change, <https://github.com/celery/celery/issues/1025>, (2012).
    - OSError: Too many open files,
    <https://github.com/celery/celery/issues/887>, (2012).
    
    * gnu/packages/python-xyz.scm (python-celery): Fix build on ci.
    [arguments]<:#test-flags>: Disable tests that fail on the CI.
    
    Change-Id: I47f132a7f4c10cfa28e756699a565cb53c4f2e04
    Reviewed-by: Cayetano Santos <csantosb@inventati.org>
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>
    Signed-off-by: Rutherther <rutherther@ditigal.xyz>

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -20761,6 +20761,8 @@ Python 2.4 and 2.5, and will draw its fixes/improvements from python-trunk.")
     (build-system pyproject-build-system)
     (arguments
      (list
+      ;; tests: 3165 passed, 26 skipped, 6 deselected, 3 xfailed, 53 warnings,
+      ;;        28958 subtests passed
       #:test-flags
       ;; The MongoDB backend test appears to expect an older version of
       ;; MongoDB which provided its own bson module, fails with "
@@ -20768,8 +20770,15 @@ Python 2.4 and 2.5, and will draw its fixes/improvements from python-trunk.")
       #~(list "--ignore=t/unit/backends/test_mongodb.py"
               ;; XXX: Requires google-cloud-firestore
               "--ignore=t/unit/backends/test_gcs.py"
-              ;; AssertionError.
-              "-k" "not test_check_privileges_no_fchown")
+              "-k" (string-join
+                    (list ;; AssertionError.
+                          "not test_check_privileges_no_fchown"
+                          ;; These 3 tests error out on ci.guix.gnu.org with
+                          ;; OSError: [Errno 24] Too many open files
+                          "test_register_with_event_loop__no_on_tick_dupes"
+                          "test_with_autoscaler_file_descriptor_safety"
+                          "test_with_file_descriptor_safety")
+                    " and not "))
       #:phases
       #~(modify-phases %standard-phases
           (add-after 'unpack 'relax-requirements
