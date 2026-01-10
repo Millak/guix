864ccf19422a6249714b653907a4537036ecaadc
Aaron Covrig <aarcov@gmail.com>
2026-01-10 05:54:02 -0500

    gnu: ruby-unicode-display-width: Update to 3.2.0.
    * gnu/packages/ruby-xyz.scm (ruby-unicode-display-width): Update to 3.2.0.
    [propagated-inputs]: Add ruby-unicode-emoji.
    
    Change-Id: Ida9c8fc4f005153ae1395ac9939441d7b405263d
    Signed-off-by: Steve George <steve@futurile.net>

--- gnu/packages/ruby-xyz.scm
+++ gnu/packages/ruby-xyz.scm
@@ -9336,6 +9336,10 @@ run.")
                           (target-riscv64?)))
        #:phases
        (modify-phases %standard-phases
+         (add-after 'unpack 'loosen-dependencies
+           (lambda _
+             (substitute* "rubocop.gemspec"
+               (("'unicode\\-display\\_width'.*") "'unicode-display_width')\n"))))
          (add-before 'check 'set-home
            (lambda _
              (setenv "HOME" (getcwd))))
@@ -11286,18 +11290,19 @@ requests.  This is useful when testing software.")
 (define-public ruby-unicode-display-width
   (package
     (name "ruby-unicode-display-width")
-    (version "2.4.2")
+    (version "3.2.0")
     (source
      (origin
        (method url-fetch)
        (uri (rubygems-uri "unicode-display_width" version))
        (sha256
         (base32
-         "1gi82k102q7bkmfi7ggn9ciypn897ylln1jk9q67kjhr39fj043a"))))
+         "0hiwhnqpq271xqari6mg996fgjps42sffm9cpk6ljn8sd2srdp8c"))))
     (build-system ruby-build-system)
     (arguments
      '(;; Test data not included.
        #:tests? #f))
+    (propagated-inputs (list ruby-unicode-emoji))
     (synopsis "Determine the monospace display width of Ruby strings")
     (description
      "@code{Unicode::DisplayWidth} is a Ruby library which can determine the
