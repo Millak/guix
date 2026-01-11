8c9ec8751a69aee449207cb2b3ed85dffb1e93ae
Roman Scherer <roman@burningswell.com>
2026-01-11 11:10:00 +0100

    gnu: go-github-com-charmbracelet-x-xpty: Use git-fetch/lfs.
    The charmbracelet/x repository uses Git LFS for binary files (*.png, *.jpg,
    *.ttf).  Using git-fetch/lfs ensures proper checkout of these files,
    preventing hash mismatches across different architectures.
    
    * gnu/packages/golang-xyz.scm (go-github-com-charmbracelet-x-xpty): Use
    git-fetch/lfs method.
    
    Relates-to: https://codeberg.org/guix/guix/pulls/5190
    Change-Id: I3377e2830dd9e5a3cec645550624f65ed3f1f0a6
    Reviewed-by: Johannes Christ <jc@jchri.st>
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/golang-xyz.scm
+++ gnu/packages/golang-xyz.scm
@@ -5182,7 +5182,7 @@ package provides an API for comparing Golden files.")
     (version "0.1.3")
     (source
      (origin
-       (method git-fetch)
+       (method git-fetch/lfs)
        (uri (git-reference
               (url "https://github.com/charmbracelet/x")
               (commit (go-version->git-ref version
