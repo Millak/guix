4e4c7cdf0a1a36a89cf2162def24731d150bc4b5
Ricardo Wurmus <rekado@elephly.net>
2026-01-14 19:42:05 +0100

    gnu: r-arules: Update to 1.7.13.
    * gnu/packages/cran.scm (r-arules): Update to 1.7.13.
    
    Change-Id: I7ceb542c223bd6e9ff4fd06288203d0ab3d8e28c
    Signed-off-by: Rutherther <rutherther@ditigal.xyz>

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -23992,14 +23992,14 @@ used.")
 (define-public r-arules
   (package
     (name "r-arules")
-    (version "1.7-11")
+    (version "1.7.13")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "arules" version))
        (sha256
         (base32
-         "0p4rxd4k7j1nhz05zw2w0rqiryvrw75gcga2nrmmmp7ghd42g527"))))
+         "1fjw5q74midxai0vw7d5q8sldcifi0xgxd46d04libph9riklijf"))))
     (build-system r-build-system)
     (propagated-inputs
      (list r-generics r-matrix))
