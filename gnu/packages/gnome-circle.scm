9a9f6d28d0a9174ea6a1b0e7b9d8f042b6f50373
Liliana Marie Prikler <liliana.prikler@gmail.com>
2026-01-15 06:42:43 +0100

    gnu: komikku-servers: Update to 1.99.0.
    * gnu/packages/gnome-circle.scm (komikku-servers): Update to 1.99.0.

--- gnu/packages/gnome-circle.scm
+++ gnu/packages/gnome-circle.scm
@@ -541,7 +541,7 @@ developed with the aim of being used with the Librem 5 phone.")
 (define-public komikku-servers
   (package
     (name "komikku-servers")
-    (version "1.84.0")
+    (version "1.99.0")
     (source
      (origin
        (method git-fetch)
@@ -551,7 +551,7 @@ developed with the aim of being used with the Librem 5 phone.")
        (file-name (git-file-name name version))
        (sha256
         (base32
-         "0sa2hq0qs20pmb13if2m37hlhk1a8741hl8pnj937az9hbsghg3g"))))
+         "1did8rh3aw6d3g1myavsma986rwashw09g3m442lv8fzmdiw18r4"))))
     (build-system copy-build-system)
     (arguments
      (list
