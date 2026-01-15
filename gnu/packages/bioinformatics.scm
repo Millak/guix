c927e30c3bc5930e63f8ca8beb0ae66ee518158f
Ricardo Wurmus <rekado@elephly.net>
2026-01-15 12:50:46 +0100

    gnu: r-zarrarray: Update to 1.0-2.3cc3e6a.
    * gnu/packages/bioinformatics.scm (r-zarrarray): Update to 1.0-2.3cc3e6a.
    [properties]: Record updater-extra-native-inputs.
    [propagated-inputs]: Remove r-pizzarr; add r-rarr.
    [native-inputs]: Add r-testthat.
    [arguments]: Disable tests.
    
    Change-Id: Id3a9b564c21ee779e4a46e02cd478d06d9dd5bc9
    Signed-off-by: Rutherther <rutherther@ditigal.xyz>

--- gnu/packages/bioinformatics.scm
+++ gnu/packages/bioinformatics.scm
@@ -23154,8 +23154,8 @@ frames with arbitrary sets of columns.")
       (license license:expat))))
 
 (define-public r-zarrarray
-  (let ((commit "508d87193b20feba8cd24fa96b33e1bc9cc49958")
-        (revision "1"))
+  (let ((commit "3cc3e6acf986074dfaece2ad23f9b56962054625")
+        (revision "2"))
     (package
       (name "r-zarrarray")
       (version (git-version "1.0" revision commit))
@@ -23167,17 +23167,22 @@ frames with arbitrary sets of columns.")
                (commit commit)))
          (file-name (git-file-name name version))
          (sha256
-          (base32 "0fz7zj9ixmz6yh709cd55abc5378b549cx454cgk274nhp24xdbr"))))
-      (properties `((upstream-name . "ZarrArray")))
+          (base32 "1qjj92cpxpm3zh05vf60h5gmcn1qady0s38hibb0l57iwdhlhrb3"))))
+      (properties
+       '((upstream-name . "ZarrArray")
+         (updater-extra-native-inputs . ("r-testthat"))))
       (build-system r-build-system)
+      ;; The tests seem to be outdated.
+      (arguments (list #:tests? #false))
       (propagated-inputs (list r-biocgenerics
                                r-delayedarray
                                r-iranges
                                r-matrix
-                               r-pizzarr
+                               r-rarr
                                r-s4arrays
                                r-s4vectors
                                r-sparsearray))
+      (native-inputs (list r-testthat))
       (home-page "https://github.com/BIMSBbioinfo/ZarrArray")
       (synopsis "Zarr backend for DelayedArray objects")
       (description "Zarr backend for @code{DelayedArray} objects.")
