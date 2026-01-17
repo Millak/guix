ad436df5cdd931ef8b442f107e07c6a9b8be5af3
Sharlatan Hellseher <sharlatanus@gmail.com>
2026-01-17 14:33:19 +0000

    gnu: Remove python-pre-commit.
    * gnu/packages/version-control.scm (python-pre-commit): Delete variable.
    
    Change-Id: I0f8ced695ba57ffd3260897bb3587bd18f096d00

--- gnu/packages/version-control.scm
+++ gnu/packages/version-control.scm
@@ -2695,9 +2695,6 @@ specify a list of hooks you want and pre-commit manages the installation and
 execution of any hook written in any language before every commit.")
     (license license:expat)))
 
-(define-deprecated-package python-pre-commit
-  pre-commit)
-
 (define-public mercurial
   (package
     (name "mercurial")
