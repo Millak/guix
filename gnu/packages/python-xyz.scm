4b388188f870eb37120514050ac9cc4530628e3e
Nicolas Graves <ngraves@ngraves.fr>
2026-01-19 02:28:22 +0100

    gnu: Deprecate python-ukpostcodeparser.
    * gnu/packages/python-xyz.scm
    (python-ukpostcodeparser): Deprecate package.
    (python-faker)[native-inputs]: Drop python-ukpostcodeparser.
    
    Change-Id: Ib80d408cc4eb7e54b79128c7d14065fd2f432972
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -22327,7 +22327,6 @@ lightweight implementation.")
      (list python-freezegun
            python-pytest
            python-setuptools
-           python-ukpostcodeparser
            python-validators))
     (propagated-inputs
      (list python-tzdata))
@@ -39071,7 +39070,7 @@ bindings for Python 3.")
 distance (Ukkonen).")
     (license license:expat)))
 
-(define-public python-ukpostcodeparser
+(define-deprecated/public python-ukpostcodeparser #f
   (package
     (name "python-ukpostcodeparser")
     (version "1.1.2")
