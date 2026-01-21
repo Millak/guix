f235a916b10d073c99b067c7442a6e7f85e83660
Sharlatan Hellseher <sharlatanus@gmail.com>
2026-01-21 14:14:01 +0000

    gnu: Remove python-cython-3.
    * gnu/packages/python-xyz.scm (python-cython-3): Delete variable.
    
    Change-Id: I702030f70a2063ef2a913b78e28f0dd132c09b7c

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -10929,9 +10929,6 @@ writing C extensions for Python as easy as Python itself.")
        (sha256
         (base32 "0d0n0yyicr7icd4f5kn1wwbjqyad6j4m640xlqlk3ixchgad7bf3"))))))
 
-;; It may be removed after 2026-01-24.
-(define-deprecated/public-alias python-cython-3 python-cython)
-
 ;; NOTE: when upgrading numpy please make sure that python-numba,
 ;; python-pandas and python-scipy still build, as these three packages are
 ;; often used together.
