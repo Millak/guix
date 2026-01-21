5576d9cc52c8fde31716bb1df43d3b72a066eaf9
Sharlatan Hellseher <sharlatanus@gmail.com>
2026-01-21 14:17:43 +0000

    gnu: Remove pudb.
    * gnu/packages/python-xyz.scm (pudb): Delete variable.
    
    Change-Id: I9cdd207262a70bd0cfec89ce36e9ee035cf2ac67

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -33815,9 +33815,6 @@ all the niceties of modern GUI-based debuggers in a more lightweight and
 keyboard-friendly package.")
     (license license:expat)))
 
-(define-deprecated-package pudb
-  python-pudb)
-
 (define-public python-iwlib
   (package
     (name "python-iwlib")
