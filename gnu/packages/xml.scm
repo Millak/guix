348032e6272e1c453e86490594c0d0f24bac4109
Nicolas Graves <ngraves@ngraves.fr>
2026-01-27 16:01:50 +0100

    gnu: xmlsec: Update to 1.3.9.
    * gnu/packages/xml.scm (xmlsec): Update to 1.3.9.
    [source]: Switch to git-fetch.
    [arguments]<#:configure-flags>: Disable dynamic loading support, as it
    is not required and causes peculiar configuration requirements.
    [inputs]: Remove libltdl.
    
    Change-Id: I538fd6ff14fdc8f52531f7cd6ebd15fa063437f6
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/xml.scm
+++ gnu/packages/xml.scm
@@ -1207,19 +1207,26 @@ XSL-T processor.  It also performs any necessary post-processing.")
 (define-public xmlsec
   (package
     (name "xmlsec")
-    (version "1.3.7")
+    (version "1.3.9")
     (source (origin
               (method url-fetch)
               (uri (string-append "https://www.aleksey.com/xmlsec/download/"
                                   "xmlsec1-" version ".tar.gz"))
               (sha256
                (base32
-                "1shk40mpaqaf05skgyxa7qxgcarjd6i1fadn2sk0b8lakfv96bnq"))))
+                "0bxnz4y097gbx1hx945h7j5pdycwbba3bfgmp6nyd1kbgb6whcd6"))))
     (build-system gnu-build-system)
+    (arguments
+     (list
+      #:configure-flags
+      ;; Enabling dynamic loading requires to set LD_LIBRARY_PATH, instead
+      ;; separate packages are used, so it has no added value.
+      #~(list "--disable-crypto-dl"
+              "--disable-apps-crypto-dl")))
     (propagated-inputs                  ; according to xmlsec1.pc
      (list libxml2 libxslt))
     (inputs
-     (list gnutls libgcrypt libltdl))
+     (list gnutls libgcrypt))
     (native-inputs
      (list pkg-config))
     (home-page "https://www.aleksey.com/xmlsec/")
