17811597996c9080096ddbc894513a538f878740
Maxim Cournoyer <maxim.cournoyer@gmail.com>
2026-01-31 20:27:43 +0100

    gnu: vte: Update to 0.82.3.
    * gnu/packages/gnome.scm (vte): Update to 0.82.3.
    [inputs]: Add fast-float and simdutf.

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -119,6 +119,8 @@
   #:use-module (gnu packages check)
   #:use-module (gnu packages cmake)
   #:use-module (gnu packages compression)
+  #:use-module (gnu packages c)
+  #:use-module (gnu packages cpp)
   #:use-module (gnu packages cups)
   #:use-module (gnu packages curl)
   #:use-module (gnu packages cyrus-sasl)
@@ -4550,7 +4552,7 @@ targeting the GNOME stack simple.")
 (define-public vte
   (package
     (name "vte")
-    (version "0.78.2")
+    (version "0.82.3")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://gnome/sources/vte/"
@@ -4558,7 +4560,7 @@ targeting the GNOME stack simple.")
                                   "vte-" version ".tar.xz"))
               (sha256
                (base32
-                "144qqk638n7fbql8542a02lp0nvh2vhci098l6s4cs1m0zgbrmrm"))))
+                "118ajdfi2hqrhsfvbkjn566lxsmi6lrvmqh339yx0c7fdy7jgikd"))))
     (build-system meson-build-system)
     (arguments
      (list #:configure-flags #~(list "-Dgtk3=false"
@@ -4573,7 +4575,7 @@ targeting the GNOME stack simple.")
            gperf
            python
            libxml2))
-    (inputs (list lz4))
+    (inputs (list fast-float lz4 simdutf))
     (propagated-inputs
      (list gtk                          ; required by vte-2.91.pc
            gnutls                       ; ditto
