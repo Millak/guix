7f2b824fbce34a17aa99058eaa70c5dd75a3adf3
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2026-02-02 22:59:48 +0100

    gnu: emacs-posframe: Update to 1.5.1.
    * gnu/packages/emacs-xyz.scm (emacs-posframe): Update to 1.5.1.
    
    Change-Id: I104d47c0006cda32fec3381ffcee8dba7371b2d7

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -23943,14 +23943,14 @@ the center of the screen and not at the bottom.")
 (define-public emacs-posframe
   (package
     (name "emacs-posframe")
-    (version "1.5.0")
+    (version "1.5.1")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://elpa.gnu.org/packages/"
                            "posframe-" version ".tar"))
        (sha256
-        (base32 "0yk38fc08fgxwai6dn6da9yykcmq3bd4x7msfnlrg081b15q9a32"))))
+        (base32 "1g1pcf83w4fv299ykvx7b93kxkc58fkr6yk39sxny5g16d4gl80g"))))
     (build-system emacs-build-system)
     ;; emacs-minimal does not include the function font-info.
     (arguments
