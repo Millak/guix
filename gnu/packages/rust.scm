f60e53b3e65fbcf9eb84c08a101db1c692a218d6
moksh <mysticmoksh@riseup.net>
2026-02-03 19:13:46 +0530

    gnu: Add rust-1.92.
    * gnu/packages/rust.scm (rust-1.92): New variable.
    
    Change-Id: I161cdaa06113639200b0d1417aa82ed641218ae7
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/rust.scm
+++ gnu/packages/rust.scm
@@ -1708,6 +1708,65 @@ ge13ca993e8ccb9ba9847cc330696e02839f328f7/jemalloc"))
                      (copy-recursively "stage2/lib"
                                        (string-append out "/lib")))))))))))))
 
+(define-public rust-1.92
+  (let ((base-rust
+         (rust-bootstrapped-package
+          rust-1.91 "1.92.0"
+          "1f6305lkp4vwj132fq232mfxdcxg0d5vymc2fpf5y9vybjkjq3cy")))
+    (package
+      (inherit base-rust)
+      (source
+       (origin
+         (inherit (package-source base-rust))
+         (snippet
+          '(begin
+             (for-each delete-file-recursively
+                       '("src/llvm-project"
+                         "vendor/curl-sys-0.4.79+curl-8.12.0/curl"
+                         "vendor/curl-sys-0.4.83+curl-8.15.0/curl"
+                         "vendor/jemalloc-sys-0.5.3+5.3.0-patched/jemalloc"
+                         "vendor/jemalloc-sys-0.5.4+5.3.0-patched/jemalloc"
+                         "vendor/libffi-sys-4.0.0/libffi"
+                         "vendor/libz-sys-1.1.21/src/zlib"
+                         "vendor/libz-sys-1.1.22/src/zlib"
+                         "vendor/libmimalloc-sys-0.1.44/c_src/mimalloc"
+                         "vendor/openssl-src-111.28.2+1.1.1w/openssl"
+                         "vendor/openssl-src-300.5.0+3.5.0/openssl"
+                         "vendor/openssl-src-300.5.3+3.5.4/openssl"
+                         "vendor/tikv-jemalloc-sys-0.5.4+5.3.0-patched/jemalloc"
+                         "vendor/tikv-jemalloc-sys-0.6.0+5.3.0-1-\
+ge13ca993e8ccb9ba9847cc330696e02839f328f7/jemalloc"))
+             ;; Remove vendored dynamically linked libraries.
+             ;; find . -not -type d -executable -exec file {} \+ | grep ELF
+             ;; Also remove the bundled (mostly Windows) libraries.
+             (for-each delete-file
+                       (find-files "vendor" "\\.(a|dll|exe|lib)$"))
+             ;; Use the packaged nghttp2.
+             (for-each
+              (lambda (ver)
+                (let ((vendored-dir
+                       (format #f "vendor/libnghttp2-sys-~a/nghttp2" ver))
+                      (build-rs
+                       (format #f "vendor/libnghttp2-sys-~a/build.rs" ver)))
+                  (delete-file-recursively vendored-dir)
+                  (delete-file build-rs)
+                  (call-with-output-file build-rs
+                    (lambda (port)
+                      (format port "fn main() {~@
+                           println!(\"cargo:rustc-link-lib=nghttp2\");~@
+                           }~%")))))
+              '("0.1.11+1.64.0"))
+             ;; Adjust vendored dependency to explicitly use rustix with libc
+             ;; backend.
+             (substitute* '("vendor/tempfile-3.14.0/Cargo.toml"
+                            "vendor/tempfile-3.16.0/Cargo.toml"
+                            "vendor/tempfile-3.19.1/Cargo.toml"
+                            "vendor/tempfile-3.20.0/Cargo.toml"
+                            "vendor/tempfile-3.21.0/Cargo.toml"
+                            "vendor/tempfile-3.23.0/Cargo.toml")
+               (("features = \\[\"fs\"" all)
+                (string-append all ", \"use-libc\""))))))))))
+
 (define (make-ignore-test-list strs)
   "Function to make creating a list to ignore tests a bit easier."
   (map (lambda (str)
