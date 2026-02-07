54c45b2dcd4b0accb0b3b469220d6c953bc14353
Sharlatan Hellseher <sharlatanus@gmail.com>
2026-02-07 23:05:46 +0000

    gnu: Remove go-github-com-stretchr-testify-bootstrap.
    * gnu/packages/golang-check.scm (go-github-com-stretchr-testify-bootstrap): Delete variable.
    
    Change-Id: Id6f474284172799f1abe0dea427bb3101339757b

--- gnu/packages/golang-check.scm
+++ gnu/packages/golang-check.scm
@@ -2737,18 +2737,6 @@ Features include:
 @end itemize")
     (license license:expat)))
 
-(define-public go-github-com-stretchr-testify-bootstrap
-  (hidden-package
-    (package
-      (inherit go-github-com-stretchr-testify)
-      (arguments
-       '(#:import-path "github.com/stretchr/testify"
-         #:tests? #f
-         #:phases (modify-phases %standard-phases
-                    (delete 'build))))
-      (propagated-inputs
-       (list go-gopkg-in-yaml-v3)))))
-
 (define-public go-github-com-stvp-go-udp-testing
   (package
     (name "go-github-com-stvp-go-udp-testing")
