c32694a2b4aceb35250ee26bbb78020a4fc1c5bb
Yan Abu Arab <yanabuarab@gmail.com>
2026-02-13 02:38:44 +0200

    gnu: python-pydantic-core: Cleanup unneeded phase.
    * gnu/packages/python-xyz.scm (python-pydantic-core)[phases]{patch-pyproject}:
    Remove phase.
    
    Merges: https://codeberg.org/guix/guix/pulls/6358
    Change-Id: I04d0aaac7aec000db4e276e61097d55f9d8c630e
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -10706,12 +10706,7 @@ errors when data is invalid.")
                         '(unpack-rust-crates
                           configure
                           check-for-pregenerated-files
-                          patch-cargo-checksums))))
-          (add-after 'unpack 'patch-pyproject
-            (lambda _
-              ;; maturin failed to parce pyproject.toml.
-              (substitute* "pyproject.toml"
-                (("^license-files = .*") "")))))))
+                          patch-cargo-checksums)))))))
     (native-inputs
      (append
       (list maturin
