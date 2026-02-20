0741844d93844227836e9adade7d55dee73f06de
Ricardo Wurmus <rekado@elephly.net>
2026-02-20 14:23:12 +0100

    gnu: r-voltron: Replace custom phase with #:skipped-tests.
    * gnu/packages/bioinformatics.scm (r-voltron)[arguments]: Remove phase
    'skip-bad-tests; provide #:skipped-tests argument.
    
    Change-Id: I00ddc80185972d5bf7e01856b8ab0c47ffcf3fd2

--- gnu/packages/bioinformatics.scm
+++ gnu/packages/bioinformatics.scm
@@ -22747,6 +22747,12 @@ patterns.")
     (build-system r-build-system)
     (arguments
      (list
+      #:skipped-tests
+      '(("test_conversion.R"
+         ;; These fail to raise an error because of our patch in the
+         ;; 'do-not-use-conda phase, which falls back to Python from Guix.
+         "as.AnnData, python path"
+         "as.ometiff, python path"))
       #:phases
       #~(modify-phases %standard-phases
           ;; By default VoltRon will use Basilisk, which uses Conda to set
@@ -22760,14 +22766,6 @@ patterns.")
                  (string-append
                   "source(paste0(system.file(package=\"VoltRon\"), \"/guix-refs.R\"));\n"
                   "return(guix_python);")))))
-          (add-after 'unpack 'skip-bad-tests
-            (lambda _
-              (with-directory-excursion "tests/testthat"
-                (substitute* "test_conversion.R"
-                  ((".*as.AnnData, python path.*" m)
-                   (string-append m "skip('skip');\n"))
-                  ((".*as.ometiff, python path.*" m)
-                   (string-append m "skip('skip');\n"))))))
           ;; We do this outside of the source code to ensure that
           ;; references are accessible to Guix.
           (add-after 'install 'record-python-reference
