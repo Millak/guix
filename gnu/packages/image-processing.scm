1c8df7a2defb6d029fd54fb8175038cbdfd9437d
Vinicius Monego <monego@posteo.net>
2026-02-21 17:43:51 -0300

    gnu: gmic: Update to 3.7.2.
    * gnu/packages/image-processing.scm (gmic): Update to 3.7.2.
    
    Change-Id: I161369edb93ffb19cf619379d826b4a0d07b6861

--- gnu/packages/image-processing.scm
+++ gnu/packages/image-processing.scm
@@ -928,14 +928,14 @@ due to its architecture which automatically parallelises the image workflows.")
 (define-public gmic
   (package
     (name "gmic")
-    (version "3.6.5")
+    (version "3.7.2")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "https://gmic.eu/files/source/gmic_"
                            version ".tar.gz"))
        (sha256
-        (base32 "16hncbj7isxcw5al3zf62n8iwpsvbnmfclihdbgq4fnwci6yb1q9"))))
+        (base32 "0bxkc4l8pychlkifxxm7qj4hj2fk67fc1wk46n9vp2y89p61hw2b"))))
     (build-system cmake-build-system)
     (arguments
      (list
