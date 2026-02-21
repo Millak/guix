fe3b37f4ee512708a3513b393e7bb8a97ea1842e
Ricardo Wurmus <rekado@elephly.net>
2026-02-21 23:34:40 +0100

    gnu: python-pytorch-for-r-torch: Use python-build-system.
    The "bdist_wheel" target fails with:
    ModuleNotFoundError: No module named 'build_bundled'
    
    * gnu/packages/machine-learning.scm (python-pytorch-for-r-torch)
    [build-system]: Revert to python-build-system.
    
    Change-Id: I4cfc0ed0c9465663b3b0159e55ad9c2c1b8e42b8

--- gnu/packages/machine-learning.scm
+++ gnu/packages/machine-learning.scm
@@ -5429,6 +5429,7 @@ in the audio domain.")
     (name "python-pytorch")
     (version %python-pytorch-for-r-torch-version)
     (source %python-pytorch-for-r-torch-src)
+    (build-system python-build-system)
     (inputs
      (modify-inputs (package-inputs python-pytorch)
        (replace "gloo" gloo-for-r-torch)))
