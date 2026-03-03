dd7a2adc5fd5c61e47889fad8bf2e3da730f7fc0
Sughosha <sughosha@disroot.org>
2026-03-03 21:57:27 +0530

    gnu: knotifyconfig: Update to 6.23.0.
    * gnu/packages/kde-frameworks.scm (knotifyconfig): Update to 6.23.0.
    
    Change-Id: I2300a3ee131f3d7dd3ee745c475a451a12976262

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -4044,7 +4044,7 @@ specification.")
 (define-public knotifyconfig
   (package
     (name "knotifyconfig")
-    (version "6.22.0")
+    (version "6.23.0")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -4053,7 +4053,7 @@ specification.")
                     name "-" version ".tar.xz"))
               (sha256
                (base32
-                "1d4xlggxszcc42bln63jyf9yadmhhshvfbs3imvnzl0q3r58dihl"))))
+                "17mbgablndi0csvldqj65lv8619ldgfpk4q36ibwx5wp8d79s6cx"))))
     (build-system cmake-build-system)
     (arguments (list #:tests? #f))
     (native-inputs
