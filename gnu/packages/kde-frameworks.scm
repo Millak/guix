deaec02d1782b5b96f9cebaa1ebffb9adb1ef04d
Sughosha <sughosha@disroot.org>
2026-03-03 21:57:22 +0530

    gnu: knotifications: Update to 6.23.0.
    * gnu/packages/kde-frameworks.scm (knotifications): Update to 6.23.0.
    
    Change-Id: I928426488386cc58c689aa750fb3c48b5f8c3fd1

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -2679,7 +2679,7 @@ asynchronous jobs.")
 (define-public knotifications
   (package
     (name "knotifications")
-    (version "6.22.0")
+    (version "6.23.0")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -2688,7 +2688,7 @@ asynchronous jobs.")
                     name "-" version ".tar.xz"))
               (sha256
                (base32
-                "1320fwma07ha7dfici3fd4wnxgyxw0z9n587mirsrpzkrkrsx6n4"))))
+                "0vkxxcwqrivqm0l19xwd1s74vcrspd0fdns67gwbkv43wg4mq191"))))
     (build-system cmake-build-system)
     (arguments
      (list #:tests? #f
