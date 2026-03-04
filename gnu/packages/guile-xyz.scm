81a4a181b1758964d81ab6c1e132629e29dd78b1
Grigory Shepelev <shegeley@gmail.com>
2026-03-04 16:37:45 +0300

    gnu: guile-wayland-scm: Update to 0.0.3.
    * gnu/packages/guile-xyz.scm (guile-wayland-scm): Update to 0.0.3.
    
    Change-Id: I5173ed91bf930519e7bbf155d55ccb7a180256cb
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/packages/guile-xyz.scm
+++ gnu/packages/guile-xyz.scm
@@ -3616,7 +3616,7 @@ Probably can be replaced with guile's built-in file-descriptor procedures.")
 (define-public guile-wayland-scm
   (package
     (name "guile-wayland-scm")
-    (version "0.0.1")
+    (version "0.0.3")
     (source (origin
               (method git-fetch)
               (uri (git-reference
@@ -3625,7 +3625,7 @@ Probably can be replaced with guile's built-in file-descriptor procedures.")
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "17xfc2826nh96ivsszv4avpcs4gbli8k3r0i1idyxkdhxa745rm7"))))
+                "0xxran6690777d64i01ap3a76zjm3panfpplljpvsdzdxhbccbld"))))
     (build-system gnu-build-system)
     (arguments
      (list
