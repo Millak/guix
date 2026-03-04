bd83194e8afdf929e5d580f676e93ef35c41d6c2
Hugo Buddelmeijer <hugo@buddelmeijer.nl>
2026-03-04 22:13:56 +0100

    gnu: python-can: Fix build.
    * gnu/packages/python-xyz.scm (python-can): Fix build.
    [native-inputs]: Remove python-future.
    
    Change-Id: I189672b60c5367a6c5af202aad27984f3c260634
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -6642,7 +6642,6 @@ your Python package version as a calendar version.")
     (propagated-inputs (list python-packaging python-wrapt))
     (native-inputs
      (list ;; python-canalystii ; Not packed yet
-           python-future
            python-hypothesis
            python-mock
            python-parameterized
