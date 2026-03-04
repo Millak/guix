dc597e30fe2ea223f36c426179ada90ec1e6e4fc
Liliana Marie Prikler <liliana.prikler@gmail.com>
2026-03-04 19:16:55 +0100

    gnu: immer: Update to 0.9.1.
    * gnu/packages/cpp.scm (immer): Update to 0.9.1.
    [#:configure-flags]: Drop “-DDISABLE_WERROR=ON”.

--- gnu/packages/cpp.scm
+++ gnu/packages/cpp.scm
@@ -2763,40 +2763,34 @@ comma separated value (CSV) files.")
       (license license:bsd-3))))
 
 (define-public immer
-  ;; Use latest commit to fix build with gcc 14.
-  (let ((commit "df6ef46d97e1fe81f397015b9aeb32505cef653b")
-        (revision "0"))
-    (package
-      (name "immer")
-      (version (git-version "0.8.1" revision commit))
-      (source (origin
-                (method git-fetch)
-                (uri (git-reference
-                       (url "https://github.com/arximboldi/immer")
-                       (commit commit)))
-                (file-name (git-file-name name version))
-                (sha256
-                 (base32 "032rb84ahvdnc1m6sj4lflrwnk4p1f2jsq1pv03xbgizp2lr2pkx"))))
-      (build-system cmake-build-system)
-      (arguments
-       (list
-        ;; -Werror appears to report false positives.
-        ;; See <https://github.com/arximboldi/immer/issues/223>.
-        #:configure-flags #~'("-DDISABLE_WERROR=ON")
-        #:modules `((guix build cmake-build-system)
-                    ((guix build gnu-build-system) #:prefix gnu:)
-                    (guix build utils))
-        #:phases
-        #~(modify-phases %standard-phases
-            (replace 'check (assoc-ref gnu:%standard-phases 'check)))))
-      (inputs (list boost libgc c-rrb))
-      (native-inputs (list catch2-3 doctest fmt pkg-config))
-      (home-page "https://sinusoid.es/immer")
-      (synopsis "Immutable data structures")
-      (description "Immer is a library of persistent and immutable data structures
+  (package
+    (name "immer")
+    (version "0.9.1")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                     (url "https://github.com/arximboldi/immer")
+                     (commit (string-append "v" version))))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32 "04ckvmi391pspqiglzgi8h57gwr8822xhlrg5qdxf5yg6scgkfj2"))))
+    (build-system cmake-build-system)
+    (arguments
+     (list
+      #:modules `((guix build cmake-build-system)
+                  ((guix build gnu-build-system) #:prefix gnu:)
+                  (guix build utils))
+      #:phases
+      #~(modify-phases %standard-phases
+          (replace 'check (assoc-ref gnu:%standard-phases 'check)))))
+    (inputs (list boost libgc c-rrb))
+    (native-inputs (list catch2-3 doctest fmt pkg-config))
+    (home-page "https://sinusoid.es/immer")
+    (synopsis "Immutable data structures")
+    (description "Immer is a library of persistent and immutable data structures
 written in C++.")
-      (properties '((lint-hidden-cpe-vendors . ("immer_project"))))
-      (license license:boost1.0))))
+    (properties '((lint-hidden-cpe-vendors . ("immer_project"))))
+    (license license:boost1.0)))
 
 (define-public zug
   (package
