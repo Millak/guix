Millak/guix

commit e1fc5936b117d9dc35cd37711e9aa032c28b3b14

gnu: vis: Remove wrapping of LUA_{,C}PATH and wrap GUIX_LUA_{,C}PATH.
* gnu/packages/text-editors.scm (vis)[arguments]<#:phases>{wrap-binary}: Wrap
GUIX_LUA_PATH and GUIX_LUA_CPATH instead of LUA_PATH and LUA_CPATH.

Change-Id: Id8ff4d37f797c6dba2516241a0ea1c4a20d5451a
Signed-off-by: Andreas Enge <andreas@enge.fr>

gnu/packages/text-editors.scm
@@ -370,16 +370,10 @@ intuitive, while also taking advantage of the capabilities of modern terminals."
          (delete 'check)                ; the tests need a wrapped vis
          (add-after 'install 'wrap-binary
            (lambda* (#:key inputs outputs #:allow-other-keys)
-             (let* ((out (assoc-ref outputs "out"))
-                    (lpeg (assoc-ref inputs "lua-lpeg"))
-                    (lua-version ,(version-major+minor (package-version lua)))
-                    (LUA_PATH (string-append lpeg "/share/lua/"
-                                             lua-version "/?.lua"))
-                    (LUA_CPATH (string-append lpeg "/lib/lua/"
-                                              lua-version "/?.so")))
+             (let* ((out (assoc-ref outputs "out")))
                (wrap-program (string-append out "/bin/vis")
-                 `("LUA_PATH" ":" prefix (,LUA_PATH))
-                 `("LUA_CPATH" ":" prefix (,LUA_CPATH)))
+                 `("GUIX_LUA_PATH" ":" prefix (,(getenv "GUIX_LUA_PATH")))
+                 `("GUIX_LUA_CPATH" ":" prefix (,(getenv "GUIX_LUA_CPATH"))))
                #t)))
          (add-after 'wrap-binary 'check
            (assoc-ref %standard-phases 'check))

commit 3765eb786c0ef2ac85ab36f0da37e64569a86be6

gnu: vis: Fix Lua paths.
* gnu/packages/text-editors.scm (vis)[arguments]<#:phases>{wrap-binary}:
  Use semicolon (;) as the separator in GUIX_LUA_PATH and GUIX_LUA_CPATH.

Fixes: e1fc5936b117 ("gnu: vis: [...] wrap GUIX_LUA_{,C}PATH.")
Change-Id: I8a32d74042f0e85fd3f9d98854a4a5bd8d220363
Signed-off-by: Gabriel Wicki <gabriel@erlikon.ch>

gnu/packages/text-editors.scm
@@ -373,8 +373,8 @@ intuitive, while also taking advantage of the capabilities of modern terminals."
            (lambda* (#:key inputs outputs #:allow-other-keys)
              (let* ((out (assoc-ref outputs "out")))
                (wrap-program (string-append out "/bin/vis")
-                 `("GUIX_LUA_PATH" ":" prefix (,(getenv "GUIX_LUA_PATH")))
-                 `("GUIX_LUA_CPATH" ":" prefix (,(getenv "GUIX_LUA_CPATH"))))
+                 `("GUIX_LUA_PATH" ";" prefix (,(getenv "GUIX_LUA_PATH")))
+                 `("GUIX_LUA_CPATH" ";" prefix (,(getenv "GUIX_LUA_CPATH"))))
                #t)))
          (add-after 'wrap-binary 'check
            (assoc-ref %standard-phases 'check))