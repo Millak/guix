Millak/guix

commit f7b8fbb504094ad617c7102579559efcc56db5fb

home: services: Add home-gc-root-service-type.
* gnu/home/services.scm (home-gc-root-service-type): New variable.

Merges: https://codeberg.org/guix/guix/pulls/4236
Change-Id: I648068fc005a993b9527ed13ae87470c5ce9b693

gnu/home/services.scm
@@ -3,6 +3,7 @@
 ;;; Copyright © 2021 Xinglu Chen <public@yoctocell.xyz>
 ;;; Copyright © 2022-2023, 2025 Ludovic Courtès <ludo@gnu.org>
 ;;; Copyright © 2023 Carlo Zancanaro <carlo@zancanaro.id.au>
+;;; Copyright © 2025 Giacomo Leidi <therewasa@fishinthecalculator.me>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -51,6 +52,7 @@
             home-activation-service-type
             home-run-on-change-service-type
             home-provenance-service-type
+            home-gc-root-service-type
 
             literal-string
             literal-string?
@@ -756,6 +758,14 @@ environment, and its configuration file, when available.")))
 
 (define sexp->home-provenance sexp->system-provenance)
 (define home-provenance system-provenance)
+
+
+;;;
+;;; Garbage collector roots.
+;;;
+
+(define home-gc-root-service-type
+  (system->home-service-type gc-root-service-type))
 
 
 ;;;

commit 5b2fcc51bc887995a0399d62371c8e2052946b95

home: services: Fix location of home-gc-root-service-type.
The home-gc-root-service-type has gnu/services.scm as location but,
being in gnu/home/services.scm, it should have
gnu/home/services.scm. The tests/home-services.scm test has been broken
since f7b8fbb504094ad617c7102579559efcc56db5fb .
This commit fixes that, by instantiating a new
service-type record instead of directly using the one built by
system->home-service.

This was initially reported by Yelninei at
https://codeberg.org/guix/guix/issues/6946#issuecomment-11318779 .

* gnu/home/services/scm (home-gc-root-service-type): Instantiate a new
service-type record, instead of directly using the one built by
system->home-service.

Fixes: https://codeberg.org/guix/guix/issues/6960
Change-Id: I4389e6bbd2b2e4b0657768228651e8b1c01fee3a

gnu/home/services.scm
@@ -765,7 +765,9 @@ environment, and its configuration file, when available.")))
 ;;;
 
 (define home-gc-root-service-type
-  (system->home-service-type gc-root-service-type))
+  (service-type
+    (inherit (system->home-service-type gc-root-service-type))
+    (default-value '())))
 
 
 ;;;