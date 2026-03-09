746ce0fe2fe7b9a99445dc467cffcb1c8702aec9
Sughosha <sughosha@disroot.org>
2026-03-09 07:42:04 +0530

    gnu: kbounce: Update to 25.12.3.
    * gnu/packages/kde-games.scm (kbounce): Update to 25.12.3.
    
    Change-Id: I89af7644dbeb171d3f90452e8e504086b2077f60

--- gnu/packages/kde-games.scm
+++ gnu/packages/kde-games.scm
@@ -606,14 +606,14 @@ This package is part of the KDE games module.")
 (define-public kbounce
   (package
     (name "kbounce")
-    (version "25.12.2")
+    (version "25.12.3")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://kde/stable/release-service/"
                            version "/src/kbounce-" version ".tar.xz"))
        (sha256
-        (base32 "081v9m1s7zccqb3myvq645z2xcg2kk1kz61iljb98bdngrmk7m6m"))))
+        (base32 "025fdkp3792dlh3b2kc2h4516sjnlv2x1pvn410c6ffcr2mrwk3d"))))
     (build-system qt-build-system)
     (arguments
      (list #:qtbase qtbase
