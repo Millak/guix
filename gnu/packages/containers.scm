78b59a2e9d2dd99deab433527d4de5cb3cea3b50
Foster Hangdaan <foster@hangdaan.email>
2026-03-11 17:37:05 -0400

    gnu: podman: Update to 5.8.1.
    * gnu/packages/containers.scm (podman): Update to 5.8.1.
    
    Merges: https://codeberg.org/guix/guix/pulls/7068
    Change-Id: I5d20b0b2b92cb08c16c739e09c461a7c69a15d4f
    Signed-off-by: Sharlatan Hellseher <sharlatanus@gmail.com>

--- gnu/packages/containers.scm
+++ gnu/packages/containers.scm
@@ -708,7 +708,7 @@ Its main purpose is to support the key usage by @code{docker-init}:
 (define-public podman
   (package
     (name "podman")
-    (version "5.8.0")
+    (version "5.8.1")
     (outputs '("out" "docker"))
     (properties
      `((output-synopsis "docker" "docker alias for podman")
@@ -721,7 +721,7 @@ Its main purpose is to support the key usage by @code{docker-init}:
              (url "https://github.com/containers/podman")
              (commit (string-append "v" version))))
        (sha256
-        (base32 "0vyl6d198iqrxx3c5vgbdx95fxkmn9fcb0napq44h8bzvjcl9fnj"))
+        (base32 "1wdsdc0nj4m1v8mn95dhavi87ad0adqmld70da59gvp2abff5f0f"))
        (file-name (git-file-name name version))))
     (build-system gnu-build-system)
     (arguments
