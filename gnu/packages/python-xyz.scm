4e0aaeac6c2d940bbd23b9098666b8e7d45fe366
Sharlatan Hellseher <sharlatanus@gmail.com>
2026-03-15 16:50:36 +0000

    gnu: python-pydantic-core: Adjust inputs.
    * gnu/packages/python-xyz.scm (python-pydantic-core)[native-inputs]:
    Remove python-dirty-equals; add python-dirty-equals-bootstrap.
    
    Change-Id: I35ac1000927495ea451f49c8ccfeb97de6e58c63

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -10812,7 +10812,7 @@ errors when data is invalid.")
     (native-inputs
      (append
       (list maturin
-            python-dirty-equals
+            python-dirty-equals-bootstrap
             python-pytest
             python-pytest-benchmark ;TODO: disable in pyproject.toml
             python-pytest-mock
