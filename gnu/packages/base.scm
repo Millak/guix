Millak/guix

commit 2a50c9598bb7fe4175c4f29df07656a7f0a07801

gnu: Reference the inherited ‘arguments’ value.
This commit was made by running this command:

  sed -e's/substitute-keyword-arguments (package-arguments [a-zA-Z0-9-]\+)/substitute-keyword-arguments arguments/g' -i gnu/packages/*.scm

… and then:

  1. reverting changes from ‘gnu/packages/rust.scm’ and
     ‘gnu/packages/java.scm’ since they would incur derivation changes and/or
     breakage;
  2. reverting the change for ‘gcc-final’ in ‘gnu/packages/commencement.scm’;
  3. reverting the change for ‘onnx-optimizer’, ‘openquest’, and ‘certbot’,
     which use ‘substitute-keyword-arguments’ for arguments that are not
     inherited (and thus ‘arguments’ would be unbound);
  4. reverting the change for ‘insight-toolkit-legacy’ and ‘wine64-staging’
     which make bogus assumptions about inherited arguments.

Change-Id: I122a7cf517b6b63cae38944b5d33ade4b1f5a89c

gnu/packages/base.scm
@@ -602,7 +602,7 @@ standard.")
                 (base32
                  "0mxhw43d4wpqmvg0l4znk1vm10fy92biyh90lzdnqjcic2lb6cg8"))))
      (arguments
-      (substitute-keyword-arguments (package-arguments coreutils-minimal)
+      (substitute-keyword-arguments arguments
         ((#:phases phases '%standard-phases)
          `(modify-phases ,phases
             (add-before 'check 'disable-broken-test
@@ -756,7 +756,7 @@ included.")
                "1cmd0riv37bqy9mwbg6n3523qgr8b3bbm5kwj19sjrasl4yq9d0c"))
              (patches '())))
    (arguments
-    (substitute-keyword-arguments (package-arguments binutils)
+    (substitute-keyword-arguments arguments
       ((#:make-flags _ #~'()) #~'())))
    (native-inputs '())
    (properties '())))
@@ -774,7 +774,7 @@ included.")
        (sha256
         (base32 "1j64m2czn9ygd5g1cjjcw7q43b18xh6qkrdl1pkm03ncjnj3wwrl"))))
     (arguments
-     (substitute-keyword-arguments (package-arguments binutils)
+     (substitute-keyword-arguments arguments
        ((#:configure-flags flags)
         #~(cons* "--enable-gold=default"
                  (delete "LDFLAGS=-static-libgcc" #$flags)))
@@ -802,7 +802,7 @@ included.")
      (modify-inputs (package-native-inputs binutils)
        (append texinfo))) ; because makeinfo is needed when building bfd alone
     (arguments
-     (substitute-keyword-arguments (package-arguments binutils)
+     (substitute-keyword-arguments arguments
        ;; Only build as a shared library
        ((#:configure-flags flags)
         #~(append #$flags '("--enable-shared" "--disable-static")))
@@ -1295,7 +1295,7 @@ with the Linux kernel.")
                                        "glibc-hurd-mach-print.patch"
                                        "glibc-hurd-gettyent.patch"))))
     (arguments
-     (substitute-keyword-arguments (package-arguments glibc)
+     (substitute-keyword-arguments arguments
        ((#:configure-flags flags #~'())
         #~(cons* "CFLAGS=-g -O2 -Wno-error=builtin-declaration-mismatch"
                  "--enable-crypt"
@@ -1335,7 +1335,7 @@ with the Linux kernel.")
                                   "glibc-hurd-clock_gettime_monotonic.patch")))
                              (origin-patches (package-source glibc-2.35)))))))
     (arguments
-     (substitute-keyword-arguments (package-arguments glibc)
+     (substitute-keyword-arguments arguments
        ((#:configure-flags flags #~'())
         #~(cons* #$(string-append
                     "CFLAGS=-g -O2"
@@ -1747,7 +1747,7 @@ and daylight-saving rules.")
   (hidden-package
     (package/inherit tzdata
       (arguments
-        (substitute-keyword-arguments (package-arguments tzdata)
+        (substitute-keyword-arguments arguments
           ((#:phases phases)
            #~(modify-phases #$phases
                (add-after 'post-install 'install-leap-seconds

commit d940f695e4e41e59f346698d359e464e831ed431

gnu: glibc@2.33: Fix build.
This fixes a regression introduced in 2a50c9598bb7fe4175c4f29df07656a7f0a07801.

* gnu/packages/base.scm (glibc-2.33): Inherit from ‘glibc’, not ‘glibc-2.35’.

Change-Id: I8ff44043c232ebefa900ace8cead4481882115c2
Signed-off-by: Ludovic Courtès <ludo@gnu.org>
Merges: #7354

gnu/packages/base.scm
@@ -1315,7 +1315,7 @@ with the Linux kernel.")
 
 (define-public glibc-2.33
   (package
-    (inherit glibc-2.35)
+    (inherit glibc)
     (name "glibc")
     (version "2.33")
     (source (origin