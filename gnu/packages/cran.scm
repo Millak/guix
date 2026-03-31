bc68c3ab949ee1b0c1db3bf35b94121a5f283ed1
Ricardo Wurmus <rekado@elephly.net>
2026-03-31 18:43:46 +0200

    gnu: r-mumin: Update to 1.48.19.
    * gnu/packages/cran.scm (r-mumin): Update to 1.48.19.
    
    Change-Id: I909e9e0b6ceb32458aad8de6e6cda0c239091f37

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -12721,13 +12721,13 @@ estimates.")
 (define-public r-mumin
   (package
     (name "r-mumin")
-    (version "1.48.11")
+    (version "1.48.19")
     (source (origin
               (method url-fetch)
               (uri (cran-uri "MuMIn" version))
               (sha256
                (base32
-                "1rzsngmnyfscr42rkb9cm85s3xisgjrax2swcab7qzzkjh355wy1"))))
+                "0jpdr4zrrdmyrhrirf9d6k9dwkx49kjlb0wmkfmj5g26cln367qh"))))
     (properties `((upstream-name . "MuMIn")))
     (build-system r-build-system)
     (propagated-inputs (list r-insight r-matrix r-nlme))
