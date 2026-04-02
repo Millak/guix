7541cb605c2133b52854e575b97a91d662fd399a
Ludovic Courtès <ludo@gnu.org>
2026-04-02 21:47:35 +0200

    gnu: libcxi: Update to 14.0.0.
    * gnu/packages/linux.scm (libcxi): Update to 14.0.0.
    
    Change-Id: I21364dd172527aec482c232668524c4c45eb56bc
    Signed-off-by: Ludovic Courtès <ludo@gnu.org>

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -10442,7 +10442,7 @@ Currently the Linux driver itself is missing from this package.")
 (define-public libcxi
   (package
     (name "libcxi")
-    (version "13.0.0")
+    (version "14.0.0")
     (home-page "https://github.com/HewlettPackard/shs-libcxi")
     (source
      (origin
@@ -10451,7 +10451,7 @@ Currently the Linux driver itself is missing from this package.")
                            (commit (string-append "release/shs-" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "1m8mds1h8plhfn2ja9br31xaiy82ky69685cp1rznanl18inaiff"))))
+        (base32 "03czzz6h9jbmfikd85mx93bn84icnia08h69afw40flmpqidp5kh"))))
     (build-system gnu-build-system)
     (arguments
      (list #:configure-flags
