129cf96915591b381ccd0a4c9b8813da2491eced
Wilko Meyer <w@wmeyer.eu>
2026-04-04 16:07:48 +0200

    gnu: linux-libre 6.18: Update to 6.18.21.
    * gnu/packages/linux.scm (linux-libre-6.18-version): Update to 6.18.21.
    (linux-libre-6.18-pristine-source): Update hash.
    
    Change-Id: If65363e23cff035a17c05872f56bdafe746f0038
    Signed-off-by: Vagrant Cascadian <vagrant@debian.org>

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -546,7 +546,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
                             (%upstream-linux-source version hash)
                             deblob-scripts-6.19)))
 
-(define-public linux-libre-6.18-version "6.18.20")
+(define-public linux-libre-6.18-version "6.18.21")
 (define-public linux-libre-6.18-gnu-revision "gnu")
 (define deblob-scripts-6.18
   (linux-libre-deblob-scripts
@@ -556,7 +556,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
    (base32 "0qlih8w8wg4v5cwbmd34rbhb6mjgbx69i9rx3s2vxw7h42q9l0ai")))
 (define-public linux-libre-6.18-pristine-source
   (let ((version linux-libre-6.18-version)
-        (hash (base32 "0lrm76rdlr92kjq3g410qdff9v49mpdf400lmsh7hq74k2ymlyl3")))
+        (hash (base32 "0ks735y6jq4yy3jaicjsj4dn4n3kk2skf9dqh9dyifipn57j2f0w")))
    (make-linux-libre-source version
                             (%upstream-linux-source version hash)
                             deblob-scripts-6.18)))
