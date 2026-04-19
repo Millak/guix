45cf15b751d45e4d780c7a636f426ea427c9b9f1
Johannes Christ <jc@jchri.st>
2026-04-19 16:25:43 +0200

    gnu: forgejo-cli: Update to 0.5.0.
    * gnu/packages/rust-apps.scm (forgejo-cli): Update to 0.5.0.
    * gnu/packages/rust-crates.scm (lookup-crate-inputs): Update forgejo-cli
    crates.
    
    Change-Id: Ib6c49565cf6b5e4db559b5f2772a0420a72e121c
    Signed-off-by: Efraim Flashner <efraim@flashner.co.il>

--- gnu/packages/rust-apps.scm
+++ gnu/packages/rust-apps.scm
@@ -1399,7 +1399,7 @@ defaults for 80% of the use cases.")
 (define-public forgejo-cli
   (package
     (name "forgejo-cli")
-    (version "0.4.1")
+    (version "0.5.0")
     (source
      (origin
        (method git-fetch)
@@ -1408,7 +1408,7 @@ defaults for 80% of the use cases.")
               (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "1279cvdbgvl8yaqrxm8mg1swqn623xdfqjx13s9clrxp923zjrmm"))))
+        (base32 "1x3v56my22lckrpkcnpmf90vrn8m45ysdn0k12dfssldr8cjxapa"))))
     (build-system cargo-build-system)
     (arguments
      (list
