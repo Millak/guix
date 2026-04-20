Millak/guix

commit 650e69784d80bcbe21efb6ceed5c41af817840d1

doc, gnu: Update URL of several packages formerly at notabug.org.
The URL change for these 5 packages was announced at
<https://lists.gnu.org/archive/html/guile-user/2026-03/msg00000.html>.

* doc/contributing.texi (Requirements): Update URLs.
* gnu/packages/gnupg.scm (guile-gcrypt)[home-page]: Update.
* gnu/packages/guile.scm (guile-sqlite3)[home-page]: Update.
(guile-zlib)[source]: Update URL and hash.
[home-page]: Update.
(guile-lzlib)[home-page]: Update.
(guile-zstd)[home-page]: Update.
* m4/guix.m4 (GUIX_CHECK_GUILE_SQLITE3): Update commit URL.

Merges guix/guix!7069

Change-Id: Ic587d3e2dcd931a44c007d9cdcd0b20e771be33b
Signed-off-by: Cayetano Santos <csantosb@inventati.org>

gnu/packages/guile.scm
@@ -829,7 +829,7 @@ Guile's foreign function interface.")
   (package
     (name "guile-sqlite3")
     (version "0.1.3")
-    (home-page "https://notabug.org/guile-sqlite3/guile-sqlite3.git")
+    (home-page "https://codeberg.org/guile-sqlite3/guile-sqlite3.git")
     (source (origin
               (method git-fetch)
               (uri (git-reference
@@ -966,13 +966,13 @@ manipulate repositories of the Git version control system.")
        ;; in the same method.
        (method url-fetch)
        (uri
-        (string-append "https://notabug.org/guile-zlib/guile-zlib/archive/v"
+        (string-append "https://codeberg.org/guile-compression/guile-zlib/archive/v"
                        version ".tar.gz"))
        (file-name (string-append name "-" version ".tar.gz"))
        (sha256
         ;; content nar-sha256: 0rwjlqr1hl2vczs16xsihw8pyj6s70p1yv9ky0sawhm6g30639k9
         (base32
-         "1h9q7rw8bh5mwa8qjik0pqwcr8v9hnk7xnvwcbsg6yfa695wlv3g"))))
+         "04p9lb3bq5y0k358s8agpksx9x68vzx330cb8jkn4qp3qj7cmnx2"))))
     (build-system gnu-build-system)
     (native-inputs (list autoconf automake pkg-config guile-3.0))
     (inputs (list guile-3.0 zlib))
@@ -981,7 +981,7 @@ manipulate repositories of the Git version control system.")
      "This package provides Guile bindings for zlib, a lossless
 data-compression library.  The bindings are written in pure Scheme by using
 Guile's foreign function interface.")
-    (home-page "https://notabug.org/guile-zlib/guile-zlib")
+    (home-page "https://codeberg.org/guile-compression/guile-zlib")
     (license license:gpl3+)))
 
 (define-public guile2.2-zlib
@@ -991,7 +991,7 @@ Guile's foreign function interface.")
   (package
     (name "guile-lzlib")
     (version "0.3.0")
-    (home-page "https://notabug.org/guile-lzlib/guile-lzlib")
+    (home-page "https://codeberg.org/guile-compression/guile-lzlib")
     (source
      (origin
        ;; Note: Until "builtin:git-download" can be taken for granted, this
@@ -1027,7 +1027,7 @@ pure Scheme by using Guile's foreign function interface.")
   (package
     (name "guile-zstd")
     (version "0.1.1")
-    (home-page "https://notabug.org/guile-zstd/guile-zstd")
+    (home-page "https://codeberg.org/guile-compression/guile-zstd")
     (source (origin
               (method git-fetch)
               (uri (git-reference (url home-page)

doc/contributing.texi
@@ -67,7 +67,7 @@ GNU Guix depends on the following packages:
 @itemize
 @item @url{https://gnu.org/software/guile/, GNU Guile}, version 3.0.x,
 version 3.0.9 or later;
-@item @url{https://notabug.org/cwebber/guile-gcrypt, Guile-Gcrypt}, version
+@item @url{https://codeberg.org/guile-gcrypt/guile-gcrypt, Guile-Gcrypt}, version
 0.1.0 or later;
 @item
 @uref{https://gitlab.com/gnutls/guile/, Guile-GnuTLS} (@pxref{Guile
@@ -76,11 +76,11 @@ gnutls-guile, GnuTLS-Guile})@footnote{The Guile bindings to
 @uref{https://gnutls.org/, GnuTLS} were distributed as part of GnuTLS
 until version 3.7.8 included.};
 @item
-@uref{https://notabug.org/guile-sqlite3/guile-sqlite3, Guile-SQLite3}, version 0.1.0
+@uref{https://codeberg.org/guile-sqlite3/guile-sqlite3, Guile-SQLite3}, version 0.1.0
 or later;
-@item @uref{https://notabug.org/guile-zlib/guile-zlib, Guile-zlib},
+@item @uref{https://codeberg.org/guile-compression/guile-zlib, Guile-zlib},
 version 0.1.0 or later;
-@item @uref{https://notabug.org/guile-lzlib/guile-lzlib, Guile-lzlib};
+@item @uref{https://codeberg.org/guile-compression/guile-lzlib, Guile-lzlib};
 @item @uref{https://www.nongnu.org/guile-avahi/, Guile-Avahi};
 @item
 @uref{https://gitlab.com/guile-git/guile-git, Guile-Git}, version 0.10.0
@@ -102,7 +102,7 @@ Support for build offloading (@pxref{Daemon Offload Setup}) and
 version 0.13.0 or later.
 
 @item
-@uref{https://notabug.org/guile-zstd/guile-zstd, Guile-zstd}, for zstd
+@uref{https://codeberg.org/guile-compression/guile-zstd, Guile-zstd}, for zstd
 compression and decompression in @command{guix publish} and for
 substitutes (@pxref{Invoking guix publish}).
 

gnu/packages/gnupg.scm
@@ -527,7 +527,7 @@ gpgpme starting with version 1.7.")
   (package
     (name "guile-gcrypt")
     (version "0.5.0")
-    (home-page "https://notabug.org/cwebber/guile-gcrypt")
+    (home-page "https://codeberg.org/guile-gcrypt/guile-gcrypt")
     (source (origin
               (method git-fetch)
               (uri (git-reference

m4/guix.m4
@@ -157,7 +157,7 @@ dnl Check whether a recent-enough Guile-Sqlite3 is available.
 AC_DEFUN([GUIX_CHECK_GUILE_SQLITE3], [
   dnl Check whether 'sqlite-bind-arguments' is available.  It was introduced
   dnl in February 2018:
-  dnl <https://notabug.org/guile-sqlite3/guile-sqlite3/commit/1cd1dec96a9999db48c0ff45bab907efc637247f>.
+  dnl <https://codeberg.org/guile-sqlite3/guile-sqlite3/commit/1cd1dec96a9999db48c0ff45bab907efc637247f>.
   AC_CACHE_CHECK([whether Guile-Sqlite3 is available and recent enough],
     [guix_cv_have_recent_guile_sqlite3],
     [GUILE_CHECK([retval],

commit 735e13d60ea755792bb189120f015247db01f56c

gnu: guile-lzlib: Fix hash.
Fixes a regression introduced in 650e69784d80bcbe21efb6ceed5c41af817840d1.

* gnu/packages/guile.scm (guile-lzlib)[source]: Update sha256.

Change-Id: Iccb8acc8d1c92d89c4e3b03dbf500e21be604692
Signed-off-by: Ludovic Courtès <ludo@gnu.org>
Fixes: guix/guix#7573
Merges: #8020

gnu/packages/guile.scm
@@ -1004,7 +1004,7 @@ Guile's foreign function interface.")
        (file-name (string-append "guile-lzlib-" version ".tar.gz"))
        (sha256
         (base32
-         "1whgmwkr1v8m63p4aaqn8blwl9vcrswwhbfv4bm0aghl5a6rryd7"))
+         "1v1pfqp6hwl0rivs7swhqnfgznxlfnws9ldmn6avnhd10filfa3a"))
        (patches (search-patches "guile-lzlib-gcc-14.patch"
                                 "guile-lzlib-hurd64.patch"))))
     (build-system gnu-build-system)