d6134742e2fb1e6b996d716cb471e5dfa64c6a92
Ricardo Wurmus <rekado@elephly.net>
2026-04-20 16:07:57 +0200

    gnu: r-pdftools: Update to 3.8.0.
    * gnu/packages/cran.scm (r-pdftools): Update to 3.8.0.
    
    Change-Id: Ida05e42c4d93be13875f448d29096d37a72d40fd

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -56323,14 +56323,14 @@ parallel programming style, which is intended for batch parallel execution.")
 (define-public r-pdftools
   (package
     (name "r-pdftools")
-    (version "3.7.0")
+    (version "3.8.0")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "pdftools" version))
        (sha256
         (base32
-         "03bfrzwi456id1fxzxgbn2a2xgb1b11p4l434j1yjqvc11hfzxq7"))))
+         "0sa0fyacf2v353yp8yr0l9fg5p1rnbrnrcivi6nw2ndl40vcf8vp"))))
     (properties
      '((upstream-name . "pdftools")
        (updater-extra-inputs . ("poppler"))))
