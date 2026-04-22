8168de0e083dc38a43627ad492df3a0f06db47c1
Noé Lopez <noelopez@free.fr>
2026-04-22 18:50:01 +0200

    gnu: gjs: Add debug output.
    * gnu/packages/gnome.scm (gjs)
    [outputs]: Add debug output.
    
    Change-Id: Ieba049f25d80323ebe6fe3d7b0dee64b511d599b
    Signed-off-by: Noé Lopez <noelopez@free.fr>

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -7973,6 +7973,7 @@ configuration program to choose applications starting on login.")
             (delete-all-but "subprojects"
                             "gobject-introspection-tests")))))
     (build-system meson-build-system)
+    (outputs '("out" "debug"))
     (arguments
      (list
       #:modules '((guix build meson-build-system)
