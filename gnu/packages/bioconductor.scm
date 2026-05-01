324f4bd9c361e5fd251622a159256a43db2fb16d
Ricardo Wurmus <rekado@elephly.net>
2026-05-01 22:13:29 +0200

    gnu: r-celldex: Update to 1.21.0.
    * gnu/packages/bioconductor.scm (r-celldex): Update to 1.21.0.
    
    Change-Id: Icaf9f2c25db279fd11c2789914a67da0a393ce3f

--- gnu/packages/bioconductor.scm
+++ gnu/packages/bioconductor.scm
@@ -2265,13 +2265,13 @@ cancer study published in Wang et al. 2005 and Minn et al 2007.")
 (define-public r-celldex
   (package
     (name "r-celldex")
-    (version "1.20.0")
+    (version "1.21.0")
     (source
      (origin
        (method url-fetch)
        (uri (bioconductor-uri "celldex" version 'experiment))
        (sha256
-        (base32 "0bpyf7rgxdy6an1r3w1qsnklhlhfvsgknshk0dxk1r54p32jw6bz"))))
+        (base32 "1qf7aw4a4g89gnnv01izyfa5km7pgknhmcg9x4530xjng4v00av4"))))
     (properties `((upstream-name . "celldex")))
     (build-system r-build-system)
     ;; Tests access the internet via r-gypsum.
