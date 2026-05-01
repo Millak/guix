bb71736c6d64734eece5c6227f1eaf7f9ff4f622
Nicolas Goaziou <mail@nicolasgoaziou.fr>
2026-05-01 22:18:49 +0200

    gnu: emacs-treesit-auto: Update to 1.0.9.
    * gnu/packages/emacs-xyz.scm (emacs-treesit-auto): Update to 1.0.9.
    
    Change-Id: I1732a618a79f06efcafdb7014655280eabb053c1

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -30025,28 +30025,27 @@ subs, classes, etc.), and REPL interaction.")
       (license license:gpl3+))))
 
 (define-public emacs-treesit-auto
-  (let ((commit "016bd286a1ba4628f833a626f8b9d497882ecdf3")) ; version bump
-    (package
-      (name "emacs-treesit-auto")
-      (version "1.0.7")
-      (source
-       (origin
-         (method git-fetch)
-         (uri (git-reference
-               (url "https://github.com/renzmann/treesit-auto")
-               (commit commit)))
-         (file-name (git-file-name name version))
-         (sha256
-          (base32 "03bvam7cpxqp4idhd235n76qdqhsbgw7m2lphy8qqwslbmcq23m4"))))
-      (build-system emacs-build-system)
-      (arguments '(#:tests? #f)) ; no tests
-      (home-page "https://github.com/renzmann/treesit-auto")
-      (synopsis
-       "Automatically install and use tree-sitter major modes in Emacs 29+")
-      (description
-       "Install a tree-sitter major mode automatically.  If the tree-sitter
+  (package
+    (name "emacs-treesit-auto")
+    (version "1.0.9")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+              (url "https://github.com/renzmann/treesit-auto")
+              (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "1qw6y3089h1n3zjr4bja6dg3gaxrmwfljxk22k31afj9bccibiqb"))))
+    (build-system emacs-build-system)
+    (arguments '(#:tests? #f)) ; no tests
+    (home-page "https://github.com/renzmann/treesit-auto")
+    (synopsis
+     "Automatically install and use tree-sitter major modes in Emacs 29+")
+    (description
+     "Install a tree-sitter major mode automatically.  If the tree-sitter
 version can't be used, fall back to the original major mode.")
-      (license license:gpl3))))
+    (license license:gpl3)))
 
 (define-public emacs-md4rd
   (package
