Millak/guix

commit 10ea4f874e5f1ff57df840a60195a073b1b0b898

gnu: ungoogled-chromium: Update to 147.0.7727.55.
Fixes various CVEs, too many to list.

* gnu/packages/chromium.scm (%preserved-third-party-files): Add
  base/third_party/nspr,
  buildtools/third_party/libc++,
  buildtools/third_party/libc++abi,
  third_party/catapult/third_party/beautifulsoup4-4.9.3,
  third_party/catapult/third_party/html5lib-1.1,
  third_party/catapult/third_party/typ,
  third_party/dawn/third_party/renderdoc,
  third_party/dawn/third_party/webgpu-headers,
  third_party/devscripts,
  third_party/devtools-frontend/src/front_end/third_party/csp_evaluator,
  third_party/devtools-frontend/src/front_end/third_party/legacy-javascript,
  third_party/devtools-frontend/src/front_end/third_party/source-map-scopes-codec,
  third_party/federated_compute/chromium/fcp/confidentialcompute,
  third_party/freetype,
  third_party/fxdiv,
  third_party/hyphenation-patterns,
  third_party/icu,
  third_party/libc++,
  third_party/libpfm4,
  third_party/libpng,
  third_party/libx11,
  third_party/libxcb-keysyms,
  third_party/llvm-libc,
  third_party/neon_2_sse,
  third_party/opus,
  third_party/pdfium/third_party/lcms,
  third_party/pdfium/third_party/libopenjpeg,
  third_party/perfetto/protos/third_party/pprof,
  third_party/perfetto/protos/third_party/primes,
  third_party/perfetto/protos/third_party/simpleperf,
  third_party/pyyaml,
  third_party/sentencepiece,
  third_party/sentencepiece/src/third_party/darts_clone,
  third_party/six and
  third_party/skia/include/third_party/vulkan.
Remove:
  net/third_party/nss,
  third_party/compiler-rt/src/lib,
  third_party/libaom/source/libaom/third_party/SVT-AV1,
  third_party/skia/third_party/vulkanmemoryallocator and
  third_party/webrtc/rtc_base/third_party/sigslot.

(%chromium-version): Update to 147.0.7727.55.
(%ungoogled-origin, %debian-origin): Update hashes.
(%debian-patches): Add debianization/safe-libcxx.patch,
  disable/enterprise-tests.patch,
  disable/rustc-allow-features.patch,
  fixes/bytemuck.patch,

gnu/packages/patches/ungoogled-chromium-custom-compiler.patch
@@ -1,0 +1,299 @@
+From 7a372a6914e9d9d6f0c914a1ca8cdff592c40593 Mon Sep 17 00:00:00 2001
+From: Matt Jolly <kangie@gentoo.org>
+Date: Thu, 12 Mar 2026 11:57:11 +1000
+Subject: [PATCH] Update compiler patch for M147 beta
+
+Signed-off-by: Matt Jolly <kangie@gentoo.org>
+--- a/build/config/compiler/BUILD.gn
++++ b/build/config/compiler/BUILD.gn
+@@ -367,9 +367,6 @@ config("compiler") {
+ 
+   configs += [
+     # See the definitions below.
+-    ":clang_revision",
+-    ":rustc_revision",
+-    ":compiler_cpu_abi",
+     ":compiler_codegen",
+     ":compiler_deterministic",
+     ":clang_warning_suppression",
+@@ -1690,43 +1687,6 @@ config("compiler_deterministic") {
+     }
+   }
+ 
+-  # Makes builds independent of absolute file path.
+-  if (is_clang && strip_absolute_paths_from_debug_symbols) {
+-    # If debug option is given, clang includes $cwd in debug info by default.
+-    # For such build, this flag generates reproducible obj files even we use
+-    # different build directory like "out/feature_a" and "out/feature_b" if
+-    # we build same files with same compile flag.
+-    # Other paths are already given in relative, no need to normalize them.
+-
+-    # -ffile-compilation-dir is an alias for both -fdebug-compilation-dir=
+-    # and -fcoverage-compilation-dir=.
+-    cflags += [ "-ffile-compilation-dir=." ]
+-
+-    # Convert absolute paths to relative paths. Expands to, for example:
+-    #   -file-prefix-map /path/to/chromium/src=../..
+-    swiftflags += [
+-      "-file-prefix-map",
+-      rebase_path("//.", "") + "=" + rebase_path("//.", root_build_dir),
+-    ]
+-    if (!is_win) {
+-      # We don't use clang -cc1as on Windows (yet? https://crbug.com/762167)
+-      asmflags = [ "-Wa,-fdebug-compilation-dir,." ]
+-    }
+-
+-    if (is_win && use_lld) {
+-      if (symbol_level == 2 || (is_clang && using_sanitizer)) {
+-        # Absolutize source file paths for PDB. Pass the real build directory
+-        # if the pdb contains source-level debug information and if linker
+-        # reproducibility is not critical.
+-        ldflags += [ "/PDBSourcePath:" + rebase_path(root_build_dir) ]
+-      } else {
+-        # Use a fake fixed base directory for paths in the pdb to make the pdb
+-        # output fully deterministic and independent of the build directory.
+-        ldflags += [ "/PDBSourcePath:o:\fake\prefix" ]
+-      }
+-    }
+-  }
+-
+   # Tells the compiler not to use absolute paths when passing the default
+   # paths to the tools it invokes. We don't want this because we don't
+   # really need it and it can mess up the RBE cache entries.
+@@ -1759,88 +1719,6 @@ config("compiler_deterministic") {
+   }
+ }
+ 
+-config("clang_revision") {
+-  if (is_clang && clang_base_path == default_clang_base_path &&
+-      current_os != "zos") {
+-    _perform_consistency_checks = current_toolchain == default_toolchain
+-    if (llvm_force_head_revision) {
+-      _head_revision_stamp_path = "//third_party/llvm-build/force_head_revision"
+-      _head_revision = ""
+-      if (path_exists(_head_revision_stamp_path)) {
+-        _head_revision = read_file(_head_revision_stamp_path, "trim string")
+-      }
+-      assert(
+-          _head_revision != "",
+-          "llvm_force_head_revision=true, but no locally built version was detected.")
+-      _clang_revision = _head_revision
+-    } else {
+-      _clang_revision = read_file(
+-              "//third_party/llvm-build/Release+Asserts/cr_build_revision",
+-              "trim string")
+-
+-      # Ensure that the synced clang version matches what's in git.
+-      if (_perform_consistency_checks) {
+-        # Parse the clang version from the Python script.
+-        _clang_version_lines = filter_include(
+-                read_file("//tools/clang/scripts/update.py", "list lines"),
+-                [ "CLANG_*REVISION = *" ])
+-        _py_revision =
+-            string_replace(_clang_version_lines[0], "CLANG_REVISION = '", "")
+-        _py_revision = string_replace(_py_revision, "'", "")
+-        _py_subrevision =
+-            string_replace(_clang_version_lines[1], "CLANG_SUB_REVISION = ", "")
+-        _expected_clang_revision = "$_py_revision-$_py_subrevision"
+-
+-        # TODO(agrieve): Change filter_include to _clang_revision ==
+-        # _expected_clang_revision once angle & webrtc use a GCS DEPS entry
+-        # rather than a update.py runhook.
+-        assert(
+-            filter_include([ _clang_revision ],
+-                           [
+-                             _expected_clang_revision,
+-                             "$_expected_clang_revision,*",
+-                           ]) != [],
+-            "clang_revision=\"$_clang_revision\" but update.py expected \"$_expected_clang_revision\". Did you forget to gclient sync?")
+-      }
+-    }
+-
+-    if (_perform_consistency_checks) {
+-      # Ensure that the revision matches the version major expected by GN.
+-      _versions_match = filter_include([ _clang_revision ],
+-                                       [ "llvmorg-$clang_version-*" ]) != []
+-      assert(
+-          _versions_match,
+-          "clang_revision=\"$_clang_revision\" but clang_version=\"$clang_version\". clang_version in build/toolchain/toolchain.gni is likely outdated.")
+-    }
+-
+-    if (toolchain_has_rust && _perform_consistency_checks &&
+-        !rust_force_head_revision) {
+-      # Ensure that the synced rust version matches what's in git.
+-      _rust_revision_lines =
+-          filter_include(read_file("//tools/rust/update_rust.py", "list lines"),
+-                         [ "RUST_*REVISION = *" ])
+-      _py_revision =
+-          string_replace(_rust_revision_lines[0], "RUST_REVISION = '", "")
+-      _py_revision = string_replace(_py_revision, "'", "")
+-      _py_subrevision =
+-          string_replace(_rust_revision_lines[1], "RUST_SUB_REVISION = ", "")
+-      _expected_rust_revision = "$_py_revision-$_py_subrevision"
+-
+-      # Ensure the rust version matches the clang version.
+-      assert(
+-          filter_include([ rustc_revision ],
+-                         [ "*-$_expected_rust_revision-*" ]) != [],
+-          "rustc_revision=\"$rustc_revision\" but update_rust.py expected \"$_expected_rust_revision\". Run \"gclient sync\"?")
+-    }
+-
+-    # This is here so that all files get recompiled after a clang roll and
+-    # when turning clang on or off. (defines are passed via the command line,
+-    # and build system rebuild things when their commandline changes). Nothing
+-    # should ever read this define.
+-    defines = [ "CR_CLANG_REVISION=\"$_clang_revision\"" ]
+-  }
+-}
+-
+ # Controls the usage of a warning suppression mapping (WSM) file to suppress
+ # warnings based on the path of the file they come from. It's controlled by the
+ # `clang_warning_suppression_file` gn argument , which points to a text file
+@@ -1896,18 +1774,6 @@ config("sanitize_c_array_bounds") {
+   }
+ }
+ 
+-config("rustc_revision") {
+-  if (rustc_revision != "") {
+-    # Similar to the `clang_revision` config, this is here so that all `.rs`
+-    # sources get recompiled after a rustc roll. Nothing should ever read this
+-    # cfg. This will not be set if a custom toolchain is used.
+-    rustflags = [
+-      "--cfg",
+-      "cr_rustc_revision=\"$rustc_revision\"",
+-    ]
+-  }
+-}
+-
+ config("compiler_arm_fpu") {
+   if (current_cpu == "arm" && !is_ios) {
+     cflags = [ "-mfpu=neon" ]
+@@ -2386,11 +2252,7 @@ config("chromium_code") {
+       defines = [ "_HAS_NODISCARD" ]
+     }
+   } else {
+-    cflags = [ "-Wall" ]
+-    if (is_clang) {
+-      # Enable extra warnings for chromium_code when we control the compiler.
+-      cflags += [ "-Wextra" ]
+-    }
++    cflags = []
+ 
+     # In Chromium code, we define __STDC_foo_MACROS in order to get the
+     # C99 macros on Mac and Linux.
+@@ -2399,32 +2261,6 @@ config("chromium_code") {
+       "__STDC_FORMAT_MACROS",
+     ]
+ 
+-    if (!is_debug && !using_sanitizer && current_cpu != "s390x" &&
+-        current_cpu != "ppc64" && current_cpu != "mips" &&
+-        current_cpu != "mips64" && current_cpu != "riscv64" &&
+-        current_cpu != "loong64") {
+-      # Non-chromium code is not guaranteed to compile cleanly with
+-      # _FORTIFY_SOURCE. Also, fortified build may fail when optimizations are
+-      # disabled, so only do that for Release build.
+-      fortify_level = "2"
+-
+-      # ChromeOS's toolchain supports a high-quality _FORTIFY_SOURCE=3
+-      # implementation with a few custom glibc patches. Use that if it's
+-      # available.
+-      if (is_chromeos_device) {
+-        fortify_level = "3"
+-      }
+-
+-      # _FORTIFY_SOURCE=3 is available in glibc 2.34+. Enable it when building
+-      # with the sysroot where support is guaranteed. Clang is also required since
+-      # the system gcc is often patched to add _FORTIFY_SOURCE=2 which would conflict
+-      # with our definition.
+-      if (use_sysroot && is_clang) {
+-        fortify_level = "3"
+-      }
+-      defines += [ "_FORTIFY_SOURCE=" + fortify_level ]
+-    }
+-
+     if (is_apple) {
+       cflags_objc = [ "-Wimplicit-retain-self" ]
+       cflags_objcc = [ "-Wimplicit-retain-self" ]
+@@ -2848,7 +2684,8 @@ config("default_stack_frames") {
+ # [0]: https://pinpoint-dot-chromeperf.appspot.com/job/147634a8be0000
+ # [1]: https://pinpoint-dot-chromeperf.appspot.com/job/132bc772be0000
+ # [2]: https://crrev.com/c/5447532
+-config("optimize") {
++config("optimize") { }
++config("xoptimize") {
+   if (is_win) {
+     # clang-cl's /O2 corresponds to clang's -O3, and really want -O2 for
+     # consistency with the other platforms.
+@@ -2897,7 +2734,8 @@ config("optimize") {
+ }
+ 
+ # Turn off optimizations.
+-config("no_optimize") {
++config("no_optimize") { }
++config("xno_optimize") {
+   if (is_win) {
+     cflags = [
+       "/Od",  # Disable optimization.
+@@ -2932,7 +2770,8 @@ config("no_optimize") {
+ # Turns up the optimization level. Used to explicitly enable -O2 instead of
+ # -Os for select targets on platforms that use optimize_for_size. No-op
+ # elsewhere.
+-config("optimize_max") {
++config("optimize_max") { }
++config("xoptimize_max") {
+   ldflags = common_optimize_on_ldflags
+   if (is_win) {
+     # Favor speed over size, /O2 must be before the common flags.
+@@ -2953,7 +2792,8 @@ config("optimize_max") {
+ #
+ # TODO(crbug.com/41259697) - rework how all of these configs are related
+ # so that we don't need this disclaimer.
+-config("optimize_speed") {
++config("optimize_speed") { }
++config("xoptimize_speed") {
+   ldflags = common_optimize_on_ldflags
+   if (is_win) {
+     # Favor speed over size, /O2 must be before the common flags.
+@@ -2970,7 +2810,8 @@ config("optimize_speed") {
+   rustflags = [ "-Copt-level=3" ]
+ }
+ 
+-config("optimize_fuzzing") {
++config("optimize_fuzzing") { }
++config("xoptimize_fuzzing") {
+   cflags = [ "-O1" ] + common_optimize_on_cflags
+   rustflags = [ "-Copt-level=1" ]
+   ldflags = common_optimize_on_ldflags
+@@ -3115,7 +2956,8 @@ config("split_dwarf") {
+ }
+ 
+ # Full symbols.
+-config("symbols") {
++config("symbols") { }
++config("xsymbols") {
+   rustflags = []
+   configs = []
+   if (is_win) {
+@@ -3275,7 +3117,8 @@ config("symbols") {
+ # Minimal symbols.
+ # This config guarantees to hold symbol for stack trace which are shown to user
+ # when crash happens in unittests running on buildbot.
+-config("minimal_symbols") {
++config("minimal_symbols") { }
++config("xminimal_symbols") {
+   configs = []
+   rustflags = []
+   if (is_win) {
+@@ -3355,7 +3198,8 @@ config("minimal_symbols") {
+ # This configuration contains function names only. That is, the compiler is
+ # told to not generate debug information and the linker then just puts function
+ # names in the final debug information.
+-config("no_symbols") {
++config("no_symbols") { }
++config("xno_symbols") {
+   if (is_win) {
+     ldflags = [ "/DEBUG" ]
+ 
+-- 
+2.52.0
+

gnu/local.mk
@@ -2522,10 +2522,11 @@ dist_patch_DATA =						\
   %D%/packages/patches/ultrastar-deluxe-no-freesans.patch		\
   %D%/packages/patches/unity-test-set-subdir-correctly.patch		\
   %D%/packages/patches/unity-test-support-int64.patch		\
+  %D%/packages/patches/ungoogled-chromium-custom-compiler.patch 	\
+  %D%/packages/patches/ungoogled-chromium-empty-parsed-rustc-args.patch	\
   %D%/packages/patches/ungoogled-chromium-extension-search-path.patch	\
   %D%/packages/patches/ungoogled-chromium-RUNPATH.patch		\
   %D%/packages/patches/ungoogled-chromium-system-nspr.patch	\
-  %D%/packages/patches/ungoogled-chromium-unbundle-icu-target.patch     \
   %D%/packages/patches/unknown-horizons-python-3.8-distro.patch	\
   %D%/packages/patches/unknown-horizons-python-3.9.patch	\
   %D%/packages/patches/unknown-horizons-python-3.10.patch	\

gnu/packages/patches/ungoogled-chromium-empty-parsed-rustc-args.patch
@@ -1,0 +1,46 @@
+From da37bd781c0371e02ffc4877c3ac1b5f2020c207 Mon Sep 17 00:00:00 2001
+From: Matt Jolly <kangie@gentoo.org>
+Date: Tue, 17 Feb 2026 15:45:01 +1000
+Subject: [PATCH] Handle case where `parsed_rustc_args.cfg` is empty
+
+This may occur when (for example) using the unbundle toolchain
+
+Signed-off-by: Matt Jolly <kangie@gentoo.org>
+--- a/third_party/rust/cxx/chromium_integration/run_cxxbridge.py
++++ b/third_party/rust/cxx/chromium_integration/run_cxxbridge.py
+@@ -44,18 +44,20 @@ def calculate_cxxbridge_args(unrecognized_script_args, rustc_print_cfg_path):
+   parser.add_argument("--cfg", action="append"),
+   parsed_rustc_args, _other_args = parser.parse_known_args(
+       args=unrecognized_script_args[:rustflags_separator])
+-  for cfg_arg in parsed_rustc_args.cfg:
+-    if "=" not in cfg_arg:  # `buildflag_header.gni` only supports bool flags.
+-      # TODO(https://crbug.com/436606652): Stop using `_BUILDFLAG_NOT_SET_`
+-      # prefix and instead use `--check-cfg` in `run_cxxbridge.py` to
+-      # detect buildflag names.
+-      FALSE_PREFIX = "_BUILDFLAG_NOT_SET_"
+-      if cfg_arg.startswith(FALSE_PREFIX):
+-        flagname = cfg_arg[len(FALSE_PREFIX):]
+-        cxxbridge_args.append(f"--cfg={flagname}=false")
+-      else:
+-        flagname = cfg_arg
+-        cxxbridge_args.append(f"--cfg={flagname}=true")
++  # Unbundled toolchain may not have any `--cfg` flags
++  if parsed_rustc_args.cfg:
++    for cfg_arg in parsed_rustc_args.cfg:
++      if "=" not in cfg_arg:  # `buildflag_header.gni` only supports bool flags.
++        # TODO(https://crbug.com/436606652): Stop using `_BUILDFLAG_NOT_SET_`
++        # prefix and instead use `--check-cfg` in `run_cxxbridge.py` to
++        # detect buildflag names.
++        FALSE_PREFIX = "_BUILDFLAG_NOT_SET_"
++        if cfg_arg.startswith(FALSE_PREFIX):
++          flagname = cfg_arg[len(FALSE_PREFIX):]
++          cxxbridge_args.append(f"--cfg={flagname}=false")
++        else:
++          flagname = cfg_arg
++          cxxbridge_args.append(f"--cfg={flagname}=true")
+ 
+   with open(rustc_print_cfg_path, 'r') as file:
+     for line in file:
+-- 
+2.52.0
+

gnu/packages/patches/ungoogled-chromium-unbundle-icu-target.patch
@@ -1,21 +1,0 @@
-From a665875b4013eed997bd042326a038e24f3296e7 Mon Sep 17 00:00:00 2001
-From: Matt Jolly <kangie@gentoo.org>
-Date: Thu, 10 Oct 2024 14:36:51 +1000
-Subject: [PATCH] unbundle: add missing icu target
-
---- a/build/linux/unbundle/icu.gn
-+++ b/build/linux/unbundle/icu.gn
-@@ -65,6 +65,10 @@ group("icuuc_public") {
-   public_deps = [ ":icuuc" ]
- }
- 
-+group("icui18n_hidden_visibility") {
-+  public_deps = [ ":icuuc" ]
-+}
-+
- shim_headers("icui18n_shim") {
-   root_path = "source/i18n"
-   headers = [
--- 
-2.46.2
-

commit 4ec740ec181e7df0afd4c7eb2efba1727155c339

gnu: ungoogled-chromium: Fix performance regression.
Fix performance regression introduced by
10ea4f874e5f1ff57df840a60195a073b1b0b898, which added
ungoogled-chromium-custom-compiler.patch.  This patch was taken from gentoo and
fixes some build failures when using our system toolchain.  However it also
removes build optimizations which we rely on, so here we adjust it to our needs.

* gnu/packages/patches/ungoogled-chromium-custom-compiler.patch: Don't patch
the optimized configuration away.
* gnu/packages/chromium.scm (%preserved-third-party-files): Remove
base/third_party/nspr, buildtools/third_party/libc++,
buildtools/third_party/libc++abi, third_party/libc++,
third_party/llvm-libc and third_party/llvm-libc/src/shared, we don't need them.
[arguments] <#:phases> {install}: Include libvulkan.so.1 to installed libs.
Add a semicolon to @@uri_scheme substitution to avoid future surprises if this
ever moves from being the last item in list.

Fixes: guix/guix#7738
Change-Id: I331908ebf0838b7fc2d567e9c429d861e882470d
Signed-off-by: Andreas Enge <andreas@enge.fr>

gnu/packages/chromium.scm
@@ -79,12 +79,9 @@
   '("base/third_party/cityhash" ;Expat
     "base/third_party/double_conversion" ;BSD-3
     "base/third_party/icu" ;Unicode, X11-style
-    "base/third_party/nspr" ;MPL-2.0
     "base/third_party/superfasthash" ;BSD-3
     "base/third_party/symbolize" ;BSD-3
     "base/third_party/xdg_user_dirs" ;Expat
-    "buildtools/third_party/libc++" ;BSD
-    "buildtools/third_party/libc++abi" ;BSD
     "net/third_party/mozilla_security_manager" ;MPL-1.1/GPL2+/LGPL2.1+
     "net/third_party/quiche" ;BSD-3
     "net/third_party/uri_template" ;ASL2.0
@@ -222,7 +219,6 @@
     "third_party/libaom/source/libaom/third_party/fastfeat" ;BSD-3
     "third_party/libaom/source/libaom/third_party/vector" ;Expat
     "third_party/libaom/source/libaom/third_party/x86inc" ;ISC
-    "third_party/libc++" ;Expat
     "third_party/libdrm" ; Expat
     "third_party/libgav1" ;ASL2.0
     "third_party/libjingle_xmpp" ;BSD-3
@@ -248,8 +244,6 @@
     "third_party/libzip" ;BSD-3
     "third_party/lit" ;BSD-3
     "third_party/lit/v3_0" ;BSD-3
-    "third_party/llvm-libc" ;ASL2.0
-    "third_party/llvm-libc/src/shared" ;ASL2.0
     "third_party/lottie" ;Expat
     "third_party/lss" ;BSD-3
     "third_party/lzma_sdk" ;public domain
@@ -901,8 +895,9 @@
                                        "libvk_swiftshader.so"
                                        "vk_swiftshader_icd.json"
 
-                                       ;; Chromium ships its own libGL
-                                       ;; implementation called ANGLE.
+                                       ;; Chromium ships its own libGL and
+                                       ;; vulkan implementation called ANGLE.
+                                       "libvulkan.so.1"
                                        "libEGL.so" "libGLESv2.so"))
                      (locales        (string-append lib "/locales"))
                      (resources      (string-append lib "/resources"))
@@ -921,7 +916,7 @@
                   (("@@MENUNAME") "Chromium")
                   (("@@PACKAGE") "chromium")
                   (("/usr/bin/@@usr_bin_symlink_name") exe)
-                  (("@@uri_scheme") "x-scheme-handler/chromium")
+                  (("@@uri_scheme") "x-scheme-handler/chromium;")
                   (("@@extra_desktop_entries") ""))
 
                 (mkdir-p man)

gnu/packages/patches/ungoogled-chromium-custom-compiler.patch
@@ -214,85 +214,6 @@ Signed-off-by: Matt Jolly <kangie@gentoo.org>
      if (is_apple) {
        cflags_objc = [ "-Wimplicit-retain-self" ]
        cflags_objcc = [ "-Wimplicit-retain-self" ]
-@@ -2848,7 +2684,8 @@ config("default_stack_frames") {
- # [0]: https://pinpoint-dot-chromeperf.appspot.com/job/147634a8be0000
- # [1]: https://pinpoint-dot-chromeperf.appspot.com/job/132bc772be0000
- # [2]: https://crrev.com/c/5447532
--config("optimize") {
-+config("optimize") { }
-+config("xoptimize") {
-   if (is_win) {
-     # clang-cl's /O2 corresponds to clang's -O3, and really want -O2 for
-     # consistency with the other platforms.
-@@ -2897,7 +2734,8 @@ config("optimize") {
- }
- 
- # Turn off optimizations.
--config("no_optimize") {
-+config("no_optimize") { }
-+config("xno_optimize") {
-   if (is_win) {
-     cflags = [
-       "/Od",  # Disable optimization.
-@@ -2932,7 +2770,8 @@ config("no_optimize") {
- # Turns up the optimization level. Used to explicitly enable -O2 instead of
- # -Os for select targets on platforms that use optimize_for_size. No-op
- # elsewhere.
--config("optimize_max") {
-+config("optimize_max") { }
-+config("xoptimize_max") {
-   ldflags = common_optimize_on_ldflags
-   if (is_win) {
-     # Favor speed over size, /O2 must be before the common flags.
-@@ -2953,7 +2792,8 @@ config("optimize_max") {
- #
- # TODO(crbug.com/41259697) - rework how all of these configs are related
- # so that we don't need this disclaimer.
--config("optimize_speed") {
-+config("optimize_speed") { }
-+config("xoptimize_speed") {
-   ldflags = common_optimize_on_ldflags
-   if (is_win) {
-     # Favor speed over size, /O2 must be before the common flags.
-@@ -2970,7 +2810,8 @@ config("optimize_speed") {
-   rustflags = [ "-Copt-level=3" ]
- }
- 
--config("optimize_fuzzing") {
-+config("optimize_fuzzing") { }
-+config("xoptimize_fuzzing") {
-   cflags = [ "-O1" ] + common_optimize_on_cflags
-   rustflags = [ "-Copt-level=1" ]
-   ldflags = common_optimize_on_ldflags
-@@ -3115,7 +2956,8 @@ config("split_dwarf") {
- }
- 
- # Full symbols.
--config("symbols") {
-+config("symbols") { }
-+config("xsymbols") {
-   rustflags = []
-   configs = []
-   if (is_win) {
-@@ -3275,7 +3117,8 @@ config("symbols") {
- # Minimal symbols.
- # This config guarantees to hold symbol for stack trace which are shown to user
- # when crash happens in unittests running on buildbot.
--config("minimal_symbols") {
-+config("minimal_symbols") { }
-+config("xminimal_symbols") {
-   configs = []
-   rustflags = []
-   if (is_win) {
-@@ -3355,7 +3198,8 @@ config("minimal_symbols") {
- # This configuration contains function names only. That is, the compiler is
- # told to not generate debug information and the linker then just puts function
- # names in the final debug information.
--config("no_symbols") {
-+config("no_symbols") { }
-+config("xno_symbols") {
-   if (is_win) {
-     ldflags = [ "/DEBUG" ]
  
 -- 
 2.52.0