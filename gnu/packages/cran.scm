8b1627e32093c8b76e10040e80c298395ad3407d
Ricardo Wurmus <rekado@elephly.net>
2026-05-06 10:48:37 +0200

    gnu: r-spdata: Update to 2.3.5.
    * gnu/packages/cran.scm (r-spdata): Update to 2.3.5.
    
    Change-Id: I0b68fd939a1fc1ef4c76a03c398e13eca3d512ac

--- gnu/packages/cran.scm
+++ gnu/packages/cran.scm
@@ -39269,14 +39269,14 @@ univariate class intervals for mapping or other graphics purposes.")
 (define-public r-spdata
   (package
     (name "r-spdata")
-    (version "2.3.4")
+    (version "2.3.5")
     (source
      (origin
        (method url-fetch)
        (uri (cran-uri "spData" version))
        (sha256
         (base32
-         "0czsg358127c391fr84pibscvw6q8a7mrvrdf6c0rw718y73353h"))))
+         "05dhmg9pvzi1qydpmjbg3p2r29v45pplvg8c3fji1is56kckak83"))))
     (properties `((upstream-name . "spData")))
     (build-system r-build-system)
     (propagated-inputs
