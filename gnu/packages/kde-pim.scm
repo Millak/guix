7bb158613d0e4515f3c8cf9e09c40e545f44bb99
Sughosha <sughosha@disroot.org>
2026-05-08 06:08:21 +0530

    gnu: kmail: Update to 26.04.1.
    * gnu/packages/kde-pim.scm (kmail): Update to 26.04.1.
    
    Change-Id: I059b9b99a157b6c59839b647caaca6a607d3c432

--- gnu/packages/kde-pim.scm
+++ gnu/packages/kde-pim.scm
@@ -1597,14 +1597,14 @@ and retrieving certificates from LDAP servers.")
 (define-public kmail
   (package
     (name "kmail")
-    (version "25.12.3")
+    (version "26.04.1")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://kde/stable/release-service/" version
                            "/src/kmail-" version ".tar.xz"))
        (sha256
-        (base32 "1h91d39khwmr4c6380w53nf112z9nk5zrd811q82hd7a8l4rivg1"))))
+        (base32 "05x2nrq41xzbhhfn7d5pd4v88vabb96nraqlbln33w6q33yw7f55"))))
     (build-system qt-build-system)
     (native-inputs
      (list extra-cmake-modules dbus kdoctools))
