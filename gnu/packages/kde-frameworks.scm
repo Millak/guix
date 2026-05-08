af94bd662131dcdc8c64c9047655e705c7d94c93
Sughosha <sughosha@disroot.org>
2026-05-08 08:50:39 +0530

    gnu: ki18n: Update to 6.24.0.
    * gnu/packages/kde-frameworks.scm (ki18n): Update to 6.24.0.
    
    Change-Id: Iba51f77fb9421191c43b716a3d3b849a9e318c2d

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -1158,7 +1158,7 @@ other special events for a geographical region.")
 (define-public ki18n
   (package
     (name "ki18n")
-    (version "6.23.0")
+    (version "6.24.0")
     (source (origin
               (method url-fetch)
               (uri (string-append
@@ -1167,7 +1167,7 @@ other special events for a geographical region.")
                     name "-" version ".tar.xz"))
               (sha256
                (base32
-                "0bqqwdbqc8ck85pisgvp29lshfyvvd9kmjyx59rlrn2qj9far4n4"))))
+                "0mqbbic2kcg72fmgmv09d578kmx9c3cim9dv9m6whr172lfbc93l"))))
     (build-system cmake-build-system)
     (propagated-inputs
      (list gettext-minimal ;required by KF6I18nConfig.cmake
