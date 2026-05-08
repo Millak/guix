585ea774bad016a53cf56c3aa0f846c03d0a2c85
Sughosha <sughosha@disroot.org>
2026-05-08 06:08:13 +0530

    gnu: ghostwriter: Update to 26.04.1.
    * gnu/packages/kde-office.scm (ghostwriter): Update to 26.04.1.
    
    Change-Id: I4d7ad8cc7e66db0d500baa17c601d4de80e0bc35

--- gnu/packages/kde-office.scm
+++ gnu/packages/kde-office.scm
@@ -251,13 +251,13 @@ translation engines.")
 (define-public ghostwriter
   (package
     (name "ghostwriter")
-    (version "25.12.3")
+    (version "26.04.1")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://kde/stable/release-service/" version
                                   "/src/ghostwriter-" version ".tar.xz"))
               (sha256
-               (base32 "15np6iwlji7k7z4ag79wys0va14ih06cgr6fg4izgn714322f0dp"))))
+               (base32 "18z12pgcan6nl74wxjf4vm3nbh1bq12cavzqs95xc7np3j2vmqpv"))))
     (build-system qt-build-system)
     (arguments
      (list #:qtbase qtbase
