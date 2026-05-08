f2f5957d2e3a461515afd4edab89625ec72bfb9d
Sughosha <sughosha@disroot.org>
2026-05-08 06:07:59 +0530

    gnu: palapeli: Update to 26.04.1.
    * gnu/packages/kde-games.scm (palapeli): Update to 26.04.1.
    
    Change-Id: Iab274ef40dfc9aaa8f737673567db01fc79247b6

--- gnu/packages/kde-games.scm
+++ gnu/packages/kde-games.scm
@@ -1208,14 +1208,14 @@ This package is part of the KDE games module.")
 (define-public palapeli
   (package
     (name "palapeli")
-    (version "25.12.3")
+    (version "26.04.1")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://kde/stable/release-service/" version
                            "/src/palapeli-" version ".tar.xz"))
        (sha256
-        (base32 "1r95rl398k9ja8qhdcbp6175wd8wllbq9053mnfz5a4gmvb5r3ss"))))
+        (base32 "0x0c5pw1qzr9jc3pw1i94xab64kyamlsfdlpxjaqznrqq2zxqm0q"))))
     (build-system qt-build-system)
     ;; As required by PalaConfig.cmake.
     (propagated-inputs
