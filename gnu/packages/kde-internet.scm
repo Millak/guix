d583bff8e0c11e06e6f570a4f66895c60e215841
Sughosha <sughosha@disroot.org>
2026-05-08 06:08:08 +0530

    gnu: kio-extras: Update to 26.04.1.
    * gnu/packages/kde-internet.scm (kio-extras): Update to 26.04.1.
    
    Change-Id: I7c79ae95b1dc8ceebc60c6af635c3b69c9010f0e

--- gnu/packages/kde-internet.scm
+++ gnu/packages/kde-internet.scm
@@ -419,7 +419,7 @@ communicate with each other.  Here's a few things KDE Connect can do:
 (define-public kio-extras
   (package
     (name "kio-extras")
-    (version "25.12.3")
+    (version "26.04.1")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://kde/stable/release-service/"
@@ -427,7 +427,7 @@ communicate with each other.  Here's a few things KDE Connect can do:
                                   version ".tar.xz"))
               (sha256
                (base32
-                "0slihvl6raz5vhr2a9wkbcygqjc0m12l0phrl3xwz5hg69iy6qdi"))
+                "0k0rw6g47ryyz16wgdywbh5rrwyj87ppyd2zwpfwmfzhwpxn8fpm"))
               (modules '((guix build utils)))
               (snippet
                ;; Fix including libproxy.
