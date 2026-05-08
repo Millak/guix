08b246d9a1a065e9c4a553cc98cf3dddefd1c9d5
Sughosha <sughosha@disroot.org>
2026-05-08 06:08:06 +0530

    gnu: libkdcraw: Update to 26.04.1.
    * gnu/packages/kde-graphics.scm (libkdcraw): Update to 26.04.1.
    (libkdcraw-qt5): Keep at 25.12.3. This is the last version supporting Qt 5.
    
    Co-authored-by: Rodion Goritskov <rodion@goritskov.com>

--- gnu/packages/kde-graphics.scm
+++ gnu/packages/kde-graphics.scm
@@ -706,14 +706,14 @@ expression library, that is used in Krita.")
 (define-public libkdcraw
   (package
     (name "libkdcraw")
-    (version "25.12.3")
+    (version "26.04.1")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://kde/stable/release-service/" version
                            "/src/" name "-" version ".tar.xz"))
        (sha256
-        (base32 "1bwr8j7ph3cfgy3kmrwkcrdq72ck95lq3qgqnrc1i8ms9gqj1ci3"))))
+        (base32 "1vwspn83v5czkmqj5dns8x39ab20c82mlxs7dll1wk7qi1s8p2fw"))))
     (build-system cmake-build-system)
     (native-inputs
      (list pkg-config extra-cmake-modules))
@@ -736,6 +736,14 @@ decode RAW picture files.")
   (package
     (inherit libkdcraw)
     (name "libkdcraw-qt5")
+    (version "25.12.3") ;; The last version with Qt5 support
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "mirror://kde/stable/release-service/" version
+                           "/src/" (package-name libkdcraw) "-" version ".tar.xz"))
+       (sha256
+        (base32 "1bwr8j7ph3cfgy3kmrwkcrdq72ck95lq3qgqnrc1i8ms9gqj1ci3"))))
     (inputs (modify-inputs inputs
               (replace "qtbase" qtbase-5)))))
 
