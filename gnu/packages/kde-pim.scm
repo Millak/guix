9832ecf43f259f1fa0665972a8e4f5b3a3d5821c
Sughosha <sughosha@disroot.org>
2026-05-08 06:08:22 +0530

    gnu: kmessagelib: Update to 26.04.1.
    * gnu/packages/kde-pim.scm (kmessagelib): Update to 26.04.1.
    
    Change-Id: Ic3e35c7892bf1510917982e2ae2ce8af07b69197

--- gnu/packages/kde-pim.scm
+++ gnu/packages/kde-pim.scm
@@ -1960,14 +1960,14 @@ using a Qt/KMime C++ API.")
 (define-public kmessagelib
   (package
     (name "kmessagelib")
-    (version "25.12.3")
+    (version "26.04.1")
     (source
      (origin
        (method url-fetch)
        (uri (string-append "mirror://kde/stable/release-service/" version
                            "/src/messagelib-" version ".tar.xz"))
        (sha256
-        (base32 "1fnnhk95mqf8j5gh2gaq9gaf67787ia652ag4n13s1bdxghyinfj"))))
+        (base32 "0v1wcv6fb6br5zma9mpvjjzkbz3qjzxnxddi9kw9fl4r69ha001v"))))
     (properties `((upstream-name . "messagelib")))
     (build-system qt-build-system)
     (native-inputs
