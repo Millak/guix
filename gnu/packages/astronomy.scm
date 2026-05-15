e466e91a62bfe218f5840b01b3017281a166abf6
Sharlatan Hellseher <sharlatanus@gmail.com>
2026-05-15 23:10:00 +0100

    gnu: indi: Update to 2.2.1.1.
    * gnu/packages/astronomy.scm (indi): Update to 2.2.1.1.
    
    Change-Id: Id9a7bb2180556c8958c890994faab7513fa9bee7

--- gnu/packages/astronomy.scm
+++ gnu/packages/astronomy.scm
@@ -1390,7 +1390,7 @@ floating-point (no compression, LZW- or ZIP-compressed), FITS 8-bit, 16-bit,
 (define-public indi
   (package
     (name "indi")
-    (version "2.2.0")
+    (version "2.2.1.1")
     (source
      (origin
        (method git-fetch)
@@ -1399,7 +1399,7 @@ floating-point (no compression, LZW- or ZIP-compressed), FITS 8-bit, 16-bit,
              (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "0rknhrv7hhwmgvsi9lgrajl6hipsxk7d7nqazk7i8cczsrxgwdjx"))))
+        (base32 "06p1hdvbs63nmrkzhp9mqw4ayvhx1wbh1w80ikq896k30i9sx5g0"))))
     (build-system cmake-build-system)
     (arguments
      (list
