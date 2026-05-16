b61162017edb9ce64243e56ca7ecc6ab09120857
Vagrant Cascadian <vagrant@debian.org>
2026-05-15 21:19:55 -0700

    gnu: linux-libre 6.12: Update to 6.12.89.
    * gnu/packages/linux.scm (linux-libre-6.12-version): Update to 6.12.89.
    (linux-libre-6.12-pristine-source): Update hash.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -583,7 +583,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
 ;; Here are the support timelines:
 ;; <https://www.kernel.org/category/releases.html>
 
-(define-public linux-libre-6.12-version "6.12.87")
+(define-public linux-libre-6.12-version "6.12.89")
 (define-public linux-libre-6.12-gnu-revision "gnu")
 (define deblob-scripts-6.12
   (linux-libre-deblob-scripts
@@ -593,7 +593,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
    (base32 "1x48hpmvajz7v5wn1vdd7l5r14mq4ngzh5npz3pj81z4f1g61i0a")))
 (define-public linux-libre-6.12-pristine-source
   (let ((version linux-libre-6.12-version)
-        (hash (base32 "0c4qidff0qs2x0mvba83cw3ksaz2af3xwabvc839xvsc9djaf4nc")))
+        (hash (base32 "184r0wji02qw2n7ncx0jrf382yk20nnf79fvafim6a4x9a0gsnjq")))
    (make-linux-libre-source version
                             (%upstream-linux-source version hash)
                             deblob-scripts-6.12)))
