b5b2f195465a3b391415d67ce48107d825779a5b
Andreas Enge <andreas@enge.fr>
2026-05-20 20:22:42 +0200

    gnu: shepherd-0.9: Disinherit from shepherd-0.8.
    * gnu/packages/admin.scm (shepherd-0.9)[inherit]: Drop field.
    [build-system, synopsis, description, license, home-page): Copy-paste
    from shepherd-0.8.
    
    Change-Id: I5c10d3cf2991b45c27b2834d2d7528adbd6fe8ed

--- gnu/packages/admin.scm
+++ gnu/packages/admin.scm
@@ -567,7 +567,7 @@ interface and is based on GNU Guile.")
 
 (define-public shepherd-0.9
   (package
-    (inherit shepherd-0.8)
+    (name "shepherd")
     (version "0.9.3")
     (source (origin
               (method url-fetch)
@@ -576,6 +576,7 @@ interface and is based on GNU Guile.")
               (sha256
                (base32
                 "0qy2yq13xhf05an5ilz7grighdxicx56211yaarqq5qigiiybc32"))))
+    (build-system gnu-build-system)
     (arguments
      (list #:configure-flags #~'("--localstatedir=/var")
            #:make-flags #~'("GUILE_AUTO_COMPILE=0")
@@ -595,10 +596,17 @@ interface and is based on GNU Guile.")
                                       (this-package-input "guile-fibers")
                                       "/lib/guile/3.0/site-ccache"))))))
                         #~%standard-phases)))
-
     (native-inputs (list pkg-config guile-3.0
                          guile-fibers-1.1))       ;for cross-compilation
-    (inputs (list guile-3.0 guile-fibers-1.1))))
+    (inputs (list guile-3.0 guile-fibers-1.1))
+    (synopsis "System service manager")
+    (description
+     "The GNU Shepherd is a daemon-managing daemon, meaning that it supervises
+the execution of system services, replacing similar functionality found in
+typical init systems.  It provides dependency-handling through a convenient
+interface and is based on GNU Guile.")
+    (license license:gpl3+)
+    (home-page "https://www.gnu.org/software/shepherd/")))
 
 (define-public shepherd-0.10
   (package
