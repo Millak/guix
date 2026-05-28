2fd57b9dcf3f9c692a5e6334991421b936f3b3cd
Aloys Berger <me@aloysberger.com>
2026-05-28 10:03:23 +1000

    gnu: gv: Improve style.
    * gnu/packages/gv.scm (gv): Reformat with 'guix style'.
      [inputs]: Drop labels.
      [arguments]<#:phases>{set-gs-file-name}: Use search-input-file
      instead of relying on labels in the input alist.
    
    Change-Id: I7829ddb6edf020178c0dd06d065dbce8dd2f90dc
    Merges: https://codeberg.org/guix/guix/pulls/8902
    Reviewed-by: Nicolas Graves <ngraves@ngraves.fr>
    Signed-off-by: Nguyễn Gia Phong <cnx@loang.net>

--- gnu/packages/gv.scm
+++ gnu/packages/gv.scm
@@ -27,48 +27,50 @@
   #:use-module ((guix licenses) #:prefix license:)
   #:use-module (guix packages)
   #:use-module (guix download)
+  #:use-module (guix gexp)
   #:use-module (guix build-system gnu))
 
 (define-public gv
   (package
-   (name "gv")
-   (version "3.7.4")
-   (source (origin
-            (method url-fetch)
-            (uri (string-append "mirror://gnu/gv/gv-"
-                                version ".tar.gz"))
-            (sha256 (base32
-                     "0q8s43z14vxm41pfa8s5h9kyyzk1fkwjhkiwbf2x70alm6rv6qi1"))))
-   (build-system gnu-build-system)
-   (arguments
-    '(#:phases (modify-phases %standard-phases
-                 (add-before 'configure 'set-gs-file-name
-                   (lambda* (#:key inputs #:allow-other-keys)
-                     ;; Set the value of 'GV.gsInterpreter' in the generated
-                     ;; 'gv_system.ad' file.
-                     (let ((gs (assoc-ref inputs "ghostscript")))
-                       (with-fluids ((%default-port-encoding "ISO-8859-1"))
-                        (substitute* "src/Makefile.in"
-                          (("GV\\.gsInterpreter:([[:blank:]]+)gs" _ blank)
-                           (string-append "GV.gsInterpreter:" blank
-                                          gs "/bin/gs"))
-                          (("GV\\.gsCmd([[:alpha:]]+):([[:blank:]]+)gs" _
-                            command blank)
-                           (string-append "GV.gsCmd" command ":"
-                                          blank gs "/bin/gs"))))
-                       #t))))))
-   (inputs `(("ghostscript" ,ghostscript/x)
-             ("libx11" ,libx11)
-             ("libxaw3d" ,libxaw3d)
-             ("libxinerama" ,libxinerama)
-             ("libxpm" ,libxpm)
-             ("zlib" ,zlib)))
-   (native-inputs
+    (name "gv")
+    (version "3.7.4")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (string-append "mirror://gnu/gv/gv-" version ".tar.gz"))
+       (sha256
+        (base32 "0q8s43z14vxm41pfa8s5h9kyyzk1fkwjhkiwbf2x70alm6rv6qi1"))))
+    (build-system gnu-build-system)
+    (arguments
+     (list
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-before 'configure 'set-gs-file-name
+            (lambda* (#:key inputs #:allow-other-keys)
+              ;; Set the value of 'GV.gsInterpreter' in the generated
+              ;; 'gv_system.ad' file.
+              (let ((gs (search-input-file inputs "bin/gs")))
+                (with-fluids ((%default-port-encoding "ISO-8859-1"))
+                  (substitute* "src/Makefile.in"
+                    (("GV\\.gsInterpreter:([[:blank:]]+)gs" _match
+                      blank)
+                     (string-append "GV.gsInterpreter:" blank gs))
+                    (("GV\\.gsCmd([[:alpha:]]+):([[:blank:]]+)gs"
+                      _match command blank)
+                     (string-append "GV.gsCmd" command ":" blank gs))))))))))
+    (inputs
+     (list ghostscript/x
+           libx11
+           libxaw3d
+           libxinerama
+           libxpm
+           zlib))
+    (native-inputs
      (list pkg-config))
-   (synopsis "PostScript and PDF viewer using Ghostscript as a back-end")
-   (description
-    "GNU GV is a graphical user interface to the Ghostscript interpreter.
+    (home-page "https://www.gnu.org/software/gv/")
+    (synopsis "PostScript and PDF viewer using Ghostscript as a back-end")
+    (description
+     "GNU GV is a graphical user interface to the Ghostscript interpreter.
 With it, one can view and navigate through PostScript and PDF documents in X
 Windows.")
-   (license license:gpl3+)
-   (home-page "https://www.gnu.org/software/gv/")))
+    (license license:gpl3+)))
