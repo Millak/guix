017c109c6ee2071c7a46dec98e5f1966d1dfaa45
Noé Lopez <noelopez@free.fr>
2026-05-29 14:39:41 +0200

    gnu: gnome-control-center: Display JPEG XL backgrounds.
    * gnu/packages/gnome.scm (gnome-control-center)
    [inputs]: Add libjxl:pixbuf-loader.
    
    Change-Id: If03a596a8d23c0497fb1f95596bafa51cc3323d2
    Signed-off-by: Noé Lopez <noelopez@free.fr>

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -9679,6 +9679,7 @@ Libadwaita.")
            libadwaita
            libgudev
            libgtop
+           `(,libjxl "pixbuf-loader")       ;for the backgrounds
            libnma
            libpwquality
            (librsvg-for-system)             ;for loading SVG files
