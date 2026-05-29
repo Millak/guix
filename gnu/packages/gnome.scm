42f2cfbecb3556b32b38a9b74ddb6b1c3b325fb3
Noé Lopez <noelopez@free.fr>
2026-05-29 11:53:06 +0200

    gnu: gnome-shell: Wrap glycin loaders.
    * gnu/packages/gnome.scm (gnome-shell)
    [inputs]: Add glycin-loaders.
    [arguments]<#:phases>: Add phase.
    
    Change-Id: I0ce43b88f6feeff07e01472f96b8a01641e5125c
    Signed-off-by: Noé Lopez <noelopez@free.fr>

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -9765,6 +9765,11 @@ printf '~a is deprecated.  Use the \"gnome-extensions\" CLI or \
               (lambda _
                 (install-file (string-append #$output "/share/applications/org.gnome.Shell.desktop")
                               (string-append #$output "/etc/xdg/autostart"))))
+            (add-after 'install 'wrap-glycin-loaders
+              (lambda* (#:key inputs #:allow-other-keys)
+                (wrap-program (string-append #$output "/bin/gnome-shell")
+                  `("XDG_DATA_DIRS" ":" prefix
+                    ,(list (dirname (search-input-directory inputs "/share/glycin-loaders")))))))
             (replace 'glib-or-gtk-wrap
               (let ((wrap (assoc-ref %standard-phases 'glib-or-gtk-wrap)))
                 (lambda* (#:key inputs outputs #:allow-other-keys)
@@ -9807,6 +9812,7 @@ printf '~a is deprecated.  Use the \"gnome-extensions\" CLI or \
            geoclue
            gjs
            gtk
+           glycin-loaders
            gnome-autoar
            gnome-bluetooth
            gnome-desktop
