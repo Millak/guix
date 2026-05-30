5bce6597c6458f7ec1ab9561be9ff636796c25e0
Anderson Torres <anderson.torres.8519@gmail.com>
2026-05-29 23:15:28 -0300

    gnu: motif: Migrate to (gnu packages toolkits).
    * gnu/packages/toolkits.scm (motif): Move to here...
    * gnu/packages/lesstif.scm (motif): ...from here.
      New deprecated variable.
    
    Change-Id: Id63a6f1a94d542dc600872f3e24a48e6d158d6bb
    Merges: https://codeberg.org/guix/guix/pulls/8960
    Reviewed-by: Liliana Marie Prikler <liliana.prikler@gmail.com>
    Signed-off-by: Nguyễn Gia Phong <cnx@loang.net>

--- gnu/packages/lesstif.scm
+++ gnu/packages/lesstif.scm
@@ -1,6 +1,4 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2013, 2025 Andreas Enge <andreas@enge.fr>
-;;; Copyright © 2022 Andy Tai <atai@atai.org>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -18,57 +16,8 @@
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (gnu packages lesstif)
-  #:use-module ((guix licenses) #:prefix license:)
-  #:use-module (guix packages)
-  #:use-module (guix download)
-  #:use-module (guix build-system gnu)
-  #:use-module (guix git-download)
-  #:use-module (gnu packages autotools)
-  #:use-module (gnu packages c)
-  #:use-module (gnu packages compiler-tools)
-  #:use-module (gnu packages fontutils)
-  #:use-module (gnu packages gcc)
-  #:use-module (gnu packages pkg-config)
-  #:use-module (gnu packages xorg))
+  #:use-module (guix deprecation))
 
-
-(define-public motif
-  ;; This commit is from February 2023 and v2.3.8 from 2017.
-  (let ((commit "0f556b0873c72ba1152a12fd54c3198ee039e413")
-        (revision "1"))
-    (package
-      (name "motif")
-      (version (git-version "2.3.8" revision commit))
-      (source
-       (origin
-         (method git-fetch)
-         (uri (git-reference
-               (url "https://git.code.sf.net/p/motif/code")
-               (commit commit)))
-         (file-name (git-file-name name version))
-         (sha256
-          (base32
-           "1g28i4978p9dpcdxv2yx4m17bchdypm3yy6m6gzchdlrv2iklhl9"))))
-      (build-system gnu-build-system)
-      (inputs
-       (list libx11 xorgproto))
-      (propagated-inputs
-       (list fontconfig freetype libxext libxft libxpm libxt xbitmaps))
-      (native-inputs
-       ;; This fails to build with GCC 14 due to missing header includes and
-       ;; other C semantics issues.
-       (list gcc-11
-             autoconf
-             automake
-             byacc
-             flex
-             libtool
-             pkg-config))
-      (home-page "https://motif.ics.com/motif")
-      (synopsis "Toolkit for the X window system")
-      (description "Motif is a standard graphical user interface, (as defined
-by the IEEE 1295 specification), used on more than 200 hardware and software
-platforms.  It provides application developers, end users, and system vendors
-with a widely used environment for standardizing application presentation on a
-wide range of platforms.")
-      (license license:lgpl2.1+))))
+;; XXX: Deprecated on 2026-06-05.
+(define-deprecated/public-alias motif
+  (@ (gnu packages toolkits) motif))
--- gnu/packages/toolkits.scm
+++ gnu/packages/toolkits.scm
@@ -1,7 +1,9 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2022, 2023 Maxim Cournoyer <maxim@guixotic.coop>
+;;; Copyright © 2013, 2025 Andreas Enge <andreas@enge.fr>
 ;;; Copyright © 2020, 2022 Tobias Geerinckx-Rice <me@tobias.gr>
+;;; Copyright © 2022 Andy Tai <atai@atai.org>
 ;;; Copyright © 2022, 2023 John Kehayias <john.kehayias@protonmail.com>
+;;; Copyright © 2022, 2023 Maxim Cournoyer <maxim@guixotic.coop>
 ;;; Copyright © 2023 Simon South <simon@simonsouth.net>
 ;;;
 ;;; This file is part of GNU Guix.
@@ -20,18 +22,23 @@
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (gnu packages toolkits)
+  #:use-module (gnu packages autotools)
+  #:use-module (gnu packages compiler-tools)
   #:use-module (gnu packages documentation)
   #:use-module (gnu packages fontutils)
+  #:use-module (gnu packages gcc)
   #:use-module (gnu packages gl)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages xorg)
   #:use-module (gnu packages python-build)
   #:use-module (gnu packages sdl)
-  #:use-module (guix gexp)
-  #:use-module ((guix licenses) #:prefix license:)
-  #:use-module (guix packages)
-  #:use-module (guix utils)
   #:use-module (guix build-system gnu)
   #:use-module (guix build-system pyproject)
-  #:use-module (guix git-download))
+  #:use-module (guix gexp)
+  #:use-module (guix git-download)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix packages)
+  #:use-module (guix utils))
 
 (define-public imgui
   (package
@@ -186,6 +193,47 @@ standard operating system features.")
     (inputs
      (modify-inputs inputs
        (delete "freetype")))))
+
+(define-public motif
+  ;; This commit is from February 2023 and v2.3.8 from 2017.
+  (let ((commit "0f556b0873c72ba1152a12fd54c3198ee039e413")
+        (revision "1"))
+    (package
+      (name "motif")
+      (version (git-version "2.3.8" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://git.code.sf.net/p/motif/code")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32
+           "1g28i4978p9dpcdxv2yx4m17bchdypm3yy6m6gzchdlrv2iklhl9"))))
+      (build-system gnu-build-system)
+      (inputs
+       (list libx11 xorgproto))
+      (propagated-inputs
+       (list fontconfig freetype libxext libxft libxpm libxt xbitmaps))
+      (native-inputs
+       ;; This fails to build with GCC 14 due to missing header includes and
+       ;; other C semantics issues.
+       (list gcc-11
+             autoconf
+             automake
+             byacc
+             flex
+             libtool
+             pkg-config))
+      (home-page "https://motif.ics.com/motif")
+      (synopsis "Toolkit for the X window system")
+      (description "Motif is a standard graphical user interface, (as defined
+by the IEEE 1295 specification), used on more than 200 hardware and software
+platforms.  It provides application developers, end users, and system vendors
+with a widely used environment for standardizing application presentation on a
+wide range of platforms.")
+      (license license:lgpl2.1+))))
 
 (define-public nuklear
   (package
