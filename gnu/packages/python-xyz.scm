3abff1976802bf112ea1014b3163cc1874e9e7ba
Sharlatan Hellseher <sharlatanus@gmail.com>
2026-05-31 23:03:20 +0100

    gnu: python-pexpect: Remove python-wheel from native-inputs.
    * gnu/packages/python-xyz.scm (python-pexpect):
    [native-inputs]: Remove python-wheel.
    
    Change-Id: I96b1c9d56be68f59379aa0b8c1681dcfcd11d683

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -15423,7 +15423,6 @@ wraps Python's standard library threading and multiprocessing objects.")
            man-db
            python-pytest
            python-setuptools
-           python-wheel
            which))
     (propagated-inputs
      (list python-ptyprocess))
