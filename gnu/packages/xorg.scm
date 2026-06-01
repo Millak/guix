df00cca9df75e540b8a032406b4fb5812163cbf2
John Kehayias <john@guixotic.coop>
2026-06-01 09:52:20 -0400

    gnu: xorg-server-xwayland: Update to 24.1.11 [security-fixes].
    A previous version, 24.1.10, fixed CVE-2026-33999, CVE-2026-34000,
    CVE-2026-34001, CVE-2026-34002, and CVE-2026-34003.  See
    <https://lists.x.org/archives/xorg-announce/2026-April/003677.html> for
    details.
    
    * gnu/packages/xorg.scm (xorg-server-xwayland): Update to 24.1.11.
    
    Change-Id: Id4e6bb4fab8e9f3e6d9f24df5eb1982265a6734f

--- gnu/packages/xorg.scm
+++ gnu/packages/xorg.scm
@@ -5650,7 +5650,7 @@ EGLStream families of extensions.")
 (define-public xorg-server-xwayland
   (package
     (name "xorg-server-xwayland")
-    (version "24.1.9")
+    (version "24.1.11")
     (source
      (origin
        (method url-fetch)
@@ -5658,7 +5658,7 @@ EGLStream families of extensions.")
                            "/xserver/xwayland-" version ".tar.xz"))
        (sha256
         (base32
-         "1l7xk92q5xahbcxjziv4xcwdl09qkk3brjyih2dxn225m0ksz5zj"))))
+         "1zrq445awfjvb80ffva26sykg0affavzxv3gpw4q81qri0d5l497"))))
     (inputs (list font-dejavu
                   dbus
                   egl-wayland
