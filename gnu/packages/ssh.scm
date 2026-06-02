ec1e89e24c8b25b319530d93e34c08125fea6ef4
Yelninei <yelninei@tutamail.com>
2026-06-02 17:08:47 +0000

    gnu: openssh: Set path to xauth when cross compiling.
    * gnu/packages/ssh.scm (openssh)[#:configure-flags]: When cross-compiling, set
    --with-xauth flag.
    
    Change-Id: I11aae7af14872d980be3679410d0f700e6b41137
    Signed-off-by: Andreas Enge <andreas@enge.fr>

--- gnu/packages/ssh.scm
+++ gnu/packages/ssh.scm
@@ -276,7 +276,11 @@ a server that supports the SSH-2 protocol.")
         ;; cross-compiled binaries because it invokes 'strip' instead of
         ;; 'TRIPLET-strip'.  Work around this.
         (if #$(%current-target-system)
-            (list "--disable-strip")
+            (append (list "--disable-strip")
+                    (let ((xauth #$(this-package-input "xauth")))
+                      (if xauth
+                          (list (string-append "--with-xauth=" xauth "/bin/xauth"))
+                          '())))
             '()))
      #:phases
      #~(modify-phases %standard-phases
