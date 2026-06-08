8e97cdd989499114496a3cf7c3c6f9e231cca7de
Anderson Torres <anderson.torres.8519@gmail.com>
2026-06-07 21:38:58 -0300

    gnu: rcm: Move to (gnu packages configuration-management).
    * gnu/packages/configuration-management.scm (rcm): Move to here...
    * gnu/packages/rcm.scm (rcm): ...from here.  New deprecated variable.
    
    Change-Id: I3c6c7b641e8e5335ba86e70c37ea484983c151ba
    Merges: https://codeberg.org/guix/guix/pulls/9144
    Signed-off-by: Nguyễn Gia Phong <cnx@loang.net>

--- gnu/packages/rcm.scm
+++ gnu/packages/rcm.scm
@@ -1,6 +1,5 @@
 ;;; GNU Guix --- Functional package management for GNU
-;;; Copyright © 2019 Josh Holland <josh@inv.alid.pw>
-;;; Copyright © 2021, 2022 Tobias Geerinckx-Rice <me@tobias.gr>
+;;; Copyright © 2026 Anderson Torres <anderson.torres.8519@gmail.com>
 ;;;
 ;;; This file is part of GNU Guix.
 ;;;
@@ -18,44 +17,8 @@
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (gnu packages rcm)
-  #:use-module (guix packages)
-  #:use-module (guix download)
-  #:use-module (guix build-system gnu)
-  #:use-module (guix licenses)
-  #:use-module (gnu packages python-check)
-  #:use-module (gnu packages perl))
+  #:use-module (guix deprecation))
 
-(define-public rcm
-  (package
-    (name "rcm")
-    (version "1.3.5")
-    (source (origin
-              (method url-fetch)
-              (uri (string-append "https://thoughtbot.github.io/rcm/dist/rcm-"
-                                  version ".tar.gz"))
-              (sha256
-               (base32 "0bdyksrd9i3lkmr9kq6dwa0l4g2403vnma5s4j9h8spi4rziwx14"))))
-    (build-system gnu-build-system)
-    (arguments '(#:phases
-                 (modify-phases %standard-phases
-                   (add-after 'patch-source-shebangs 'patch-tests
-                     (lambda _
-                       (substitute* '("test/rcrc-tilde.t"
-                                      "test/rcdn-hooks-run-in-order.t"
-                                      "test/rcup-hooks-run-in-order.t")
-                         (("/bin/sh") (which "sh")))
-                       (substitute* "test/rcup-hooks.t"
-                         (("/usr/bin/env") (which "env"))))))
-                  #:parallel-tests? #f))
-    (native-inputs (list perl python-cram))
-    (synopsis "Management suite for dotfiles")
-    (description "The rcm suite of tools is for managing dotfiles directories.  This is
-a directory containing all the @code{.*rc} files in your home directory
-(@code{.zshrc}, @code{.vimrc}, and so on). These files have gone by many
-names in history, such as “rc files” because they typically end in rc
-or “dotfiles” because they begin with a period.  This suite is useful
-for committing your rc files to a central repository to share, but it also
-scales to a more complex situation such as multiple source directories
-shared between computers with some host-specific or task-specific files.")
-    (license bsd-3)
-    (home-page "https://github.com/thoughtbot/rcm")))
+;; Deprecated on 2026-06-07.
+(define-deprecated/public-alias rcm
+  (@ (gnu packages configuration-management) rcm))
