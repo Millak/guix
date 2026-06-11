f747e1192ae0a8f32b0357ef2fb814370d7c3d11
Sharlatan Hellseher <sharlatanus@gmail.com>
2026-06-11 15:25:24 +0100

    gnu: python-snowballstemmer: Drop python-wheel.
    * gnu/packages/python-xyz.scm (python-snowballstemmer):
    [native-inputs]: Remove python-wheel.
    
    Change-Id: Iec1013e0639e77e283ecaadaa307212ac4c0a97b

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -24603,8 +24603,7 @@ Its features include
      (list
       #:tests? #f)) ; no tests
     (native-inputs
-     (list python-setuptools
-           python-wheel))
+     (list python-setuptools))
     (home-page "https://github.com/shibukawa/snowball_py")
     (synopsis "Snowball stemming library collection for Python")
     (description
