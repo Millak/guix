9b719c04baee7caffab9932ecaca2293b5e4cbdf
Sharlatan Hellseher <sharlatanus@gmail.com>
2026-06-14 20:12:18 +0100

    gnu: python-chardet: Drop python-wheel.
    Upstream started utilizing Anthropic's Claude code assistant since v6,
    which makes the update and status of it unclear, see guix/guix#9242.
    
    * gnu/packages/python-xyz.scm (python-chardet):
    [native-inputs]: Remove python-wheel.
    
    Change-Id: Ia8194b759cfdcc85cfafccf224c66850fa9a07d8

--- gnu/packages/python-xyz.scm
+++ gnu/packages/python-xyz.scm
@@ -19941,6 +19941,7 @@ JavaScript, leveraging popular online obfuscators.")
 (define-public python-chardet
   (package
     (name "python-chardet")
+    ;; XXX: Before update, check status of guix/guix#9242.
     (version "5.2.0")
     (source
      (origin
@@ -19950,7 +19951,7 @@ JavaScript, leveraging popular online obfuscators.")
         (base32
          "1xqwkjijq2fxn8rhcvfddl14lp39jllqbh527yy19i58g7s6yfqv"))))
     (native-inputs
-     (list python-pytest python-setuptools python-wheel))
+     (list python-pytest python-setuptools))
     (build-system pyproject-build-system)
     (arguments
      (list #:test-flags
