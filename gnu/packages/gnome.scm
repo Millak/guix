e38917997a6af0f1814f7f978ce1bb50a894bacc
Liliana Marie Prikler <liliana.prikler@gmail.com>
2026-06-21 09:48:42 +0200

    gnu: gnome-shell: Adjust wrapper for GI 2.0.
    * gnu/packages/gnome.scm (gnome)[#:phases]<wrap-programs>: Use
    GIRepository.dup_default() to get Repository “singleton”.
    
    Signed-off-by: Noé Lopez <noelopez@free.fr>

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -9719,7 +9719,7 @@ properties, screen resolution, and other GNOME parameters.")
                     (("^import " all)
                      (string-append "'" gi-typelib-path "'.split(':').forEach("
                                     "path => imports.gi.GIRepository.Repository."
-                                    "prepend_search_path(path));\n"
+                                    "dup_default().prepend_search_path(path));\n"
                                     all)))
                   ;; Screencast requires a pipewire service running
                   ;; (i.e. as provided by home-pipewire-service-type)
@@ -9728,7 +9728,7 @@ properties, screen resolution, and other GNOME parameters.")
                     (("^import " all)
                      (string-append "'" gi-typelib-path "'.split(':').forEach("
                                     "path => imports.gi.GIRepository.Repository."
-                                    "prepend_search_path(path));\n"
+                                    "dup_default().prepend_search_path(path));\n"
                                     "imports.gi.GLib.setenv('GST_PLUGIN_SYSTEM_PATH',"
                                     "[imports.gi.GLib.getenv('GST_PLUGIN_SYSTEM_PATH'),"
                                     "'" gst-plugin-path "'].filter(v => v).join(':'),"
