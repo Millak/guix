a0d9232a9a905c366c9750e050d4e485b40bf9aa
Sharlatan Hellseher <sharlatanus@gmail.com>
2026-07-03 11:25:17 +0100

    gnu: librelane: Relax requirements on Rich.
    * gnu/packages/electronics.scm (librelane):
    [phases]{relax-requirements}: Reduce upper version bar for Rich.
    [inputs]: Remove python-pcpp.

--- gnu/packages/electronics.scm
+++ gnu/packages/electronics.scm
@@ -1748,7 +1748,8 @@ an embedded event driven algorithm.")
           (add-after 'unpack 'relax-requirements
             (lambda _
               (substitute* "pyproject.toml"
-                (("click>=8,<8.3") "click>=8"))))
+                (("click>=8,<8.3") "click>=8")
+                (("rich>=12,<15") "rich"))))
           (add-after 'compress-documentation 'wrap-program
             (lambda _
               (wrap-program (string-append #$output "/bin/librelane")
@@ -1788,7 +1789,6 @@ an embedded event driven algorithm.")
            python-klayout
            python-lln-libparse
            python-lxml
-           python-pcpp
            python-psutil
            python-pyyaml
            python-rapidfuzz
