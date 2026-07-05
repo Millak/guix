227ea275fbecb8cadbcce71290bd64cb0179a3fa
Sughosha <sughosha@disroot.org>
2026-07-05 07:40:00 +0530

    gnu: papers: Replace poppler-next with poppler.
    * gnu/packages/gnome.scm (papers)[inputs]: Remove poppler-next; add poppler.

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -2734,7 +2734,7 @@ on the GNOME Desktop with a single simple application.")
             libspelling
             libtiff
             nautilus                    ; for nautilus extension
-            poppler-next
+            poppler
             zlib
             (cargo-inputs 'papers)))
     (home-page "https://apps.gnome.org/Papers")
