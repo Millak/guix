eae9e62ee203c566136b1e638dbfb952c3022c60
Sughosha <sughosha@disroot.org>
2026-07-05 20:48:42 +0530

    gnu: kwallet-5: Depend on gpgmepp instead of gpgme-2.
    Since the C++ bindings for GPGME are moved to GPGME++, use it instead.
    
    * gnu/packages/kde-frameworks.scm (kwallet-5)[inputs]: Remove gpgme-2; add
    gpgmepp.

--- gnu/packages/kde-frameworks.scm
+++ gnu/packages/kde-frameworks.scm
@@ -4587,7 +4587,7 @@ the passwords on KDE work spaces.")
     (native-inputs
      (list extra-cmake-modules kdoctools-5))
     (inputs
-     (list gpgme-2
+     (list gpgmepp
            kauth-5
            kcodecs-5
            kconfig-5
