3f60b8c688709bc8ec4896d5a86465fd2a82ffc2
Vagrant Cascadian <vagrant@debian.org>
2026-07-18 17:38:31 -0700

    gnu: linux-libre 6.12: Update to 6.12.96.
    * gnu/packages/linux.scm (linux-libre-6.12-version): Update to 6.12.96.
    (linux-libre-6.12-pristine-source): Update hash.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -598,7 +598,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
 ;; Here are the support timelines:
 ;; <https://www.kernel.org/category/releases.html>
 
-(define-public linux-libre-6.12-version "6.12.95")
+(define-public linux-libre-6.12-version "6.12.96")
 (define-public linux-libre-6.12-gnu-revision "gnu")
 (define deblob-scripts-6.12
   (linux-libre-deblob-scripts
@@ -608,7 +608,7 @@ corresponding UPSTREAM-SOURCE (an origin), using the given DEBLOB-SCRIPTS."
    (base32 "1x48hpmvajz7v5wn1vdd7l5r14mq4ngzh5npz3pj81z4f1g61i0a")))
 (define-public linux-libre-6.12-pristine-source
   (let ((version linux-libre-6.12-version)
-        (hash (base32 "1xmrsi0kimirky4cailnkkrbd72pp9n8irfx6lfmss8yrcgwbs59")))
+        (hash (base32 "1hapz6xz7plq56jc0drbzcsfcm1amlnphwbfhl0klsxkb9finbkx")))
    (make-linux-libre-source version
                             (%upstream-linux-source version hash)
                             deblob-scripts-6.12)))
