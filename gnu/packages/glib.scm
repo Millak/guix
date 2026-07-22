76c3bf901c2f951885089441f2441f7f8a4b0354
matper <matper@noreply.codeberg.org>
2026-07-22 15:40:22 +0200

    gnu: template-glib: Update to 3.40.0.
    * gnu/packages/glib.scm (template-glib): Update to 3.40.0.
    [inputs]: Remove gobject-introspection.
    [native-inputs]: Add gobject-introspection.
    
    Signed-off-by: Liliana Marie Prikler <liliana.prikler@gmail.com>

--- gnu/packages/glib.scm
+++ gnu/packages/glib.scm
@@ -1344,7 +1344,7 @@ other API remains the same.")
 (define-public template-glib
   (package
     (name "template-glib")
-    (version "3.38.0")
+    (version "3.40.0")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://gnome/sources/" name "/"
@@ -1352,13 +1352,14 @@ other API remains the same.")
                                   name "-" version ".tar.xz"))
               (sha256
                (base32
-                "04mk1h2jzij91z5an08irah76wx6qpggf8n45rzypwnw4g10vl20"))))
+                "1mfgrz01an5jlvrwn49s8dgll6yxz8jchnn5dbv6vk14dhpyqcz5"))))
     (build-system meson-build-system)
-    (inputs (list gettext-minimal glib gobject-introspection))
+    (inputs (list gettext-minimal glib))
     (native-inputs
      (list bison
            flex
-           `(,glib "bin") ;for glib-mkenums
+           `(,glib "bin")               ;for glib-mkenums
+           gobject-introspection        ;for g-ir-scanner
            pkg-config
            vala))
     (home-page "https://gitlab.gnome.org/GNOME/template-glib")
