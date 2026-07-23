9178b7f62cde9b1047fa1e2688c27ced36feac12
André Batista <nandre@riseup.net>
2026-07-23 12:47:08 -0300

    gnu: noscript: Update to 13.6.31.1984.
    * gnu/packages/browser-extensions.scm (noscript): Update to 13.6.31.1984.
    
    Merges: https://codeberg.org/guix/guix/pulls/10157
    Signed-off-by: Nguyễn Gia Phong <cnx@loang.net>

--- gnu/packages/browser-extensions.scm
+++ gnu/packages/browser-extensions.scm
@@ -377,7 +377,7 @@ with the @uref{https://keepassxc.org, KeePassXC} password manager.")
 (define noscript
   (package
     (name "noscript")
-    (version "13.6.24.1984")
+    (version "13.6.31.1984")
     (source (origin
               (method url-fetch/zipbomb)
               (uri (string-append
@@ -385,7 +385,7 @@ with the @uref{https://keepassxc.org, KeePassXC} password manager.")
                     version ".xpi"))
               (sha256
                (base32
-                "14mf7a99lm34fwijz43vsiab8xcd4pxyg9qnjw31gfk7bagr2s96"))))
+                "07xddljhx8a83pq8g2j6kj86srng4srlbbl2qllg645nascniq98"))))
     (build-system copy-build-system)
     (properties '((addon-id . "{73a6fe31-595d-460b-a920-fcc0f8843232}")))
     (arguments
