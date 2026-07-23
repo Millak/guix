241e2ae2d36cb82bb39b9781a0690ed4205cf54f
Zheng Junjie <z572@z572.online>
2026-07-23 13:58:00 +0800

    gnu: libsoup-minimal: Remove the unused inputs.
    * gnu/packages/gnome.scm (libsoup-minimal)[native-inputs]: Remove httpd/pinned.
    
    Signed-off-by: Maxim Cournoyer <maxim@guixotic.coop>

--- gnu/packages/gnome.scm
+++ gnu/packages/gnome.scm
@@ -5053,8 +5053,7 @@ as OpenStreetMap, OpenCycleMap, OpenAerialMap and Maps.")
            python-wrapper
            vala
            curl
-           gnutls                       ;for 'certtool'
-           httpd/pinned))
+           gnutls))                     ;for 'certtool'
     (propagated-inputs
      ;; libsoup-3.0.pc refers to all of these (except where otherwise noted)
      (list brotli
