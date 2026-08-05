841f14057828401edc35ddb0737f6bf559eb7ed1
Andreas Enge <andreas@enge.fr>
2026-08-05 12:58:23 +0200

    gnu: Remove minetest-basic-materials.
    * gnu/packages/minetest.scm (minetest-basic-materials): Delete variable.

--- gnu/packages/minetest.scm
+++ gnu/packages/minetest.scm
@@ -29,9 +29,6 @@
   #:use-module (guix packages)
   #:use-module (gnu packages luanti))
 
-(define-deprecated-package minetest-basic-materials
-  luanti-basic-materials)
-
 (define-deprecated-package minetest-coloredwood
   luanti-coloredwood)
 
