cc5dc4054a9ed92deec8335b23503ad5493c5709
Andreas Enge <andreas@enge.fr>
2026-08-05 12:52:37 +0200

    gnu: Remove show-me-the-key.
    * gnu/packages/video.scm (show-me-the-key): Delete variable.

--- gnu/packages/video.scm
+++ gnu/packages/video.scm
@@ -7504,9 +7504,6 @@ Users in group @code{wheel} can skip password authentication.")
     (home-page "https://github.com/AlynxZhou/showmethekey")
     (license license:asl2.0)))
 
-(define-deprecated-package show-me-the-key
-  showmethekey)
-
 ;;;
 ;;; Avoid adding new packages to the end of this file. To reduce the chances
 ;;; of a merge conflict, place them above by existing packages with similar
