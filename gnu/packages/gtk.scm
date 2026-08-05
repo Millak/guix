f6de4aa35cc491586ec88f40efaad91b6b048d4a
Andreas Enge <andreas@enge.fr>
2026-08-05 12:16:50 +0200

    gnu: Remove atk.
    * gnu/packages/gtk.scm (atk): Delete variable.

--- gnu/packages/gtk.scm
+++ gnu/packages/gtk.scm
@@ -873,9 +873,6 @@ is part of the GNOME accessibility project.")
     (properties (alist-delete 'hidden?
                               (package-properties at-spi2-core)))))
 
-(define-deprecated-package atk
-  at-spi2-core)
-
 (define-public gtk+-2
   (package
     (name "gtk+")
