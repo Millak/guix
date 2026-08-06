5375f33fd48ffc3b39ecc1c5993e299258a043d8
Sharlatan Hellseher <sharlatanus@gmail.com>
2026-08-06 18:37:35 +0100

    gnu: Add go-github-com-sourcegraph-zoekt.
    * gnu/packages/golang-vcs.scm (go-github-com-sourcegraph-zoekt): New variable.
    
    Merges: guix/guix!10402

--- gnu/packages/golang-vcs.scm
+++ gnu/packages/golang-vcs.scm
@@ -33,6 +33,7 @@
   #:use-module (gnu packages golang)
   #:use-module (gnu packages golang-build)
   #:use-module (gnu packages golang-check)
+  #:use-module (gnu packages golang-compression)
   #:use-module (gnu packages golang-crypto)
   #:use-module (gnu packages golang-maths)
   #:use-module (gnu packages golang-web)
@@ -713,6 +714,110 @@ It's a static checker for GitHub Actions workflow files.")
      "This package provides a Go wrapper for @code{universal-ctags}.")
     (license license:asl2.0)))
 
+(define-public go-github-com-sourcegraph-zoekt
+  (package
+    (name "go-github-com-sourcegraph-zoekt")
+    (properties '((commit . "a3af895d493fe19ed648a2b3db863e62757d0199")
+                  (revision . "0")
+                  (go-pseudo-version . "0.0.0-20260806152957-a3af895d493f")))
+    (version (git-version "0.0.0"
+                          (assoc-ref properties 'revision)
+                          (assoc-ref properties 'commit)))
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+              (url "https://github.com/sourcegraph/zoekt")
+              (commit (assoc-ref properties 'commit))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0h5jsdlinam59j3rwz6qh80yyb43zk5yh1rd2qsmhmnmg0wn5nzh"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:import-path "github.com/sourcegraph/zoekt"
+      #:unpack-path "github.com/sourcegraph/zoekt"
+      #:phases
+      #~(modify-phases %standard-phases
+          (add-after 'unpack 'fix-shell-path
+            (lambda* (#:key inputs unpack-path #:allow-other-keys)
+              (with-directory-excursion (string-append "src/" unpack-path)
+                (substitute* (find-files "." "\\.go$")
+                  (("/bin/sh")
+                   (search-input-file inputs "bin/sh")))))))))
+    (native-inputs
+     ;; Tests and cmd/ inputs which don't have to be propagated.
+     (list git-minimal/pinned
+           go-github-com-adalogics-go-fuzz-headers
+           go-github-com-felixge-fgprof
+           go-github-com-google-go-cmp
+           go-github-com-google-slothfs
+           go-github-com-sourcegraph-log
+           go-github-com-stretchr-testify
+           go-github-com-xeipuuv-gojsonschema
+           go-pgregory-net-rapid))
+    (propagated-inputs
+     (list go-cloud-google-com-go-profiler
+           go-code-gitea-io-sdk-gitea
+           go-github-com-andygrunwald-go-gerrit
+           go-github-com-bmatcuk-doublestar-v4
+           go-github-com-cespare-xxhash-v2
+           go-github-com-dustin-go-humanize
+           go-github-com-fsnotify-fsnotify
+           go-github-com-gfleury-go-bitbucket-v1
+           go-github-com-go-enry-go-enry-v2
+           go-github-com-go-git-go-billy-v5
+           go-github-com-go-git-go-git-v5
+           go-github-com-gobwas-glob
+           go-github-com-google-go-github-v78
+           go-github-com-grafana-regexp
+           go-github-com-grpc-ecosystem-go-grpc-middleware-providers-prometheus
+           go-github-com-grpc-ecosystem-go-grpc-middleware-v2
+           go-github-com-mxk-go-flowrate
+           go-github-com-opentracing-opentracing-go
+           go-github-com-peterbourgon-ff-v3
+           go-github-com-pkg-errors
+           go-github-com-prometheus-client-golang
+           go-github-com-prometheus-procfs
+           go-github-com-roaringbitmap-roaring-v2
+           go-github-com-rs-xid
+           go-github-com-shirou-gopsutil-v3
+           go-github-com-sourcegraph-go-ctags
+           go-github-com-sourcegraph-mountinfo
+           go-github-com-uber-jaeger-client-go
+           go-github-com-uber-jaeger-lib
+           go-github-com-wasilibs-go-re2
+           go-gitlab-com-gitlab-org-api-client-go
+           go-go-opentelemetry-io-contrib-instrumentation-google-golang-org-grpc-otelgrpc
+           go-go-opentelemetry-io-contrib-propagators-jaeger
+           go-go-opentelemetry-io-contrib-propagators-ot
+           go-go-opentelemetry-io-otel
+           go-go-opentelemetry-io-otel-bridge-opentracing
+           go-go-opentelemetry-io-otel-exporters-otlp-otlptrace
+           go-go-opentelemetry-io-otel-exporters-otlp-otlptrace-otlptracegrpc
+           go-go-opentelemetry-io-otel-exporters-otlp-otlptrace-otlptracehttp
+           go-go-opentelemetry-io-otel-sdk
+           go-go-opentelemetry-io-otel-trace
+           go-go-uber-org-atomic
+           go-go-uber-org-automaxprocs
+           go-go-uber-org-multierr
+           go-golang-org-x-net
+           go-golang-org-x-oauth2
+           go-golang-org-x-sync
+           go-golang-org-x-sys
+           go-google-golang-org-grpc
+           go-google-golang-org-protobuf))
+    (home-page "https://github.com/sourcegraph/zoekt")
+    (synopsis "Fast trigram based code search")
+    (description
+     "Zoekt is a text search engine intended for use with source code,
+supporting fast substring and regexp matching on source code, with a rich
+query language that includes boolean operators (and, or, not).  It can search
+individual repositories, and search across many repositories in a large
+codebase.  Zoekt ranks search results using a combination of code-related
+signals like whether the match is on a symbol.")
+    (license license:asl2.0)))
+
 (define-public go-gitlab-com-digitalxero-go-conventional-commit
   (package
     (name "go-gitlab-com-digitalxero-go-conventional-commit")
@@ -841,6 +946,54 @@ directory.  It is a perfect \"sidecar\" container in Kubernetes - it can
 periodically pull files down from a repository so that an application can
 consume them.")
     (license license:asl2.0)))
+
+(define-public zoekt
+  (package/inherit go-github-com-sourcegraph-zoekt
+    (name "zoekt")
+    (arguments
+     (substitute-keyword-arguments arguments
+       ((#:import-path _ ) "github.com/sourcegraph/zoekt/cmd/...")
+       ((#:install-source? _ #t) #f)
+       ((#:tests? _ #t) #f)))
+    (native-inputs
+     (append
+      (package-native-inputs go-github-com-sourcegraph-zoekt)
+      (package-propagated-inputs go-github-com-sourcegraph-zoekt)))
+    (propagated-inputs '())
+    (inputs '())
+    (description
+     "This package provides a collection of Zoekt command line utilities:
+@itemize
+@item zoekt - supports searching over an index directory or shard
+@item zoekt-archive-index - indexes a git archive
+@item zoekt-dynamic-indexserver - starts a server to manage dynamic indexing
+@item zoekt-git-clone - fetches all repos of a user or organization and clones
+them
+@item zoekt-git-index - indexes a single git repository
+@item zoekt-index - indexes a directory of files
+@item zoekt-indexserver - starts a service that periodically reindexes
+repositories
+@item zoekt-local-sync - synchronizes a Zoekt index with Git repositories
+discovered under local directory roots
+@item zoekt-merge-index - merges a set of index shards into a compound shard
+@item zoekt-mirror-bitbucket-server - fetches all repos of a bitbucket
+project, optionally of a specific type, and clones them
+@item zoekt-mirror-gerrit - fetches all repos of a Gerrit host
+@item zoekt-mirror-gitea - fetches all repos of a Gitea user or organization
+and clones them
+@item zoekt-mirror-github - fetches all repos of a GitHub user or organization
+and clones them
+@item zoekt-mirror-gitiles - fetches all repos of a Gitiles host
+@item zoekt-mirror-gitlab - fetches all repos for a user from GitLab
+@item zoekt-repo-index - indexes repository that uses the
+@url{https://android.googlesource.com/tools/repo, Android repo tool}
+@item zoekt-sourcegraph-indexserver - periodically reindexes repositories from
+a Sourcegraph instance
+@item zoekt-test - compares the zoekt results with raw substring search
+@item zoekt-webserver - starts a server that responds to search queries, using
+an index generated by another program such as zoekt-indexserver
+@end itemize
+")))
 
 ;;;
 ;;; Avoid adding new packages to the end of this file. To reduce the chances
