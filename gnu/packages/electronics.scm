71f625d78ec78c7204da7c39b98f9639d8ae4954
Cayetano Santos <csantosb@inventati.org>
2026-08-11 10:16:54 +0200

    gnu: nextpnr: Update to 0.11.1.
    * gnu/packages/electronics.scm (nextpnr): Update to 0.11.1.

--- gnu/packages/electronics.scm
+++ gnu/packages/electronics.scm
@@ -2391,7 +2391,7 @@ layout of a circuit corresponds to the desired netlists.")
 (define-public nextpnr
   (package
     (name "nextpnr")
-    (version "0.11")
+    (version "0.11.1")
     (source
      (origin
        (method git-fetch)
@@ -2425,7 +2425,7 @@ layout of a circuit corresponds to the desired netlists.")
                             "imgui"
                             "qtimgui")))
        (sha256
-        (base32 "1cw4ch2wjzgwraq6s714mfbhrl70f7akpzbbxgj9rw4f28r7lsmw"))))
+        (base32 "1mb2dywv4gd52vzmml7rlyqdv4psw0l0gfy0mgwsknxblps3aha1"))))
     (outputs '("out" "bba"))
     (build-system qt-build-system)
     (arguments
