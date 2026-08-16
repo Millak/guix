277cdcc5b3fd48d52e680418822ac95bf05ab6ae
Sharlatan Hellseher <sharlatanus@gmail.com>
2026-08-16 22:11:15 +0100

    gnu: go-1.26: Update to 1.26.6 [security-fixes].
    go1.26.6 (released 2026-08-13) includes security fixes to the go
    command, and the crypto/tls, encoding/asn1, encoding/xml, html/template,
    net, net/http, and net/url packages, as well as bug fixes to the
    compiler, the linker, the runtime, and the crypto/tls and os packages.
    
    See: <https://github.com/golang/go/milestone/441>,
    <https://groups.google.com/g/golang-announce/c/94pEornpRlI>
    
    Contains fixes for:
    CVE-2026-56865: x/mod/sumdb/tlog: fix transparency log tile verification
                    bypass
    CVE-2026-56864: x/mod/sumdb: ignore unrelated, unauthenticated hashes in
                    Lookup
    CVE-2026-56859: encoding/xml: add recursion depth guard during decode
    CVE-2026-56853: net/http: apply ReadHeaderTimeout when doing unencrypted
                    HTTP/2 check
    CVE-2026-56860: net/url: avoid quadratic complexity in resolvePath
    CVE-2026-46600: golang.org/x/net/dns/dnsmessage: panic when parsing
                    invalid SVCB record
    CVE-2026-56862: crypto/tls: limit handshake messages we are willing to
                    accept post-handshake
    CVE-2026-56858: html/template: fix Javascript regexp context tracking
    CVE-2026-39821: x/net/idna: failure to reject ASCII-only
                    Punycode-encoded labels
    CVE-2026-33818: encoding/asn1: enforce maximum recursion depth
    
    * gnu/packages/golang.scm (go-1.26): Update to 1.26.6.

--- gnu/packages/golang.scm
+++ gnu/packages/golang.scm
@@ -1139,7 +1139,7 @@ in the style of communicating sequential processes (@dfn{CSP}).")
   (package
     (inherit go-1.24)
     (name "go")
-    (version "1.26.5")
+    (version "1.26.6")
     (source
      (origin
        (method git-fetch)
@@ -1148,7 +1148,7 @@ in the style of communicating sequential processes (@dfn{CSP}).")
              (commit (string-append "go" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "0kip7ak6ablgvpaf20mqy8an9g9y9nyslsf3hbk1z5gmy10789lg"))))
+        (base32 "0gjlgap2m12a04dcz4pggdcfnwnd663yk30y9qas9j18vrhawy92"))))
     (arguments
      (substitute-keyword-arguments arguments
        ((#:phases phases)
