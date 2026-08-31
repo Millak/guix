d6dc2187fa51e25953056bc747629c237a02a874
Morgan Smith <Morgan.J.Smith@outlook.com>
2026-08-31 11:03:22 -0400

    gnu: Remove emacs-auth-source-pass.
    On 2021-06-27, the project readme was replaced with this statement:
    
      This project has been integrated into Emacs.  As a consequence, I'm closing
      this repository. Please consider contributing to Emacs directly.
    
    * gnu/packages/emacs-xyz.scm (emacs-auth-source-pass): Delete variable.
    
    Signed-off-by: Liliana Marie Prikler <liliana.prikler@gmail.com>

--- gnu/packages/emacs-xyz.scm
+++ gnu/packages/emacs-xyz.scm
@@ -29641,33 +29641,6 @@ OTP URIs from screenshots of QR codes, and to export them back to QR codes if
 needed.")
     (license license:gpl3+)))
 
-(define-public emacs-auth-source-pass
-  (let ((commit "847a1f54ed48856b4dfaaa184583ef2c84173edf")
-        (revision "2"))
-    (package
-      (name "emacs-auth-source-pass")
-      (version (git-version "5.0.0" revision commit))
-      (source
-       (origin
-         (method git-fetch)
-         (uri (git-reference
-               (url "https://github.com/DamienCassou/auth-password-store")
-               (commit commit)))
-         (file-name (git-file-name name version))
-         (sha256
-          (base32 "0g48z5w6n3c54zqfpx65dfyl9jqbl15idvbb1hhw2pd9f9r8fykk"))))
-      (build-system emacs-build-system)
-      (arguments
-       `(#:test-command '("emacs" "--batch"
-                          "-L" "."
-                          "-l" "test/auth-source-pass-tests.el"
-                          "-f" "ert-run-tests-batch-and-exit")))
-      (home-page "https://github.com/DamienCassou/auth-password-store")
-      (synopsis "Integrate @code{auth-source} with @code{password-store}")
-      (description "This package helps Emacs integrate with the Unix
-@code{password-store} application.")
-      (license license:gpl3+))))
-
 (define-public emacs-pass
   (package
     (name "emacs-pass")
