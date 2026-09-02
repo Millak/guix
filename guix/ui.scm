Millak/guix

commit a6094158aa4fb8ec217e41c000264ba6e2095a3b

gnupg: Handle missing gpg / gpgv binaries in refresh.
Previously, this would fail with the following error:

    From https://poppler.freedesktop.org/poppler-26.04.0.tar.xz.sig...
     ….0.tar.xz.sig  833B                 1.8MiB/s 00:00 ▕██████████████████▏ 100.0%
    In execvp of gpgv: No such file or directory
    Would you like to add this key to keyring '/home/jc/.config/guix/upstream/trustedkeys.kbx'?
    yes
    Backtrace:
      [...]
    guix/ui.scm:921:18: Wrong type (expecting string): #f

With this commit, instead it gives a more actionable error:

    From https://poppler.freedesktop.org/poppler-26.05.0.tar.xz.sig...
     ….0.tar.xz.sig  833B                 664KiB/s 00:00 ▕██████████████████▏ 100.0%
    guix refresh: error: spawn: No such file or directory: "gpgv"

* guix/gnupg.scm (gnupg-verify): Use `spawn` to spawn `gpgv`, which
  raises if `gpgv` is missing.
  (gnupg-status-good-signature?): Likewise with `gpg`.
* guix/ui.scm <top level>: Change `spawn` to use an error-reporting
  wrapper which includes the executable name in errors.

Change-Id: I5add4defe78fd9b2b3f07ee0595ed34cbb00e3e8
Signed-off-by: Ludovic Courtès <ludo@gnu.org>
Merges: #8239

guix/ui.scm
@@ -750,6 +750,9 @@ FILE."
 (set! mkdir
   (error-reporting-wrapper mkdir (directory . args) directory))
 
+(set! spawn
+  (error-reporting-wrapper spawn (filename . args) filename))
+
 (define (make-regexp* regexp . flags)
   "Like 'make-regexp' but error out if REGEXP is invalid, reporting the error
 nicely."

guix/gnupg.scm
@@ -154,13 +154,14 @@ revoked.  Return a status s-exp if GnuPG failed."
           (loop (read-line input)
                 (cons (status-line->sexp line) result)))))
 
-  (let* ((pipe   (open-pipe* OPEN_READ (%gpgv-command) "--status-fd=1"
-                             "--keyring" keyring sig file))
-         (status (parse-status pipe)))
-    ;; Ignore PIPE's exit status since STATUS above should contain all the
-    ;; info we need.
-    (close-pipe pipe)
-    status))
+  (let* ((input+output (pipe))
+         (gpgv (%gpgv-command))
+         (gpgv-flags (list gpgv "--status-fd=1" "--keyring"
+                           keyring sig file))
+         (pid (spawn (%gpgv-command) gpgv-flags
+                     #:output (cdr input+output))))
+    (close-port (cdr input+output))
+    (parse-status (car input+output))))
 
 (define (gnupg-status-good-signature? status)
   "If STATUS, as returned by `gnupg-verify', denotes a good signature, return
@@ -193,13 +194,15 @@ GnuPG's default/configured one.  The key is added to KEYRING."
     (mkdir-p (dirname keyring))
     (call-with-output-file keyring (const #t))) ;create an empty keybox
 
-  (zero? (apply system*
-                `(,(%gpg-command)
-                  ,@(if server
-                        (list "--keyserver" server)
-                        '())
-                  "--no-default-keyring" "--keyring" ,keyring
-                  "--recv-keys" ,fingerprint/key-id))))
+  (let* ((keyserver-flags (if server '("--keyserver" server) '()))
+         (default-flags (list "--no-default-keyring"
+                              "--keyring" keyring
+                              "--recv-keys" fingerprint/key-id))
+         (gpg-flags (append keyserver-flags default-flags))
+         (pid (spawn (%gpg-command) gpg-flags)))
+        (match (waitpid pid)
+          ((_ . status)
+           (zero? status)))))
 
 (define* (gnupg-verify* sig file
                         #:key

commit 0099dbafd40bf4d71884ce2d709fef1edac18f53

ui: Modify ‘spawn’ only when it is defined.
This fixes a regression introduced in a6094158aa4fb8ec217e41c000264ba6e2095a3b
whereby upgrading from an old Guix revision, running on Guile version earlier
than 3.0.9, would break while building ‘module-import-compiled.drv’.

* guix/ui.scm <top level>: Set ‘spawn’ only when it’s defined.

Reported-by: Yelninei <yelninei@tutamail.com>
Reported-by: Danny Milosavljevic <dannym@friendly-machines.com>
Fixes: guix/guix#9852
Fixes: guix/guix#10809
Signed-off-by: Ludovic Courtès <ludo@gnu.org>
Merges: #10830

guix/ui.scm
@@ -750,8 +750,11 @@ FILE."
 (set! mkdir
   (error-reporting-wrapper mkdir (directory . args) directory))
 
-(set! spawn
-  (error-reporting-wrapper spawn (filename . args) filename))
+;; 'spawn' appeared in Guile 3.0.9.  Address the situation where updating from
+;; an old Guix running on an older version of Guile lacking 'spawn'.
+(when (module-defined? the-scm-module 'spawn)
+  (set! spawn
+    (error-reporting-wrapper spawn (filename . args) filename)))
 
 (define (make-regexp* regexp . flags)
   "Like 'make-regexp' but error out if REGEXP is invalid, reporting the error