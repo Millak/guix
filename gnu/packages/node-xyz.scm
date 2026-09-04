b0ace93478c370ee67c8a4d19270d875d070fe80
Orahcio Felício de Sousa <orahcio@gmail.com>
2026-09-04 01:44:28 -0300

    gnu: node-chalk: Update to 4.1.2.
    * gnu/packages/node-xyz.scm (node-chalk): Update to 4.1.2.
    [inputs]: Move after arguments and reindent.
    Remove node-escape-string-regexp.
    
    Signed-off-by: Liliana Marie Prikler <liliana.prikler@gmail.com>
    Merges: guix/guix#11044

--- gnu/packages/node-xyz.scm
+++ gnu/packages/node-xyz.scm
@@ -517,26 +517,25 @@ buffer masking and unmasking operations.")
 (define-public node-chalk
   (package
     (name "node-chalk")
-    (version "2.4.2")
-    (source (origin
-      (method git-fetch)
-      (uri (git-reference
-        (url "https://github.com/chalk/chalk")
-        (commit (string-append "v" version))))
-      (file-name (git-file-name name version))
-      (sha256
-        (base32 "1isx2l8a11k5arz1p3kd7mbdb8kjr85l1axdm7yya57vwlkdkc2y"))))
+    (version "4.1.2")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+              (url "https://github.com/chalk/chalk")
+              (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "18dp8qy796xl8psnpwppas3z39jnpijc3gyln4mbnihdj7zps114"))))
     (build-system node-build-system)
-    (inputs (list
-      node-ansi-styles
-      node-supports-color
-      node-escape-string-regexp))
     (arguments (list
       #:tests? #f ; FIXME: Tests reuire 'xo', 'c8', 'ava', and 'tsd'.
       #:phases #~(modify-phases %standard-phases
         (add-before 'patch-dependencies 'modify-package (lambda _
           (modify-json
             (delete-dev-dependencies)))))))
+    (inputs (list node-ansi-styles
+                  node-supports-color))
     (synopsis "Terminal string styling library")
     (description "Chalk comes with an easy to use composable API where you just chain and\
  nest the styles you want.")
