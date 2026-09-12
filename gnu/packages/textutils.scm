bf4417909923f69b96f95819ffcd102ac390f25e
Sören Tempel <soeren+git@soeren-tempel.net>
2026-09-12 14:30:23 +0200

    gnu: libxo: Update to 2.1.0.
    * gnu/packages/textutils.scm (libxo) Update to 2.1.0.
    [arguments] <#:phases>:: Remove 'process-javascript phase.
    [inputs]: Remove labels, remove js-jquery, remove js-qtip2, remove js-qtip2.css.
    [license]: Adjust to account for removed js/css files.
    [native-inputs]: Remove labels, add byacc.

--- gnu/packages/textutils.scm
+++ gnu/packages/textutils.scm
@@ -77,6 +77,7 @@
   #:use-module (gnu packages base)
   #:use-module (gnu packages check)
   #:use-module (gnu packages certs)
+  #:use-module (gnu packages compiler-tools)
   #:use-module (gnu packages compression)
   #:use-module (gnu packages gcc)
   #:use-module (gnu packages gettext)
@@ -322,7 +323,7 @@ the Hannon Lab.")
 (define-public libxo
   (package
     (name "libxo")
-    (version "1.7.5")
+    (version "2.1.0")
     (source
      (origin
        (method git-fetch)
@@ -331,65 +332,14 @@ the Hannon Lab.")
              (commit version)))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "115jv067msym0lsxkiz95ddvspd6smvww37248xkqyin0rxb2m0j"))
-       (snippet
-         '(for-each
-            delete-file
-            '("xohtml/external/jquery.js"
-              "xohtml/external/jquery.qtip.css"
-              "xohtml/external/jquery.qtip.js")))))
+        (base32 "19dmhds6yz8bfdgfbibmzpwn324g0s4f8lca4zm4v9jh6ah8jmwr"))))
     (build-system gnu-build-system)
-    (arguments
-     (list
-      #:modules '((guix build gnu-build-system)
-                  (guix build minify-build-system)
-                  (guix build utils)
-                  (ice-9 match))
-      #:imported-modules
-      `(,@%default-gnu-imported-modules
-         (guix build minify-build-system))
-      #:phases
-      '(modify-phases (@ (guix build gnu-build-system) %standard-phases)
-         (add-after 'unpack 'process-javascript
-           (lambda* (#:key inputs #:allow-other-keys)
-             (with-directory-excursion "xohtml/external"
-               (for-each (match-lambda
-                           ((source . target)
-                            (minify source #:target target)))
-                         `((,(assoc-ref inputs "js-jquery")
-                            . "jquery.js")
-                           (,(assoc-ref inputs "js-qtip2")
-                            . "jquery.qtip.js")
-                           (,(assoc-ref inputs "js-qtip2.css")
-                            . "jquery.qtip.css")))))))))
-    (inputs
-      `(("perl", perl)
-        ("js-jquery"
-         ,(origin
-            (method url-fetch)
-            (uri "https://code.jquery.com/jquery-1.7.0.js")
-            (sha256
-             (base32
-              "1qz3jmzg5ac6nkgivzyjgd7rr3vvkwzqv525s080mx10hvn8a63w"))))
-        ("js-qtip2"
-         ,(origin
-            (method url-fetch)
-            (uri "https://cdnjs.cloudflare.com/ajax/libs/qtip2/2.1.1/jquery.qtip.js")
-            (sha256
-             (base32
-              "05aq2x5hd8jnpnhkn41ivsbdg3rqmk093bb2vqzswbihvsizyx64"))))
-        ("js-qtip2.css"
-         ,(origin
-            (method url-fetch)
-            (uri "https://cdnjs.cloudflare.com/ajax/libs/qtip2/2.1.1/jquery.qtip.css")
-            (sha256
-             (base32
-              "18h10klb84jqpasvjymlaskj0442wcain434iwydikpdxmrr7g0n"))))))
-    (native-inputs
-     `(("autoconf" ,autoconf)
-       ("automake" ,automake)
-       ("esbuild" ,esbuild)
-       ("libtool" ,libtool)))
+    (inputs (list perl))
+    (native-inputs (list autoconf
+                         automake
+                         byacc
+                         esbuild
+                         libtool))
     (home-page "https://juniper.github.io/libxo/libxo-manual.html")
     (synopsis "Library for Generating Text, XML, JSON, and HTML Output")
     (description
@@ -398,8 +348,7 @@ JSON, and HTML output using a common set of function calls.  The caller
 decides at run time which output style should be produced.  Afterwards, the caller
 calls the function @code{xo_emit} to produce output in a format described using
 a format string.")
-    ;; license:expat: jquery and qtip2.
-    (license (list license:bsd-2 license:expat))))
+    (license license:bsd-2)))
 
 (define-public cityhash
   (let ((commit "8af9b8c"))
