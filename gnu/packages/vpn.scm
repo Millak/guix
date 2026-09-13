61c147ba3540d8161b15311a16de2810f1ccaba7
Cayetano Santos <csantosb@disroot.org>
2026-09-13 15:24:11 +0200

    gnu: amneziawg-linux-kernel-module: Update to 3.1.20260906.
    * gnu/packages/vpn.scm (amneziawg-linux-kernel-module): Update to
    3.1.20260906.
    
    Merges guix/guix!11218

--- gnu/packages/vpn.scm
+++ gnu/packages/vpn.scm
@@ -111,7 +111,7 @@
 (define-public amneziawg-linux-kernel-module
   (package
     (name "amneziawg-linux-kernel-module")
-    (version "1.0.20260329")
+    (version "3.1.20260906")
     (source
      (origin
        (method git-fetch)
@@ -121,7 +121,7 @@
              (commit (string-append "v" version))))
        (file-name (git-file-name name version))
        (sha256
-        (base32 "0ygy87cdmxjb7r6sipkniqi3m0952la2sssissbscnlr6sc6rjs2"))))
+        (base32 "10iniwala2h6r05rjmyrrvy019xvd3k4s5kz8ass228ga0gi248c"))))
     (build-system linux-module-build-system)
     (arguments
      (list
