ef8b13e85896b03cd7258a9fc0b8890cfed1b37a
Vagrant Cascadian <vagrant@debian.org>
2026-09-13 22:50:17 -0700

    gnu: linux-libre 6.18: Update to 6.18.51.
    * gnu/packages/linux.scm (linux-libre-6.18-version): Update to 6.18.51.
    (linux-libre-6.18-pristine-source): Update hash.

--- gnu/packages/linux.scm
+++ gnu/packages/linux.scm
@@ -626,7 +626,7 @@ GEXP)."
 ;; Here are the support timelines:
 ;; <https://www.kernel.org/category/releases.html>
 
-(define-public linux-libre-6.18-version "6.18.49")
+(define-public linux-libre-6.18-version "6.18.51")
 (define-public linux-libre-6.18-gnu-revision "gnu")
 (define deblob-scripts-6.18
   (linux-libre-deblob-scripts
@@ -636,7 +636,7 @@ GEXP)."
    (base32 "0yk2c643d2gykwbl3cibgwr9aaqrfg9m3q9xgha98yy4vzcyqbhr")))
 (define-public linux-libre-6.18-pristine-source
   (let ((version linux-libre-6.18-version)
-        (hash (base32 "14c1l9hbqcjlzfl7bssa43yqsg26sycp5plx4wfnzshz24rnz0mf")))
+        (hash (base32 "04znimsjfimxxaarzaksn4jvkj1xr5ba7yh1j691ykdzb3w60bxs")))
    (make-linux-libre-source version
                             (%upstream-linux-source version hash)
                             deblob-scripts-6.18)))
