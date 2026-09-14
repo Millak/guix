01d1522e9743d0c6bbe8af05cf89cad2d9c9dd68
Sharlatan Hellseher <sharlatanus@gmail.com>
2026-09-14 13:40:45 +0100

    gnu: Add go-github-com-cloudfoundry-jibber-jabber.
    * gnu/packages/golang-xyz.scm (go-github-com-cloudfoundry-jibber-jabber): New variable.

--- gnu/packages/golang-xyz.scm
+++ gnu/packages/golang-xyz.scm
@@ -6823,6 +6823,32 @@ strings and byte slices without conversions.")
 sentences and graphemes.")
     (license license:expat)))
 
+(define-public go-github-com-cloudfoundry-jibber-jabber
+  (package
+    (name "go-github-com-cloudfoundry-jibber-jabber")
+    (version "0.0.0-20151120183258-bcc4c8345a21")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+              (url "https://github.com/cloudfoundry-attic/jibber_jabber")
+              (commit (go-version->git-ref version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "0q31q03sxfwrdgbv559bgm9gr5cmyzp1al0zli9nlkwa2v9hw5fi"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:import-path "github.com/cloudfoundry/jibber_jabber"))
+    (native-inputs
+     (list go-github-com-onsi-ginkgo))
+    (home-page "https://github.com/cloudfoundry/jibber_jabber")
+    (synopsis "icale detection for Golang")
+    (description
+     "Jibber Jabber is a Go Library that can be used to detect an operating
+system's current language.")
+    (license license:asl2.0)))
+
 (define-public go-github-com-cloudwego-iasm
   (package
     (name "go-github-com-cloudwego-iasm")
