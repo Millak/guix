c6ee5fda193c9a1443af2b91663a91f64d372581
jgart <jgart@dismail.de>
2026-09-15 23:24:24 +0200

    gnu: trealla: Update to 3.10.7.
    * gnu/packages/prolog.scm (trealla): Update to 3.10.7.

--- gnu/packages/prolog.scm
+++ gnu/packages/prolog.scm
@@ -237,7 +237,7 @@ it.")
 (define-public trealla
   (package
     (name "trealla")
-    (version "3.10.4")
+    (version "3.10.7")
     (source
      (origin
        (method git-fetch)
@@ -246,7 +246,7 @@ it.")
           (url "https://github.com/trealla-prolog/trealla")
           (commit (string-append "v" version))))
        (sha256
-        (base32 "1wd4gpdqivjacmgaj5cjqhhi784xbq4d3ll9bx6mm6khazjzqhf8"))
+        (base32 "17zghmd7icw69yp7whyabgbgrx7gj4yxzvr01b2w2qsffqjajcfy"))
        (file-name (git-file-name name version))))
     (build-system gnu-build-system)
     (inputs
