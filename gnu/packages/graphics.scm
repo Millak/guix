Millak/guix

commit 9ab31b265cd978019db0877234cd6c624f9076d1

gnu: implot: Update to 1.0.
* gnu/packages/toolkits.scm (implot): Update to 1.0.
[inputs]: Replace imgui-1.91 with imgui.
(implot-0): New variable.
* gnu/packages/graphics.scm (mangohud) [inputs]: Replace implot with implot-0.

gnu/packages/graphics.scm
@@ -1564,7 +1564,7 @@ distills complex, animated scenes into a set of baked geometric results.")
            glslang
            hwdata
            imgui-for-mangohud
-           implot
+           implot-0
            libx11
            libxkbcommon
            mesa

gnu/packages/toolkits.scm
@@ -3,7 +3,7 @@
 ;;; Copyright © 2020, 2022 Tobias Geerinckx-Rice <me@tobias.gr>
 ;;; Copyright © 2022 Andy Tai <atai@atai.org>
 ;;; Copyright © 2022, 2023 John Kehayias <john.kehayias@protonmail.com>
-;;; Copyright © 2022, 2023 Maxim Cournoyer <maxim@guixotic.coop>
+;;; Copyright © 2022-2023, 2026 Maxim Cournoyer <maxim@guixotic.coop>
 ;;; Copyright © 2023 Simon South <simon@simonsouth.net>
 ;;;
 ;;; This file is part of GNU Guix.
@@ -277,7 +277,7 @@ used either in header only mode or in implementation mode.")
 (define-public implot
   (package
     (name "implot")
-    (version "0.16")
+    (version "1.0")
     (source (origin
               (method git-fetch)
               (uri (git-reference
@@ -286,7 +286,7 @@ used either in header only mode or in implementation mode.")
               (file-name (git-file-name name version))
               (sha256
                (base32
-                "0vbsw1qsi7j6pdz77840pxcqcghcl9fld80hchajbhpp1jr1a2gz"))))
+                "1pcq6nn82qpkwk27kyiaxky4pnaz9kj3fc3q25laj154r83ppikl"))))
     (build-system gnu-build-system)
     (arguments
      (list
@@ -313,8 +313,7 @@ used either in header only mode or in implementation mode.")
                                                        "/include/implot")))
                         '("implot.h" "implot_internal.h")))))
       #:tests? #f))                     ; no test suite
-    (inputs
-     (list imgui-1.91))
+    (inputs (list imgui))
     (home-page "https://github.com/epezent/implot")
     (synopsis "Immediate-mode C++ plotting library for ImGui")
     (description "ImPlot is an immediate-mode, GPU-accelerated plotting
@@ -322,3 +321,18 @@ library in C++ for the ImGui GUI library, suitable for creating real-time
 visualizations or interactive plots.  Like ImGui, ImPlot is self-contained and
 requires only minimal code to integrate with existing applications.")
     (license license:expat)))
+
+(define-public implot-0
+  (package
+    (inherit implot)
+    (name "implot")
+    (version "0.17")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                     (url "https://github.com/epezent/implot")
+                     (commit (string-append "v" version))))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "1rp4lxgr11yc3z3cmlrpyaf916n74wp7401r21v7xbybfd2cvp0w"))))))

commit f0af43359e0cbaaacd5cac136a5e90b56528fb24

gnu: mangohud: Fix execution and enable/add tests.
This is a follow-up to commit 9ab31b265cd978019d ("gnu: implot: Update to
1.0."), which broke run time.

* gnu/packages/toolkits.scm (implot-for-mangohud): New variable.
* gnu/packages/graphics.scm (mangohud) [inputs]: Replace implot-0 with
implot-for-mangohud.

Reported-by: John Kehayias <john@guixotic.coop>

gnu/packages/graphics.scm
@@ -1463,6 +1463,8 @@ other vector formats such as:
 distills complex, animated scenes into a set of baked geometric results.")
     (license license:bsd-3)))
 
+;;; This package lacks a proper test suite.  To test manually:
+;;; ./pre-inst-env guix shell mangohud vulkan-tools -- mangohud vkcube
 (define-public mangohud
   (package
     (name "mangohud")
@@ -1542,7 +1544,7 @@ distills complex, animated scenes into a set of baked geometric results.")
            glslang
            hwdata
            imgui-for-mangohud
-           implot-0
+           implot-for-mangohud
            libx11
            libxkbcommon
            mesa

gnu/packages/toolkits.scm
@@ -350,3 +350,9 @@ requires only minimal code to integrate with existing applications.")
               (sha256
                (base32
                 "1rp4lxgr11yc3z3cmlrpyaf916n74wp7401r21v7xbybfd2cvp0w"))))))
+
+(define-public implot-for-mangohud
+  (package
+    (inherit implot-0)
+    (inputs (modify-inputs inputs
+              (replace "imgui" imgui-for-mangohud)))))